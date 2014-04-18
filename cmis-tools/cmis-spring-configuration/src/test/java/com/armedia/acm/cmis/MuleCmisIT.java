451d5098997f2829bd8e5e92d49cb2c22caf5d29
David Miller <david.miller@armedia.com>
2014-04-18 14:20:18 -0400

    AFDP-65 - Business Rules: Alfresco folder structure - create the folder in Alfresco.

---
+++ cmis-tools/cmis-spring-configuration/src/test/java/com/armedia/acm/cmis/MuleCmisIT.java
@@ -0,0 +1,37 @@
+package com.armedia.acm.cmis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "/spring/spring-library-mule-context-manager.xml"
+})
+public class MuleCmisIT
+{
+    @Autowired
+    private MuleClient muleClient;
+
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void createOrFindFolder() throws Exception
+    {
+        String path = "/Sites/acm/documentLibrary/Complaints/testComplaint";
+
+        MuleMessage reply = muleClient.send("vm://createFolder.in", path, null);
+
+        log.info("Reply payload of type: " + reply.getPayload().getClass().getName());
+
+
+
+    }
+}
