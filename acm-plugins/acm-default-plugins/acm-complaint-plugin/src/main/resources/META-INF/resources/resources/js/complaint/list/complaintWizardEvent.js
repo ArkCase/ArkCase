551b6dec1ed521815a0bc16c358521fd5d9531fc
James Wu <jwu@armedia.com>
2014-04-30 10:10:45 -0400

    Conflict? What conflict?

---
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/resources/META-INF/resources/resources/js/complaint/list/complaintWizardEvent.js
@@ -0,0 +1,126 @@
+/**
+ * ComplaintWizard.Event
+ *
+ * event handlers for objects
+ *
+ * @author jwu
+ */
+ComplaintWizard.Event = {
+    initialize : function() {
+    }
+
+    ,onClickBtnSave : function(btn) {
+        var data0 =
+        {
+            //"complaintId": null,
+            "originator": {
+                //"id": null,
+                "title": "Mr.",
+                "givenName": "John",
+                "familyName": "Doe",
+                "company": "Contoso"
+//                ,"addresses": [
+//                    {
+//                        "type": "home",
+//                        "streetAddress": "123 Main St.",
+//                        "streetAddress2": null,
+//                        "city": "Peoria",
+//                        "state": "IL",
+//                        "zip": "12345"
+//                    }
+//                ]
+//                ,"contactMethods": [
+//                    {
+//                        "type": "Mobile Phone",
+//                        "value": "123-456-7890"
+//                    },
+//                    {
+//                        "type": "Personal E-mail",
+//                        "value": "john.doe@gmail.com"
+//                    }
+//                ],
+//                "securityTags": ["Anonymous", "Confidential", "Top Secret"]
+            }
+        };
+
+        var data = ComplaintWizard.Object.getComplaintData();
+        ComplaintWizard.Service.createComplaint(data);
+    }
+
+    ,test : function(btn) {
+
+
+        var url = "/acm" + "/api/latest/plugin/complaint";
+        var param =
+        {
+            //"complaintId": null,
+            "originator": {
+            //"id": null,
+                "title": "Mr.",
+                "givenName": "John",
+                "familyName": "Doe",
+                "company": "Contoso",
+                "addresses": [
+                {
+                    "type": "home",
+                    "streetAddress": "123 Main St.",
+                    "streetAddress2": null,
+                    "city": "Peoria",
+                    "state": "IL",
+                    "zip": "12345"
+                }
+            ],
+                "contactMethods": [
+                {
+                    "type": "Mobile Phone",
+                    "value": "123-456-7890"
+                },
+                {
+                    "type": "Personal E-mail",
+                    "value": "john.doe@gmail.com"
+                }
+            ],
+                "securityTags": ["Anonymous", "Confidential", "Top Secret"]
+        }
+        };
+
+        var paramSmall =
+        {
+            "originator": {
+                "title": "Mr.",
+                "givenName": "John",
+                "familyName": "Doe",
+                "company": "Contoso"
+            }
+        };
+
+        $.ajax({type: 'POST'
+            ,url: url
+            ,async: true
+            ,data: JSON.stringify(paramSmall)
+            ,dataType: 'json'
+            ,contentType: "application/json; charset=utf-8"
+            //,processData: false
+            ,beforeSend: function(x) {
+                if (x && x.overrideMimeType) {
+                    x.overrideMimeType("application/json;charset=UTF-8");
+                }
+            }
+            ,success: function(response) {
+                alert("ajax success");
+            }
+            ,error: function(xhr, status, error) {
+                var msg = xhr.responseText;
+                alert("ajax error:" + msg);
+            }
+            ,complete: function (xhr, status)
+            {
+                //alert("ajax complete");
+            }
+        });
+
+    }
+
+    ,onPostInit: function() {
+    }
+};
