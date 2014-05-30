230259d3bbfae80e7449e97321345b0652d13c7d
David Miller <david.miller@armedia.com>
2014-05-30 15:13:23 -0400

    AFDP-120 - Add approvers to business process - raise application events when any business process is started or stopped.

---
+++ activiti-tools/activiti-spring-configuration/src/main/java/com/armedia/acm/activiti/ActivitiProcessEventHandler.java
@@ -0,0 +1,52 @@
+package com.armedia.acm.activiti;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+
+import java.util.Map;
+
+/**
+ * Created by armdev on 5/30/14.
+ */
+public class ActivitiProcessEventHandler implements ApplicationEventPublisherAware
+{
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private RuntimeService runtimeService;
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    // IDEA says this method is not called; but actually it is called from Activiti.  Do NOT delete this method.
+    public void handleProcessEvent(String eventName, ProcessInstance execution)
+    {
+        log.info("Got an Activiti event; eventName: " + eventName + "; " +
+            "process instance id: " + execution.getProcessInstanceId());
+
+        Map<String, Object> processVariables = getRuntimeService().getVariables(execution.getId());
+
+        AcmBusinessProcessEvent event = new AcmBusinessProcessEvent(execution);
+        event.setEventType("com.armedia.acm.activiti.business-process." + eventName);
+        event.setProcessVariables(processVariables);
+
+        applicationEventPublisher.publishEvent(event);
+
+    }
+
+    public RuntimeService getRuntimeService()
+    {
+        return runtimeService;
+    }
+
+    public void setRuntimeService(RuntimeService runtimeService)
+    {
+        this.runtimeService = runtimeService;
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher)
+    {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+}
