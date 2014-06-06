c2f942bcd5873f227fd9cc967d235488ffc9e195
David Miller <david.miller@armedia.com>
2014-06-06 11:06:04 -0400

    AFDP-130 - Update complaint status based on Activiti events

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/dao/ComplaintDao.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/dao/ComplaintDao.java
@@ -3,11 +3,14 @@ package com.armedia.acm.plugins.complaint.dao;
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.plugins.complaint.model.Complaint;
 import com.armedia.acm.plugins.complaint.model.ComplaintListView;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,6 +34,23 @@ public class ComplaintDao extends AcmAbstractDao<Complaint>
 
         return results;
 
+    }
+
+    @Transactional
+    public int updateComplaintStatus(Long complaintId, String newStatus, String modifier)
+    {
+        Query updateStatusQuery = getEm().createQuery(
+                "UPDATE Complaint " +
+                        "SET status = :newStatus, " +
+                        "modified = :modified, " +
+                        "modifier = :modifier " +
+                        "WHERE complaintId = :complaintId");
+        updateStatusQuery.setParameter("newStatus", newStatus);
+        updateStatusQuery.setParameter("modified", new Date());
+        updateStatusQuery.setParameter("modifier", modifier);
+        updateStatusQuery.setParameter("complaintId", complaintId);
+
+        return updateStatusQuery.executeUpdate();
     }
 
 }
