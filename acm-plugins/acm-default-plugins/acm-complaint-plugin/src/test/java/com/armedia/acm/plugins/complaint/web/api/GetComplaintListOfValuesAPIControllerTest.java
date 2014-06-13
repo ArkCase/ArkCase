bf85f67ce999a2b238a4670dada78a489376530d
David Miller <david.miller@armedia.com>
2014-06-13 10:45:43 -0400

    AFDP-139 - Convert MVC error handler to the Spring MVC @ExceptionHandler mechanism - list tasks controller tests - error handling for Complaint List of Values Controller

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/web/api/GetComplaintListOfValuesAPIControllerTest.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/web/api/GetComplaintListOfValuesAPIControllerTest.java
@@ -6,29 +6,42 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.easymock.EasyMockSupport;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.security.core.Authentication;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-/**
- * Created by armdev on 4/28/14.
- */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:/spring/spring-web-acm-web.xml",
+        "classpath:/spring/spring-library-complaint-plugin-test.xml"
+})
 public class GetComplaintListOfValuesAPIControllerTest extends EasyMockSupport
 {
     private MockMvc mockMvc;
     private Authentication mockAuthentication;
+
+    @Autowired
+    private ExceptionHandlerExceptionResolver exceptionResolver;
 
     private GetComplaintListOfValuesAPIController unit;
 
@@ -44,7 +57,8 @@ public class GetComplaintListOfValuesAPIControllerTest extends EasyMockSupport
     {
         unit = new GetComplaintListOfValuesAPIController();
 
-        mockMvc = MockMvcBuilders.standaloneSetup(unit).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(unit).setHandlerExceptionResolvers(exceptionResolver).build();
+
         mockListOfValuesService = createMock(ListOfValuesService.class);
         mockAuthentication = createMock(Authentication.class);
 
@@ -93,6 +107,27 @@ public class GetComplaintListOfValuesAPIControllerTest extends EasyMockSupport
     }
 
     @Test
+    public void getComplaintTypes_exception() throws Exception
+    {
+        expect(mockListOfValuesService.lookupListOfStringValues(typeDescriptor)).andThrow(new CannotGetJdbcConnectionException(
+                "testException", new SQLException("testException")));
+
+        // MVC test classes must call getName() somehow
+        expect(mockAuthentication.getName()).andReturn("user");
+
+        replayAll();
+
+        mockMvc.perform(
+                get("/api/latest/plugin/complaint/types")
+                        .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                        .principal(mockAuthentication))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN));
+
+        verifyAll();
+    }
+
+    @Test
     public void getComplaintPriorities() throws Exception
     {
         List<String> priorityList = Arrays.asList("Cold", "Medium", "Hot");
@@ -126,6 +161,27 @@ public class GetComplaintListOfValuesAPIControllerTest extends EasyMockSupport
         assertEquals(3, priorities.length);
 
 
+    }
+
+    @Test
+    public void getComplaintPriorities_exception() throws Exception
+    {
+        expect(mockListOfValuesService.lookupListOfStringValues(priorityDescriptor)).andThrow(new CannotGetJdbcConnectionException(
+                "testException", new SQLException("testException")));
+
+        // MVC test classes must call getName() somehow
+        expect(mockAuthentication.getName()).andReturn("user");
+
+        replayAll();
+
+        mockMvc.perform(
+                get("/api/latest/plugin/complaint/priorities")
+                        .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                        .principal(mockAuthentication))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN));
+
+        verifyAll();
     }
 
 }
