ab0e4365a19ac1733ad9fc438ac5b17a437e6ed0
David Miller <david.miller@armedia.com>
2014-06-13 16:16:14 -0400

    AFDP-139 - Convert MVC error handler to the Spring MVC @ExceptionHandler mechanism - find all complaints error handling

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/web/api/FindAllComplaintsAPIController.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/web/api/FindAllComplaintsAPIController.java
@@ -1,8 +1,11 @@
 package com.armedia.acm.plugins.complaint.web.api;
 
+import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
 import com.armedia.acm.plugins.complaint.dao.ComplaintDao;
 import com.armedia.acm.plugins.complaint.model.ComplaintListView;
 import com.armedia.acm.plugins.complaint.service.ComplaintEventPublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -20,22 +24,32 @@ public class FindAllComplaintsAPIController
     private ComplaintDao complaintDao;
     private ComplaintEventPublisher eventPublisher;
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<ComplaintListView> retrieveListOfComplaints(
             Authentication authentication,
             HttpSession session
-    )
+    ) throws AcmListObjectsFailedException
     {
         String ipAddress = (String) session.getAttribute("acm_ip_address");
 
-        List<ComplaintListView> complaints = getComplaintDao().listAllComplaints();
-
-        for ( ComplaintListView clv : complaints )
+        try
         {
-            getEventPublisher().publishComplaintSearchResultEvent(clv, authentication, ipAddress);
+            List<ComplaintListView> complaints = getComplaintDao().listAllComplaints();
+
+            for ( ComplaintListView clv : complaints )
+            {
+                getEventPublisher().publishComplaintSearchResultEvent(clv, authentication, ipAddress);
+            }
+            return complaints;
         }
-        return complaints;
+        catch (PersistenceException e)
+        {
+            log.error("Could not list complaints: " + e.getMessage(), e);
+            throw new AcmListObjectsFailedException("complaint", e.getMessage(), e);
+        }
     }
 
 
