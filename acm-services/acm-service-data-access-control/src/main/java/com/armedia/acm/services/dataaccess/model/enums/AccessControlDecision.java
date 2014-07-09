1ae751d94e3ae93869bd83b397ad198c182e375c
David Miller <david.miller@armedia.com>
2014-07-09 11:28:07 -0400

    AFDP-166 - Default ACL store and retrieve service - generator to create default access control entries from the AcmApplication object

---
+++ acm-services/acm-service-data-access-control/src/main/java/com/armedia/acm/services/dataaccess/model/enums/AccessControlDecision.java
@@ -0,0 +1,13 @@
+package com.armedia.acm.services.dataaccess.model.enums;
+
+/**
+ * Created by armdev on 7/9/14.
+ */
+public enum AccessControlDecision
+{
+    MANDATORY_GRANT,
+    MANDATORY_DENY,
+    GRANT,
+    DENY
+
+}
