84cd301123b98ee42e73b6d4b7032668283e88a5
tsedalugebeyehu <tsedalu.gebeyehu@ARM-T530-0170.armedia.com>
2014-07-31 16:09:39 -0400

    AFDP-221: Service:Allow people to have aliases

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/Person.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/Person.java
@@ -2,9 +2,11 @@ package com.armedia.acm.plugins.person.model;
 
 import com.armedia.acm.plugins.addressable.model.ContactMethod;
 import com.armedia.acm.plugins.addressable.model.PostalAddress;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static javax.persistence.CascadeType.ALL;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -15,15 +17,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by armdev on 4/7/14.
@@ -92,6 +93,9 @@ public class Person implements Serializable
     )
     @Column(name = "cm_security_tag")
     private List<String> securityTags = new ArrayList<>();
+    
+    @OneToMany(cascade=ALL, mappedBy="person")   
+    private List<PersonAlias> personAlias = new ArrayList<>();
 
     @PrePersist
     protected void beforeInsert()
@@ -268,9 +272,18 @@ public class Person implements Serializable
     {
         return securityTags;
     }
-
+    
     public void setSecurityTags(List<String> securityTags)
     {
         this.securityTags = securityTags;
     }
+
+    public List<PersonAlias> getPersonAlias() {
+        return personAlias;
+    }
+
+    public void setPersonAlias(List<PersonAlias> personAlias) {
+        this.personAlias = personAlias;
+    }
+
 }
