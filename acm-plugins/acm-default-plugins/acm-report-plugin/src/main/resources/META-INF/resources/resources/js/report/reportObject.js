423045b396233d400c8c5b5c39855686180b4a63
Manoj Dhungana <manojdhungana@gmail.com>
2014-08-06 12:59:04 -0400

    UI:Create Report Panel. AFDP-293

--- acm-plugins/acm-default-plugins/acm-report-plugin/src/main/resources/META-INF/resources/resources/js/report/reportObject.js
+++ acm-plugins/acm-default-plugins/acm-report-plugin/src/main/resources/META-INF/resources/resources/js/report/reportObject.js
@@ -7,10 +7,37 @@
  */
 Report.Object = {
     initialize : function() {
-        this.$btnTest = $("#test");
-        this.$btnTest.click(function(e) {Report.Event.onClickBtnTest(e);});
-    }
+        this.$selReport = $("#selectReport");
 
+        this.$edtStartDate = $("#startDate");
+        //this.$edtStartDate(Acm.getCurrentDay());
+        this.setValueStartDate(Acm.getCurrentDay());
+
+        this.$edtEndDate = $("#endDate");
+        this.setValueEndDate(Acm.getCurrentDay());
+
+
+        this.$btnGenerateReport = $("#generateReport");
+        this.$btnGenerateReport.click(function(e) {Report.Event.onClickBtnGenerateReport(e);});
+
+        //this.$btnTest = $("#test");
+        //this.$btnTest.click(function(e) {Report.Event.onClickBtnTest(e);});
+    }
+    ,getValueStartDate: function() {
+        return Acm.Object.getPlaceHolderInput(this.$edtStartDate);
+    }
+    ,getValueEndDate: function() {
+        return Acm.Object.getPlaceHolderInput(this.$edtEndDate);
+    }
+    ,getSelectedValueSelReport: function() {
+        return Acm.Object.getSelectValueIgnoreFirst(this.$selReport);
+    }
+    ,setValueStartDate: function(val) {
+        return Acm.Object.setPlaceHolderInput(this.$edtStartDate, val);
+    }
+    ,setValueEndDate: function(val) {
+        return Acm.Object.setPlaceHolderInput(this.$edtEndDate, val);
+    }
 };
 
 
