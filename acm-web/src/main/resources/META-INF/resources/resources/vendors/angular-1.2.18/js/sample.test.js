6c89069cd1d4b9fa0bb11f6508bc1ec2c03b8c38
marjan.stefanoski <marjan.stefanoski@armedia.com>
2014-08-22 15:55:55 +0200

    url for myCompliants fixed

--- acm-web/src/main/resources/META-INF/resources/resources/vendors/angular-1.2.18/js/sample.test.js
+++ acm-web/src/main/resources/META-INF/resources/resources/vendors/angular-1.2.18/js/sample.test.js
@@ -4826,7 +4826,7 @@ angular.module("ui.bootstrap", ["ui.bootstrap.transition", "ui.bootstrap.collaps
     }
 ]).controller("myComplaintsCtrl", ["$scope", "$filter", "$http", "ngTableParams",
     function($scope, $filter, $http, ngTableParams) {
-        var url = App.Object.getContextPath() + "/api/latest/plugin/complaints/forUser/" + App.Object.getUserName();
+        var url = App.Object.getContextPath() + "/api/latest/plugin/complaint/forUser/" + App.Object.getUserName();
         $http.get(url).success(function(data) {
             $scope.tableParams = new ngTableParams({
                 page: 1,
