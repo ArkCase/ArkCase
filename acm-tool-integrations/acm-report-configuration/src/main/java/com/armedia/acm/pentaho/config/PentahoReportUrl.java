4790273e2cd2a10abf1ab0fdd107945bd84445f2
wudc <wudc@yahoo.com>
2014-08-28 14:54:31 -0400

    AFDP-407: Added Pentaho report integration implementation for the complaint report.

---
+++ acm-tool-integrations/acm-report-configuration/src/main/java/com/armedia/acm/pentaho/config/PentahoReportUrl.java
@@ -0,0 +1,65 @@
+package com.armedia.acm.pentaho.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.armedia.acm.report.config.ReportUrl;
+
+public class PentahoReportUrl implements ReportUrl{
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private static final String REPORT_SERVER_URL = "PENTAHO_SERVER_URL";
+    private static final String REPORT_SERVER_PORT = "PENTAHO_SERVER_PORT";
+
+    /**
+     * List of form-specific properties.
+     */
+    private Map<String, Object> reportsProperties;
+    
+	public Map<String, Object> getReportsProperties() {
+		return reportsProperties;
+	}
+
+	public void setReportsProperties(Map<String, Object> formProperties) {
+		System.out.println("formProperties: " + formProperties);
+		this.reportsProperties = formProperties;
+	}
+
+	@Override
+	public String getNewReportUrl(String reportName) {
+		return getReportUrlPath(reportName);
+	}
+
+	public String getReportURL() {
+        //form url data
+        StringBuilder builder = new StringBuilder();
+        String serverFormUrl = getReportsProperties().get(REPORT_SERVER_URL).toString();
+        builder.append(serverFormUrl);
+        String serverFormPort = getReportsProperties().get(REPORT_SERVER_PORT).toString();
+        builder.append(serverFormPort);
+        String url = builder.toString();
+        log.debug("getReportURL(): " + url);
+        return url;
+	}
+
+	/**
+	 * Url path for create a new complaint report form in Pentaho.
+	 * 
+	 * @return
+	 */
+	public String getReportUrlPath(String reportName) {
+        StringBuilder builder = new StringBuilder();
+        String serverFormUrl = getReportsProperties().get(REPORT_SERVER_URL).toString();
+        builder.append(serverFormUrl);
+        String serverFormPort = getReportsProperties().get(REPORT_SERVER_PORT).toString();
+        builder.append(serverFormPort);
+		String pathStr = getReportsProperties().get(reportName).toString();
+        builder.append(pathStr);
+		String path = builder.toString();
+        log.debug("getReportUrlPath(): " + path);
+		return path;
+	}
+
+}
