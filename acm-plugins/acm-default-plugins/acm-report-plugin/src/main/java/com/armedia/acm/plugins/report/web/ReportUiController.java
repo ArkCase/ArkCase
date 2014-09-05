c428845b839e55c952923d2229ec429c500dc316
wudc <wudc@yahoo.com>
2014-09-04 22:14:38 -0400

    AFDP-407: Refined the Report properties implementation and added unit tests.
    The updated implementation reads in whatever was in the properties and
    uses the values to populate the reports list. This implementation
    provides the flexibility to add and remove report urls from the
    properties file without requiring codes modifications.

--- acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/web/ReportUiController.java
+++ acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/web/ReportUiController.java
@@ -1,8 +1,5 @@
 package com.armedia.acm.plugins.report.web;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +15,6 @@ public class ReportUiController
     private Logger log = LoggerFactory.getLogger(getClass());
     private AcmPageDescriptor pageDescriptor;
 	private ReportUrl reportUrl;
-	private static final String COMPLAINT_REPORT = "COMPLAINT_REPORT";
-	private static final String BILLING_REPORT = "BILLING_REPORT";
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView showReportPage()
@@ -27,24 +22,9 @@ public class ReportUiController
         ModelAndView retval = new ModelAndView();
         retval.setViewName("report");
         retval.addObject("pageDescriptor", getPageDescriptor());
-        retval.addObject("reportUrlsMap", getReportUrlsMap());
+        retval.addObject("reportUrlsMap", reportUrl.getNewReportUrlList());
         
         return retval;
-    }
-
-    /**
-     * Private method to organize the report urls and stores them into a map.
-     * 
-     * @return
-     */
-    private Map<String, String> getReportUrlsMap() {
-        Map<String,String> urlsMap = new TreeMap<String, String>();
-        urlsMap.put("Complaint Report",  reportUrl.getNewReportUrl(COMPLAINT_REPORT));
-        urlsMap.put("Billing Report", reportUrl.getNewReportUrl(BILLING_REPORT));
-        
-        log.debug("Report urls: " + urlsMap);
-
-        return urlsMap;
     }
     
     public AcmPageDescriptor getPageDescriptor() {
