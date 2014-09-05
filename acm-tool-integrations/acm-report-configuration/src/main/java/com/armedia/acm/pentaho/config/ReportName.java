64bfc1a8fc3e6e08c886b3c2a4c1b184dec6e555
kjsgadh <kjsgadh@gmail.com>
2014-09-05 17:32:51 -0400

    Reporting changes for AFDP 267,340,341,343,344

--- acm-tool-integrations/acm-report-configuration/src/main/java/com/armedia/acm/pentaho/config/ReportName.java
+++ acm-tool-integrations/acm-report-configuration/src/main/java/com/armedia/acm/pentaho/config/ReportName.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum ReportName {
+	CLOSE_CASE_REPORT("Close Case Report"),
+	OPEN_CASE_REPORT("Open Case Report"),
 	COMPLAINT_REPORT("Complaint Report"),
 	BILLING_REPORT("Billing Report");
 
