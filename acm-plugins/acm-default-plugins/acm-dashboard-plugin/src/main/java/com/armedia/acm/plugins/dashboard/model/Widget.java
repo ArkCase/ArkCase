580f3271f1e9617aaa50f74cdefa14576cfec3ca
marjan.stefanoski <marjan.stefanoski@armedia.com>
2014-09-18 21:28:53 +0200

    Tables for role based dashboard created (acm_widget & acm_widget_role)

--- acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/model/Widget.java
+++ acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/model/Widget.java
@@ -1,40 +1,40 @@
-//package com.armedia.acm.plugins.dashboard.model;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import javax.persistence.*;
-//
-///**
-// * Created by marjan.stefanoski on 9/12/2014.
-// */
-//@Entity
-//@Table(name = "acm_widget")
-//public class Widget {
-//    private static final long serialVersionUID = -1154137631399833851L;
-//    private transient final Logger log = LoggerFactory.getLogger(getClass());
-//
-//    @Id
-//    @Column(name = "cm_widget_id")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long widgetId;
-//
-//    @Column(name = "cm_widget_name")
-//    private String widgetName;
-//
-//    public Long getWidgetId() {
-//        return widgetId;
-//    }
-//
-//    public void setWidgetId(Long widgetId) {
-//        this.widgetId = widgetId;
-//    }
-//
-//    public String getWidgetName() {
-//        return widgetName;
-//    }
-//
-//    public void setWidgetName(String widgetName) {
-//        this.widgetName = widgetName;
-//    }
-//}
+package com.armedia.acm.plugins.dashboard.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.*;
+
+/**
+* Created by marjan.stefanoski on 9/12/2014.
+*/
+@Entity
+@Table(name = "acm_widget")
+public class Widget {
+    private static final long serialVersionUID = -1154137631399833851L;
+    private transient final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Id
+    @Column(name = "cm_widget_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long widgetId;
+
+    @Column(name = "cm_widget_name")
+    private String widgetName;
+
+    public Long getWidgetId() {
+        return widgetId;
+    }
+
+    public void setWidgetId(Long widgetId) {
+        this.widgetId = widgetId;
+    }
+
+    public String getWidgetName() {
+        return widgetName;
+    }
+
+    public void setWidgetName(String widgetName) {
+        this.widgetName = widgetName;
+    }
+}
--- acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/model/WidgetRolePrimaryKey.java
+++ acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/model/WidgetRolePrimaryKey.java
@@ -1,26 +1,26 @@
-//package com.armedia.acm.plugins.dashboard.model;
-//
-///**
-// * Created by marjan.stefanoski on 9/12/2014.
-// */
-//public class WidgetRolePrimaryKey {
-//    private String roleName;
-//
-//    private String widgetId;
-//
-//    public String getRoleName() {
-//        return roleName;
-//    }
-//
-//    public void setRoleName(String roleName) {
-//        this.roleName = roleName;
-//    }
-//
-//    public String getWidgetId() {
-//        return widgetId;
-//    }
-//
-//    public void setWidgetId(String widgetId) {
-//        this.widgetId = widgetId;
-//    }
-//}
+package com.armedia.acm.plugins.dashboard.model;
+
+/**
+* Created by marjan.stefanoski on 9/12/2014.
+*/
+public class WidgetRolePrimaryKey {
+    private String roleName;
+
+    private Long widgetId;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Long getWidgetId() {
+        return widgetId;
+    }
+
+    public void setWidgetId(Long widgetId) {
+        this.widgetId = widgetId;
+    }
+}
