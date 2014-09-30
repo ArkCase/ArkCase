e64c522277cd2574cc2ae8897ef051ecfbaeef98
James Wu <jwu@armedia.com>
2014-09-30 13:36:38 -0400

    remove PageDescriptor

--- acm-plugins/acm-plugins-samples/hello-world-plugin/src/main/java/com/armedia/acm/plugins/sample/HelloWorldController.java
+++ acm-plugins/acm-plugins-samples/hello-world-plugin/src/main/java/com/armedia/acm/plugins/sample/HelloWorldController.java
@@ -4,21 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import com.armedia.acm.web.AcmPageDescriptor;
 
 @Controller
 @RequestMapping("/plugin/sample/helloWorld")
 public class HelloWorldController
 {
     private String name;
-    private AcmPageDescriptor pageDescriptor;
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView homePage()
     {
         ModelAndView retval = new ModelAndView("helloWorld");
         retval.getModel().put("name", getName());
-        retval.addObject("pageDescriptor", getPageDescriptor());
 
         return retval;
     }
@@ -31,13 +28,5 @@ public class HelloWorldController
     public void setName(String name)
     {
         this.name = name;
-    }
-
-    public AcmPageDescriptor getPageDescriptor() {
-        return pageDescriptor;
-    }
-
-    public void setPageDescriptor(AcmPageDescriptor pageDescriptor) {
-        this.pageDescriptor = pageDescriptor;
     }
 }
