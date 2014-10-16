e1847d6dc2ca7e2fa1a1148e9379e958bbf7cfd6
marjan.stefanoski <marjan.stefanoski@armedia.com>
2014-10-16 22:00:49 +0200

    AFDP-575: LDAP sync extended to sync email. Added new mail column in acm_user table (AcmUser) and some of the needed structure for the pofile plugin is created

---
+++ acm-plugins/acm-default-plugins/acm-profile-plugin/src/main/java/com/armedia/acm/plugins/profile/service/ProfileEventPublisher.java
@@ -0,0 +1,25 @@
+package com.armedia.acm.plugins.profile.service;
+
+import com.armedia.acm.auth.AcmAuthenticationDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.security.core.Authentication;
+
+/**
+ * Created by marjan.stefanoski on 16.10.2014.
+ */
+public class ProfileEventPublisher implements ApplicationEventPublisherAware {
+
+    private ApplicationEventPublisher eventPublisher;
+    private transient final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        eventPublisher = applicationEventPublisher;
+    }
+
+
+}
