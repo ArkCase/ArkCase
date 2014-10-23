cd6644d1780e6ede029d894c8a3bcf32a6eaad20
David Miller <david.miller@armedia.com>
2014-10-23 19:00:38 -0400

    AFDP-595 - Index Person records in SOLR - use a custom SOLR field type for case-insensitive strings

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/model/solr/SolrAdvancedSearchDocument.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/model/solr/SolrAdvancedSearchDocument.java
@@ -15,21 +15,21 @@ public class SolrAdvancedSearchDocument implements SolrBaseDocument
     private String title_parseable;
     private String name;
     private Date incident_date_tdt;
-    private String priority_s;
-    private String assignee_id_s;
-    private String assignee_first_name_s;
-    private String assignee_last_name_s;
-    private String incident_type_s;
-    private String status_s;
-    private String person_title_s;
-    private String first_name_s;
-    private String last_name_s;
-    private String type_s;
-    private String value_s;
-    private String location_street_address_s;
-    private String location_city_s;
-    private String location_state_s;
-    private String location_postal_code_s;
+    private String priority_lcs;
+    private String assignee_id_lcs;
+    private String assignee_first_name_lcs;
+    private String assignee_last_name_lcs;
+    private String incident_type_lcs;
+    private String status_lcs;
+    private String person_title_lcs;
+    private String first_name_lcs;
+    private String last_name_lcs;
+    private String type_lcs;
+    private String value_parseable;
+    private String location_street_address_lcs;
+    private String location_city_lcs;
+    private String location_state_lcs;
+    private String location_postal_code_sdo;
     private List<SolrBaseDocument> _childDocuments_ = new ArrayList<>();
     private String child_id_s;
     private String child_type_s;
@@ -99,154 +99,154 @@ public class SolrAdvancedSearchDocument implements SolrBaseDocument
         this.incident_date_tdt = incident_date_tdt;
     }
 
-    public String getPriority_s()
+    public String getPriority_lcs()
     {
-        return priority_s;
+        return priority_lcs;
     }
 
-    public void setPriority_s(String priority_s)
+    public void setPriority_lcs(String priority_lcs)
     {
-        this.priority_s = priority_s;
+        this.priority_lcs = priority_lcs;
     }
 
-    public String getAssignee_id_s()
+    public String getAssignee_id_lcs()
     {
-        return assignee_id_s;
+        return assignee_id_lcs;
     }
 
-    public void setAssignee_id_s(String assignee_id_s)
+    public void setAssignee_id_lcs(String assignee_id_lcs)
     {
-        this.assignee_id_s = assignee_id_s;
+        this.assignee_id_lcs = assignee_id_lcs;
     }
 
-    public String getAssignee_first_name_s()
+    public String getAssignee_first_name_lcs()
     {
-        return assignee_first_name_s;
+        return assignee_first_name_lcs;
     }
 
-    public void setAssignee_first_name_s(String assignee_first_name_s)
+    public void setAssignee_first_name_lcs(String assignee_first_name_lcs)
     {
-        this.assignee_first_name_s = assignee_first_name_s;
+        this.assignee_first_name_lcs = assignee_first_name_lcs;
     }
 
-    public String getAssignee_last_name_s()
+    public String getAssignee_last_name_lcs()
     {
-        return assignee_last_name_s;
+        return assignee_last_name_lcs;
     }
 
-    public void setAssignee_last_name_s(String assignee_last_name_s)
+    public void setAssignee_last_name_lcs(String assignee_last_name_lcs)
     {
-        this.assignee_last_name_s = assignee_last_name_s;
+        this.assignee_last_name_lcs = assignee_last_name_lcs;
     }
 
-    public String getIncident_type_s()
+    public String getIncident_type_lcs()
     {
-        return incident_type_s;
+        return incident_type_lcs;
     }
 
-    public void setIncident_type_s(String incident_type_s)
+    public void setIncident_type_lcs(String incident_type_lcs)
     {
-        this.incident_type_s = incident_type_s;
+        this.incident_type_lcs = incident_type_lcs;
     }
 
-    public String getStatus_s()
+    public String getStatus_lcs()
     {
-        return status_s;
+        return status_lcs;
     }
 
-    public void setStatus_s(String status_s)
+    public void setStatus_lcs(String status_lcs)
     {
-        this.status_s = status_s;
+        this.status_lcs = status_lcs;
     }
 
-    public String getPerson_title_s()
+    public String getPerson_title_lcs()
     {
-        return person_title_s;
+        return person_title_lcs;
     }
 
-    public void setPerson_title_s(String person_title_s)
+    public void setPerson_title_lcs(String person_title_lcs)
     {
-        this.person_title_s = person_title_s;
+        this.person_title_lcs = person_title_lcs;
     }
 
-    public String getFirst_name_s()
+    public String getFirst_name_lcs()
     {
-        return first_name_s;
+        return first_name_lcs;
     }
 
-    public void setFirst_name_s(String first_name_s)
+    public void setFirst_name_lcs(String first_name_lcs)
     {
-        this.first_name_s = first_name_s;
+        this.first_name_lcs = first_name_lcs;
     }
 
-    public String getLast_name_s()
+    public String getLast_name_lcs()
     {
-        return last_name_s;
+        return last_name_lcs;
     }
 
-    public void setLast_name_s(String last_name_s)
+    public void setLast_name_lcs(String last_name_lcs)
     {
-        this.last_name_s = last_name_s;
+        this.last_name_lcs = last_name_lcs;
     }
 
-    public String getLocation_street_address_s()
+    public String getLocation_street_address_lcs()
     {
-        return location_street_address_s;
+        return location_street_address_lcs;
     }
 
-    public void setLocation_street_address_s(String location_street_address_s)
+    public void setLocation_street_address_lcs(String location_street_address_lcs)
     {
-        this.location_street_address_s = location_street_address_s;
+        this.location_street_address_lcs = location_street_address_lcs;
     }
 
-    public String getLocation_city_s()
+    public String getLocation_city_lcs()
     {
-        return location_city_s;
+        return location_city_lcs;
     }
 
-    public void setLocation_city_s(String location_city_s)
+    public void setLocation_city_lcs(String location_city_lcs)
     {
-        this.location_city_s = location_city_s;
+        this.location_city_lcs = location_city_lcs;
     }
 
-    public String getLocation_state_s()
+    public String getLocation_state_lcs()
     {
-        return location_state_s;
+        return location_state_lcs;
     }
 
-    public void setLocation_state_s(String location_state_s)
+    public void setLocation_state_lcs(String location_state_lcs)
     {
-        this.location_state_s = location_state_s;
+        this.location_state_lcs = location_state_lcs;
     }
 
-    public String getLocation_postal_code_s()
+    public String getLocation_postal_code_sdo()
     {
-        return location_postal_code_s;
+        return location_postal_code_sdo;
     }
 
-    public void setLocation_postal_code_s(String location_postal_code_s)
+    public void setLocation_postal_code_sdo(String location_postal_code_sdo)
     {
-        this.location_postal_code_s = location_postal_code_s;
+        this.location_postal_code_sdo = location_postal_code_sdo;
     }
 
-    public String getType_s()
+    public String getType_lcs()
     {
-        return type_s;
+        return type_lcs;
     }
 
-    public void setType_s(String type_s)
+    public void setType_lcs(String type_lcs)
     {
-        this.type_s = type_s;
+        this.type_lcs = type_lcs;
     }
 
-    public String getValue_s()
+    public String getValue_parseable()
     {
-        return value_s;
+        return value_parseable;
     }
 
-    public void setValue_s(String value_s)
+    public void setValue_parseable(String value_parseable)
     {
-        this.value_s = value_s;
+        this.value_parseable = value_parseable;
     }
 
     public List<SolrBaseDocument> get_childDocuments_()
