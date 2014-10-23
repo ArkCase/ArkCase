c88a5e7ca36f4179620cffef8d6dfd7134e68d52
David Miller <david.miller@armedia.com>
2014-10-23 09:44:20 -0400

    AFDP-595 - Index Person records in SOLR - initial interface plugin modules can implement to send objects to SOLR

---
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/plugins/search/model/solr/AcmObjectToSolrDocTransformer.java
@@ -0,0 +1,15 @@
+package com.armedia.acm.plugins.search.model.solr;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+/**
+ * Created by armdev on 10/22/14.
+ */
+public interface AcmObjectToSolrDocTransformer<T>
+{
+    SolrAdvancedSearchDocument toSolrAdvancedSearch(T in) throws JsonProcessingException;
+
+    SolrDocument toSolrQuickSearch(T in) throws JsonProcessingException;
+
+    boolean isAcmObjectTypeSupported(Class acmObjectType);
+}
