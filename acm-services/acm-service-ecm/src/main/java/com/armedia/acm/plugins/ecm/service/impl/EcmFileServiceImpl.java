744bb23d6eeda9d2b5cd763411e27758291038cd
David Miller <david.miller@armedia.com>
2014-10-30 17:50:56 -0400

    AFDP-628 - Start Document Approval Workflow When Close Complaint Form is Submitted - When saving form PDF renditions and XML, ensure a database connection is only opened after the file has been uploaded to Alfresco; this avoids a possible connection timeout

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileServiceImpl.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -39,7 +38,6 @@ public class EcmFileServiceImpl implements ApplicationEventPublisherAware, EcmFi
 
 
     @Override
-    @Transactional
     public ResponseEntity<? extends Object> upload(
             MultipartFile file,
             String acceptHeader,
