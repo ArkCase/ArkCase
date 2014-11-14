daf86687950f165d5fe03a40f11294b599d41de4
David Miller <david.miller@armedia.com>
2014-11-14 14:44:33 -0500

    AFDP-637 - Document Workflow Event Handler: Open Full Investigation - open new investigation for close complaint requests with disposition of "open_investigation"

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/CloseCompaintRequestService.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/CloseCompaintRequestService.java
@@ -1,10 +1,17 @@
 package com.armedia.acm.plugins.complaint.service;
 
+import com.armedia.acm.plugins.casefile.model.CaseFile;
+import com.armedia.acm.plugins.casefile.service.SaveCaseService;
 import com.armedia.acm.plugins.complaint.dao.CloseComplaintRequestDao;
 import com.armedia.acm.plugins.complaint.dao.ComplaintDao;
 import com.armedia.acm.plugins.complaint.model.CloseComplaintRequest;
 import com.armedia.acm.plugins.complaint.model.Complaint;
-import com.armedia.acm.plugins.complaint.model.ComplaintClosedEvent;
+import com.armedia.acm.plugins.objectassociation.model.ObjectAssociation;
+import org.mule.api.MuleException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
@@ -17,33 +24,89 @@ public class CloseCompaintRequestService
     private ComplaintDao complaintDao;
     private CloseComplaintRequestDao closeComplaintRequestDao;
     private ComplaintEventPublisher complaintEventPublisher;
+    private SaveCaseService saveCaseService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Transactional
     public void handleCloseComplaintRequestApproved(
             Long complaintId,
             Long closeComplaintRequestId,
             String user,
-            Date approvalDate)
+            Date approvalDate) throws MuleException
     {
-        updateComplaintStatus(complaintId, user, approvalDate);
+        Complaint updatedComplaint = updateComplaintStatus(complaintId, user, approvalDate);
 
-        updateCloseComplaintRequestStatus(closeComplaintRequestId);
+        CloseComplaintRequest updatedRequest = updateCloseComplaintRequestStatus(closeComplaintRequestId);
+
+        boolean shouldFullInvestigationBeOpened = shallWeOpenAFullInvestigation(updatedRequest);
+        log.debug("Open a new investigation? " + shouldFullInvestigationBeOpened);
+
+        if ( shouldFullInvestigationBeOpened )
+        {
+            CaseFile fullInvestigation = openFullInvestigation(updatedComplaint, user);
+            log.debug("Opened a full investigation: " + fullInvestigation.getCaseNumber());
+        }
+
+
     }
 
-    private void updateCloseComplaintRequestStatus(Long closeComplaintRequestId)
+    private CaseFile openFullInvestigation(Complaint updatedComplaint, String userId) throws MuleException
+    {
+        CaseFile caseFile = new CaseFile();
+        caseFile.setStatus("ACTIVE");
+        caseFile.setCaseType(updatedComplaint.getComplaintType());
+
+        String details = "This case file is based on Complaint '" + updatedComplaint.getComplaintNumber() + "'.";
+        caseFile.setDetails(details);
+
+        caseFile.setPriority(updatedComplaint.getPriority());
+        caseFile.setTitle(updatedComplaint.getComplaintTitle());
+
+        ObjectAssociation originalComplaint = new ObjectAssociation();
+        originalComplaint.setTargetId(updatedComplaint.getComplaintId());
+        originalComplaint.setTargetName(updatedComplaint.getComplaintNumber());
+        originalComplaint.setTargetType("COMPLAINT");
+
+        caseFile.addChildObject(originalComplaint);
+
+        log.debug("About to save case file");
+
+        // here we need a full Authentication object
+        Authentication auth = new UsernamePasswordAuthenticationToken(userId, userId);
+        CaseFile fullInvestigation = getSaveCaseService().saveCase(caseFile, auth, null);
+
+        return fullInvestigation;
+    }
+
+    private boolean shallWeOpenAFullInvestigation(CloseComplaintRequest updatedRequest)
+    {
+        if ( updatedRequest.getDisposition() == null )
+        {
+            log.debug("No disposition for request ID '" + updatedRequest.getId() + "'");
+            return false;
+        }
+
+        return "open_investigation".equals(updatedRequest.getDisposition().getDispositionType());
+    }
+
+    private CloseComplaintRequest updateCloseComplaintRequestStatus(Long closeComplaintRequestId)
     {
         CloseComplaintRequest ccr = getCloseComplaintRequestDao().find(closeComplaintRequestId);
         ccr.setStatus("APPROVED");
-        getCloseComplaintRequestDao().save(ccr);
+        CloseComplaintRequest updated = getCloseComplaintRequestDao().save(ccr);
+        return updated;
     }
 
-    private void updateComplaintStatus(Long complaintId, String user, Date approvalDate)
+    private Complaint updateComplaintStatus(Long complaintId, String user, Date approvalDate)
     {
         Complaint c = getComplaintDao().find(complaintId);
         c.setStatus("CLOSED");
         c = getComplaintDao().save(c);
 
         getComplaintEventPublisher().publishComplaintClosedEvent(c, user, true, approvalDate);
+
+        return c;
     }
 
 
@@ -75,5 +138,15 @@ public class CloseCompaintRequestService
     public void setComplaintEventPublisher(ComplaintEventPublisher complaintEventPublisher)
     {
         this.complaintEventPublisher = complaintEventPublisher;
+    }
+
+    public SaveCaseService getSaveCaseService()
+    {
+        return saveCaseService;
+    }
+
+    public void setSaveCaseService(SaveCaseService saveCaseService)
+    {
+        this.saveCaseService = saveCaseService;
     }
 }
