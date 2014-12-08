764baa63addfe0cef9f5df71f4212e5ac7485ca2
James Wu <jwu@armedia.com>
2014-12-08 15:02:17 -0500

    connect audit table to api; hard coded milestone tracker

--- acm-plugins/acm-default-plugins/acm-audit-plugin/src/main/resources/META-INF/resources/resources/js/audit/AuditService.js
+++ acm-plugins/acm-default-plugins/acm-audit-plugin/src/main/resources/META-INF/resources/resources/js/audit/AuditService.js
@@ -11,7 +11,7 @@ Audit.Service = {
     ,onInitialized: function() {
     }
 
-    ,API_RETRIEVE_AUDIT_LIST         : "/api/v1/plugin/audit/page"
+    ,API_RETRIEVE_AUDIT_LIST         : "/api/latest/plugin/audit/page"
 
     ,retrieveAuditListDeferred : function(postData, jtParams, sortMap, callbackSuccess, callbackError) {
         var pageIndex = jtParams.jtStartIndex;
@@ -19,9 +19,6 @@ Audit.Service = {
         return AcmEx.Service.JTable.deferredPagingListAction(postData, jtParams, sortMap
             ,function() {
                 return  App.getContextPath() + Audit.Service.API_RETRIEVE_AUDIT_LIST;
-
-                //for test
-                //return App.getContextPath() + "/api/latest/plugin/search/CASE_FILE";
             }
             ,function(data) {
                 var jtData = null
@@ -44,47 +41,13 @@ Audit.Service = {
                         auditList.push(audit);
                     }
                     Audit.Model.cacheAuditList.put(pageIndex, auditList);
+                    Audit.Model.setTotalCount(data.totalCount);
 
                     jtData = callbackSuccess(auditList);
                 }
 
                 return jtData;
             }
-//            ,function(data) {
-//                var jtData = null
-//                if (Audit.Model.validateAudit(data)) {
-//                    var responseHeader = data.responseHeader;
-//                    if (0 == responseHeader.status) {
-//                        //response.start should match to jtParams.jtStartIndex
-//                        //response.docs.length should be <= jtParams.jtPageSize
-//
-//                        var response = data.response;
-//                        var auditList = [];
-//                        for (var i = 0; i < response.docs.length; i++) {
-//                            var doc = response.docs[i];
-//                            var audit = {};
-//                            audit.id       = doc.object_id_s;
-//                            audit.dateTime = Acm.getDateFromDatetime(doc.create_dt);
-//                            audit.user     = Acm.goodValue(doc.assignee_s);
-//                            audit.activity = Acm.goodValue(response.docs[i].name);
-//                            audit.result   = Acm.goodValue(doc.status_s);
-//                            audit.ip       = Acm.goodValue(doc.priority_i);
-//                            audit.objectId = doc.object_id_s;
-//                            auditList.push(audit);
-//                        }
-//                        Audit.Model.cacheAuditList.put(pageIndex, auditList);
-//
-//                        jtData = callbackSuccess(auditList);
-//
-//                    } else {
-//                        if (Acm.isNotEmpty(data.error)) {
-//                            //todo: report error to controller. data.error.msg + "(" + data.error.code + ")";
-//                        }
-//                    }
-//                }
-//
-//                return jtData;
-//            }
         );
     }
 
