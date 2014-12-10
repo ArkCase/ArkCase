c8ab23716b964b55b273ac9a8ba7dfe0f5f49c30
David Miller <david.miller@armedia.com>
2014-12-10 12:37:06 -0500

    AFDP-764 - Task Module - Add case number to the task title in quotes.  Also fixed the inclusive gateways to use the inclusive gateway element instead of a parallel gateway.

--- acm-plugins/acm-extra-plugins/acm-personnel-security-plugin/src/test/java/com/armedia/acm/plugins/personnelsecurity/service/BackgroundInvestigationBusinessProcessIT.java
+++ acm-plugins/acm-extra-plugins/acm-personnel-security-plugin/src/test/java/com/armedia/acm/plugins/personnelsecurity/service/BackgroundInvestigationBusinessProcessIT.java
@@ -2,6 +2,8 @@ package com.armedia.acm.plugins.personnelsecurity.service;
 
 
 import com.armedia.acm.plugins.personnelsecurity.casestatus.service.CaseFileStateService;
+import com.armedia.acm.plugins.personnelsecurity.correspondence.service.PersonnelSecurityCorrespondenceService;
+import com.armedia.acm.plugins.personnelsecurity.cvs.service.ClearanceVerificationSystemExportService;
 import com.armedia.acm.service.milestone.service.MilestoneService;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.ProcessEngine;
@@ -21,6 +23,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -57,7 +61,23 @@ public class BackgroundInvestigationBusinessProcessIT
     @Qualifier(value = "caseFileStateService")
     private CaseFileStateService caseFileStateService;
 
+    @Autowired
+    @Qualifier(value = "clearanceVerificationSystemExportService")
+    private ClearanceVerificationSystemExportService clearanceVerificationSystemExportService;
+
+    @Autowired
+    @Qualifier(value = "personnelSecurityCorrespondenceService")
+    private PersonnelSecurityCorrespondenceService personnelSecurityCorrespondenceService;
+
     private Object[] mocks;
+
+    private Long caseId = 12345L;
+    private String caseNumber = "20140530_001";
+    private String folderId = "folderId";
+    private String subjectLastName = "Garcia";
+    private String processName = "personnelSecurityBackgroundInvestigation";
+    private String defaultAdjudicator = "sally-acm";
+    Map<String, Object> pvars = new HashMap<>();
 
     @Before
     public void setUp() throws Exception
@@ -65,31 +85,10 @@ public class BackgroundInvestigationBusinessProcessIT
 
         // deploy
         repo.createDeployment()
-                .addClasspathResource("activiti/personnelSecurityBackgroundInvestigation_v5.bpmn20.xml")
+                .addClasspathResource("activiti/personnelSecurityBackgroundInvestigation_v6.bpmn20.xml")
                 .deploy();
 
-        mocks = new Object[] { mockMilestoneService, caseFileStateService };
-
-    }
-
-    @After
-    public void shutDown() throws Exception
-    {
-        pe.close();
-    }
-
-    @Test
-    public void startProcess_processStart_create4TasksSetCaseStatusAndCallMilestoneService() throws Exception
-    {
-        Long caseId = 12345L;
-        String caseNumber = "20140530_001";
-        String folderId = "folderId";
-        String subjectLastName = "Garcia";
-        String processName = "personnelSecurityBackgroundInvestigation";
-
-        int expectedVerifyTasks = 4;
-
-        Map<String, Object> pvars = new HashMap<>();
+        mocks = new Object[] { mockMilestoneService, caseFileStateService, clearanceVerificationSystemExportService };
 
         pvars.put("OBJECT_TYPE", "CASE_FILE");
         pvars.put("OBJECT_ID", caseId);
@@ -99,28 +98,151 @@ public class BackgroundInvestigationBusinessProcessIT
         pvars.put("REQUEST_ID", caseId);
         pvars.put("OBJECT_FOLDER_ID", folderId);
         pvars.put("SUBJECT_LAST_NAME", subjectLastName);
+    }
 
-        log.debug("starting process: " + processName);
+    @After
+    public void shutDown() throws Exception
+    {
+        pe.close();
+    }
 
-        assertNotNull(mockMilestoneService);
-        log.debug("type: " + mockMilestoneService.getClass().getName());
-
+    @Test
+    public void startProcess_processStart_happyPath() throws Exception
+    {
+        // these calls should happen when process is started
         mockMilestoneService.saveMilestone(caseId, "CASE_FILE", "Initiated");
         caseFileStateService.changeCaseFileState(caseId, "ACTIVE");
+
+        // should happen when adjudication task is completed, no matter what the outcome
+        mockMilestoneService.saveMilestone(caseId, "CASE_FILE", "Adjudication");
+        clearanceVerificationSystemExportService.exportDeterminationRecord(
+                defaultAdjudicator,
+                caseId,
+                caseNumber,
+                folderId,
+                subjectLastName,
+                "GRANT_CLEARANCE");
+        personnelSecurityCorrespondenceService.generateClearanceGrantedCorrespondence(subjectLastName);
+
+        // should happen after clearance is issued
+        mockMilestoneService.saveMilestone(caseId, "CASE_FILE", "Issued");
+
+        // always happens at end of process
+        mockMilestoneService.saveMilestone(caseId, "CASE_FILE", "Closed");
+        caseFileStateService.changeCaseFileState(caseId, "CLOSED");
 
         replay(mocks);
 
         ProcessInstance pi = rt.startProcessInstanceByKey(processName, pvars);
 
+        happyPath_verifyInitialUserTasks(pi);
+
+        happyPath_completeInitialUserTasks(pi);
+
+        happyPath_verifyAdjudicationTask(pi);
+
+        happyPath_completeAdjudicationTask(pi);
+
+        happyPath_verifyIssueClearanceTask(pi);
+
+        happyPath_completeIssueClearanceTask(pi);
+
         verify(mocks);
 
-        List<Task> verifyTasks = ts.createTaskQuery().processInstanceId(pi.getProcessInstanceId()).list();
+
+    }
+
+    private void happyPath_completeIssueClearanceTask(ProcessInstance pi)
+    {
+        String caseNumberInQuotes = "'" + caseNumber + "'";
+        completeTask("Issue Clearance " + caseNumberInQuotes, "issueClearanceOutcome", "COMPLETE");
+    }
+
+    private void happyPath_verifyIssueClearanceTask(ProcessInstance pi)
+    {
+        List<Task> userTasks = ts.createTaskQuery().processInstanceId(pi.getProcessInstanceId()).list();
+
+        int expectedTasks = 1;
+
+        assertEquals(expectedTasks, userTasks.size());
+
+        String caseNumberInQuotes = "'" + caseNumber + "'";
+
+        String expectedName = "Issue Clearance " + caseNumberInQuotes;
+
+        assertEquals(expectedName, userTasks.get(0).getName());
+    }
+
+    private void happyPath_completeAdjudicationTask(ProcessInstance pi)
+    {
+        String caseNumberInQuotes = "'" + caseNumber + "'";
+        completeTask("Adjudicate Clearance Request " + caseNumberInQuotes, "adjudicationOutcome", "GRANT_CLEARANCE");
+    }
+
+    /** should now have one user task to adjudicate the case
+     *
+     * @param pi
+     */
+    private void happyPath_verifyAdjudicationTask(ProcessInstance pi)
+    {
+        List<Task> userTasks = ts.createTaskQuery().processInstanceId(pi.getProcessInstanceId()).list();
+
+        int expectedTasks = 1;
+
+        assertEquals(expectedTasks, userTasks.size());
+
+        String caseNumberInQuotes = "'" + caseNumber + "'";
+
+        String expectedName = "Adjudicate Clearance Request " + caseNumberInQuotes;
+
+        assertEquals(expectedName, userTasks.get(0).getName());
+    }
+
+    private void happyPath_completeInitialUserTasks(ProcessInstance pi)
+    {
+        String caseNumberInQuotes = "'" + caseNumber + "'";
+        completeTask("eVerify " + caseNumberInQuotes, "eVerifyOutcome", "COMPLETE");
+        completeTask("Verify Address History " + caseNumberInQuotes, "verifyAddressOutcome", "COMPLETE");
+        completeTask("Verify Employment History " + caseNumberInQuotes, "verifyEmploymentOutcome", "COMPLETE");
+        completeTask("Verify SF86 Packet " + caseNumberInQuotes, "sf86Outcome", "COMPLETE");
+    }
+
+    private void completeTask(String taskName, String outcomeName, String outcomeValue)
+    {
+        Task task = ts.createTaskQuery().taskName(taskName).singleResult();
+        assertNotNull(task);
+        ts.setVariable(task.getId(), outcomeName, outcomeValue);
+        ts.complete(task.getId());
+    }
+
+    private void happyPath_verifyInitialUserTasks(ProcessInstance pi)
+    {
+        List<Task> verifyTasks = ts.createTaskQuery().processInstanceId(pi.getProcessInstanceId()).orderByTaskName().asc().list();
+
+        int expectedVerifyTasks = 4;
 
         log.debug("Found " + verifyTasks.size() + " verify tasks.");
 
-        assertEquals(expectedVerifyTasks, verifyTasks.size());
-    }
+        String caseNumberInQuotes = "'" + caseNumber + "'";
 
+        // note, activiti sorts task names in a case-sensitive way, so all caps come before all lowercase apparently.
+        List<String> expectedNames = Arrays.asList(
+                "Verify Address History " + caseNumberInQuotes,
+                "Verify Employment History " + caseNumberInQuotes,
+                "Verify SF86 Packet " + caseNumberInQuotes,
+                "eVerify " + caseNumberInQuotes
+        );
+
+        assertEquals(expectedVerifyTasks, verifyTasks.size());
+
+        List<String> foundTaskNames = new ArrayList<>();
+        for ( Task task : verifyTasks )
+        {
+            foundTaskNames.add(task.getName());
+        }
+
+        assertEquals(expectedNames, foundTaskNames);
+    }
 
 
 }
