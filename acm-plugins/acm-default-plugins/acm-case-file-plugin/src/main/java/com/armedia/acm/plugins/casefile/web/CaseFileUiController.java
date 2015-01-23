c54f76f4e594801804c77555d62e308b5fc4282e
James Wu <jwu@armedia.com>
2015-01-23 17:48:37 -0500

    fixed fast search to work with new facet search page

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/CaseFileUiController.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/CaseFileUiController.java
@@ -51,12 +51,12 @@ public class CaseFileUiController
         Map<String, Object> props = plugin.getPluginProperties();
         if (null != props) {
             try {
-                Object propFilter = props.get("tree.filter");
+                Object propFilter = props.get("search.tree.filter");
                 if (null != propFilter) {
                     JSONArray treeFilter = new JSONArray(propFilter.toString());
                     mv.addObject("treeFilter", treeFilter);
                 }
-                Object propSort = props.get("tree.sort");
+                Object propSort = props.get("search.tree.sort");
                 if (null != propSort) {
                     JSONArray treeSort = new JSONArray(propSort.toString());
                     mv.addObject("treeSort", treeSort);
