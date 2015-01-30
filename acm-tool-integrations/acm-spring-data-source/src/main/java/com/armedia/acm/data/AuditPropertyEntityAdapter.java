c9584f8337c12f5d05c27254c29631cb532ca003
David Miller <david.miller@armedia.com>
2015-01-30 18:39:02 -0500

    AFDP-823 - The SOLR batch index job should not constantly reindex every single object

--- acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AuditPropertyEntityAdapter.java
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AuditPropertyEntityAdapter.java
@@ -2,13 +2,20 @@ package com.armedia.acm.data;
 
 import org.eclipse.persistence.descriptors.DescriptorEvent;
 import org.eclipse.persistence.descriptors.DescriptorEventAdapter;
+import org.eclipse.persistence.mappings.DatabaseMapping;
+import org.eclipse.persistence.sessions.Record;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
 /**
- * Created by armdev on 10/30/14.
+ * Update entity objects with the creator, created, modifier, and modified audit fields; thus, the application just
+ * updates the entities based on business requirements, and this adapter takes care of the audit fields.
+ *
+ * EclipseLink JPA provider raises aboutToInsert and aboutToUpdate <strong>after</strong> it has calculated the
+ * change set, and <strong>only</strong> if the objects actually have changes.  That means we have to update the
+ * EclipseLink change set directly; and we also update the POJO entity fields, so the client sees the updates.
  */
 public class AuditPropertyEntityAdapter extends DescriptorEventAdapter
 {
@@ -16,21 +23,46 @@ public class AuditPropertyEntityAdapter extends DescriptorEventAdapter
     private ThreadLocal<String> userId = new ThreadLocal<>();
 
     @Override
-    public void preInsert(DescriptorEvent event)
+    public void aboutToInsert(DescriptorEvent event)
     {
-        super.preInsert(event);
+        super.aboutToInsert(event);
+
+        Record record = event.getRecord();
         Object data = event.getObject();
 
+        // this method is called after JPA already computed what will be inserted.  So we have to modify the
+        // insert record directly, instead of just modifying the AcmEntity.
         if ( data instanceof AcmEntity )
         {
             log.trace("Entity type '" + data.getClass() + "' is an AcmEntity, setting insert fields.");
             AcmEntity entity = (AcmEntity) data;
 
             Date today = new Date();
+            String user = getUserId();
+
+            String created = getDatabaseColumnName(event, "created");
+            record.put(created, today);
+
+            String creator = getDatabaseColumnName(event, "creator");
+            record.put(creator, getUserId());
+
+            // some entities, notably AcmNote, do not support mods, and so they will not have modified or modifier.
+            String modified = getDatabaseColumnName(event, "modified");
+            if ( modified != null )
+            {
+                record.put(modified, today);
+            }
+
+            String modifier = getDatabaseColumnName(event, "modifier");
+            if ( modifier != null )
+            {
+                record.put(modifier, getUserId());
+            }
+
+            // note, we still have to update the object itself, so the client will have the right values
+
             entity.setCreated(today);
             entity.setModified(today);
-
-            String user = getUserId();
             entity.setCreator(user);
             entity.setModifier(user);
         }
@@ -40,27 +72,50 @@ public class AuditPropertyEntityAdapter extends DescriptorEventAdapter
         }
     }
 
-    @Override
-    public void preUpdate(DescriptorEvent event)
+    private String getDatabaseColumnName(DescriptorEvent event, String pojoPropertyName)
     {
-        super.preUpdate(event);
+        DatabaseMapping mapping = event.getDescriptor().getMappingForAttributeName(pojoPropertyName);
+        return mapping == null ? null : mapping.getField().getName();
+    }
 
+    @Override
+    public void aboutToUpdate(DescriptorEvent event)
+    {
+        super.aboutToUpdate(event);
+
+        Record record = event.getRecord();
         Object data = event.getObject();
 
+        // this method is called after JPA already computed what will be inserted.  So we have to modify the
+        // insert record directly, instead of just modifying the AcmEntity.
         if ( data instanceof AcmEntity )
         {
-            log.trace("Entity type '" + data.getClass() + "' is an AcmEntity, setting update fields.");
+            log.debug("Entity type '" + data.getClass() + "' is an AcmEntity, setting update fields.");
             AcmEntity entity = (AcmEntity) data;
 
             Date today = new Date();
-            entity.setModified(today);
-
             String user = getUserId();
+
+            String modified = getDatabaseColumnName(event, "modified");
+            if ( modified != null )
+            {
+                record.put(modified, today);
+            }
+
+            String modifier = getDatabaseColumnName(event, "modifier");
+            if ( modifier != null )
+            {
+                record.put(modifier, getUserId());
+            }
+
+            // note, we still have to update the object itself, so the client will have the right values
+
+            entity.setModified(today);
             entity.setModifier(user);
         }
         else
         {
-            log.trace("Entity type '" + data.getClass() + "' is NOT an AcmEntity, NOT setting update fields.");
+            log.debug("Entity type '" + data.getClass() + "' is NOT an AcmEntity, NOT setting update fields.");
         }
     }
 
