1748f21129aae28b89ae5a70fa1bddb0b0d1a14c
James Wu <jwu@armedia.com>
2015-02-05 09:54:49 -0500

    Adding parent to search result. Save before update to api changes

--- acm-services/acm-service-search/src/main/resources/META-INF/resources/resources/js/search/searchView.js
+++ acm-services/acm-service-search/src/main/resources/META-INF/resources/resources/js/search/searchView.js
@@ -5,14 +5,38 @@
  */
 Search.View = {
     create : function() {
+        if (Search.View.MicroData.create)        {Search.View.MicroData.create();}
         if (Search.View.Query.create)            {Search.View.Query.create();}
         if (Search.View.Facet.create)            {Search.View.Facet.create();}
         if (Search.View.Results.create)          {Search.View.Results.create();}
     }
     ,onInitialized: function() {
+        if (Search.View.MicroData.onInitialized) {Search.View.MicroData.onInitialized();}
         if (Search.View.Query.onInitialized)     {Search.View.Query.onInitialized();}
         if (Search.View.Facet.onInitialized)     {Search.View.Facet.onInitialized();}
         if (Search.View.Results.onInitialized)   {Search.View.Results.onInitialized();}
+    }
+
+    ,MicroData: {
+        create : function() {
+            this.objectTypes = Acm.Object.MicroData.getJson("objectTypes");
+        }
+        ,onInitialized: function() {
+        }
+
+        ,findObjectType: function(type) {
+            var found = null;
+            if (Acm.isArray(this.objectTypes)) {
+                for (var i = 0; i < this.objectTypes.length; i++) {
+                    var objectType = this.objectTypes[i];
+                    if (Acm.compare(type, objectType.type)) {
+                        found = objectType;
+                        break;
+                    }
+                }
+            }
+            return found;
+        }
     }
 
     ,Query: {
@@ -69,6 +93,10 @@ Search.View = {
         }
 
         ,onClickCheckBox: function(event, ctrl) {
+            if(event.ctrlKey) {
+                return;
+            }
+
             var selected = [];
             var $checked = Search.View.Facet.$divFacet.find("input:checked");
             $checked.each(function(){
@@ -179,85 +207,7 @@ Search.View = {
                     }
                     html += "</div>";
                 }
-/*
-                if (0 < Search.Model.getCountFacetFields(facet)){
-                    html += "<div name='facet_fields'>";
-                    for(var i = 0; i < facet.facet_fields.length; i++) {
-                        if (0 < Acm.goodValue(facet.facet_fields[i].count, 0)) {
-                            if (Acm.isArray(facet.facet_fields[i].values)) {
-                                var display = this._getFacetDisplay(facet.facet_fields[i].label, facet.facet_fields[i].key);
-                                html += "<div name='" + display + "'>";
-                                html += "<label class='label'>" + display + "</label>";
-                                for (var j = 0; j < facet.facet_fields[i].values.length; j++) {
-                                    if (0 < Acm.goodValue(facet.facet_fields[i].values[j].count, 0)) {
-                                        html += "</br><input type='checkbox' value='" + Acm.goodValue(facet.facet_fields[i].values[j].name) + "'";
-                                        if (Search.Model.findFilter(si, display, Acm.goodValue(facet.facet_fields[i].values[j].name))) {
-                                            html += " checked";
-                                        }
-                                        html += ">" + Acm.goodValue(facet.facet_fields[i].values[j].name)
-                                            + "(<span>" + facet.facet_fields[i].values[j].count + "</span>)</input>";
-                                    }
-                                }
-                                html += "</div>";
-                            }
-                        }
-                    }
-                    html += "</div>";
-                }
-
-
-                if (0 < Search.Model.getCountFacetQueries(facet)){
-                    html += "<div name='facet_queries'>";
-                    for(var i = 0; i < facet.facet_queries.length; i++) {
-                        if (0 < Acm.goodValue(facet.facet_queries[i].count, 0)) {
-                            if (Acm.isArray(facet.facet_queries[i].values)) {
-                                var display = this._getFacetDisplay(facet.facet_queries[i].label, facet.facet_queries[i].key);
-                                html += "<div name='" + display + "'>";
-                                html += "<label class='label'>" + display + "</label>";
-                                for (var j = 0; j < facet.facet_queries[i].values.length; j++) {
-                                    if (0 < Acm.goodValue(facet.facet_queries[i].values[j].count, 0)) {
-                                        html += "</br><input type='checkbox' value='" + Acm.goodValue(facet.facet_queries[i].values[j].name) + "'";
-                                        if (Search.Model.findFilter(si, display, Acm.goodValue(facet.facet_queries[i].values[j].name))) {
-                                            html += " checked";
-                                        }
-                                        html += ">" + Acm.goodValue(facet.facet_queries[i].values[j].name)
-                                            + "(<span>" + facet.facet_queries[i].values[j].count + "</span>)</input>";
-                                    }
-                                }
-                                html += "</div>";
-                            }
-                        }
-                    }
-                    html += "</div>";
-                }
-
-
-                if (0 < Search.Model.getCountFacetDates(facet)){
-                    html += "<div name='facet_dates'>";
-                    for(var i = 0; i < facet.facet_dates.length; i++) {
-                        if (0 < Acm.goodValue(facet.facet_dates[i].count, 0)) {
-                            if (Acm.isArray(facet.facet_dates[i].values)) {
-                                var display = this._getFacetDisplay(facet.facet_dates[i].label, facet.facet_dates[i].key);
-                                html += "<div name='" + display + "'>";
-                                html += "<label class='label'>" + display + "</label>";
-                                for (var j = 0; j < facet.facet_dates[i].values.length; j++) {
-                                    if (0 < Acm.goodValue(facet.facet_dates[i].values[j].count, 0)) {
-                                        html += "</br><input type='checkbox' value='" + Acm.goodValue(facet.facet_dates[i].values[j].name) + "'";
-                                        if (Search.Model.findFilter(si, display, Acm.goodValue(facet.facet_dates[i].values[j].name))) {
-                                            html += " checked";
-                                        }
-                                        html += ">" + Acm.goodValue(facet.facet_dates[i].values[j].name)
-                                            + "(<span>" + facet.facet_dates[i].values[j].count + "</span>)</input>";
-                                    }
-                                }
-                                html += "</div>";
-                            }
-                        }
-                    }
-                    html += "</div>";
-                }
-*/
-            }
+            }  //end if validateSearchFacet
 
             this.setHtmlDivFacet(html);
 
@@ -298,6 +248,8 @@ Search.View = {
                     Record.name    = Acm.goodValue(result.docs[i].name);
                     Record.type    = Acm.goodValue(result.docs[i].object_type_s);
                     Record.title   = Acm.goodValue(result.docs[i].title_parseable);
+                    Record.parent  = Acm.goodValue(result.docs[i].name);
+                    Record.parentType = Acm.goodValue(result.docs[i].object_type_s);
                     Record.owner   = Acm.goodValue(result.docs[i].assignee_full_name_lcs); //owner_s
                     Record.created = Acm.goodValue(result.docs[i].modified_date_tdt); //create_tdt
                     jtData.Records.push(Record);
@@ -372,20 +324,28 @@ Search.View = {
                             ,width: '15%'
                             ,sorting: false
                             ,display: function(data) {
-                                var url = App.getContextPath();
-                                if (App.OBJTYPE_CASE == data.record.type) {
-                                    url += "/plugin/casefile/" + data.record.id;
-                                } else if (App.OBJTYPE_COMPLAINT == data.record.type) {
-                                    url += "/plugin/complaint/" + data.record.id;
-                                } else if (App.OBJTYPE_TASK == data.record.type) {
-                                    url += "/plugin/task/" + data.record.id;
-                                } else if (App.OBJTYPE_DOCUMENT == data.record.type) {
-                                    url += "/plugin/document/" + data.record.id;
-                                } else if (App.OBJTYPE_PEOPLE == data.record.type) {
-                                    url += "/plugin/people/" + data.record.id;
-                                }else if (App.OBJTYPE_PERSON == data.record.type) {
-                                    url += "/plugin/person/" + data.record.id;
+                                var url = "#";
+                                var objectType = Search.View.MicroData.findObjectType(Acm.goodValue(data.record.type));
+                                if (objectType) {
+                                    url = App.getContextPath() + Acm.goodValue(objectType.url) + Acm.goodValue(data.record.id) + Acm.goodValue(objectType.urlEnd);
                                 }
+
+//                                var url = App.getContextPath();
+//                                if (App.OBJTYPE_CASE == data.record.type) {
+//                                    url += "/plugin/casefile/" + data.record.id;
+//                                } else if (App.OBJTYPE_COMPLAINT == data.record.type) {
+//                                    url += "/plugin/complaint/" + data.record.id;
+//                                } else if (App.OBJTYPE_TASK == data.record.type) {
+//                                    url += "/plugin/task/" + data.record.id;
+//                                } else if (App.OBJTYPE_DOCUMENT == data.record.type) {
+//                                    url += "/plugin/document/" + data.record.id;
+//                                } else if (App.OBJTYPE_PEOPLE == data.record.type) {
+//                                    url += "/plugin/people/" + data.record.id;
+//                                }else if (App.OBJTYPE_PERSON == data.record.type) {
+//                                    url += "/plugin/person/" + data.record.id;
+//                                }
+
+
                                 var $lnk = $("<a href='" + url + "'>" + data.record.name + "</a>");
                                 //$lnk.click(function(){alert("click" + data.record.id)});
 
@@ -403,6 +363,25 @@ Search.View = {
                         ,title: {
                             title: 'Title'
                             ,width: '30%'
+                        }
+                        ,parent: {
+                            title: 'Parent'
+                            ,width: '15%'
+                            ,sorting: false
+                            ,display: function(data) {
+                                var url = "#";
+                                var objectType = Search.View.MicroData.findObjectType(Acm.goodValue(data.record.parentType));
+                                if (objectType) {
+                                    url = App.getContextPath() + Acm.goodValue(objectType.url) + Acm.goodValue(data.record.id) + Acm.goodValue(objectType.urlEnd);
+                                }
+                                var $lnk = $("<a href='" + url + "'>" + data.record.name + "</a>");
+                                return $lnk;
+                            }
+                        }
+                        ,parentType: {
+                            title: 'Parent Type'
+                            ,sorting: false
+                            ,list: false
                         }
                         ,owner: {
                             title: 'Assignee'
