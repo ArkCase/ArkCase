a1f406abe668ce76fd9b16b65814eacca73a1df1
David Miller <david.miller@armedia.com>
2015-02-12 13:30:31 -0500

    AFDP-162 - ACM permission evaluator - moved search access control checker from search module to DAC module... so DAC module can depend on search, and not the other way around

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/service/CaseFileToSolrTransformer.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/service/CaseFileToSolrTransformer.java
@@ -5,7 +5,7 @@ import com.armedia.acm.plugins.casefile.model.CaseFile;
 import com.armedia.acm.services.search.model.solr.SolrAdvancedSearchDocument;
 import com.armedia.acm.services.search.model.solr.SolrDocument;
 import com.armedia.acm.services.search.service.AcmObjectToSolrDocTransformer;
-import com.armedia.acm.services.search.service.SearchAccessControlFields;
+import com.armedia.acm.services.dataaccess.service.SearchAccessControlFields;
 import com.armedia.acm.services.users.dao.ldap.UserDao;
 import com.armedia.acm.services.participants.model.AcmParticipant;
 import com.armedia.acm.services.users.model.AcmUser;
