9c2bc52d341d63f73a8cba15d7fa106b83964394
Riste Tutureski <riste.tutureski@armedia.com>
2015-02-13 15:22:05 +0100

    AFDP-227: Sending assign/unasign notification by email

--- acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/dao/NotificationDao.java
+++ acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/dao/NotificationDao.java
@@ -3,12 +3,17 @@ package com.armedia.acm.services.notification.dao;
 
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.services.notification.model.Notification;
+import com.armedia.acm.services.notification.model.NotificationConstants;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class NotificationDao extends AcmAbstractDao<Notification>
@@ -69,6 +74,39 @@ public class NotificationDao extends AcmAbstractDao<Notification>
         Notification notificationToBeDeleted = (Notification) queryToDelete.getSingleResult();
     entityManager.remove(notificationToBeDeleted);
 }
+    
+    public List<Notification> executeQuery(Date lastRunDate, int firstResult, int maxResult, String query)
+	{
+    	TypedQuery<Object[]> select = getEm().createQuery(query, Object[].class);
+		
+		select.setParameter("lastRunDate", lastRunDate);
+		select.setFirstResult(firstResult);
+		select.setMaxResults(maxResult);
+		
+		List<Notification> retval = new ArrayList<>();
+		
+		for (Object[] obj : select.getResultList())
+		{
+			Notification notification = new Notification();
+			
+			notification.setUser((String) obj[0]);
+			notification.setTitle((String) obj[1]);
+			notification.setNote((String) obj[2]);
+			notification.setType((String) obj[3]);
+			notification.setParentId((Long) obj[4]);
+			notification.setParentType((String) obj[5]);
+			notification.setParentName((String) obj[6]);
+			notification.setParentTitle((String) obj[7]);
+			notification.setUserEmail((String) obj[8]);
+			notification.setStatus(NotificationConstants.STATUS_NEW);
+			notification.setAction(NotificationConstants.ACTION_DEFAULT);
+			notification.setData("{\"usr\":\"/plugin/" + ((String) obj[5]).toLowerCase() + "/" + ((Long) obj[4]) + "\"}");
+			
+			retval.add(notification);
+		}
+		
+		return retval;
+	}
 
     public EntityManager getEntityManager()
     {
