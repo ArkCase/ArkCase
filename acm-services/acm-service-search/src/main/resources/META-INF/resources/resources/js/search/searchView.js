12f65bcabfa522667a7205ebf59127ebdded1a24
James Wu <jwu@armedia.com>
2015-02-16 12:56:26 -0500

    remove code for test

--- acm-services/acm-service-search/src/main/resources/META-INF/resources/resources/js/search/searchView.js
+++ acm-services/acm-service-search/src/main/resources/META-INF/resources/resources/js/search/searchView.js
@@ -37,91 +37,91 @@ Search.View = {
         }
     }
 
-    ,getJtArgs: function() {
-        return {
-            fields: {
-                id: {
-                    title: 'ID'
-                    ,key: true
-                    ,list: false
-                    ,create: false
-                    ,edit: false
-                    ,sorting: false
-                }
-                ,name: {
-                    title: 'Name5'
-                    ,width: '15%'
-                    ,sorting: false
-                    ,display: function(data) {
-                        return SearchBase.View.Results.displayName(data);
-                    }
-                }
-                ,type: {
-                    title: 'Type'
-                    //,options: [App.OBJTYPE_CASE, App.OBJTYPE_COMPLAINT, App.OBJTYPE_TASK, App.OBJTYPE_DOCUMENT]
-                    ,sorting: false
-                }
-                ,title: {
-                    title: 'Title'
-                    ,width: '30%'
-                }
-                ,parentId: {
-                    title: 'Parent ID'
-                    ,key: false
-                    ,list: false
-                    ,create: false
-                    ,edit: false
-                    ,sorting: false
-                }
-                ,parentName: {
-                    title: 'Parent'
-                    ,width: '15%'
-                    ,sorting: false
-                    ,display: function(data) {
-                        return SearchBase.View.Results.displayParent(data);
-                    }
-                }
-                ,parentType: {
-                    title: 'Parent Type'
-                    ,sorting: false
-                    ,list: false
-                }
-                ,owner: {
-                    title: 'Assignee'
-                    ,width: '15%'
-                    ,sorting: false
-                }
-                ,modified: {
-                    title: 'Modified'
-                    ,type: 'textarea'
-                    ,width: '20%'
-                    ,sorting: false
-                }
-            } //end field
-        };
-    }
-
-    ,jtDataMaker: function(result) {
-        var jtData = AcmEx.Object.JTable.getEmptyRecords();
-        if (result) {
-            for (var i = 0; i < result.docs.length; i++) {
-                var Record = {};
-                Record.id = result.docs[i].object_id_s;
-                Record.name       = Acm.goodValue(result.docs[i].name);
-                Record.type       = Acm.goodValue(result.docs[i].object_type_s);
-                Record.title      = Acm.goodValue(result.docs[i].title_parseable);
-                Record.parentId   = Acm.goodValue(result.docs[i].parent_id_s);
-                Record.parentName = Acm.goodValue(result.docs[i].parent_number_lcs);
-                Record.parentType = Acm.goodValue(result.docs[i].parent_type_s);
-                Record.owner      = Acm.goodValue(result.docs[i].assignee_full_name_lcs); //owner_s
-                Record.modified   = Acm.getDateTimeFromDatetime(result.docs[i].modified_date_tdt);
-                jtData.Records.push(Record);
-            }
-
-            jtData.TotalRecordCount = result.numFound;
-        }
-        return jtData;
-    }
+//    ,getJtArgs: function() {
+//        return {
+//            fields: {
+//                id: {
+//                    title: 'ID'
+//                    ,key: true
+//                    ,list: false
+//                    ,create: false
+//                    ,edit: false
+//                    ,sorting: false
+//                }
+//                ,name: {
+//                    title: 'Name5'
+//                    ,width: '15%'
+//                    ,sorting: false
+//                    ,display: function(data) {
+//                        return SearchBase.View.Results.displayName(data);
+//                    }
+//                }
+//                ,type: {
+//                    title: 'Type'
+//                    //,options: [App.OBJTYPE_CASE, App.OBJTYPE_COMPLAINT, App.OBJTYPE_TASK, App.OBJTYPE_DOCUMENT]
+//                    ,sorting: false
+//                }
+//                ,title: {
+//                    title: 'Title'
+//                    ,width: '30%'
+//                }
+//                ,parentId: {
+//                    title: 'Parent ID'
+//                    ,key: false
+//                    ,list: false
+//                    ,create: false
+//                    ,edit: false
+//                    ,sorting: false
+//                }
+//                ,parentName: {
+//                    title: 'Parent'
+//                    ,width: '15%'
+//                    ,sorting: false
+//                    ,display: function(data) {
+//                        return SearchBase.View.Results.displayParent(data);
+//                    }
+//                }
+//                ,parentType: {
+//                    title: 'Parent Type'
+//                    ,sorting: false
+//                    ,list: false
+//                }
+//                ,owner: {
+//                    title: 'Assignee'
+//                    ,width: '15%'
+//                    ,sorting: false
+//                }
+//                ,modified: {
+//                    title: 'Modified'
+//                    ,type: 'textarea'
+//                    ,width: '20%'
+//                    ,sorting: false
+//                }
+//            } //end field
+//        };
+//    }
+//
+//    ,jtDataMaker: function(result) {
+//        var jtData = AcmEx.Object.JTable.getEmptyRecords();
+//        if (result) {
+//            for (var i = 0; i < result.docs.length; i++) {
+//                var Record = {};
+//                Record.id = result.docs[i].object_id_s;
+//                Record.name       = Acm.goodValue(result.docs[i].name);
+//                Record.type       = Acm.goodValue(result.docs[i].object_type_s);
+//                Record.title      = Acm.goodValue(result.docs[i].title_parseable);
+//                Record.parentId   = Acm.goodValue(result.docs[i].parent_id_s);
+//                Record.parentName = Acm.goodValue(result.docs[i].parent_number_lcs);
+//                Record.parentType = Acm.goodValue(result.docs[i].parent_type_s);
+//                Record.owner      = Acm.goodValue(result.docs[i].assignee_full_name_lcs); //owner_s
+//                Record.modified   = Acm.getDateTimeFromDatetime(result.docs[i].modified_date_tdt);
+//                jtData.Records.push(Record);
+//            }
+//
+//            jtData.TotalRecordCount = result.numFound;
+//        }
+//        return jtData;
+//    }
 
 
 };
