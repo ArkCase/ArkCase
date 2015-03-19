7dc784add37b96fb92f9b210465558de786049a2
Riste Tutureski <riste.tutureski@armedia.com>
2015-03-19 22:32:41 +0100

    AFDP-911: Costsheet services and jUnit tests - Removing MERGE from cascade list for Cost and Time

--- acm-services/acm-service-costsheet/src/main/java/com/armedia/acm/services/costsheet/model/AcmCost.java
+++ acm-services/acm-service-costsheet/src/main/java/com/armedia/acm/services/costsheet/model/AcmCost.java
@@ -38,7 +38,7 @@ public class AcmCost implements Serializable, AcmObject, AcmEntity {
 	private Long id;
 	
 	@JsonIgnore
-    @ManyToOne(cascade = { CascadeType.ALL}, optional = false)
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REMOVE}, optional = false)
     @JoinColumn(name="cm_cost_costsheet_id") 
     private AcmCostsheet costsheet;
 	
