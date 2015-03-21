70d8f55b242b46837217657784e64f3dafbbbfd7
mnj11 <manoj.dhungana@armedia.com>
2015-03-21 16:27:48 -0400

    UI Cost module

--- acm-services/acm-service-costsheet/src/main/resources/META-INF/resources/resources/js/costsheet/costsheetController.js
+++ acm-services/acm-service-costsheet/src/main/resources/META-INF/resources/resources/js/costsheet/costsheetController.js
@@ -8,6 +8,27 @@ Costsheet.Controller = {
     }
     ,onInitialized: function() {
     }
+    ,VIEW_SAVED_DETAIL                   : "costsheet-view-saved-detail"
+    ,viewSavedDetail: function(costsheet, details) {
+        Acm.Dispatcher.fireEvent(this.VIEW_SAVED_DETAIL, costsheet, details);
+    }
+    ,MODEL_SAVED_DETAIL                   : "costsheet-model-saved-detail"
+    ,modelSavedDetail: function(costsheet, details) {
+        Acm.Dispatcher.fireEvent(this.MODEL_SAVED_DETAIL, costsheet, details);
+    }
+    ,VIEW_EDITTED_COSTSHEET               : "costsheet-view-editted-costsheet"
+    ,viewEdittedCostsheet: function(costsheet) {
+        Acm.Dispatcher.fireEvent(this.VIEW_EDITTED_COSTSHEET, costsheet);
+    }
+    ,VIEW_ADDED_COSTSHEET                 : "costsheet-view-added-costsheet"
+    ,viewAddedCostsheet: function(costsheet) {
+        Acm.Dispatcher.fireEvent(this.VIEW_ADDED_COSTSHEET, costsheet);
+    }
+
+    ,MODEL_RETRIEVED_PARENT_OBJECT        : "costsheet-model-retrieved-parent-object"
+    ,modelRetrievedParentObject: function(parentObject) {
+        Acm.Dispatcher.fireEvent(this.MODEL_RETRIEVED_PARENT_OBJECT, parentObject);
+    }
 };
 
 
