605f230946bd9d08e2bf25c79c3090c64db37502
Riste Tutureski <riste.tutureski@armedia.com>
2015-04-28 21:31:46 +0200

    AFDP-993: Change parent id and type for Solr Task docment

--- acm-plugins/acm-default-plugins/acm-task-plugin/src/main/java/com/armedia/acm/plugins/task/service/impl/TaskToSolrTransformer.java
+++ acm-plugins/acm-default-plugins/acm-task-plugin/src/main/java/com/armedia/acm/plugins/task/service/impl/TaskToSolrTransformer.java
@@ -51,11 +51,11 @@ public class TaskToSolrTransformer implements AcmObjectToSolrDocTransformer<AcmT
         doc.setObject_type_s("TASK");
         doc.setObject_sub_type_s(in.getBusinessProcessName());
         doc.setPriority_lcs(in.getPriority());
-        doc.setParent_type_s(in.getAttachedToObjectType());
-        if ( in.getAttachedToObjectId() != null )
+        doc.setParent_type_s(in.getParentObjectType());
+        if ( in.getParentObjectId() != null )
         {
-            doc.setParent_id_s(Long.toString(in.getAttachedToObjectId()));
-            doc.setParent_ref_s(Long.toString(in.getAttachedToObjectId()) + "-" + in.getAttachedToObjectType());
+            doc.setParent_id_s(Long.toString(in.getParentObjectId()));
+            doc.setParent_ref_s(Long.toString(in.getParentObjectId()) + "-" + in.getParentObjectType());
         }
         doc.setName(in.getTitle());
         doc.setStatus_lcs(in.getStatus());
@@ -99,8 +99,8 @@ public class TaskToSolrTransformer implements AcmObjectToSolrDocTransformer<AcmT
         doc.setOwner_s(in.getOwner());
         doc.setId(in.getId() + "-TASK");
         doc.setPriority_s(in.getPriority());
-        doc.setParent_object_type_s(in.getAttachedToObjectType());
-        doc.setParent_object_id_i(in.getAttachedToObjectId());
+        doc.setParent_object_type_s(in.getParentObjectType());
+        doc.setParent_object_id_i(in.getParentObjectId());
 
         if ( in.getAttachedToObjectId() != null )
         {
