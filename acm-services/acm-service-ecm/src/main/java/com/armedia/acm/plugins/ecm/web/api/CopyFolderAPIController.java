efa91558576e359154fd4f368e9e10a3df3fe697
marjan.stefanoski <marjan4jc@gmail.com>
2015-05-01 18:35:53 +0200

    copyFolder service.

---
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/web/api/CopyFolderAPIController.java
@@ -0,0 +1,80 @@
+package com.armedia.acm.plugins.ecm.web.api;
+
+import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
+import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
+import com.armedia.acm.plugins.ecm.model.AcmFolder;
+import com.armedia.acm.plugins.ecm.model.AcmFolderConstants;
+import com.armedia.acm.plugins.ecm.model.EcmFileConstants;
+import com.armedia.acm.plugins.ecm.service.AcmFolderService;
+import com.armedia.acm.plugins.ecm.service.FolderEventPublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by marjan.stefanoski on 01.05.2015.
+ */
+@Controller
+@RequestMapping({"/api/v1/service/ecm", "/api/latest/service/ecm"})
+public class CopyFolderAPIController {
+
+    private transient final Logger log = LoggerFactory.getLogger(getClass());
+
+    private AcmFolderService folderService;
+    private FolderEventPublisher folderEventPublisher;
+
+    @RequestMapping(value = "/folder/copy/{folderId}/{dstFolderId}/{targetObjectType}/{targetObjectId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public AcmFolder copyFolder(
+            @PathVariable("folderId") Long folderId,
+            @PathVariable("dstFolderId") Long dstFolderId,
+            @PathVariable("targetObjectType") String targetObjectType,
+            @PathVariable("targetObjectId") Long targetObjectId,
+            Authentication authentication,
+            HttpSession session
+    ) throws AcmUserActionFailedException {
+
+        if (log.isInfoEnabled()) {
+            log.info("Folder with id: " + folderId + " will be copy into folder with id: " + dstFolderId);
+        }
+        String ipAddress = (String) session.getAttribute(AcmFolderConstants.IP_ADDRESS_ATTRIBUTE);
+        AcmFolder source = getFolderService().findById(folderId);
+        try {
+            if (log.isInfoEnabled()) {
+                log.info("Folder with id: " + folderId + " successfully copied to the location with id: " + dstFolderId);
+            }
+            AcmFolder folder = getFolderService().copyFolder(folderId,dstFolderId,targetObjectId,targetObjectType);
+            getFolderEventPublisher().publishFolderCopiedEvent(source, authentication, ipAddress, true);
+            getFolderEventPublisher().publishFolderCreatedEvent(folder,authentication,ipAddress,true);
+            return folder;
+        } catch ( AcmCreateObjectFailedException | AcmObjectNotFoundException e ) {
+            if (log.isErrorEnabled()) {
+                log.error("Exception occurred while trying to copy folder with id: " + folderId + " to the location with id:" + dstFolderId + "  "+e.getMessage(),e);
+            }
+            getFolderEventPublisher().publishFolderCopiedEvent(source,authentication,ipAddress,false);
+            throw new AcmUserActionFailedException(AcmFolderConstants.USER_ACTION_COPY_FOLDER,AcmFolderConstants.OBJECT_FOLDER_TYPE,source.getId(),"Exception occurred while trying to copy folder",e);
+        }
+    }
+
+    public AcmFolderService getFolderService() {
+        return folderService;
+    }
+
+    public void setFolderService(AcmFolderService folderService) {
+        this.folderService = folderService;
+    }
+
+    public FolderEventPublisher getFolderEventPublisher() {
+        return folderEventPublisher;
+    }
+
+    public void setFolderEventPublisher(FolderEventPublisher folderEventPublisher) {
+        this.folderEventPublisher = folderEventPublisher;
+    }
+}
