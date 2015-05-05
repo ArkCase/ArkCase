90a179d80548622acee9d78599c9ab0405e19f64
David Miller <david.miller@armedia.com>
2015-05-05 14:38:55 -0400

    AFDP-1017 Can no longer create adhoc task - server error: specify the date format for the task date fields.

---
+++ acm-services/acm-service-search/src/test/java/com/armedia/acm/services/search/model/DatePojo.java
@@ -0,0 +1,25 @@
+package com.armedia.acm.services.search.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+/**
+ * Created by armdev on 5/5/15.
+ */
+public class DatePojo
+{
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public Date getDate()
+    {
+        return date;
+    }
+
+    public void setDate(Date date)
+    {
+        this.date = date;
+    }
+}
