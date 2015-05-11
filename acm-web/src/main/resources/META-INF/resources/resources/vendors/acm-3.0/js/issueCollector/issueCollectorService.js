bcf6afd384a62d90066f7e48a0a805be05076ac5
mnj11 <manoj.dhungana@armedia.com>
2015-05-11 18:18:28 -0400

    refactor

--- acm-web/src/main/resources/META-INF/resources/resources/vendors/acm-3.0/js/issueCollector/issueCollectorService.js
+++ acm-web/src/main/resources/META-INF/resources/resources/vendors/acm-3.0/js/issueCollector/issueCollectorService.js
@@ -13,15 +13,13 @@ IssueCollector.Service = {
 
     ,API_GET_JIRA_ISSUE_COLLECTOR             : "http://internal.armedia.com/jira/s/31413758042897b94fd2d74d89768365-T/en_US9cltp4/6346/2/1.4.16/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=aad5f79b"
 
-    ,getIssueCollector: function(issueCollectorFlag) {
-        if(issueCollectorFlag){
-            Acm.Service.ajax({
-                type: "GET"
-                ,url: this.API_GET_JIRA_ISSUE_COLLECTOR
-                ,cache: true
-                ,dataType: "script"
-            });
-        }
+    ,getIssueCollector: function() {
+        Acm.Service.ajax({
+            type: "GET"
+            ,url: this.API_GET_JIRA_ISSUE_COLLECTOR
+            ,cache: true
+            ,dataType: "script"
+        });
     }
 };
 
