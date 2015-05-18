0ff2bef3f77a8929362a02422ed58b360ee576d6
Riste Tutureski <riste.tutureski@armedia.com>
2015-05-18 19:35:46 +0200

    AFDP-1045: PIJ Form (Without saving additional data)

---
+++ acm-forms/acm-form-project/src/main/java/com/armedia/acm/form/project/model/ProjectConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.armedia.acm.form.project.model;
+
+/**
+ * @author riste.tutureski
+ *
+ */
+public interface ProjectConstants {
+
+	public static final String PROJECT = "Project";
+	
+}
