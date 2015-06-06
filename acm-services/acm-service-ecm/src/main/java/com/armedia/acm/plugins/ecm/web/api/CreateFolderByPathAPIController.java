5a76c1b246bdc7e90b92d39092851bce9ecc0d32
David Miller <david.miller@armedia.com>
2015-06-06 14:51:15 -0400

    Lodgement - enhance service to accept an optional list of document IDs which will be moved to the new folder.

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/web/api/CreateFolderByPathAPIController.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/web/api/CreateFolderByPathAPIController.java
@@ -6,8 +6,12 @@ import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
 import com.armedia.acm.plugins.ecm.exception.AcmFolderException;
 import com.armedia.acm.plugins.ecm.model.AcmFolder;
 import com.armedia.acm.plugins.ecm.model.AcmFolderConstants;
+import com.armedia.acm.plugins.ecm.model.EcmFile;
 import com.armedia.acm.plugins.ecm.service.AcmFolderService;
+import com.armedia.acm.plugins.ecm.service.EcmFileService;
+import com.armedia.acm.plugins.ecm.service.FileEventPublisher;
 import com.armedia.acm.plugins.ecm.service.FolderEventPublisher;
+import com.armedia.acm.plugins.ecm.utils.FolderAndFilesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.text.NumberFormat;
 
 /**
  * Created by marjan.stefanoski on 02.04.2015.
@@ -25,8 +30,11 @@ import javax.servlet.http.HttpSession;
 public class CreateFolderByPathAPIController {
 
     private AcmFolderService folderService;
+    private EcmFileService ecmFileService;
+    private FolderAndFilesUtils folderAndFilesUtils;
 
     private FolderEventPublisher folderEventPublisher;
+    private FileEventPublisher fileEventPublisher;
 
     private transient final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -36,6 +44,7 @@ public class CreateFolderByPathAPIController {
             @RequestParam("targetObjectType") String targetObjectType,
             @RequestParam("targetObjectId") Long targetObjectId,
             @RequestParam("newPath") String newPath,
+            @RequestParam(value = "docIds", required = false) String docIds,
             Authentication authentication,
             HttpSession session) throws AcmCreateObjectFailedException, AcmUserActionFailedException, AcmObjectNotFoundException, AcmFolderException {
         /**
@@ -53,6 +62,9 @@ public class CreateFolderByPathAPIController {
             if( log.isInfoEnabled() ) {
                 log.info("Created new folder " + newFolder.getId());
             }
+
+            moveDocumentsToNewFolder(targetObjectType, targetObjectId, docIds, newFolder, authentication, ipAddress);
+
             getFolderEventPublisher().publishFolderCreatedEvent(newFolder,authentication,ipAddress,true);
             return newFolder;
         } catch (  AcmCreateObjectFailedException e) {
@@ -70,6 +82,39 @@ public class CreateFolderByPathAPIController {
         }
     }
 
+    private void moveDocumentsToNewFolder(
+            String targetObjectType,
+            Long targetObjectId,
+            String docIds,
+            AcmFolder newFolder,
+            Authentication auth,
+            String ipAddress)
+            throws AcmUserActionFailedException, AcmObjectNotFoundException, AcmCreateObjectFailedException
+    {
+        if ( docIds != null )
+        {
+            String[] arrDocIds = docIds.split(",");
+            for ( String docId : arrDocIds )
+            {
+                if ( docId == null || docId.trim().isEmpty() )
+                {
+                    continue;
+                }
+
+                Long lngDocId = getFolderAndFilesUtils().convertToLong(docId.trim());
+                if ( lngDocId == null )
+                {
+                    continue;
+                }
+
+                EcmFile moved = getEcmFileService().moveFile(lngDocId, targetObjectId, targetObjectType, newFolder.getId());
+                getFileEventPublisher().publishFileMovedEvent(moved, auth, ipAddress, true);
+
+
+            }
+        }
+    }
+
     public FolderEventPublisher getFolderEventPublisher() {
         return folderEventPublisher;
     }
@@ -84,6 +129,36 @@ public class CreateFolderByPathAPIController {
 
     public void setFolderService(AcmFolderService folderService) {
         this.folderService = folderService;
+    }
+
+    public EcmFileService getEcmFileService()
+    {
+        return ecmFileService;
+    }
+
+    public void setEcmFileService(EcmFileService ecmFileService)
+    {
+        this.ecmFileService = ecmFileService;
+    }
+
+    public FolderAndFilesUtils getFolderAndFilesUtils()
+    {
+        return folderAndFilesUtils;
+    }
+
+    public void setFolderAndFilesUtils(FolderAndFilesUtils folderAndFilesUtils)
+    {
+        this.folderAndFilesUtils = folderAndFilesUtils;
+    }
+
+    public FileEventPublisher getFileEventPublisher()
+    {
+        return fileEventPublisher;
+    }
+
+    public void setFileEventPublisher(FileEventPublisher fileEventPublisher)
+    {
+        this.fileEventPublisher = fileEventPublisher;
     }
 }
 
