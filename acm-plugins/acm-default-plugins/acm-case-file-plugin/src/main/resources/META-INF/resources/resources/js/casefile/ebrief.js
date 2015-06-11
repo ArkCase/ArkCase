edbccb1706a9d802b216db8f351f6c3e80522f20
mnj11 <manoj.dhungana@armedia.com>
2015-06-11 16:07:40 -0400

    email issue fix for lodge and reject

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/resources/META-INF/resources/resources/js/casefile/ebrief.js
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/resources/META-INF/resources/resources/js/casefile/ebrief.js
@@ -474,12 +474,14 @@ CaseFile.prepare = function() {
             this.$btnRefreshDocs = $("#btnRefreshDocs").on("click", function(e) {CaseFile.View.Documents.onClickBtnRefreshDocs(e, this);});
 
             this.$dlgLodgeDocs  = $("#dlgLodgeDocs");
-            this.$edtBmailAddr  = $("#edtBmailAddr");
+            //changes by manoj (not required)
+            //this.$edtBmailAddr  = $("#edtBmailAddr");
             this.$divLodgeDocs  = $("#divLodgeDocs");
             this.createJTableLodgeDocs(this.$divLodgeDocs);
 
             this.$dlgRejectDocs    = $("#dlgRejectDocs");
-            this.$edtBmailReject   = $("#edtBmailReject");
+            //changes by manoj (not required)
+            //this.$edtBmailReject   = $("#edtBmailReject");
             this.$edtRejectReason  = $("#edtRejectReason");
             this.$divRejectDocs    = $("#divRejectDocs");
             this.createJTableRejectDocs(this.$divRejectDocs);
@@ -500,15 +502,29 @@ CaseFile.prepare = function() {
             CaseFile.View.Documents.setValueEdtBmailAddr("");
             AcmEx.Object.JTable.load(CaseFile.View.Documents.$divLodgeDocs);
             Acm.Dialog.modal(CaseFile.View.Documents.$dlgLodgeDocs, function() {
-                var emailAddresses = CaseFile.View.Documents.getValueEdtBmailAddr();
+                //changes by manoj (not required)
+                /*var emailAddresses = CaseFile.View.Documents.getValueEdtBmailAddr();
                 if (Acm.isEmpty(emailAddresses)) {
                     Acm.Dialog.alert("Email Address is required");
                     return;
-                }
+                }*/
                 var nodes = DocTree.View.getEffectiveNodes();
                 if (DocTree.View.validateNodes(nodes)) {
-                    DocTree.Controller.viewSentEmail(emailNotifications);
-                    var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes);
+                    //changes by manoj(added)
+                    var emailAddresses = CaseFile.View.Documents.getCurrentUserEmailAddress();
+                    if (Acm.isNotEmpty(emailAddresses)) {
+                        var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes, "Lodged Document(s)");
+                        if(Acm.isNotEmpty(emailNotifications)){
+                            DocTree.Controller.viewSentEmail(emailNotifications);
+                        }
+                    }
+                    //changes end
+
+                    //changes by manoj (not required)
+                    /*
+                     var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes, reason);
+                     DocTree.Controller.viewSentEmail(emailNotifications);
+                     */
 
                     var folderMap = {};
                     for (var i = 0; i < nodes.length; i++) {
@@ -586,16 +602,32 @@ CaseFile.prepare = function() {
             CaseFile.View.Documents.setValueEdtRejectReason("");
             AcmEx.Object.JTable.load(CaseFile.View.Documents.$divRejectDocs);
             Acm.Dialog.modal(CaseFile.View.Documents.$dlgRejectDocs, function() {
-                var emailAddresses = CaseFile.View.Documents.getValueEdtBmailReject();
+                //changes by manoj (not required)
+
+                /*var emailAddresses = CaseFile.View.Documents.getValueEdtBmailReject();
                 if (Acm.isEmpty(emailAddresses)) {
                     Acm.Dialog.alert("Email Address is required");
                     return;
-                }
+                }*/
                 var reason = CaseFile.View.Documents.getValueEdtRejectReason();
                 var nodes = DocTree.View.getEffectiveNodes();
                 if (DocTree.View.validateNodes(nodes)) {
-                    DocTree.Controller.viewSentEmail(emailNotifications);
-                    var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes, reason);
+
+                    //changes by manoj
+                    var emailAddresses = CaseFile.View.Documents.getCurrentUserEmailAddress();
+                    if (Acm.isNotEmpty(emailAddresses)) {
+                        var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes, "Rejected Document(s)");
+                        if(Acm.isNotEmpty(emailNotifications)){
+                            DocTree.Controller.viewSentEmail(emailNotifications);
+                        }
+                    }
+                    //changes end
+
+                    //changes by manoj (not required)
+                    /*
+                     var emailNotifications = DocTree.View.Email.makeEmailData(emailAddresses, nodes, reason);
+                     DocTree.Controller.viewSentEmail(emailNotifications);
+                     */
 
                     var folderMap = {};
                     for (var i = 0; i < nodes.length; i++) {
@@ -619,6 +651,19 @@ CaseFile.prepare = function() {
                 }
             });
         }
+        //changes by manoj (added)
+
+        ,getCurrentUserEmailAddress: function(){
+            var emailAddresses = [];
+            var profileInfo = Sidebar.Model.Profile.getProfileInfo();
+            if(Acm.isNotEmpty(profileInfo) && Acm.isNotEmpty(profileInfo.email)){
+                emailAddresses.push(Acm.goodValue(profileInfo.email));
+            }
+            return emailAddresses;
+        }
+
+        //changes end
+
         ,onClickBtnRefreshDocs: function(event,ctrl){
             DocTree.View.tree.reload(DocTree.View.Source.source());
         }
