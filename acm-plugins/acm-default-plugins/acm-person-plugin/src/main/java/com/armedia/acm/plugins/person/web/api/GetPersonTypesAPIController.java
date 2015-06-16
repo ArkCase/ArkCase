b2368adc1b8169bed550c443730fd2a12a34a723
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-06-16 19:28:09 +0200

    -drop table ACM_PERSON_TYPE_LU -controller now reads from properties -fixed tests

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/web/api/GetPersonTypesAPIController.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/web/api/GetPersonTypesAPIController.java
@@ -1,60 +1,42 @@
 package com.armedia.acm.plugins.person.web.api;
 
 
-import com.armedia.acm.configuration.ListOfValuesService;
-import com.armedia.acm.configuration.LookupTableDescriptor;
 import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
-import org.springframework.dao.DataAccessException;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 @Controller
-@RequestMapping({ "/api/v1/plugin/person", "/api/latest/plugin/person" })
+@RequestMapping({"/api/v1/plugin/person", "/api/latest/plugin/person"})
 public class GetPersonTypesAPIController {
-    
-    private ListOfValuesService listOfValuesService;
-    private LookupTableDescriptor typesDescriptor;
+
+    private Properties personProperties;
 
     @RequestMapping(
             value = "types",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<String> getPersonTypes() throws AcmListObjectsFailedException
-    {
-        try
-        {
-            List<String> types = getListOfValuesService().lookupListOfStringValues(getTypesDescriptor());
-            return types;
-        }
-        catch (DataAccessException e)
-        {
-            throw new AcmListObjectsFailedException("person types", e.getMessage(), e);
-        }
+    public
+    @ResponseBody
+    List<String> getPersonTypes() throws AcmListObjectsFailedException {
+
+        String commaSeparated = personProperties.getProperty("person.types");
+        if (StringUtils.isEmpty(commaSeparated))
+            throw new AcmListObjectsFailedException("PersonTypes", "person property with key [person.types] is not set or is empty", null);
+
+        String[] retval = commaSeparated.split(",");
+        return Arrays.asList(retval);
+
     }
 
-
-    public ListOfValuesService getListOfValuesService()
-    {
-        return listOfValuesService;
-    }
-
-    public void setListOfValuesService(ListOfValuesService listOfValuesService)
-    {
-        this.listOfValuesService = listOfValuesService;
-    }
-
-    public LookupTableDescriptor getTypesDescriptor()
-    {
-        return typesDescriptor;
-    }
-
-    public void setTypesDescriptor(LookupTableDescriptor typesDescriptor)
-    {
-        this.typesDescriptor = typesDescriptor;
+    public void setPersonProperties(Properties personProperties) {
+        this.personProperties = personProperties;
     }
 }
