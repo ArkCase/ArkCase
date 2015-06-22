05bb69515acf71457243590c7da2635457342639
James Wu <jwu@armedia.com>
2015-06-22 08:58:57 -0400

    comment out retired code

--- acm-services/acm-service-ecm/src/main/resources/META-INF/resources/resources/js/docTree/docTreeService.js
+++ acm-services/acm-service-ecm/src/main/resources/META-INF/resources/resources/js/docTree/docTreeService.js
@@ -29,49 +29,49 @@ DocTree.Service = {
     ,API_LODGE_DOCUMENT               : "/api/latest/service/ecm/createFolderByPath"             //  ?targetObjectType={objType}&targetObjectId={objId}&newPath={fullPath}
 
 
-    ,retrieveFolderListDeferred: function(objType, objId, folderId, pageId, callerData, callbackSuccess) {
-        var setting = DocTree.Model.Config.getSetting();
-        var url = DocTree.Service.API_RETRIEVE_FOLDER_LIST_ + objType + "/" + objId;
-        //var url = DocTree.Service.API_RETRIEVE_FOLDER_LIST_ + objType + "/" + objId;
-        if (0 < folderId) {
-            url += "/" + folderId;
-        }
-        url += "?start=" + pageId;
-        url += "&n=" + DocTree.Model.Config.getMaxRows();
-        if (Acm.isNotEmpty(setting.sortBy) && Acm.isNotEmpty(setting.sortDirection)) {
-            url += "&s=" + setting.sortBy + "&dir=" + setting.sortDirection;
-        }
-
-        return Acm.Service.call({type: "GET"
-            ,url: url
-            ,callback: function(data) {
-                var folderList = null;
-                if (DocTree.Model.validateFolderList(data)) {
-                    folderList = data;
-                    var setting = DocTree.Model.Config.getSetting();
-                    setting.maxRows = Acm.goodValue(folderList.maxRows, 0);
-                    setting.sortBy = Acm.goodValue(folderList.sortBy);
-                    setting.sortDirection = Acm.goodValue(folderList.sortDirection);
-
-                    var cacheKey = DocTree.Model.getCacheKey(folderId, pageId);
-                    DocTree.Model.cacheFolderList.put(cacheKey, folderList);
-                }
-
-                var rc = callbackSuccess(folderList);
-                DocTree.Controller.modelRetrievedFolderList(folderList, objType, objId, folderId, pageId, callerData);
-                return rc;
-            }
-        });
-
-    }
-
-
-    //
-    // folderNode is a concept in View, Model should not use any View object.
-    // This is a hack for now until we find way to get Frevvo to pass data back uploaded files to UI
-    //
-    ,checkUploadForm: function(objType, objId, folderId, pageId, folderNode, fileType) {
-        return DocTree.Service.retrieveFolderListDeferred(objType, objId, folderId, pageId, folderNode, function(folderListLatest) {
+//    ,retrieveFolderListDeferred: function(objType, objId, folderId, pageId, callerData, callbackSuccess) {
+//        var setting = DocTree.Model.Config.getSetting();
+//        var url = DocTree.Service.API_RETRIEVE_FOLDER_LIST_ + objType + "/" + objId;
+//        //var url = DocTree.Service.API_RETRIEVE_FOLDER_LIST_ + objType + "/" + objId;
+//        if (0 < folderId) {
+//            url += "/" + folderId;
+//        }
+//        url += "?start=" + pageId;
+//        url += "&n=" + DocTree.Model.Config.getMaxRows();
+//        if (Acm.isNotEmpty(setting.sortBy) && Acm.isNotEmpty(setting.sortDirection)) {
+//            url += "&s=" + setting.sortBy + "&dir=" + setting.sortDirection;
+//        }
+//
+//        return Acm.Service.call({type: "GET"
+//            ,url: url
+//            ,callback: function(data) {
+//                var folderList = null;
+//                if (DocTree.Model.validateFolderList(data)) {
+//                    folderList = data;
+//                    var setting = DocTree.Model.Config.getSetting();
+//                    setting.maxRows = Acm.goodValue(folderList.maxRows, 0);
+//                    setting.sortBy = Acm.goodValue(folderList.sortBy);
+//                    setting.sortDirection = Acm.goodValue(folderList.sortDirection);
+//
+//                    var cacheKey = DocTree.Model.getCacheKey(folderId, pageId);
+//                    DocTree.Model.cacheFolderList.put(cacheKey, folderList);
+//                }
+//
+//                var rc = callbackSuccess(folderList);
+//                DocTree.Controller.modelRetrievedFolderList(folderList, objType, objId, folderId, pageId, callerData);
+//                return rc;
+//            }
+//        });
+//
+//    }
+//
+//
+//    //
+//    // folderNode is a concept in View, Model should not use any View object.
+//    // This is a hack for now until we find way to get Frevvo to pass data back uploaded files to UI
+//    //
+//    ,checkUploadForm: function(objType, objId, folderId, pageId, folderNode, fileType) {
+//        return DocTree.Service.retrieveFolderListDeferred(objType, objId, folderId, pageId, folderNode, function(folderListLatest) {
 //            var mock = {};
 //            var i = folderListLatest.children.length - 1;
 //            mock.objectId   = folderListLatest.children[i].objectId + 1001;
@@ -87,6 +87,7 @@ DocTree.Service = {
 //            mock.version    = "1.1";
 //            mock.versionList  = [{versionTag:"1.0"},{versionTag:"1.1"}];
 //            folderListLatest.children.push(mock);
+//            folderListLatest.totalChildren++;
 //            mock = {};
 //            i = folderListLatest.children.length - 1;
 //            mock.objectId   = folderListLatest.children[i].objectId + 1002;
@@ -102,34 +103,35 @@ DocTree.Service = {
 //            mock.version    = "1.2";
 //            mock.versionList  = [{versionTag:"1.0"}, {versionTag:"1.1"}, {versionTag:"1.2"}];
 //            folderListLatest.children.push(mock);
-
-            var uploadedFiles = null;
-            if (DocTree.Model.validateFolderList(folderListLatest)) {
-                var newChildren = [];
-                for (var i = folderListLatest.children.length - 1; 0 <= i; i--) {
-                    if (folderListLatest.children[i].type == fileType) {
-                        if (!DocTree.Model.findChildNodeById(folderNode, folderListLatest.children[i].objectId)) { //not found in the tree node, must be newly created
-                            newChildren.push(folderListLatest.children[i]);
-                        }
-                    }
-                }
-                if (!Acm.isArrayEmpty(newChildren)) {
-                    var cacheKey = DocTree.Model.getCacheKey(folderId, pageId);
-                    var folderList = DocTree.Model.cacheFolderList.get(cacheKey);
-                    if (DocTree.Model.validateFolderList(folderList)) {
-                        uploadedFiles = [];
-                        for (var i = 0; i < newChildren.length; i++) {
-                            var uploadedFile = DocTree.Model.fileToSolrData(newChildren[i]);
-                            uploadedFiles.push(uploadedFile);
-                            folderList.children.push(uploadedFile);
-                            folderList.totalChildren++;
-                        }
-                    } //end if validateFolderList
-                } //end if (!Acm.isArrayEmpty(newChildren))
-            }
-            return uploadedFiles;
-        });
-    }
+//            folderListLatest.totalChildren++;
+//
+//            var uploadedFiles = null;
+//            if (DocTree.Model.validateFolderList(folderListLatest)) {
+//                var newChildren = [];
+//                for (var i = folderListLatest.children.length - 1; 0 <= i; i--) {
+//                    if (folderListLatest.children[i].type == fileType) {
+//                        if (!DocTree.View.findChildNodeById(folderNode, folderListLatest.children[i].objectId)) { //not found in the tree node, must be newly created
+//                            newChildren.push(folderListLatest.children[i]);
+//                        }
+//                    }
+//                }
+//                if (!Acm.isArrayEmpty(newChildren)) {
+//                    var cacheKey = DocTree.Model.getCacheKey(folderId, pageId);
+//                    var folderList = DocTree.Model.cacheFolderList.get(cacheKey);
+//                    if (DocTree.Model.validateFolderList(folderList)) {
+//                        uploadedFiles = [];
+//                        for (var i = 0; i < newChildren.length; i++) {
+//                            var uploadedFile = DocTree.Model.fileToSolrData(newChildren[i]);
+//                            uploadedFiles.push(uploadedFile);
+//                            //folderList.children.push(uploadedFile);
+//                            //folderList.totalChildren++;
+//                        }
+//                    } //end if validateFolderList
+//                } //end if (!Acm.isArrayEmpty(newChildren))
+//            }
+//            return uploadedFiles;
+//        });
+//    }
 
 
     ,sendEmail: function(emailNotifications) {
