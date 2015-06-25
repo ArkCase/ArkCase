e127309a57cd52640b5469e68a5900ed745ce3dd
sergeik <Sergei.Kolomiets@gmail.com>
2015-06-24 23:05:50 -0400

    Added Constants interface to RolesPrivileges. Added Links Forms/Workflows table manual columns sizing

---
+++ acm-plugins/acm-default-plugins/acm-admin-plugin/src/main/java/com/armedia/acm/plugins/admin/web/api/RolePrivilegesConstants.java
@@ -0,0 +1,9 @@
+package com.armedia.acm.plugins.admin.web.api;
+
+/**
+ * Created by admin on 6/24/15.
+ */
+public interface RolePrivilegesConstants {
+    String PROP_ROLE_NAME = "roleName";
+    String PROP_APPLICATION_ROLES = "application.roles";
+}
