75cefadd23181c8cc03e8ec905eb58cd3d4c62f3
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-06-26 22:49:50 +0200

    AFDP-1134

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/model/CaseFileConstants.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/model/CaseFileConstants.java
@@ -9,7 +9,9 @@ package com.armedia.acm.plugins.casefile.model;
  */
 public interface CaseFileConstants {
 
-	String OBJECT_TYPE = "CASE_FILE";
+    String CASE_FILE_NUMBER_REGEX_PATTERN = "2\\d\\d\\d[01]\\d[0123]\\d_[\\d]*";
+
+    String OBJECT_TYPE = "CASE_FILE";
 	
 	String ACTIVE_CASE_FORM_KEY = "active.case.form";
 	
