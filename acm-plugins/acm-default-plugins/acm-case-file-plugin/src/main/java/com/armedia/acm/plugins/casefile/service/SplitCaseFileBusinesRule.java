faf792c4fb99ecf4d1a9097427197843aaf76cc8
David Miller <david.miller@armedia.com>
2015-07-01 19:07:10 -0400

    AFDP-1213 - Split on the cloud doesn't work - Error was because default ArkCase rules do not set the case file title; the null title on the copied case caused the problem.  Added Split Case business rules, to control which attributes are copied from the source case to the copy.

---
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/service/SplitCaseFileBusinesRule.java
@@ -0,0 +1,13 @@
+package com.armedia.acm.plugins.casefile.service;
+
+import com.armedia.acm.drools.SimpleStatelessSingleObjectRuleManager;
+import com.armedia.acm.plugins.casefile.model.CaseFile;
+
+import java.util.Map;
+
+/**
+ * Created by armdev on 7/1/15.
+ */
+public class SplitCaseFileBusinesRule extends SimpleStatelessSingleObjectRuleManager<Map<String, CaseFile>>
+{
+}
