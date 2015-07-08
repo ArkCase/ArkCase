bc941f64abfc5c3dc0ff6cb91b3a6ea8789aef41
David Miller <david.miller@armedia.com>
2015-07-08 15:43:51 -0400

    AFDP-1218 - Update official records declared with the old information - add configuration parameters to control each different RMA integration point; added unit tests

--- acm-plugins/acm-extra-plugins/acm-alfresco-rma-integration/src/main/java/com/armedia/acm/plugins/alfrescorma/model/AlfrescoRmaPluginConstants.java
+++ acm-plugins/acm-extra-plugins/acm-alfresco-rma-integration/src/main/java/com/armedia/acm/plugins/alfrescorma/model/AlfrescoRmaPluginConstants.java
@@ -12,4 +12,12 @@ public interface AlfrescoRmaPluginConstants
     String DEFAULT_ORIGINATOR_ORG = "Armedia LLC";
 
     String RECORD_MULE_ENDPOINT = "jms://rmaRecord.in";
+    String FOLDER_MULE_ENDPOINT = "jms://rmaFolder.in";
+
+    String CASE_CLOSED_EVENT = "com.armedia.acm.casefile.event.closed";
+
+    String CASE_CLOSE_INTEGRATION_KEY = "alfresco.rma.declare-records-on-case-close";
+    String COMPLAINT_CLOSE_INTEGRATION_KEY = "alfresco.rma.declare-records-on-complaint-close";
+    String COMPLAINT_FOLDER_INTEGRATION_KEY = "alfresco.rma.create-record-folder-on-complaint-create";
+    String FILE_INTEGRATION_KEY = "alfresco.rma.declare-record-folder-on-file-upload";
 }
--- acm-plugins/acm-extra-plugins/acm-alfresco-rma-integration/src/main/java/com/armedia/acm/plugins/alfrescorma/service/AcmCaseFileStatusChangedListener.java
+++ acm-plugins/acm-extra-plugins/acm-alfresco-rma-integration/src/main/java/com/armedia/acm/plugins/alfrescorma/service/AcmCaseFileStatusChangedListener.java
@@ -1,11 +1,11 @@
 package com.armedia.acm.plugins.alfrescorma.service;
 
+import com.armedia.acm.plugins.alfrescorma.model.AlfrescoRmaPluginConstants;
 import com.armedia.acm.plugins.casefile.model.CaseEvent;
 import com.armedia.acm.plugins.casefile.model.CaseFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 public class AcmCaseFileStatusChangedListener implements ApplicationListener<CaseEvent>
 {
@@ -16,15 +16,20 @@ public class AcmCaseFileStatusChangedListener implements ApplicationListener<Cas
     @Override
     public void onApplicationEvent(CaseEvent event)
     {
-        if ("com.armedia.acm.casefile.event.closed".equals(event.getEventType().toLowerCase()))
+        boolean proceed = getAlfrescoRecordsService().checkIntegrationEnabled(AlfrescoRmaPluginConstants.CASE_CLOSE_INTEGRATION_KEY);
+
+        if ( !proceed )
+        {
+            return;
+        }
+
+        if ( AlfrescoRmaPluginConstants.CASE_CLOSED_EVENT.equals(event.getEventType().toLowerCase()))
         {
             CaseFile caseFile = event.getCaseFile();
 
             if (null != caseFile)
             {
-                UsernamePasswordAuthenticationToken auth =
-                        new UsernamePasswordAuthenticationToken(event.getUserId(), event.getUserId());
-                getAlfrescoRecordsService().declareAllContainerFilesAsRecords(auth, caseFile.getContainer(),
+                getAlfrescoRecordsService().declareAllContainerFilesAsRecords(event.getEventUser(), caseFile.getContainer(),
                         event.getEventDate(), caseFile.getCaseNumber());
 
             }
