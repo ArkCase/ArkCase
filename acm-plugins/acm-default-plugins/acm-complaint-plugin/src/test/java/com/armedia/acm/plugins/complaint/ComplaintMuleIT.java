8d16feb059dbd71f4b3d2b86cdd2a2a8e716607f
David Miller <david.miller@armedia.com>
2015-07-10 13:29:26 -0400

    Library upgrades - new approach for integration tests - More complaint integration test fixes

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/ComplaintMuleIT.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/ComplaintMuleIT.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,26 +23,41 @@ import javax.persistence.PersistenceContext;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-		"/spring/spring-library-object-history.xml",
-		"/spring/spring-library-data-source.xml",
-        "/spring/spring-library-complaint-plugin-test.xml",
-        "/spring/spring-library-complaint-plugin-test-mule.xml",
-        "/spring/spring-library-complaint-plugin-test-ecm.xml",
-        "/spring/spring-library-object-association-plugin.xml",
-        "/spring/spring-library-complaint.xml",
-        "/spring/spring-library-activiti-actions.xml",
-        "/spring/spring-library-activiti-configuration.xml",
-        "/spring/spring-library-folder-watcher.xml",
-        "/spring/spring-library-drools-monitor.xml",
-        "/spring/spring-library-user-service.xml",
-        "/spring/spring-library-context-holder.xml",
-        "/spring/spring-library-particpants.xml",
-        "/spring/spring-library-data-access-control.xml",
-        "/spring/spring-library-search.xml",
-        "/spring/spring-library-property-file-manager.xml",
-        "/spring/spring-library-person.xml",
-        "/spring/spring-library-ecm-file.xml"})
+@ContextHierarchy({
+        @ContextConfiguration(
+                name = "mule",
+                locations = {"/spring/spring-library-complaint-plugin-test-mule.xml",}
+        ),
+        @ContextConfiguration(name = "spring",
+                locations = {
+                        "/spring/spring-library-object-history.xml",
+                        "/spring/spring-library-data-source.xml",
+                        "/spring/spring-library-object-association-plugin.xml",
+                        "/spring/spring-library-complaint-plugin-test.xml",
+                        "/spring/spring-library-complaint.xml",
+                        "/spring/spring-library-activiti-actions.xml",
+                        "/spring/spring-library-activiti-configuration.xml",
+                        "/spring/spring-library-folder-watcher.xml",
+                        "/spring/spring-library-drools-monitor.xml",
+                        "/spring/spring-library-user-service.xml",
+                        "/spring/spring-library-context-holder.xml",
+                        "/spring/spring-library-data-access-control.xml",
+                        "/spring/spring-library-search.xml",
+                        "/spring/spring-library-ecm-file.xml",
+                        "/spring/spring-library-particpants.xml",
+                        "/spring/spring-library-property-file-manager.xml",
+                        "/spring/spring-library-person.xml",
+                        "/spring/spring-library-case-file.xml",
+                        "/spring/spring-library-ms-outlook-integration.xml",
+                        "/spring/spring-library-ms-outlook-plugin.xml",
+                        "/spring/spring-library-profile.xml",
+                        "/spring/spring-library-acm-encryption.xml",
+                        "/spring/spring-library-task.xml",
+                        "/spring/spring-library-note.xml",
+                        "/spring/spring-library-event.xml"
+                })
+        }
+)
 @TransactionConfiguration(defaultRollback = false, transactionManager = "transactionManager")
 public class ComplaintMuleIT
 {
