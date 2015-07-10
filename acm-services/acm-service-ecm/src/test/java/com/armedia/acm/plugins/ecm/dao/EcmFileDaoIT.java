5412ab6b96a13f92e09eb640de590c9cacf19bb5
David Miller <david.miller@armedia.com>
2015-07-10 16:31:40 -0400

    Library upgrades - new approach for integration tests - All integration tests are passing

--- acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/dao/EcmFileDaoIT.java
+++ acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/dao/EcmFileDaoIT.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,16 +27,23 @@ import static org.junit.Assert.*;
  * Created by armdev on 4/22/14.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/spring/spring-library-data-source.xml",
-        "/spring/spring-library-ecm-file.xml",
-        "/spring/spring-library-context-holder.xml",
-        "/spring/spring-library-search.xml",
-        "/spring/spring-library-property-file-manager.xml",
-        "/spring/spring-library-data-access-control.xml",
-        "/spring/spring-library-particpants.xml",
-        "/spring/spring-library-folder-watcher.xml",
-        "/spring/spring-library-activiti-configuration.xml",
-        "/spring/spring-library-drools-monitor.xml"
+@ContextHierarchy({
+        @ContextConfiguration(
+                name = "mule",
+                locations = {"/spring/spring-library-ecm-plugin-test-mule.xml",}
+        ),
+        @ContextConfiguration(name = "spring",
+                locations = {"/spring/spring-library-data-source.xml",
+                        "/spring/spring-library-ecm-file.xml",
+                        "/spring/spring-library-context-holder.xml",
+                        "/spring/spring-library-search.xml",
+                        "/spring/spring-library-property-file-manager.xml",
+                        "/spring/spring-library-data-access-control.xml",
+                        "/spring/spring-library-particpants.xml",
+                        "/spring/spring-library-folder-watcher.xml",
+                        "/spring/spring-library-activiti-configuration.xml",
+                        "/spring/spring-library-drools-monitor.xml"
+                })
 })
 @TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
 public class EcmFileDaoIT
