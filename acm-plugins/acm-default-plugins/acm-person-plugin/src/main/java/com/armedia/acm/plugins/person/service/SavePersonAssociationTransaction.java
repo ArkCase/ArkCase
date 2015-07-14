41c252688a863f1e585b46272bf4b5d2bb01711d
David Miller <david.miller@armedia.com>
2015-07-14 17:48:21 -0400

    AFDP-1165 - Update ArkCase Libraries - fix save person association flow

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/SavePersonAssociationTransaction.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/SavePersonAssociationTransaction.java
@@ -1,19 +1,23 @@
 package com.armedia.acm.plugins.person.service;
 
 import com.armedia.acm.data.AuditPropertyEntityAdapter;
+import com.armedia.acm.muletools.mulecontextmanager.MuleContextManager;
+import com.armedia.acm.plugins.person.dao.PersonAssociationDao;
 import com.armedia.acm.plugins.person.model.PersonAssociation;
-import java.util.HashMap;
-import java.util.Map;
+import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SavePersonAssociationTransaction
 {
-    private MuleClient muleClient;
+    private MuleContextManager muleContextManager;
     private AuditPropertyEntityAdapter auditPropertyEntityAdapter;
+    private PersonAssociationDao personAssociationDao;
 
     @Transactional
     public PersonAssociation savePersonAsssociation(
@@ -24,7 +28,11 @@ public class SavePersonAssociationTransaction
         Map<String, Object> messageProps = new HashMap<>();
         messageProps.put("acmUser", authentication);
         messageProps.put("auditAdapter", getAuditPropertyEntityAdapter());
-        MuleMessage received = getMuleClient().send("vm://savePersonAssociation.in", personAssociation, messageProps);
+        messageProps.put("acmPersonAssociationDao", getPersonAssociationDao());
+
+        MuleMessage request = new DefaultMuleMessage(personAssociation, messageProps, getMuleContextManager().getMuleContext());
+
+        MuleMessage received = getMuleContextManager().getMuleClient().send("vm://savePersonAssociation.in", request);
         PersonAssociation saved = received.getPayload(PersonAssociation.class);
         MuleException e = received.getInboundProperty("saveException");
 
@@ -37,15 +45,6 @@ public class SavePersonAssociationTransaction
 
     }
 
-    public MuleClient getMuleClient()
-    {
-        return muleClient;
-    }
-
-    public void setMuleClient(MuleClient muleClient)
-    {
-        this.muleClient = muleClient;
-    }
 
     public AuditPropertyEntityAdapter getAuditPropertyEntityAdapter()
     {
@@ -55,5 +54,25 @@ public class SavePersonAssociationTransaction
     public void setAuditPropertyEntityAdapter(AuditPropertyEntityAdapter auditPropertyEntityAdapter)
     {
         this.auditPropertyEntityAdapter = auditPropertyEntityAdapter;
+    }
+
+    public MuleContextManager getMuleContextManager()
+    {
+        return muleContextManager;
+    }
+
+    public void setMuleContextManager(MuleContextManager muleContextManager)
+    {
+        this.muleContextManager = muleContextManager;
+    }
+
+    public PersonAssociationDao getPersonAssociationDao()
+    {
+        return personAssociationDao;
+    }
+
+    public void setPersonAssociationDao(PersonAssociationDao personAssociationDao)
+    {
+        this.personAssociationDao = personAssociationDao;
     }
 }
