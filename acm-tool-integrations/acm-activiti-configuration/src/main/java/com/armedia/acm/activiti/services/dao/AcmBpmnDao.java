d34672415d72e4354682ccc1b2e3298fd85395cd
David Miller <david.miller@armedia.com>
2015-07-30 09:46:16 -0400

    Add acm-service-ecm as a dependency in MS Outlook Integration project - code review changes

--- acm-tool-integrations/acm-activiti-configuration/src/main/java/com/armedia/acm/activiti/services/dao/AcmBpmnDao.java
+++ acm-tool-integrations/acm-activiti-configuration/src/main/java/com/armedia/acm/activiti/services/dao/AcmBpmnDao.java
@@ -13,7 +13,8 @@ import java.util.List;
 /**
  * Created by nebojsha on 14.04.2015.
  */
-public class AcmBpmnDao  {
+public class AcmBpmnDao
+{
 
     @PersistenceContext
     private EntityManager em;
@@ -26,7 +27,8 @@ public class AcmBpmnDao  {
     }
 
 
-    public long count() {
+    public long count()
+    {
         String queryText =
                 "SELECT COUNT(apd.id) FROM AcmProcessDefinition apd";
         Query query = getEm().createQuery(queryText);
@@ -34,72 +36,85 @@ public class AcmBpmnDao  {
         return count;
     }
 
-    public List<AcmProcessDefinition> list(String orderBy, boolean isAsc) {
+    public List<AcmProcessDefinition> list(String orderBy, boolean isAsc)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.id in (SELECT MIN(apdid.id) FROM AcmProcessDefinition apdid GROUP BY apdid.key)   ORDER BY apd." + orderBy + (isAsc ? " ASC" : " DESC");
-        Query query = getEm().createQuery(queryText);
+        TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class);
         return query.getResultList();
     }
 
-    public List<AcmProcessDefinition> listPage(int start, int length, String orderBy, boolean isAsc) {
+    public List<AcmProcessDefinition> listPage(int start, int length, String orderBy, boolean isAsc)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.id in (SELECT MIN(apdid.id) FROM AcmProcessDefinition apdid GROUP BY apdid.key)   ORDER BY apd." + orderBy + (isAsc ? " ASC" : " DESC");
-        Query query = getEm().createQuery(queryText).setFirstResult(start).setMaxResults(length);
+        TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class).setFirstResult(start).setMaxResults(length);
         return query.getResultList();
     }
 
-    public List<AcmProcessDefinition> listAllVersions(AcmProcessDefinition processDefinition) {
+    public List<AcmProcessDefinition> listAllVersions(AcmProcessDefinition processDefinition)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.key = :key AND apd.version <> :version ORDER BY apd.version DESC";
-        Query query = getEm().createQuery(queryText);
+        TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class);
         query.setParameter("key", processDefinition.getKey());
         query.setParameter("version", processDefinition.getVersion());
 
         return query.getResultList();
     }
 
-    public void remove(AcmProcessDefinition processDefinition) {
+    public void remove(AcmProcessDefinition processDefinition)
+    {
         getEm().remove(processDefinition);
     }
 
 
-    public AcmProcessDefinition getActive(String processDefinitionKey) {
+    public AcmProcessDefinition getActive(String processDefinitionKey)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.key = :key and apd.active = true";
         TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class);
         query.setParameter("key", processDefinitionKey);
 
-        try {
+        try
+        {
             return query.getSingleResult();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             return null;
         }
     }
 
-    public AcmProcessDefinition getByKeyAndVersion(String key, int version) {
+    public AcmProcessDefinition getByKeyAndVersion(String key, int version)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.key = :key and apd.version =:version";
         TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class);
         query.setParameter("key", key);
         query.setParameter("version", version);
 
-        try {
+        try
+        {
             return query.getSingleResult();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             return null;
         }
     }
 
-    public AcmProcessDefinition getByKeyAndDigest(String key, String digest) {
+    public AcmProcessDefinition getByKeyAndDigest(String key, String digest)
+    {
         String queryText =
                 "SELECT apd FROM AcmProcessDefinition apd WHERE apd.key = :key and apd.md5Hash =:digest";
         TypedQuery<AcmProcessDefinition> query = getEm().createQuery(queryText, AcmProcessDefinition.class);
         query.setParameter("key", key);
         query.setParameter("digest", digest);
 
-        try {
+        try
+        {
             return query.getSingleResult();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             return null;
         }
     }
