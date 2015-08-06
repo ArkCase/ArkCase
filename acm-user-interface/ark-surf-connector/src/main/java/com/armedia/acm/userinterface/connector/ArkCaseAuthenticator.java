7663cdeeac60599a504beda9aaccd2eb181cfe72
Riste Tutureski <riste.tutureski@armedia.com>
2015-08-06 16:20:06 +0200

    Angular UI app - Authentication improvements

--- acm-user-interface/ark-surf-connector/src/main/java/com/armedia/acm/userinterface/connector/ArkCaseAuthenticator.java
+++ acm-user-interface/ark-surf-connector/src/main/java/com/armedia/acm/userinterface/connector/ArkCaseAuthenticator.java
@@ -1,16 +1,12 @@
 package com.armedia.acm.userinterface.connector;
 
+import com.armedia.acm.userinterface.connector.exception.ServiceUnavailableException;
 import com.armedia.acm.userinterface.connector.model.UserInterfaceConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.extensions.surf.exception.AuthenticationException;
-import org.springframework.extensions.webscripts.Status;
-import org.springframework.extensions.webscripts.connector.AbstractAuthenticator;
-import org.springframework.extensions.webscripts.connector.ConnectorSession;
-import org.springframework.extensions.webscripts.connector.Credentials;
-import org.springframework.extensions.webscripts.connector.HttpMethod;
-import org.springframework.extensions.webscripts.connector.RemoteClient;
-import org.springframework.extensions.webscripts.connector.Response;
+import org.springframework.extensions.webscripts.connector.*;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,15 +28,13 @@ public class ArkCaseAuthenticator extends AbstractAuthenticator
 
         if (credentials != null &&
                 (user = (String) credentials.getProperty(Credentials.CREDENTIAL_USERNAME)) != null &&
-                (pass = (String) credentials.getProperty(Credentials.CREDENTIAL_PASSWORD)) != null)
+                (pass = (String) credentials.getProperty(Credentials.CREDENTIAL_PASSWORD)) != null &&
+                !user.isEmpty() && !pass.isEmpty())
         {
             // Build a new remote client
             RemoteClient remoteClient = new RemoteClient(endpoint);
 
-            if (LOG.isDebugEnabled())
-            {
-                LOG.debug("Authenticating user: " + user);
-            }
+            LOG.debug("Authenticating user: " + user);
 
             Map<String, String> requestProperties = new HashMap<String, String>();
             requestProperties.put("Accept", "text/plain");
@@ -50,55 +44,28 @@ public class ArkCaseAuthenticator extends AbstractAuthenticator
             remoteClient.setRequestProperties(requestProperties);
             Response response = remoteClient.call(UserInterfaceConstants.API_LOGIN);
 
-            // read back the ticket
-            if (response.getStatus().getCode() == 200)
+            LOG.debug("Response Code: " + response.getStatus().getCode());
+
+            switch (response.getStatus().getCode())
             {
-                String ticket = null;
-                try
-                {
-                    ticket = response.getResponse();
-                }
-                catch (Exception e)
-                {
-                    throw new AuthenticationException("Unable to retrieve login ticket from ArkCase", e);
-                }
-
-                if (LOG.isDebugEnabled())
-                {
-                    LOG.debug("Ticket: " + ticket);
-                }
-
-                // place the ticket back into the connector session
-                if (connectorSession != null)
-                {
-                    connectorSession.setParameter(UserInterfaceConstants.ACM_TICKET, ticket);
-
-                    // signal that this succeeded
-                    cs = connectorSession;
-                }
-            }
-            else if (response.getStatus().getCode() == Status.STATUS_NO_CONTENT)
-            {
-                if (LOG.isDebugEnabled())
-                {
-                    LOG.debug("SC_NO_CONTENT(204) status received - retreiving auth cookies...");
-                }
-
-                // The login created an empty response, probably with cookies in the connectorSession. We succeeded.
-                processResponse(response, connectorSession);
-                cs = connectorSession;
-            }
-            else
-            {
-                if (LOG.isDebugEnabled())
-                {
-                    LOG.debug("Authentication failed, received response code: " + response.getStatus().getCode());
-                }
+                case 200:
+                    cs = doAuthentication(response, connectorSession);
+                    break;
+                case 401:
+                    LOG.error(UserInterfaceConstants.ERROR_MESSAGE_401);
+                    throw new BadCredentialsException(UserInterfaceConstants.ERROR_MESSAGE_401) {};
+                case 404:
+                    LOG.error(UserInterfaceConstants.ERROR_MESSAGE_404);
+                    throw new ServiceUnavailableException(UserInterfaceConstants.ERROR_MESSAGE_404) {};
+                default:
+                    LOG.error(UserInterfaceConstants.ERROR_MESSAGE_DEFAULT);
+                    throw new AuthenticationException(UserInterfaceConstants.ERROR_MESSAGE_DEFAULT) {};
             }
         }
-        else if (LOG.isDebugEnabled())
+        else
         {
-            LOG.debug("No user credentials available - cannot authenticate.");
+            LOG.error(UserInterfaceConstants.ERROR_MESSAGE_NO_CREDENTIALS);
+            throw new BadCredentialsException(UserInterfaceConstants.ERROR_MESSAGE_NO_CREDENTIALS){};
         }
 
         return cs;
@@ -108,5 +75,28 @@ public class ArkCaseAuthenticator extends AbstractAuthenticator
     public boolean isAuthenticated(String endpoint, ConnectorSession connectorSession)
     {
         return (connectorSession.getParameter(UserInterfaceConstants.ACM_TICKET) != null) || (connectorSession.getCookieNames().length != 0);
+    }
+
+    private ConnectorSession doAuthentication(Response response, ConnectorSession connectorSession) throws AuthenticationException
+    {
+        try
+        {
+            String ticket = response.getResponse();
+            LOG.debug("Ticket: " + ticket);
+
+            if (ticket == null || ticket.isEmpty())
+            {
+                throw new AuthenticationException(UserInterfaceConstants.ERROR_MESSAGE_NO_TICKET){};
+            }
+
+            connectorSession.setParameter(UserInterfaceConstants.ACM_TICKET, ticket);
+
+        }
+        catch (Exception e)
+        {
+            throw new AuthenticationException(UserInterfaceConstants.ERROR_MESSAGE_NO_TICKET){};
+        }
+
+        return connectorSession;
     }
 }
