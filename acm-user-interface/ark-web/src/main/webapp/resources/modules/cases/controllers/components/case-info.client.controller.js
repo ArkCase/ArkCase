7197cee3732a8edee4bcbcf48affcf0bbece38b9
James Wu <jwu@armedia.com>
2015-08-25 10:45:53 -0400

    Add xeditable and summernote to case page

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-info.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-info.client.controller.js
@@ -2,30 +2,46 @@
 
 angular.module('cases').controller('CaseInfoController', ['$scope', '$stateParams', 'ConfigService', 'CasesService',
     function($scope, $stateParams, ConfigService, CasesService) {
-        $scope.$on('component-config', applyConfig);
-        $scope.$on('case-selected', onSelectedCase);
         $scope.$emit('req-component-config', 'info');
-        $scope.components = null;
+
         $scope.config = null;
-
-        $scope.case = null;
-        $scope.caseInfo = null;
-
+        $scope.$on('component-config', applyConfig);
         function applyConfig(e, componentId, config) {
             if (componentId == 'info') {
                 $scope.config = config;
             }
         }
 
-        function onSelectedCase(e, selectedCase){
-            if (!$scope.case || $scope.case.object_id_s != selectedCase.object_id_s) {
-                $scope.case = selectedCase;
-                $scope.caseInfo = CasesService.get({id: selectedCase.object_id_s});
-            }
-        };
-
+        $scope.components = null;
         ConfigService.getModule({moduleId: 'cases'}, function(moduleConfig){
             $scope.components = moduleConfig.components;
         });
+
+
+        $scope.kase = null;
+        $scope.caseInfo = null;
+        $scope.$on('case-selected', function onSelectedCase(e, selectedCase) {
+            //if (!$scope.case || $scope.case.object_id_s != selectedCase.object_id_s) {
+            $scope.kase = selectedCase;
+            //$scope.caseInfo = CasesService.get({id: selectedCase.object_id_s});
+            //}
+        });
+        $scope.$on('case-retrieved', function(e, data){
+            if (data) {
+                $scope.caseInfo = data;
+            }
+        });
+
+
+        $scope.updateTitle = function() {
+            alert("update case title:" + $scope.caseInfo.title);
+            //return $http.post('/updateTitle', $scope.caseInfo.title);
+        };
+
+        $scope.priorities = [
+            { value: "High"     ,text: "High" }
+            ,{ value: "Medium"  ,text: "Medium" }
+            ,{ value: "Low"     ,text: "Low" }
+        ];
     }
 ]);
