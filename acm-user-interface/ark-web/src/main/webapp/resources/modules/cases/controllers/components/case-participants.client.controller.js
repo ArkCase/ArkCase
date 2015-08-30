6daf3faf8b37015b87b5aac875683e930cd6a9bf
James Wu <jwu@armedia.com>
2015-08-30 16:54:28 -0400

    Convert tab indent to spaces

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-participants.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-participants.client.controller.js
@@ -1,85 +1,85 @@
 'use strict';
 
 angular.module('cases').controller('CaseParticipantsController', ['$scope', '$stateParams', "$q", 'CasesService', 'LookupService',
-	function($scope, $stateParams, $q, CasesService, LookupService) {
-		$scope.$emit('req-component-config', 'participants');
+    function($scope, $stateParams, $q, CasesService, LookupService) {
+        $scope.$emit('req-component-config', 'participants');
 
-		$scope.config = null;
+        $scope.config = null;
         $scope.$on('component-config', applyConfig);
-		function applyConfig(e, componentId, config) {
-			if (componentId == 'participants' && !$scope.config) {
-				$scope.config = config;
-				$scope.gridOptions = {
-					enableColumnResizing: true,
-					enableRowSelection: true,
-					enableRowHeaderSelection: false,
-					multiSelect: false,
-					noUnselect : false,
+        function applyConfig(e, componentId, config) {
+            if (componentId == 'participants' && !$scope.config) {
+                $scope.config = config;
+                $scope.gridOptions = {
+                    enableColumnResizing: true,
+                    enableRowSelection: true,
+                    enableRowHeaderSelection: false,
+                    multiSelect: false,
+                    noUnselect : false,
 
-					paginationPageSizes: config.paginationPageSizes,
-					paginationPageSize: config.paginationPageSize,
-					enableFiltering: config.enableFiltering,
+                    paginationPageSizes: config.paginationPageSizes,
+                    paginationPageSize: config.paginationPageSize,
+                    enableFiltering: config.enableFiltering,
 
-					columnDefs: config.columnDefs,
-					onRegisterApi: function(gridApi) {
-						$scope.gridApi = gridApi;
-						gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
-							Acm.log("do save, newValue=" + newValue);
-							if ("participantTypes" === colDef.lookup) {
-								if ("*" === newValue) {
-									rowEntity.participantNames = [
-										{id: "*", name: "*"}
-									];
-								} else if ("owning group" === newValue) {
-									rowEntity.participantNames = $scope.participantGroups;
-								} else {
-									rowEntity.participantNames = $scope.participantUsers;
-								}
+                    columnDefs: config.columnDefs,
+                    onRegisterApi: function(gridApi) {
+                        $scope.gridApi = gridApi;
+                        gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
+                            Acm.log("do save, newValue=" + newValue);
+                            if ("participantTypes" === colDef.lookup) {
+                                if ("*" === newValue) {
+                                    rowEntity.participantNames = [
+                                        {id: "*", name: "*"}
+                                    ];
+                                } else if ("owning group" === newValue) {
+                                    rowEntity.participantNames = $scope.participantGroups;
+                                } else {
+                                    rowEntity.participantNames = $scope.participantUsers;
+                                }
 
-								$scope.$apply();
-							}
-						});
-					}
-				};
+                                $scope.$apply();
+                            }
+                        });
+                    }
+                };
 
 
                 var promiseTypes = Acm.servicePromise($q, {
-					service: LookupService.getParticipantTypes
-					,callback: function(data){
-						$scope.participantTypes = [{type: "*", name: "*"}];
-						_.forEach(data, function(v, k) {
-							$scope.participantTypes.push({type: k, name: v});
-						});
-						return $scope.participantTypes;
-					}
+                    service: LookupService.getParticipantTypes
+                    ,callback: function(data){
+                        $scope.participantTypes = [{type: "*", name: "*"}];
+                        _.forEach(data, function(v, k) {
+                            $scope.participantTypes.push({type: k, name: v});
+                        });
+                        return $scope.participantTypes;
+                    }
                 });
                 var promiseUsers = Acm.servicePromise($q, {
                     service: LookupService.getUsers
                     ,callback: function(data){
                         $scope.participantUsers = [];
                         var arr = Acm.goodObjValue([data, "response", "docs"], []);
-						for (var i = 0; i < arr.length; i++) {
-							var user = {};
-							user.id = arr[i].object_id_s;
-							user.name = arr[i].name;
-							$scope.participantUsers.push(user);
-						}
+                        for (var i = 0; i < arr.length; i++) {
+                            var user = {};
+                            user.id = arr[i].object_id_s;
+                            user.name = arr[i].name;
+                            $scope.participantUsers.push(user);
+                        }
                         return $scope.participantUsers;
                     }
                 });
                 var promiseGroups = Acm.servicePromise($q, {
                     service: LookupService.getGroups
-					,callback: function(data){
-						$scope.participantGroups = [];
-						var arr = Acm.goodObjValue([data, "response", "docs"], []);
-						for (var i = 0; i < arr.length; i++) {
-							var group = {};
-							group.id = arr[i].object_id_s;
-							group.name = arr[i].name;
-							$scope.participantGroups.push(group);
-						}
-						return $scope.participantGroups;
-					}
+                    ,callback: function(data){
+                        $scope.participantGroups = [];
+                        var arr = Acm.goodObjValue([data, "response", "docs"], []);
+                        for (var i = 0; i < arr.length; i++) {
+                            var group = {};
+                            group.id = arr[i].object_id_s;
+                            group.name = arr[i].name;
+                            $scope.participantGroups.push(group);
+                        }
+                        return $scope.participantGroups;
+                    }
                 });
 
                 $q.all([promiseTypes, promiseUsers, promiseGroups]).then(function(data) {
@@ -90,71 +90,71 @@ angular.module('cases').controller('CaseParticipantsController', ['$scope', '$st
                         if ("participantTypes" == $scope.config.columnDefs[i].lookup) {
                             $scope.gridOptions.columnDefs[i].enableCellEdit = true;
                             $scope.gridOptions.columnDefs[i].editableCellTemplate = "ui-grid/dropdownEditor";
-							$scope.gridOptions.columnDefs[i].editDropdownIdLabel = "type";
-							$scope.gridOptions.columnDefs[i].editDropdownValueLabel = "name";
-							$scope.gridOptions.columnDefs[i].editDropdownOptionsArray = $scope.participantTypes;
-							$scope.gridOptions.columnDefs[i].cellFilter = "mapParticipantTypeName: col.colDef.editDropdownOptionsArray";
+                            $scope.gridOptions.columnDefs[i].editDropdownIdLabel = "type";
+                            $scope.gridOptions.columnDefs[i].editDropdownValueLabel = "name";
+                            $scope.gridOptions.columnDefs[i].editDropdownOptionsArray = $scope.participantTypes;
+                            $scope.gridOptions.columnDefs[i].cellFilter = "mapParticipantTypeName: col.colDef.editDropdownOptionsArray";
 
 
                         } else if ("participantNames" == $scope.config.columnDefs[i].lookup) {
                             $scope.gridOptions.columnDefs[i].enableCellEdit = true;
                             $scope.gridOptions.columnDefs[i].editableCellTemplate = "ui-grid/dropdownEditor";
-							$scope.gridOptions.columnDefs[i].editDropdownValueLabel = "name";
-							$scope.gridOptions.columnDefs[i].editDropdownRowEntityOptionsArrayPath = "participantNames";
-							$scope.gridOptions.columnDefs[i].cellFilter = "mapParticipantName: row.entity.participantNames";
+                            $scope.gridOptions.columnDefs[i].editDropdownValueLabel = "name";
+                            $scope.gridOptions.columnDefs[i].editDropdownRowEntityOptionsArrayPath = "participantNames";
+                            $scope.gridOptions.columnDefs[i].cellFilter = "mapParticipantName: row.entity.participantNames";
                         }
                     }
 
-					//$scope.$apply();
+                    //$scope.$apply();
                 });
 
-			}
-		}
+            }
+        }
 
 
-		$scope.$on('case-retrieved', function(e, data){
-			if (data) {
-				$scope.gridOptions.data = data.participants;
-				_.each($scope.gridOptions.data, function(item) {
-					if ("*" === item.participantType) {
-						item.participantNames = [
-							{id: "*", name: "*"}
-						];
-					} else if ("owning group" === item.participantType) {
-						item.participantNames = $scope.participantGroups;
-					} else {
-						item.participantNames = $scope.participantUsers;
-					}
-				});
-			}
-		});
-	}
+        $scope.$on('case-retrieved', function(e, data){
+            if (data) {
+                $scope.gridOptions.data = data.participants;
+                _.each($scope.gridOptions.data, function(item) {
+                    if ("*" === item.participantType) {
+                        item.participantNames = [
+                            {id: "*", name: "*"}
+                        ];
+                    } else if ("owning group" === item.participantType) {
+                        item.participantNames = $scope.participantGroups;
+                    } else {
+                        item.participantNames = $scope.participantUsers;
+                    }
+                });
+            }
+        });
+    }
 ])
 
 .filter('mapParticipantTypeName', function() {
-	return function(input, typeNames) {
-		var find = _(typeNames).filter(function(typeName) {
-			return typeName.type == input;
-		})
-		.pluck("name")
-		.value()
-		;
+    return function(input, typeNames) {
+        var find = _(typeNames).filter(function(typeName) {
+            return typeName.type == input;
+        })
+        .pluck("name")
+        .value()
+        ;
 
-		return (0 < find.length)? find[0] : input;
-	};
+        return (0 < find.length)? find[0] : input;
+    };
 
 })
 .filter('mapParticipantName', function() {
-	return function(input, participantNames) {
-		var find = _(participantNames).filter(function(participantName) {
-			return participantName.id == input;
-		})
-		.pluck("name")
-		.value()
-		;
+    return function(input, participantNames) {
+        var find = _(participantNames).filter(function(participantName) {
+            return participantName.id == input;
+        })
+        .pluck("name")
+        .value()
+        ;
 
-		return (0 < find.length)? find[0] : input;
-	};
+        return (0 < find.length)? find[0] : input;
+    };
 
 })
 ;
