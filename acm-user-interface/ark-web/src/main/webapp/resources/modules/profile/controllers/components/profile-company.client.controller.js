833aba7771ae15dcd8e62d820514180c47018b09
unknown <alex.mo@ARM-T440p-0267.armedia.com>
2015-08-31 17:29:21 -0400

    Rewriting AngularJS for user profile

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/profile/controllers/components/profile-company.client.controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('profile').controller('CompanyController', ['$scope', 'ConfigService',
+	function($scope, ConfigService) {
+		$scope.config = ConfigService.getModule({moduleId: 'profile'});
+		$scope.$on('req-component-config', onConfigRequest);
+
+		function onConfigRequest(e, componentId) {
+			$scope.config.$promise.then(function(config){
+				var componentConfig = _.find(config.components, {id: componentId})
+				$scope.$broadcast('component-config', componentId, componentConfig);
+			});
+		}
+	}
+]);
+angular.module('profile').run(function(editableOptions, editableThemes) {
+  editableThemes.bs3.inputClass = 'input-sm';
+  editableThemes.bs3.buttonsClass = 'btn-sm';
+  editableOptions.theme = 'bs3';
+});
