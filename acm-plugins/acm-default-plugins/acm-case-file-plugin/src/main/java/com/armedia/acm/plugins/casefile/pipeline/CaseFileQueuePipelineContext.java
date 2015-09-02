45fb80bd4f3200de0ebcc314c5f1eb686574a6bc
David Miller <david.miller@armedia.com>
2015-09-01 22:41:42 -0400

    SBI-578: case file queue management updates

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/pipeline/CaseFileQueuePipelineContext.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/pipeline/CaseFileQueuePipelineContext.java
@@ -5,6 +5,17 @@ import com.armedia.acm.services.pipeline.PipelineContext;
 /**
  * Created by armdev on 8/26/15.
  */
-public class CaseFileQueuePipelineContext implements PipelineContext
+public class CaseFileQueuePipelineContext extends CaseFilePipelineContext implements PipelineContext
 {
+    private String enqueueName;
+
+    public String getEnqueueName()
+    {
+        return enqueueName;
+    }
+
+    public void setEnqueueName(String enqueueName)
+    {
+        this.enqueueName = enqueueName;
+    }
 }
