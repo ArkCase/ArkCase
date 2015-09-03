e0fcf9f43936da1ddf395034fb8d353bf0b2c933
mnj11 <manoj.dhungana@armedia.com>
2015-09-03 11:37:59 -0400

    search page structure, and missing reports file

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/search/config/search.client.routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('search').config(['$stateProvider',
+	function($stateProvider) {
+		$stateProvider.
+		state('search', {
+			url: '/search',
+			templateUrl: 'modules/search/views/search.client.view.html',
+			resolve: {
+				translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+					$translatePartialLoader.addPart('search');
+					return $translate.refresh();
+				}]
+			}
+		});
+	}
+]);
