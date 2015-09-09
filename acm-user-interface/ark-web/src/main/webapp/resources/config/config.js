1e60a59bcaba38ce4a48af6f0044f4c853de5297
sergey <sergei.kolomiets@gmail.com>
2015-09-09 11:34:34 -0400

    UI fixes

--- acm-user-interface/ark-web/src/main/webapp/resources/config/config.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/config/config.js
@@ -11,7 +11,7 @@ var _ = require('lodash'),
  */
 module.exports = _.extend(
     require('./env/all')
-    //, require('./env/' + process.env.NODE_ENV) || {}
+//    ,require('./env/' + process.env.NODE_ENV) || {}
 );
 
 /**
