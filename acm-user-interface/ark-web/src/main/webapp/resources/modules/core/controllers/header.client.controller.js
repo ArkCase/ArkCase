a21cfd0aa4eed5094b25cd7565307c2f2468da03
unknown <alex.mo@ARM-T440p-0267.armedia.com>
2015-09-21 18:37:13 -0400

    This commit include: 1. complete the facet search functionality 2. complete the simple search functionality (search from the header input)
    After this commit, the following work are done:
    1. search from input field in search module
    2. search from input field in header
    3. display result data in ui-grid
    4. display available facets.
    5. allowing facet search by check/unchecked the checkbox for facets.
    
    Is there anything else missing?

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/core/controllers/header.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/core/controllers/header.client.controller.js
@@ -1,18 +1,37 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
-		$scope.authentication = Authentication;
-		$scope.isCollapsed = false;
-		$scope.menu = Menus.getMenu('topbar');
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$state', 'ResultService', 'SearchService',
+    function ($scope, Authentication, Menus, $state, ResultService, SearchService) {
+        $scope.authentication = Authentication;
+        $scope.isCollapsed = false;
+        $scope.menu = Menus.getMenu('topbar');
 
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+        $scope.toggleCollapsibleMenu = function () {
+            $scope.isCollapsed = !$scope.isCollapsed;
+        };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
-	}
+        // Collapsing the menu after navigation
+        $scope.$on('$stateChangeSuccess', function () {
+            $scope.isCollapsed = false;
+        });
+        $scope.search = function () {
+            $state.go('search');
+            serviceCall();
+        };
+        $scope.keyDown = function (event) {
+            if (event.keyCode == 13) {
+                $state.go('search');
+                serviceCall();
+            }
+        };
+        function serviceCall() {
+            SearchService.queryFacetedSearch({
+                input: $scope.inputQuery + '*',
+                start: 0,
+                n: 10},
+            function (data) {
+                ResultService.passData(data, $scope.inputQuery + '*');
+            });
+        }
+    }
 ]);
--- acm-user-interface/ark-web/src/main/webapp/resources/modules/search/services/faceted-search.client.service.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/search/services/faceted-search.client.service.js
@@ -7,7 +7,39 @@ angular.module('services').factory('SearchService', ['$resource',
         return $resource('proxy/arkcase/api/latest/plugin/search', {}, {
             queryFacetedSearch: {
                 method: 'GET',
-                url: "proxy/arkcase/api/v1/plugin/search/facetedSearch?q=:input",
+                url: "proxy/arkcase/api/v1/plugin/search/facetedSearch?q=:input&n=:n&start=:start",
+                cache: true,
+                isArray: false,
+                transformResponse: function (data, headerGetter) {
+                    var result = {};
+                    var searchObj = JSON.parse(data);
+
+                    // Process Faceted fields
+                    if (searchObj && searchObj.facet_counts && searchObj.facet_counts.facet_fields) {
+                        var fields = searchObj.facet_counts.facet_fields;
+                        var newFields = {};
+                        _.forEach(fields, function (items, fieldName) {
+                            // Convert field's dirty array to the array of structures
+                            var newItems = [];
+                            for (var i = 0; i < items.length; i += 2) {
+                                if (items[i + 1] > 0) {
+                                    newItems.push({
+                                        name: items[i],
+                                        count: items[i + 1]
+                                    });
+                                }
+                            }
+                            newFields[fieldName] = newItems;
+                        });
+
+                        searchObj.facet_counts.facet_fields = newFields;
+                    }
+                    return searchObj;
+                }
+            },
+            queryFilteredSearch: {
+                method: 'GET',
+                url: "proxy/arkcase/api/v1/plugin/search/facetedSearch?q=:input&filters=fq=:filters&n=:n&start=:start",
                 cache: true,
                 isArray: false,
                 transformResponse: function (data, headerGetter) {
