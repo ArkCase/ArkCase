85664821c37a74a962dccca475d6c7be3b596805
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-10-01 17:41:46 +0200

    SBI-805 -  Get Next Order from Queue -added two new methods in objectLockService for retrieving documents with and without lock -created tests for them

--- acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
+++ acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
@@ -1,13 +1,44 @@
 package com.armedia.acm.service.objectlock.service;
 
 import com.armedia.acm.service.objectlock.model.AcmObjectLock;
+import org.mule.api.MuleException;
 import org.springframework.security.core.Authentication;
 
 /**
  * Created by nebojsha on 25.08.2015.
  */
-public interface AcmObjectLockService {
+public interface AcmObjectLockService
+{
     AcmObjectLock createLock(Long objectId, String objectType, Authentication auth);
 
     void removeLock(Long objectId, String objectType);
+
+    /**
+     * queries documents which doesn't have lock.
+     *
+     * @param objectType document object type. Can be null, than returns every document.
+     * @param auth       Authentication
+     * @param firstRow   start row
+     * @param maxRows    max rows
+     * @param sort       name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams   additional filter. Can be null.
+     * @return return response from solr as String
+     * @throws MuleException
+     */
+    String getDocumentsWithoutLock(String objectType, Authentication auth, int firstRow, int maxRows, String sort, String fqParams) throws MuleException;
+
+    /**
+     * queries documents which doesn't have lock.
+     *
+     * @param objectType       document object type. Can be null, than returns every document.
+     * @param auth             Authentication
+     * @param filterByAssignee weather to filter documents by assignee
+     * @param firstRow         start row
+     * @param maxRows          max rows
+     * @param sort             name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams         additional filter. Can be null.
+     * @return return response from solr as String
+     * @throws MuleException
+     */
+    String getDocumentsWithLock(String objectType, Authentication auth, boolean filterByAssignee, int firstRow, int maxRows, String sort, String fqParams) throws MuleException;
 }
