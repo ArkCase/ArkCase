7d0c698c11bc9e76b9a344fc2e808560bea716ca
Petar Ilin <petar.ilin@armedia.com>
2015-10-02 16:25:14 +0200

    SBI-601: more PDF-related functionality moved to Arkcase PDF utilities module

--- acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfServiceImpl.java
+++ acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfServiceImpl.java
@@ -1,11 +1,17 @@
 package com.armedia.acm.pdf.service;
 
+import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
 import com.armedia.acm.pdf.PdfServiceException;
+import com.armedia.acm.plugins.ecm.model.EcmFile;
+import com.armedia.acm.plugins.ecm.service.EcmFileService;
 import org.apache.fop.apps.FOPException;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFMergerUtility;
+import org.mule.api.MuleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,8 +25,11 @@ import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.Map;
@@ -36,6 +45,11 @@ public class PdfServiceImpl implements PdfService
      * Logger instance.
      */
     private Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * Ecm file service.
+     */
+    private EcmFileService ecmFileService;
 
     /**
      * Random number generator.
@@ -146,5 +160,92 @@ public class PdfServiceImpl implements PdfService
     {
         Source source = new DOMSource();
         return generatePdf(xslFilename, source, parameters);
+    }
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param is               input stream of the document we are appending
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    @Override
+    public PDDocument append(PDDocument pdDocument, InputStream is, PDFMergerUtility pdfMergerUtility) throws PdfServiceException
+    {
+        try
+        {
+            if (pdDocument == null) // first document
+            {
+                pdDocument = PDDocument.load(is);
+            } else
+            {
+                PDDocument next = PDDocument.load(is);
+                pdfMergerUtility.appendDocument(pdDocument, next);
+                next.close();
+            }
+        } catch (IOException e)
+        {
+            log.error("Unable to generate PDF document", e);
+            throw new PdfServiceException(e);
+        }
+        return pdDocument;
+    }
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param ecmFile          document we are appending as ECM file
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    @Override
+    public PDDocument append(PDDocument pdDocument, EcmFile ecmFile, PDFMergerUtility pdfMergerUtility) throws PdfServiceException
+    {
+        InputStream is = null;
+        try
+        {
+            is = ecmFileService.downloadAsInputStream(ecmFile.getFileId());
+        } catch (MuleException | AcmUserActionFailedException e)
+        {
+            throw new PdfServiceException(e);
+        }
+        return append(pdDocument, is, pdfMergerUtility);
+    }
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param filename         path to the document we are appending
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    @Override
+    public PDDocument append(PDDocument pdDocument, String filename, PDFMergerUtility pdfMergerUtility) throws PdfServiceException
+    {
+        InputStream is = null;
+        try
+        {
+            is = new FileInputStream(filename);
+        } catch (FileNotFoundException e)
+        {
+            throw new PdfServiceException(e);
+        }
+        return append(pdDocument, is, pdfMergerUtility);
+    }
+
+    public EcmFileService getEcmFileService()
+    {
+        return ecmFileService;
+    }
+
+    public void setEcmFileService(EcmFileService ecmFileService)
+    {
+        this.ecmFileService = ecmFileService;
     }
 }
