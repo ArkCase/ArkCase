054db2ec471f804db40edb6ed875937fd55b7e9e
Petar Ilin <petar.ilin@armedia.com>
2015-10-02 16:25:14 +0200

    SBI-601: more PDF-related functionality moved to Arkcase PDF utilities module

--- acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfService.java
+++ acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfService.java
@@ -1,9 +1,13 @@
 package com.armedia.acm.pdf.service;
 
 import com.armedia.acm.pdf.PdfServiceException;
+import com.armedia.acm.plugins.ecm.model.EcmFile;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFMergerUtility;
 
 import javax.xml.transform.Source;
 import java.io.File;
+import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -59,4 +63,38 @@ public interface PdfService
      * @throws PdfServiceException on PDF creation error
      */
     String generatePdf(String xslFilename, Map<String, String> parameters) throws PdfServiceException;
+
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param is               input stream of the document we are appending
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    PDDocument append(PDDocument pdDocument, InputStream is, PDFMergerUtility pdfMergerUtility) throws PdfServiceException;
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param ecmFile          document we are appending as ECM file
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    PDDocument append(PDDocument pdDocument, EcmFile ecmFile, PDFMergerUtility pdfMergerUtility) throws PdfServiceException;
+
+    /**
+     * Append one PDF file to another (incremental merge)
+     *
+     * @param pdDocument       source PDF, the document we are appending to
+     * @param filename         path to the document we are appending
+     * @param pdfMergerUtility PDF merger utility
+     * @return merged document
+     * @throws PdfServiceException on error while merging
+     */
+    PDDocument append(PDDocument pdDocument, String filename, PDFMergerUtility pdfMergerUtility) throws PdfServiceException;
 }
