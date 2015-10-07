1232caa51ede9f17ff90d5fa94d602f084ebf1a5
sergey <sergei.kolomiets@gmail.com>
2015-10-07 17:23:38 -0400

    Added "collapsed" attribute support

--- acm-user-interface/ark-web/src/main/webapp/resources/directives/panel-view/panel-view.client.directive.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/directives/panel-view/panel-view.client.directive.js
@@ -7,13 +7,19 @@ angular.module('directives').directive('panelView', ['$q',
             transclude: true,
             scope: {
                 header: '@',
-                collapsible: '@'
+                collapsible: '@',
+                collapsed: '@'
             },
 
             link: function (scope, element, attrs) {
                 if (!attrs.collapsible) {
                     attrs.collapsible = 'true';
                 }
+
+                if (attrs.collapsed == 'true') {
+                    scope.isCollapsed = true;
+                }
+
                 scope.onCollapseIconClick = function($event) {
                     $event.preventDefault();
                     scope.isCollapsed = !scope.isCollapsed
