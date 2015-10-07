55a558a42134156b3fbe0d76690c73119b4408d1
sergey <sergei.kolomiets@gmail.com>
2015-10-07 16:56:18 -0400

    Added panel directive

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/directives/panel-view/panel-view.client.directive.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('directives').directive('panelView', ['$q',
+    function ($q) {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                header: '@',
+                collapsible: '@'
+            },
+
+            link: function (scope, element, attrs) {
+                if (!attrs.collapsible) {
+                    attrs.collapsible = 'true';
+                }
+                scope.onCollapseIconClick = function($event) {
+                    $event.preventDefault();
+                    scope.isCollapsed = !scope.isCollapsed
+                };
+            },
+
+            templateUrl: 'directives/panel-view/panel-view.client.view.html'
+        };
+    }
+]);
