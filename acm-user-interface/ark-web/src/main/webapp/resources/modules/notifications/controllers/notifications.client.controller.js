c296d284953bf5c274f6a8376b30a391b5ce8ff6
mnj11 <manoj.dhungana@armedia.com>
2015-10-28 20:33:21 -0400

    code cleanup

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/notifications/controllers/notifications.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/notifications/controllers/notifications.client.controller.js
@@ -3,12 +3,6 @@
 angular.module('notifications').controller('NotificationsController', ['$scope', 'ConfigService',
 	function($scope, ConfigService) {
 		$scope.config = ConfigService.getModule({moduleId: 'notifications'});
-		/*$scope.config.$promise.then(function(config){
-			$scope.filter = config.filter;
-			var componentConfig = _.find(config.components, {id: 'notificationsFacetedSearch'})
-			$scope.configured = componentConfig;
-		});*/
-
 		$scope.$on('req-component-config', onConfigRequest);
 
 		function onConfigRequest(e, componentId) {
