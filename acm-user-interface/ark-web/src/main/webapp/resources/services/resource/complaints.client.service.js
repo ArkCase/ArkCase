3b184909ad9adefd6744c339cd270749393ac0ca
James Wu <jwu@armedia.com>
2015-11-05 15:06:18 -0500

    add welcome page

--- acm-user-interface/ark-web/src/main/webapp/resources/services/resource/complaints.client.service.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/services/resource/complaints.client.service.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/**
+ * @ngdoc service
+ * @name services.service:ComplaintsService
+ *
+ * @description
+ *
+ * {@link https://github.com/Armedia/ACM3/blob/develop/acm-user-interface/ark-web/src/main/webapp/resources/services/resource/complaints.client.service.js services/resource/complaints.client.service.js}
+
+ * CasesService includes group of REST calls related to Cases module. Functions are implemented using $resoruce.
+ */
 angular.module('services').factory('ComplaintsService', ['$resource',
 	function($resource) {
 		return $resource('proxy/arkcase/api/latest/plugin',{
@@ -21,9 +31,34 @@ angular.module('services').factory('ComplaintsService', ['$resource',
 				method: 'GET',
 				cache: true,
 				url: 'proxy/arkcase/api/latest/plugin/search/children?parentType=COMPLAINT&childType=TASK&parentId=:id&start=:startWith&n=:count'
-			},
+			}
 
-			queryComplaints: {
+			/**
+			 * @ngdoc method
+			 * @name queryComplaints
+			 * @methodOf services.service:ComplaintsService
+			 *
+			 * @description
+			 * Query list of complaints from SOLR.
+			 *
+			 * @param {Object} params Map of input parameter.
+			 * @param {Number} params.start  zero based index of result starts from
+			 * @param {Number} params.n max number of list to return
+			 * @param {String} params.sort  sort value. Allowed choice is based on backend specification
+			 * @param {String} params.filters  filter value. Allowed choice is based on backend specification
+			 * @param {Function} onSuccess (Optional)Callback function of success query.
+			 * @param {Function} onError (Optional)Callback function when fail.
+			 *
+			 * @returns {Object} Object returned by $resource
+			 */
+			, queryComplaints: {
+				method: 'GET',
+				url: 'proxy/arkcase/api/latest/plugin/search/COMPLAINT?start=:start&n=:n&sort=:sort&filters=:filters',
+				cache: false,
+				isArray: false
+			}
+
+			, queryComplaints_old: {
 				method: 'GET',
 				cache: true,
 				url: 'proxy/arkcase/api/latest/plugin/search/COMPLAINT?start=0&n=50&s=sort-created-date-desc&filters=my-complaints-assigned',
