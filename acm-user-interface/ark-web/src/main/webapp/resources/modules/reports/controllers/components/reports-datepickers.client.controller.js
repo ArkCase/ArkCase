9c22c3aa22d9abfa6d349d1ad314ffd74b5ff5fd
mnj11 <manoj.dhungana@armedia.com>
2015-11-10 10:05:11 -0500

    reports module rewrite

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
@@ -1,11 +1,20 @@
 'use strict';
-
+/**
+ * @ngdoc controller
+ * @name reports.controller:Reports.DatepickersController
+ *
+ * @description
+ * {@link https://github.com/Armedia/ACM3/tree/develop/acm-user-interface/ark-web/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js modules/reports/controllers/components/reports-datepickers.client.controller.js}
+ *
+ * The Reports module's date pickers controller
+ */
 angular.module('cases').controller('Reports.DatepickersController', ['$scope',
     function ($scope) {
         $scope.$on('component-config', applyConfig);
+        $scope.$on('reports-data-retrieved',updateData);
         $scope.$emit('req-component-config', 'datepickers');
-        $scope.config = null;
 
+        $scope.config = null;
         function applyConfig(e, componentId, config) {
             if (componentId == 'datepickers') {
                 $scope.config = config;
@@ -17,10 +26,13 @@ angular.module('cases').controller('Reports.DatepickersController', ['$scope',
         $scope.opened.openedEnd = false;
 
         $scope.open = function ($event, datepicker) {
-            $event.preventDefault();
             $event.stopPropagation();
             $scope.opened[datepicker] = true;
         };
+
+        function updateData(e, reportsData){
+            $scope.reportsData = reportsData;
+        }
 
     }
 ]);
