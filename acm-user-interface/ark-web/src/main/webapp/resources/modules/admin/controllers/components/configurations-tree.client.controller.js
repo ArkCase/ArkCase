4a1019053a174f7f41de772c0ad8708a5dd668f8
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-11-11 16:07:14 +0100

    AFDP-1232-admin-tree-view -used tree view for the admin, which is configured in config.json, and tree. -created empty templates and controllers for all admin components

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/configurations-tree.client.controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('admin').controller('Admin.ConfigurationsTreeController',
+    ['$scope', '$state', '$stateParams', '$log', 'ConfigService', '$q', '$parse',
+        function ($scope, $state, $stateParams, $log, ConfigService, $q, $parse) {
+            function setTitle(treeNode) {
+                if (treeNode.children !== undefined && treeNode.children.length > 0) {
+                    angular.forEach(treeNode.children, function (child) {
+                        setTitle(child);
+                    });
+                }
+                //translate provided title key
+                treeNode["title"] = $parse("'" + treeNode.title + "' | translate")();
+            };
+
+            //data for the tree view
+            $scope.treeData = [];
+            ConfigService.getModule({moduleId: 'admin'}).$promise.then(function (config) {
+                if (config.adminTree) {
+                    var treeData = config.adminTree;
+                    angular.forEach(treeData, function (node) {
+                        setTitle(node)
+                    });
+                     $scope.treeData = treeData;
+                }
+            });
+
+            $scope.selectData = function (selectedData) {
+                if (selectedData.templateName !== undefined) {
+                    $state.go('admin.view-node',
+                        {
+                            nodeName: selectedData.templateName
+                        },
+                        {
+                            inherit: false,
+                            location: true
+                        });
+                }
+            };
+        }
+    ]);
---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/form_workflows.link.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('admin').controller('Admin.FormWorkflowsLinkController', ['$scope',
+    function ($scope) {
+    }
+]);
