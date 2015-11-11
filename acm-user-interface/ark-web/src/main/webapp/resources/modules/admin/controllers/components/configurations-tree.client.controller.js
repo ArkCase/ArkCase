af9eb8106e90510d9ecc9c04abc21aaf1d285169
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-11-11 17:50:29 +0100

    AFDP-1232-admin-module -fixed code review comments

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/configurations-tree.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/configurations-tree.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('admin').controller('Admin.ConfigurationsTreeController',
-    ['$scope', '$state', '$stateParams', '$log', 'ConfigService', '$q', '$parse',
-        function ($scope, $state, $stateParams, $log, ConfigService, $q, $parse) {
+    ['$scope', '$state', '$stateParams', '$log', 'ConfigService', '$q', '$translate',
+        function ($scope, $state, $stateParams, $log, ConfigService, $q, $translate) {
             function setTitle(treeNode) {
                 if (treeNode.children !== undefined && treeNode.children.length > 0) {
                     angular.forEach(treeNode.children, function (child) {
@@ -10,7 +10,8 @@ angular.module('admin').controller('Admin.ConfigurationsTreeController',
                     });
                 }
                 //translate provided title key
-                treeNode["title"] = $parse("'" + treeNode.title + "' | translate")();
+                treeNode["title"] = $translate.instant(treeNode.title);
+
             };
 
             //data for the tree view
@@ -21,7 +22,7 @@ angular.module('admin').controller('Admin.ConfigurationsTreeController',
                     angular.forEach(treeData, function (node) {
                         setTitle(node)
                     });
-                     $scope.treeData = treeData;
+                    $scope.treeData = treeData;
                 }
             });
 
