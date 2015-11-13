3f553e81226fd7ce404e3082a4ca081753ee4bc5
sergey <sergei.kolomiets@gmail.com>
2015-11-13 15:58:17 -0500

    Updated permission service and directive

--- acm-user-interface/ark-web/src/main/webapp/resources/directives/permission/permission.client.directive.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/directives/permission/permission.client.directive.js
@@ -13,7 +13,8 @@
  * Action name consists of 2 parts: module name and acrionName, for example: 'cases.createCase'
  *
  * @param {String} permission Acion name that includes module name, for example: 'cases.createCase'
- * @param {String} [disableaction=disable] Defines action that should be applied to disabled UI control ('disable', 'hide')
+ * @param {String} permission-properties Data object that shhould be passed to permissions service
+ * @param {String} [permission-action=disable] Defines action that should be applied to disabled UI control ('disable', 'hide')
  *
  * @example
  <example>
@@ -32,42 +33,76 @@
 angular.module('directives').directive('permission', ['$q', 'MessageService', 'PermissionsService',
     function ($q, MessageService, PermissionsService) {
         return {
+            priority: 100,
             restrict: 'A',
+            scope: {
+                permission: '@',
+                permissionAction: '@',
+                permissionProperties: '='
+            },
 
-            link: function (scope, element, attrs) {
-                var actionName = attrs.permission;
-                var disableAction = attrs.disableaction;
+            link: {
+                pre: function (scope, element, attrs) {
+                    element.on('click', {
+                            actionName: scope.permission,
+                            element: element
+                        },
+                        onElementClick
+                    );
+                },
 
-                attrs.$observe('disabled', function () {
-                    setPermission(element, actionName, disableAction);
-                });
+                post: function (scope, element, attrs) {
+                    var actionName = scope.permission;
+                    var permissionAction = scope.permissionAction;
 
-                setPermission(element, actionName, disableAction);
-                element.on('click', function(e) {
-                    e.preventDefault();
-                    MessageService.info('Action ' + actionName + ' is disabled');
-                });
+                    scope.$watchCollection('permissionProperties', function (newValue, oldValue) {
+                        $q.when(newValue).then(function (permissionProperties) {
+                            setPermission(element, actionName, permissionProperties, permissionAction);
+                        }, true);
+                    });
+
+                    attrs.$observe('disabled', function () {
+                        setPermission(element, actionName, scope.permissionProperties, permissionAction);
+                    });
+
+                }
             }
         };
 
-        function setPermission(element, actionName, disableAction) {
-            PermissionsService.getActionPermission(actionName)
+        function setPermission(element, actionName, permissionProperties, permissionAction) {
+            PermissionsService.getActionPermission(actionName, permissionProperties)
                 .then(
                     function success(enabled) {
 
                         if (enabled === false) {
                             // Hide element if
-                            if (disableAction == 'hide') {
-                                element.css({'visibility': 'hidden'});
+                            if (permissionAction == 'hide') {
+                                element.css({'display': 'none'});
                             } else {
-                                // Disable element by default
                                 element.attr('disabled', true);
                             }
+                            element.attr('permission-disabled', true);
+                        } else {
+                            // Hide element if
+                            if (permissionAction == 'hide') {
+                                element.css({'display': ''});
+                            } else {
+                                element.attr('disabled', false);
+                            }
+                            element.attr('permission-disabled', false);
                         }
                     }, function error() {
                         debugger;
                     }
                 );
+        };
+
+        function onElementClick(e) {
+            if (e.data.element.attr('permission-disabled') ==='true') {
+                e.stopImmediatePropagation();
+                e.preventDefault();
+                MessageService.info('You are not authorized to take the action ' + e.data.actionName);
+            }
         }
     }
 ]);
--- acm-user-interface/ark-web/src/main/webapp/resources/services/auth/permissions.client.service.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/services/auth/permissions.client.service.js
@@ -1,59 +1,89 @@
 'use strict';
 
-angular.module('services').factory('PermissionsService', ['$q', '$http', '$log',
-    function ($q, $http) {
-        var permissions = {};
+
+angular.module('services').factory('PermissionsService', ['$q', '$http', '$log', 'Authentication',
+    function ($q, $http, $log, Authentication) {
+        // Iniital rules loading
+        var rules = queryRules();
+        var userInfo = Authentication.queryUserInfo();
 
         return {
-            getActionPermission: function (actionName) {
-                var deferred = $q.defer();
-                if (actionName) {
-                    // Get moduleId from action name.
-                    var actionNameItems = actionName.split('.');
-                    if (actionNameItems.length > 1) {
-                        // Check module permissions availability
-                        var moduleId = actionNameItems[0];
-                        var actionId = actionNameItems[1];
-                        if (permissions[moduleId]) {
-                            deferred.resolve(permissions[moduleId][actionId]);
-                        } else {
-                            this.queryModulePermissions(moduleId)
-                                .then(
-                                    function success(modulePermissions){
-                                        permissions = _.extend(permissions, modulePermissions);
-                                        deferred.resolve(permissions[moduleId][actionId]);
-                                    },
-                                    function error(){
-                                        $log.error('Module ' + moduleId + ' is absent');
-                                        permissions[moduleId] = {};
-                                        deferred.reject();
-                                    }
-                                );
-                        }
-                    } else {
-                        deferred.reject();
-                    }
-                } else {
-                    deferred.reject();
+            getActionPermission: function (actionName, objectProperties) {
+                if (!actionName) {
+                    $log.error('Permission Action name is undefined');
+                    return $q.resolve(null);
                 }
-                return deferred.promise;
-            },
 
-            queryModulePermissions: function (moduleId) {
-                var deferred = $q.defer();
+                if (rules && rules.accessControlRuleList && userInfo && userInfo.$resolved) {
+                    return $q.resolve(processAction(actionName, objectProperties));
+                } else {
+                    var deferred = $q.defer();
+                    var rulesPromise = queryRules();
+                    var userInfoPromise = Authentication.queryUserInfo();
 
-                $http.get('modules_config/config/modules/' + moduleId + '/permissions/permissions.json')
-                    .then(
-                        function success(result){
-                            permissions[moduleId] = result.data;
-                            deferred.resolve(result.data);
-                        },
-                        function error(){
-                            deferred.reject();
-                        }
-                    );
-                return deferred.promise;
+                    $q.all([rules, userInfo])
+                        .then(
+                            function success(result) {
+                                rules = result[0].data;
+                                userInfo = result[1];
+                                var permissionResult = processAction(actionName, objectProperties)
+                                deferred.resolve(permissionResult);
+                            },
+                            function error() {
+                                deferred.reject();
+                            }
+                        );
+                    return deferred.promise;
+                }
             }
+        };
+
+
+        /**
+         *
+         * @param {String }actionName
+         * @param {Object} objectProperties
+         * @returns {Boolean} true if action is enabled, or false if action is disabled
+         */
+        function processAction(actionName, objectProperties) {
+            var isEnabled = true;
+            var actions = _.filter(rules.accessControlRuleList, {actionName: actionName});
+            // If actions found
+            if (actions.length > 0) {
+                // Process all found actions objects
+                _.forEach(actions, function (action) {
+                    // TODO Check roles
+
+
+                    isEnabled = true;
+                    // Check objectProperties
+                    if (action.objectProperties) {
+                        _.forEach(action.objectProperties, function (value, key) {
+                            isEnabled = isEnabled && (_.get(objectProperties, key) === value );
+                            // exit from loop if properties are not equal
+                            return isEnabled;
+                        });
+                    }
+                    // Return if action object passed
+                    return !isEnabled;
+                });
+            } else {
+                $log.error('Action ' + actionName + ' was not found in rules list');
+            }
+            return isEnabled;
+        }
+
+        /**
+         * Load permissions rules from server
+         *
+         * @returns {HttpPromise} Future rules object
+         */
+        function queryRules() {
+            return $http({
+                method: 'GET',
+                url: 'test-data/rules.json',
+                cache: true
+            });
         }
     }
 ]);
