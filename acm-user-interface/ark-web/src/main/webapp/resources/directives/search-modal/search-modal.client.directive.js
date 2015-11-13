9f51ae2d27ce31d873b3d5e7271c55a64961b83c
mnj11 <manoj.dhungana@armedia.com>
2015-11-12 22:48:23 -0500

    consider filters sent from config

--- acm-user-interface/ark-web/src/main/webapp/resources/directives/search-modal/search-modal.client.directive.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/directives/search-modal/search-modal.client.directive.js
@@ -32,7 +32,7 @@ angular.module('directives').directive('searchModal', ['SearchService', 'Search.
                 scope.currentFacetSelection = [];
                 scope.selectedItem = null;
                 scope.queryExistingItems = function (){
-                    var query = SearchQueryBuilder.buildFacetedSearchQuery(scope.searchQuery + "*",scope.filter,scope.pageSize,scope.start);
+                    var query = SearchQueryBuilder.buildFacetedSearchQuery(scope.searchQuery + "*",scope.filters,scope.pageSize,scope.start);
                     if(query){
                         SearchService.queryFilteredSearch({
                                 query: query
@@ -60,19 +60,19 @@ angular.module('directives').directive('searchModal', ['SearchService', 'Search.
 
                 scope.selectFacet = function (checked, facet, field){
                     if(checked){
-                        if(scope.filter){
-                            scope.filter += '&fq="' + facet + '":' + field;
+                        if(scope.filters){
+                            scope.filters += '&fq="' + facet + '":' + field;
                         }
                         else{
-                            scope.filter += 'fq="' + facet + '":' + field;
+                            scope.filters += 'fq="' + facet + '":' + field;
                         }
                         scope.queryExistingItems();
                     }else{
-                        if(scope.filter.indexOf('&fq="' + facet + '":' + field) > -1){
-                            scope.filter = scope.filter.split('&fq="' + facet + '":' + field).join('');
+                        if(scope.filters.indexOf('&fq="' + facet + '":' + field) > -1){
+                            scope.filters = scope.filters.split('&fq="' + facet + '":' + field).join('');
                         }
-                        else if(scope.filter.indexOf('fq="' + facet + '":' + field) > -1){
-                            scope.filter='';
+                        else if(scope.filters.indexOf('fq="' + facet + '":' + field) > -1){
+                            scope.filters='';
                         }
                         scope.queryExistingItems();
                     }
@@ -122,6 +122,11 @@ angular.module('directives').directive('searchModal', ['SearchService', 'Search.
                                 scope.pageSize = pageSize;
                                 scope.queryExistingItems();
                             });
+                        }
+                    }
+                    if(scope.gridOptions){
+                        if(scope.filter){
+                            scope.filters = 'fq=' + scope.filter;
                         }
                     }
                 }
