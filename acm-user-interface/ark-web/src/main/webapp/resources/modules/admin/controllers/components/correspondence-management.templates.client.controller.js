eba3ce4f5155f596842b16bb84a823725b11a0c4
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2015-11-21 01:11:55 +0100

    AFDP-1382 -created correspondence management templates component

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/correspondence-management.templates.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/controllers/components/correspondence-management.templates.client.controller.js
@@ -1,6 +1,50 @@
 'use strict';
 
-angular.module('admin').controller('Admin.CorrespondenceManagementTemplatesController', ['$scope',
-    function ($scope) {
+angular.module('admin').controller('Admin.CorrespondenceManagementTemplatesController', ['$scope', 'Admin.CorrespondenceManagementTemplatesService',
+    function ($scope, correspondenceService) {
+
+        $scope.gridOptions = {
+            enableColumnResizing: true,
+            enableRowSelection: true,
+            enableRowHeaderSelection: false,
+            multiSelect: false,
+            noUnselect: false,
+            columnDefs: [],
+            data: []
+        };
+
+        function AddFullPath(data) {
+            angular.forEach(data, function (row, index) {
+                row.fullPath = correspondenceService.fullDownloadPath(row.path);
+            });
+        }
+
+        function ReloadGrid() {
+            var templatesPromise = correspondenceService.retrieveTemplatesList();
+            templatesPromise.then(function (templates) {
+                AddFullPath(templates.data);
+                $scope.gridOptions.data = templates.data;
+            });
+        }
+
+        //get config and init grid settings
+        $scope.config.$promise.then(function (config) {
+            var componentConfig = _.find(config.components, {id: 'correspondenceManagementTemplates'});
+            var columnDefs = componentConfig.columnDefs;
+
+            $scope.gridOptions.columnDefs = columnDefs;
+
+            $scope.upload = upload;
+
+            function upload(files) {
+                $scope.selectedFiles = files;
+                correspondenceService.uploadTemplate(files).then(
+                    function (result) {
+                        ReloadGrid();
+                    }
+                );
+            }
+            ReloadGrid();
+        });
     }
 ]);
---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/services/correspondence-management.templates.client.service.js
@@ -0,0 +1,77 @@
+/**
+ * Created by nebojsha on 11/20/2015.
+ */
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name admin.service:Admin.CorrespondenceManagementTemplatesService
+ *
+ * @description
+ *
+ * {@link https://github.com/Armedia/ACM3/blob/develop/acm-user-interface/ark-web/src/main/webapp/resources/modules/admin/services/correspondence-management.templates.client.service.js modules/admin/services/correspondence-management.templates.client.service.js}
+ *
+ * The Admin.DashboardConfigService provides correspondence Management calls functionality
+ */
+angular.module('admin').service('Admin.CorrespondenceManagementTemplatesService', ['$http', 'Upload',
+    function ($http, Upload) {
+        return ({
+            retrieveTemplatesList: retrieveTemplatesList,
+            fullDownloadPath: fullDownloadPath,
+            uploadTemplate: uploadTemplate
+
+        });
+
+        /**
+         * @ngdoc method
+         * @name retrieveTemplatesList
+         * @methodOf admin.service:Admin.CorrespondenceManagementTemplatesService
+         *
+         * @description
+         * Performs retrieving correspondence management templates.
+         *
+         * @returns {HttpPromise} Future info about widgets
+         */
+        function retrieveTemplatesList() {
+            return $http({
+                method: "GET",
+                url: "proxy/arkcase//api/latest/plugin/admin/template/list"
+            });
+        };
+
+        /**
+         * @ngdoc method
+         * @name fullDownloadPath
+         * @methodOf admin.service:Admin.CorrespondenceManagementTemplatesService
+         *
+         * @description
+         * Performs get full path for download
+         *
+         * @param {String} path file path
+         *
+         * @returns {String} full download path
+         */
+        function fullDownloadPath(path) {
+            return 'proxy/arkcase/api/latest/plugin/admin/template?filePath=' + path;
+        };
+
+        /**
+         * @ngdoc method
+         * @name uploadTemplate
+         * @methodOf admin.service:Admin.CorrespondenceManagementTemplatesService
+         *
+         * @description
+         * Uploads correspondence management template
+         *
+         * @param {array} files array of files
+         *
+         * @returns {HttpPromise} Future info about file upload
+         */
+        function uploadTemplate(files) {
+            return Upload.upload({
+                url: 'proxy/arkcase/api/latest/plugin/admin/template',
+                file: files
+            });
+        };
+    }]);
