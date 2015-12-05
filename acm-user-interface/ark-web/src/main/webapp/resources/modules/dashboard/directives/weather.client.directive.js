1b16e08deee57507245b7e16da3257daa831120d
marjan.stefanoski <marjan4jc@gmail.com>
2015-12-05 11:06:57 +0100

    directive stile changed.

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/directives/weather.client.directive.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/directives/weather.client.directive.js
@@ -27,19 +27,28 @@
  </example>
  */
 
-angular.module("dashboard.weather").directive('weatherIcon', function () {
-    return {
+angular.module("dashboard.weather").directive('weatherIcon', weatherIcon);
+
+function weatherIcon() {
+
+    var directive = {
         restrict: 'EA',
         replace: true,
+        controller: WeatherIconController,
+        controllerAs: 'vm',
+        bindToController: true,
         scope: {
-            imageId: '@id'
+            id: '@id'
         },
-        controller: function ($scope) {
-            $scope.imgurl = function () {
-                var baseUrl = 'assets/img/dashboard/widgets/weather/';
-                return baseUrl + $scope.imageId + ".png";
-            };
-        },
-        template: '<div class="pull-left" "><img ng-src="{{ imgurl() }}"></div>'
+        template: '<div class="pull-left" "><img ng-src="{{ vm.imgurl() }}"></div>'
     };
-})
+    return directive;
+}
+
+function WeatherIconController() {
+    var vm = this;
+    vm.imgurl = function () {
+        var baseUrl = 'assets/img/dashboard/widgets/weather/';
+        return baseUrl + vm.id + ".png";
+    };
+}
