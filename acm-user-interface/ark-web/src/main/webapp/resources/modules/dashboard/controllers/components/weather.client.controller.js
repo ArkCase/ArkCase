31227d321c43ba3a6ba086e8d8e23b1a149ef562
marjan.stefanoski <marjan4jc@gmail.com>
2015-12-05 09:25:10 +0100

    removed commented code

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/weather.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/weather.client.controller.js
@@ -10,21 +10,11 @@ angular.module("dashboard.weather").controller("Dashboard.WeatherController", ["
         function applyConfig(e, componentId, config) {
             if (componentId == 'weather') {
 
-                //var configObj = {
-                //    params: {
-                //        units: config.units,
-                //        callback: "JSON_CALLBACK",
-                //        APPID: config.APPID,
-                //        q: location
-                //    }
-                //}
-
+                var url = "http://api.openweathermap.org/data/2.5/weather";
                 var appid = config.APPID;
                 var units = config.units;
 
                 vm.units = config.units;
-
-                var url = "http://api.openweathermap.org/data/2.5/weather";
 
                 WidgetService.getWeather(url, appid, location, units).then(function (weather) {
                     vm.weather = weather;
