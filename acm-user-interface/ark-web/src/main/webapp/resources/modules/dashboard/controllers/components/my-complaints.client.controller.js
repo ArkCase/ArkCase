b05237729be82fbe8d47ef9f5c649d6afa367c35
marjan.stefanoski <marjan4jc@gmail.com>
2015-12-11 16:14:37 +0100

    my complaints widget.

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/my-complaints.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/my-complaints.client.controller.js
@@ -1,18 +1,78 @@
 'use strict';
 
-angular.module('dashboard.my-complaints', ['adf.provider'])
-    .config(function (dashboardProvider) {
-        dashboardProvider
-            .widget('myComplaints', {
-                title: 'My Complaints',
-                description: 'Displays my complaints',
-                controller: 'Dashboard.MyComplaintsController',
-                controllerAs: 'myComplaints',
-                templateUrl: 'modules/dashboard/views/components/my-complaints.client.view.html'
-            });
-    })
-    .controller('Dashboard.MyComplaintsController', ['$scope',
-        function ($scope) {
+angular.module('dashboard.my-complaints')
+    .controller('Dashboard.MyComplaintsController', ['$scope', '$translate', 'Authentication', 'Dashboard.DashboardService',
+        function ($scope, $translate, Authentication, DashboardService) {
 
+            var vm = this;
+
+            $scope.$on('component-config', applyConfig);
+            $scope.$emit('req-component-config', 'myComplaints');
+            vm.config = null;
+            var userInfo = null;
+
+
+            vm.gridOptions = {
+                enableColumnResizing: true,
+                enableRowSelection: true,
+                enableSelectAll: false,
+                enableRowHeaderSelection: false,
+                useExternalPagination: true,
+                useExternalSorting: true,
+                multiSelect: false,
+                noUnselect: false,
+                columnDefs: [],
+                onRegisterApi: function (gridApi) {
+                    vm.gridApi = gridApi;
+
+                    gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
+                        if (sortColumns.length == 0) {
+                            paginationOptions.sort = null;
+                        } else {
+                            paginationOptions.sortBy = sortColumns[0].name;
+                            paginationOptions.sortDir = sortColumns[0].sort.direction;
+                        }
+                        getPage();
+                    });
+                    gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+                        paginationOptions.pageNumber = newPage;
+                        paginationOptions.pageSize = pageSize;
+                        getPage();
+                    });
+                }
+            };
+
+
+            function applyConfig(e, componentId, config) {
+                if (componentId == 'myComplaints') {
+                    vm.config = config;
+                    vm.gridOptions.columnDefs = config.columnDefs;
+                    vm.gridOptions.enableFiltering = config.enableFiltering;
+                    vm.gridOptions.paginationPageSizes = config.paginationPageSizes;
+                    vm.gridOptions.paginationPageSize = config.paginationPageSize;
+                    paginationOptions.pageSize = config.paginationPageSize;
+
+                    Authentication.queryUserInfo().then(function (responseUserInfo) {
+                        userInfo = responseUserInfo;
+                        getPage();
+                        return userInfo;
+                    });
+                }
+            }
+
+            function getPage() {
+                DashboardService.queryMyComplaints({
+                        userId: userInfo.userId,
+                        sortBy: paginationOptions.sortBy,
+                        sortDir: paginationOptions.sortDir,
+                        startWith: (paginationOptions.pageNumber - 1) * paginationOptions.pageSize,
+                        pageSize: paginationOptions.pageSize
+                    },
+                    function (data) {
+                        vm.gridOptions.data = data.response.docs;
+                        vm.gridOptions.totalItems = data.response.numFound;
+                    }
+                );
+            }
         }
     ]);
