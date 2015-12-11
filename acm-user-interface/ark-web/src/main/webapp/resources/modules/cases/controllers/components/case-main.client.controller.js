a457d5c8f79a9b52747b93395aec92a27d7574d3
maksud.sharif <maksud.sharif@armedia.com>
2015-12-11 15:58:46 -0500

    AFDP-1622 - Create a new widget
    **Do not merge**
    
    Add dashboard to 'Case Overview' page.
    Add new example dashboard widget: "Details"

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-main.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-main.client.controller.js
@@ -1,17 +1,38 @@
 'use strict';
 
-angular.module('cases').controller('Cases.MainController', ['$scope', 'ConfigService',
-	function($scope, ConfigService) {
-		$scope.$emit('req-component-config', 'main');
-		$scope.$on('component-config', function (e, componentId, config) {
-			if (componentId == 'main') {
-				$scope.config = config;
-			}
-		});
-
+angular.module('cases').controller('Cases.MainController', ['$scope', '$translate', 'dashboard', 'ConfigService', 'Dashboard.DashboardService',
+	function($scope, $translate, dashboard, ConfigService, DashboardService) {
 		ConfigService.getModuleConfig("cases").then(function (moduleConfig) {
 			$scope.components = moduleConfig.components;
+			$scope.config = _.find(moduleConfig.components, {id: "main"});
 			return moduleConfig;
+		});
+
+		_.forEach(dashboard.widgets, function(widget, widgetId) {
+			widget.title = $translate.instant('dashboard.widgets.' + widgetId + '.title');
+			widget.description = $translate.instant('dashboard.widgets.' + widgetId + '.description');
+		});
+
+		$scope.dashboard = {
+			structure: '6-6',
+			collapsible: false,
+			maximizable: false,
+			model: {
+				titleTemplateUrl: 'modules/dashboard/views/dashboard-title.client.view.html'
+			}
+		}
+
+		DashboardService.getConfig({}, function (data) {
+			$scope.dashboard.model = angular.fromJson(data.dashboardConfig);
+
+			// Set Dashboard custom title
+			$scope.dashboard.model.titleTemplateUrl = 'modules/dashboard/views/dashboard-title.client.view.html';
+		});
+
+		$scope.$on('adfDashboardChanged', function (event, name, model) {
+			DashboardService.saveConfig({
+				dashboardConfig: angular.toJson(model)
+			});
 		});
 	}
 ]);
