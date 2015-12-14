1027924b2c4eef27d6b61ccedd6639143da025e4
mnj11 <manoj.dhungana@armedia.com>
2015-12-14 12:32:39 -0500

    calendar widget

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/complaints/controllers/components/complaint-calendar.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/complaints/controllers/components/complaint-calendar.client.controller.js
@@ -1,14 +1,76 @@
 'use strict';
 
-angular.module('complaints').controller('Complaints.CalendarController', ['$scope',
-    function ($scope) {
-        var z = 1;
-        return;
+angular.module('complaints').controller('Complaints.CalendarController', ['$scope','$timeout', '$compile', 'uiCalendarConfig', 'Object.CalendarService','Complaint.InfoService',
+    function($scope,$timeout, $compile, uiCalendarConfig, CalendarService,ComplaintInfoService) {
+
         $scope.$emit('req-component-config', 'calendar');
         $scope.$on('component-config', function (e, componentId, config) {
             if (componentId == 'calendar') {
                 $scope.config = config;
             }
         });
+
+        $scope.$on('complaint-updated', function (e, data) {
+            if (ComplaintInfoService.validateComplaintInfo(data)) {
+                $scope.complaintInfo = data;
+            }
+        });
+
+        /* Calendar config object */
+        var currentView = "month";
+        $scope.uiConfig = {
+            calendar:{
+                height: 450,
+                editable: true,
+                header:{
+                    left: 'month agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                buttonText: {
+                    today:    'Today',
+                    month:    'Month',
+                    week:     'Week',
+                    day:      'Day'
+                }
+            }
+        };
+
+        /* Event source that contains calendar events */
+        $scope.events = [];
+
+        /* Event sources array */
+        $scope.eventSources = [$scope.events];
+
+        $scope.$watchCollection('complaintInfo', function(newValue, oldValue){
+            if(newValue && newValue.container){
+                CalendarService.queryCalendarEvents(newValue.container.calendarFolderId).then(function(calendarEvents){
+                    if(calendarEvents.items){
+                        for(var i = 0; i < calendarEvents.items.length; i++){
+                            var calendarEvent = {};
+                            calendarEvent.id = calendarEvents.items[i].id;
+                            calendarEvent.title = calendarEvents.items[i].subject;
+                            calendarEvent.start = calendarEvents.items[i].startDate;
+                            calendarEvent.end = calendarEvents.items[i].endDate;
+                            //calendarEvent.detail = Calendar.View.OutlookCalendar.makeDetail(calendarEvents.items[i]);
+                            calendarEvent.className = "b-l b-2x b-info";
+                            calendarEvent.allDay = calendarEvents.items[i].allDayEvent;
+                            $scope.events.push(calendarEvent);
+                        }
+                    }
+                });
+            }
+        });
+
+        /* Render calendar widget */
+        $scope.renderCalender = function(calendar) {
+            if(uiCalendarConfig.calendars[calendar]){
+                uiCalendarConfig.calendars[calendar].fullCalendar('render');
+            }
+        };
+
+        $timeout(function () {
+            $scope.renderCalender('complaintsCalendar');
+        }, 1000);
     }
 ]);
