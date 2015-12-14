a01c18777d82bf08e70dbd299d5207111f9e615c
Nick Ferguson <nickferguson.naf@gmail.com>
2015-12-14 15:03:48 -0500

    Work begun on Choose Assignee selection window

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/tasks/controllers/components/task-new-task.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/tasks/controllers/components/task-new-task.client.controller.js
@@ -29,9 +29,37 @@ angular.module('tasks').controller('Tasks.NewTaskController', ['$scope', '$state
         $scope.opened = {};
         $scope.opened.openedStart = false;
         $scope.opened.openedEnd = false;
+        $scope.saved = false;
 
         $scope.saveNewTask = function () {
+            $scope.saved = true;
             TaskNewTaskService.saveAdHocTask($scope.config.data);
-        }
+        };
+
+        $scope.userSearch = function () {
+            $event.preventDefault();
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'modules/tasks/views/components/task-user-search.client.view.html',
+                controller: 'Tasks.UserSearchController',
+                size: 'lg',
+                resolve: {
+                    $userSearchScope: function () {
+                        return $scope;
+                    },
+                    $filter: function () {
+                        return $scope.config.userSearch.userFacetFilter;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (chosenUser) {
+                if (chosenUser) {
+                    console.log("A user was chosen for this task");
+                }
+            }, function () {
+                // Cancel button was clicked.
+            });
+        };
     }
 ]);
