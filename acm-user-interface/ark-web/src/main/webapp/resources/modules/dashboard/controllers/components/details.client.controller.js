7458d889a4594538d0a936e68004c7badad8e976
maksud.sharif <maksud.sharif@armedia.com>
2015-12-21 15:10:30 -0500

    Modify details widget to take into account different modules

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/details.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/details.client.controller.js
@@ -12,8 +12,10 @@ angular.module('dashboard.details', ['adf.provider'])
                 }
             );
     })
-    .controller('Dashboard.DetailsController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Case.InfoService','Authentication', 'Dashboard.DashboardService',
-        function ($scope, $translate, $stateParams, Util, CaseInfoService, Authentication, DashboardService) {
+    .controller('Dashboard.DetailsController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Case.InfoService'
+        , 'Complaint.InfoService', 'Task.InfoService', 'CostTracking.InfoService', 'TimeTracking.InfoService', 'Authentication', 'Dashboard.DashboardService',
+        function ($scope, $translate, $stateParams, Util, CaseInfoService, ComplaintInfoService, TaskInfoService
+            , CostTrackingInfoService, TimeTrackingInfoService, Authentication, DashboardService) {
 
             $scope.$on('component-config', applyConfig);
             $scope.$emit('req-component-config', 'main');
@@ -31,17 +33,76 @@ angular.module('dashboard.details', ['adf.provider'])
                     $scope.gridOptions.columnDefs = config.widgets[0].columnDefs; //widgets[0] = details
 
                     //set gridOptions.data
-                    CaseInfoService.getCaseInfo($stateParams.id).then(
-                        function (data) {
-                            $scope.gridOptions.data = [Util.omitNg(data)];
-                            $scope.gridOptions.totalItems = 1;
+                    if ($stateParams.type) {
+                        if ($stateParams.type == "casefile") {
+                            CaseInfoService.getCaseInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = [Util.omitNg(data)];
+                                    $scope.gridOptions.totalItems = 1;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
                         }
-                        , function (error) {
-                            $scope.caseInfo = null;
-                            $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
-                            return error;
+                        else if ($stateParams.type == 'complaint') {
+                            ComplaintInfoService.getComplaintInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = [Util.omitNg(data)];
+                                    $scope.gridOptions.totalItems = 1;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
                         }
-                    );
+                        else if ($stateParams.type == 'task') {
+                            TaskInfoService.getTaskInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = [Util.omitNg(data)];
+                                    $scope.gridOptions.totalItems = 1;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
+                        }
+                        else if ($stateParams.type == 'cost') {
+                            CostTrackingInfoService.getCostTrackingInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = [Util.omitNg(data)];
+                                    $scope.gridOptions.totalItems = 1;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
+                        }
+                        else if ($stateParams.type == 'time') {
+                            TimeTrackingInfoService.getTimeTrackingInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = [Util.omitNg(data)];
+                                    $scope.gridOptions.totalItems = 1;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
+                        }
+                        else {
+                            //do nothing
+                        }
+                    }
                 }
             }
         }
