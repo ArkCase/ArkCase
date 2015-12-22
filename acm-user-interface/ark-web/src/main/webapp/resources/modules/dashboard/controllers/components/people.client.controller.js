3519167a0847e19f2f28f35b288763bd578d8e0f
maksud.sharif <maksud.sharif@armedia.com>
2015-12-21 22:06:41 -0500

    Add People widget
    Refactor widgets to use proper configs when available.

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/people.client.controller.js
@@ -0,0 +1,68 @@
+'use strict';
+
+angular.module('dashboard.people', ['adf.provider'])
+    .config(function (dashboardProvider) {
+        dashboardProvider
+            .widget('people', {
+                    title: 'People Widget',
+                    description: 'Displays people',
+                    controller: 'Dashboard.PeopleController',
+                    reload: true,
+                    templateUrl: 'modules/dashboard/views/components/people.client.view.html'
+                }
+            );
+    })
+    .controller('Dashboard.PeopleController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Case.InfoService', 'Complaint.InfoService','Authentication', 'Dashboard.DashboardService',
+        function ($scope, $translate, $stateParams, Util, CaseInfoService, ComplaintInfoService, Authentication, DashboardService) {
+
+            $scope.$on('component-config', applyConfig);
+            $scope.$emit('req-component-config', 'main');
+            $scope.config = null;
+            //var userInfo = null;
+
+            $scope.gridOptions = {
+                enableColumnResizing: true,
+                columnDefs: []
+            };
+
+            function applyConfig(e, componentId, config) {
+                if (componentId == 'main') {
+                    $scope.config = config;
+                    $scope.gridOptions.columnDefs = config.widgets[1].columnDefs; //widget[1] = people
+
+                    //set gridOptions.data
+                    if ($stateParams.type) {
+                        if ($stateParams.type == "casefile") {
+                            CaseInfoService.getCaseInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = data.personAssociations;
+                                    $scope.gridOptions.totalItems = $scope.gridOptions.data.length;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("cases.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
+                        }
+                        else if ($stateParams.type == 'complaint') {
+                            ComplaintInfoService.getComplaintInfo($stateParams.id).then(
+                                function (data) {
+                                    $scope.gridOptions.data = data.personAssociations;;
+                                    $scope.gridOptions.totalItems = $scope.gridOptions.data.length;
+                                }
+                                , function (error) {
+                                    $scope.caseInfo = null;
+                                    $scope.progressMsg = $translate.instant("complaint.progressError") + " " + id;
+                                    return error;
+                                }
+                            );
+                        }
+                        else {
+                            //do nothing
+                        }
+                    }
+                }
+            }
+        }
+    ]);
