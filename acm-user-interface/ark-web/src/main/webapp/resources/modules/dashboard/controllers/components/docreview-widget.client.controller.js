5048c0ad4861f3519bb828effe4476060ece4169
maksud.sharif <maksud.sharif@armedia.com>
2015-12-23 13:37:27 -0500

    minor fix for docreview widget

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/docreview-widget.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/docreview-widget.client.controller.js
@@ -29,7 +29,7 @@ angular.module('dashboard.docreview', ['adf.provider'])
             function applyConfig(e, componentId, config) {
                 if (componentId == 'main') {
                     $scope.config = config;
-                    $scope.gridOptions.columnDefs = config.widgets[1].columnDefs; //tasks.config.widget[1] = rework
+                    $scope.gridOptions.columnDefs = config.widgets[2].columnDefs; //tasks.config.widget[2] = docsreview
 
                     //set gridOptions.data
                     if ($stateParams.type) {
