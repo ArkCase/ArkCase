f372a808163e9d77211648967a5446289a823adf
Bojan Mickoski <bojan.mickoski@armedia.com>
2015-12-25 14:21:10 +0100

    AFDP-1675: Change state for frevvo forms for all modules

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/frevvo/config/frevvo.client.routes.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/frevvo/config/frevvo.client.routes.js
@@ -19,29 +19,9 @@ angular.module('frevvo').config(['$stateProvider',
                 params: {name: 'new-case'},
                 templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
             })
-            .state('frevvo-edit-case', {
-                url: '/frevvo-edit-case',
-                params: {name: 'edit-case', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
-            .state('frevvo-change-case-status', {
-                url: '/frevvo-change-case-status',
-                params: {name: 'change-case-status', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
-            .state('frevvo-reinvestigate', {
-                url: '/frevvo-reinvestigate',
-                params: {name: 'reinvestigate', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
             .state('frevvo-new-complaint', {
                 url: '/frevvo-new-complaint',
                 params: {name: 'new-complaint'},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
-            .state('frevvo-close-complaint', {
-                url: '/frevvo-close-complaint',
-                params: {name: 'close-complaint', arg: null},
                 templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
             })
             .state('frevvo-new-costsheet', {
@@ -49,31 +29,12 @@ angular.module('frevvo').config(['$stateProvider',
                 params: {name: 'new-costsheet'},
                 templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
             })
-            .state('frevvo-edit-costsheet', {
-                url: '/frevvo-edit-costsheet',
-                params: {name: 'edit-costsheet', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
             .state('frevvo-new-timesheet', {
                 url: '/frevvo-new-timesheet',
                 params: {name: 'new-timesheet'},
                 templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
             })
-            .state('frevvo-edit-timesheet', {
-                url: '/frevvo-edit-timesheet',
-                params: {name: 'edit-timesheet', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
-            .state('frevvo-new-plainform', {
-                url: '/frevvo-new-plainform',
-                params: {name: 'new-plainform', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
-            .state('frevvo-edit-plainform', {
-                url: '/frevvo-edit-plainform',
-                params: {name: 'edit-plainform', arg: null},
-                templateUrl: 'modules/frevvo/views/frevvo.client.view.html'
-            })
+
         ;
     }
 ]);
