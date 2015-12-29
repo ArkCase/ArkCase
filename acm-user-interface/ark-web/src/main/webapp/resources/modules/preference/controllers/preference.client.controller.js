ab3db2ac784e766b5ee58ab88107f1f1c540d799
Nick Ferguson <nickferguson.naf@gmail.com>
2015-12-29 13:25:52 -0500

    TODO: List Widgets instead of modules Toggle buttons for everything Toggle Collapsed || Expanded

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/preference/controllers/preference.client.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('preference').controller('Preference.Controller', ['$scope', 'ConfigService', '$modal',
+    function ($scope, ConfigService, $modal) {
+        $scope.config = ConfigService.getModule({moduleId: 'preference'});
+        $scope.$on('req-component-config', onConfigRequest);
+
+        $scope.$on('module-selected', moduleSelected);
+
+        function moduleSelected(e, module) {
+          $scope.$broadcast('module-selected', module);
+        }
+
+        function onConfigRequest(e, componentId) {
+            $scope.config.$promise.then(function (config) {
+                var componentConfig = _.find(config.components, {id: componentId});
+                $scope.$broadcast('component-config', componentId, componentConfig);
+            });
+        }
+    }
+]);
