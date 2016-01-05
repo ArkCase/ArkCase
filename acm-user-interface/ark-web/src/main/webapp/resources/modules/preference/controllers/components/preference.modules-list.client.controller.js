2d465763d602d119ac84e476326cff7387b9e7e1
Nick Ferguson <nickferguson.naf@gmail.com>
2016-01-05 08:06:05 -0500

    Module filtering and page layout fix, widgets don't list until a module is clicked.

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/preference/controllers/components/preference.modules-list.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/preference/controllers/components/preference.modules-list.client.controller.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('preference').controller('Preference.ModulesListController', ['$scope', '$state', '$stateParams', 'ConfigService',
-  function ($scope, $state, $stateParams, ConfigService) {
+angular.module('preference').controller('Preference.ModulesListController', ['$scope', '$state', '$stateParams', 'ConfigService', 'dashboard',
+  function($scope, $state, $stateParams, ConfigService, dashboard) {
     $scope.modules = ConfigService.queryModules();
     $scope.selectModule = selectModule;
 
     function selectModule(newActive) {
-      var prevActive = _.find($scope.modules, {active: true});
+      var prevActive = _.find($scope.modules, {
+        active: true
+      });
       if (prevActive) {
         prevActive.active = false;
       }
