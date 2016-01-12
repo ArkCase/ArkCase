48acfc1a9dcde66d240fbc8f8a7433e213a3b7c3
maksud.sharif <maksud.sharif@armedia.com>
2016-01-12 13:51:07 -0500

    People widget refactor with config changes

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/people-widget.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/people-widget.client.controller.js
@@ -12,8 +12,10 @@ angular.module('dashboard.people', ['adf.provider'])
                 }
             );
     })
-    .controller('Dashboard.PeopleController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Case.InfoService', 'Complaint.InfoService','Authentication', 'Dashboard.DashboardService',
-        function ($scope, $translate, $stateParams, Util, CaseInfoService, ComplaintInfoService, Authentication, DashboardService) {
+    .controller('Dashboard.PeopleController', ['$scope', '$translate', '$stateParams', '$q', 'UtilService'
+        , 'Case.InfoService', 'Complaint.InfoService','Authentication', 'Dashboard.DashboardService', 'ConfigService',
+        function ($scope, $translate, $stateParams, $q, Util, CaseInfoService, ComplaintInfoService, Authentication
+            , DashboardService, ConfigService) {
 
             $scope.$on('component-config', applyConfig);
             $scope.$emit('req-component-config', 'main');
@@ -24,6 +26,38 @@ angular.module('dashboard.people', ['adf.provider'])
                 enableColumnResizing: true,
                 columnDefs: []
             };
+
+            var promiseConfig;
+            var promiseInfo;
+            var modules = [
+                {name: "CASE_FILE", configName: "cases", getInfo: CaseInfoService.getCaseInfo}
+                ,   {name: "COMPLAINT", configName: "complaints", getInfo: ComplaintInfoService.getComplaintInfo}
+            ];
+
+            var module = _.find(modules, function (module) {
+                return module.name == $stateParams.type;
+            });
+
+            if (module) {
+                promiseConfig = ConfigService.getModuleConfig(module.configName);
+                promiseInfo = module.getInfo($stateParams.id);
+
+                $q.all([promiseConfig, promiseInfo]).then(function (data) {
+                        var config = _.find(data[0].components, {id: "main"});
+                        var info = data[1];
+                        var widgetInfo = _.find(config.widgets, function (widget) {
+                            return widget.id === "people";
+                        });
+                        $scope.config = config;
+                        $scope.gridOptions.columnDefs = widgetInfo.columnDefs;
+                        $scope.gridOptions.data = info.personAssociations;
+                        $scope.gridOptions.totalItems = $scope.gridOptions.data.length;
+                    },
+                    function (err) {
+
+                    }
+                );
+            }
 
             function applyConfig(e, componentId, config) {
                 if (componentId == 'main') {
