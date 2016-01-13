847ecabbb454fb8ab438e7b090b55553a1752fae
maksud.sharif <maksud.sharif@armedia.com>
2016-01-13 09:42:56 -0500

    Modify table object widgets to use new config changes

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/rework-details-widget.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/rework-details-widget.client.controller.js
@@ -12,8 +12,9 @@ angular.module('dashboard.reworkdetails', ['adf.provider'])
                 }
             );
     })
-    .controller('Dashboard.LocationController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Task.InfoService', 'Authentication', 'Dashboard.DashboardService',
-        function ($scope, $translate, $stateParams, Util, TaskInfoService, Authentication, DashboardService) {
+    .controller('Dashboard.LocationController', ['$scope', '$translate', '$stateParams', '$q', 'UtilService', 'Task.InfoService'
+        , 'Authentication', 'Dashboard.DashboardService', 'ConfigService',
+        function ($scope, $translate, $stateParams, $q, Util, TaskInfoService, Authentication, DashboardService, ConfigService) {
 
             $scope.$on('component-config', applyConfig);
             $scope.$emit('req-component-config', 'main');
@@ -24,6 +25,43 @@ angular.module('dashboard.reworkdetails', ['adf.provider'])
                 enableColumnResizing: true,
                 columnDefs: []
             };
+
+            var promiseConfig;
+            var promiseInfo;
+            var modules = [
+                {name: "TASK", configName: "tasks", getInfo: TaskInfoService.getTaskInfo}
+                , {name: "ADHOC", configName: "tasks", getInfo: TaskInfoService.getTaskInfo}
+            ]
+
+            var module = _.find(modules, function (module) {
+                return module.name == $stateParams.type;
+            });
+
+            if (module) {
+                promiseConfig = ConfigService.getModuleConfig(module.configName);
+                promiseInfo = module.getInfo($stateParams.id);
+
+                $q.all([promiseConfig, promiseInfo]).then(function (data) {
+                        var config = _.find(data[0].components, {id: "main"});
+                        var info = data[1];
+                        var widgetInfo = _.find(config.widgets, function (widget) {
+                            return widget.id === "reworkdetails";
+                        });
+                        $scope.config = config;
+                        $scope.gridOptions.columnDefs = widgetInfo.columnDefs;
+
+                        $scope.gridOptions.data = [info];
+                        if(!$scope.gridOptions.data[0].reworkInstructions) {
+                            $scope.gridOptions.data[0].taskStartDate = "";
+                            $scope.gridOptions.data[0].assignee = "";
+                        }
+                        $scope.gridOptions.totalItems = $scope.gridOptions.data ? 1 : 0;
+                    },
+                    function (err) {
+
+                    }
+                );
+            }
 
             function applyConfig(e, componentId, config) {
                 if (componentId == 'main') {
--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/tasks-widget.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/tasks-widget.client.controller.js
@@ -12,10 +12,11 @@ angular.module('dashboard.tasks', ['adf.provider'])
                 }
             );
     })
-    .controller('Dashboard.TasksController', ['$scope', '$translate', '$stateParams', 'UtilService', 'Case.InfoService'
-        , 'Complaint.InfoService','Authentication', 'Dashboard.DashboardService', 'ObjectService', 'Object.TaskService',
-        function ($scope, $translate, $stateParams, Util, CaseInfoService, ComplaintInfoService, Authentication
-            , DashboardService, ObjectService, ObjectTaskService) {
+    .controller('Dashboard.TasksController', ['$scope', '$translate', '$stateParams', '$q', 'UtilService', 'Case.InfoService'
+        , 'Complaint.InfoService','Authentication', 'Dashboard.DashboardService', 'ObjectService', 'Object.TaskService'
+        , 'ConfigService',
+        function ($scope, $translate, $stateParams, $q, Util, CaseInfoService, ComplaintInfoService, Authentication
+            , DashboardService, ObjectService, ObjectTaskService, ConfigService) {
 
             $scope.$on('component-config', applyConfig);
             $scope.$emit('req-component-config', 'main');
@@ -26,6 +27,40 @@ angular.module('dashboard.tasks', ['adf.provider'])
                 enableColumnResizing: true,
                 columnDefs: []
             };
+
+            var promiseConfig;
+            var promiseInfo;
+            var modules = [
+                {name: "CASE_FILE", configName: "cases", getInfo: ObjectTaskService.queryChildTasks, objectType: ObjectService.ObjectTypes.CASE_FILE}
+                , {name: "COMPLAINT", configName: "complaints", getInfo: ObjectTaskService.queryChildTasks, objectType: ObjectService.ObjectTypes.COMPLAINT}
+            ]
+
+            var module = _.find(modules, function (module) {
+                return module.name == $stateParams.type;
+            });
+
+            if (module) {
+                promiseConfig = ConfigService.getModuleConfig(module.configName);
+                promiseInfo = module.getInfo(module.objectType, $stateParams.id, 0, 5);
+
+                $q.all([promiseConfig, promiseInfo]).then(function (data) {
+                        var config = _.find(data[0].components, {id: "main"});
+                        var info = data[1];
+                        var widgetInfo = _.find(config.widgets, function (widget) {
+                            return widget.id === "tasks";
+                        });
+                        $scope.config = config;
+                        $scope.gridOptions.columnDefs = widgetInfo.columnDefs;
+
+                        var tasks = info.response.docs;
+                        $scope.gridOptions.data = tasks;
+                        $scope.gridOptions.totalItems = info.response.numFound;
+                    },
+                    function (err) {
+
+                    }
+                );
+            }
 
             function applyConfig(e, componentId, config) {
                 if (componentId == 'main') {
