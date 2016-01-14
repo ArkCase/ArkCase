8f65626583ddb51824be3d31e8cdaec11804bf13
maksud.sharif <maksud.sharif@armedia.com>
2016-01-14 16:04:53 -0500

    Update preference page to use mock service calls.
    *subject to change based on Marjan's service updates.

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/preference/services/preference.client.service.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/preference/services/preference.client.service.js
@@ -3,22 +3,15 @@
 angular.module('preference').factory('Preference.PreferenceService', ['$resource',
     function ($resource) {
         return $resource('', {}, {
-            getWidgets: {
+            getEnabledWidgets: {
                 method: 'GET',
                 url: 'proxy/arkcase/api/v1/plugin/dashboard/get',
                 data: ''
             },
 
             updateWidgetStatus: {
-                method: 'GET',
-                url: 'proxy/arkcase/api/v1/plugin/casefile/number/by/queue',
-                isArray: false,
-                data: ''
-            },
-
-            saveConfig: {
                 method: 'POST',
-                url: 'proxy/arkcase/api/v1/plugin/dashboard/set'
+                url: 'proxy/arkcase/api/v1/plugin/casefile/number/by/queue'
             }
         })
     }
