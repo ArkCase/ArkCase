0c4bd6de66b9a5c4c250edeffe4692651b1bb5c7
Bojan Mickoski <bojan.mickoski@armedia.com>
2016-01-14 17:55:09 +0100

    AFDP-1713: Tags for Document details module

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/document-details/controllers/components/tags.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/document-details/controllers/components/tags.client.controller.js
@@ -1,8 +1,107 @@
 'use strict';
 
-angular.module('document-details').controller('Document.TagsController', ['$scope', '$stateParams', '$q'
-    , 'UtilService', 'ConfigService', 'Helper.UiGridService', 'ObjectService',
-    function ($scope, $stateParams, $q, Util, ConfigService, HelperUiGridService, ObjectService) {
+angular.module('document-details').controller('Document.TagsController', ['$scope', '$filter', '$stateParams', '$q'
+    , '$modal', 'UtilService', 'ConfigService', 'Helper.UiGridService', 'ObjectService', 'Object.TagsService', 'MessageService', '$translate',
+    function ($scope, $filter, $stateParams, $q, $modal, Util, ConfigService, HelperUiGridService, ObjectService, ObjectTagsService, messageService, $translate) {
+
+        $scope.selectedTag = {};
+        var gridHelper = new HelperUiGridService.Grid({scope: $scope});
+
+        var promiseTypes = ObjectTagsService.retrieveAllTags().then(
+            function (tags) {
+                $scope.createdTags = tags;
+                return tags;
+            }
+        );
+
+        var promiseConfig = ConfigService.getComponentConfig("document-details", "tags").then(function (config) {
+            gridHelper.addDeleteButton(config.columnDefs, "grid.appScope.deleteRow(row.entity)");
+            gridHelper.setColumnDefs(config);
+            gridHelper.setBasicOptions(config);
+            gridHelper.disableGridScrolling(config);
+
+            $scope.retrieveGridData();
+        });
+
+        $scope.retrieveGridData = function () {
+            if (Util.goodPositive($stateParams.id)) {
+                var promiseQueryTags = ObjectTagsService.retrieveAssociatedTags($stateParams.id, ObjectService.ObjectTypes.FILE);
+                $q.all([promiseQueryTags, promiseConfig]).then(function (data) {
+                    $scope.tags = data[0];
+                    $scope.gridOptions = $scope.gridOptions || {};
+                    $scope.gridOptions.data = $scope.tags;
+                    $scope.gridOptions.totalItems = $scope.tags.length;
+                });
+            }
+        };
+
+        $scope.addNew = function () {
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'modules/document-details/views/components/tags-modal.client.view.html',
+                controller: 'Document.TagsModalController',
+                size: 'lg',
+                resolve: {
+                    $scopeTag: function () {
+                        return $scope;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (data) {
+                if ($scope.selectedTag) {
+                    var tagsFound = _.filter($scope.tags, function (tag) {
+                        return tag.id == $scope.selectedTag.id;
+                    });
+                    if (tagsFound.length == 0) {
+                        ObjectTagsService.associateNewTag($stateParams.id, ObjectService.ObjectTypes.FILE, $scope.selectedTag.id).then(
+                            function () {
+                                $scope.tags.push($scope.selectedTag);
+                                $scope.gridOptions.data = $scope.tags;
+                                $scope.gridOptions.totalItems = $scope.tags.length;
+                            }
+                        );
+                    }
+                    else {
+                        messageService.info($translate.instant('documentDetails.comp.tags.message.tagAssociated'));
+                    }
+                }
+                else {
+                    var tagsCreated = _.filter($scope.createdTags, function (tag) {
+                        return tag.tagName == data.tag.name || tag.tagDescription == data.tag.description
+                            || tag.tagText == data.tag.text;
+                    });
+                    if (tagsCreated.length == 0) {
+                        ObjectTagsService.createNewTag(data.tag.name, data.tag.description, data.tag.text).then(
+                            function (tagCreated) {
+                                ObjectTagsService.associateNewTag($stateParams.id, ObjectService.ObjectTypes.FILE, tagCreated.id).then(
+                                    function () {
+                                        $scope.tags.push(tagCreated);
+                                        $scope.gridOptions.data = $scope.tags;
+                                        $scope.gridOptions.totalItems = $scope.tags.length;
+                                    }
+                                );
+                            }
+                        )
+                    }
+                    else {
+                        messageService.info($translate.instant('documentDetails.comp.tags.message.tagExists'));
+                    }
+                }
+
+            }, function () {
+                // Cancel button was clicked.
+            });
+        };
+
+        $scope.deleteRow = function (rowEntity) {
+            ObjectTagsService.removeAssociatedTag($stateParams.id, ObjectService.ObjectTypes.FILE, rowEntity.id).then(function () {
+                gridHelper.deleteRow(rowEntity);
+                messageService.info($translate.instant('documentDetails.comp.tags.message.delete.success'));
+            }, function () {
+                messageService.error($translate.instant('documentDetails.comp.tags.message.delete.error'));
+            });
+        };
 
     }
 ]);
