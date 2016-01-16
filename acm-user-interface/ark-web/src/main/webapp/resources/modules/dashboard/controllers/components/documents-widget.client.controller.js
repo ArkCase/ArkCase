f602c0f7cac425204c8dc58efdd32ea5ed92ecec
maksud.sharif <maksud.sharif@armedia.com>
2016-01-15 20:16:45 -0500

    Revise Documents chart widget to use new Services
    Add new REST endpoint to handle data manipulation.
    Add new UI service call to retrieve required data
    Modify 'documents-widget' to use new service

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/documents-widget.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/dashboard/controllers/components/documents-widget.client.controller.js
@@ -16,7 +16,7 @@ angular.module('dashboard.documents', ['adf.provider'])
         , 'Helper.ObjectBrowserService', 'EcmService', 'Case.InfoService', 'ObjectService', 'Complaint.InfoService'
         , 'Task.InfoService',
         function ($scope, config, $stateParams, $translate, DashboardService, HelperObjectBrowserService, Ecm, CaseInfoService
-        , ObjectService, ComplaintInfoService, TaskInfoService) {
+            , ObjectService, ComplaintInfoService, TaskInfoService) {
 
             var vm = this;
 
@@ -47,74 +47,29 @@ angular.module('dashboard.documents', ['adf.provider'])
                         return params;
                     })
                     .then(function (params) {
-                        Ecm.retrieveFolderList(params, function (folderList) {
-                            var folderData = [{name:  'base', count: 0}];
-                            var folders = [];
-                            if(folderList.children && folderList.children.length) {
-                                _.forEach(folderList.children, function(child) {
-                                    if(child.objectType == 'folder'){
-                                        var folder = {};
-                                        folder.name = child.name;
-                                        folder.objectId = child.objectId;
-                                        folders.push(folder);
-                                    } else {
-                                        folderData[0].count++;
-                                    }
-                                });
-                                _.forEach(folders, function(folder) {
-                                    //findDocumentCount
-                                    var documentCount = findDocumentCount(folder, params);
-                                    //add document count and name of folder to folderData
-                                    var folderInfo = {};
-                                    folderInfo.name = folder.name;
-                                    folderInfo.count = documentCount;
-                                    folderData.push(folderInfo);
-                                });
+                        Ecm.getFolderDocumentCounts(params,
+                            function(data) {
+                                $scope.folderData = data;
+                            },
+                            function(error){
+
                             }
-
-                            $scope.folderData = folderData;
-                        },
-                        function(error) {
-
-                        });
+                        );
                     });
 
-                //$scope.folderData has structure:
-                // [
-                //  {name: $nameOfFolder, count: $numberOfDocuments},
-                //  {name: $nameOfFolder, count: $numberOfDocuments}
-                //]
+                /**
+                 * $scope.folderData has structure:
+                 *[
+                 * {name: $nameOfFolder, count: $numberOfDocuments},
+                 * {name: $nameOfFolder, count: $numberOfDocuments}
+                 *]
+                 * "base" folder corresponds to "/" folder in doc-tree
+                 * use $scope.folderData to populate chart data
+                 **/
 
                 vm.showChart = chartData.length > 0 ? true : false;
                 vm.data = [chartData];
                 vm.labels = labels;
             }
-
-            var findDocumentCount = function (folder, params) {
-                params.folderId = folder.objectId;
-                var count = 0;
-                Ecm.retrieveFolderList(params, function (folderList) {
-                        if (folderList.children && folderList.children.length) {
-                            _.forEach(folderList.children, function (child) {
-                                if (child.objectType == 'folder') {
-                                    var folder = {};
-                                    folder.name = child.name;
-                                    folder.objectId = child.objectId;
-                                    folders.push(folder);
-                                } else {
-                                    count++;
-                                }
-                            });
-                            _.forEach(folder, function (folder) {
-                                count += findDocumentCount(folder, params);
-                            });
-                        }
-                    },
-                    function (error) {
-
-                    });
-                return count;
-            }
-
         }
     ]);
