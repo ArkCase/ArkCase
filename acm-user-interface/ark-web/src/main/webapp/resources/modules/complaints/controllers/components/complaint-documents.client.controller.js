b0a17f4d7cb414556bff4306aeea7dc11077dcd8
Bojan Mickoski <bojan.mickoski@armedia.com>
2016-01-28 13:34:53 +0100

    AFDP-1753: Create shared function forf Frevvo plain forms for doc-tree driective

--- acm-user-interface/ark-web/src/main/webapp/resources/modules/complaints/controllers/components/complaint-documents.client.controller.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/complaints/controllers/components/complaint-documents.client.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('complaints').controller('Complaints.DocumentsController', ['$scope', '$stateParams', '$modal'
     , 'UtilService', 'ConfigService', 'ObjectService', 'Object.LookupService', 'Complaint.InfoService'
-    , 'Helper.ObjectBrowserService'
+    , 'Helper.ObjectBrowserService', 'DocTreeService'
     , function ($scope, $stateParams, $modal
         , Util, ConfigService, ObjectService, ObjectLookupService, ComplaintInfoService
-        , HelperObjectBrowserService) {
+        , HelperObjectBrowserService, DocTreeService) {
 
         ConfigService.getComponentConfig("complaints", "documents").then(function (componentConfig) {
             $scope.config = componentConfig;
@@ -46,43 +46,9 @@ angular.module('complaints').controller('Complaints.DocumentsController', ['$sco
             $scope.objectId = complaintInfo.complaintId;
         });
 
-        var silentReplace = function (value, replace, replacement) {
-            if (!Util.isEmpty(value) && value.replace) {
-                value = value.replace(replace, replacement);
-            }
-            return value;
-        };
         $scope.uploadForm = function (type, folderId, onCloseForm) {
-            if ($scope.complaintInfo) {
-                //Complaint.View.Documents.getFileTypeByType(type);
-                var fileType = _.find($scope.fileTypes, {type: type});
-                if (ObjectLookupService.validatePlainForm(fileType)) {
-                    var data = "_data=(";
+            return DocTreeService.uploadFrevvoForm(type, folderId, onCloseForm, $scope.complaintInfo, $scope.fileTypes);
+        };
 
-                    var url = fileType.url;
-                    var urlParameters = fileType.urlParameters;
-                    var parametersAsString = '';
-                    for (var i = 0; i < urlParameters.length; i++) {
-                        var key = urlParameters[i].name;
-                        var value = '';
-                        if (!Util.isEmpty(urlParameters[i].defaultValue)) {
-                            value = silentReplace(urlParameters[i].defaultValue, "'", "_0027_");
-                        } else if (!Util.isEmpty(urlParameters[i].keyValue)) {
-                            var _value = _.get($scope.complaintInfo, urlParameters[i].keyValue)
-                            if (!Util.isEmpty(_value)) {
-                                value = silentReplace(_value, "'", "_0027_");
-                            }
-                        }
-                        value = encodeURIComponent(value);
-                        parametersAsString += key + ":'" + Util.goodValue(value) + "',";
-                    }
-                    parametersAsString += "folderId:'" + folderId + "',";
-                    data += parametersAsString;
-
-                    url = url.replace("_data=(", data);
-                    return url;
-                }
-            }
-        }
     }
 ]);
