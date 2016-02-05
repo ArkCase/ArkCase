753b48e9df6ec5e274013edd6b845d666ff9328f
Bojan Mickoski <bojan.mickoski@armedia.com>
2016-02-05 21:45:10 +0100

    Changes per code review

--- acm-user-interface/ark-web/src/main/webapp/resources/filters/capitalize/capitalizeFirstLetter.client.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/filters/capitalize/capitalizeFirstLetter.client.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('filters').filter('capitalizeFirst', function () {
+angular.module('filters').filter('capitalizeFirst',['UtilService', function (Util) {
     return function(input) {
         if (input!=null)
-            input = input.toLowerCase();
+            input = Util.goodValue(input).toLowerCase();
         return input.substring(0,1).toUpperCase()+input.substring(1);
     };
-})
+}])
 ;
