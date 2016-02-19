241c652c5c313a3c622b4bbb7b0fc7f6f00f712e
David Miller <david.miller@armedia.com>
2016-02-19 15:21:39 -0500

    Changes for AcmBpmnServiceIT to pass

---
+++ acm-tool-integrations/acm-activiti-configuration/src/test/java/com/armedia/acm/activiti/TestActivitiEntityEventListener.java
@@ -0,0 +1,10 @@
+package com.armedia.acm.activiti;
+
+import org.activiti.engine.delegate.event.BaseEntityEventListener;
+
+/**
+ * Created by dmiller on 2/19/16.
+ */
+public class TestActivitiEntityEventListener extends BaseEntityEventListener
+{
+}
