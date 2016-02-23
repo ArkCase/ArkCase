7ad70640b95fdd76207119e13772d02f3d031cdf
Bojan Mickoski <bojan.mickoski@armedia.com>
2016-02-23 16:41:47 +0100

    AFDP-1646 Adding tags for Cases, Complaints, Tasks, Time, Cost module

---
+++ acm-user-interface/ark-web/src/main/webapp/resources/modules/cases/controllers/components/case-tags.client.controller.js
@@ -0,0 +1,103 @@
+'use strict';
+
+angular.module('cases').controller('Cases.TagsController', ['$scope', '$q', '$stateParams', '$translate'
+    , 'UtilService', 'ObjectService', 'Case.InfoService'
+    , 'Helper.UiGridService', 'Helper.ObjectBrowserService', 'Object.TagsService', '$modal', 'MessageService'
+    , function ($scope, $q, $stateParams, $translate
+        , Util, ObjectService, CaseInfoService
+        , HelperUiGridService, HelperObjectBrowserService, ObjectTagsService, $modal, messageService) {
+
+		$scope.tags = [];
+	
+        var componentHelper = new HelperObjectBrowserService.Component({
+            scope: $scope
+            , stateParams: $stateParams
+            , moduleId: "cases"
+            , componentId: "tags"
+            , retrieveObjectInfo: CaseInfoService.getCaseInfo
+            , validateObjectInfo: CaseInfoService.validateCaseInfo
+            , onConfigRetrieved: function (componentConfig) {
+                return onConfigRetrieved(componentConfig);
+            }
+            , onObjectInfoRetrieved: function (objectInfo) {
+            	onObjectInfoRetrieved(objectInfo);
+            }
+        });
+
+        var gridHelper = new HelperUiGridService.Grid({scope: $scope});
+        
+        $scope.addNew = function () {
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'modules/cases/views/components/case-tags-modal.client.view.html',
+                controller: 'Cases.TagsModalController',
+                size: 'lg'
+            });
+
+            modalInstance.result.then(function (tags) {            	
+            	_.forEach(tags, function(tag) {         	
+            		if(tag.id){
+            			if(tag.object_id_s){
+		            		var tagsFound = _.filter($scope.tags, function (tagAss) {
+		                        return tagAss.id == tag.object_id_s;
+		                    });
+		            		if(tagsFound.length == 0) {
+			            		ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.CASE_FILE, tag.object_id_s).then(
+			                            function (returnedTag) {
+			                            	var tagToAdd = angular.copy(returnedTag);
+											tagToAdd.tagName = tag.tags_s;
+			                                $scope.tags.push(tagToAdd);
+			                                $scope.gridOptions.data = $scope.tags;
+			                                $scope.gridOptions.totalItems = $scope.tags.length;
+			                            }
+			                        );
+		            		}
+		            		else {
+		            			messageService.info(tag.tags_s + " " + $translate.instant('cases.comp.tags.message.tagAssociated'));
+		            			_.remove(tagsFound, function(){
+		            				return tag;
+		            			});
+		            		}
+            			}
+            			else {
+            				ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.CASE_FILE, tag.id).then(
+		                            function () {
+		                                $scope.tags.push(tag);
+		                                $scope.gridOptions.data = $scope.tags;
+		                                $scope.gridOptions.totalItems = $scope.tags.length;
+		                            }
+		                        );
+            			}
+            		}
+            	});             
+            	
+            }, function () {
+                // Cancel button was clicked.
+            });
+        };
+
+        var onConfigRetrieved = function (config) {
+
+        	$scope.config = config;
+            gridHelper.setColumnDefs(config);
+            gridHelper.setBasicOptions(config);
+            gridHelper.disableGridScrolling(config);
+
+        };
+
+        var onObjectInfoRetrieved = function (objectInfo) {
+            $scope.objectInfo = objectInfo;
+
+            var currentObjectId = Util.goodMapValue(objectInfo, "id");
+            if (Util.goodPositive(currentObjectId, false)) {
+            	var promiseQueryTags = ObjectTagsService.getAssociateTags(currentObjectId, ObjectService.ObjectTypes.CASE_FILE);
+                $q.all([promiseQueryTags]).then(function (data) {
+                    $scope.tags = data[0];
+                    $scope.gridOptions = $scope.gridOptions || {};
+                    $scope.gridOptions.data = $scope.tags;
+                    $scope.gridOptions.totalItems = $scope.tags.length;
+                });
+            }
+        };
+    }
+]);
