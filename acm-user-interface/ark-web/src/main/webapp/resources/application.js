39eed0fb9e05b6f3b0e24cf677abffff02794c70
sergey <skolomiets@armedia.com>
2016-02-26 10:13:40 -0500

    AFDP-1625 Labels management

--- acm-user-interface/ark-web/src/main/webapp/resources/application.js
+++ acm-user-interface/ark-web/src/main/webapp/resources/application.js
@@ -43,12 +43,13 @@ angular.module(ApplicationConfiguration.applicationModuleName).config([
 ]);
 
 
-//Then define the init function for starting up the application
+// Load language info before start Angular application
 angular.element(document).ready(function () {
-    //Get language
     $.getJSON('proxy/arkcase/api/latest/plugin/admin/labelmanagement/default-language', function(result){
         ACM_SETTINGS.LANG = result.defaultLang || ACM_SETTINGS.LANG;
-        // Then init the app
+        angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+    }).fail(function(){
+        // If language is missed then use default lang settings (en)
         angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
     });
 });
