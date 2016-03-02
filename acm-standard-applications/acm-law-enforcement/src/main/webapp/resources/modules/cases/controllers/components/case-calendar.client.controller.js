67742bfdb6d8b53a2a140da9d47112dac0068a1d
Dragan Simonovski <dragan.simonovski@armedia.com>
2016-03-02 02:29:21 +0100

    AFDP-1344 Calendar is fixed to show events and refresh button is added

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-calendar.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-calendar.client.controller.js
@@ -14,61 +14,72 @@ angular.module('cases').controller('Cases.CalendarController', ['$scope', '$stat
             , validateObjectInfo: CaseInfoService.validateCaseInfo
 		});
 
+        /* Event source that contains calendar events */
+        $scope.events = [];
+
+        /* Event sources array */
+        $scope.eventSources = [$scope.events];
+
+        $scope.folderId = '';
+
+        $scope.$watchCollection('objectInfo', function (newValue, oldValue) {
+            if (newValue && newValue.container && newValue.container.calendarFolderId) {
+                $scope.folderId = newValue.container.calendarFolderId;
+                getDataAndRenderCalendar();
+            }
+        });
+
+        /* Render calendar widget */
+        $scope.renderCalender = function (calendar) {
+            if (uiCalendarConfig.calendars[calendar]) {
+                uiCalendarConfig.calendars[calendar].fullCalendar('render');
+            }
+
+        };
 
         /* Calendar config object */
-		$scope.uiConfig = {
-			calendar: {
-				height: 450,
-				editable: true,
-				header: {
-					left: 'month agendaWeek agendaDay',
-					center: 'title',
-					right: 'today prev,next'
-				},
-				buttonText: {
-					today: 'Today',
-					month: 'Month',
-					week: 'Week',
-					day: 'Day'
-				}
-			}
-		};
+        $scope.uiConfig = {
+            calendar: {
+                height: 450,
+                editable: false,
+                header: {
+                    left: 'month agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                buttonText: {
+                    today: 'Today',
+                    month: 'Month',
+                    week: 'Week',
+                    day: 'Day'
+                }
+            }
+        };
 
-		/* Event source that contains calendar events */
-		$scope.events = [];
+        $scope.onClickRefresh = function () {
+            //reset cache first
+            CalendarService.resetCalendarEventsCache($scope.folderId);
+            getDataAndRenderCalendar();
+        };
 
-		/* Event sources array */
-		$scope.eventSources = [$scope.events];
-
-		$scope.$watchCollection('objectInfo', function (newValue, oldValue) {
-			if (newValue && newValue.container) {
-				CalendarService.queryCalendarEvents(newValue.container.calendarFolderId).then(function (calendarEvents) {
-					if (calendarEvents.items) {
-						for (var i = 0; i < calendarEvents.items.length; i++) {
-							var calendarEvent = {};
-							calendarEvent.id = calendarEvents.items[i].id;
-							calendarEvent.title = calendarEvents.items[i].subject;
-							calendarEvent.start = calendarEvents.items[i].startDate;
-							calendarEvent.end = calendarEvents.items[i].endDate;
-							//calendarEvent.detail = Calendar.View.OutlookCalendar.makeDetail(calendarEvents.items[i]);
-							calendarEvent.className = "b-l b-2x b-info";
-							calendarEvent.allDay = calendarEvents.items[i].allDayEvent;
-							$scope.events.push(calendarEvent);
-						}
-					}
-				});
-			}
-		});
-
-		/* Render calendar widget */
-		$scope.renderCalender = function (calendar) {
-			if (uiCalendarConfig.calendars[calendar]) {
-				uiCalendarConfig.calendars[calendar].fullCalendar('render');
-			}
-		};
-
-		$timeout(function () {
-			$scope.renderCalender('casesCalendar');
-		}, 1000);
-	}
+        var getDataAndRenderCalendar = function () {
+            CalendarService.queryCalendarEvents($scope.folderId).then(function (calendarEvents) {
+                if (calendarEvents.items) {
+                    $scope.events.splice(0, $scope.events.length);
+                    for (var i = 0; i < calendarEvents.items.length; i++) {
+                        var calendarEvent = {};
+                        calendarEvent.id = calendarEvents.items[i].id;
+                        calendarEvent.title = calendarEvents.items[i].subject;
+                        calendarEvent.start = calendarEvents.items[i].startDate;
+                        calendarEvent.end = calendarEvents.items[i].endDate;
+                        calendarEvent.body = calendarEvents.items[i].body;//Calendar.View.OutlookCalendar.makeDetail(calendarEvents.items[i]);
+                        calendarEvent.className = "b-l b-2x b-info";
+                        calendarEvent.allDay = calendarEvents.items[i].allDayEvent;
+                        $scope.events.push(calendarEvent);
+                    }
+                    $scope.renderCalender('casesCalendar');
+                }
+            });
+        };
+    }
 ]);
