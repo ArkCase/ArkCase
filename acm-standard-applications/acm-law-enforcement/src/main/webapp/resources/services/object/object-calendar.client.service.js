22331715ab3ebb08e7ced7cfc219dd72e36ca68c
Dragan Simonovski <dragan.simonovski@armedia.com>
2016-03-02 02:29:21 +0100

    AFDP-1344 Calendar is fixed to show events and refresh button is added

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/object/object-calendar.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/object/object-calendar.client.service.js
@@ -10,93 +10,84 @@
 
  * Object.CalendarService includes group of REST calls to retrieve and save Calendar info;
  */
-angular.module('services').factory('Object.CalendarService', ['$resource', 'UtilService','StoreService',
+angular.module('services').factory('Object.CalendarService', ['$resource', 'UtilService', 'StoreService',
     function ($resource, Util, Store) {
-        var Service = $resource('api/latest/plugin', {}, {
-            /**
-             * @ngdoc method
-             * @name get
-             * @methodOf services:Object.CalendarService
-             *
-             * @description
-             * Query calendar data from database.
-             *
-             * @param {Number} calendarFolderId  Calendar Folder ID
-             * @param {Function} onSuccess (Optional)Callback function of success query.
-             * @param {Function} onError (Optional) Callback function when fail.
-             *
-             * @returns {Object} Object returned by $resource
-             */
-            _getCalendarEvents: {
-                method: 'GET',
-                url: 'api/v1/plugin/outlook/calendar' + "?folderId=:calendarFolderId",
-                cache: false,
-                isArray: false
-            }
-        });
-
+        var Service = this;
         Service.SessionCacheNames = {};
         Service.CacheNames = {
             CALENDAR_EVENTS: "CalendarEvents"
         };
 
+
+        /**
+         * @ngdoc method
+         * @name resetCalendarEventsCache
+         * @methodOf services:Object.CalendarService
+         *
+         * @description
+         * Delete calendar events cached items.
+         *
+         * @param {String} calendarFolderId  Calendar Folder ID
+         */
+        Service.resetCalendarEventsCache = function (calendarFolderId) {
+            var cacheCalendarEvents = new Store.CacheFifo(Service.CacheNames.CALENDAR_EVENTS);
+            var cacheKey = calendarFolderId;
+            cacheCalendarEvents.remove(cacheKey);
+        };
+
         /**
          * @ngdoc method
          * @name queryCostsheets
-         * @methodOf services:Object.CostService
+         * @methodOf services:Object.CalendarService
          *
          * @description
-         * Query cost sheets for an object.
+         * Query calendar events.
          *
-         * @param {String} objectType  Object type
+         * @param {String} calendarFolderId  Calendar Folder ID
          * @param {Number} objectId  Object ID
          *
          * @returns {Object} Promise
          */
         Service.queryCalendarEvents = function (calendarFolderId) {
+            var service = $resource('api/latest/plugin', {}, {
+                /**
+                 * @ngdoc method
+                 * @name get
+                 * @methodOf services:Object.CalendarService
+                 *
+                 * @description
+                 * Query calendar data from database.
+                 *
+                 * @param {Number} calendarFolderId  Calendar Folder ID
+                 * @param {Function} onSuccess (Optional)Callback function of success query.
+                 * @param {Function} onError (Optional) Callback function when fail.
+                 *
+                 * @returns {Object} Object returned by $resource
+                 */
+                _getCalendarEvents: {
+                    method: 'GET',
+                    url: 'api/v1/plugin/outlook/calendar?folderId=' + encodeURIComponent(calendarFolderId),
+                    cache: false,
+                    isArray: false
+                }
+            });
+
             var cacheCalendarEvents = new Store.CacheFifo(Service.CacheNames.CALENDAR_EVENTS);
             var cacheKey = calendarFolderId;
             var calendarEvents = cacheCalendarEvents.get(cacheKey);
 
             return Util.serviceCall({
-                service: Service._getCalendarEvents
-                , param: {
-                    calendarFolderId: calendarFolderId
-                }
+                service: service._getCalendarEvents
+                , param: {}
                 , result: calendarEvents
                 , onSuccess: function (data) {
-                    if (Service.validateCalendarEvents(data)) {
+                    if (!Util.isEmpty(data) && Util.isArray(data.items) && !Util.isEmpty(data.totalItems)) {
                         calendarEvents = data;
                         cacheCalendarEvents.put(cacheKey, calendarEvents);
                         return calendarEvents;
                     }
                 }
             });
-        };
-
-        /**
-         * @ngdoc method
-         * @name validateCalendarEvents
-         * @methodOf services:Object.CalendarService
-         *
-         * @description
-         * Validate calendar events data
-         *
-         * @param {Object} data  Data to be validated
-         *
-         * @returns {Boolean} Return true if data is valid
-         */
-        Service.validateCalendarEvents= function(data) {
-            if (Acm.isEmpty(data)) {
-                return false;
-            }
-            if (Acm.isNotArray(data.items)) {
-                return false;
-            }
-            if (Acm.isEmpty(data.totalItems)) {
-                return false;
-            }
-            return true;
         };
 
         return Service;
