1519367d721794d237d6e9c0b005c39d0100e47a
Dragan Simonovski <dragan.simonovski@armedia.com>
2016-03-03 03:51:48 +0100

    New directive popover-html-unsafe added for tooltip on events in calendar. Calendar extracted to directive coreCalendar and is used in complaints and casefile modules

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-calendar.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-calendar.client.controller.js
@@ -1,9 +1,7 @@
 'use strict';
 
-angular.module('cases').controller('Cases.CalendarController', ['$scope', '$stateParams', '$timeout', 'ConfigService'
-    , 'uiCalendarConfig', 'Object.CalendarService', 'Case.InfoService', 'Helper.ObjectBrowserService'
-    , function ($scope, $stateParams, $timeout, ConfigService
-        , uiCalendarConfig, CalendarService, CaseInfoService, HelperObjectBrowserService) {
+angular.module('cases').controller('Cases.CalendarController', ['$scope', '$stateParams', 'Case.InfoService', 'Helper.ObjectBrowserService'
+    , function ($scope, $stateParams, CaseInfoService, HelperObjectBrowserService) {
 
         new HelperObjectBrowserService.Component({
             scope: $scope
@@ -12,74 +10,20 @@ angular.module('cases').controller('Cases.CalendarController', ['$scope', '$stat
             , componentId: "calendar"
             , retrieveObjectInfo: CaseInfoService.getCaseInfo
             , validateObjectInfo: CaseInfoService.validateCaseInfo
-		});
-
-        /* Event source that contains calendar events */
-        $scope.events = [];
-
-        /* Event sources array */
-        $scope.eventSources = [$scope.events];
-
-        $scope.folderId = '';
+            , onConfigRetrieved: function (componentConfig) {
+                return onConfigRetrieved(componentConfig);
+            }
+        });
 
         $scope.$watchCollection('objectInfo', function (newValue, oldValue) {
             if (newValue && newValue.container && newValue.container.calendarFolderId) {
                 $scope.folderId = newValue.container.calendarFolderId;
-                getDataAndRenderCalendar();
             }
         });
 
-        /* Render calendar widget */
-        $scope.renderCalender = function (calendar) {
-            if (uiCalendarConfig.calendars[calendar]) {
-                uiCalendarConfig.calendars[calendar].fullCalendar('render');
-            }
-
-        };
-
-        /* Calendar config object */
-        $scope.uiConfig = {
-            calendar: {
-                height: 450,
-                editable: false,
-                header: {
-                    left: 'month agendaWeek agendaDay',
-                    center: 'title',
-                    right: 'today prev,next'
-                },
-                buttonText: {
-                    today: 'Today',
-                    month: 'Month',
-                    week: 'Week',
-                    day: 'Day'
-                }
-            }
-        };
-
-        $scope.onClickRefresh = function () {
-            //reset cache first
-            CalendarService.resetCalendarEventsCache($scope.folderId);
-            getDataAndRenderCalendar();
-        };
-
-        var getDataAndRenderCalendar = function () {
-            CalendarService.queryCalendarEvents($scope.folderId).then(function (calendarEvents) {
-                if (calendarEvents.items) {
-                    $scope.events.splice(0, $scope.events.length);
-                    for (var i = 0; i < calendarEvents.items.length; i++) {
-                        var calendarEvent = {};
-                        calendarEvent.id = calendarEvents.items[i].id;
-                        calendarEvent.title = calendarEvents.items[i].subject;
-                        calendarEvent.start = calendarEvents.items[i].startDate;
-                        calendarEvent.end = calendarEvents.items[i].endDate;
-                        calendarEvent.body = calendarEvents.items[i].body;//Calendar.View.OutlookCalendar.makeDetail(calendarEvents.items[i]);
-                        calendarEvent.className = "b-l b-2x b-info";
-                        calendarEvent.allDay = calendarEvents.items[i].allDayEvent;
-                        $scope.events.push(calendarEvent);
-                    }
-                    $scope.renderCalender('casesCalendar');
-                }
-            });
+        var onConfigRetrieved = function (config) {
+            $scope.config = config;
+            return config;
         };
     }
 ]);
--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-calendar.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-calendar.client.controller.js
@@ -1,85 +1,29 @@
 'use strict';
 
-angular.module('complaints').controller('Complaints.CalendarController', ['$scope', '$stateParams', '$timeout'
-    , 'ConfigService', 'uiCalendarConfig', 'Object.CalendarService', 'Complaint.InfoService', 'Helper.ObjectBrowserService'
-    , function ($scope, $stateParams, $timeout
-        , ConfigService, uiCalendarConfig, CalendarService, ComplaintInfoService, HelperObjectBrowserService) {
+angular.module('complaints').controller('Complaints.CalendarController', ['$scope', '$stateParams', 'Complaint.InfoService', 'Helper.ObjectBrowserService'
+    , function ($scope, $stateParams, ComplaintInfoService, HelperObjectBrowserService) {
 
         new HelperObjectBrowserService.Component({
             scope: $scope
             , stateParams: $stateParams
-            , moduleId: "complaints"
+            , moduleId: "cases"
             , componentId: "calendar"
             , retrieveObjectInfo: ComplaintInfoService.getComplaintInfo
             , validateObjectInfo: ComplaintInfoService.validateComplaintInfo
+            , onConfigRetrieved: function (componentConfig) {
+                return onConfigRetrieved(componentConfig);
+            }
         });
-
-        /* Event source that contains calendar events */
-        $scope.events = [];
-
-        /* Event sources array */
-        $scope.eventSources = [$scope.events];
-
-        $scope.folderId = '';
 
         $scope.$watchCollection('objectInfo', function (newValue, oldValue) {
             if (newValue && newValue.container && newValue.container.calendarFolderId) {
                 $scope.folderId = newValue.container.calendarFolderId;
-                getDataAndRenderCalendar();
             }
         });
 
-        /* Render calendar widget */
-        $scope.renderCalender = function (calendar) {
-            if (uiCalendarConfig.calendars[calendar]) {
-                uiCalendarConfig.calendars[calendar].fullCalendar('render');
-            }
-
-        };
-
-        /* Calendar config object */
-        $scope.uiConfig = {
-            calendar: {
-                height: 450,
-                editable: false,
-                header: {
-                    left: 'month agendaWeek agendaDay',
-                    center: 'title',
-                    right: 'today prev,next'
-                },
-                buttonText: {
-                    today: 'Today',
-                    month: 'Month',
-                    week: 'Week',
-                    day: 'Day'
-                }
-            }
-        };
-
-        $scope.onClickRefresh = function () {
-            //reset cache first
-            CalendarService.resetCalendarEventsCache($scope.folderId);
-            getDataAndRenderCalendar();
-        };
-
-        var getDataAndRenderCalendar = function () {
-            CalendarService.queryCalendarEvents($scope.folderId).then(function (calendarEvents) {
-                if (calendarEvents.items) {
-                    $scope.events.splice(0, $scope.events.length);
-                    for (var i = 0; i < calendarEvents.items.length; i++) {
-                        var calendarEvent = {};
-                        calendarEvent.id = calendarEvents.items[i].id;
-                        calendarEvent.title = calendarEvents.items[i].subject;
-                        calendarEvent.start = calendarEvents.items[i].startDate;
-                        calendarEvent.end = calendarEvents.items[i].endDate;
-                        calendarEvent.body = calendarEvents.items[i].body;//Calendar.View.OutlookCalendar.makeDetail(calendarEvents.items[i]);
-                        calendarEvent.className = "b-l b-2x b-info";
-                        calendarEvent.allDay = calendarEvents.items[i].allDayEvent;
-                        $scope.events.push(calendarEvent);
-                    }
-                    $scope.renderCalender('casesCalendar');
-                }
-            });
+        var onConfigRetrieved = function (config) {
+            $scope.config = config;
+            return config;
         };
     }
 ]);
