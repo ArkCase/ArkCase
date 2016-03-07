e0c7567fb727b35739a075cf11f76326dbdd043e
sergey <skolomiets@armedia.com>
2016-03-07 12:14:28 -0500

    [AFDP-1911] ArkCase Application - Implement Minfying

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/application.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/application.js
@@ -7,7 +7,6 @@ var ACM_SETTINGS = {
 //Start by defining the main module and adding the module dependencies
 var app = angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
-
 // Setting HTML5 Location Mode
 angular.module(ApplicationConfiguration.applicationModuleName).config([
         '$locationProvider', '$translateProvider', '$translatePartialLoaderProvider', '$httpProvider',
