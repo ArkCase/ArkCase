eb95da75c74a4dc5b19044d91513504c199da093
David Miller <david.miller@armedia.com>
2016-03-08 15:31:41 -0500

    ArkCase 3.0.49 release: add user service to various integration tests that now require the userJpaDao bean.

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/test/java/com/armedia/acm/plugins/person/PersonAliasIT.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/test/java/com/armedia/acm/plugins/person/PersonAliasIT.java
@@ -4,10 +4,6 @@ import com.armedia.acm.data.AuditPropertyEntityAdapter;
 import com.armedia.acm.plugins.person.dao.PersonAliasDao;
 import com.armedia.acm.plugins.person.model.Person;
 import com.armedia.acm.plugins.person.model.PersonAlias;
-import java.util.Date;
-
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,14 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import static org.junit.Assert.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/spring/spring-library-data-source.xml",
-                                   "/spring/spring-library-person.xml",
-                                   "/spring/spring-library-person-plugin-test-mule.xml",
-                                   "/spring/spring-library-context-holder.xml",
-                                   "/spring/spring-library-property-file-manager.xml",
-                                   "/spring/spring-library-acm-encryption.xml"
-                                   })
+        "/spring/spring-library-person.xml",
+        "/spring/spring-library-person-plugin-test-mule.xml",
+        "/spring/spring-library-context-holder.xml",
+        "/spring/spring-library-property-file-manager.xml",
+        "/spring/spring-library-acm-encryption.xml", "/spring/spring-library-user-service.xml"
+})
 @TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
 public class PersonAliasIT
 {
@@ -55,7 +53,7 @@ public class PersonAliasIT
     public void savePersonAlias() throws Exception
     {
         PersonAlias pa = new PersonAlias();
-        
+
         pa.setAliasType("Others");
         pa.setAliasValue("ACM");
 
