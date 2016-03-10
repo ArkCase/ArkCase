81a9c800ab4747b4f9e4a8f27324090ee8388e73
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-03-10 14:55:03 +0100

    AFDP-1266 Configure new notifications
    - Implemented new notification for correspondence added

---
+++ acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/model/CorrespondenceAddedEvent.java
@@ -0,0 +1,32 @@
+package com.armedia.acm.correspondence.model;
+
+
+import com.armedia.acm.auth.AcmAuthenticationDetails;
+import com.armedia.acm.core.model.AcmEvent;
+import com.armedia.acm.plugins.ecm.model.EcmFile;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
+public class CorrespondenceAddedEvent extends AcmEvent {
+
+    public static final String EVENT_TYPE = "com.armedia.acm.correspondence.file.added";
+
+    public CorrespondenceAddedEvent(EcmFile source, Authentication authentication, boolean succeeded) {
+        super(source);
+
+
+        setEventType(EVENT_TYPE);
+        setObjectType("FILE");
+        setObjectId(source.getFileId());
+        setEventDate(new Date());
+        setUserId(source.getModifier());
+        setSucceeded(succeeded);
+
+        if (authentication.getDetails() != null && authentication.getDetails() instanceof AcmAuthenticationDetails) {
+            setIpAddress(((AcmAuthenticationDetails) authentication.getDetails()).getRemoteAddress());
+        }
+
+
+    }
+}
