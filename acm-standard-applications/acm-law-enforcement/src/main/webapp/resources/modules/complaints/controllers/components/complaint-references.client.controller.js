e67eaeaca8aebde949c11b23a44d662ecad88a36
Vladimir Radeski <vladimir.radeski@armedia.com>
2016-03-21 19:20:11 +0100

    Implement the ability to add a reference to a Complaint or Case

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-references.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-references.client.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('complaints').controller('Complaints.ReferencesController', ['$scope', '$stateParams'
-    , 'UtilService', 'ConfigService', 'Complaint.InfoService', 'Helper.UiGridService', 'Helper.ObjectBrowserService'
+    , 'UtilService', 'ConfigService', 'Complaint.InfoService', 'Helper.UiGridService', 'Helper.ObjectBrowserService', '$modal', 'Object.ReferenceService'
     , function ($scope, $stateParams
-        , Util, ConfigService, ComplaintInfoService, HelperUiGridService, HelperObjectBrowserService) {
+        , Util, ConfigService, ComplaintInfoService, HelperUiGridService, HelperObjectBrowserService, $modal, referenceService) {
 
         var componentHelper = new HelperObjectBrowserService.Component({
             scope: $scope
@@ -47,6 +47,60 @@ angular.module('complaints').controller('Complaints.ReferencesController', ['$sc
             var targetType = Util.goodMapValue(rowEntity, "targetType");
             var targetId = Util.goodMapValue(rowEntity, "targetId");
             gridHelper.showObject(targetType, targetId);
+        };
+
+
+        ConfigService.getModuleConfig("complaints").then(function (moduleConfig) {
+        	$scope.modalConfig = _.find(moduleConfig.components, {id: "referenceSearchGrid"});
+            return moduleConfig;
+        });
+
+        $scope.refresh = function () {
+            $scope.$emit('report-object-refreshed', $stateParams.id);
+        };
+
+        // open addreference modal
+        $scope.addReference = function () {
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'modules/complaints/views/components/complaint-reference-modal.client.view.html',
+                controller: 'Complaints.ReferenceModalController',
+                size: 'lg',
+                resolve: {
+                    $filter: function () {
+                        return $scope.modalConfig.searchFilter;
+                    },
+                    $config: function () {
+                        return $scope.modalConfig;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (chosenFile) {
+                if (chosenFile) {
+                    var reference = {};
+                    reference.referenceId = chosenFile.object_id_s;
+                    reference.referenceTitle = chosenFile.title_parseable;
+                    reference.referenceType = chosenFile.object_type_s;
+                    reference.referenceNumber = chosenFile.name;
+                    reference.targetId = $stateParams.id;
+                    reference.targetType = 'COMPLAINT';
+                    referenceService.addReference(reference).then(
+                        function (objectSaved) {
+                            $scope.refresh();
+                            return objectSaved;
+                        },
+                        function (error) {
+                            return error;
+                        }
+                    );
+                    return;
+                }
+            }, function () {
+                // Cancel button was clicked.
+                return [];
+            });
+
         };
 
     }
