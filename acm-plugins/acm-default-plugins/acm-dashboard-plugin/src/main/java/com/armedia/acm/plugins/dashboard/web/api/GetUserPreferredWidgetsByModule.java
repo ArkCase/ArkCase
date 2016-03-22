3dcd7896aaad3b7058a24f85d7da850743d301ff
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2016-03-22 16:27:09 +0100

    AFDP-1902 Implement SAML authentication in Arkcase
    Fix bugs with usernames containing upper letters and dots.

--- acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/web/api/GetUserPreferredWidgetsByModule.java
+++ acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/web/api/GetUserPreferredWidgetsByModule.java
@@ -4,6 +4,7 @@ import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
 import com.armedia.acm.plugins.dashboard.exception.AcmWidgetException;
 import com.armedia.acm.plugins.dashboard.model.userPreference.PreferredWidgetsDto;
 import com.armedia.acm.plugins.dashboard.service.UserPreferenceService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -20,22 +21,19 @@ import javax.servlet.http.HttpSession;
  * Created by marjan.stefanoski on 14.01.2016.
  */
 @Controller
-@RequestMapping({"/api/v1/plugin/dashboard/widgets", "/api/latest/plugin/dashboard/widgets"})
+@RequestMapping({ "/api/v1/plugin/dashboard/widgets", "/api/latest/plugin/dashboard/widgets" })
 public class GetUserPreferredWidgetsByModule
 {
     private UserPreferenceService userPreferenceService;
     private Logger log = LoggerFactory.getLogger(getClass());
 
-
     @RequestMapping(value = "/preferred/{moduleName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public PreferredWidgetsDto getPreferredWidgets(
-            @PathVariable("moduleName") String moduleName,
-            Authentication authentication,
+    public PreferredWidgetsDto getPreferredWidgets(@PathVariable("moduleName") String moduleName, Authentication authentication,
             HttpSession session) throws AcmWidgetException, AcmObjectNotFoundException
     {
 
-        String userId = authentication.getName().toLowerCase();
+        String userId = authentication.getName();
         if (log.isInfoEnabled())
         {
             log.info("Finding widgets for user  based on the user preference for user: " + userId + "'");
@@ -44,7 +42,8 @@ public class GetUserPreferredWidgetsByModule
         try
         {
             result = userPreferenceService.getPreferredWidgetsByUserAndModule(userId, moduleName);
-        } catch (AcmObjectNotFoundException e)
+        }
+        catch (AcmObjectNotFoundException e)
         {
             throw e;
         }
