f3e491ef275e1e6f9d7741bc877fd17d993efed4
Lazo Lazarev <lazo.lazarev@armedia.com>
2016-04-05 11:28:12 +0200

    AFDP-1873 Changed formatting.

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
@@ -1,61 +1,48 @@
 'use strict';
 
-angular
-        .module('tasks')
-        .controller(
-                'Tasks.NotesController',
-                [
-                        '$scope',
-                        '$stateParams',
-                        'ConfigService',
-                        'ObjectService',
-                        'Helper.ObjectBrowserService',
-                        'Task.InfoService',
-                        function($scope, $stateParams, ConfigService,
-                                ObjectService, HelperObjectBrowserService,
-                                TaskInfoService) {
+angular.module('tasks').controller('Tasks.NotesController', ['$scope', '$stateParams', 'ConfigService', 'ObjectService'
+    , function ($scope, $stateParams, ConfigService, ObjectService) {
 
-                            ConfigService
-                                    .getComponentConfig("tasks", "notes")
-                                    .then(
-                                            function(config) {
-                                                $scope.notesInit = {
-                                                    objectType : ObjectService.ObjectTypes.TASK,
-                                                    currentObjectId : $stateParams.id
-                                                };
-                                                $scope.config = config;
-                                                return config;
-                                            });
+        ConfigService.getComponentConfig("tasks", "notes").then(function (config) {
+            $scope.notesInit = {
+                objectType: ObjectService.ObjectTypes.TASK,
+                currentObjectId: $stateParams.id
+            };
+            $scope.config = config;
+            return config;
+        });
 
-                            var componentHelper = new HelperObjectBrowserService.Component(
-                                    {
-                                        scope : $scope,
-                                        stateParams : $stateParams,
-                                        moduleId : "tasks",
-                                        componentId : "notes",
-                                        retrieveObjectInfo : TaskInfoService.getTaskInfo,
-                                        validateObjectInfo : TaskInfoService.validateTaskInfo,
-                                        onConfigRetrieved : function(
-                                                componentConfig) {
-                                            return onConfigRetrieved(componentConfig);
-                                        },
-                                        onObjectInfoRetrieved : function(
-                                                objectInfo) {
-                                            onObjectInfoRetrieved(objectInfo);
-                                        }
-                                    });
+        var componentHelper = new HelperObjectBrowserService.Component(
+            {
+                scope : $scope,
+                stateParams : $stateParams,
+                moduleId : "tasks",
+                componentId : "notes",
+                retrieveObjectInfo : TaskInfoService.getTaskInfo,
+                validateObjectInfo : TaskInfoService.validateTaskInfo,
+                onConfigRetrieved : function(
+                        componentConfig) {
+                    return onConfigRetrieved(componentConfig);
+                },
+                onObjectInfoRetrieved : function(
+                        objectInfo) {
+                    onObjectInfoRetrieved(objectInfo);
+                }
+            });
 
-                            var onConfigRetrieved = function(config) {
+        var onConfigRetrieved = function(config) {
+        
+            $scope.config = config;
+        
+        };
 
-                                $scope.config = config;
+        var onObjectInfoRetrieved = function(objectInfo) {
+        
+            $scope.objectInfo = objectInfo;
+            $scope.notesInit.notesEnabled = !objectInfo.completed;
+        
+        };
 
-                            };
+    }
 
-                            var onObjectInfoRetrieved = function(objectInfo) {
-
-                                $scope.objectInfo = objectInfo;
-                                $scope.notesInit.notesEnabled = !objectInfo.completed;
-
-                            };
-
-                        } ]);
+]);
