fbcf44b18fc45e8ac3a1dbf6a6eefc9ba434f613
Petar Ilin <petar.ilin@armedia.com>
2016-04-07 09:42:20 +0200

    SBI-1311 add colon delimiter between hostname and port when creating URLs

--- acm-tool-integrations/acm-report-configuration/src/main/java/com/armedia/acm/pentaho/config/PentahoReportUrl.java
+++ acm-tool-integrations/acm-report-configuration/src/main/java/com/armedia/acm/pentaho/config/PentahoReportUrl.java
@@ -1,15 +1,15 @@
 package com.armedia.acm.pentaho.config;
 
+import com.armedia.acm.report.config.ReportUrl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.armedia.acm.report.config.ReportUrl;
-
-public class PentahoReportUrl implements ReportUrl{
+public class PentahoReportUrl implements ReportUrl
+{
     private Logger log = LoggerFactory.getLogger(getClass());
     private static final String REPORT_SERVER_URL = "PENTAHO_SERVER_URL";
     private static final String REPORT_SERVER_PORT = "PENTAHO_SERVER_PORT";
@@ -22,112 +22,108 @@ public class PentahoReportUrl implements ReportUrl{
      */
     private Map<String, Object> reportsProperties;
     private Properties reportServerConfigurationProperties;
-    
-	public Map<String, Object> getReportsProperties() {
-		return reportsProperties;
-	}
 
-	public void setReportsProperties(Map<String, Object> reportProperties) {
-		this.reportsProperties = reportProperties;
-	}
+    public Map<String, Object> getReportsProperties()
+    {
+        return reportsProperties;
+    }
 
-	@Override
-	public String getNewReportUrl(String reportName) {
-		return getReportUrlPath(reportName);
-	}
-	
-	public String getReportURL() {
-        //form url data
+    public void setReportsProperties(Map<String, Object> reportProperties)
+    {
+        this.reportsProperties = reportProperties;
+    }
+
+    @Override
+    public String getNewReportUrl(String reportName)
+    {
+        return getReportUrlPath(reportName);
+    }
+
+    /**
+     * Url path for create a new complaint report form in Pentaho.
+     *
+     * @return
+     */
+    public String getReportUrlPath(String reportName)
+    {
         StringBuilder builder = new StringBuilder();
         String serverFormUrl = getReportServerConfigurationProperties().get(REPORT_SERVER_URL).toString();
         builder.append(serverFormUrl);
         String serverFormPort = getReportServerConfigurationProperties().get(REPORT_SERVER_PORT).toString();
-        builder.append(serverFormPort);
-        String url = builder.toString();
-        log.debug("getReportURL(): " + url);
-        return url;
-	}
-
-	/**
-	 * Url path for create a new complaint report form in Pentaho.
-	 * 
-	 * @return
-	 */
-	public String getReportUrlPath(String reportName) {
-        StringBuilder builder = new StringBuilder();
-        String serverFormUrl = getReportServerConfigurationProperties().get(REPORT_SERVER_URL).toString();
-        builder.append(serverFormUrl);
-        String serverFormPort = getReportServerConfigurationProperties().get(REPORT_SERVER_PORT).toString();
-        builder.append(serverFormPort);
-		String pathStr = getReportsProperties().get(reportName).toString();
+        builder.append(":").append(serverFormPort);
+        String pathStr = getReportsProperties().get(reportName).toString();
         builder.append(pathStr);
-		String path = builder.toString();
+        String path = builder.toString();
         log.debug("getReportUrlPath(): " + path);
-		return path;
-	}
-	
-	/**
-	 * This method get all the report urls in the properties file and
-	 * return a sorted map.
-	 * 
-	 * @return
-	 */
-	@Override
-	public Map<String, String> getNewReportUrlList() 
-	{
-        Map<String,String> urlsMap = new TreeMap<String, String>();
+        return path;
+    }
+
+    /**
+     * This method get all the report urls in the properties file and
+     * return a sorted map.
+     *
+     * @return
+     */
+    @Override
+    public Map<String, String> getNewReportUrlList()
+    {
+        Map<String, String> urlsMap = new TreeMap<String, String>();
         String serverFormUrl = getReportServerConfigurationProperties().get(REPORT_SERVER_URL).toString();
         String serverFormPort = getReportServerConfigurationProperties().get(REPORT_SERVER_PORT).toString();
 
-        for(Map.Entry<String, Object> entry : getReportsProperties().entrySet())
+        for (Map.Entry<String, Object> entry : getReportsProperties().entrySet())
         {
             String keyStr = entry.getKey();
-            
-        	ReportName enumName = ReportName.valueOf(keyStr);
-        	if (null != enumName) 
-        	{
-            	urlsMap.put(enumName.getDisplayName(), formulateUrl(serverFormUrl, serverFormPort, entry.getValue()));            		
-        	}
-            
+
+            ReportName enumName = ReportName.valueOf(keyStr);
+            if (null != enumName)
+            {
+                urlsMap.put(enumName.getDisplayName(), formulateUrl(serverFormUrl, serverFormPort, entry.getValue()));
+            }
+
         }
-        
-		return urlsMap;
-	}
-	
-	private String formulateUrl(String host, String port, Object path) {
+
+        return urlsMap;
+    }
+
+    private String formulateUrl(String host, String port, Object path)
+    {
         StringBuilder builder = new StringBuilder();
         builder.append(host);
+        builder.append(":");
         builder.append(port);
-    	builder.append(path);
-		return builder.toString();
-	}
-	
-	public String getReportsUrl()
-	{
-		StringBuilder builder = new StringBuilder();
-		
+        builder.append(path);
+        return builder.toString();
+    }
+
+    public String getReportsUrl()
+    {
+        StringBuilder builder = new StringBuilder();
+
         String serverFormUrl = getReportServerConfigurationProperties().get(PENTAHO_SERVER_INTERNAL_URL).toString();
 
         builder.append(serverFormUrl);
 
         String serverFormPort = getReportServerConfigurationProperties().get(PENTAHO_SERVER_INTERNAL_PORT).toString();
-        builder.append(serverFormPort);
-        
+        builder.append(":").append(serverFormPort);
+
         String pentahoReportsUrl = getReportServerConfigurationProperties().get(REPORTS_URL).toString();
         builder.append(pentahoReportsUrl);
-        
+
         String url = builder.toString();
         log.debug("getReportsUrl(): " + url);
-        
+
         return url;
-	}
+    }
 
-	public Properties getReportServerConfigurationProperties() {
-		return reportServerConfigurationProperties;
-	}
+    public Properties getReportServerConfigurationProperties()
+    {
+        return reportServerConfigurationProperties;
+    }
 
-	public void setReportServerConfigurationProperties(
-			Properties reportServerConfigurationProperties) {
-		this.reportServerConfigurationProperties = reportServerConfigurationProperties;
-	}
+    public void setReportServerConfigurationProperties(
+            Properties reportServerConfigurationProperties)
+    {
+        this.reportServerConfigurationProperties = reportServerConfigurationProperties;
+    }
 }
