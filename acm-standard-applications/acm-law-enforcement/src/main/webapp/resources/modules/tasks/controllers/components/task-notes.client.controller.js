9b4ed9842dce50b0b66a60c30aff01a89c09bcc0
Lazo Lazarev <lazo.lazarev@armedia.com>
2016-04-07 19:11:55 +0200

    AFDP-1873 Task properties edition is disabled/enabled depending on 'completed' value of 'objectInfo' with use of access control rules.

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
@@ -12,37 +12,6 @@ angular.module('tasks').controller('Tasks.NotesController', ['$scope', '$statePa
             return config;
         });
 
-        var componentHelper = new HelperObjectBrowserService.Component(
-            {
-                scope : $scope,
-                stateParams : $stateParams,
-                moduleId : "tasks",
-                componentId : "notes",
-                retrieveObjectInfo : TaskInfoService.getTaskInfo,
-                validateObjectInfo : TaskInfoService.validateTaskInfo,
-                onConfigRetrieved : function(
-                        componentConfig) {
-                    return onConfigRetrieved(componentConfig);
-                },
-                onObjectInfoRetrieved : function(
-                        objectInfo) {
-                    onObjectInfoRetrieved(objectInfo);
-                }
-            });
-
-        var onConfigRetrieved = function(config) {
-        
-            $scope.config = config;
-        
-        };
-
-        var onObjectInfoRetrieved = function(objectInfo) {
-        
-            $scope.objectInfo = objectInfo;
-            $scope.notesInit.notesEnabled = !objectInfo.completed;
-        
-        };
-
     }
 
 ]);
