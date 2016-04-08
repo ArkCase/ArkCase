a87dc0470a01e20820d43e66ca469f033e2c1997
Lazo Lazarev <lazo.lazarev@armedia.com>
2016-04-08 13:54:36 +0200

    AFDP-1873 Notes addition enabled/disabled based on 'complete' status of a task by evaluation of access control rules.

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-notes.client.controller.js
@@ -1,16 +1,52 @@
 'use strict';
 
-angular.module('tasks').controller('Tasks.NotesController', ['$scope', '$stateParams', 'ConfigService', 'ObjectService'
-    , function ($scope, $stateParams, ConfigService, ObjectService) {
+angular.module('tasks').controller('Tasks.NotesController', ['$scope', '$stateParams', 'ConfigService', 'ObjectService', 'Helper.ObjectBrowserService', 'PermissionsService', 'Task.InfoService'
+    , function ($scope, $stateParams, ConfigService, ObjectService, HelperObjectBrowserService, PermissionsService, TaskInfoService) {
 
         ConfigService.getComponentConfig("tasks", "notes").then(function (config) {
             $scope.notesInit = {
                 objectType: ObjectService.ObjectTypes.TASK,
-                currentObjectId: $stateParams.id
+                currentObjectId: $stateParams.id,
+                isReadOnly: false
             };
             $scope.config = config;
             return config;
         });
+
+        var componentHelper = new HelperObjectBrowserService.Component(
+                {
+                    scope : $scope,
+                    stateParams : $stateParams,
+                    moduleId : "tasks",
+                    componentId : "notes",
+                    retrieveObjectInfo : TaskInfoService.getTaskInfo,
+                    validateObjectInfo : TaskInfoService.validateTaskInfo,
+                    onConfigRetrieved : function(
+                            componentConfig) {
+                        return onConfigRetrieved(componentConfig);
+                    },
+                    onObjectInfoRetrieved : function(objectInfo) {
+                        onObjectInfoRetrieved(objectInfo);
+                    }
+                });
+
+        var onConfigRetrieved = function(config) {
+
+            $scope.config = config;
+
+        };
+
+        var onObjectInfoRetrieved = function(objectInfo) {
+
+            $scope.objectInfo = objectInfo;
+            PermissionsService.getActionPermission('editNote', objectInfo).then(function(result) {
+
+                $scope.notesInit.isReadOnly = !result;
+
+            });
+
+        };
+
 
     }
 
