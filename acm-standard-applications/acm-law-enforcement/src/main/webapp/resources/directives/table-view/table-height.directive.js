9ffb117058cd350d5cb3425667e6b9af799544c2
sergey <sergei.kolomiets@gmail.com>
2016-04-20 15:16:43 -0400

    [SBI-1477] Can't scroll up down when viewing a list in the queues Also fixed and cleaned directives ngDoc comments.

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/table-view/table-height.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/table-view/table-height.directive.js
@@ -1,16 +1,38 @@
 'use strict';
 
+/**
+ * @ngdoc directive
+ * @name global.directive:adjustTableHeight
+ * @restrict A
+ *
+ * @description
+ *
+ * {@link https://gitlab.armedia.com/arkcase/ACM3/blob/develop/acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/table-view/table-height.directive.js directives/table-view/table-height.directive.js}
+ *
+ * The adjust-table-height directive makes ui-grid 100% height. Also it solves ui-grid scroll problem (https://github.com/angular-ui/ui-grid/issues/1926).
+ *
+ * @param {number} adjust-table-height Number of grid's rows
+ *
+ * @example
+ <example>
+ <file name="index.html">
+     <div ui-grid="gridOptions"
+        adjust-table-height="gridOptions.data.length"
+        ui-grid-auto-resize>
+     </div>
+ </file>
+ </example>
+ */
 angular.module('directives').directive('adjustTableHeight', function () {
-
         return {
             restrict: 'A',
             link: function ($scope, element, attrs) {
 
                 // This hack allows to scroll page that contains grid.
                 // Solution is described at https://github.com/angular-ui/ui-grid/issues/1926
-                $scope.$watch('isDisplayed', function(newValue, oldValue) {
+                $scope.$watch('isDisplayed', function (newValue, oldValue) {
                     var viewport = element.find('.ui-grid-render-container');
-                    ['touchstart', 'touchmove', 'touchend','keydown', 'wheel', 'mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'].forEach(function (eventName) {
+                    ['touchstart', 'touchmove', 'touchend', 'keydown', 'wheel', 'mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'].forEach(function (eventName) {
                         viewport.unbind(eventName);
                     });
                 });
