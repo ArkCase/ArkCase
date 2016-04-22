2f350a8d653506b90c589013b2aa1388f6a7679d
Riste Tutureski <riste.tutureski@armedia.com>
2016-04-22 11:03:19 +0200

    AFDP-2070: Adding Legacy System ID field to Case, Complaint, File and Task

---
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmLegacySystemEntity.java
@@ -0,0 +1,10 @@
+package com.armedia.acm.data;
+
+/**
+ * Created by riste.tutureski on 4/22/2016.
+ */
+public interface AcmLegacySystemEntity
+{
+    String getLegacySystemId();
+    void setLegacySystemId(String legacySystemId);
+}
