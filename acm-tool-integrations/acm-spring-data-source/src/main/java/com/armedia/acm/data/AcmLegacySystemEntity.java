4ac8c830af4a62530462c9a045a692afebe2d578
Riste Tutureski <riste.tutureski@armedia.com>
2016-04-25 09:34:28 +0200

    AFDP-2070: Adding Legacy System ID field to Case, Complaint, File and Task - Code review changes

--- acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmLegacySystemEntity.java
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmLegacySystemEntity.java
@@ -1,6 +1,10 @@
 package com.armedia.acm.data;
 
 /**
+ * Support traceability from legacy systems by storing the ID from the legacy system in ArkCase.
+ * The legacy system ID is set during data migration. This allows for traceability back to the
+ * source system during data migrations.
+ *
  * Created by riste.tutureski on 4/22/2016.
  */
 public interface AcmLegacySystemEntity
