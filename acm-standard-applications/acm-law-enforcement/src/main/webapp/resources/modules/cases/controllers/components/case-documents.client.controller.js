d4b47206e75e11aa28cfc4191e21e24066c78473
Dragan Simonovski <dragan.simonovski@armedia.com>
2016-04-28 11:18:03 +0200

    changes for cancelEditing on frontend, check for cancelLock permission added for admin users, property for checking of owner of parent object added to configuration

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
@@ -32,6 +32,7 @@ angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$sta
 
         var onConfigRetrieved = function (config) {
             $scope.treeConfig = config.docTree;
+            $scope.allowParentOwnerToCancel = config.docTree.allowParentOwnerToCancel;
         };
 
         ObjectLookupService.getFormTypes(ObjectService.ObjectTypes.CASE_FILE).then(
@@ -69,96 +70,125 @@ angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$sta
 
         $scope.onAllowCmd = function (cmd, nodes) {
             if (1 == nodes.length) {
+                var fileObject = nodes[0].data;
+                var lock = fileObject.lock;
                 if ("checkin" == cmd) {
-                    if (!nodes[0].data.lock) {
+                    if (!lock) {
                         //there is no lock so checkin should be disabled
                         return "disable";
                     }
-                    else if (nodes[0].data.lock
-                        && nodes[0].data.lock.creator !== $scope.user) {
+                    else if (lock
+                        && lock.creator !== $scope.user) {
                         //there is lock on object but it is not by the user so checkin is disabled
                         return "disable";
                     }
-                    else if (nodes[0].data.lock.lockType !== ObjectService.LockTypes.CHECKOUT_LOCK) {
+                    else if (lock.lockType !== ObjectService.LockTypes.CHECKOUT_LOCK) {
                         //object has lock and it is by the user but it isn't checkout
                         //it is probably edit in word so checkin should be disabled
                         return "disable";
                     }
                     else {
                         //object has lock, lockType is checkout and user is creator
-                        return "";
 
                         //user should be able to unlock the file with checkin if he had permisison
                         //to checkout the file, there is not need to check for permission for unlock
+                        //but because we will use unlock service which is checking for unlock permission
+                        //we should make the check because otherwise server will return access denied
 
-                        /*var allowDeffered = $q.defer();
+                        var df = $q.defer();
 
-                         //check permission for unlock
-                         PermissionsService.getActionPermission('unlock', nodes[0].data)
-                         .then(function success(hasPermission) {
-                         if (hasPermission)
-                         allowDeffered.resolve("");
-                         else
-                         allowDeffered.resolve("disable");
-                         },
-                         function error() {
-                         allowDeffered.resolve("disable");
-                         }
-                         );
-                         return allowDeffered.promise;*/
+                        //check permission for unlock
+                        PermissionsService.getActionPermission('unlock', fileObject)
+                            .then(function success(hasPermission) {
+                                    if (hasPermission)
+                                        df.resolve("");
+                                    else
+                                        df.resolve("disable");
+                                },
+                                function error() {
+                                    df.resolve("disable");
+                                }
+                            );
+                        return df.promise;
                     }
                 }
                 else if ("cancelEditing" == cmd) {
-                    if (!nodes[0].data.lock) {
+                    if (!lock) {
                         //there is no lock so cancel is disabled
                         return "disable";
                     }
-                    else if (nodes[0].data.lock
-                        && (nodes[0].data.lock.creator !== $scope.user
-                        || nodes[0].data.lock.creator !== $scope.assignee)) {
-                        //object has lock, the user is not creator or owner of parent object
+                    else if (lock.creator == $scope.user
+                        || ($scope.allowParentOwnerToCancel && lock.creator == $scope.assignee)) {
 
-                        //we will check for permissions if it is user that has permission to unlock
-                        var allowDeffered = $q.defer();
-                        //check permission for unlock
-                        PermissionsService.getActionPermission('unlock', nodes[0].data)
-                            .then(function success(hasPermission) {
-                                    if (hasPermission)
-                                        allowDeffered.resolve("");
-                                    else
-                                        allowDeffered.resolve("disable");
-                                },
-                                function error() {
-                                    allowDeffered.resolve("disable");
-                                }
-                            );
-                        return allowDeffered.promise;
-                    }
-                    else {
                         //object has lock and user is creator or owner of parent object
                         //so they should be able to unlock
-                        return "";
+                        //because backend will expect unlock permission we should check for it
+                        var df = $q.defer();
+
+                        PermissionsService.getActionPermission('unlock', fileObject)
+                            .then(function success(hasPermission) {
+                                    if (hasPermission)
+                                        df.resolve("");
+                                    else
+                                        df.resolve("disable");
+                                },
+                                function error() {
+                                    df.resolve("disable");
+                                }
+                            );
+                    }
+                    else {
+                        //object has lock, the user is not creator or owner of parent object
+
+                        //we will check for permissions if it is user that has permission to cancelLock (admin users)
+                        //and after that for unlock because backend will return access denied without unlock permission
+                        var df = $q.defer();
+
+                        PermissionsService.getActionPermission('cancelLock', fileObject)
+                            .then(function success(hasCancelPermission) {
+                                    if (hasCancelPermission) {
+                                        //check permission for unlock
+                                        PermissionsService.getActionPermission('unlock', fileObject)
+                                            .then(function success(hasPermission) {
+                                                    if (hasPermission)
+                                                        df.resolve("");
+                                                    else
+                                                        df.resolve("disable");
+                                                },
+                                                function error() {
+                                                    df.resolve("disable");
+                                                }
+                                            );
+                                    }
+                                    else
+                                        df.resolve("disable");
+                                },
+                                function error() {
+                                    df.resolve("disable");
+                                }
+                            );
+                        return df.promise;
                     }
                 }
                 else if ("checkout" == cmd || "editWithWord") {
-                    if (nodes[0].data.lock) {
+                    if (lock) {
                         return "disable";
                     } else {
-                        var allowDeffered = $q.defer();
+                        var df = $q.defer();
                         //check permission for lock
-                        PermissionsService.getActionPermission('lock', nodes[0].data)
+                        PermissionsService.getActionPermission('lock', fileObject)
                             .then(function success(hasPermission) {
                                     if (hasPermission)
-                                        allowDeffered.resolve("");
+                                        df.resolve("");
                                     else
-                                        allowDeffered.resolve("disable");
+                                        df.resolve("disable");
 
                                 },
                                 function error() {
-                                    allowDeffered.resolve("disable");
+                                    df.resolve("disable");
                                 }
                             );
-                        return allowDeffered.promise;
+                        return df.promise;
                     }
                 }
             }
