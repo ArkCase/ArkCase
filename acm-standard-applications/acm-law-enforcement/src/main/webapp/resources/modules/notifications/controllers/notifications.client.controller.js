319d00c27f3e7f67206680bf7ba9c55c1d9bebc5
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-05-04 13:34:50 +0200

    AFDP-2150_Notifications_filtered_by_user
    Show notifications for the signed in user
    
    
    Former-commit-id: 82e8866618936519f3c51ec896ed97e1581de43f

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/notifications/controllers/notifications.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/notifications/controllers/notifications.client.controller.js
@@ -1,14 +1,21 @@
 'use strict';
 
-angular.module('notifications').controller('NotificationsController', ['$scope', 'ConfigService',
-	function($scope, ConfigService) {
+angular.module('notifications').controller('NotificationsController', ['$scope', 'ConfigService', 'Authentication',
+	function($scope, ConfigService, Authentication) {
 		$scope.config = ConfigService.getModule({moduleId: 'notifications'});
 		$scope.$on('req-component-config', onConfigRequest);
+
+		Authentication.queryUserInfo().then(
+			function (userInfo) {
+				$scope.user = userInfo;
+				return userInfo;
+			}
+		);
 
 		function onConfigRequest(e, componentId) {
 			$scope.config.$promise.then(function(config){
 				var componentConfig = _.find(config.components, {id: componentId})
-				$scope.$broadcast('component-config', componentId, componentConfig);
+				$scope.$broadcast('component-config', componentId, componentConfig, $scope.user);
 			});
 		}
 	}
