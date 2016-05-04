810834eb02ae761ef96547f9d69e924dee8e2324
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-05-04 14:36:52 +0200

    fix event to be auditable
    Former-commit-id: 11a82f5299cf48a16cf1068ed9ff9fcef07de5f8

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/EcmFileUpdatedEvent.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/EcmFileUpdatedEvent.java
@@ -1,5 +1,6 @@
 package com.armedia.acm.plugins.ecm.model;
 
+import com.armedia.acm.auth.AcmAuthenticationDetails;
 import com.armedia.acm.core.model.AcmEvent;
 import org.springframework.security.core.Authentication;
 
@@ -10,14 +11,19 @@ import java.util.Date;
  */
 public class EcmFileUpdatedEvent extends AcmEvent
 {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public EcmFileUpdatedEvent(EcmFile updated, Authentication authentication)
+    public EcmFileUpdatedEvent(EcmFile updated, Authentication auth)
     {
-    	super(updated);
-    	setEventType("com.armedia.acm.ecm.file.updated");
-        setObjectType("FILE");
+        super(updated);
+        setEventType("com.armedia.acm.ecm.file.updated");
+        setObjectType(EcmFileConstants.OBJECT_FILE_TYPE);
         setObjectId(updated.getFileId());
         setEventDate(new Date());
+        if (auth.getDetails() != null && auth.getDetails() instanceof AcmAuthenticationDetails)
+        {
+            setIpAddress(((AcmAuthenticationDetails) auth.getDetails()).getRemoteAddress());
+        }
+        setUserId(auth.getName());
     }
 }
