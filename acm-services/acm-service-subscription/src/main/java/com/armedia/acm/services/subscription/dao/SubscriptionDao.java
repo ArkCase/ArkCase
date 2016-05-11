6de946f734bb7618c6575ecb9286744c9922cc42
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-05-11 12:55:28 +0200

    AFDP-1940_Delete_subscription_events_when_user_unsubscribes
    Add 'cm_subscription_id' column in 'acm_subscription_events' to have information for which subscription is the event
    Add solr filter to hide subscription events for deleted subscriptions
    Add deleteSubscriptionEvents to asynchronously delete events for deleted subscriptions
    small refactoring around module constants
    
    
    Former-commit-id: f54eb7fb81f19658886943d42614379661aa48d0

--- acm-services/acm-service-subscription/src/main/java/com/armedia/acm/services/subscription/dao/SubscriptionDao.java
+++ acm-services/acm-service-subscription/src/main/java/com/armedia/acm/services/subscription/dao/SubscriptionDao.java
@@ -4,8 +4,11 @@ import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.services.subscription.model.AcmSubscription;
 import com.armedia.acm.services.subscription.model.AcmSubscriptionEvent;
+import com.armedia.acm.services.subscription.model.SubscriptionConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Query;
@@ -21,8 +24,6 @@ import java.util.List;
 public class SubscriptionDao extends AcmAbstractDao<AcmSubscription>
 {
     private Logger LOG = LoggerFactory.getLogger(getClass());
-
-    private final String AUDIT_ACTIVITY_RESULT_SUCCESS = "success";
 
     @Override
     protected Class<AcmSubscription> getPersistenceClass()
@@ -71,12 +72,12 @@ public class SubscriptionDao extends AcmAbstractDao<AcmSubscription>
         // this should be native query? Actually acm_audit_log table is mapped as JPA entity class AuditEvent
         // and I used this model.
         Query query = getEm().createQuery("SELECT aud.objectType, aud.objectId, "
-                + "aud.userId, aud.eventDate, aud.fullEventType, sub.userId, " + "sub.objectName, sub.objectTitle "
+                + "aud.userId, aud.eventDate, aud.fullEventType, sub.userId, " + "sub.objectName, sub.objectTitle, sub.subscriptionId "
                 + "FROM AuditEvent aud, AcmSubscription sub " + "WHERE sub.subscriptionObjectType = aud.objectType "
                 + "AND sub.objectId = aud.objectId " + "AND aud.eventResult =:activityResult " + "AND aud.eventDate >:lastRunDate "
                 + (eventsToBeRemoved != null ? "AND aud.fullEventType NOT IN :eventsToBeRemoved " : ""));
 
-        query.setParameter("activityResult", AUDIT_ACTIVITY_RESULT_SUCCESS);
+        query.setParameter("activityResult", SubscriptionConstants.AUDIT_ACTIVITY_RESULT_SUCCESS);
         query.setParameter("lastRunDate", lastRunDate);
         if (eventsToBeRemoved != null)
         {
@@ -97,6 +98,7 @@ public class SubscriptionDao extends AcmAbstractDao<AcmSubscription>
             subscriptionEvent.setSubscriptionOwner((String) row[i++]);
             subscriptionEvent.setEventObjectName((String) row[i++]);
             subscriptionEvent.setEventObjectNumber((String) row[i++]);
+            subscriptionEvent.setRelatedSubscriptionId(String.format("%d-%s", row[i++], SubscriptionConstants.OBJECT_TYPE));
             result.add(subscriptionEvent);
         }
         if (result.isEmpty())
@@ -130,5 +132,23 @@ public class SubscriptionDao extends AcmAbstractDao<AcmSubscription>
             rowCount = 1;
         }
         return rowCount;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Async("deleteTaskExecutor")
+    public void deleteSubscriptionEvents(String userId, Long objectId, String objectType)
+    {
+        TypedQuery<AcmSubscriptionEvent> query = getEm().createQuery("select event from AcmSubscriptionEvent event where event.eventObjectType=:objectType " +
+                "and event.eventObjectId=:objectId and event.subscriptionOwner=:userId", AcmSubscriptionEvent.class);
+
+        query.setParameter("objectType", objectType);
+        query.setParameter("objectId", objectId);
+        query.setParameter("userId", userId);
+
+        List<AcmSubscriptionEvent> subscriptionEvents = query.getResultList();
+        for (AcmSubscriptionEvent event : subscriptionEvents)
+        {
+            getEm().remove(event);
+        }
     }
 }
