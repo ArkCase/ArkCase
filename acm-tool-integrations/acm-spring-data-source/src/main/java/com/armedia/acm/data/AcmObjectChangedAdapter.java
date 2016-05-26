a7c80957e430b55634668b7a6c3966adfc1d34c4
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2016-05-26 15:24:03 +0200

    AFDP-1860 refactored the notifier and object changed adapter added notifications for acmTask
    Former-commit-id: 6129df45da1bd047432b55e5c3cc166a204fe24e

--- acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmObjectChangedAdapter.java
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmObjectChangedAdapter.java
@@ -18,121 +18,28 @@ import static com.armedia.acm.data.AcmObjectEventConstants.*;
  */
 public class AcmObjectChangedAdapter extends DescriptorEventAdapter
 {
-    private transient Logger log = LoggerFactory.getLogger(getClass());
     private AcmObjectChangedNotifier acmObjectChangedNotifier;
 
     @Override
     public void postDelete(DescriptorEvent event)
     {
-        updateObjectInfoAndSend(ACTION_DELETE, event.getSource());
+        acmObjectChangedNotifier.notifyChange(ACTION_DELETE, event.getSource());
     }
 
     @Override
     public void postInsert(DescriptorEvent event)
     {
-        updateObjectInfoAndSend(ACTION_INSERT, event.getSource());
+        acmObjectChangedNotifier.notifyChange(ACTION_INSERT, event.getSource());
     }
 
     @Override
     public void postUpdate(DescriptorEvent event)
     {
-        updateObjectInfoAndSend(ACTION_UPDATE, event.getSource());
+        acmObjectChangedNotifier.notifyChange(ACTION_UPDATE, event.getSource());
     }
 
     public void setAcmObjectChangedNotifier(AcmObjectChangedNotifier acmObjectChangedNotifier)
     {
         this.acmObjectChangedNotifier = acmObjectChangedNotifier;
-    }
-
-    /**
-     * fill data from the original object to the event and send to the notifier
-     *
-     * @param action what action was performed
-     * @param object actual object instance
-     */
-    private void updateObjectInfoAndSend(String action, Object object)
-    {
-        AcmObjectEvent objectChangedEvent = new AcmObjectEvent(action);
-        updateAcmObjectInfo(objectChangedEvent, object);
-        updateAcmEntityInfo(objectChangedEvent, object);
-        updateAcmParentObjectInfo(objectChangedEvent, object);
-
-        //send event to the notifier
-        acmObjectChangedNotifier.notifyChange(objectChangedEvent);
-    }
-
-    /**
-     * update objectChanged with acm object info
-     *
-     * @param objectChangedEvent instance of objectChangedEvent
-     * @param object             Object
-     */
-    private void updateAcmObjectInfo(AcmObjectEvent objectChangedEvent, Object object)
-    {
-        if (!(object instanceof AcmObject))
-        {
-            //not an instance of AcmObject, nothing to do
-            return;
-        }
-        AcmObject acmObject = (AcmObject) object;
-        objectChangedEvent.setObjectId(acmObject.getId());
-        objectChangedEvent.setObjectType(acmObject.getObjectType());
-    }
-
-    /**
-     * update objectChanged with acm entity info
-     *
-     * @param objectChangedEvent instance of objectChangedEvent
-     * @param object             Object
-     */
-    private void updateAcmEntityInfo(AcmObjectEvent objectChangedEvent, Object object)
-    {
-        if (!(object instanceof AcmEntity))
-        {
-            //not an instance of AcmObject, nothing to do
-            return;
-        }
-        AcmEntity acmEntity = (AcmEntity) object;
-        Date date = null;
-        String userId = null;
-        switch (objectChangedEvent.getAction())
-        {
-            case ACTION_UPDATE:
-                date = acmEntity.getModified();
-                userId = acmEntity.getModifier();
-                break;
-            case ACTION_INSERT:
-                date = acmEntity.getCreated();
-                userId = acmEntity.getCreator();
-                break;
-            case ACTION_DELETE:
-                date = acmEntity.getModified();
-                userId = acmEntity.getModifier();
-                break;
-            default:
-                log.warn("ACTION must be provided before AcmEntity info is chosen.");
-        }
-
-        objectChangedEvent.setDate(date);
-        objectChangedEvent.setUser(userId);
-    }
-
-    /**
-     * update objectChanged with parent info
-     *
-     * @param objectChangedEvent instance of objectChangedEvent
-     * @param object             Object
-     */
-    private void updateAcmParentObjectInfo(AcmObjectEvent objectChangedEvent, Object object)
-    {
-        if (!(object instanceof AcmParentObjectInfo))
-        {
-            //not an instance of AcmObject, nothing to do
-            return;
-        }
-        AcmParentObjectInfo parentObjectInfo = (AcmParentObjectInfo) object;
-
-        objectChangedEvent.setParentObjectId(parentObjectInfo.getParentObjectId());
-        objectChangedEvent.setParentObjectType(parentObjectInfo.getParentObjectType());
     }
 }
--- acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmObjectChangedNotifier.java
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AcmObjectChangedNotifier.java
@@ -1,5 +1,7 @@
 package com.armedia.acm.data;
 
+import com.armedia.acm.core.AcmObject;
+import com.armedia.acm.core.AcmParentObjectInfo;
 import org.codehaus.plexus.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,8 +9,13 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.armedia.acm.data.AcmObjectEventConstants.ACTION_DELETE;
+import static com.armedia.acm.data.AcmObjectEventConstants.ACTION_INSERT;
+import static com.armedia.acm.data.AcmObjectEventConstants.ACTION_UPDATE;
 
 /**
  * Created by nebojsha on 06.05.2016.
@@ -21,16 +28,21 @@ public class AcmObjectChangedNotifier
     private Set<String> includeParentObjectTypes = new HashSet<>();
     private Set<String> includeClassNames = new HashSet<>();
 
-    public void notifyChange(AcmObjectEvent acmObject)
+    public void notifyChange(String action, Object object)
     {
-        if (includeClassNames.contains(acmObject.getClassName())
-                || includeObjectTypes.contains(acmObject.getObjectType())
-                || includeParentObjectTypes.contains(acmObject.getParentObjectType()))
+        AcmObjectEvent objectChangedEvent = new AcmObjectEvent(action);
+        updateAcmObjectInfo(objectChangedEvent, object);
+        updateAcmEntityInfo(objectChangedEvent, object);
+        updateAcmParentObjectInfo(objectChangedEvent, object);
+
+        if (includeClassNames.contains(objectChangedEvent.getClassName())
+                || includeObjectTypes.contains(objectChangedEvent.getObjectType())
+                || includeParentObjectTypes.contains(objectChangedEvent.getParentObjectType()))
         {
-            createAndSendMessage(acmObject);
+            createAndSendMessage(objectChangedEvent);
         } else
         {
-            log.debug("Object is not eligible for notifying, didn't pass the filters. {}", acmObject);
+            log.debug("Object is not eligible for notifying, didn't pass the filters. {}", objectChangedEvent);
         }
     }
 
@@ -84,4 +96,82 @@ public class AcmObjectChangedNotifier
         }
         return parsedCSV;
     }
+
+    /**
+     * update objectChanged with acm object info
+     *
+     * @param objectChangedEvent instance of objectChangedEvent
+     * @param object             Object
+     */
+    private void updateAcmObjectInfo(AcmObjectEvent objectChangedEvent, Object object)
+    {
+        if (!(object instanceof AcmObject))
+        {
+            //not an instance of AcmObject, nothing to do
+            return;
+        }
+        AcmObject acmObject = (AcmObject) object;
+        objectChangedEvent.setObjectId(acmObject.getId());
+        objectChangedEvent.setObjectType(acmObject.getObjectType());
+        objectChangedEvent.setClassName(object.getClass().getName());
+    }
+
+    /**
+     * update objectChanged with acm entity info
+     *
+     * @param objectChangedEvent instance of objectChangedEvent
+     * @param object             Object
+     */
+    private void updateAcmEntityInfo(AcmObjectEvent objectChangedEvent, Object object)
+    {
+        if (!(object instanceof AcmEntity))
+        {
+            //not an instance of AcmObject, nothing to do
+            return;
+        }
+        AcmEntity acmEntity = (AcmEntity) object;
+        Date date = null;
+        String userId = null;
+
+        switch (objectChangedEvent.getAction())
+        {
+            case ACTION_UPDATE:
+                date = acmEntity.getModified();
+                userId = acmEntity.getModifier();
+                break;
+            case ACTION_INSERT:
+                date = acmEntity.getCreated();
+                userId = acmEntity.getCreator();
+                break;
+            case ACTION_DELETE:
+                date = acmEntity.getModified();
+                userId = acmEntity.getModifier();
+                break;
+            default:
+                log.warn("ACTION must be provided before AcmEntity info is chosen.");
+        }
+
+        objectChangedEvent.setDate(date);
+        objectChangedEvent.setUser(userId);
+    }
+
+    /**
+     * update objectChanged with parent info
+     *
+     * @param objectChangedEvent instance of objectChangedEvent
+     * @param object             Object
+     */
+    private void updateAcmParentObjectInfo(AcmObjectEvent objectChangedEvent, Object object)
+    {
+        if (!(object instanceof AcmParentObjectInfo))
+        {
+            //not an instance of AcmObject, nothing to do
+            return;
+        }
+        AcmParentObjectInfo parentObjectInfo = (AcmParentObjectInfo) object;
+
+        objectChangedEvent.setParentObjectId(parentObjectInfo.getParentObjectId());
+        objectChangedEvent.setParentObjectType(parentObjectInfo.getParentObjectType());
+    }
+
 }
