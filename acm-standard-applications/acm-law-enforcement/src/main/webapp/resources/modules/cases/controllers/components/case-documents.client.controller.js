838f3539609de250832458f1ac6d737d294208b3
James Wu <jwu@armedia.com>
2016-06-06 00:05:07 -0400

    implement doc tree extension
    Former-commit-id: be66290577665a8ba953efa57cb433883990372d

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$stateParams', '$modal', '$q', '$timeout'
     , 'UtilService', 'ConfigService', 'ObjectService', 'Object.LookupService', 'Case.InfoService', 'DocTreeService'
-    , 'Helper.ObjectBrowserService', 'Authentication', 'PermissionsService', 'Object.ModelService'
+    , 'Helper.ObjectBrowserService', 'Authentication', 'PermissionsService', 'Object.ModelService', 'DocTreeExt.Case'
     , function ($scope, $stateParams, $modal, $q, $timeout
         , Util, ConfigService, ObjectService, ObjectLookupService, CaseInfoService, DocTreeService
-        , HelperObjectBrowserService, Authentication, PermissionsService, ObjectModelService) {
+        , HelperObjectBrowserService, Authentication, PermissionsService, ObjectModelService, DocTreeExtCase) {
 
         Authentication.queryUserInfo().then(
             function (userInfo) {
@@ -73,6 +73,8 @@ angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$sta
         $scope.onInitTree = function(treeControl) {
             $scope.treeControl = treeControl;
 
+            DocTreeExtCase.onInitTree(treeControl, $scope);
+
             //$scope.treeControl.addCommandHandler({
             //    name: "sample"
             //    , onAllowCmd: function(nodes) {
@@ -94,172 +96,6 @@ angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$sta
             //        console.log("Possible to add onPostCmd code here");
             //    }
             //});
-
-            $scope.treeControl.addCommandHandler({
-                name: "checkout"
-                , onAllowCmd: function(nodes) {
-                    var fileObject = nodes[0].data;
-                    var lock = fileObject.lock;
-                    if (lock) {
-                        return "disable";
-                    } else {
-                        var df = $q.defer();
-                        //check permission for lock
-                        PermissionsService.getActionPermission('lock', fileObject)
-                            .then(function success(hasPermission) {
-                                    if (hasPermission)
-                                        df.resolve("");
-                                    else
-                                        df.resolve("disable");
-
-                                },
-                                function error() {
-                                    df.resolve("disable");
-                                }
-                            );
-                        return df.promise;
-                    }
-                }
-            });
-
-            $scope.treeControl.addCommandHandler({
-                name: "checkin"
-                , onAllowCmd: function(nodes) {
-                    var fileObject = nodes[0].data;
-                    var lock = fileObject.lock;
-                    if (!lock) {
-                        //there is no lock so checkin should be disabled
-                        return "disable";
-                    }
-                    else if (lock
-                        && lock.creator !== $scope.user) {
-                        //there is lock on object but it is not by the user so checkin is disabled
-                        return "disable";
-                    }
-                    else if (lock.lockType !== ObjectService.LockTypes.CHECKOUT_LOCK) {
-                        //object has lock and it is by the user but it isn't checkout
-                        //it is probably edit in word so checkin should be disabled
-                        return "disable";
-                    }
-                    else {
-                        //object has lock, lockType is checkout and user is creator
-
-                        //user should be able to unlock the file with checkin if he had permisison
-                        //to checkout the file, there is not need to check for permission for unlock
-                        //but because we will use unlock service which is checking for unlock permission
-                        //we should make the check because otherwise server will return access denied
-
-                        var df = $q.defer();
-
-                        //check permission for unlock
-                        PermissionsService.getActionPermission('unlock', fileObject)
-                            .then(function success(hasPermission) {
-                                    if (hasPermission)
-                                        df.resolve("");
-                                    else
-                                        df.resolve("disable");
-                                },
-                                function error() {
-                                    df.resolve("disable");
-                                }
-                            );
-                        return df.promise;
-                    }
-                }
-            });
-
-
-            $scope.treeControl.addCommandHandler({
-                name: "editWithWord"
-                , onAllowCmd: function(nodes) {
-                    var fileObject = nodes[0].data;
-                    var lock = fileObject.lock;
-                    if (lock) {
-                        return "disable";
-                    } else {
-                        var df = $q.defer();
-                        //check permission for lock
-                        PermissionsService.getActionPermission('lock', fileObject)
-                            .then(function success(hasPermission) {
-                                    if (hasPermission)
-                                        df.resolve("");
-                                    else
-                                        df.resolve("disable");
-
-                                },
-                                function error() {
-                                    df.resolve("disable");
-                                }
-                            );
-                        return df.promise;
-                    }
-                }
-            });
-
-
-            $scope.treeControl.addCommandHandler({
-                name: "cancelEditing"
-                , onAllowCmd: function(nodes) {
-                    var fileObject = nodes[0].data;
-                    var lock = fileObject.lock;
-                    if (!lock) {
-                        //there is no lock so cancel is disabled
-                        return "disable";
-                    }
-                    else if (lock.creator == $scope.user
-                        || ($scope.allowParentOwnerToCancel && lock.creator == $scope.assignee)) {
-
-                        //object has lock and user is creator or owner of parent object
-                        //so they should be able to unlock
-                        //because backend will expect unlock permission we should check for it
-                        var df = $q.defer();
-
-                        PermissionsService.getActionPermission('unlock', fileObject)
-                            .then(function success(hasPermission) {
-                                    if (hasPermission)
-                                        df.resolve("");
-                                    else
-                                        df.resolve("disable");
-                                },
-                                function error() {
-                                    df.resolve("disable");
-                                }
-                            );
-                    }
-                    else {
-                        //object has lock, the user is not creator or owner of parent object
-
-                        //we will check for permissions if it is user that has permission to cancelLock (admin users)
-                        //and after that for unlock because backend will return access denied without unlock permission
-                        var df = $q.defer();
-
-                        PermissionsService.getActionPermission('cancelLock', fileObject)
-                            .then(function success(hasCancelPermission) {
-                                    if (hasCancelPermission) {
-                                        //check permission for unlock
-                                        PermissionsService.getActionPermission('unlock', fileObject)
-                                            .then(function success(hasPermission) {
-                                                    if (hasPermission)
-                                                        df.resolve("");
-                                                    else
-                                                        df.resolve("disable");
-                                                },
-                                                function error() {
-                                                    df.resolve("disable");
-                                                }
-                                            );
-                                    }
-                                    else
-                                        df.resolve("disable");
-                                },
-                                function error() {
-                                    df.resolve("disable");
-                                }
-                            );
-                        return df.promise;
-                    }
-                }
-            });
 
         };
 
