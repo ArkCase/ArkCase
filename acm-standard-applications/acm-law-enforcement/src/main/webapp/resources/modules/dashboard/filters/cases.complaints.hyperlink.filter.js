afbcd666e8580fcb5b6f0564cf6f8f12a2b73821
Ivo Shurbanovski <ivo.shurbanovski@armedia.com>
2016-06-09 14:55:09 +0200

    AFDP-2203 Case Id column wrong
    Case Id column is renamed to ID and does not show
    wrong data.
    
    
    Former-commit-id: 63cd43aa06367210c2773aad37a10109566bf66b

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/filters/cases.complaints.hyperlink.filter.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/filters/cases.complaints.hyperlink.filter.js
@@ -1,5 +1,16 @@
 'use strict'
 
+/**
+ * @ngdoc filter
+ * @name dashboard.my-tasks.filter:hyperlinkFilter
+ *
+ * @description
+ *
+ * {@link https://***REMOVED***/arkcase/ACM3/tree/develop/acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/filters/cases.complaints.hyperlink.filter.js modules/dashboard/filters/cases.complaints.hyperlink.filter.js}
+ *
+ * Filters the input value of attachedToObjectType to define the hyperlink (/cases/ or /complaints/)
+ */
+
 angular.module("dashboard.my-tasks").filter('hyperlinkFilter', function ($filter) {
     return function (input, char) {
         if(input == null) {
