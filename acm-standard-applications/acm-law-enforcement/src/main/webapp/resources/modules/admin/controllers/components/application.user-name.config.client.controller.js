39b2ab420f0d0f41c6d55fe10ec29ba4f528c5ab
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-06-15 14:18:34 +0200

    removed not neccesarry cancel() functions, formatting changed.
    Former-commit-id: fb4adc9014e69f1c9dd980511e132735a2130013

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/application.user-name.config.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/application.user-name.config.client.controller.js
@@ -2,43 +2,37 @@
 
 angular.module('admin').controller('Admin.UserNameConfigController', ['$scope', '$q', '$modal', 'Admin.ApplicationSettingsService',
     function ($scope, $q, $modal, ApplicationSettingsService) {
-		var oldPropertyValue;
+        var oldPropertyValue;
         ApplicationSettingsService.getProperty(ApplicationSettingsService.PROPERTIES.DISPLAY_USERNAME).then(function (response) {
             $scope.nameProperty = response.data[ApplicationSettingsService.PROPERTIES.DISPLAY_USERNAME];
             oldPropertyValue = $scope.nameProperty;
         });
 
         $scope.applyChanges = function () {
-        	if (oldPropertyValue != $scope.nameProperty) {
-        		ApplicationSettingsService.setProperty(
-        				ApplicationSettingsService.PROPERTIES.DISPLAY_USERNAME,
-        				$scope.nameProperty
-        		);
-        		$modal.open({
-        			templateUrl: 'modules/admin/views/components/application-user-name.config.modal-info.client.view.html',
-        			controller: 'AdminUserInfoModalController',
-        			backdrop: false,
-        			size: 'sm'
-        		});
-        		oldPropertyValue = $scope.nameProperty;
-        	}
+            if (oldPropertyValue != $scope.nameProperty) {
+                ApplicationSettingsService.setProperty(
+                        ApplicationSettingsService.PROPERTIES.DISPLAY_USERNAME,
+                        $scope.nameProperty
+                );
+                $modal.open({
+                    templateUrl: 'modules/admin/views/components/application-user-name.config.modal-info.client.view.html',
+                    controller: 'AdminUserInfoModalController',
+                    backdrop: false,
+                    size: 'sm'
+                });
+                oldPropertyValue = $scope.nameProperty;
+            }
         }
         $scope.close = function () {
-        	$modalInstance.dismiss('cancel');
-        };
-        $scope.cancel = function () {
-        	$modalInstance.dismiss('cancel');
+            $modalInstance.dismiss('cancel');
         };
     }
 ]);
 
 angular.module('admin').controller('AdminUserInfoModalController', ['$scope', '$modalInstance', '$modal',
     function($scope, $modalInstance, $modal) {
-	    $scope.close = function() {
-	    	$modalInstance.dismiss('cancel');
-		};
-		$scope.cancel = function() {
-			$modalInstance.dismiss('cancel');					
-		};
-	}
+        $scope.close = function() {
+            $modalInstance.dismiss('cancel');
+        };
+    }
 ]);
