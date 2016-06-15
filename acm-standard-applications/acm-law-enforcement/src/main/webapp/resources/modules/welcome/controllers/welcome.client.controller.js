40d5edb3d16d31d265ecfae22804473b728b6bda
Ivo Shurbanovski <ivo.shurbanovski@armedia.com>
2016-06-15 14:12:12 +0200

    AFDP-2144 Manualy typed url
    Fixed the redirection when manualy typed url in browser.
    
    
    Former-commit-id: 75b1e9be79c0d52379ef9c48c6ddc77d3df904e4

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/welcome/controllers/welcome.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/welcome/controllers/welcome.client.controller.js
@@ -1,27 +1,41 @@
 'use strict';
 
-angular.module('welcome').controller('WelcomeController', ['$state', '$window'
-    , 'Acm.LoginService', 'Acm.AppService'
-    , function ($state, $window, AcmLoginService, AcmAppService
-    ) {
-        AcmLoginService.resetCaches();
-        AcmLoginService.setLogin(true);
-        AcmLoginService.setLastIdle();
+angular.module('welcome').controller(
+    'WelcomeController',
+    [
+        '$state',
+        '$window',
+        'Acm.LoginService',
+        'Acm.AppService',
+        function ($state, $window, AcmLoginService, AcmAppService) {
+            AcmLoginService.resetCaches();
+            AcmLoginService.setLogin(true);
+            AcmLoginService.setLastIdle();
 
-        // TODO: check if this is needed. It is used in 'user-info.client.controller.js'. Doesn't seem like it is necessary...
-        // In the 'goodbye.client.controller.js' the AcmLoginService.setLogin(false) will do nothing because the page will already be redirected to logout page.
-        //
-        //jwu: setLogin() flag is needed for auto logout. setLogin(false) in goodbye is now fixed to call before redirection.
+            // TODO: check if this is needed. It is used in
+            // 'user-info.client.controller.js'. Doesn't seem like it is
+            // necessary...
+            // In the 'goodbye.client.controller.js' the
+            // AcmLoginService.setLogin(false) will do nothing because
+            // the page will already be redirected to logout page.
+            //
+            // jwu: setLogin() flag is needed for auto logout.
+            // setLogin(false) in goodbye is now fixed to call before
+            // redirection.
 
-        if (sessionStorage.redirectState) {
-            // redirect to the last remembered state  
-            var redirectState = angular.fromJson(sessionStorage.redirectState);
-            $state.go(redirectState.name, redirectState.params);
-        } else if (localStorage.redirectURL){
-            // redirect to hash passed in the URL of the login page
-            $window.location.href = AcmAppService.getAppUrl('home.html' + localStorage.redirectURL);
-        } else {
-            $state.go("dashboard");
-        }
-    }
-]);
+            if (sessionStorage.redirectState) {
+                // redirect to the last remembered state
+                var redirectState = angular
+                    .fromJson(sessionStorage.redirectState);
+                sessionStorage.removeItem("redirectState");
+                $state.go(redirectState.hash.split('/')[1]);
+            } else if (localStorage.redirectURL) {
+                // redirect to hash passed in the URL of the login page
+                localStorage.removeItem("redirectUrl");
+                $window.location.href = AcmAppService
+                    .getAppUrl('home.html'
+                        + localStorage.redirectURL);
+            } else {
+                $state.go("dashboard");
+            }
+        }]);
