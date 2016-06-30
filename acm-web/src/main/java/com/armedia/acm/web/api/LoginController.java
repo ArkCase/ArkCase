b91c438dafe11c396b36de7ff5f61a638947eee9
unknown <dragan.simonovski@ARM-T530-020.armedia.com>
2016-06-30 15:55:10 +0200

    Update the JSAP Warning Message Functionality
    Former-commit-id: 0e06a66d6b64fd077e3b7da0c068215acf233055

--- acm-web/src/main/java/com/armedia/acm/web/api/LoginController.java
+++ acm-web/src/main/java/com/armedia/acm/web/api/LoginController.java
@@ -1,10 +1,12 @@
 package com.armedia.acm.web.api;
 
 import com.armedia.acm.web.api.service.LoginWarningMessageService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by dragan.simonovski on 05/11/2016.
@@ -22,6 +24,18 @@ public class LoginController
         model.addAttribute("warningMessage", loginWarningMessageService.getMessage());
 
         return "login";
+    }
+
+    /**
+     * Retrieve login warning configuration
+     *
+     * @return login warning configuration
+     */
+    @RequestMapping(value = "/warning", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public LoginWarningMessageService getWarning()
+    {
+        return loginWarningMessageService;
     }
 
     public void setLoginWarningMessageService(LoginWarningMessageService loginWarningMessageService)
