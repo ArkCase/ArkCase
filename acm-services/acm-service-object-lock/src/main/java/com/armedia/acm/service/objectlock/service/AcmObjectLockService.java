e4bdf77cb898f029fef322733c5d049dd9797820
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-07-07 20:23:25 +0200

    When editing file with Word, locking control in ArkCase DB moved into WebDAV.
    Former-commit-id: 7c5c7f444540d9c53042926570df0a4431929c32

--- acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
+++ acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
@@ -1,6 +1,7 @@
 package com.armedia.acm.service.objectlock.service;
 
 import com.armedia.acm.service.objectlock.model.AcmObjectLock;
+
 import org.mule.api.MuleException;
 import org.springframework.security.core.Authentication;
 
@@ -9,51 +10,74 @@ import org.springframework.security.core.Authentication;
  */
 public interface AcmObjectLockService
 {
-    AcmObjectLock createLock(Long objectId, String objectType, String lockType, Authentication auth);
+    AcmObjectLock createLock(Long objectId, String objectType, String lockType, Boolean lockInDB, Authentication auth);
 
     void removeLock(Long objectId, String objectType, String lockType, Authentication auth);
 
     /**
      * queries documents which doesn't have lock.
      *
-     * @param objectType document object type. Can be null, than returns every document.
-     * @param auth       Authentication
-     * @param firstRow   start row
-     * @param maxRows    max rows
-     * @param sort       name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams   additional filter. Can be null.
+     * @param objectType
+     *            document object type. Can be null, than returns every document.
+     * @param auth
+     *            Authentication
+     * @param firstRow
+     *            start row
+     * @param maxRows
+     *            max rows
+     * @param sort
+     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams
+     *            additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
-    String getDocumentsWithoutLock(String objectType, Authentication auth, int firstRow, int maxRows, String sort, String fqParams) throws MuleException;
+    String getDocumentsWithoutLock(String objectType, Authentication auth, int firstRow, int maxRows, String sort, String fqParams)
+            throws MuleException;
 
     /**
      * queries documents which have lock.
      *
-     * @param objectType     document object type. Can be null, than returns every document.
-     * @param auth           Authentication
-     * @param lockHeldByUser filter locks by lock held by user. Can be null.
-     * @param firstRow       start row
-     * @param maxRows        max rows
-     * @param sort           name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams       additional filter. Can be null.
+     * @param objectType
+     *            document object type. Can be null, than returns every document.
+     * @param auth
+     *            Authentication
+     * @param lockHeldByUser
+     *            filter locks by lock held by user. Can be null.
+     * @param firstRow
+     *            start row
+     * @param maxRows
+     *            max rows
+     * @param sort
+     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams
+     *            additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
-    String getDocumentsWithLock(String objectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort, String fqParams) throws MuleException;
+    String getDocumentsWithLock(String objectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort,
+            String fqParams) throws MuleException;
 
     /**
      * queries documents which holders for object lock
      *
-     * @param parentObjectType parent document object type. Can be null, than returns every document.
-     * @param auth             Authentication
-     * @param lockHeldByUser   filter locks by lock held by user. Can be null.
-     * @param firstRow         start row
-     * @param maxRows          max rows
-     * @param sort             name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams         additional filter. Can be null.
+     * @param parentObjectType
+     *            parent document object type. Can be null, than returns every document.
+     * @param auth
+     *            Authentication
+     * @param lockHeldByUser
+     *            filter locks by lock held by user. Can be null.
+     * @param firstRow
+     *            start row
+     * @param maxRows
+     *            max rows
+     * @param sort
+     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams
+     *            additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
-    String getObjectLocks(String parentObjectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort, String fqParams) throws MuleException;
+    String getObjectLocks(String parentObjectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort,
+            String fqParams) throws MuleException;
 }
