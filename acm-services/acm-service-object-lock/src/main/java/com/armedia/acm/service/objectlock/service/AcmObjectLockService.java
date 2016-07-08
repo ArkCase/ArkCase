bc7aa85248fcd01ee75aab30c190399822c532fe
David Miller <david.miller@armedia.com>
2016-07-08 11:40:07 -0400

    AFDP-2321: added backwards-compatible version of the createLock method, to avoid breaking any extensions that may be calling it
    Former-commit-id: 212f328481fda326aba7bdc26b7dd053783b9fcf

--- acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
+++ acm-services/acm-service-object-lock/src/main/java/com/armedia/acm/service/objectlock/service/AcmObjectLockService.java
@@ -1,7 +1,6 @@
 package com.armedia.acm.service.objectlock.service;
 
 import com.armedia.acm.service.objectlock.model.AcmObjectLock;
-
 import org.mule.api.MuleException;
 import org.springframework.security.core.Authentication;
 
@@ -12,23 +11,29 @@ public interface AcmObjectLockService
 {
     AcmObjectLock createLock(Long objectId, String objectType, String lockType, Boolean lockInDB, Authentication auth);
 
+    /**
+     * This version of createLock is to maintain backwards compatibility with the method that takes a "LockInDB"
+     * parameter.  It calls the new method with a value of TRUE for lockInDB.
+     *
+     * @param objectId
+     * @param objectType
+     * @param lockType
+     * @param auth
+     * @return
+     */
+    AcmObjectLock createLock(Long objectId, String objectType, String lockType, Authentication auth);
+
     void removeLock(Long objectId, String objectType, String lockType, Authentication auth);
 
     /**
      * queries documents which doesn't have lock.
      *
-     * @param objectType
-     *            document object type. Can be null, than returns every document.
-     * @param auth
-     *            Authentication
-     * @param firstRow
-     *            start row
-     * @param maxRows
-     *            max rows
-     * @param sort
-     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams
-     *            additional filter. Can be null.
+     * @param objectType document object type. Can be null, than returns every document.
+     * @param auth       Authentication
+     * @param firstRow   start row
+     * @param maxRows    max rows
+     * @param sort       name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams   additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
@@ -38,46 +43,32 @@ public interface AcmObjectLockService
     /**
      * queries documents which have lock.
      *
-     * @param objectType
-     *            document object type. Can be null, than returns every document.
-     * @param auth
-     *            Authentication
-     * @param lockHeldByUser
-     *            filter locks by lock held by user. Can be null.
-     * @param firstRow
-     *            start row
-     * @param maxRows
-     *            max rows
-     * @param sort
-     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams
-     *            additional filter. Can be null.
+     * @param objectType     document object type. Can be null, than returns every document.
+     * @param auth           Authentication
+     * @param lockHeldByUser filter locks by lock held by user. Can be null.
+     * @param firstRow       start row
+     * @param maxRows        max rows
+     * @param sort           name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams       additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
     String getDocumentsWithLock(String objectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort,
-            String fqParams) throws MuleException;
+                                String fqParams) throws MuleException;
 
     /**
      * queries documents which holders for object lock
      *
-     * @param parentObjectType
-     *            parent document object type. Can be null, than returns every document.
-     * @param auth
-     *            Authentication
-     * @param lockHeldByUser
-     *            filter locks by lock held by user. Can be null.
-     * @param firstRow
-     *            start row
-     * @param maxRows
-     *            max rows
-     * @param sort
-     *            name of the fields with (asc|desc) separated by semicolon. Can be empty.
-     * @param fqParams
-     *            additional filter. Can be null.
+     * @param parentObjectType parent document object type. Can be null, than returns every document.
+     * @param auth             Authentication
+     * @param lockHeldByUser   filter locks by lock held by user. Can be null.
+     * @param firstRow         start row
+     * @param maxRows          max rows
+     * @param sort             name of the fields with (asc|desc) separated by semicolon. Can be empty.
+     * @param fqParams         additional filter. Can be null.
      * @return return response from solr as String
      * @throws MuleException
      */
     String getObjectLocks(String parentObjectType, Authentication auth, String lockHeldByUser, int firstRow, int maxRows, String sort,
-            String fqParams) throws MuleException;
+                          String fqParams) throws MuleException;
 }
