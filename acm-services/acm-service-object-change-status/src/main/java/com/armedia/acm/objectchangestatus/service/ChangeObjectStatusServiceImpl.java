76a9b83fb95725f771d72c4419726e4b04d5133f
Lazo Lazarev <lazo.lazarev@armedia.com>
2016-07-14 12:37:32 +0200

    AFDP-2353 Start intake process.
    Former-commit-id: 787bf48691950a6d25a054c9fec53337eac346b8

--- acm-services/acm-service-object-change-status/src/main/java/com/armedia/acm/objectchangestatus/service/ChangeObjectStatusServiceImpl.java
+++ acm-services/acm-service-object-change-status/src/main/java/com/armedia/acm/objectchangestatus/service/ChangeObjectStatusServiceImpl.java
@@ -1,42 +1,66 @@
 /**
- * 
+ *
  */
 package com.armedia.acm.objectchangestatus.service;
 
 import com.armedia.acm.core.AcmStatefulEntity;
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.data.service.AcmDataService;
+import com.armedia.acm.services.users.service.tracker.UserTrackerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author riste.tutureski
  *
  */
-public class ChangeObjectStatusServiceImpl implements ChangeObjectStatusService {
+public class ChangeObjectStatusServiceImpl implements ChangeObjectStatusService
+{
 
-	private AcmDataService acmDataService;
-	
-	@Override
-	public void change(Long objectId, String objectType, String status) 
-	{
-		AcmAbstractDao<AcmStatefulEntity> dao = getAcmDataService().getDaoByObjectType(objectType);
-		
-		if (dao != null)
-		{
-			AcmStatefulEntity entity = dao.find(objectId);
-			
-			if (entity != null)
-			{
-				entity.setStatus(status);
-				dao.save(entity);
-			}
-		}
-	}
+    private AcmDataService acmDataService;
 
-	public AcmDataService getAcmDataService() {
-		return acmDataService;
-	}
+    private UserTrackerService userTrackerService;
 
-	public void setAcmDataService(AcmDataService acmDataService) {
-		this.acmDataService = acmDataService;
-	}
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void change(Long objectId, String objectType, String status)
+    {
+
+        log.debug("User from IP {}.", userTrackerService.getTrackedUser().getIpAddress());
+
+        AcmAbstractDao<AcmStatefulEntity> dao = getAcmDataService().getDaoByObjectType(objectType);
+
+        if (dao != null)
+        {
+            AcmStatefulEntity entity = dao.find(objectId);
+
+            if (entity != null)
+            {
+                entity.setStatus(status);
+                dao.save(entity);
+            }
+        }
+    }
+
+    public AcmDataService getAcmDataService()
+    {
+        return acmDataService;
+    }
+
+    public void setAcmDataService(AcmDataService acmDataService)
+    {
+        this.acmDataService = acmDataService;
+    }
+
+    public UserTrackerService getUserTrackerService()
+    {
+        return userTrackerService;
+    }
+
+    public void setUserTrackerService(UserTrackerService userTrackerService)
+    {
+        this.userTrackerService = userTrackerService;
+    }
 }
