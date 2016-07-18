0613b4850e8df16f7d9404090c1b082c9213543f
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-07-18 14:07:29 +0200

    AFDP-2364: Sync single user
    Former-commit-id: 87813d2197cc5b715b0efe505551f25af4d61563

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/dao/ldap/SpringLdapDao.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/dao/ldap/SpringLdapDao.java
@@ -7,7 +7,9 @@ import com.armedia.acm.services.users.model.ldap.AcmLdapConfig;
 import com.armedia.acm.services.users.model.ldap.AcmLdapSyncConfig;
 import com.armedia.acm.services.users.model.ldap.AcmUserGroupsContextMapper;
 import com.armedia.acm.services.users.model.ldap.SimpleAuthenticationSource;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ldap.control.PagedResultsCookie;
@@ -272,36 +274,35 @@ public class SpringLdapDao
         return acmGroups;
     }
 
-    public AcmUser findUser(String username, LdapTemplate template, AcmLdapSyncConfig config)
+    public AcmUser findUser(String username, LdapTemplate template, AcmLdapSyncConfig config, String [] attributes)
     {
-        AcmUser user = null;
-
         SearchControls searchControls = new SearchControls();
         searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 
         userGroupsContextMapper.setUserIdAttributeName(config.getUserIdAttributeName());
         userGroupsContextMapper.setMailAttributeName(config.getMailAttributeName());
 
+        if (attributes != null)
+        {
+            String[] allAttributes = ArrayUtils.addAll(attributes,
+                    config.getUserIdAttributeName(), config.getMailAttributeName());
+            searchControls.setReturningAttributes(allAttributes);
+        }
+
         List<AcmUser> results = template.search(config.getUserSearchBase(),
                 String.format(config.getUserSearchFilter(), username), searchControls, userGroupsContextMapper);
 
-        if (results != null && !results.isEmpty())
+        if (CollectionUtils.isNotEmpty(results))
         {
             // Return the first entity that will be found. The above search can return multiple results under one domain if
             // "sAMAccountName" is the same for two users. This in theory should not be the case, but just in case, return only the first one.
             AcmUser acmUser = results.get(0);
-            if (acmUser != null)
-            {
-                // append user domain name if set. Used in Single Sign-On scenario.
-                String userDomainSuffix = ((config.getUserDomain() == null || config.getUserDomain().trim().equals("")) ? "" : "@" + config.getUserDomain());
-                log.debug("Adding user domain sufix to the usernames: {}", userDomainSuffix);
-                user.setUserId(user.getUserId() + userDomainSuffix);
-            }
-        }
 
-        if (user != null)
-        {
-            return user;
+            // append user domain name if set. Used in Single Sign-On scenario.
+            String userDomainSuffix = (StringUtils.isBlank(config.getUserDomain()) ? "" : "@" + config.getUserDomain());
+            log.debug("Adding user domain sufix to the username: {}", userDomainSuffix);
+            acmUser.setUserId(acmUser.getUserId() + userDomainSuffix);
+            return acmUser;
         }
 
         throw new UsernameNotFoundException("User with id [" + username + "] cannot be found");
