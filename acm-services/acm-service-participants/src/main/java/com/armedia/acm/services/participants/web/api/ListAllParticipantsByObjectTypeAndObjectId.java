1e92e6a23d0313da4470e2a3cadc3f0ef74da67c
Will Phillips <will.phillips@armedia.com>
2016-07-21 17:23:06 -0400

    Participant: Fixed ListAllParticipantsByObjectTypeAndObjectId controller (was calling database instead of Solr)
    Former-commit-id: 4b4420883c0df5de0c1808bb208eb6e8222a5003

--- acm-services/acm-service-participants/src/main/java/com/armedia/acm/services/participants/web/api/ListAllParticipantsByObjectTypeAndObjectId.java
+++ acm-services/acm-service-participants/src/main/java/com/armedia/acm/services/participants/web/api/ListAllParticipantsByObjectTypeAndObjectId.java
@@ -1,11 +1,11 @@
 package com.armedia.acm.services.participants.web.api;
 
-import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
-import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
-import com.armedia.acm.services.participants.model.AcmParticipant;
-import com.armedia.acm.services.participants.service.AcmParticipantEventPublisher;
-import com.armedia.acm.services.participants.service.AcmParticipantService;
-import org.eclipse.persistence.annotations.CascadeOnDelete;
+import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
+import com.armedia.acm.services.participants.model.ParticipantConstants;
+import com.armedia.acm.services.search.model.SolrCore;
+import com.armedia.acm.services.search.service.ExecuteSolrQuery;
+import org.json.JSONObject;
+import org.mule.api.MuleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -14,9 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * Created by marjan.stefanoski on 01.04.2015.
@@ -26,30 +25,47 @@ import java.util.List;
 @RequestMapping({"/api/v1/service/participant","/api/latest/service/participant"})
 public class ListAllParticipantsByObjectTypeAndObjectId {
 
-    private AcmParticipantService acmParticipantService;
+    private ExecuteSolrQuery executeSolrQuery;
 
     private transient final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping(value = "/{objectType}/{objectId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<AcmParticipant> listParticipants(
+    public String listParticipants(
             @PathVariable(value = "objectType") String objectType,
             @PathVariable(value = "objectId") Long objectId,
+            @RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
+            @RequestParam(value = "n", required = false, defaultValue = "10") int maxRows,
             Authentication authentication
-    ) throws AcmObjectNotFoundException {
+    ) throws MuleException, AcmListObjectsFailedException {
         if (log.isInfoEnabled()){
             log.info("List all participants on object ['" + objectType + "]:[" + objectId + "]");
         }
-        List<AcmParticipant>  participants = getAcmParticipantService().listAllParticipantsPerObjectTypeAndId(objectType,objectId);
-        return participants;
+
+        String query = "object_type_s:" + ParticipantConstants.OBJECT_TYPE + " AND parent_object_id_i:" + objectId + " AND parent_object_type_s:" + objectType;
+        String sort = "create_date_tdt DESC";
+
+        String results = getExecuteSolrQuery().getResultsByPredefinedQuery(authentication, SolrCore.ADVANCED_SEARCH,
+                query, startRow, maxRows, sort);
+
+        JSONObject resultsObject = new JSONObject(results);
+
+        if (resultsObject.has("response"))
+        {
+            return resultsObject.getJSONObject("response").toString();
+        }
+
+        throw new AcmListObjectsFailedException("wrong input", "user: ", null);
     }
 
-    public AcmParticipantService getAcmParticipantService() {
-        return acmParticipantService;
+    public ExecuteSolrQuery getExecuteSolrQuery()
+    {
+        return executeSolrQuery;
     }
 
-    public void setAcmParticipantService(AcmParticipantService acmParticipantService) {
-        this.acmParticipantService = acmParticipantService;
+    public void setExecuteSolrQuery(ExecuteSolrQuery executeSolrQuery)
+    {
+        this.executeSolrQuery = executeSolrQuery;
     }
 }
 
