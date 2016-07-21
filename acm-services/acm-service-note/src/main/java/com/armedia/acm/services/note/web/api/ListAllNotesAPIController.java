b0ea89f90959d7db96279d49094d15d6816e9844
Will Phillips <will.phillips@armedia.com>
2016-07-21 15:22:48 -0400

    Notes: Fixed ListAllNotes controller (was calling database instead of Solr)
    Former-commit-id: ba9438951cdf0cc27566968c4e01811215b95b9a

--- acm-services/acm-service-note/src/main/java/com/armedia/acm/services/note/web/api/ListAllNotesAPIController.java
+++ acm-services/acm-service-note/src/main/java/com/armedia/acm/services/note/web/api/ListAllNotesAPIController.java
@@ -1,15 +1,15 @@
 package com.armedia.acm.services.note.web.api;
 
+import com.armedia.acm.services.note.model.NoteConstants;
+import com.armedia.acm.services.search.model.SolrCore;
 import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
-import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
-import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
-import com.armedia.acm.services.note.dao.NoteDao;
-import com.armedia.acm.services.note.model.Note;
-import com.armedia.acm.services.note.service.NoteEventPublisher;
-
+import com.armedia.acm.services.search.service.ExecuteSolrQuery;
+import org.json.JSONObject;
+import org.mule.api.MuleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,47 +17,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.persistence.PersistenceException;
-
-import java.util.List;
-
 @Controller
 @RequestMapping({ "/api/v1/plugin/note", "/api/latest/plugin/note" })
 public class ListAllNotesAPIController {
 
-    private NoteDao noteDao;
+    private ExecuteSolrQuery executeSolrQuery;
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping(value = "/{parentType}/{parentId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<Note> findAllNotesInParentObject(
+    public String findNotesInParentObject(
             @PathVariable("parentType") String parentType,
             @PathVariable("parentId") Long parentId,
-            @RequestParam(value = "type", required = false, defaultValue = "GENERAL") String type
-    ) throws AcmObjectNotFoundException, AcmUserActionFailedException, AcmListObjectsFailedException {
+            @RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
+            @RequestParam(value = "n", required = false, defaultValue = "10") int maxRows,
+            @RequestParam(value = "type", required = false, defaultValue = "GENERAL") String type,
+            Authentication authentication
+    ) throws MuleException, AcmListObjectsFailedException {
         if (log.isInfoEnabled()) {
             log.info("Finding all notes");
         }
-        if(type != null && parentId != null && parentType != null){
-            try {
-                List<Note> noteList = getNoteDao().listNotes(type, parentId, parentType);
-                log.debug("noteList size " + noteList.size());
-                return noteList;
-            } catch (PersistenceException e) {
-                throw new AcmListObjectsFailedException("p", e.getMessage(), e);
+        if(type != null && parentId != null && parentType != null) {
+            String query = "object_type_s:" + NoteConstants.OBJECT_TYPE + " AND parent_object_id_i:" + parentId + " AND parent_object_type_s:" + parentType + " AND note_type_s:" + type;
+            String sort = "create_date_tdt DESC";
+
+            String results = getExecuteSolrQuery().getResultsByPredefinedQuery(authentication, SolrCore.ADVANCED_SEARCH,
+                    query, startRow, maxRows, sort);
+            JSONObject resultsObject = new JSONObject(results);
+
+            if (resultsObject.has("response"))
+            {
+                return resultsObject.getJSONObject("response").toString();
             }
         }
         throw new AcmListObjectsFailedException("wrong input", "user: ", null);
     }
 
-    public NoteDao getNoteDao()
+    public ExecuteSolrQuery getExecuteSolrQuery()
     {
-        return noteDao;
+        return executeSolrQuery;
     }
 
-    public void setNoteDao(NoteDao noteDao)
+    public void setExecuteSolrQuery(ExecuteSolrQuery executeSolrQuery)
     {
-        this.noteDao = noteDao;
+        this.executeSolrQuery = executeSolrQuery;
     }
 }
