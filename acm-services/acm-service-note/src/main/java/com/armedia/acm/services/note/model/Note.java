e790b596a88f8f34e09a996007636325c5df2c9a
Ike Kottlowski <immanuel.kottlowski@armedia.com>
2016-07-23 10:55:35 -0400

    added version history to the notes table in document details
    Former-commit-id: 7624d28cc59865adc71335945238661870e328af

--- acm-services/acm-service-note/src/main/java/com/armedia/acm/services/note/model/Note.java
+++ acm-services/acm-service-note/src/main/java/com/armedia/acm/services/note/model/Note.java
@@ -4,17 +4,7 @@ import com.armedia.acm.data.AcmEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -70,122 +60,101 @@ public class Note implements Serializable, AcmEntity
     private Date modified;
 
     @PrePersist
-    public void beforeInsert()
-    {
+    public void beforeInsert() {
         Date today = new Date();
         setCreated(today);
 
-        if (null == getType())
-        {
+        if (null == getType()) {
             setType(NoteConstants.NOTE_GENERAL);
         }
     }
 
-    public Long getId()
-    {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id)
-    {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getNote()
-    {
+    public String getNote() {
         return note;
     }
 
-    public void setNote(String note)
-    {
+    public void setNote(String note) {
         this.note = note;
     }
 
-    public String getType()
-    {
+    public String getType() {
         return type;
     }
 
-    public void setType(String type)
-    {
+    public void setType(String type) {
         this.type = type;
     }
 
     @Override
-    public String getCreator()
-    {
+    public String getCreator() {
         return creator;
     }
 
     @Override
-    public void setCreator(String creator)
-    {
+    public void setCreator(String creator) {
         this.creator = creator;
     }
 
     @Override
-    public Date getCreated()
-    {
+    public Date getCreated() {
         return created;
     }
 
     @Override
-    public void setCreated(Date created)
-    {
+    public void setCreated(Date created) {
         this.created = created;
     }
 
-    public Long getParentId()
-    {
+    public Long getParentId() {
         return parentId;
     }
 
-    public void setParentId(Long parentId)
-    {
+    public void setParentId(Long parentId) {
         this.parentId = parentId;
     }
 
-    public String getParentType()
-    {
+    public String getParentType() {
         return parentType;
     }
 
-    public void setParentType(String parentType)
-    {
+    public void setParentType(String parentType) {
         this.parentType = parentType;
     }
 
-    public String getTag()
-    {
+    public String getTag() {
         return tag;
     }
 
-    public void setTag(String tag)
-    {
+    public void setTag(String tag) {
         this.tag = tag;
     }
 
     @Override
-    public String getModifier()
-    {
+    public String getModifier() {
         return modifier;
     }
 
     @Override
-    public void setModifier(String modifier)
-    {
+    public void setModifier(String modifier) {
         this.modifier = modifier;
     }
 
     @Override
-    public Date getModified()
-    {
+    public Date getModified() {
         return modified;
     }
 
     @Override
-    public void setModified(Date modified)
-    {
+    public void setModified(Date modified) {
         this.modified = modified;
     }
+
 }
