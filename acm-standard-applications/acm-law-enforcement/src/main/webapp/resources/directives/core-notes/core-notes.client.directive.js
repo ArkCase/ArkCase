638b5da9a9d2f6ccef1758a39dec6057ea4900fc
Will Phillips <will.phillips@armedia.com>
2016-07-25 15:29:01 -0400

    Notes: Added frontend support for experimental showFullUserNames
    Former-commit-id: 7ae89d403ca8cef7e006634d78f5208ce691e0ad

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-notes/core-notes.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-notes/core-notes.client.directive.js
@@ -95,7 +95,7 @@ angular.module('directives').directive('coreNotes', ['$q', '$modal', '$translate
                             gridHelper.setColumnDefs(config);
                             gridHelper.setBasicOptions(config);
                             gridHelper.disableGridScrolling(config);
-                            gridHelper.setUserNameFilter(promiseUsers);
+                            gridHelper.showUserFullNames();
                             scope.retrieveGridData();
                         }
                     }
@@ -118,10 +118,10 @@ angular.module('directives').directive('coreNotes', ['$q', '$modal', '$translate
                     if (Util.goodPositive(scope.notesInit.currentObjectId, false)) {
                         var info = scope.notesInit;
                         var promiseQueryNotes = ObjectNoteService.queryNotes(info.objectType, info.currentObjectId, info.noteType);
-                        $q.all([promiseQueryNotes, promiseUsers]).then(function (data) {
+                        $q.all([promiseQueryNotes]).then(function (data) {
                             var notes = data[0];
-                            scope.gridOptions.data = notes;
-                            scope.gridOptions.totalItems = notes.length;
+                            scope.gridOptions.data = notes.docs;
+                            scope.gridOptions.totalItems = notes.docs.length;
                         });
                     }
                 };
