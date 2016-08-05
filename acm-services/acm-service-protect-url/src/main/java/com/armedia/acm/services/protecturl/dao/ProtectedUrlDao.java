d1eb0b99567aa91e601aa863258bc4c9ca581b12
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2016-08-05 13:33:52 +0200

    protectUrl-improvements added new dao, service method to retrive protected urls by original url
    Former-commit-id: a3d1e6cbe2f226473b58845d17e6c1d9de457c51

--- acm-services/acm-service-protect-url/src/main/java/com/armedia/acm/services/protecturl/dao/ProtectedUrlDao.java
+++ acm-services/acm-service-protect-url/src/main/java/com/armedia/acm/services/protecturl/dao/ProtectedUrlDao.java
@@ -5,6 +5,7 @@ import com.armedia.acm.services.protecturl.model.ProtectedUrl;
 
 import javax.persistence.Query;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by nebojsha on 27.07.2016.
@@ -28,6 +29,19 @@ public class ProtectedUrlDao extends AcmAbstractDao<ProtectedUrl>
         Query protectedUrlQuery = getEm().createQuery("SELECT pu FROM ProtectedUrl pu WHERE pu.obfuscatedUrl=:obfuscatedUrl");
         protectedUrlQuery.setParameter("obfuscatedUrl", obfuscatedUrl);
         return (ProtectedUrl) protectedUrlQuery.getSingleResult();
+    }
+
+    /**
+     * returns list of ProtectedUrl if found for given parameter of originalUrl, if not found throws NoResultException
+     *
+     * @param originalUrl String  originalUrl
+     * @return instance of List<ProtectedUrl>
+     */
+    public List<ProtectedUrl> findByRealUrl(String originalUrl)
+    {
+        Query protectedUrlQuery = getEm().createQuery("SELECT pu FROM ProtectedUrl pu WHERE pu.originalUrl=:originalUrl");
+        protectedUrlQuery.setParameter("originalUrl", originalUrl);
+        return protectedUrlQuery.getResultList();
     }
 
     /**
