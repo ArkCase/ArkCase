37637befa8987cd341ac1757edc87c1c2d5c6c95
marst <marjan.stefanoski@armedia.com>
2016-08-08 16:33:51 +0200

    AFDP-2236 not tested widget (New Cases)
    Former-commit-id: 9049e7895448b106e3f4c5aa89d7103427137b7c

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/controllers/components/new-cases.client.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('dashboard.new-cases').controller('Dashboard.NewCasesController', ['$scope', '$translate', 'Authentication', 'Dashboard.DashboardService',
+    function ($scope, $translate, Authentication, DashboardService) {
+
+        var vm = this;
+
+        $scope.$on('component-config', applyConfig);
+        $scope.$emit('req-component-config', 'newCases');
+
+        vm.config = null;
+
+        function applyConfig(e, componentId, config) {
+            if (componentId == 'newCases') {
+
+                DashboardService.queryNewCases(function (solrData) {
+
+                    var data = {};
+                    var chartData = [];
+                    var labels = [];
+                    angular.forEach(solrData.response.caseDocs, function (caseDoc) {
+                        var day = caseDoc.create_date_tdt;
+                        day = day.substring(0, day.indexOf("T"));
+                        data[day] ? data[day]++ : data[day] = 1;
+                    });
+
+                    angular.forEach(data, function (index, value) {
+                        labels.push(value);
+                        chartData.push(index);
+                    });
+
+                    vm.showChart = labels.length > 0 && chartData.length > 0 ? true : false;
+
+                    vm.labels = labels.reverse();
+                    vm.data = [chartData.reverse()];
+                    vm.series = [config.title];
+                })
+            }
+        }
+    }
+]);
+
