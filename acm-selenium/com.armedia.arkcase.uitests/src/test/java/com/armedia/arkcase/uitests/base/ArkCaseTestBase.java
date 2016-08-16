b7d5d34094881580897c9d8324b857a7c931b1f7
Sanja Paskova <sanja.paskova@armedia.com>
2016-08-16 10:05:23 -0400

    Added remote parameters for hub and port on selenium grid
    Former-commit-id: 0bf924076e6b59b2010adeee29799ffd940e3169

--- acm-selenium/com.armedia.arkcase.uitests/src/test/java/com/armedia/arkcase/uitests/base/ArkCaseTestBase.java
+++ acm-selenium/com.armedia.arkcase.uitests/src/test/java/com/armedia/arkcase/uitests/base/ArkCaseTestBase.java
@@ -33,69 +33,109 @@ public class ArkCaseTestBase {
 
 	@BeforeClass
 	public static void setUp() throws Exception {
-        String remote = TestsPoperties.getRemoteInfo();
-        if (remote == "no")
-        {
-		ArkCaseUtils folder = new ArkCaseUtils();
+        ArkCaseUtils folder = new ArkCaseUtils();
 		folder.createFolder();
 		browser = TestsPoperties.getBrowser();
-		switch (browser) {
-		case "firefox": {
-			FirefoxProfile fprofile = new FirefoxProfile();
-			File file = new File("/.arkcase/seleniumTests/seleniumDownload/");
-			fprofile.setPreference("browser.download.dir", System.getProperty("user.home") + file);
-			fprofile.setPreference("browser.download.folderList", 2);
-			fprofile.setPreference("browser.helperApps.neverAsk.saveToDisk",
-					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;" + "application/pdf;"
-							+ "application/vnd.openxmlformats-officedocument.wordprocessingml.document;"
-							+ "text/plain; " + "img/png" + "text/csv");
-			fprofile.setPreference("browser.download.manager.showWhenStarting", false);
-			fprofile.setPreference("pdfjs.disabled", true);
-			driver = new FirefoxDriver(fprofile);
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-			// baseUrl = TestsPoperties.getBaseUrlCore();
-			driver.manage().window().maximize();
-			break;
-		}
-		case "ie": {
-			String exe = (System.getProperty("user.home") + "/.arkcase/seleniumTests/drivers/IEDriverServer.exe");
-			System.setProperty("webdriver.ie.driver", exe);
-			driver = new InternetExplorerDriver();
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-			// baseUrl = TestsPoperties.getBaseUrlCore();
-			driver.manage().window().maximize();
-			break;
-		}
-		case "chrome": {
-			String exePath = (System.getProperty("user.home") + "/.arkcase/seleniumTests/drivers/chromedriver.exe");
-			System.setProperty("webdriver.chrome.driver", exePath);
-			driver = new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-			// baseUrl = TestsPoperties.getBaseUrlCore();
-			driver.manage().window().maximize();
-			break;
-		}
-		case "safari": {
-			driver = new SafariDriver();
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-			// baseUrl = TestsPoperties.getBaseUrlCore();
-			driver.manage().window().maximize();
-			break;
-		}
-		}
-        }
-		else 
+		String remote = TestsPoperties.getRemoteInfo();
+		String hubip =TestsPoperties.getHubIP();
+		String hubport =TestsPoperties.getHubPort();
+		switch (remote) {
+		case "yes":
 		{
-			 DesiredCapabilities cap = new DesiredCapabilities();			 
-			 cap.setBrowserName(TestsPoperties.getBrowser());			 
-			 cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
-			 driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
-			 driver.manage().window().maximize();
-			 
+			switch (browser){
+			case "firefox": {
+				DesiredCapabilities cap = DesiredCapabilities.firefox();
+				driver = new RemoteWebDriver(new URL("http://" + hubip + ":" + hubport + "/wd/hub"), cap);				
+				break;
+			}
+			case "chrome": {
+				DesiredCapabilities cap = DesiredCapabilities.chrome();
+				driver = new RemoteWebDriver(new URL("http://" + hubip + ":" + hubport + "/wd/hub"), cap);				
+				break;
+			}
+			case "ie":{
+				DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
+				driver = new RemoteWebDriver(new URL("http://" + hubip + ":" + hubport + "/wd/hub"), cap);					
+				break;
+			}
+			case "safari":{
+				DesiredCapabilities cap = DesiredCapabilities.safari();
+				driver = new RemoteWebDriver(new URL("http://" + hubip + ":" + hubport + "/wd/hub"), cap);			
+				break;
+			}
+			
+	     	//change localhost:2222 with 10.21.10.50:4444
+			}				
+			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			break;
+			
+		}
+			
+		case "no":
+		{			
+			switch (browser) {
+	    	case "firefox": {
+				FirefoxProfile fprofile = new FirefoxProfile();
+				File file = new File("/.arkcase/seleniumTests/seleniumDownload/");
+				fprofile.setPreference("browser.download.dir", System.getProperty("user.home") + file);
+				fprofile.setPreference("browser.download.folderList", 2);
+				fprofile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+						"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;" + "application/pdf;"
+								+ "application/vnd.openxmlformats-officedocument.wordprocessingml.document;"
+								+ "text/plain; " + "img/png" + "text/csv");
+				fprofile.setPreference("browser.download.manager.showWhenStarting", false);
+				fprofile.setPreference("pdfjs.disabled", true);
+				driver = new FirefoxDriver();
+				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				// baseUrl = TestsPoperties.getBaseUrlCore();
+				driver.manage().window().maximize();
+				break;
+			}
+			case "ie": {
+				String exe = (System.getProperty("user.home") + "/.arkcase/seleniumTests/drivers/IEDriverServer.exe");
+				System.setProperty("webdriver.ie.driver", exe);
+				driver = new InternetExplorerDriver();
+				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				// baseUrl = TestsPoperties.getBaseUrlCore();
+				driver.manage().window().maximize();
+				break;
+			}
+			case "chrome": {
+				String exePath = (System.getProperty("user.home") + "/.arkcase/seleniumTests/drivers/chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", exePath);
+				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				// baseUrl = TestsPoperties.getBaseUrlCore();
+				driver.manage().window().maximize();
+				break;
+			}
+			case "safari": {
+				driver = new SafariDriver();
+				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				// baseUrl = TestsPoperties.getBaseUrlCore();
+				driver.manage().window().maximize();
+				break;
+			}
+			}
+             break;
+		}
+		}
+		}
 		
-		}
-
-		}
+		
+			
+		
+		
+			
+		
+		
+		
+		
+     	
+		
+		//baseUrl = TestsPoperties.getBaseUrlCore();
+		
 
 	
 
