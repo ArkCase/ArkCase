e0205f89c64cf8f8fd47ec6ded0410084051ef6f
David Miller <david.miller@armedia.com>
2016-08-16 13:14:27 -0400

    AFDP-2419 - decrypt property values returned by the config service
    Former-commit-id: ec2e1b7d0dfe5b2818198b866fb25f80a9c88ad3

--- acm-services/acm-service-config/src/main/java/com/armedia/acm/services/config/model/PropertyConfig.java
+++ acm-services/acm-service-config/src/main/java/com/armedia/acm/services/config/model/PropertyConfig.java
@@ -1,56 +1,107 @@
 package com.armedia.acm.services.config.model;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.armedia.acm.core.exceptions.AcmEncryptionException;
+import com.armedia.acm.crypto.properties.AcmEncryptablePropertyUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 
-public class PropertyConfig implements AcmConfig, Serializable
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PropertyConfig implements AcmConfig, Serializable, InitializingBean
 {
     private transient final Logger log = LoggerFactory.getLogger(getClass());
     private static final long serialVersionUID = -1L;
 
     private String configName;
-    private Map<Object, Object> properties;
+    private Map<Object, Object> properties = new HashMap<>();
     private String configDescription;
+    private AcmEncryptablePropertyUtils encryptablePropertyUtils;
 
-    public String getConfigAsJson() {
+    public String getConfigAsJson()
+    {
         String json = "{}";
         ObjectMapper om = new ObjectMapper();
-        try {
-            json =  om.writeValueAsString(getProperties());
-        } catch (JsonProcessingException e) {
+        try
+        {
+            json = om.writeValueAsString(getProperties());
+        } catch (JsonProcessingException e)
+        {
             log.error(e.getMessage());
         }
         return json;
     }
-    public String getConfigName() {
+
+    public String getConfigName()
+    {
         return configName;
     }
 
-    public void setConfigName(String configName) {
+    public void setConfigName(String configName)
+    {
         this.configName = configName;
     }
 
-    public Map<Object, Object> getProperties() {
+    public Map<Object, Object> getProperties()
+    {
         return properties;
     }
 
-    public void setProperties(Map<Object, Object> properties) {
+    public void setProperties(Map<Object, Object> properties)
+    {
         this.properties = properties;
     }
 
     @Override
-    public String getConfigDescription() {
+    public String getConfigDescription()
+    {
         return configDescription;
     }
 
-    public void setConfigDescription(String configDescription) {
+    public void setConfigDescription(String configDescription)
+    {
         this.configDescription = configDescription;
+    }
+
+    public AcmEncryptablePropertyUtils getEncryptablePropertyUtils()
+    {
+        return encryptablePropertyUtils;
+    }
+
+    public void setEncryptablePropertyUtils(AcmEncryptablePropertyUtils encryptablePropertyUtils)
+    {
+        this.encryptablePropertyUtils = encryptablePropertyUtils;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        log.debug("config name: {}", getConfigName());
+        if (properties != null)
+        {
+            for (Map.Entry<Object, Object> prop : properties.entrySet())
+            {
+                if (prop.getKey() != null && prop.getKey() instanceof String)
+                {
+                    String value = (String) prop.getValue();
+                    try
+                    {
+                        String decryptedIfNecessary = getEncryptablePropertyUtils().decryptPropertyValue(value);
+                        if (value != decryptedIfNecessary)
+                        {
+                            log.debug("Decrypted property {}", prop.getKey());
+                        }
+                        prop.setValue(decryptedIfNecessary);
+                    } catch (AcmEncryptionException aee)
+                    {
+                        log.debug("Could not decrypt property value: {}", aee.getMessage(), aee);
+                    }
+                }
+            }
+        }
     }
 }
