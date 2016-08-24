8d7876483342966dafdce5305bd83ad012971ec8
Ike Kottlowski <immanuel.kottlowski@armedia.com>
2016-08-24 15:24:04 -0400

    adding java functionality to costsheet
    Former-commit-id: e9887f3e93e3d617c3694011c488e4e851a4face

--- acm-services/acm-service-costsheet/src/main/java/com/armedia/acm/services/costsheet/web/GetCostsheetsAPIController.java
+++ acm-services/acm-service-costsheet/src/main/java/com/armedia/acm/services/costsheet/web/GetCostsheetsAPIController.java
@@ -1,8 +1,11 @@
 /**
- * 
+ *
  */
 package com.armedia.acm.services.costsheet.web;
 
+import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
+import com.armedia.acm.services.costsheet.model.CostsheetConstants;
+import com.armedia.acm.services.costsheet.service.CostsheetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -13,48 +16,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
-import com.armedia.acm.services.costsheet.model.CostsheetConstants;
-import com.armedia.acm.services.costsheet.service.CostsheetService;
-
 /**
  * @author riste.tutureski
- *
  */
 @Controller
-@RequestMapping({ "/api/v1/service/costsheet", "/api/latest/service/costsheet" })
-public class GetCostsheetsAPIController {
+@RequestMapping({"/api/v1/service/costsheet", "/api/latest/service/costsheet"})
+public class GetCostsheetsAPIController
+{
 
-	private Logger LOG = LoggerFactory.getLogger(getClass());
-	private CostsheetService costsheetService;
-	
-	@RequestMapping(value="", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public String getCostsheets(@RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
-            @RequestParam(value = "n", required = false, defaultValue = "10") int maxRows,			
-            @RequestParam(value = "s", required = false, defaultValue = "") String sort,
-            Authentication auth) throws AcmListObjectsFailedException
-	{
-		if (LOG.isInfoEnabled()) 
-		{
-			LOG.info("Taking all costsheets.");
-		}
-		
-		String jsonResponse = getCostsheetService().getObjectsFromSolr(CostsheetConstants.OBJECT_TYPE, auth, startRow, maxRows, sort, null);
-		
-		if (jsonResponse == null)
-		{
-			throw new AcmListObjectsFailedException(CostsheetConstants.OBJECT_TYPE, "Could not retrieve list of Costsheets.", new Throwable());
-		}
-		
-		return jsonResponse;
-	}
+    private Logger LOG = LoggerFactory.getLogger(getClass());
+    private CostsheetService costsheetService;
 
-	public CostsheetService getCostsheetService() {
-		return costsheetService;
-	}
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getCostsheets(@RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
+                                @RequestParam(value = "n", required = false, defaultValue = "10") int maxRows,
+                                @RequestParam(value = "s", required = false, defaultValue = "") String sort,
+                                Authentication auth) throws AcmListObjectsFailedException
+    {
+        if (LOG.isInfoEnabled())
+        {
+            LOG.info("Taking all costsheets.");
+        }
 
-	public void setCostsheetService(CostsheetService costsheetService) {
-		this.costsheetService = costsheetService;
-	}
+        String jsonResponse = getCostsheetService().getObjectsFromSolr(CostsheetConstants.OBJECT_TYPE, auth, startRow, maxRows, sort, "*", null);
+
+        if (jsonResponse == null)
+        {
+            throw new AcmListObjectsFailedException(CostsheetConstants.OBJECT_TYPE, "Could not retrieve list of Costsheets.", new Throwable());
+        }
+
+        return jsonResponse;
+    }
+
+    public CostsheetService getCostsheetService()
+    {
+        return costsheetService;
+    }
+
+    public void setCostsheetService(CostsheetService costsheetService)
+    {
+        this.costsheetService = costsheetService;
+    }
 }
--- acm-services/acm-service-costsheet/src/test/java/com/armedia/acm/services/costsheet/web/GetCostsheetsAPIControllerTest.java
+++ acm-services/acm-service-costsheet/src/test/java/com/armedia/acm/services/costsheet/web/GetCostsheetsAPIControllerTest.java
@@ -1,15 +1,13 @@
 /**
- * 
+ *
  */
 package com.armedia.acm.services.costsheet.web;
 
+import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
+import com.armedia.acm.services.costsheet.service.CostsheetService;
+import com.armedia.acm.services.search.service.SearchResults;
 import org.apache.commons.io.IOUtils;
 import org.easymock.EasyMockSupport;
-
-import static org.easymock.EasyMock.expect;
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -28,102 +26,101 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
-import com.armedia.acm.core.exceptions.AcmListObjectsFailedException;
-import com.armedia.acm.services.costsheet.service.CostsheetService;
-import com.armedia.acm.services.search.service.SearchResults;
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 /**
  * @author riste.tutureski
- *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath:/spring/spring-library-service-costsheet-test.xml"
 })
-public class GetCostsheetsAPIControllerTest extends EasyMockSupport {
+public class GetCostsheetsAPIControllerTest extends EasyMockSupport
+{
 
-	private Logger LOG = LoggerFactory.getLogger(getClass());
-	
-	private MockMvc mockMvc;
-	private CostsheetService mockCostsheetService;
-	private GetCostsheetsAPIController unit;
-	private Authentication mockAuthentication;
-	private SearchResults searchResults;
-	
-	@Autowired
+    private Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private MockMvc mockMvc;
+    private CostsheetService mockCostsheetService;
+    private GetCostsheetsAPIController unit;
+    private Authentication mockAuthentication;
+    private SearchResults searchResults;
+
+    @Autowired
     private ExceptionHandlerExceptionResolver exceptionResolver;
-	
-	@Before
+
+    @Before
     public void setUp() throws Exception
     {
-		mockCostsheetService = createMock(CostsheetService.class);
-		unit = new GetCostsheetsAPIController();
-		mockMvc = MockMvcBuilders.standaloneSetup(unit).setHandlerExceptionResolvers(exceptionResolver).build();
-		mockAuthentication = createMock(Authentication.class);
-		searchResults = new SearchResults();
-		
-		unit.setCostsheetService(mockCostsheetService);
+        mockCostsheetService = createMock(CostsheetService.class);
+        unit = new GetCostsheetsAPIController();
+        mockMvc = MockMvcBuilders.standaloneSetup(unit).setHandlerExceptionResolvers(exceptionResolver).build();
+        mockAuthentication = createMock(Authentication.class);
+        searchResults = new SearchResults();
+
+        unit.setCostsheetService(mockCostsheetService);
     }
-	
-	@Test
-	public void getCostsheetsSuccessTest() throws Exception
-	{
-		String expected = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("txt/expectedSolrResponse.txt"));
-		
-		expect(mockAuthentication.getName()).andReturn("acm-user");
-		expect(mockCostsheetService.getObjectsFromSolr("COSTSHEET", mockAuthentication, 0, 10, "", null)).andReturn(expected);
-		
-		replayAll();
-		
-		MvcResult result = mockMvc.perform(
-	            get("/api/v1/service/costsheet")
-                    .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .principal(mockAuthentication))
-	                .andReturn();
-		
-		verifyAll();
-		
-		LOG.info("Results: " + result.getResponse().getContentAsString());
-		
-		int numFound = searchResults.getNumFound(result.getResponse().getContentAsString());
-		JSONArray docs = searchResults.getDocuments(result.getResponse().getContentAsString());
-		JSONObject doc = docs.getJSONObject(0);
-		
-		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
-		assertEquals(1, numFound);
-		assertEquals("0001-COSTSHEET", doc.getString("id"));
-	}
-	
-	@Test
-	public void getCostsheetsFailedTest() throws Exception
-	{		
-		Class<?> expectedThrowableClass = AcmListObjectsFailedException.class;
-		
-		expect(mockAuthentication.getName()).andReturn("acm-user");
-		expect(mockCostsheetService.getObjectsFromSolr("COSTSHEET", mockAuthentication, 0, 10, "", null)).andReturn(null);
-		
-		replayAll();
-		
-		MvcResult result = null;
-		Exception exception = null;
-		try
-		{
-			result = mockMvc.perform(
-	            get("/api/v1/service/costsheet")
-                    .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .principal(mockAuthentication))
-	                .andReturn();
-		}
-		catch(Exception e)
-		{
-			exception = e;
-		}
-		
-		verifyAll();
-		
-		assertEquals(null, result);
-		assertEquals(expectedThrowableClass, exception.getCause().getClass());
-	}	
+
+    @Test
+    public void getCostsheetsSuccessTest() throws Exception
+    {
+        String expected = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("txt/expectedSolrResponse.txt"));
+
+        expect(mockAuthentication.getName()).andReturn("acm-user");
+        expect(mockCostsheetService.getObjectsFromSolr("COSTSHEET", mockAuthentication, 0, 10, "", "*", null)).andReturn(expected);
+
+        replayAll();
+
+        MvcResult result = mockMvc.perform(
+                get("/api/v1/service/costsheet")
+                        .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .principal(mockAuthentication))
+                .andReturn();
+
+        verifyAll();
+
+        LOG.info("Results: " + result.getResponse().getContentAsString());
+
+        int numFound = searchResults.getNumFound(result.getResponse().getContentAsString());
+        JSONArray docs = searchResults.getDocuments(result.getResponse().getContentAsString());
+        JSONObject doc = docs.getJSONObject(0);
+
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+        assertEquals(1, numFound);
+        assertEquals("0001-COSTSHEET", doc.getString("id"));
+    }
+
+    @Test
+    public void getCostsheetsFailedTest() throws Exception
+    {
+        Class<?> expectedThrowableClass = AcmListObjectsFailedException.class;
+
+        expect(mockAuthentication.getName()).andReturn("acm-user");
+        expect(mockCostsheetService.getObjectsFromSolr("COSTSHEET", mockAuthentication, 0, 10, "", "*", null)).andReturn(null);
+
+        replayAll();
+
+        MvcResult result = null;
+        Exception exception = null;
+        try
+        {
+            result = mockMvc.perform(
+                    get("/api/v1/service/costsheet")
+                            .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .principal(mockAuthentication))
+                    .andReturn();
+        } catch (Exception e)
+        {
+            exception = e;
+        }
+
+        verifyAll();
+
+        assertEquals(null, result);
+        assertEquals(expectedThrowableClass, exception.getCause().getClass());
+    }
 }
