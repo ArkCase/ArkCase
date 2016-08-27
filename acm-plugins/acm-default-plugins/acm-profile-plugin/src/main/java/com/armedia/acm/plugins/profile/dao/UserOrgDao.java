5c9c3d860c12a1f504fb1d26c86b72d899bfd05e
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-08-27 23:40:42 +0200

    Fix bug AFDP-1946&AFDP-2459
    - Removed logic from controller to 'UserOrgService'
    - Introduced new service 'OrganizationService'
    - Merged two controllers in one 'ProfileInfoAPIController'
    
    
    Former-commit-id: 1e9115c58b7d5cfa76c7c17094c05b5d0d098366

--- acm-plugins/acm-default-plugins/acm-profile-plugin/src/main/java/com/armedia/acm/plugins/profile/dao/UserOrgDao.java
+++ acm-plugins/acm-default-plugins/acm-profile-plugin/src/main/java/com/armedia/acm/plugins/profile/dao/UserOrgDao.java
@@ -1,63 +1,28 @@
 package com.armedia.acm.plugins.profile.dao;
 
-import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.plugins.profile.model.UserOrg;
-import com.armedia.acm.plugins.profile.model.UserOrgConstants;
-import com.armedia.acm.services.users.model.AcmUser;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
-import java.util.List;
-
-/**
- * Created by marjan.stefanoski on 20.10.2014.
- */
 public class UserOrgDao extends AcmAbstractDao<UserOrg>
 {
 
-    private transient final Logger log = LoggerFactory.getLogger(getClass());
-
-    public UserOrg getUserOrgForUserId(String userId) throws AcmObjectNotFoundException
+    public UserOrg getUserOrgForUserId(String userId)
     {
         String jpql = "SELECT uo FROM UserOrg uo where uo.user.userId = :userId";
-
         TypedQuery<UserOrg> query = getEm().createQuery(jpql, UserOrg.class);
-
         query.setParameter("userId", userId);
-
         try
         {
-            UserOrg retval = query.getSingleResult();
-            return retval;
+            return query.getSingleResult();
         } catch (NoResultException e)
         {
-            throw new AcmObjectNotFoundException(UserOrgConstants.OBJECT_TYPE, null, "No user profile for user " + "'" + userId + "'", e);
+            return null;
         }
-    }
-
-    public UserOrg getUserOrgForUser(AcmUser user) throws AcmObjectNotFoundException
-    {
-
-        CriteriaBuilder builder = getEm().getCriteriaBuilder();
-        CriteriaQuery<UserOrg> query = builder.createQuery(UserOrg.class);
-        Root<UserOrg> d = query.from(UserOrg.class);
-        query.select(d).where(builder.equal(d.get("user"), user));
-        TypedQuery<UserOrg> dbQuery = getEm().createQuery(query);
-        List<UserOrg> results = null;
-        results = dbQuery.getResultList();
-        if (results.isEmpty())
-        {
-            throw new AcmObjectNotFoundException("profile", null, "Object not found", null);
-        }
-        return results.get(0);
     }
 
     @Override
