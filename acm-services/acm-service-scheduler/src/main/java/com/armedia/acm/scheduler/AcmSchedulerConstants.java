dad569a90a9fb24b8d92abdfbd8e829f01e198d6
Lazo Lazarev <lazo.lazarev@armedia.com>
2016-08-29 11:11:29 +0200

    AFDP-2455 Added documentation.
    Former-commit-id: a0341dce177926a3164b6dc4c6f7177861a1a5a6

--- acm-services/acm-service-scheduler/src/main/java/com/armedia/acm/scheduler/AcmSchedulerConstants.java
+++ acm-services/acm-service-scheduler/src/main/java/com/armedia/acm/scheduler/AcmSchedulerConstants.java
@@ -1,9 +1,9 @@
-/**
- *
- */
 package com.armedia.acm.scheduler;
 
 /**
+ * A non functional interface that contains constants used by the scheduler infrastructure for processing the scheduler
+ * configuration.
+ *
  * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Aug 25, 2016
  *
  */
@@ -11,42 +11,43 @@ public interface AcmSchedulerConstants
 {
 
     /**
-     *
+     * The scheduler task name.
      */
     String SCHEDULED_TASKS_CONFIGUTATION_FILENAME = "scheduledTasks.json";
 
     /**
-     *
+     * Key in the scheduler configuration that specifies if scheduler is enabled.
      */
     String SCHEDULE_ENABLED_KEY = "scheduleEnabled";
 
     /**
-     *
+     * Key in the scheduler configuration that specifies the interval at which the scheduler thread should be invoked.
      */
     String SCHEDULE_INTERVAL_KEY = "scheduleInterval";
 
     /**
-     *
+     * Key in the scheduler configuration that specifies the tasks configuration.
      */
     String TASKS_KEY = "tasks";
 
     /**
-     *
+     * Key in the task configuration that specifies the task name.
      */
     String NAME_KEY = "name";
 
     /**
-     *
+     * Key in the task configuration that specifies how often a task should be run.
      */
     String HOW_OFTEN_KEY = "howOften";
 
     /**
-     *
+     * Key in the task configuration that specifies when was the last time a task was run.
      */
     String TASK_LAST_RUN_KEY = "taskLastRun";
 
     /**
-     *
+     * Key in the task configuration that specifies the name (id) under which a bean is registered in the Spring
+     * context.
      */
     String BEAN_NAME_KEY = "beanName";
 
