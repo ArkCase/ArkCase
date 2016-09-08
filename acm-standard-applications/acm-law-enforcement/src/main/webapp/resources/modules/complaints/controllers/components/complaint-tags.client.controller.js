3680775f7a86b9960bd4a38ae29730d2f6f4bc1d
Teng Wang <teng.wang@armedia.com>
2016-09-08 14:21:52 -0400

    Fix tags creator display in modules
    Former-commit-id: f1d9b291cd4cb3ca1c9fc4b8a676cba2acd52f42

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-tags.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-tags.client.controller.js
@@ -7,8 +7,8 @@ angular.module('complaints').controller('Complaints.TagsController', ['$scope', 
         , Util, ObjectService, ConfigService, ObjectCostService, ComplaintInfoService
         , HelperUiGridService, HelperObjectBrowserService, ObjectTagsService, $modal, messageService) {
 
-		$scope.tags = [];
-	
+        $scope.tags = [];
+
         var componentHelper = new HelperObjectBrowserService.Component({
             scope: $scope
             , stateParams: $stateParams
@@ -20,13 +20,13 @@ angular.module('complaints').controller('Complaints.TagsController', ['$scope', 
                 return onConfigRetrieved(componentConfig);
             }
             , onObjectInfoRetrieved: function (objectInfo) {
-            	onObjectInfoRetrieved(objectInfo);
+                onObjectInfoRetrieved(objectInfo);
             }
         });
 
         var gridHelper = new HelperUiGridService.Grid({scope: $scope});
-        
-        
+        var promiseUsers = gridHelper.getUsers();
+
         $scope.addNew = function () {
             var modalInstance = $modal.open({
                 animation: $scope.animationsEnabled,
@@ -35,44 +35,44 @@ angular.module('complaints').controller('Complaints.TagsController', ['$scope', 
                 size: 'lg'
             });
 
-            modalInstance.result.then(function (tags) {            	
-            	_.forEach(tags, function(tag) {         	
-            		if(tag.id){
-            			if(tag.object_id_s){
-		            		var tagsFound = _.filter($scope.tags, function (tagAss) {
-		                        return tagAss.id == tag.object_id_s;
-		                    });
-		            		if(tagsFound.length == 0) {
-			            		ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.COMPLAINT, tag.object_id_s).then(
-			                            function (returnedTag) {
-			                            	var tagToAdd = angular.copy(returnedTag);
-											tagToAdd.tagName = tag.tags_s;
-                                            tagToAdd.id = returnedTag.tagId;
-			                                $scope.tags.push(tagToAdd);
-			                                $scope.gridOptions.data = $scope.tags;
-			                                $scope.gridOptions.totalItems = $scope.tags.length;
-			                            }
-			                        );
-		            		}
-		            		else {
-		            			messageService.info(tag.tags_s + " " + $translate.instant('complaints.comp.tags.message.tagAssociated'));
-		            			_.remove(tagsFound, function(){
-		            				return tag;
-		            			});
-		            		}
-            			}
-            			else {
-            				ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.COMPLAINT, tag.id).then(
-		                            function () {
-		                                $scope.tags.push(tag);
-		                                $scope.gridOptions.data = $scope.tags;
-		                                $scope.gridOptions.totalItems = $scope.tags.length;
-		                            }
-		                        );
-            			}
-            		}
-            	});             
-            	
+            modalInstance.result.then(function (tags) {
+                _.forEach(tags, function (tag) {
+                    if (tag.id) {
+                        if (tag.object_id_s) {
+                            var tagsFound = _.filter($scope.tags, function (tagAss) {
+                                return tagAss.id == tag.object_id_s;
+                            });
+                            if (tagsFound.length == 0) {
+                                ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.COMPLAINT, tag.object_id_s).then(
+                                    function (returnedTag) {
+                                        var tagToAdd = angular.copy(returnedTag);
+                                        tagToAdd.tagName = tag.tags_s;
+                                        tagToAdd.id = returnedTag.tagId;
+                                        $scope.tags.push(tagToAdd);
+                                        $scope.gridOptions.data = $scope.tags;
+                                        $scope.gridOptions.totalItems = $scope.tags.length;
+                                    }
+                                );
+                            }
+                            else {
+                                messageService.info(tag.tags_s + " " + $translate.instant('complaints.comp.tags.message.tagAssociated'));
+                                _.remove(tagsFound, function () {
+                                    return tag;
+                                });
+                            }
+                        }
+                        else {
+                            ObjectTagsService.associateTag(componentHelper.currentObjectId, ObjectService.ObjectTypes.COMPLAINT, tag.id).then(
+                                function () {
+                                    $scope.tags.push(tag);
+                                    $scope.gridOptions.data = $scope.tags;
+                                    $scope.gridOptions.totalItems = $scope.tags.length;
+                                }
+                            );
+                        }
+                    }
+                });
+
             }, function () {
                 // Cancel button was clicked.
             });
@@ -80,12 +80,12 @@ angular.module('complaints').controller('Complaints.TagsController', ['$scope', 
 
         var onConfigRetrieved = function (config) {
 
-        	$scope.config = config;
+            $scope.config = config;
             gridHelper.addButton(config, "delete");
             gridHelper.setColumnDefs(config);
             gridHelper.setBasicOptions(config);
             gridHelper.disableGridScrolling(config);
-            gridHelper.showUserFullNames();
+            gridHelper.setUserNameFilter(promiseUsers);
 
         };
 
@@ -94,8 +94,8 @@ angular.module('complaints').controller('Complaints.TagsController', ['$scope', 
 
             var currentObjectId = Util.goodMapValue(objectInfo, "complaintId");
             if (Util.goodPositive(currentObjectId, false)) {
-            	var promiseQueryTags = ObjectTagsService.getAssociateTags(currentObjectId, ObjectService.ObjectTypes.COMPLAINT);
-                $q.all([promiseQueryTags]).then(function (data) {
+                var promiseQueryTags = ObjectTagsService.getAssociateTags(currentObjectId, ObjectService.ObjectTypes.COMPLAINT);
+                $q.all([promiseQueryTags, promiseUsers]).then(function (data) {
                     $scope.tags = data[0];
                     $scope.gridOptions = $scope.gridOptions || {};
                     $scope.gridOptions.data = $scope.tags;
