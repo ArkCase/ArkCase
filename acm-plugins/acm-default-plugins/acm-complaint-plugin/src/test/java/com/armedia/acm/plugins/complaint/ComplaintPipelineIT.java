12ef43dd974a4bcffdccc63e14f802113925b834
David Miller <david.miller@armedia.com>
2016-09-09 15:39:55 -0400

    AFDP-2535 remove all references to drools deployer, along with the deployer itself
    Former-commit-id: af84e833483f096558a552de3e07808e7ca22cff

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/ComplaintPipelineIT.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/test/java/com/armedia/acm/plugins/complaint/ComplaintPipelineIT.java
@@ -32,7 +32,6 @@ import static org.junit.Assert.assertNotNull;
                 "/spring/spring-library-activiti-actions.xml",
                 "/spring/spring-library-activiti-configuration.xml",
                 "/spring/spring-library-folder-watcher.xml",
-                "/spring/spring-library-drools-monitor.xml",
                 "/spring/spring-library-user-service.xml",
                 "/spring/spring-library-context-holder.xml",
                 "/spring/spring-library-data-access-control.xml",
