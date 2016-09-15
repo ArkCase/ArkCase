e5dfdcdb0789f70d5a8c4e3bf057f07762e95ed4
Sanja Paskova <sanja.paskova@armedia.com>
2016-09-15 10:00:38 -0400

    Added multiple browser run capabilities
    Former-commit-id: fee530df40aa1fc3c21ffaf18d23f5a76fc1fdae

--- acm-protractor/conf.js
+++ acm-protractor/conf.js
@@ -1,9 +1,28 @@
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-
-    cababilities: {
-        'browserName': 'chrome'
-    },
+		  directConnect: true,
+		  //next 3 lines are for run on selenium GRID, the path should be changed to take drivers from ACM configuraion project
+		  //not working for safari browser, should be investigated why? 
+		  //seleniumAddress: 'http://localhost:4444/wd/hub',		  
+		  //seleniumArgs: '-Dwebdriver.ie.driver=C:/Users/sanja.paskova/AppData/Roaming/npm/node_modules/protractor/selenium/IEDriverServer_x64_2.52.0.exe',
+		  //seleniumArgs: '-Dwebdriver.safari.driver=C:/Users/sanja.paskova/AppData/Roaming/npm/node_modules/protractor/selenium/SafariDriver.safariextz',
+		  // Capabilities to be passed to the webdriver instance.
+		  multiCapabilities: [{
+		    'browserName': 'chrome',
+			'maxInstances': 5
+		  }],
+		  //if you want to run in paralel comment previous line and uncomment all above
+//		  }, {
+//			    'browserName': 'internet explorer',  
+//		        'maxInstances': 5,		
+//		        'version': '11'	
+//		  }, {
+//			    'browserName': 'firefox',  
+//		        'maxInstances': 5
+//		  
+//		  }, {
+//		    'browserName': 'safari'	,
+//			'maxInstances': 5
+//		  }],
 
     specs: ['./tasks_page.spec.js'],
 
