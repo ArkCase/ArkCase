17f797f9aef0c7d3f76d8769bdd5a64eec6bfd26
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-09-22 12:19:21 +0200

    Changes after code review
    Former-commit-id: 8cae6a730806622df82855917ee8d6c8ede21b2b

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/search/search.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/search/search.client.directive.js
@@ -273,10 +273,16 @@ angular.module('directives').directive('search', ['SearchService', 'Search.Query
                         scope.customization.showParentObject(objectData);
 
                     } else {
-                        var parentReference = Util.goodMapValue(objectData, "parent_ref_s", "-");
-                        var objectId = parentReference.substring(0, parentReference.indexOf('-'));
-                        var objectTypeKey = parentReference.substring(parentReference.indexOf('-') + 1);
-                        ObjectService.showObject(objectTypeKey, objectId);
+                    	if(typeof (objectData.related_object_type_s && objectData.related_object_id_l) !== 'undefined'){
+                    		var objectTypeKey = Util.goodMapValue(objectData, "related_object_type_s");
+                            var objectId = Util.goodMapValue(objectData, "related_object_id_l");
+                    	}else {
+                            var parentReference = Util.goodMapValue(objectData, "parent_ref_s", "-");
+                            var objectId = parentReference.substring(0, parentReference.indexOf('-'));
+                            var objectTypeKey = parentReference.substring(parentReference.indexOf('-') + 1);
+                           
+                    	}
+                    	 ObjectService.showObject(objectTypeKey, objectId);
                     }
                 };
                 
