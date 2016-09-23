38665caace225f66772e29d782fcf52726623aa7
James Wu <jwu@armedia.com>
2016-09-23 15:05:44 -0400

    remove unnecessary doctree object changed event handling
    Former-commit-id: 2ae48c77e1f8f8ad50a86f67d712779b5dfe788e

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/controllers/components/case-documents.client.controller.js
@@ -106,17 +106,21 @@ angular.module('cases').controller('Cases.DocumentsController', ['$scope', '$sta
             //    }
             //});
 
-            //if there is subscription from other object we want to unsubscribe
-            //we want to have only one subscription from the current object
-            if ($scope.subscription) {
-                $scope.$bus.unsubscribe($scope.subscription);
-            }
-            var eventName = "object.changed/" + $scope.objectType + "/" + $scope.objectId;
-            $scope.subscription = $scope.$bus.subscribe(eventName, function (data) {
-                if (data.objectType == 'FILE') {
-                    $scope.treeControl.refreshTree();
-                }
-            });
+            //
+            // Unnecessary object changed event handling
+            //
+            //
+            ////if there is subscription from other object we want to unsubscribe
+            ////we want to have only one subscription from the current object
+            //if ($scope.subscription) {
+            //    $scope.$bus.unsubscribe($scope.subscription);
+            //}
+            //var eventName = "object.changed/" + $scope.objectType + "/" + $scope.objectId;
+            //$scope.subscription = $scope.$bus.subscribe(eventName, function (data) {
+            //    if (data.objectType == 'FILE') {
+            //        $scope.treeControl.refreshTree();
+            //    }
+            //});
         };
 
         $scope.onClickRefresh = function () {
