45bd91cb718d8fdd5010ade0466b65fcedf47ffd
Sanja Paskova <sanja.paskova@armedia.com>
2016-09-28 04:59:33 -0400

    added reporter that creates screenshot after every test function and generates xml file
    Former-commit-id: 42f5de6a15a25a6208465e24988ee3c7e796cfa1

--- acm-protractor/conf/conf.js
+++ acm-protractor/conf/conf.js
@@ -41,10 +41,23 @@ exports.config = {
     },
 
 
-    onPrepare: function() {
+    onPrepare: function () {
+    	var AllureReporter = require(process.env['USERPROFILE'] + '/node_modules/jasmine-allure-reporter');        
+        jasmine.getEnv().addReporter(new AllureReporter({
+        	allureReport: {
+        		resultsDir: 'allure-results'
+        	}
+        }));
+        jasmine.getEnv().afterEach(function(done){
+          browser.takeScreenshot().then(function (png) {
+            allure.createAttachment('Screenshot', function () {
+              return new Buffer(png, 'base64')
+            }, 'image/png')();
+            done();
+          })  
+        });
+        browser.driver.manage().window().maximize();       
         browser.driver.get('http://cloud.arkcase.com/arkcase/login');
-        browser.driver.manage().window().maximize();
-        browser.ignoresynchronization = true;
-
-    },
+        
+    }
 };
