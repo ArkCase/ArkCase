9a5d21bee2cbb89ec0683013b8e70adc83fd4211
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-10-03 15:06:36 +0200

    AFDP-2358 Complaint person details related events
    - Add logic for publishing an event if "PersonAlias" is deleted, added or updated.
    - Add unit tests for "PersonAlias" events
    
    
    Former-commit-id: 4476d733252c038672a52f0f83ad7e33e68e7538

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/ComplaintEventListener.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/ComplaintEventListener.java
@@ -6,6 +6,9 @@ import com.armedia.acm.plugins.addressable.model.PostalAddress;
 import com.armedia.acm.plugins.complaint.model.Complaint;
 import com.armedia.acm.plugins.complaint.model.ComplaintConstants;
 import com.armedia.acm.plugins.outlook.service.OutlookContainerCalendarService;
+import com.armedia.acm.plugins.person.model.Person;
+import com.armedia.acm.plugins.person.model.PersonAlias;
+import com.armedia.acm.plugins.person.model.PersonAssociation;
 import com.armedia.acm.service.objecthistory.dao.AcmAssignmentDao;
 import com.armedia.acm.service.objecthistory.model.AcmAssignment;
 import com.armedia.acm.service.objecthistory.model.AcmObjectHistory;
@@ -17,7 +20,11 @@ import com.armedia.acm.services.participants.utils.ParticipantUtils;
 import microsoft.exchange.webservices.data.core.enumeration.service.DeleteMode;
 import org.springframework.context.ApplicationListener;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ComplaintEventListener implements ApplicationListener<AcmObjectHistoryEvent>
 {
@@ -30,6 +37,8 @@ public class ComplaintEventListener implements ApplicationListener<AcmObjectHist
     private boolean shouldDeleteCalendarFolder;
     private String complaintStatusClosed;
 
+    private String ipAddress;
+
     @Override
     public void onApplicationEvent(AcmObjectHistoryEvent event)
     {
@@ -41,6 +50,9 @@ public class ComplaintEventListener implements ApplicationListener<AcmObjectHist
 
             if (isComplaint)
             {
+
+                ipAddress = event.getIpAddress();
+
                 // Converter for JSON string to Object
                 AcmUnmarshaller converter = ObjectConverter.createJSONUnmarshaller();
 
@@ -62,30 +74,36 @@ public class ComplaintEventListener implements ApplicationListener<AcmObjectHist
 
                     if (isPriorityChanged(existing, updatedComplaint))
                     {
-                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, event.getIpAddress(), "priority.changed");
+                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "priority.changed");
                     }
 
                     if (isDetailsChanged(existing, updatedComplaint))
                     {
-                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, event.getIpAddress(), "details.changed");
+                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "details.changed");
                     }
 
                     if (isStatusChanged(existing, updatedComplaint))
                     {
                         String calId = updatedComplaint.getContainer().getCalendarFolderId();
                         if (updatedComplaint.getStatus().equals(complaintStatusClosed) &&
-                                shouldDeleteCalendarFolder && calId != null){
+                                shouldDeleteCalendarFolder && calId != null)
+                        {
 
                             //delete shared calendar if complaint closed
                             getCalendarService().deleteFolder(updatedComplaint.getContainer().getContainerObjectId(),
                                     calId, DeleteMode.MoveToDeletedItems);
                         }
-                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, event.getIpAddress(), "status.changed");
+                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "status.changed");
                     }
 
                     if (isLocationChanged(existing, updatedComplaint))
                     {
-                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, event.getIpAddress(), "location.updated");
+                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "location.updated");
+                    }
+
+                    if (isPersonAliasUpdated(existing, updatedComplaint))
+                    {
+                        getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "personAlias.updated");
                     }
 
                     checkParticipants(existing, updatedComplaint, event.getIpAddress());
@@ -97,7 +115,7 @@ public class ComplaintEventListener implements ApplicationListener<AcmObjectHist
                     getAcmAssignmentDao().save(acmAssignment);
 
                     // Raise an event
-                    getAcmObjectHistoryEventPublisher().publishAssigneeChangeEvent(acmAssignment, event.getUserId(), event.getIpAddress());
+                    getAcmObjectHistoryEventPublisher().publishAssigneeChangeEvent(acmAssignment, event.getUserId(), ipAddress);
                 }
             }
         }
@@ -198,6 +216,84 @@ public class ComplaintEventListener implements ApplicationListener<AcmObjectHist
         String status = complaint.getStatus();
         return !updatedStatus.equals(status);
     }
+
+    private boolean isPersonAliasUpdated(Complaint complaint, Complaint updatedComplaint)
+    {
+        Set<PersonAssociation> existingPersonAssociation = new HashSet<>(complaint.getPersonAssociations());
+        Set<PersonAssociation> updatedPersonAssociation = new HashSet<>(updatedComplaint.getPersonAssociations());
+        Map<Long, Person> updatedPersons = updatedPersonAssociation.stream()
+                .collect(Collectors.toMap(pa -> pa.getPerson().getId(), pa -> pa.getPerson()));
+
+        boolean isPersonAliasAddedOrRemoved = false;
+        for (PersonAssociation pa : existingPersonAssociation)
+        {
+            Person person = pa.getPerson();
+            Person matchingPerson = updatedPersons.get(person.getId());
+            List<PersonAlias> existingPersonAliases = person.getPersonAliases();
+            List<PersonAlias> updatedPersonAliases = matchingPerson.getPersonAliases();
+
+            if (isPersonAliasAdded(existingPersonAliases, updatedPersonAliases))
+            {
+                getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "personAlias.added");
+                isPersonAliasAddedOrRemoved = true;
+            }
+            if (isPersonAliasRemoved(existingPersonAliases, updatedPersonAliases))
+            {
+                getComplaintEventPublisher().publishComplaintModified(updatedComplaint, ipAddress, "personAlias.deleted");
+                isPersonAliasAddedOrRemoved = true;
+            }
+            if (isPersonAliasAddedOrRemoved)
+            {
+                return false;
+            } else if (isPersonAliasEdited(existingPersonAliases, updatedPersonAliases))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPersonAliasEdited(List<PersonAlias> existingPersonAliases, List<PersonAlias> updatedPersonAliases)
+    {
+        for (PersonAlias personAlias : updatedPersonAliases)
+        {
+            for (PersonAlias exPersonAlias : existingPersonAliases)
+            {
+                if (!personAlias.equals(exPersonAlias))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isPersonAliasAdded(List<PersonAlias> existing, List<PersonAlias> updated)
+    {
+        for (PersonAlias personAlias : updated)
+        {
+            if (!existing.contains(personAlias))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPersonAliasRemoved(List<PersonAlias> existing, List<PersonAlias> updated)
+    {
+
+        for (PersonAlias personAlias : existing)
+        {
+            if (!updated.contains(personAlias))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
     private boolean checkExecution(String objectType)
     {
