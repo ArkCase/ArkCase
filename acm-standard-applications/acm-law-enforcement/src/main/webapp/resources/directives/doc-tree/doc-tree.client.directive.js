673876cbf6851bbefc76f196b813086047f3a9aa
James Wu <jwu@armedia.com>
2016-10-05 13:38:35 -0400

    separate email out to extension file
    Former-commit-id: b40b2c14e11c5b44d50131e6515802e9b2e25eae

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
@@ -84,11 +84,11 @@
  */
 angular.module('directives').directive('docTree', ['$q', '$translate', '$modal', '$filter', '$log', '$timeout', '$injector'
     , 'Acm.StoreService', 'UtilService', 'Util.DateService', 'ConfigService', 'LookupService'
-    , 'EcmService', 'Ecm.EmailService', 'Ecm.RecordService', 'Authentication', 'Helper.NoteService', 'Object.NoteService'
+    , 'EcmService', 'Ecm.RecordService', 'Authentication', 'Helper.NoteService', 'Object.NoteService'
     , '$browser', '$location', 'Object.LockingService', 'ObjectService', 'Object.CorrespondenceService', 'TicketService'
     , function ($q, $translate, $modal, $filter, $log, $timeout, $injector
         , Store, Util, UtilDateService, ConfigService, LookupService
-        , Ecm, EcmEmailService, EcmRecordService, Authentication, HelperNoteService, ObjectNoteService
+        , Ecm, EcmRecordService, Authentication, HelperNoteService, ObjectNoteService
         , $browser, $location, LockingService, ObjectService, ObjectCorrespondenceService, TicketService
     ) {
 
@@ -98,12 +98,12 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
 
         var cacheTree = new Store.CacheFifo();
         var cacheFolderList = new Store.CacheFifo();
-        var promiseGetEmailParams = LookupService.getConfig("notification").then(function (data) {
-            Email.arkcaseUrl = Util.goodValue(data["arkcase.url"]);
-            Email.arkcasePort = Util.goodValue(data["arkcase.port"]);
-            Email.allowMailFilesAsAttachments = ("true" == Util.goodValue(data["notification.allowMailFilesAsAttachments"], "true"));
-            Email.allowMailFilesToExternalAddresses = ("true" == Util.goodValue(data["notification.allowMailFilesToExternalAddresses"], "true"));
-        });
+        //var promiseGetEmailParams = LookupService.getConfig("notification").then(function (data) {
+        //    Email.arkcaseUrl = Util.goodValue(data["arkcase.url"]);
+        //    Email.arkcasePort = Util.goodValue(data["arkcase.port"]);
+        //    Email.allowMailFilesAsAttachments = ("true" == Util.goodValue(data["notification.allowMailFilesAsAttachments"], "true"));
+        //    Email.allowMailFilesToExternalAddresses = ("true" == Util.goodValue(data["notification.allowMailFilesToExternalAddresses"], "true"));
+        //});
 
         var DocTree = {
             CLIPBOARD: null
@@ -1454,13 +1454,13 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                                 });
                             }
                         }
-                        , {
-                            name: "email",
-                            execute: function (nodes, args) {
-                                var node = nodes[0];
-                                Email.openModal(nodes);
-                            }
-                        }
+                        //, {
+                        //    name: "email",
+                        //    execute: function (nodes, args) {
+                        //        var node = nodes[0];
+                        //        Email.openModal(nodes);
+                        //    }
+                        //}
                         , {
                             name: "declare",
                             execute: function (nodes, args) {
@@ -3796,108 +3796,108 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
             }
         };
 
-        var Email = {
-            arkcaseUrl: "localhost"
-            , arkcasePort: ""
-            , allowMailFilesAsAttachments: true
-            , allowMailFilesToExternalAddresses: true
-            , API_DOWNLOAD_DOCUMENT: "/api/v1/plugin/ecm/download?ecmFileId="
-
-            , openModal: function (nodes) {
-                var params = {
-                    config: Util.goodMapValue(DocTree.treeConfig, "emailDialog", {})
-                };
-
-                var modalInstance = $modal.open({
-                    templateUrl: "directives/doc-tree/doc-tree.email.dialog.html"
-                    , controller: 'directives.DocTreeEmailDialogController'
-                    , animation: true
-                    , size: 'lg'
-                    , resolve: {
-                        params: function () {
-                            return params;
-                        }
-                    }
-                });
-                modalInstance.result.then(function (recipients) {
-                    if (!Util.isArrayEmpty(recipients)) {
-                        var emailAddresses = _.pluck(recipients, "email");
-
-                        if (Email.allowMailFilesAsAttachments) {
-                            var emailData = Email._makeEmailDataForEmailWithAttachments(emailAddresses, nodes);
-                            EcmEmailService.sendEmailWithAttachments(emailData);
-                        }
-                        else {
-                            //var emailData = Email._makeEmailDataForEmailWithLinks(emailAddresses, nodes, title);
-                            var emailData = Email._makeEmailDataForEmailWithLinks(emailAddresses, nodes);
-                            EcmEmailService.sendEmail(emailData);
-                        }
-                    }
-                });
-            }
-
-            , _buildSubject: function () {
-                var subject = Util.goodMapValue(DocTree, "treeConfig.email.emailSubject");
-                var regex = new RegExp(Util.goodMapValue(DocTree, "treeConfig.email.subjectRegex"));
-                var match = subject.match(regex);
-                if (match) {
-                    var objectType = match[Util.goodMapValue(DocTree, "treeConfig.email.objectTypeRegexGroup")];
-                    var objectNumber = match[Util.goodMapValue(DocTree, "treeConfig.email.objectNumberRegexGroup")];
-                    if (objectType && objectNumber) {
-                        return objectType + DocTree.objectInfo[objectNumber];
-                    }
-                }
-
-                return "";
-            }
-            , _makeEmailDataForEmailWithLinks: function (emailAddresses, nodes, title) {
-                var emailData = {};
-                emailData.subject = this._buildSubject();
-                emailData.title = Util.goodValue(title, $translate.instant("common.directive.docTree.email.defaultTitle"));
-                emailData.header = $translate.instant("common.directive.docTree.email.headerForLinks");
-                emailData.footer = "\n\n" + $translate.instant("common.directive.docTree.email.footerForLinks");
-                emailData.emailAddresses = emailAddresses;
-                emailData.fileIds = Email._extractFileIds(nodes);
-                emailData.baseUrl = Email._makeBaseUrl();
-                return emailData;
-            }
-            , _makeEmailDataForEmailWithAttachments: function (emailAddresses, nodes) {
-                var emailData = {};
-                emailData.subject = this._buildSubject();
-                emailData.body = $translate.instant("common.directive.docTree.email.bodyForAttachment");
-                emailData.header = $translate.instant("common.directive.docTree.email.headerForAttachment");
-                emailData.footer = $translate.instant("common.directive.docTree.email.footerForAttachment");
-                emailData.emailAddresses = emailAddresses;
-                var attachmentIds = [];
-                if (Util.isArray(nodes)) {
-                    for (var i = 0; i < nodes.length; i++) {
-                        attachmentIds.push(Util.goodMapValue(nodes[i], "data.objectId"));
-                    }
-                }
-
-                emailData.attachmentIds = attachmentIds;
-                return emailData;
-            }
-            , _extractFileIds: function (nodes) {
-                var fileIds = [];
-                if (Util.isArray(nodes)) {
-                    for (var i = 0; i < nodes.length; i++) {
-                        fileIds.push(Util.goodMapValue(nodes[i], "data.objectId"));
-                    }
-                }
-                return fileIds;
-            }
-            , _makeBaseUrl: function () {
-                var url = Util.goodValue(Email.arkcaseUrl);
-                if (!Util.isEmpty(Email.arkcasePort)) {
-                    url += ":" + Util.goodValue(Email.arkcasePort);
-                }
-                var baseHref = $browser.baseHref().slice(0, -1);
-                url += baseHref + Email.API_DOWNLOAD_DOCUMENT;
-                return url;
-            }
-
-        }; // end Email
+        //var Email = {
+        //    arkcaseUrl: "localhost"
+        //    , arkcasePort: ""
+        //    , allowMailFilesAsAttachments: true
+        //    , allowMailFilesToExternalAddresses: true
+        //    , API_DOWNLOAD_DOCUMENT: "/api/v1/plugin/ecm/download?ecmFileId="
+        //
+        //    , openModal: function (nodes) {
+        //        var params = {
+        //            config: Util.goodMapValue(DocTree.treeConfig, "emailDialog", {})
+        //        };
+        //
+        //        var modalInstance = $modal.open({
+        //            templateUrl: "directives/doc-tree/doc-tree-ext.email.dialog.html"
+        //            , controller: 'directives.DocTreeEmailDialogController'
+        //            , animation: true
+        //            , size: 'lg'
+        //            , resolve: {
+        //                params: function () {
+        //                    return params;
+        //                }
+        //            }
+        //        });
+        //        modalInstance.result.then(function (recipients) {
+        //            if (!Util.isArrayEmpty(recipients)) {
+        //                var emailAddresses = _.pluck(recipients, "email");
+        //
+        //                if (Email.allowMailFilesAsAttachments) {
+        //                    var emailData = Email._makeEmailDataForEmailWithAttachments(emailAddresses, nodes);
+        //                    EcmEmailService.sendEmailWithAttachments(emailData);
+        //                }
+        //                else {
+        //                    //var emailData = Email._makeEmailDataForEmailWithLinks(emailAddresses, nodes, title);
+        //                    var emailData = Email._makeEmailDataForEmailWithLinks(emailAddresses, nodes);
+        //                    EcmEmailService.sendEmail(emailData);
+        //                }
+        //            }
+        //        });
+        //    }
+        //
+        //    , _buildSubject: function () {
+        //        var subject = Util.goodMapValue(DocTree, "treeConfig.email.emailSubject");
+        //        var regex = new RegExp(Util.goodMapValue(DocTree, "treeConfig.email.subjectRegex"));
+        //        var match = subject.match(regex);
+        //        if (match) {
+        //            var objectType = match[Util.goodMapValue(DocTree, "treeConfig.email.objectTypeRegexGroup")];
+        //            var objectNumber = match[Util.goodMapValue(DocTree, "treeConfig.email.objectNumberRegexGroup")];
+        //            if (objectType && objectNumber) {
+        //                return objectType + DocTree.objectInfo[objectNumber];
+        //            }
+        //        }
+        //
+        //        return "";
+        //    }
+        //    , _makeEmailDataForEmailWithLinks: function (emailAddresses, nodes, title) {
+        //        var emailData = {};
+        //        emailData.subject = this._buildSubject();
+        //        emailData.title = Util.goodValue(title, $translate.instant("common.directive.docTree.email.defaultTitle"));
+        //        emailData.header = $translate.instant("common.directive.docTree.email.headerForLinks");
+        //        emailData.footer = "\n\n" + $translate.instant("common.directive.docTree.email.footerForLinks");
+        //        emailData.emailAddresses = emailAddresses;
+        //        emailData.fileIds = Email._extractFileIds(nodes);
+        //        emailData.baseUrl = Email._makeBaseUrl();
+        //        return emailData;
+        //    }
+        //    , _makeEmailDataForEmailWithAttachments: function (emailAddresses, nodes) {
+        //        var emailData = {};
+        //        emailData.subject = this._buildSubject();
+        //        emailData.body = $translate.instant("common.directive.docTree.email.bodyForAttachment");
+        //        emailData.header = $translate.instant("common.directive.docTree.email.headerForAttachment");
+        //        emailData.footer = $translate.instant("common.directive.docTree.email.footerForAttachment");
+        //        emailData.emailAddresses = emailAddresses;
+        //        var attachmentIds = [];
+        //        if (Util.isArray(nodes)) {
+        //            for (var i = 0; i < nodes.length; i++) {
+        //                attachmentIds.push(Util.goodMapValue(nodes[i], "data.objectId"));
+        //            }
+        //        }
+        //
+        //        emailData.attachmentIds = attachmentIds;
+        //        return emailData;
+        //    }
+        //    , _extractFileIds: function (nodes) {
+        //        var fileIds = [];
+        //        if (Util.isArray(nodes)) {
+        //            for (var i = 0; i < nodes.length; i++) {
+        //                fileIds.push(Util.goodMapValue(nodes[i], "data.objectId"));
+        //            }
+        //        }
+        //        return fileIds;
+        //    }
+        //    , _makeBaseUrl: function () {
+        //        var url = Util.goodValue(Email.arkcaseUrl);
+        //        if (!Util.isEmpty(Email.arkcasePort)) {
+        //            url += ":" + Util.goodValue(Email.arkcasePort);
+        //        }
+        //        var baseHref = $browser.baseHref().slice(0, -1);
+        //        url += baseHref + Email.API_DOWNLOAD_DOCUMENT;
+        //        return url;
+        //    }
+        //
+        //}; // end Email
 
         //var Comment = {
         //    openModal: function (node) {
@@ -4736,63 +4736,4 @@ angular.module('directives').controller('directives.DocTreeDndDialogController',
 );
 
 
-angular.module('directives').controller('directives.DocTreeEmailDialogController', ['$scope', '$modalInstance'
-        , 'UtilService', 'params'
-        , function ($scope, $modalInstance, Util, params) {
-            $scope.modalInstance = $modalInstance;
-            $scope.config = params.config;
-
-            $scope.recipients = [];
-            $scope.onItemsSelected = function (selectedItems, lastSelectedItems, isSelected) {
-                var recipientTokens = Util.goodValue($scope.recipientsStr).split(";");
-                _.each(lastSelectedItems, function (selectedItem) {
-                    var found = _.find($scope.recipients, function (recipient) {
-                        return Util.compare(selectedItem.name, recipient.name) || Util.compare(selectedItem.email_lcs, recipient.email)
-                    });
-                    if (isSelected && !found) {
-                        $scope.recipients.push({
-                            name: Util.goodValue(selectedItem.name)
-                            , email: Util.goodValue(selectedItem.email_lcs)
-                        });
-
-                    } else if (!isSelected && found) {
-                        _.remove($scope.recipients, found);
-                    }
-                });
-
-                $scope.recipientsStr = _.pluck($scope.recipients, "name").join(";");
-            };
-            $scope.onChangeRecipients = function () {
-                var recipientsNew = [];
-                var recipientTokens = Util.goodValue($scope.recipientsStr).split(";");
-                _.each(recipientTokens, function (token) {
-                    token = token.trim();
-                    if (!Util.isEmpty(token)) {
-                        var found = _.find($scope.recipients, function (recipient) {
-                            return (token == recipient.name || token == recipient.email);
-                        });
-                        if (found) {
-                            recipientsNew.push(found);
-                        } else {
-                            var recipientUserTyped = {name: token, email: token};
-                            recipientsNew.push(recipientUserTyped);
-                        }
-                    }
-                });
-                $scope.recipients = recipientsNew;
-            };
-            $scope.onClickCancel = function () {
-                $modalInstance.close(false);
-            };
-            $scope.onClickOk = function () {
-                //var a = $scope.searchControl.getSelectedItems();
-                $modalInstance.close($scope.recipients);
-            };
-            $scope.disableOk = function () {
-                return Util.isEmpty($scope.recipientsStr);
-            };
-
-        }
-    ]
-);
 
