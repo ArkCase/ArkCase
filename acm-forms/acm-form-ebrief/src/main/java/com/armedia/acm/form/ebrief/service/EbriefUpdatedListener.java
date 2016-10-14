e2608a6960ccc4aa6873e65ac05df58d476f6eef
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-10-14 10:03:52 +0200

    Changed com.armedia.acm.casefile.event -> com.armedia.acm.casefile for unification with other object type events and for corresponding to event names in auditPlugin properties. Raised event for creating casefile in close complaint.
    Former-commit-id: b66036b3a2c6113f3eef6c3313c542473cd28475

--- acm-forms/acm-form-ebrief/src/main/java/com/armedia/acm/form/ebrief/service/EbriefUpdatedListener.java
+++ acm-forms/acm-form-ebrief/src/main/java/com/armedia/acm/form/ebrief/service/EbriefUpdatedListener.java
@@ -3,66 +3,69 @@
  */
 package com.armedia.acm.form.ebrief.service;
 
-import java.util.Properties;
-
+import com.armedia.acm.form.ebrief.model.EbriefForm;
+import com.armedia.acm.frevvo.config.FrevvoFormName;
 import com.armedia.acm.frevvo.config.FrevvoFormService;
+import com.armedia.acm.plugins.casefile.model.CaseEvent;
+import com.armedia.acm.plugins.casefile.model.CaseFileConstants;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 
-import com.armedia.acm.form.ebrief.model.EbriefForm;
-import com.armedia.acm.frevvo.config.FrevvoFormName;
-import com.armedia.acm.plugins.casefile.model.CaseEvent;
-import com.armedia.acm.plugins.casefile.model.CaseFileConstants;
+import java.util.Properties;
 
 /**
  * @author riste.tutureski
  *
  */
-public class EbriefUpdatedListener implements ApplicationListener<CaseEvent> {
+public class EbriefUpdatedListener implements ApplicationListener<CaseEvent>
+{
 
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
-	
-	private Properties properties;
-	private FrevvoFormService ebriefService;
-	
-	@Override
-	public void onApplicationEvent(CaseEvent event) {
-		if ("com.armedia.acm.casefile.event.updated".equals(event.getEventType().toLowerCase()))
-		{
-			LOG.debug("Updating Frevvo XML file ...");
-			
-			if (getProperties() != null)
-			{
-				if (getProperties().containsKey(CaseFileConstants.ACTIVE_CASE_FORM_KEY))
-				{
-					String activeFormName = (String) getProperties().get(CaseFileConstants.ACTIVE_CASE_FORM_KEY);
-					
-					if (FrevvoFormName.EBRIEF.equals(activeFormName))
-					{
-						getEbriefService().updateXML(event.getCaseFile(), event.getEventUser(), EbriefForm.class);
-					}
-				}
-			}
-		}
-	}
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-	
-	public Properties getProperties() {
-		return properties;
-	}
+    private Properties properties;
+    private FrevvoFormService ebriefService;
 
-	public void setProperties(Properties properties) {
-		this.properties = properties;
-	}
+    @Override
+    public void onApplicationEvent(CaseEvent event)
+    {
+        if ("com.armedia.acm.casefile.updated".equals(event.getEventType().toLowerCase()))
+        {
+            LOG.debug("Updating Frevvo XML file ...");
 
+            if (getProperties() != null)
+            {
+                if (getProperties().containsKey(CaseFileConstants.ACTIVE_CASE_FORM_KEY))
+                {
+                    String activeFormName = (String) getProperties().get(CaseFileConstants.ACTIVE_CASE_FORM_KEY);
 
-	public FrevvoFormService getEbriefService() {
-		return ebriefService;
-	}
+                    if (FrevvoFormName.EBRIEF.equals(activeFormName))
+                    {
+                        getEbriefService().updateXML(event.getCaseFile(), event.getEventUser(), EbriefForm.class);
+                    }
+                }
+            }
+        }
+    }
 
+    public Properties getProperties()
+    {
+        return properties;
+    }
 
-	public void setEbriefService(EbriefService ebriefService) {
-		this.ebriefService = ebriefService;
-	}
+    public void setProperties(Properties properties)
+    {
+        this.properties = properties;
+    }
+
+    public FrevvoFormService getEbriefService()
+    {
+        return ebriefService;
+    }
+
+    public void setEbriefService(EbriefService ebriefService)
+    {
+        this.ebriefService = ebriefService;
+    }
 }
