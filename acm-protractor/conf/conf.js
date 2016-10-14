91235f3dec531188e44266cfec55cceecad6ad3c
Sanja Paskova <sanja.paskova@armedia.com>
2016-10-14 03:18:14 -0400

    Added multiple tests to be run in spec file in order to run this tests on remote machine after selenium tests
    Former-commit-id: 7b1b76d0e2ccc7dec2f45fcd75ce69b964d6a7f1

--- acm-protractor/conf/conf.js
+++ acm-protractor/conf/conf.js
@@ -39,10 +39,17 @@ exports.config = {
     //          'maxInstances': 5
     //        }],
 
-    specs: ['../test_spec/task_test.spec.js'],
+    specs: [            
+           '../test_spec/dashboard_test.spec.js',
+           '../test_spec/user_test.spec.js'
+ //         '../test_spec/task_test.spec.js',
+//           '../test_spec/tasks_test.spec.js'            
+    ],
+    
 
     jasmineNodeOpts: {
         showColors: true,
+        defaultTimeoutInterval: 50000
     },
     beforeLaunch: function() {
         return new Promise(function(resolve){
@@ -51,9 +58,9 @@ exports.config = {
       },
     onPrepare: function () {
     	jasmine.getEnv().addReporter(reporter);
-        browser.driver.manage().window().maximize();       
-        browser.driver.get('https://core.arkcase.dev.armedia.com/arkcase/login');  
-        
+        browser.driver.manage().window().maximize();    
+        browser.driver.get('https://core.arkcase.dev.armedia.com/arkcase/login'); 
+        browser.manage().timeouts().setScriptTimeout(60000);
     },
     afterLaunch: function(exitCode) {
         return new Promise(function(resolve){
