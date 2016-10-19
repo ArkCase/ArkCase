e5775a2be5563213385fb8e1a0b7bea45135e8a7
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-10-19 13:32:20 +0200

    Revert changes for new events on database level
    Former-commit-id: 44affce7c1592c7087a045f8c9fb438c4ba90b16

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/Organization.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/Organization.java
@@ -30,6 +30,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -249,5 +250,32 @@ public class Organization implements Serializable, AcmEntity
     public void setClassName(String className)
     {
         this.className = className;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(organizationId, that.organizationId) &&
+                Objects.equals(organizationType, that.organizationType) &&
+                Objects.equals(organizationTypes, that.organizationTypes) &&
+                Objects.equals(organizationValue, that.organizationValue) &&
+                Objects.equals(created, that.created) &&
+                Objects.equals(creator, that.creator) &&
+                Objects.equals(modified, that.modified) &&
+                Objects.equals(modifier, that.modifier) &&
+                Objects.equals(identifications, that.identifications) &&
+                Objects.equals(addresses, that.addresses) &&
+                Objects.equals(contactMethods, that.contactMethods) &&
+                Objects.equals(className, that.className);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(organizationId, organizationType, organizationTypes, organizationValue, created, creator,
+                modified, modifier, identifications, addresses, contactMethods, className);
     }
 }
