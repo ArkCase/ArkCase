04ee754bcd3126f68877fc69c859be2f169481c7
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2016-10-19 09:13:12 +0200

    New project with the original mule-module-cmis files
    Former-commit-id: 45dcf35eff8e0cc40e5c1b7195a1bc8ffb078253

---
+++ acm-tool-integrations/acm-mule-module-cmis/src/main/java/org/mule/module/cmis/process/ProcessCallbackProcessInterceptor.java
@@ -0,0 +1,27 @@
+
+package org.mule.module.cmis.process;
+
+import javax.annotation.Generated;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.routing.filter.Filter;
+
+@Generated(value = "Mule DevKit Version 3.4.0", date = "2014-05-13T04:20:32-03:00", comments = "Build 3.4.0.1555.8df15c1")
+public class ProcessCallbackProcessInterceptor<T, O >implements ProcessInterceptor<T, O>
+{
+
+
+    public T execute(ProcessCallback<T, O> processCallback, O object, MessageProcessor messageProcessor, MuleEvent event)
+        throws Exception
+    {
+        return processCallback.process(object);
+    }
+
+    public T execute(ProcessCallback<T, O> processCallback, O object, Filter filter, MuleMessage message)
+        throws Exception
+    {
+        return processCallback.process(object);
+    }
+
+}
---
+++ acm-tool-integrations/acm-mule-module-cmis/src/main/java/org/mule/tooling/ui/contribution/CmisActivator.java
@@ -0,0 +1,39 @@
+
+package org.mule.tooling.ui.contribution;
+
+import javax.annotation.Generated;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+
+/**
+ * The activator class controls the plug-in life cycle
+ * 
+ */
+@Generated(value = "Mule DevKit Version 3.4.0", date = "2014-05-13T04:20:32-03:00", comments = "Build 3.4.0.1555.8df15c1")
+public class CmisActivator
+    extends AbstractUIPlugin
+{
+
+    public final static String PLUGIN_ID = "org.mule.tooling.ui.contribution.cmis";
+    private static org.mule.tooling.ui.contribution.CmisActivator plugin;
+
+    public void start(BundleContext context)
+        throws Exception
+    {
+        super.start(context);
+        plugin = this;
+    }
+
+    public void stop(BundleContext context)
+        throws Exception
+    {
+        plugin = null;
+        super.stop(context);
+    }
+
+    public static org.mule.tooling.ui.contribution.CmisActivator getDefault() {
+        return plugin;
+    }
+
+}
