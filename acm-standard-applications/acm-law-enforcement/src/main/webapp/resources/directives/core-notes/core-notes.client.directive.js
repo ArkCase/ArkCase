5f49bc3d9f20e1d36ade26f5cb16d74fe02e8281
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-10-19 15:33:05 +0200

    Changes after code review and consultation with Dragan Simonovski.
    Former-commit-id: 5750bd525f0e8dfa7d161a70e60a890784224099

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-notes/core-notes.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-notes/core-notes.client.directive.js
@@ -72,33 +72,34 @@ angular.module('directives').directive('coreNotes', ['$q', '$modal', '$translate
                 scope.$watchCollection('config', function (config, oldValue) {
                     if (!scope.notesInit.noteTitle) {
                         scope.notesInit.noteTitle = $translate.instant("common.directive.coreNotes.title");
-                        if (config) {
-                            // if buttons were defined in config, try to use the getConfigurableButton method
-                            if (Util.goodArray(config.buttons)) {
-                                _.each(config.buttons, function (button) {
-                                    gridHelper.addConfigurableButton(config, button);
-                                });
-                                // check if the config had an entry for 'edit'. If not, add in default edit button
-                                if (!hasButton(config, "edit")) {
-                                    gridHelper.addButton(config, "edit");
-                                }
-                                // check if the config had an entry for 'delete'. If not, add in default delete button
-                                if (!hasButton(config, "delete")) {
-                                    gridHelper.addButton(config, "delete");
-                                }
-                            } else {
-                                // no buttons were found in the config, use the default settings
+                    }
+                    if (config) {
+                        // if buttons were defined in config, try to use the getConfigurableButton method
+                        if (Util.goodArray(config.buttons)) {
+                            _.each(config.buttons, function (button) {
+                                gridHelper.addConfigurableButton(config, button);
+                            });
+                            // check if the config had an entry for 'edit'. If not, add in default edit button
+                            if (!hasButton(config, "edit")) {
                                 gridHelper.addButton(config, "edit");
+                            }
+                            // check if the config had an entry for 'delete'. If not, add in default delete button
+                            if (!hasButton(config, "delete")) {
                                 gridHelper.addButton(config, "delete");
                             }
-                            gridHelper.setColumnDefs(config);
-                            gridHelper.setBasicOptions(config);
-                            gridHelper.disableGridScrolling(config);
-                            gridHelper.setExternalPaging(config, scope.retrieveGridData);
-                            gridHelper.setUserNameFilter(promiseUsers);
-                            scope.retrieveGridData();
+                        } else {
+                            // no buttons were found in the config, use the default settings
+                            gridHelper.addButton(config, "edit");
+                            gridHelper.addButton(config, "delete");
                         }
+                        gridHelper.setColumnDefs(config);
+                        gridHelper.setBasicOptions(config);
+                        gridHelper.disableGridScrolling(config);
+                        gridHelper.setExternalPaging(config, scope.retrieveGridData);
+                        gridHelper.setUserNameFilter(promiseUsers);
+                        scope.retrieveGridData();
                     }
+                    
                 });
 
                 // check if config file has an entry for any named button
@@ -119,7 +120,7 @@ angular.module('directives').directive('coreNotes', ['$q', '$modal', '$translate
                     var promiseQueryNotes = ObjectNoteService.queryNotesPage(
                         info.objectType
                         , info.currentObjectId
-                        , info.noteTitle
+                        , info.noteType
                         , Util.goodValue(scope.start, 0)
                         , Util.goodValue(scope.pageSize, 10)
                         , Util.goodMapValue(scope.sort, "by")
