f01524cd74f87bc495b95f0ccd55cd276af94715
Nadica Cuculova <nadica.cuculova@armedia.com>
2016-10-25 13:23:03 +0200

    Cleanup
    Former-commit-id: 358ccb1424edf899b57397b9b803f6762185679b

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/PersonEventPublisher.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/PersonEventPublisher.java
@@ -6,12 +6,15 @@ import com.armedia.acm.plugins.person.model.PersonAddEvent;
 import com.armedia.acm.plugins.person.model.PersonModifiedEvent;
 import com.armedia.acm.plugins.person.model.PersonPersistenceEvent;
 import com.armedia.acm.plugins.person.model.PersonUpdatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 
 
 public class PersonEventPublisher implements ApplicationEventPublisherAware
 {
+    private transient final Logger log = LoggerFactory.getLogger(getClass());
     private ApplicationEventPublisher eventPublisher;
 
     @Override
@@ -22,6 +25,7 @@ public class PersonEventPublisher implements ApplicationEventPublisherAware
 
     public void publishPersonEvent(Person source, String ipAddress, boolean newPerson, boolean succeeded)
     {
+        log.debug("Publishing a person event.");
         PersonPersistenceEvent personPersistenceEvent =
                 newPerson ? new PersonAddEvent(source) : new PersonUpdatedEvent(source, ipAddress);
         personPersistenceEvent.setSucceeded(succeeded);
@@ -30,6 +34,7 @@ public class PersonEventPublisher implements ApplicationEventPublisherAware
 
     public void publishPersonEvent(Person source, boolean newPerson, boolean succeeded)
     {
+        log.debug("Publishing a person event.");
         String ipAddress = AuthenticationUtils.getUserIpAddress();
         PersonModifiedEvent personPersistenceEvent = new PersonModifiedEvent(source, ipAddress);
         if (newPerson)
