1ae4a16ec65c0d3dc26790405fcc56bc28f09cba
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-10-28 13:22:15 +0200

    If saved request contains '/arkcase/stomp' in url, use home page. Added web socket channel interceptor on backend side for logging purposes.
    Former-commit-id: 91001d857313a91edc304777d0569e15130b3710

--- acm-services/acm-service-login/src/main/java/com/armedia/acm/auth/AcmLoginSuccessHandler.java
+++ acm-services/acm-service-login/src/main/java/com/armedia/acm/auth/AcmLoginSuccessHandler.java
@@ -1,16 +1,17 @@
 package com.armedia.acm.auth;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class AcmLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler
@@ -22,25 +23,26 @@ public class AcmLoginSuccessHandler extends SavedRequestAwareAuthenticationSucce
     private SessionAuthenticationStrategy sessionAuthenticationStrategy;
 
     @Override
-    public void onAuthenticationSuccess(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication)
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws ServletException, IOException
     {
-        if ( log.isDebugEnabled() )
-        {
-            log.debug("Authentication details is of type: " + (authentication.getDetails() == null ? null :
-                    authentication.getDetails().getClass().getName()) );
-        }
+        log.debug("Authentication details is of type: {}",
+                authentication.getDetails() == null ? null : authentication.getDetails().getClass().getName());
 
         getLoginSuccessOperations().onSuccessfulAuthentication(request, authentication);
-
+        SavedRequest savedRequest = (SavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+        if (savedRequest != null)
+        {
+            String requestUrl = savedRequest.getRedirectUrl();
+            if (requestUrl != null && requestUrl.contains("/arkcase/stomp"))
+            {
+                request.getSession().removeAttribute("SPRING_SECURITY_SAVED_REQUEST");
+            }
+        }
         super.onAuthenticationSuccess(request, response, authentication);
         sessionRegistry.registerNewSession(request.getSession().getId(), authentication.getPrincipal());
         sessionAuthenticationStrategy.onAuthentication(authentication, request, response);
     }
-
 
     public AcmLoginSuccessOperations getLoginSuccessOperations()
     {
