c5463bb71c352cbdeeb8d9d17a9cae2efb356543
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-10-28 11:39:08 +0200

    AFDP-2486: Fix the rename document
    Former-commit-id: 2c26269e67b3e5bd5f0bb90fe2b80651069cbe1c

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
@@ -675,7 +675,7 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                 if (Validator.validateFancyTreeNode(node)) {
                     $(node.span).removeClass("pending");
                     if(!node.folder) {
-                      node.title = node.title.replace($translate.instant("common.directive.docTree.waitUploading"), node.data.name);
+                      node.title = node.data.name;
                       node.renderTitle();
                     }
                     node.setStatus("ok");
