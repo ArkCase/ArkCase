5c76e635882cf7034cbbaa8ec8d68260e0aa2fa7
James Wu <jwu@armedia.com>
2016-10-28 10:42:26 -0400

    prevent duplicate popup messages
    Former-commit-id: 097aee28ac80d578800623c1cdbde776fb90514e

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/common/message.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/common/message.client.service.js
@@ -10,53 +10,64 @@
  * The MessageService displays notify messages
  *
  */
-
-// Authentication service for user variables
-angular.module('services').factory('MessageService', ['$injector', '$translate',
-    function ($injector, $translate) {
+angular.module('services').factory('MessageService', ['$injector', '$translate', 'Acm.StoreService', 'UtilService', 'Util.TimerService',
+    function ($injector, $translate, Store, Util, TimerService) {
+        var MSG_SHELF_LIFE = 5000; //5 seconds
         var notify = null;
         var ConfigService = null;
-        var config = null;
-        var notifyOptions = {};
-
+        var defaultOptions = {};
+        var msgCache = new Store.CacheFifo("AcmNotifyMessages");
 
         /**
          * Displays notify message
          * @param msg
-         * @param notifyOptions
+         * @param customOptions
          */
         function showMessage(msg, customOptions) {
             if (notify) {
-                var notifyData = {} || customOptions;
-                notifyData = _.extend(notifyOptions, customOptions);
-                notifyData.message = msg;
-                notify(notifyData);
+                var notifyData = _.extend({}, defaultOptions, customOptions);
+
+                if (Util.goodValue(notifyData.allowDuplicate, false)) {
+                    notifyData.message = msg;
+                    notify(notifyData);
+
+                } else {
+                    var nowDate = new Date();
+                    var nowTimestamp = nowDate.getTime();
+                    var lastTimestamp = Util.goodNumber(msgCache.get(msg), 0);
+                    if (MSG_SHELF_LIFE < nowTimestamp - lastTimestamp) {
+                        msgCache.put(msg, nowTimestamp);
+                        notifyData.message = msg;
+                        notify(notifyData);
+                    }
+                }
             }
         }
 
         /**
-         * Inject config and notify dependencies and show Message
-         * @param msg
-         * @param notifyOptions
+         * Initialise services and read configuration. We use $injector to avoid 'circular dependency issue'
          */
-        function showNotifyMessage(msg, customOptions) {
-            // We use injector to avoid 'circular dependency issue'
+        function init() {
             if (!ConfigService) {
                 ConfigService = $injector.get('ConfigService');
-                config = ConfigService.getModule({moduleId: 'common'}, function (config) {
-                    if (config.notifyMessage) {
-                        notifyOptions = config.notifyMessage;
-
-                        if (!notify) {
-                            notify = $injector.get('notify');
-                            showMessage(msg, customOptions);
-                        }
-                    }
+                ConfigService.getModule({moduleId: 'common'}, function (config) {
+                    defaultOptions = Util.goodMapValue(config, "notifyMessage", {});
                 });
-            } else {
-                showMessage(msg, customOptions);
+            }
+
+            if (!notify) {
+                notify = $injector.get('notify');
             }
         }
+
+        TimerService.useTimer("initMessageService"
+            , 50     //delay 50 milliseconds
+            , function () {
+                init();
+                return false;
+            }
+        );
+
 
         return {
 
@@ -71,7 +82,7 @@ angular.module('services').factory('MessageService', ['$injector', '$translate',
              * This method displays error message in notify popup window.
              */
             error: function (message) {
-                showNotifyMessage(message);
+                showMessage(message);
             },
 
             /**
@@ -79,13 +90,13 @@ angular.module('services').factory('MessageService', ['$injector', '$translate',
              * @name httpError
              * @methodOf services.service:MessageService
              *
-             * @param {HttpResponse} httpResponse Http resposne
+             * @param {HttpResponse} httpResponse Http response
              *
              * @description
              * This method takes information from httpResponse and displays notify error message
              */
             httpError: function (response) {
-                var notifyOptions = {};
+                var defaultOptions = {};
                 if (response && response.config) {
                     var msg = '';
                     if (response.status == 503) {
@@ -102,7 +113,7 @@ angular.module('services').factory('MessageService', ['$injector', '$translate',
                         ].join('');
                     }
 
-                    showNotifyMessage(msg);
+                    showMessage(msg);
                 }
             },
 
@@ -118,7 +129,7 @@ angular.module('services').factory('MessageService', ['$injector', '$translate',
              */
             info: function (message) {
                 // TODO: create templates for info and error notify windows
-                showNotifyMessage(message, {position: 'left'});
+                showMessage(message, {position: 'left'});
             }
         };
     }
