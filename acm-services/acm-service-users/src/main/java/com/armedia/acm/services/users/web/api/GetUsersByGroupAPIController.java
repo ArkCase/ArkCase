c21c92d5ca2d48c5911eb972d4a329078168ebe4
Petar Ilin <petar.ilin@armedia.com>
2016-11-16 20:13:09 +0100

    SSP-61 URL encode ampersand character in group names
    Former-commit-id: 17ec044a205361c48f7f99a7d006428079c91d4d

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/web/api/GetUsersByGroupAPIController.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/web/api/GetUsersByGroupAPIController.java
@@ -35,6 +35,7 @@ public class GetUsersByGroupAPIController
         {
             group = "\"" + group + "\"";
         }
+        group = group.replace("&", "%26"); // instead of URL encoding
         StringBuilder query = new StringBuilder();
         query.append("object_type_s").append(":").append("USER");
         query.append(" AND ").append("groups_id_ss").append(":").append(group);
