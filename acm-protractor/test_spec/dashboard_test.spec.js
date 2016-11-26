ea20a0ff1056a1ed5b2c340c0b12bbdbaeccf487
Milan Jovanovski <milan.jovanovski@armedia.com>
2016-11-26 14:00:28 +0100

    Updated and moved the functions for notes,references,people,tags and history to base page so they can be reused. New time tracking page and script.
    Former-commit-id: 395ccecf7f05cf3ea788e830a0b03f21aabff207

--- acm-protractor/test_spec/dashboard_test.spec.js
+++ acm-protractor/test_spec/dashboard_test.spec.js
@@ -30,60 +30,58 @@ describe('dashboard page test', function() {
 
     it('should add/delete widget cases by status', function() {
 
-    	dashPage.clickEditButton().clickAddWidgetButton().addWidget("CasesByStatus").clickSaveChangesButton();       
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleCasesByStatus);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();            
-       
-    });
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("CasesByStatus").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleCasesByStatus);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
 
+    });
 
     it('should add/delete widget my cases', function() {
 
         dashPage.clickEditButton().clickAddWidgetButton().addWidget("MyCases").clickSaveChangesButton();
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleMyCases);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();   
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleMyCases);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
 
     });
 
-
     it('should add/delete widget my complaints', function() {
 
-    	dashPage.clickEditButton().clickAddWidgetButton().addWidget("MyComplaints").clickSaveChangesButton();
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleMyComplaints);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();  
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("MyComplaints").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleMyComplaints);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
     });
 
 
     it('should add/delete widget new complaints', function() {
 
-    	dashPage.clickEditButton().clickAddWidgetButton().addWidget("NewComplaints").clickSaveChangesButton();
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleNewComplaints);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();  
-        
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("NewComplaints").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleNewComplaints);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
+
     });
 
 
     it('should add/delete widget team workload', function() {
 
-    	dashPage.clickEditButton().clickAddWidgetButton().addWidget("TeamWorkload").clickSaveChangesButton();
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleTeamWorkload);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();  
-        
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("TeamWorkload").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleTeamWorkload);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
+
     });
 
     it('should add/delete widget weather', function() {
 
-    	dashPage.clickEditButton().clickAddWidgetButton().addWidget("Weather").clickSaveChangesButton();
-        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleWeather);        
-        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton(); 
-        
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("Weather").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleWeather);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
+
     });
 
     it('should add/delete widget news', function() {
 
- dashPage.clickEditButton().clickAddWidgetButton().addWidget("News").clickSaveChangesButton();
- expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleNews);
- dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
- });
- });
+        dashPage.clickEditButton().clickAddWidgetButton().addWidget("News").clickSaveChangesButton();
+        expect(dashPage.returnWidgetTitle()).toEqual(Objects.dashboardpage.data.widgetTitleNews);
+        dashPage.clickEditButton().removeWidgetButton().clickSaveChangesButton();
+    });
 
+});
