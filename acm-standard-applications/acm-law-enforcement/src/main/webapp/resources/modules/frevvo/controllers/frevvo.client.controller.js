13b579e901b36a849922a3951f0350224a93e004
Riste Tutureski <riste.tutureski@armedia.com>
2016-12-01 10:25:20 +0100

    AFDP-2100: Fixing Frevvo iframe delay
    Former-commit-id: 2137638d12875eb6f87d005b94f165f7fa5fa5cb

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/frevvo/controllers/frevvo.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/frevvo/controllers/frevvo.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('frevvo').controller('FrevvoController', ['$rootScope', '$scope', '$stateParams', '$sce', '$q', '$modal', '$translate'
+angular.module('frevvo').controller('FrevvoController', ['$rootScope', '$scope', '$stateParams', '$sce', '$q', '$modal', '$translate', '$interval'
     , 'UtilService', 'ConfigService', 'TicketService', 'LookupService', 'Frevvo.FormService', 'ServCommService'
-    , function ($rootScope, $scope, $stateParams, $sce, $q, $modal, $translate
+    , function ($rootScope, $scope, $stateParams, $sce, $q, $modal, $translate, $interval
         , Util, ConfigService, TicketService, LookupService, FrevvoFormService, ServCommService) {
 
         var promiseConfig = ConfigService.getModuleConfig("frevvo");
@@ -24,26 +24,62 @@ angular.module('frevvo').controller('FrevvoController', ['$rootScope', '$scope',
         });
 
         $scope.iframeLoaded = function(){
-            if ($rootScope.frevvoMessaging == null) {
-                $rootScope.frevvoMessaging = {};
-                $rootScope.frevvoMessaging.receiver = document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')[0].contentWindow;
-                $rootScope.frevvoMessaging.send = function send(message) {
-                    $rootScope.frevvoMessaging.receiver.postMessage(message, '*');
-                }
-                $rootScope.frevvoMessaging.receive = function receive(e) {
-                    if (!Util.isEmpty(e.data.source) &&  e.data.source == "frevvo" && !Util.isEmpty(e.data.action)) {
-                        // Do actions sent from Frevvo
-                        if (e.data.action == "open-user-picker") {
-                            pickUser(e.data);
+            startInitFrevvoMessaging();
+        };
+
+        var initFrevvoMessagingPromise;
+        function startInitFrevvoMessaging() {
+            stopInitFrevvoMessaging();
+            initFrevvoMessagingPromise = $interval(initFrevvoMessaging, 250);
+        }
+
+        function stopInitFrevvoMessaging() {
+            $interval.cancel(initFrevvoMessagingPromise);
+        }
+
+        function initFrevvoMessaging() {
+            var frevvoIframe = getFrevvoIframe();
+            if (!Util.isEmpty(frevvoIframe)) {
+                stopInitFrevvoMessaging();
+                if (Util.isEmpty($rootScope.frevvoMessaging)) {
+                    $rootScope.frevvoMessaging = {};
+                    $rootScope.frevvoMessaging.receiver = frevvoIframe;
+                    $rootScope.frevvoMessaging.send = function send(message) {
+                        if (!Util.isEmpty($rootScope.frevvoMessaging.receiver)) {
+                            $rootScope.frevvoMessaging.receiver.postMessage(message, '*');
                         }
                     }
-                }
+                    $rootScope.frevvoMessaging.receive = function receive(e) {
+                        if (!Util.isEmpty(e) && !Util.isEmpty(e.data) && !Util.isEmpty(e.data.source) && e.data.source == "frevvo" && !Util.isEmpty(e.data.action)) {
+                            // Do actions sent from Frevvo
+                            if (e.data.action == "open-user-picker") {
+                                pickUser(e.data);
+                            }
+                        }
+                    }
 
-                window.addEventListener("message", $rootScope.frevvoMessaging.receive);
-            } else {
-                $rootScope.frevvoMessaging.receiver = document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')[0].contentWindow;
+                    window.addEventListener("message", $rootScope.frevvoMessaging.receive);
+                } else {
+                    $rootScope.frevvoMessaging.receiver = frevvoIframe;
+                }
             }
-        };
+        }
+
+        function getFrevvoIframe() {
+            if (!Util.isEmpty(document) &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe')) &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe').contentWindow) &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe').contentWindow.document) &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')) &&
+                document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe').length > 0 &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')[0]) &&
+                !Util.isEmpty(document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')[0].contentWindow)
+            ) {
+                return document.getElementById('frevvoFormIframe').contentWindow.document.getElementsByTagName('iframe')[0].contentWindow;
+            }
+
+            return null;
+        }
 
         function pickUser(data) {
             var params = {};
