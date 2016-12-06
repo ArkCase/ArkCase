9f8f0e6725a380c39d7e7ab7185b2c1782e88c2c
Teng Wang <teng.wang@armedia.com>
2016-12-05 19:48:20 -0500

    Check assignee for uniqueness
    Former-commit-id: c9e6abdd85b434dcb969dd70d5d32302e162cb3d

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/object/object-participant.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/object/object-participant.client.service.js
@@ -231,6 +231,40 @@ angular.module('services').factory('Object.ParticipantService', ['$resource', '$
 
         /**
          * @ngdoc method
+         * @name validateAssignee
+         * @methodOf services:Object.ParticipantService
+         *
+         * @description
+         * Check if there is one and only one assignee in the participant array
+         *
+         * @param {Object} data Array of participants to be validated
+         *
+         * @return {Object} Promise
+         */
+        Service.validateAssignee = function (data) {
+            if (_.filter(data, function (pa) {
+                    return Util.compare("assignee", pa.participantType);
+                }).length != 1) {
+                return false;
+            }
+            return true;
+        };
+
+        /**
+         * @ngdoc method
+         * @name validateType
+         * @methodOf services:Object.ParticipantService
+         * @description Check if the type of participant is consistent with the given USER or GROUP type
+         * @param {Object} data Participant object to be validated
+         * @param {Object} type Given type
+         * @returns {boolean} Promise
+         */
+        Service.validateType = function (data, type) {
+            return data.participantType == "owning group" ? (type == "GROUP") : (type == "USER");
+        };
+
+        /**
+         * @ngdoc method
          * @name validateParticipants
          * @methodOf services:Object.ParticipantService
          *
