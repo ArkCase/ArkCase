97b88c9c925f96e7dfa86e66c0bf0a657b1c16e3
Sanja Paskova <sanja.paskova@armedia.com>
2016-12-07 08:17:17 -0500

    added test for issue AFDP-1210
    Former-commit-id: 50d314cf33cc7b96044024fe70eba82b8564ce2c

--- acm-protractor/conf/conf.js
+++ acm-protractor/conf/conf.js
@@ -39,11 +39,12 @@ exports.config = {
     // }],
 
     specs: [
-          '../test_spec/dashboard_test.spec.js',
-          '../test_spec/task_test.spec.js',
-          '../test_spec/case_test.spec.js',
-          '../test_spec/complaint_test.spec.js',
-          '../test_spec/user_test.spec.js'
+           '../test_spec/dashboard_test.spec.js',
+           '../test_spec/task_test.spec.js',
+           '../test_spec/case_test.spec.js',
+           '../test_spec/complaint_test.spec.js',
+           '../test_spec/user_test.spec.js',
+           '../test_spec/notification_test.js'
     ],
 
     jasmineNodeOpts: {
