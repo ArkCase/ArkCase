12264628da0fa06958fe56becd3e0b0f64b9caab
jovan.ivanovski <jovan.ivanovski@armedia.com>
2016-12-07 13:21:16 +0100

    Message is thrown when new case and new complaint are created. In both scenarios, when we are the owner of the new record and we create it from frevvo, and when someone else creates a new one again we are notified.
    Former-commit-id: d18149a92755652bcbd0169492dc0176666673be

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/complaints-list.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/complaints-list.client.controller.js
@@ -12,15 +12,18 @@ angular.module('complaints').controller('ComplaintsListController', ['$scope', '
         $scope.$bus.subscribe(eventName, function (data) {
             if (data.objectType === ObjectService.ObjectTypes.COMPLAINT) {
                 var frevvoRequest = ServCommService.popRequest("frevvo", "new-complaint");
+                var objectTypeString = $translate.instant('common.objectTypes.' + data.objectType);
                 if (frevvoRequest) {
-                    MessageService.info(data.objectType + " with ID " + data.objectId + " was created. Please refresh complaints list to load it.");
-                    //   ObjectService.gotoState(ObjectService.ObjectTypes.COMPLAINT, data.objectId);
+                    ObjectService.gotoUrl(ObjectService.ObjectTypes.COMPLAINT, data.objectId);
+                    MessageService.info(objectTypeString + " with ID " + data.objectId + " was created.");
                 }
                 else {
-                    MessageService.info(data.objectType + " with ID " + data.objectId + " was created");
+                    if (!objectTypeString) {
+                        objectTypeString = data.objectType;
+                    }
+                    MessageService.info(objectTypeString + " with ID " + data.objectId + " was created.");
                 }
             }
-
         });
 
         //"treeConfig", "treeData", "onLoad", and "onSelect" will be set by Tree Helper
