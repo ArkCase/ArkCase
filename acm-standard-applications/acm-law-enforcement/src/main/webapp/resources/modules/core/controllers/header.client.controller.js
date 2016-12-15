609b9237a665dd1e43b4c3679a0acd8d1a24f20e
Marjan Stefanoski <marjan.stefanoski@armedia.com>
2016-12-15 21:18:46 +0100

    AFDP-2813
    Implemented autosuggest in the quickSearch text box in the right corner.
    CSS need to be corrected, so the  autosuggest list will appear correctly.
     The argument pased from header controller to search directive is not working as expected and because of that phrase search can not be issued.
    
    
    Former-commit-id: 106281933cfbe99ba37e8886326ae1fc21ed8371

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/core/controllers/header.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/core/controllers/header.client.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$state', 'ServCommService'
-    , function ($scope, Authentication, Menus, $state, ServCommService) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$q', '$state', 'ServCommService', 'Search.AutoSuggestService'
+    , function ($scope, Authentication, Menus, $q, $state, ServCommService, AutoSuggestService) {
         $scope.$emit('req-component-config', 'header');
         $scope.authentication = Authentication;
         $scope.isCollapsed = false;
         $scope.menu = Menus.getMenu('topbar');
+
+        var config = null;
 
         $scope.config = null;
         $scope.start = '';
@@ -14,15 +16,37 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
         $scope.data = {};
         $scope.data.inputQuery = '';
         $scope.$on('component-config', applyConfig);
+        // $scope.$emit('req-component-config', 'header');
         function applyConfig(e, componentId, config) {
             if (componentId == 'header') {
                 $scope.config = config;
                 $scope.start = config.searchParams.start;
                 $scope.count = config.searchParams.n;
+                $scope.typeAheadColumn = config.typeAheadColumn;
             }
         };
 
         ServCommService.handleRequest();
+
+        $scope.queryTypeahead = function (typeaheadQuery) {
+            var deferred = $q.defer();
+            var typeAheadColumn = "title_parseable";
+            if ($scope.typeAheadColumn) {
+                typeAheadColumn = $scope.typeAheadColumn;
+            }
+            if (typeaheadQuery.length >= 2) {
+                AutoSuggestService.autoSuggest(typeaheadQuery, "QUICK", null).then(function (res) {
+                    var results = _.pluck(res, typeAheadColumn);
+                    deferred.resolve(results);
+                });
+                return deferred.promise;
+            }
+        }
+
+        var isSelected = false;
+        $scope.onSelect = function ($item, $model, $label) {
+            isSelected = true;
+        };
 
 
         $scope.toggleCollapsibleMenu = function () {
@@ -42,8 +66,10 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 
         $scope.keyDown = function (event) {
             if (event.keyCode == 13) {
+                $scope.isSelected = isSelected;
                 $state.go('quick-search', {
-                    query: $scope.data.inputQuery
+                    query: $scope.data.inputQuery,
+                    isSelected: $scope.isSelected
                 });
             }
         };
