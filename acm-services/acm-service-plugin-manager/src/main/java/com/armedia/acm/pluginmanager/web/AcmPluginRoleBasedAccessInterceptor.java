52e69d07335e2cd6be69168ecdc091fcc4341805
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-12-20 11:32:29 +0100

    Intercept views for modules and check corresponding privileges.
    Former-commit-id: c178a0d1f4ea3958caf8e200be421ec77d0951c5

--- acm-services/acm-service-plugin-manager/src/main/java/com/armedia/acm/pluginmanager/web/AcmPluginRoleBasedAccessInterceptor.java
+++ acm-services/acm-service-plugin-manager/src/main/java/com/armedia/acm/pluginmanager/web/AcmPluginRoleBasedAccessInterceptor.java
@@ -1,8 +1,8 @@
 package com.armedia.acm.pluginmanager.web;
 
-import com.armedia.acm.core.exceptions.AcmNotAuthorizedException;
 import com.armedia.acm.pluginmanager.model.AcmPluginUrlPrivilege;
 import com.armedia.acm.pluginmanager.service.AcmPluginManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -10,12 +10,13 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import java.util.List;
 import java.util.Map;
 
 /**
- * Ensure the user has the required privilege to execute a plugin URL.  Only URLs in the /plugin namespace are
- * handled by this interceptor.
+ * Ensure the user has the required privilege to execute a plugin URL. Only URLs in the /plugin namespace are handled by
+ * this interceptor.
  */
 public class AcmPluginRoleBasedAccessInterceptor extends HandlerInterceptorAdapter
 {
@@ -29,55 +30,54 @@ public class AcmPluginRoleBasedAccessInterceptor extends HandlerInterceptorAdapt
         String method = request.getMethod();
         String url = request.getServletPath();
 
-        if ( log.isDebugEnabled() )
+        if (log.isDebugEnabled())
         {
             log.debug("Checking user privilege for url: " + method + " " + url);
         }
 
         HttpSession session = request.getSession(false);
 
-        if ( session == null )
+        if (session == null)
         {
-            // no session yet - login must not have completed?  Anyway, proceed with the next handler.
+            // no session yet - login must not have completed? Anyway, proceed with the next handler.
             return true;
         }
 
         Map<String, Boolean> userPrivileges = (Map<String, Boolean>) session.getAttribute("acm_privileges");
-        if ( userPrivileges == null )
+        if (userPrivileges == null)
         {
-            // no user privileges.  somehow the user is logged in and is calling a plugin URL, but does not have
-            // privileges in the user session.  Somehow the login success handler did not run.  This is an
-            // anomalous situation.  Better return HTTP 403.
-            throw new AcmNotAuthorizedException(request.getServletPath());
+            // no user privileges. somehow the user is logged in and is calling a plugin URL, but does not have
+            // privileges in the user session. Somehow the login success handler did not run. This is an
+            // anomalous situation. Better return HTTP 403.
+            // throw new AcmNotAuthorizedException(request.getServletPath());
+            response.setStatus(response.SC_FORBIDDEN);
         }
 
         List<AcmPluginUrlPrivilege> urlPrivileges = getAcmPluginManager().getUrlPrivileges();
         boolean hasPrivilege = determinePrivilege(method, url, userPrivileges, urlPrivileges);
 
-        if ( ! hasPrivilege )
+        if (!hasPrivilege)
         {
-            throw new AcmNotAuthorizedException(request.getServletPath());
+            // throw new AcmNotAuthorizedException(request.getServletPath());
+            response.setStatus(response.SC_FORBIDDEN);
         }
 
         return true;
     }
 
-    protected boolean determinePrivilege(
-            String method,
-            String url,
-            Map<String, Boolean> userPrivileges,
+    protected boolean determinePrivilege(String method, String url, Map<String, Boolean> userPrivileges,
             List<AcmPluginUrlPrivilege> urlPrivileges)
     {
         boolean hasPrivilege = false;
-        for ( AcmPluginUrlPrivilege urlPrivilege : urlPrivileges )
+        for (AcmPluginUrlPrivilege urlPrivilege : urlPrivileges)
         {
-            if ( urlPrivilege.matches(url, method) )
+            if (urlPrivilege.matches(url, method))
             {
                 String requiredPrivilege = urlPrivilege.getRequiredPrivilege().getPrivilegeName();
-                if ( log.isDebugEnabled() )
+                if (log.isDebugEnabled())
                 {
-                    log.debug("Required privilege for " + method + " " + url + ": " + requiredPrivilege +
-                        "; user has privilege: " + userPrivileges.containsKey(requiredPrivilege));
+                    log.debug("Required privilege for " + method + " " + url + ": " + requiredPrivilege + "; user has privilege: "
+                            + userPrivileges.containsKey(requiredPrivilege));
                 }
                 hasPrivilege = userPrivileges.containsKey(requiredPrivilege) ? userPrivileges.get(requiredPrivilege) : false;
             }
