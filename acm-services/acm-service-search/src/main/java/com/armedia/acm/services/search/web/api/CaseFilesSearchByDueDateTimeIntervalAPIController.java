3b622da1f939ccd96429a46f8039e807d9cfc3d0
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2016-12-27 18:40:50 +0100

    Removed new line from json response for frevvo used services.
    Former-commit-id: 918854b9e01ef3b09bd22b9bf679ac1b8fbe29bc

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/web/api/CaseFilesSearchByDueDateTimeIntervalAPIController.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/web/api/CaseFilesSearchByDueDateTimeIntervalAPIController.java
@@ -3,6 +3,7 @@ package com.armedia.acm.services.search.web.api;
 import com.armedia.acm.services.search.model.SolrCore;
 import com.armedia.acm.services.search.model.TimePeriodForSearch;
 import com.armedia.acm.services.search.service.ExecuteSolrQuery;
+
 import org.mule.api.MuleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -23,8 +25,9 @@ import java.net.URLEncoder;
  */
 
 @Controller
-@RequestMapping( { "/api/v1/plugin/search", "/api/latest/plugin/search"} )
-public class CaseFilesSearchByDueDateTimeIntervalAPIController {
+@RequestMapping({ "/api/v1/plugin/search", "/api/latest/plugin/search" })
+public class CaseFilesSearchByDueDateTimeIntervalAPIController
+{
     private transient final Logger log = LoggerFactory.getLogger(getClass());
 
     private ExecuteSolrQuery executeSolrQuery;
@@ -32,47 +35,47 @@ public class CaseFilesSearchByDueDateTimeIntervalAPIController {
     @RequestMapping(value = "/caseFilesSearch/byTimeInterval", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 
     @ResponseBody
-    public String caseFiles(
-            @RequestParam(value = "timePeriod" ,required = true) String timePeriod,
+    public String caseFiles(@RequestParam(value = "timePeriod", required = true) String timePeriod,
             @RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
-            @RequestParam(value = "n", required = false, defaultValue = "5") int maxRows,
-            Authentication authentication,
-            HttpServletResponse httpResponse
-    ) throws MuleException, UnsupportedEncodingException
+            @RequestParam(value = "n", required = false, defaultValue = "5") int maxRows, Authentication authentication,
+            HttpServletResponse httpResponse) throws MuleException, UnsupportedEncodingException
     {
 
-        if (log.isDebugEnabled()) {
-            if("all".equals(timePeriod)) {
-                log.debug("User '" + authentication.getName() + "' is searching for all Case Files grouped by status");
-            }
-            log.debug("User '" + authentication.getName() + "' is searching for all Case Files grouped by status with dueDate the last:'" + timePeriod + "' ");
+        if ("all".equals(timePeriod))
+        {
+            log.debug("User '{}' is searching for all Case Files grouped by status", authentication.getName());
         }
+        log.debug("User '{}' is searching for all Case Files grouped by status with dueDate the last:'{}' ", authentication.getName(),
+                timePeriod);
 
         String query = "object_type_s:CASE_FILE";
         String sort = "AND dueDate_tdt ASC";
 
-        switch (TimePeriodForSearch.getTimePeriod(timePeriod)){
-             case ALL:
-                 break;
-             case LAST_MONTH:
-                 query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-1MONTH TO *]", "UTF-8");
-                 break;
-             case LAST_YEAR:
-                 query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-1YEAR TO *]", "UTF-8");
-                 break;
-             case LAST_WEEK:
-                 query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-7DAY TO *]", "UTF-8");
-                 break;
-             default:
-                 break;
+        switch (TimePeriodForSearch.getTimePeriod(timePeriod))
+        {
+        case ALL:
+            break;
+        case LAST_MONTH:
+            query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-1MONTH TO *]", "UTF-8");
+            break;
+        case LAST_YEAR:
+            query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-1YEAR TO *]", "UTF-8");
+            break;
+        case LAST_WEEK:
+            query += URLEncoder.encode("AND dueDate_tdt:[NOW/DAY-7DAY TO *]", "UTF-8");
+            break;
+        default:
+            break;
         }
 
-        query +=" AND group=true AND group.field=status_lcs AND group.limit=0";
+        query += " AND group=true AND group.field=status_lcs AND group.limit=0";
         query = query.replaceAll(" ", "+");
         sort = sort.replaceAll(" ", "+");
 
-        String results = getExecuteSolrQuery().getResultsByPredefinedQuery(authentication, SolrCore.ADVANCED_SEARCH,
-                query, startRow, maxRows, sort);
+        String results = getExecuteSolrQuery().getResultsByPredefinedQuery(authentication, SolrCore.ADVANCED_SEARCH, query, startRow,
+                maxRows, sort);
+
+        results = results.replaceAll("\\n", "");
 
         httpResponse.addHeader("X-JSON", results);
 
