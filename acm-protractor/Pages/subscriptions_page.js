cd2517084f1359dbcfc584bfedcb3e32e61c042d
Milan Jovanovski <milan.jovanovski@armedia.com>
2017-01-20 18:41:21 +0100

    Updated the test script and functions
    Former-commit-id: 030421cea746fbd53b3c625ae6d001167117e92e

--- acm-protractor/Pages/subscriptions_page.js
+++ acm-protractor/Pages/subscriptions_page.js
@@ -1,4 +1,5 @@
 var Objects = require('../json/Objects.json');
+var util = require('../util/utils.js');
 var EC = protractor.ExpectedConditions;
 var subscriptionModule = element(by.css(Objects.subscriptionsPage.locators.subscriptionsModule));
 var subscriptionsTitle = element(by.xpath(Objects.subscriptionsPage.locators.subscriptionsTitle));
@@ -8,14 +9,15 @@ var resultEvent = element.all(by.repeater(Objects.subscriptionsPage.locators.res
 var resultParentName = element.all(by.repeater(Objects.subscriptionsPage.locators.resultTable)).get(1);
 var resultParentType = element.all(by.repeater(Objects.subscriptionsPage.locators.resultTable)).get(2);
 var resultModfied = element.all(by.repeater(Objects.subscriptionsPage.locators.resultTable)).get(3);
-
+var parentTypeSort = element.all(by.xpath(Objects.subscriptionsPage.locators.sort)).get(2);
+var modifiedBySort = element.all(by.xpath(Objects.subscriptionsPage.locators.sort)).get(3);
 
 var subscriptionPage = function() {
 
     this.clickSubcriptionsModule = function() {
 
         browser.executeScript('arguments[0].click()', subscriptionModule);
-        browser.sleep(10000);
+
     }
 
 
@@ -23,7 +25,7 @@ var subscriptionPage = function() {
 
         browser.wait(EC.visibilityOf(element(by.cssContainingText(Objects.subscriptionsPage.locators.facets, subscription))), 20000, "Checkbox for" + " " + subscription + "Is not displayed").then(function() {
             element(by.cssContainingText(Objects.subscriptionsPage.locators.facets, subscription)).click().then(function() {
-                browser.sleep(5000);
+                browser.wait(EC.visibilityOf(element(by.cssContainingText(Objects.subscriptionsPage.locators.facets, subscription))), 10000);
             });
         });
     }
@@ -33,11 +35,50 @@ var subscriptionPage = function() {
         browser.wait(EC.visibilityOf(element(by.model(Objects.subscriptionsPage.locators.subscriptionsInput))), 20000, "Search input for subscription is not displayed").then(function() {
             searchSubscriptionInput.sendKeys(data).then(function() {
                 searchBtn.click().then(function() {
-                    browser.sleep(5000);
+                    browser.wait(EC.visibilityOf(element.all(by.repeater(Objects.subscriptionsPage.locators.resultTable)).get(2)), 10000);
                 });
             });
         });
     }
+
+
+    this.clickParentTypeSort = function() {
+        parentTypeSort.click();
+
+    }
+    this.clikmodifiedBySort = function() {
+        modifiedBySort.click();
+    }
+
+    this.returnModifiedByMonth = function() {
+
+        resultModfied.getText().then(function(text) {
+            var res = text.substring(0, 2);
+            var months = [util.returnCurrentMonth(), util.returnPreviousMonth()];
+            expect(months).toContain(res);
+
+        });
+    }
+
+    this.returnModifiedByYear = function() {
+
+        resultModfied.getText().then(function(text) {
+            var res = text.substring(6, 10);
+            var years = [util.returnpreviousYear(), util.returnCurrentYear()];
+            expect(years).toContain(res);
+        });
+
+    }
+
+    this.returnModifiedByWeek = function() {
+
+        resultModfied.getText().then(function(text) {
+            var res = text.substring(3, 5);
+            var week = util.previousWeek();
+            expect(week).toContain(res);
+        });
+    }
+
 
     this.returnResultParentType = function() {
         return resultParentType.getText();
--- acm-protractor/test_spec/notification_test.js
+++ acm-protractor/test_spec/notification_test.js
@@ -3,6 +3,7 @@ var notificationPage = require('../Pages/notifications_page.js');
 var loginPage = require('../Pages/login_page.js');
 var Objects = require('../json/Objects.json');
 var utils = require('../util/utils.js');
+var using = require(process.env['USERPROFILE'] + '/node_modules/jasmine-data-provider');
 var flag = false;
 
 function testAsync(done) {
@@ -35,40 +36,43 @@ describe('notification page test', function() {
         expect(notificationPage.returnDescription()).toContain(notificationPage.returnModifiedBy());
     });
 
-    it('should select parent type participant and verify the object type in the result table', function() {
-
-        notificationPage.navigateToPage("Notifications");
-        notificationPage.selectNotification("Participant")
-        expect(notificationPage.returnObjectType()).toEqual("PARTICIPANT");
-    });
-
-
-    it('should select parent type task and verify it the object type in result table', function() {
-
-        notificationPage.navigateToPage("Notifications");
-        notificationPage.selectNotification("Task")
-        expect(notificationPage.returnObjectType()).toEqual("TASK");
-    });
-
-    it('should select parent type task and verify it the object type in result table', function() {
-
-        notificationPage.navigateToPage("Notifications");
-        notificationPage.selectNotification("Task")
-        expect(notificationPage.returnObjectType()).toEqual("TASK");
-    });
-
     it('should select previous month and verify it the result table', function() {
 
         notificationPage.navigateToPage("Notifications");
         notificationPage.selectNotification("Previous Month")
-        expect(notificationPage.returnModifiedBy()).toContain(utils.returnToday('/'))
+        notificationPage.clickSortModifiedBy();
+        notificationPage.returnModifiedByMonth();
+
+    });
+
+    it('should select previous Year and verify it the result table', function() {
+
+        notificationPage.navigateToPage("Notifications");
+        notificationPage.selectNotification("Previous Year")
+        notificationPage.clickSortModifiedBy();
+        notificationPage.returnModifiedByYear();
     });
 
     it('should select previous week and verify it the result table', function() {
 
         notificationPage.navigateToPage("Notifications");
         notificationPage.selectNotification("Previous Week")
-        expect(notificationPage.returnModifiedBy()).toContain(utils.returnToday('/'))
+        notificationPage.clickSortModifiedBy();
+        notificationPage.returnModifiedByWeek();
     });
 
+    using([{ parentType: "File", expected: "FILE" }, { parentType: "Note", expected: "NOTE" },
+        { parentType: "Task", expected: "TASK" }, { parentType: "Complaint", expected: "COMPLAINT" },
+        { parentType: "Case File", expected: "CASE_FILE" }, { parentType: "Participant", expected: "PARTICIPANT" }
+    ], function(data) {
+
+        it('should select parent type and verify the object type in the result table ', function() {
+
+            notificationPage.navigateToPage("Notifications");
+            notificationPage.selectNotification(data.parentType)
+            notificationPage.clicksortObjectType();
+            expect(notificationPage.returnObjectType()).toEqual(data.expected);
+        });
+
+    });
 });
