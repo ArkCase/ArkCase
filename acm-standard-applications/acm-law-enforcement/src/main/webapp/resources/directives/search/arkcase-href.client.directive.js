ebede66ea32a2aaa26fc6bdd9e3404ef9113d0f1
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2017-01-24 16:28:06 +0100

    Added url parameter to directive. Notification object has url as solr attribute.
    Former-commit-id: f1f662719ed1dd5393ef84da139897e8dd5ab2af

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/search/arkcase-href.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/search/arkcase-href.client.directive.js
@@ -13,7 +13,8 @@ angular.module('directives').directive('arkcaseHref', ['UtilService', 'ObjectSer
             restrict: 'A',
             scope: {
                 objectData: '=',
-                isParent: '='
+                isParent: '=',
+                url: '='
             },
             link: function(scope, element, attrs) {
                 scope.$watch('objectData', function(newValue, oldValue) {
@@ -21,27 +22,31 @@ angular.module('directives').directive('arkcaseHref', ['UtilService', 'ObjectSer
                 });
                 
                 function buildUrl(objectData) {
-                    var objectType = Util.goodMapValue(objectData, "object_type_s");
-                    var objectId = Util.goodMapValue(objectData, "object_id_s");
-                    if (scope.isParent) {
-                        var parentReference = Util.goodMapValue(objectData, "parent_ref_s", "-");
-                        objectType  = parentReference.substring(parentReference.indexOf('-') + 1);
-                        objectId = parentReference.substring(0, parentReference.indexOf('-'));
-                    }
-                    if (objectType == ObjectService.ObjectTypes.TASK) {
-                        objectType = (Util.goodMapValue(objectData, "adhocTask_b", false)) ? ObjectService.ObjectTypes.ADHOC_TASK : ObjectService.ObjectTypes.TASK;
-                    }
-                    ObjectLookupService.getObjectTypes().then(
-                        function (objectTypes) {
-                            var url = '';
-                            var foundObjectType = _.find(objectTypes, {key: objectType});
-                            if (Util.goodMapValue(foundObjectType, "url", false)) {
-                                url = foundObjectType.url;
-                                url = url.replace(":id", objectId);
-                            }
-                            element.attr('href', url);
+                    if (scope.url == undefined) {
+                        var objectType = Util.goodMapValue(objectData, "object_type_s");
+                        var objectId = Util.goodMapValue(objectData, "object_id_s");
+                        if (scope.isParent) {
+                            var parentReference = Util.goodMapValue(objectData, "parent_ref_s", "-");
+                            objectType  = parentReference.substring(parentReference.indexOf('-') + 1);
+                            objectId = parentReference.substring(0, parentReference.indexOf('-'));
                         }
-                    );
+                        if (objectType == ObjectService.ObjectTypes.TASK) {
+                            objectType = (Util.goodMapValue(objectData, "adhocTask_b", false)) ? ObjectService.ObjectTypes.ADHOC_TASK : ObjectService.ObjectTypes.TASK;
+                        }
+                        ObjectLookupService.getObjectTypes().then(
+                            function (objectTypes) {
+                                var objectUrl = '';
+                                var foundObjectType = _.find(objectTypes, {key: objectType});
+                                if (Util.goodMapValue(foundObjectType, "url", false)) {
+                                    objectUrl = foundObjectType.url;
+                                    objectUrl = objectUrl.replace(":id", objectId);
+                                }
+                                element.attr('href', objectUrl);
+                            }
+                        );
+                    } else {
+                        element.attr('href', scope.url);
+                    }
                 };
                 
             }
