83cbddbd85f61a32cf067a066a39ceb9b963fc4b
Lazo Lazarev <lazo.lazarev@armedia.com>
2017-01-25 17:05:41 +0100

    AFDP-2915 Created the controller for correspondence query beans.
    Former-commit-id: fa6e72ea25576a6d9ecdfece4329bf18c315cc4f

--- acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/model/CorrespondenceQuery.java
+++ acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/model/CorrespondenceQuery.java
@@ -1,5 +1,6 @@
 package com.armedia.acm.correspondence.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,22 +8,49 @@ import java.util.List;
  */
 public class CorrespondenceQuery
 {
-    private String jpaQuery;
-    private List<String> fieldNames;
+    private QueryType queryType;
 
-    public String getJpaQuery() {
+    private String jpaQuery;
+
+    private List<String> fieldNames = new ArrayList<>();
+
+    /**
+     * @return the queryType
+     */
+    public QueryType getQueryType()
+    {
+        return queryType;
+    }
+
+    /**
+     * @param queryType the queryType to set
+     */
+    public void setQueryType(QueryType queryType)
+    {
+        this.queryType = queryType;
+    }
+
+    public String getJpaQuery()
+    {
         return jpaQuery;
     }
 
-    public void setJpaQuery(String jpaQuery) {
+    public void setJpaQuery(String jpaQuery)
+    {
         this.jpaQuery = jpaQuery;
     }
 
-    public List<String> getFieldNames() {
-        return fieldNames;
+    public List<String> getFieldNames()
+    {
+        return new ArrayList<>(fieldNames);
     }
 
-    public void setFieldNames(List<String> fieldNames) {
-        this.fieldNames = fieldNames;
+    public void setFieldNames(List<String> fieldNames)
+    {
+        if (fieldNames != null && !fieldNames.isEmpty())
+        {
+            this.fieldNames.clear();
+            this.fieldNames.addAll(fieldNames);
+        }
     }
 }
