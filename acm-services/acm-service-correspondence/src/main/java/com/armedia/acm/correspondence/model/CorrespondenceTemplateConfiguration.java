acbd59edb0af1fbbb14a15fc6962517d4337c13c
Lazo Lazarev <lazo.lazarev@armedia.com>
2017-01-26 23:08:53 +0100

    AFDP-2915 Added 'CorrespondenceTemplateManager' to create and handle instances of 'CorrespondenceTemplate' from JSON configuration instead of XML bean definitions.
    Former-commit-id: af916eb24885041a37392223c3aa200ea481a768

---
+++ acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/model/CorrespondenceTemplateConfiguration.java
@@ -0,0 +1,152 @@
+/**
+ *
+ */
+package com.armedia.acm.correspondence.model;
+
+import java.util.Map;
+
+/**
+ * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Jan 26, 2017
+ *
+ */
+public class CorrespondenceTemplateConfiguration
+{
+
+    /**
+     * The document type of the generated correspondence, e.g. &quot;Report of Investigation&quot;
+     */
+    private String documentType;
+
+    /**
+     * Must match a filename in the $HOME/.acm/correspondenceTemplates folder. This file must be a Microsoft Word .docx
+     * file, in the Office XML file format first introduced by Word 2007.
+     */
+    private String templateFilename;
+
+    /**
+     * Text of the JPA query to retrieve the data needed by the template. This query must return a column for each
+     * template substitution variable. It must be a valid JPA query.
+     */
+    private String correspondenceQueryBeanId;
+
+    /**
+     * Each entry must exactly match a field names form the query (key in this map) and substitution variable in the
+     * Word template (value in this map);
+     * <p/>
+     * Suppose the Word template includes the substitution variable text &quot;${Create Date}&quot; and &quot;${Subject
+     * Name}&quot;. In this case, the jpaQuery should return both the Create Date and the Subject Name. If &quot;Subject
+     * Name&quot; is the first column returned, and &quot;Create Date&quot; is the second column, then this list
+     * <strong>must</strong> include ${Subject Name} and ${Create Date} in that order.
+     *
+     */
+    private Map<String, String> templateSubstitutionVariables;
+
+    /**
+     * The date format that should be applied to any date. All date columns will be displayed in the generated
+     * correspondence in this format. Must be a valid date format according to Java DateFormat rules.
+     */
+    private String dateFormatString;
+
+    /**
+     * The number format that should be applied to any number. All number columns will be displayed in the generated
+     * correspondence in this format. Must be a valid number format according to Java NumberFormat rules.
+     */
+    private String numberFormatString;
+
+    /**
+     * @return the documentType
+     */
+    public String getDocumentType()
+    {
+        return documentType;
+    }
+
+    /**
+     * @param documentType the documentType to set
+     */
+    public void setDocumentType(String documentType)
+    {
+        this.documentType = documentType;
+    }
+
+    /**
+     * @return the templateFilename
+     */
+    public String getTemplateFilename()
+    {
+        return templateFilename;
+    }
+
+    /**
+     * @param templateFilename the templateFilename to set
+     */
+    public void setTemplateFilename(String templateFilename)
+    {
+        this.templateFilename = templateFilename;
+    }
+
+    /**
+     * @return the correspondenceQueryBeanId
+     */
+    public String getCorrespondenceQueryBeanId()
+    {
+        return correspondenceQueryBeanId;
+    }
+
+    /**
+     * @param correspondenceQueryBeanId the correspondenceQueryBeanId to set
+     */
+    public void setCorrespondenceQueryBeanId(String correspondenceQueryBeanId)
+    {
+        this.correspondenceQueryBeanId = correspondenceQueryBeanId;
+    }
+
+    /**
+     * @return the templateSubstitutionVariables
+     */
+    public Map<String, String> getTemplateSubstitutionVariables()
+    {
+        return templateSubstitutionVariables;
+    }
+
+    /**
+     * @param templateSubstitutionVariables the templateSubstitutionVariables to set
+     */
+    public void setTemplateSubstitutionVariables(Map<String, String> templateSubstitutionVariables)
+    {
+        this.templateSubstitutionVariables = templateSubstitutionVariables;
+    }
+
+    /**
+     * @return the dateFormatString
+     */
+    public String getDateFormatString()
+    {
+        return dateFormatString;
+    }
+
+    /**
+     * @param dateFormatString the dateFormatString to set
+     */
+    public void setDateFormatString(String dateFormatString)
+    {
+        this.dateFormatString = dateFormatString;
+    }
+
+    /**
+     * @return the numberFormatString
+     */
+    public String getNumberFormatString()
+    {
+        return numberFormatString;
+    }
+
+    /**
+     * @param numberFormatString the numberFormatString to set
+     */
+    public void setNumberFormatString(String numberFormatString)
+    {
+        this.numberFormatString = numberFormatString;
+    }
+
+}
---
+++ acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/service/CorrespondenceTemplateManager.java
@@ -0,0 +1,162 @@
+package com.armedia.acm.correspondence.service;
+
+import com.armedia.acm.correspondence.model.CorrespondenceQuery;
+import com.armedia.acm.correspondence.model.CorrespondenceTemplate;
+import com.armedia.acm.correspondence.model.CorrespondenceTemplateConfiguration;
+import com.armedia.acm.spring.SpringContextHolder;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Jan 26, 2017
+ *
+ */
+public class CorrespondenceTemplateManager implements InitializingBean
+{
+
+    private SpringContextHolder springContextHolder;
+
+    private Resource correspondenceTemplatesConfiguration;
+
+    private List<CorrespondenceTemplate> templates = new ArrayList<>();
+
+    Map<String, CorrespondenceQuery> correspondenceQueryBeansMap;
+
+    /**
+     * @param springContextHolder the springContextHolder to set
+     */
+    public void setSpringContextHolder(SpringContextHolder springContextHolder)
+    {
+        this.springContextHolder = springContextHolder;
+    }
+
+    /**
+     * @param correspondenceTemplatesConfigurationPath the correspondenceTemplatesConfigurationPath to set
+     */
+    public void setCorrespondenceTemplatesConfigurationPath(Resource correspondenceTemplatesConfigurationPath)
+    {
+        correspondenceTemplatesConfiguration = correspondenceTemplatesConfigurationPath;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        File file = correspondenceTemplatesConfiguration.getFile();
+        String resource = FileUtils.readFileToString(file);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<CorrespondenceTemplateConfiguration> templateConfigurations = mapper.readValue(resource,
+                new TypeReference<List<CorrespondenceTemplateConfiguration>>()
+                {
+                });
+
+        correspondenceQueryBeansMap = springContextHolder.getAllBeansOfType(CorrespondenceQuery.class);
+
+        templates.addAll(templateConfigurations.stream().map(this::mapTemplateFromConfiguration).collect(Collectors.toList()));
+
+        correspondenceQueryBeansMap = null;
+    }
+
+    private CorrespondenceTemplate mapTemplateFromConfiguration(CorrespondenceTemplateConfiguration configuration)
+    {
+        CorrespondenceTemplate template = new CorrespondenceTemplate();
+
+        template.setDocumentType(configuration.getDocumentType());
+        template.setTemplateFilename(configuration.getTemplateFilename());
+        template.setQuery(correspondenceQueryBeansMap.get(configuration.getCorrespondenceQueryBeanId()));
+        template.setTemplateSubstitutionVariables(configuration.getTemplateSubstitutionVariables());
+        template.setDateFormatString(configuration.getDateFormatString());
+        template.setNumberFormatString(configuration.getNumberFormatString());
+
+        return template;
+    }
+
+    /**
+     * @return the templates
+     */
+    public List<CorrespondenceTemplate> getTemplates()
+    {
+        return new ArrayList<>(templates);
+    }
+
+    public void addTemplate(CorrespondenceTemplate template) throws IOException
+    {
+        Optional<CorrespondenceTemplate> existing = templates.stream()
+                .filter(t -> t.getTemplateFilename().equals(template.getTemplateFilename())).findAny();
+        if (existing.isPresent())
+        {
+            CorrespondenceTemplate existingTemplate = existing.get();
+            updateTemplate(existingTemplate, template);
+        } else
+        {
+            templates.add(template);
+        }
+
+        updateConfiguration(templates);
+        // should update the complaintCorrespondenceForms.json and caseCorrespondenceForms.json as well
+    }
+
+    /**
+     * @param existingTemplate
+     * @param template
+     */
+    private void updateTemplate(CorrespondenceTemplate existingTemplate, CorrespondenceTemplate template)
+    {
+        existingTemplate.setDocumentType(template.getDocumentType());
+        existingTemplate.setTemplateFilename(template.getTemplateFilename());
+        existingTemplate.setQuery(template.getQuery());
+        existingTemplate.setTemplateSubstitutionVariables(template.getTemplateSubstitutionVariables());
+        existingTemplate.setDateFormatString(template.getDateFormatString());
+        existingTemplate.setNumberFormatString(template.getNumberFormatString());
+    }
+
+    /**
+     * @param templates2
+     * @throws IOException
+     */
+    private void updateConfiguration(List<CorrespondenceTemplate> templates) throws IOException
+    {
+        List<CorrespondenceTemplateConfiguration> configurations = templates.stream().map(this::mapConfigurationFromTemplate)
+                .collect(Collectors.toList());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String configurationsOutput = mapper.writeValueAsString(configurations);
+
+        File file = correspondenceTemplatesConfiguration.getFile();
+        FileUtils.writeStringToFile(file, configurationsOutput);
+
+    }
+
+    private CorrespondenceTemplateConfiguration mapConfigurationFromTemplate(CorrespondenceTemplate template)
+    {
+        CorrespondenceTemplateConfiguration configuration = new CorrespondenceTemplateConfiguration();
+
+        configuration.setDocumentType(template.getDocumentType());
+        configuration.setTemplateFilename(configuration.getTemplateFilename());
+        configuration.setCorrespondenceQueryBeanId(null);
+        configuration.setTemplateSubstitutionVariables(template.getTemplateSubstitutionVariables());
+        configuration.setDateFormatString(template.getDateFormatString());
+        configuration.setNumberFormatString(template.getNumberFormatString());
+
+        return configuration;
+    }
+
+}
