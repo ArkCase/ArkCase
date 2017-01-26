07d783ddb514bdefb2d799e3441e588c2a32033e
David Miller <david.miller@armedia.com>
2017-01-26 15:11:29 -0500

    AFDP-2915 Flush the EM before detaching, so we are sure any case file changes are saved.
    Former-commit-id: 7b32089f142613e5153c546346cfab050df1cae8

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/api/SaveCaseFileAPIController.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/api/SaveCaseFileAPIController.java
@@ -59,6 +59,7 @@ public class SaveCaseFileAPIController
             CaseFile saved = getSaveCaseService().saveCase(in, auth, ipAddress);
 
             // Detach the object to prevent event handler changes from being persisted to the database
+            getCaseFileDao().getEm().flush();
             getCaseFileDao().getEm().detach(saved);
 
             // since the approver list is not persisted to the database, we want to send them back to the caller...
