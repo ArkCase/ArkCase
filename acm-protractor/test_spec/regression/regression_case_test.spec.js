38c704ceb7522fc3e4cedc1c8b58e0701dd0448e
Sanja Paskova <sanja.paskova@armedia.com>
2017-02-01 07:16:34 -0500

    Added automated test for AFDP-1200
    Former-commit-id: f0d85190e80fe385d941e73368b5ed6d0cd09120

--- acm-protractor/test_spec/regression/regression_case_test.spec.js
+++ acm-protractor/test_spec/regression/regression_case_test.spec.js
@@ -12,6 +12,8 @@ var timeTrackingPage = require('../../Pages/time_tracking_page.js');
 var costTrackingPage = require('../../Pages/cost_tracking_page.js');
 var using = require(process.env['USERPROFILE'] + '/node_modules/jasmine-data-provider');
 var preferencesPage = require('../../Pages/preference_page.js');
+var auditPage = require('../../Pages/audit_page.js');
+
 
 function testAsync(done) {
 
@@ -45,7 +47,7 @@ describe('case page tests', function() {
 
     });
 
-    it('should create new case and try to add owner and no access from participant tab for same user and verify the alert message', function() {
+    xit('should create new case and try to add owner and no access from participant tab for same user and verify the alert message', function() {
 
             casePage.clickNewButton().navigateToNewCasePage().switchToIframes().submitGeneralInformation(Objects.casepage.data.caseTitle, "Arson").clickNextBtn().initiatorInformation(Objects.casepage.data.firstName, Objects.casepage.data.lastName);
             casePage.clickParticipantTab().selectParticipant("Owner", Objects.casepage.data.approverSamuel);
@@ -54,6 +56,25 @@ describe('case page tests', function() {
             casePage.switchToIframes();
             expect(casePage.returnParticipantTypeAlert()).toEqual("This action is not allowed. No Access and Owner is conflict combination.");
             casePage.switchToDefaultContent();
+    });
+
+    it('should open document and send email and then check history table and auditing', function() {
+
+        casePage.clickModuleCasesFiles();
+        casePage.waitForCaseID();
+        var caseid = element(by.xpath(Objects.casepage.locators.caseID)).getText();
+        casePage.clickExpandFancyTreeTopElementAndSubLink("Documents").doubleClickRootFolder().rightClickDocument().clickDocAction("Email").sendEmail();
+        casePage.clickSublink("History");
+        //currently sending emails is not working to be added code for check in history table and also audit report
+        // expect(casePage.returnHistoryEventName()).toEqual(Objects.casepage.data.historyEvent);
+        // expect(casePage.returnHistoryDate()).toContain(utils.returnToday("/"));
+        // expect(casePage.returnHistoryUser()).toEqual(Objects.casepage.data.assigneeSamuel);
+        // casePage.navigateToPage("Audit");
+        // auditPage.runReport("Files", "", utils.returnToday("/"), utils.returnToday("/"));
+        // auditPage.switchToAuditframes();
+        // auditPage.validateAuditReportTitles(Objects.auditPage.data.auditReportColumn1Title, Objects.auditPage.data.auditReportColumn2Title, Objects.auditPage.data.auditReportColumn3Title, Objects.auditPage.data.auditReportColumn4Title, Objects.auditPage.data.auditReportColumn5Title, Objects.auditPage.data.auditReportColumn6Title, Objects.auditPage.data.auditReportColumn7Title);
+        // auditPage.validateAuditReportValues(utils.returnToday("/"), Objects.taskspage.data.assigneeSamuel, "Case Viewed", "success", caseid, "CASE_FILE" );
+
     });
 
 
