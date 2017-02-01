acca25f35db3dd5a71fd8b4a791cef1bb878ec73
ARMEDIA\john.faniyi <john.faniyi@armedia.com>
2017-02-01 13:01:04 -0500

    Updated the Search Modal directiveto include and extra filter parameter. Changed the New Task view to exclude the Group Task checkbox, replaced that logic with the User/Group picker Modal that allows the user to pick either an assignee or a group. Changed the owning group options menu to the Group picker so that the user can change groups using the Group picker instead of the options dropdown menu.
    Former-commit-id: 36a1dd4df329217b6853377e0e898da0242a3f9c

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-group-picker-modal-controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-group-picker-modal-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('tasks').controller('Tasks.AssigneePickerController', ['$scope', '$modal', '$modalInstance',
+angular.module('tasks').controller('Tasks.GroupPickerController', ['$scope', '$modal', '$modalInstance',
     '$translate', 'UtilService', 'ConfigService', '$q', 'owningGroup',
     function ($scope, $modal, $modalInstance, $translate, Util, ConfigService, $q, owningGroup) {
 
@@ -19,18 +19,18 @@ angular.module('tasks').controller('Tasks.AssigneePickerController', ['$scope', 
         $scope.onClickCancel = function () {
             $modalInstance.dismiss('cancel');
         };
-        $scope.pickAssignee = function () {
+        $scope.pickGroup = function () {
 
             var params = {};
             $scope.owningGroup = owningGroup;
 
-            params.header = $translate.instant("tasks.comp.assigneePickerModal.searchAssigneeHeader");
-            params.filter = 'fq="object_type_s":(GROUP OR USER)';
+            params.header = $translate.instant("tasks.comp.groupPickerModal.searchGroupHeader");
+            params.filter = 'fq="object_type_s": GROUP';
             params.extraFilter = '&fq="name": ';
             params.config = Util.goodMapValue($scope.config, "dialogUserPicker");
 
             var modalInstance = $modal.open({
-                templateUrl: "modules/tasks/views/components/task-assignee-picker-search-modal.client.view.html",
+                templateUrl: "modules/tasks/views/components/task-group-picker-search-modal.client.view.html",
                 controller: ['$scope', '$modalInstance', 'params', function ($scope, $modalInstance, params) {
                     $scope.modalInstance = $modalInstance;
                     $scope.header = params.header;
