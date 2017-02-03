761f939e67edc6878ed1f7ff3f0c9caa9bf90df9
Sanja Paskova <sanja.paskova@armedia.com>
2017-02-03 10:57:19 -0500

    Added changes in case and complaints test because new features
    Former-commit-id: 38e2c79a93fe71dabb5771e68b03e1d6ea3ae625

--- acm-protractor/conf/conf.js
+++ acm-protractor/conf/conf.js
@@ -42,7 +42,7 @@ exports.config = {
     specs: [
 
         //any test can be run with command "protractor conf.js, just place it here"
-
+       
     ],
         //any suite can be run with command "protractor conf.js --suite=selected"
 
