796b33d62560aa361a1c00e2caf63998d1352d61
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-02-03 17:03:50 +0100

    Work in progress: updated the model. Created rules handler on case-file-plugin level. Now we need to decide whether we want to return list of errors or just a single one. At this moment the logic is a mixture of both.
    Former-commit-id: 85d9a60dd32a8d85c356f96b830afb593779b3c9

--- acm-services/acm-service-participants/src/main/java/com/armedia/acm/services/participants/model/CheckParticipantListModel.java
+++ acm-services/acm-service-participants/src/main/java/com/armedia/acm/services/participants/model/CheckParticipantListModel.java
@@ -2,13 +2,16 @@ package com.armedia.acm.services.participants.model;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CheckParticipantListModel
 {
     private List<String> errorsList = new ArrayList<>();
     private List<AcmParticipant> participantList;
     private String objectType;
+    private Map<String, List<String>> participants;
 
     public List<String> getErrorsList()
     {
@@ -28,6 +31,36 @@ public class CheckParticipantListModel
     public void setParticipantList(List<AcmParticipant> participantList)
     {
         this.participantList = participantList;
+
+        if (this.participantList != null)
+        {
+            this.participantList.stream().forEach(item -> addToParticipantListByType(item));
+        }
+    }
+
+    private void addToParticipantListByType(AcmParticipant participant)
+    {
+        if (participant != null)
+        {
+            if (this.participants == null)
+            {
+                this.participants = new HashMap<>();
+            }
+
+            List<String> participants = new ArrayList<>();
+            String type = participant.getParticipantType();
+            String id = participant.getParticipantLdapId();
+
+            if (this.participants.containsKey(type))
+            {
+                participants = this.participants.get(type);
+
+            }
+
+            participants.add(id);
+
+            this.participants.put(type, participants);
+        }
     }
 
     public String getObjectType()
@@ -43,5 +76,10 @@ public class CheckParticipantListModel
     public void addErrorMessage(String reason)
     {
         errorsList.add(reason);
+    }
+
+    public Map<String, List<String>> getParticipants()
+    {
+        return participants;
     }
 }
