b655d3dab3f14c012b049fbebf7683de543e5107
Lazo Lazarev <lazo.lazarev@armedia.com>
2017-02-16 13:51:40 +0100

    AFDP-2780 Updated the Category controller to query Solr instance for GET calls.
    Former-commit-id: 0a4ce0a74771ed18679b42120e306801f03c50ea

--- acm-plugins/acm-default-plugins/acm-category-plugin/src/main/java/com/armedia/acm/plugins/category/web/api/CategoryManagementAPIController.java
+++ acm-plugins/acm-default-plugins/acm-category-plugin/src/main/java/com/armedia/acm/plugins/category/web/api/CategoryManagementAPIController.java
@@ -4,13 +4,20 @@ import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
 import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
 import com.armedia.acm.core.exceptions.AcmUpdateObjectFailedException;
 import com.armedia.acm.plugins.category.model.Category;
+import com.armedia.acm.plugins.category.model.CategoryStatus;
 import com.armedia.acm.plugins.category.service.CategoryService;
+import com.armedia.acm.services.search.model.SolrCore;
+import com.armedia.acm.services.search.service.ExecuteSolrQuery;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.mule.api.MuleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +26,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -35,31 +46,67 @@ public class CategoryManagementAPIController
 
     private CategoryService categoryService;
 
+    private ExecuteSolrQuery executeSolrQuery;
+
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<Category> getCategories()
+    public List<Category> getCategories(Authentication auth, @RequestParam(value = "start", required = false, defaultValue = "0") int start,
+            @RequestParam(value = "n", required = false, defaultValue = "10") int n,
+            @RequestParam(value = "s", required = false, defaultValue = "ASC") String s) throws AcmObjectNotFoundException
     {
-        return categoryService.getRoot();
+        String query = String.format("object_type_s:CATEGORY AND -parent_id_s:*&sort=queue_order_s %s", s);
+        try
+        {
+            String solrResponse = executeSolrQuery.getResultsByPredefinedQuery(auth, SolrCore.ADVANCED_SEARCH, query, start, n, "");
+            return extractCategories(solrResponse);
+        } catch (MuleException | IOException e)
+        {
+            log.error("Error while executing Solr query: {}", query, e);
+            throw new AcmObjectNotFoundException("Category", null, "Could not retreive the root categories.", e);
+        }
+
     }
 
     @RequestMapping(value = "/{categoryId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Category getCategory(@PathVariable(value = "categoryId") Long categoryId,
-            @RequestParam(value = "start", required = false, defaultValue = "0") int start,
-            @RequestParam(value = "n", required = false, defaultValue = "10") int n,
-            @RequestParam(value = "s", required = false, defaultValue = "DESC") String s) throws AcmObjectNotFoundException
+    public Category getCategory(Authentication auth, @PathVariable(value = "categoryId") Long categoryId) throws AcmObjectNotFoundException
     {
-        return categoryService.get(categoryId);
+        String query = String.format("object_type_s:CATEGORY AND object_id_s:%d&sort=queue_order_s ASC", categoryId);
+        try
+        {
+            String solrResponse = executeSolrQuery.getResultsByPredefinedQuery(auth, SolrCore.ADVANCED_SEARCH, query, 0, 1, "");
+            List<Category> result = extractCategories(solrResponse);
+            if (result.isEmpty())
+            {
+                throw new AcmObjectNotFoundException("Category", categoryId, String.format("Category with id %d not found.", categoryId));
+            }
+            return result.get(0);
+        } catch (MuleException | IOException e)
+        {
+            log.error("Error while executing Solr query: {}", query, e);
+            throw new AcmObjectNotFoundException("Category", categoryId, String.format("Category with id %d not found.", categoryId), e);
+        }
     }
 
     @RequestMapping(value = "/{categoryId}/children", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<Category> getCategoryChildren(@PathVariable(value = "categoryId") Long categoryId,
+    public List<Category> getCategoryChildren(Authentication auth, @PathVariable(value = "categoryId") Long categoryId,
             @RequestParam(value = "start", required = false, defaultValue = "0") int start,
             @RequestParam(value = "n", required = false, defaultValue = "10") int n,
-            @RequestParam(value = "s", required = false, defaultValue = "DESC") String s) throws AcmObjectNotFoundException
+            @RequestParam(value = "s", required = false, defaultValue = "ASC") String s) throws AcmObjectNotFoundException
     {
-        return categoryService.getChildren(categoryId);
+        String query = String.format("object_type_s:CATEGORY AND parent_id_s:%d&sort=queue_order_s %s", categoryId, s);
+        try
+        {
+            String solrResponse = executeSolrQuery.getResultsByPredefinedQuery(auth, SolrCore.ADVANCED_SEARCH, query, start, n, "");
+            return extractCategories(solrResponse);
+        } catch (MuleException | IOException e)
+        {
+            log.error("Error while executing Solr query: {}", query, e);
+            throw new AcmObjectNotFoundException("Category", categoryId,
+                    String.format("Categories for parent Category with id %d not found.", categoryId), e);
+        }
+
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -138,12 +185,57 @@ public class CategoryManagementAPIController
     }
 
     /**
+     * @param solrResponse
+     * @return @throws IOException @throws
+     */
+    private List<Category> extractCategories(String solrResponse) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(solrResponse);
+        JsonNode docsJson = json.get("response").get("docs");
+        List<Category> categories = new ArrayList<>();
+        SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        docsJson.forEach(doc -> {
+            try
+            {
+                Category category = new Category();
+
+                category.setId(doc.get("object_id_s").asLong());
+                category.setName(doc.get("name").asText());
+                category.setDescription(doc.get("description_no_html_tags_parseable").asText());
+                // setParent
+                category.setCreator(doc.get("creator_lcs").asText());
+                category.setCreated(dateParser.parse(doc.get("create_date_tdt").asText()));
+                category.setModifier(doc.get("modifier_lcs").asText());
+                category.setModified(dateParser.parse(doc.get("modified_date_" + "tdt").asText()));
+                category.setStatus(CategoryStatus.valueOf(doc.get("status_lcs").asText()));
+
+                categories.add(category);
+            } catch (ParseException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+        return categories;
+    }
+
+    /**
      * @param categoryService
      *            the categoryService to set
      */
     public void setCategoryService(CategoryService categoryService)
     {
         this.categoryService = categoryService;
+    }
+
+    /**
+     * @param executeSolrQuery
+     *            the executeSolrQuery to set
+     */
+    public void setExecuteSolrQuery(ExecuteSolrQuery executeSolrQuery)
+    {
+        this.executeSolrQuery = executeSolrQuery;
     }
 
 }
