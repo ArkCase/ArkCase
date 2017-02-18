79b3652b2207a37d997dce85b2b0f246219e8baa
Maksud Sharif <maksud.sharif@armedia.com>
2017-02-18 14:28:41 -0500

    Allow extension for user metadata mapping
    Former-commit-id: cfaa18130003822d1e223481f46e317378fd1130

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/ldap/LdapSyncDatabaseHelper.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/ldap/LdapSyncDatabaseHelper.java
@@ -126,18 +126,20 @@ public class LdapSyncDatabaseHelper
 
             user.setUserDirectoryName(directoryName);
 
-            // append ad-hoc groups
-           Optional.ofNullable(getUserDao().findByUserId(user.getUserId()))
-                   .ifPresent(acmUser -> acmUser.getGroups()
-                       .stream()
-                       .filter(g -> "ADHOC_GROUP".equals(g.getType()))
-                       .forEach(user::addGroup)
-           );
+            preserveUserMetadata(user);
 
             AcmUser saved = getUserDao().save(user);
             retval.add(saved);
         }
         return retval;
+    }
+
+    protected void preserveUserMetadata(AcmUser user)
+    {
+        AcmUser existing = getUserDao().findByUserId(user.getUserId());
+        if(existing != null) {
+            existing.getGroups().stream().filter(g -> "ADHOC_GROUP".equalsIgnoreCase(g.getType())).forEach(user::addGroup);
+        }
     }
 
     protected void persistApplicationRoles(Set<String> applicationRoles, String roleType, Map<String, String> childParentPair)
