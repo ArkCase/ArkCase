6dce1267aed492da9fbabdde984bb1b037b946e3
Petar Ilin <petar.ilin@armedia.com>
2017-03-14 19:44:24 +0100

    AFDP-3175 multiple CMIS backends support
    Former-commit-id: 6c54f4c5d2c2d213668e9a70411f89ca6d573d02

--- acm-tool-integrations/acm-mule-context-manager/src/main/java/com/armedia/acm/muletools/mulecontextmanager/MuleContextManager.java
+++ acm-tool-integrations/acm-mule-context-manager/src/main/java/com/armedia/acm/muletools/mulecontextmanager/MuleContextManager.java
@@ -1,7 +1,6 @@
 package com.armedia.acm.muletools.mulecontextmanager;
 
 import com.armedia.acm.web.api.MDCConstants;
-
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleContext;
 import org.mule.api.MuleException;
@@ -28,9 +27,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.activation.DataHandler;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -41,6 +40,24 @@ public class MuleContextManager implements ApplicationContextAware
     private transient Logger log = LoggerFactory.getLogger(getClass());
     private String muleConfigFilePattern;
     private List<String> specificConfigFiles;
+
+    /**
+     * Map of CMIS configurations.
+     */
+    private Map<String, String> cmisConfigurations = new HashMap<>();
+
+    /**
+     * Register CMIS configuration with the registry.
+     * Invoked from bean configuration files ($HOME/.arkcase/acm/cmis/mule-config-*-cmis.xml)
+     *
+     * @param configId        unique configuration identifier
+     * @param versioningState versioning state value associated with this config (NONE, MINIR, MAJOR)
+     */
+    public void registerCmisConfig(String configId, String versioningState)
+    {
+        cmisConfigurations.put(configId, versioningState);
+        log.error("Configuration [{}] added with versioningState value [{}]", configId, versioningState);
+    }
 
     public void dispatch(String endpoint, Object payload, Map<String, Object> messageProperties) throws MuleException
     {
