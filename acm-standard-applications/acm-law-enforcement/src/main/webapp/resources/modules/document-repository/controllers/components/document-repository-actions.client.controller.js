918169ad2401fed9f2490f627516deb6662566f0
Nadica Cuculova <nadica.cuculova@armedia.com>
2017-04-03 15:18:27 +0200

    AFDP-3348: - Add audit events (updated, created) - Add action buttons (subscribe, restrict) - Add widgets for new module - Expand top node in doc-tree for documents in new module - Hide new facet in subscriptions and notifications
    Former-commit-id: c4c02f23006f5db7b9a139c6a3afe360b2ffa48e

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-repository/controllers/components/document-repository-actions.client.controller.js
@@ -0,0 +1,78 @@
+'use strict';
+
+angular.module('document-repository').controller('DocumentRepository.ActionsController', ['$scope', '$state'
+    , '$stateParams', '$q', '$modal', 'UtilService', 'ConfigService', 'ObjectService', 'Authentication'
+    , 'Object.SubscriptionService', 'Object.ModelService', 'DocumentRepository.InfoService', 'Helper.ObjectBrowserService'
+    , function ($scope, $state, $stateParams, $q, $modal
+        , Util, ConfigService, ObjectService, Authentication, ObjectSubscriptionService, ObjectModelService
+        , DocumentRepositoryInfoService, HelperObjectBrowserService) {
+
+        new HelperObjectBrowserService.Component({
+            scope: $scope
+            , stateParams: $stateParams
+            , moduleId: "document-repository"
+            , componentId: "actions"
+            , retrieveObjectInfo: DocumentRepositoryInfoService.getDocumentRepositoryInfo
+            , validateObjectInfo: DocumentRepositoryInfoService.validateDocumentRepositoryInfo
+            , onObjectInfoRetrieved: function (objectInfo) {
+                onObjectInfoRetrieved(objectInfo);
+            }
+        });
+
+        var onObjectInfoRetrieved = function (objectInfo) {
+            $scope.restricted = objectInfo.restricted;
+
+            Authentication.queryUserInfo().then(function (userInfo) {
+                $scope.userId = userInfo.userId;
+                ObjectSubscriptionService.getSubscriptions(userInfo.userId, ObjectService.ObjectTypes.DOC_REPO,
+                    $scope.objectInfo.id)
+                    .then(function (subscriptions) {
+                        var found = _.find(subscriptions, {
+                            userId: userInfo.userId,
+                            subscriptionObjectType: ObjectService.ObjectTypes.DOC_REPO,
+                            objectId: $scope.objectInfo.id
+                        });
+                        $scope.showBtnSubscribe = Util.isEmpty(found);
+                        $scope.showBtnUnsubscribe = !$scope.showBtnSubscribe;
+                    });
+            });
+        };
+
+        $scope.onClickRestrict = function ($event) {
+            if ($scope.restricted != $scope.objectInfo.restricted) {
+                $scope.objectInfo.restricted = $scope.restricted;
+
+                var docRepoInfo = Util.omitNg($scope.objectInfo);
+                DocumentRepositoryInfoService.saveDocumentRepository(docRepoInfo)
+                    .then(function () {
+
+                    }, function () {
+                        $scope.restricted = !$scope.restricted;
+                    });
+            }
+        };
+
+        $scope.subscribe = function (docRepoInfo) {
+            ObjectSubscriptionService.subscribe($scope.userId, ObjectService.ObjectTypes.DOC_REPO, docRepoInfo.id)
+                .then(function (data) {
+                    $scope.showBtnSubscribe = false;
+                    $scope.showBtnUnsubscribe = !$scope.showBtnSubscribe;
+                    return data;
+                });
+        };
+        $scope.unsubscribe = function (docRepoInfo) {
+            ObjectSubscriptionService.unsubscribe($scope.userId, ObjectService.ObjectTypes.DOC_REPO, docRepoInfo.id)
+                .then(function (data) {
+                    $scope.showBtnSubscribe = true;
+                    $scope.showBtnUnsubscribe = !$scope.showBtnSubscribe;
+                    return data;
+                });
+        };
+
+        $scope.refresh = function () {
+            $scope.$emit('report-object-refreshed', $stateParams.id);
+        };
+    }
+]);
+
+
