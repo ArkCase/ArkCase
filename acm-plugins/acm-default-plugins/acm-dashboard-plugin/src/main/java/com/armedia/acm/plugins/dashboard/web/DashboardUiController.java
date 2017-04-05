3fcf96b4a2b3369e88282109a99aab08baddfdb7
David Miller <david.miller@armedia.com>
2017-04-05 08:19:15 -0400

    Revert "Merge branch 'feature/AFDP-3343_ControllerAdjustments' into 'develop'"
    This reverts merge request !1116
    
    Former-commit-id: e4986decd8b8a91bb7f06cfd0409e3ddf4a25ef0

---
+++ acm-plugins/acm-default-plugins/acm-dashboard-plugin/src/main/java/com/armedia/acm/plugins/dashboard/web/DashboardUiController.java
@@ -0,0 +1,42 @@
+package com.armedia.acm.plugins.dashboard.web;
+
+import com.armedia.acm.pluginmanager.model.AcmPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//@RequestMapping({"/plugin/dashboard", "/", "/home"})
+public class DashboardUiController
+{
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private AcmPlugin dashboardPlugin;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showHomePage()
+    {
+        ModelAndView retval = new ModelAndView();
+        retval.setViewName("home");
+        String dashboardFile = null;
+        if (!dashboardPlugin.getPluginProperties().isEmpty() && dashboardPlugin.getPluginProperties().containsKey("acm.dashboardFile"))
+        {
+            dashboardFile = (String) dashboardPlugin.getPluginProperties().get("acm.dashboardFile");
+        } else
+        {
+            log.error("dashboardPlugin.properties or property value is missing, users will not have dashboard");
+        }
+        retval.addObject("dashboardFileName", dashboardFile);
+        return retval;
+    }
+
+    public AcmPlugin getDashboardPlugin()
+    {
+        return dashboardPlugin;
+    }
+
+    public void setDashboardPlugin(AcmPlugin dashboardPlugin)
+    {
+        this.dashboardPlugin = dashboardPlugin;
+    }
+}
