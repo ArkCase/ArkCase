110105a645eef470581f8d35e782da813deba8a7
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-04-12 01:34:07 +0200

    AFDP-3316
    Former-commit-id: d225307ba07410c888b268528930177c4caa5b41

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-pictures-upload.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-pictures-upload.client.controller.js
@@ -1,11 +1,7 @@
-angular.module('tasks').controller('Person.PictureUploadDialogController', ['$scope', '$modalInstance',
+angular.module('people').controller('Person.PictureUploadDialogController', ['$scope', '$modalInstance',
         function ($scope, $modalInstance) {
             $scope.userPicture = null;
             $scope.isDefault = false;
-            $scope.upload = function (data) {
-                console.log(data);
-                console.log($scope.userPicture);
-            };
             $scope.onClickCancel = function () {
                 $modalInstance.dismiss('Cancel');
             };
