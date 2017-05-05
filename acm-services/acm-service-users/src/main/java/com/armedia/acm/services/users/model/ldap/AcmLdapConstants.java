7a3c8d4a52b00cfb823e687ae9709f2db0a76d38
Nadica Cuculova <nadica.cuculova@armedia.com>
2017-05-05 16:29:00 +0200

    AFDP-3058: - Do not set up memberOf if directory is AD - Set correct password attribute when directory is AD/OpenLdap - Fix ad user template file name
    Former-commit-id: a5a0e6bf551f8ffd03752fda8e3777173f85da21

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/model/ldap/AcmLdapConstants.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/model/ldap/AcmLdapConstants.java
@@ -1,8 +1,6 @@
 package com.armedia.acm.services.users.model.ldap;
 
-/**
- * Created by armdev on 4/15/15.
- */
+
 public interface AcmLdapConstants
 {
     int RETRY_ATTEMPTS = 3;
@@ -16,13 +14,12 @@ public interface AcmLdapConstants
     String LDAP_MAIL_ATTR = "mail";
     String LDAP_MEMBER_OF_ATTR = "memberOf";
     String LDAP_MEMBER_ATTR = "member";
-    String LDAP_ACCOUNT_CONTROL_ATTR = "userAccountControl";
     String LDAP_PASSWORD_ATTR = "password";
+    String LDAP_UNICODE_PASSWORD_ATTR = "unicodePassword";
     String LDAP_UID_NUMBER_ATTR = "uidNumber";
     String LDAP_GID_NUMBER_ATTR = "gidNumber";
     String LDAP_HOME_DIRECTORY_ATTR = "homeDirectory";
     String LDAP_OBJECT_CLASS_ATTR = "objectClass";
-    String LDAP_SHADOW_WARNING_ATTR = "shadowWarning";
-    String LDAP_SHADOW_LAST_CHANGE_ATTR = "userAccountControl";
-    String LDAP_SHADOW_MAX_ATTR = "userAccountControl";
+    String LDAP_AD = "activedirectory";
+    String LDAP_OPENLDAP = "openldap";
 }
