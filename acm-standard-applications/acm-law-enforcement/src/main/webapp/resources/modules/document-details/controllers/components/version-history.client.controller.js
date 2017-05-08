633f8149462704496b1eec165e6db7ea51c3950b
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2017-05-08 17:22:50 +0200

    When selecting different file version, snowbound viewer is reloaded accordingly.
    Former-commit-id: 27f35c7e1cf25d39ae31c15d63357b970dd05cfe

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-details/controllers/components/version-history.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-details/controllers/components/version-history.client.controller.js
@@ -1,27 +1,53 @@
 'use strict';
 
 angular.module('document-details').controller('Document.VersionHistoryController', ['$scope', '$stateParams', '$q'
-    , 'UtilService', 'ConfigService', 'Helper.UiGridService',
-    function ($scope, $stateParams, $q, Util, ConfigService, HelperUiGridService) {
+    , 'UtilService', 'ConfigService', 'Helper.UiGridService', '$timeout',
+    function ($scope, $stateParams, $q, Util, ConfigService, HelperUiGridService, $timeout) {
 
         $scope.$on('document-data', updateVersionHistory);
         $scope.versions = [];
+        $scope.activeVersionTag = {};
+        $scope.selectedRows = [];
 
         function updateVersionHistory(event, documentDetails) {
             if (documentDetails.versions && documentDetails.versions.length) {
                 $scope.versions = documentDetails.versions;
+                $scope.activeVersionTag = documentDetails.activeVersionTag;
             }
         }
 
         var gridHelper = new HelperUiGridService.Grid({scope: $scope});
         
+        $scope.gridOptions = {
+                enableRowSelection: true,
+                enableFiltering: false,
+                enableRowHeaderSelection: true,
+                enableFullRowSelection: true,
+                data: [],
+                onRegisterApi: function(gridApi) {
+                    $scope.gridApi = gridApi;
+                    gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                        $scope.selectedRows = gridApi.selection.getSelectedRows();
+                        $scope.updateViewerOpenDocuments();
+                    });
+     
+                    gridApi.selection.on.rowSelectionChangedBatch($scope, function (rows) {
+                        $scope.selectedRows = gridApi.selection.getSelectedRows();
+                        $scope.updateViewerOpenDocuments();
+                    });
+               }
+            };
+        
+        $scope.updateViewerOpenDocuments = function () {
+            $scope.$emit('update-viewer-opened-versions', $scope.selectedRows);
+        };
+        
         $scope.$watchCollection('versions', function (newValue, oldValue) {
             if (newValue && newValue.length) {
-            	$q.all([gridHelper.getUsers(), ConfigService.getComponentConfig("document-details", "versionHistory")]).then(function (data) {
-                    gridHelper.setColumnDefs(data[1]);
-                    gridHelper.setBasicOptions(data[1]);
-                    gridHelper.disableGridScrolling(data[1]);
-                    gridHelper.setExternalPaging(data[1], $scope.retrieveGridData);
+                $q.all([gridHelper.getUsers(), ConfigService.getComponentConfig("document-details", "versionHistory")]).then(function (data) {
+                    $scope.gridOptions.columnDefs = data[1].columnDefs;
+                    $scope.gridOptions.paginationPageSizes = data[1].paginationPageSizes;
+                    $scope.gridOptions.paginationPageSize = data[1].paginationPageSize;
                     gridHelper.setUserNameFilter(data[0]);
 
                     $scope.retrieveGridData();
@@ -36,6 +62,13 @@ angular.module('document-details').controller('Document.VersionHistoryController
                 $scope.gridOptions = $scope.gridOptions ||
                     {};
                 $scope.gridOptions.data = $scope.versions;
+                _.forEach($scope.gridOptions.data, function (versionData) {
+                    if (versionData.versionTag == $scope.activeVersionTag) {
+                        $timeout(function () {
+                            $scope.gridApi.selection.selectRow(versionData);
+                        }, 100);
+                    }
+                });
                 $scope.gridOptions.totalItems = $scope.versions.length;
             }
         }
