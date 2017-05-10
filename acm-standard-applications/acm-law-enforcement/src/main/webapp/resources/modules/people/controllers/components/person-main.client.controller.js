dfe7ab96e7f44b6d608a8c9b5fbfaad770af087e
dragan.simonovski <dragan.simonovski@armedia.com>
2017-05-10 14:16:16 +0200

    modules are rearanged by ticket AFDP-3538. AFDP-3540 and AFDP-3539 about people/organizations modules in preferences. Only details and history widgets AFDP-3590 bug solved with jsog identity problems
    Former-commit-id: e977d5115b9136f318770c290f12cd6f6f6ab81c

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-main.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-main.client.controller.js
@@ -1,38 +1,37 @@
 'use strict';
 
-angular.module('people').controller('People.MainController', ['$scope', '$stateParams', '$translate'
-    , 'Acm.StoreService', 'UtilService', 'ConfigService', 'Person.InfoService', 'dashboard', 'Dashboard.DashboardService'
-    , function ($scope, $stateParams, $translate
-        , Store, Util, ConfigService, PersonInfoService, dashboard, DashboardService) {
+angular.module('people').controller('People.MainController', ['$scope', '$stateParams', '$translate', 'UtilService', 'ConfigService'
+    , 'Person.InfoService', 'ObjectService', 'Object.NoteService', 'Object.TaskService'
+    , 'Object.AuditService', 'Object.CostService', 'Object.TimeService', 'dashboard', 'Dashboard.DashboardService', 'Acm.StoreService'
+    , function ($scope, $stateParams, $translate, Util, ConfigService
+        , PersonInfoService, ObjectService, ObjectNoteService, ObjectTaskService
+        , ObjectAuditService, ObjectCostService, ObjectTimeService, dashboard, DashboardService, Store) {
 
-        /*        //$scope.$emit('main-component-started');
-
-         ConfigService.getModuleConfig("people").then(function (moduleConfig) {
-         $scope.components = moduleConfig.components;
-         $scope.config = _.find(moduleConfig.components, {id: "main"});
-
-         return moduleConfig;
-         });
+        var promiseConfig = ConfigService.getModuleConfig("people").then(function (moduleConfig) {
+            $scope.components = moduleConfig.components;
+            $scope.config = _.find(moduleConfig.components, {id: "main"});
+            return moduleConfig;
+        });
 
 
-         _.forEach(dashboard.widgets, function (widget, widgetId) {
-         widget.title = $translate.instant('dashboard.widgets.' + widgetId + '.title');
-         widget.description = $translate.instant('dashboard.widgets.' + widgetId + '.description');
-         });
+        _.forEach(dashboard.widgets, function (widget, widgetId) {
+            widget.title = $translate.instant('dashboard.widgets.' + widgetId + '.title');
+            widget.description = $translate.instant('dashboard.widgets.' + widgetId + '.description');
+        });
 
-         $scope.dashboard = {
-         structure: '12',
-         collapsible: false,
-         maximizable: false,
-         complaintModel: {
-         titleTemplateUrl: 'modules/dashboard/views/module-dashboard-title.client.view.html'
-         }
-         };
+        $scope.dashboard = {
+            structure: '12',
+            collapsible: false,
+            maximizable: false,
+            personModel: {
+                titleTemplateUrl: 'modules/dashboard/views/module-dashboard-title.client.view.html'
+            }
+        };
 
-         DashboardService.getConfig({moduleName: "COMPLAINT"}, function (data) {
-         $scope.dashboard.complaintModel = angular.fromJson(data.dashboardConfig);
-         $scope.dashboard.complaintModel.titleTemplateUrl = 'modules/dashboard/views/module-dashboard-title.client.view.html';
-         $scope.$emit("collapsed", data.collapsed);
-         });*/
+        DashboardService.getConfig({moduleName: "PERSON"}, function (data) {
+            $scope.dashboard.personModel = angular.fromJson(data.dashboardConfig);
+            $scope.dashboard.personModel.titleTemplateUrl = 'modules/dashboard/views/module-dashboard-title.client.view.html';
+            $scope.$emit("collapsed", data.collapsed);
+        });
     }
 ]);
