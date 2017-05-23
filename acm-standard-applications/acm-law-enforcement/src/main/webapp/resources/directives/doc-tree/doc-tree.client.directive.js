bb5170d6a6f8144088ce0fc5436d7c288446d5d7
Marko Stojanoski <marko.stojanoski@armedia.com>
2017-05-23 23:47:23 +0200

    Frontend: Updated modal for document upload with Language dropdown and added functionality.
    Former-commit-id: 1ec1d709fee6e0d22d27f6124935ae38dd748fac

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
@@ -1287,7 +1287,8 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                     }
 
                     var files = args.files;
-                    var promiseUploadFile = DocTree.doSubmitFormUploadFile(files);
+                    var fileLang = args.fileLang; 
+                    var promiseUploadFile = DocTree.doSubmitFormUploadFile(files, fileLang);
                     $q.when(promiseUploadFile).then(function (data) {
                         args.data = data;
                         DocTree.uploadSetting = null;
@@ -1540,7 +1541,10 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                         , {
                             name: "file/",
                             getArgs: function (data) {
-                                return {fileType: data.cmd.substring(this.name.length)};
+                                return {
+                                		fileType: data.cmd.split("/")[1],
+                                		fileLang: data.cmd.split("/")[2]
+                                	};
                             },
                             execute: function (nodes, args) {
                                 var selectFiles = DocTree.Command.findHandler("selectFiles/");
@@ -2283,7 +2287,7 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                     }
                     return dfd.promise();
                 }
-                , uploadFiles: function (formData, folderNode, names, fileType) {
+                , uploadFiles: function (formData, folderNode, names, fileType, fileLang) {
                     var dfd = $.Deferred();
                     if (!DocTree.isFolderNode(folderNode)) {
                         dfd.reject();
@@ -2294,6 +2298,9 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                         var promiseUploadFiles = Util.serviceCall({
                             service: Ecm.uploadFiles
                             , data: formData
+                            , param: {
+                            	fileLang: fileLang
+                            }
                             , onSuccess: function (data) {
                                 if (Validator.validateUploadInfo(data)) {
                                     var uploadInfo = data;
@@ -3500,7 +3507,7 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
                 }
             }
             //, doSubmitFormUploadFile: function (files, doRefresh) {
-            , doSubmitFormUploadFile: function (files) {
+            , doSubmitFormUploadFile: function (files, fileLang) {
                 if (!DocTree.uploadSetting) {
                     return Util.errorPromise("upload file error");
                 }
@@ -3529,7 +3536,7 @@ angular.module('directives').directive('docTree', ['$q', '$translate', '$modal',
 
                 var cacheKey = DocTree.getCacheKeyByNode(folderNode);
                 if (DocTree.uploadSetting.uploadFileNew) {
-                    DocTree.Op.uploadFiles(fd, folderNode, names, fileType)
+                    DocTree.Op.uploadFiles(fd, folderNode, names, fileType, fileLang)
                         .then(function (data) {
                                 _.each(data.nodes, function (node) {
                                     DocTree.markNodeOk(node)
