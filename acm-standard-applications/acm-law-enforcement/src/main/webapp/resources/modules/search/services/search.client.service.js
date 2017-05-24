959f79e5343b0cd867e03147beceaefc2daa91fc
Teng Wang <teng.wang@armedia.com>
2017-05-23 21:38:47 -0400

    Fix autosuggest search issue
    Former-commit-id: 0d9ce0ba5cdedcd5ffa2fd15197e4ed27816d090

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/search/services/search.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/search/services/search.client.service.js
@@ -161,7 +161,7 @@ angular.module('search').factory('SearchService', ['$resource', 'UtilService', '
                 cache: false,
                 isArray: false,
                 transformResponse: function (data, headerGetter) {
-                    if (Service.validateSolrData(data)) {
+                    if (Service.validateSolrData(JSON.parse(data))) {
                         var searchObj = JSON.parse(data);
                         return searchObj;
                     }
@@ -185,7 +185,7 @@ angular.module('search').factory('SearchService', ['$resource', 'UtilService', '
                 cache: false,
                 isArray: false,
                 transformResponse: function (data, headerGetter) {
-                    if (Service.validateSolrData(data)) {
+                    if (Service.validateSolrData(JSON.parse(data))) {
                         var searchObj = JSON.parse(data);
                         return searchObj;
                     }
@@ -219,12 +219,14 @@ angular.module('search').factory('SearchService', ['$resource', 'UtilService', '
             //            if (0 != responseHeader.status) {
             //                return false;
             //            }
+            /*
             if (Util.isEmpty(data.responseHeader.params)) {
                 return false;
             }
             if (Util.isEmpty(data.responseHeader.params.q)) {
                 return false;
             }
+            */
 
             if (Util.isEmpty(data.response.numFound) || Util.isEmpty(data.response.start)) {
                 return false;
