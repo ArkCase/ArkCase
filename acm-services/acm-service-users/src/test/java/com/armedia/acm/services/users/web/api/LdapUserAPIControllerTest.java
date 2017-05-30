534d03d155c20c66950d02671586f752b4eb4bd2
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-05-30 14:58:36 +0200

    Mockito test added
    Former-commit-id: d63a4ee21aea1f4518769816525be584ac8ee5d4

--- acm-services/acm-service-users/src/test/java/com/armedia/acm/services/users/web/api/LdapUserAPIControllerTest.java
+++ acm-services/acm-service-users/src/test/java/com/armedia/acm/services/users/web/api/LdapUserAPIControllerTest.java
@@ -1,146 +1,92 @@
 package com.armedia.acm.services.users.web.api;
 
+import com.armedia.acm.core.exceptions.AcmAppErrorJsonMsg;
 import com.armedia.acm.services.users.dao.ldap.UserDao;
 import com.armedia.acm.services.users.model.AcmUser;
+import com.armedia.acm.services.users.model.ldap.AcmLdapActionFailedException;
+import com.armedia.acm.services.users.service.AcmUserEventPublisher;
 import com.armedia.acm.services.users.service.ldap.LdapUserService;
 import org.easymock.EasyMockSupport;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
-import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-        "classpath:/spring/spring-web-acm-web.xml",
-        "classpath:/spring/spring-config-user-service-test-dummy-beans.xml"
-})
+@RunWith(MockitoJUnitRunner.class)
 public class LdapUserAPIControllerTest extends EasyMockSupport
 {
-
     private Logger LOG = LoggerFactory.getLogger(getClass());
 
     private MockMvc mockMvc;
-    private LdapUserAPIController unit;
-    private Authentication mockAuthentication;
+
+    @Mock
     private UserDao mockUserDao;
+
+    @Mock
+    private AcmUserEventPublisher mockUserEventPublisher;
+
+    @Mock
     private LdapUserService mockLdapUserService;
 
-    @Autowired
-    private ExceptionHandlerExceptionResolver exceptionResolver;
+    @InjectMocks
+    @Spy
+    LdapUserAPIController ldapUserAPIController;
+
 
     @Before
-    public void setUp() throws Exception
+    public void setUp()
     {
-        setUnit(new LdapUserAPIController());
-        setMockMvc(MockMvcBuilders.standaloneSetup(getUnit()).setHandlerExceptionResolvers(getExceptionResolver()).build());
-        setMockAuthentication(createMock(Authentication.class));
-        setMockUserDao(createMock(UserDao.class));
-        setMockLdapUserService(createMock(LdapUserService.class));
+        mockMvc = MockMvcBuilders.standaloneSetup(ldapUserAPIController).build();
+        mockLdapUserService.setUserDao(mockUserDao);
     }
 
     @Test
     public void removeLdapUserTest() throws Exception
     {
-        String directory = "directory";
+        String directory = "armedia";
         AcmUser user = new AcmUser();
+
         user.setUserId("test-user");
         user.setUserState("TEST");
         user.setFirstName("First Name");
         user.setLastName("Last Name");
 
-        expect(getMockUserDao().findByUserId(user.getUserId())).andReturn(user);
-        expect(mockLdapUserService.removeLdapUser(user.getUserId(), directory)).andReturn(null);
+        mockBehaviour(user);
 
-        expect(getMockAuthentication().getName()).andReturn("user");
-
-        replayAll();
-
-        MvcResult result = getMockMvc().perform(
+        MvcResult result = mockMvc.perform(
                 delete("/api/v1/ldap/" + directory + "/users/" + user.getUserId())
                         .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .principal(getMockAuthentication()))
-                .andReturn();
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
 
         LOG.info("Results: " + result.getResponse().getContentAsString());
-
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+
+        verify(mockLdapUserService, times(1)).removeLdapUser(anyString(), anyString());
+        verifyAll();
+
     }
 
-    public MockMvc getMockMvc()
+    private void mockBehaviour(AcmUser user) throws AcmLdapActionFailedException, AcmAppErrorJsonMsg
     {
-        return mockMvc;
+        when(mockLdapUserService.getUserDao()).thenReturn(mockUserDao);
+        when(mockUserDao.findByUserId(anyString())).thenReturn(user);
+        when(mockLdapUserService.removeLdapUser(anyString(), anyString())).thenReturn(user);
+        doNothing().when(ldapUserAPIController).checkIfLdapManagementIsAllowed(anyString());
     }
-
-    public void setMockMvc(MockMvc mockMvc)
-    {
-        this.mockMvc = mockMvc;
-    }
-
-    public LdapUserAPIController getUnit()
-    {
-        return unit;
-    }
-
-    public void setUnit(LdapUserAPIController unit)
-    {
-        this.unit = unit;
-    }
-
-    public Authentication getMockAuthentication()
-    {
-        return mockAuthentication;
-    }
-
-    public void setMockAuthentication(Authentication mockAuthentication)
-    {
-        this.mockAuthentication = mockAuthentication;
-    }
-
-    public ExceptionHandlerExceptionResolver getExceptionResolver()
-    {
-        return exceptionResolver;
-    }
-
-    public void setExceptionResolver(
-            ExceptionHandlerExceptionResolver exceptionResolver)
-    {
-        this.exceptionResolver = exceptionResolver;
-    }
-
-    public UserDao getMockUserDao()
-    {
-        return mockUserDao;
-    }
-
-    public void setMockUserDao(UserDao mockUserDao)
-    {
-        this.mockUserDao = mockUserDao;
-    }
-
-    public LdapUserService getMockLdapUserService()
-    {
-        return mockLdapUserService;
-    }
-
-    public void setMockLdapUserService(LdapUserService mockLdapUserService)
-    {
-        this.mockLdapUserService = mockLdapUserService;
-    }
-
 }
