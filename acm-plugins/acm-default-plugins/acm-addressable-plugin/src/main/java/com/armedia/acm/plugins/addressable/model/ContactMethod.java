bbc53674fd762f8dc1c0f6244118436d70f87560
Petar Ilin <petar.ilin@armedia.com>
2017-05-31 18:49:56 +0200

    Default implementations added to PostalAddress and ContactMethod classes (@JsonTypeInfo)
    Former-commit-id: 03e75daf1c3a8300d0f6c803206b0fe3c0f4cb17

--- acm-plugins/acm-default-plugins/acm-addressable-plugin/src/main/java/com/armedia/acm/plugins/addressable/model/ContactMethod.java
+++ acm-plugins/acm-default-plugins/acm-addressable-plugin/src/main/java/com/armedia/acm/plugins/addressable/model/ContactMethod.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "acm_contact_method")
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className", defaultImpl = ContactMethod.class)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "cm_class_name", discriminatorType = DiscriminatorType.STRING)
 @DiscriminatorValue("com.armedia.acm.plugins.addressable.model.ContactMethod")
