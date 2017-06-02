c89e35395a415002c38ea6dd43bf83c5994afac6
Manoj Dhungana <manoj.dhungana@armedia.com>
2017-06-02 10:26:46 -0400

    Updated pipelien IT
    Former-commit-id: 1993515050913e20033db18269ea31de8b34bd94

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/test/java/com/armedia/acm/plugins/person/OrganizationPipelineIT.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/test/java/com/armedia/acm/plugins/person/OrganizationPipelineIT.java
@@ -1,6 +1,10 @@
 package com.armedia.acm.plugins.person;
 
 import com.armedia.acm.data.AuditPropertyEntityAdapter;
+import com.armedia.acm.plugins.addressable.model.ContactMethod;
+import com.armedia.acm.plugins.addressable.model.PostalAddress;
+import com.armedia.acm.plugins.person.dao.OrganizationDao;
+import com.armedia.acm.plugins.person.model.Identification;
 import com.armedia.acm.plugins.person.model.Organization;
 import com.armedia.acm.plugins.person.service.OrganizationServiceImpl;
 import org.junit.Before;
@@ -18,7 +22,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -45,6 +51,9 @@ public class OrganizationPipelineIT
     @Autowired
     private OrganizationServiceImpl organizationService;
 
+    @Autowired
+    private OrganizationDao organizationDao;
+
     private Organization organization = new Organization();
 
     private Logger log = LoggerFactory.getLogger(getClass());
@@ -67,16 +76,58 @@ public class OrganizationPipelineIT
     public void saveOrganization() throws Exception
     {
 
-        organization.setOrganizationId(12345L);
-        organization.setCreator("auditUser");
+        Organization org = new Organization();
+
+        org.setOrganizationType("sample");
+        org.setOrganizationValue("tech net");
+
+
+        Identification i1 = new Identification();
+        i1.setIdentificationIssuer("issuer1");
+        i1.setIdentificationNumber("131312312");
+        i1.setIdentificationType("VISA");
+        i1.setIdentificationYearIssued(new Date());
+
+        ContactMethod cm1 = new ContactMethod();
+        cm1.setType("mobile");
+        cm1.setValue("23123123");
+
+        ContactMethod cm2 = new ContactMethod();
+        cm2.setType("mobile");
+        cm2.setValue("23123123");
+
+        PostalAddress pa1 = new PostalAddress();
+        pa1.setCity("Amaurot");
+        pa1.setCountry("Utopia");
+        pa1.setType("type");
+
+        PostalAddress pa2 = new PostalAddress();
+        pa2.setCity("Atlanta");
+        pa2.setCountry("Georgia");
+        pa2.setType("type");
+
+        org.getIdentifications().add(i1);
+        org.getContactMethods().add(cm1);
+        org.getContactMethods().add(cm2);
+        org.getAddresses().add(pa1);
+        org.getAddresses().add(pa2);
 
         Authentication auth = new UsernamePasswordAuthenticationToken("testUser", "testUser");
 
-        Organization saved = organizationService.saveOrganization(organization, auth, "ipaddress");
+        Organization saved = organizationService.saveOrganization(org, auth, "ipaddress");
 
         entityManager.flush();
 
         assertNotNull(saved.getOrganizationId());
+
+        saved = organizationDao.find(saved.getOrganizationId());
+        assertEquals(1, saved.getIdentifications().size());
+        assertEquals(2, saved.getContactMethods().size());
+        assertEquals(2, saved.getAddresses().size());
+
+        assertEquals("com.armedia.acm.plugins.person.model.Organization", saved.getClassName());
+
+        assertEquals("sample", org.getOrganizationType());
 
         log.info("New organization id: " + saved.getOrganizationId());
     }
