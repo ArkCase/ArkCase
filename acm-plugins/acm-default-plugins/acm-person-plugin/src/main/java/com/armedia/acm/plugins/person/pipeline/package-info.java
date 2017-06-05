f3ef057b25e9641522daa22a334220afea5bf636
Manoj Dhungana <manoj.dhungana@armedia.com>
2017-06-05 12:20:06 -0400

    Remove package-info java files
    Former-commit-id: 5fdee34431ac121866f9af6b59a1c847e3489a7f

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/pipeline/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Pipeline context implementation for organization.
- */
-package com.armedia.acm.plugins.person.pipeline;
--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/pipeline/postsave/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Post-save pipeline handler implementations go here.
- */
-package com.armedia.acm.plugins.person.pipeline.postsave;
