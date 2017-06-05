ebd7b07a3d689d5a84213e98ec0c17ce83028626
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-06-05 16:17:45 +0200

    todo's removed, formatter fixed
    Former-commit-id: ea2f7ed2aa8e589920764236224758defdc8f665

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/model/event/LdapGroupDeletedEvent.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/model/event/LdapGroupDeletedEvent.java
@@ -4,7 +4,6 @@ import com.armedia.acm.services.users.model.group.AcmGroup;
 
 public class LdapGroupDeletedEvent extends GroupPersistenceEvent
 {
-    //todo Add this static string to ACM conf project
     private static final String EVENT_TYPE = "com.armedia.acm.ldapGroup.deleted";
 
     public LdapGroupDeletedEvent(AcmGroup source, String groupName)
