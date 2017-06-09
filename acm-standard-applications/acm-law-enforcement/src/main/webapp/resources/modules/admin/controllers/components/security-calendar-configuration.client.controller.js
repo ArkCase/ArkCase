961769383be9158b346dc2701985a9ea31b852ba
Vladimir Radeski <vladimir.radeski@armedia.com>
2017-06-09 14:01:45 +0200

    AFDP-3748 - remove password required for already configured object type calendar integration and other fixes
    Former-commit-id: 83de06c13a3778eeb559a3cbdf0e376e2b9d307e

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/security-calendar-configuration.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/security-calendar-configuration.client.controller.js
@@ -3,8 +3,13 @@
 angular.module('admin').controller('Admin.SecurityCalendarConfigurationController', ['$scope', 'Admin.CalendarConfigurationService', 'MessageService', 'ConfigService',
     function($scope, CalendarConfigurationService, MessageService, ConfigService) {
 
-        $scope.calendarConfigDataModel = {};
+        $scope.isLoading = false;
+        $scope.calendarConfigDataModel = {
+            configurationsByType: {}
+        };
         $scope.configurableObjectTypes = [];
+        $scope.passwordRequirementByObjectType = {};
+        $scope.validEmailsByObjectType = {};
         $scope.purgeSelectOptions = [
             {
                 value: 'RETAIN_INDEFINITELY',
@@ -20,38 +25,48 @@ angular.module('admin').controller('Admin.SecurityCalendarConfigurationControlle
             }
         ];
 
-        $scope.validEmailsByObjectType = {};
-        $scope.noCalendarIntegrationEnabled = true;
-
-        /*Check if integration is enabled for at least one configurableObjectType to enable save button*/
-        var checkIfCalendarIntegrationEnabled = function() {
-            for(var i = 0; i <= $scope.configurableObjectTypes.length; i++) {
-                if($scope.calendarConfigDataModel.configurationsByType[$scope.configurableObjectTypes[i].id].integrationEnabled) {
-                    $scope.noCalendarIntegrationEnabled = false;
-                    break;
-                } else {
-                    $scope.noCalendarIntegrationEnabled = true;
+        var processCalendarConfigData = function(calendarAdminConfig) {
+            _.forEach($scope.configurableObjectTypes, function(configurableObjectType) {
+                // Check if there is Admin Calendar Configuration available for each object type defined in the component config
+                if(calendarAdminConfig.data.configurationsByType[configurableObjectType.id]) {
+                    $scope.calendarConfigDataModel.configurationsByType[configurableObjectType.id] = calendarAdminConfig.data.configurationsByType[configurableObjectType.id];
                 }
-            }
+                /*Check if password required by object type*/
+                if(!calendarAdminConfig.data.configurationsByType[configurableObjectType.id].systemEmail) {
+                    $scope.passwordRequirementByObjectType[configurableObjectType.id] = true;
+                }
+            });
+
         };
 
-        /*Get component config and current calendar configuration*/
-        ConfigService.getComponentConfig('admin', 'securityCalendarConfig').then(function(res) {
-            $scope.configurableObjectTypes = res.configurableObjectTypes;
-            CalendarConfigurationService.getCurrentCalendarConfiguration().then(function(res) {
-                $scope.calendarConfigDataModel = res.data;
-                checkIfCalendarIntegrationEnabled();
+        /*Get component config and get current admin calendar configuration for each object type defined in the component config*/
+        var getConfigurationData = function() {
+            $scope.isLoading = true;
+            ConfigService.getComponentConfig('admin', 'securityCalendarConfig').then(function(componentConfigRes) {
+                CalendarConfigurationService.getCurrentCalendarConfiguration().then(function(calendarAdminConfigRes) {
+                    $scope.isLoading = false;
+                    $scope.configurableObjectTypes = componentConfigRes.configurableObjectTypes;
+                    processCalendarConfigData(calendarAdminConfigRes);
+                }, function(err) {
+                    $scope.isLoading = false;
+                    MessageService.errorAction();
+                });
+            }, function(err) {
+                $scope.isLoading = false;
+                MessageService.errorAction();
             });
-        });
+        };
 
         /*Perform validation of the email*/
         $scope.validateEmail = function(systemEmail, password, configurableObjectType) {
+            $scope.isLoading = true;
             var emailCredentials = {
                 email: systemEmail,
                 password: password
             };
 
             CalendarConfigurationService.validateCalendarConfigurationSystemEmail(emailCredentials).then(function(res) {
+                $scope.isLoading = false;
                 MessageService.succsessAction();
                 if(res.data) {
                     $scope.validEmailsByObjectType[configurableObjectType.id] = 'VALID';
@@ -59,31 +74,34 @@ angular.module('admin').controller('Admin.SecurityCalendarConfigurationControlle
                     $scope.validEmailsByObjectType[configurableObjectType.id] = 'NOT_VALID';
                 }
             }, function(err) {
+                $scope.isLoading = false;
                 MessageService.errorAction();
                 $scope.validEmailsByObjectType[configurableObjectType.id] = 'NOT_VALID';
             });
         };
 
-        $scope.integrationEnabledChanged = function() {
-            checkIfCalendarIntegrationEnabled();
-        };
-
-        /*Remove success/error validation message when email input is changed*/
         $scope.systemEmailInputChanged = function(configurableObjectType) {
+            /*Remove success/error validation message when email input is changed*/
             if($scope.validEmailsByObjectType[configurableObjectType.id]) {
                 $scope.validEmailsByObjectType[configurableObjectType.id] = null;
             }
+            $scope.passwordRequirementByObjectType[configurableObjectType.id] = true;
         };
 
         $scope.saveChanges = function() {
+            $scope.isLoading = true;
             CalendarConfigurationService.saveCalendarConfiguration($scope.calendarConfigDataModel)
                 .then(function(res) {
+                    $scope.isLoading = false;
                     // remove all success/error validation messages
                     $scope.validEmailsByObjectType = {};
                     MessageService.succsessAction();
                 }, function(err) {
+                    $scope.isLoading = false;
                     MessageService.errorAction();
                 });
         };
+
+        getConfigurationData();
     }
 ]);
