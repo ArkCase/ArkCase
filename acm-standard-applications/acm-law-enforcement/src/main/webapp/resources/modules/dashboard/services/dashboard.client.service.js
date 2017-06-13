295889909201bd487ad02ec120123e94a9f28abf
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-06-13 14:45:04 +0200

    missing typo
    Former-commit-id: 429e20f85aaaeb14bc61d33a1bd15ab3be3a8317

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/services/dashboard.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/services/dashboard.client.service.js
@@ -53,7 +53,7 @@ angular.module('dashboard').factory('Dashboard.DashboardService', ['$resource',
             queryMyComplaints: {
                 method: 'GET',
                 url: 'api/v1/plugin/search/advancedSearch?q=(assignee_id_lcs\\::userId+' +
-                'OR+(assignee_id_lcs\\:""+AND+assignee_group_id_lcs\\:userGroupList))+' +
+                'OR+(assignee_id_lcs\\:""+AND+assignee_group_id_lcs\\::userGroupList))+' +
                 'AND+object_type_s\\:COMPLAINT+' +
                 'AND+NOT+status_lcs\\:CLOSED&start=:startWith&n=:pageSize&s=:sortBy :sortDir',
                 isArray: false,
@@ -62,7 +62,7 @@ angular.module('dashboard').factory('Dashboard.DashboardService', ['$resource',
             queryMyCases: {
                 method: 'GET',
                 url: 'api/v1/plugin/search/advancedSearch?q=(assignee_id_lcs\\::userId+' +
-                'OR+(assignee_id_lcs\\:""+AND+assignee_group_id_lcs\\:userGroupList))+' +
+                'OR+(assignee_id_lcs\\:""+AND+assignee_group_id_lcs\\::userGroupList))+' +
                 'AND+object_type_s\\:CASE_FILE+' +
                 'AND+NOT+status_lcs\\:CLOSED&start=:startWith&n=:pageSize&s=:sortBy :sortDir',
                 isArray: false,
