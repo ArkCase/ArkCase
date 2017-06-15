cdd4d287b7a3c48c90d61bca95f53a7d59ef7594
Ivo Shurbanovski <ivo.shurbanovski@armedia.com>
2017-06-15 19:47:27 +0200

    SBI-1873 Clone LDAP user frontend functionality
    Former-commit-id: 14b05e99bd1c37eb6d2e938c016dcb108c089f3d

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/services/security.ldap-user-management.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/services/security.ldap-user-management.client.service.js
@@ -3,31 +3,46 @@
 angular.module('admin').factory('Admin.LdapUserManagementService', ['$resource', '$http', '$q',
     function ($resource, $http, $q) {
         return ({
-            queryUsers: queryUsers,
+            queryGroupsByDirectory: queryGroupsByDirectory,
             addGroupsToUser: addGroupsToUser,
-            removeGroupsFromUser: removeGroupsFromUser
+            removeGroupsFromUser: removeGroupsFromUser,
+            cloneUser: cloneUser
         });
 
-        function queryUsers() {
+        function queryGroupsByDirectory(directory) {
             return $http({
                 method: 'GET',
-                url: 'api/latest/plugin/admin/user/management/users'
+                url: 'api/latest/users/directory/groups/get?directory=' + directory
             });
         };
 
-        function addGroupsToUser(user, groups) {
-            var url = 'api/latest/plugin/admin/' + user + '/management/'+ groups.join();
+        function addGroupsToUser(user, groups, directory) {
+            var url = 'api/latest/ldap/' + directory + '/manage/' + user +'/groups/add';
+            return $http({
+                method: 'PUT',
+                url: url,
+                data: groups
+            });
+        };
+
+        function removeGroupsFromUser(user, groups, directory) {
+            var url = 'api/latest/ldap/' + directory + '/manage/' + user +'/groups/remove';
+            return $http({
+                method: 'PUT',
+                url: url,
+                data: groups
+            });
+        };
+
+        function cloneUser(user) {
+            var url = 'api/latest/ldap/' + user.selectedUser.directory + '/users/' + user.selectedUser.key;
             return $http({
                 method: 'POST',
-                url: url
-            });
-        };
-
-        function removeGroupsFromUser(user, groups) {
-            var url = 'api/latest/plugin/admin/' + user + '/management/'+ groups.join();
-            return $http({
-                method: 'DELETE',
-                url: url
+                url: url,
+                data: {
+                    acmUser: user.acmUser,
+                    password: user.password
+                }
             });
         };
     }
