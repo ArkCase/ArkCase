f5894650332ed4297c3d60470b4850a2a78a89a5
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-06-16 13:46:20 +0200

    Both Jira ticket, AFDP-3821 Deleting assignee from participants and ticket, AFDP- 3794 Deleting owning groups from participant will be handled.
    Former-commit-id: 2f3dac36d89e17f61b40795738ecf29053ef4624

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
@@ -197,7 +197,18 @@ angular.module('directives').directive('coreParticipants', ['$stateParams', '$q'
                 };
 
                 scope.deleteRow = function (rowEntity) {
-                    gridHelper.deleteRow(rowEntity);
+                    var typeOwningGroup = "owning group";
+                    var typeAssignee = "assignee";
+
+                    if(rowEntity.participantType == typeOwningGroup) {
+                        MessageService.error($translate.instant("common.directive.coreParticipants.message.error.owninggroupDelete"));
+                    }
+                    else if(rowEntity.participantType == typeAssignee) {
+                        MessageService.error($translate.instant("common.directive.coreParticipants.message.error.assigneeDelete"));
+                    }
+                    else {
+                        gridHelper.deleteRow(rowEntity);
+                    }
                     var id = Util.goodMapValue(rowEntity, "id", 0);
                     if (0 < id) {    //do not need to call service when deleting a new row
                         saveObjectInfoAndRefresh();
