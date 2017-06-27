40fe76434f6763af4ed3e96769e225a37d3b9eac
Joseph McGrady <joseph.mcgrady@armedia.com>
2017-06-27 16:22:37 -0400

    default value fix
    Former-commit-id: 01e6a40423568bdf494191d31494ddd6c871afb7

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/PersonOrganizationAssociation.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/PersonOrganizationAssociation.java
@@ -77,7 +77,7 @@ public class PersonOrganizationAssociation implements Serializable, AcmEntity
 
     @Column(name = "cm_default_organization")
     @Convert(converter = BooleanToStringConverter.class)
-    private boolean defaultOrganization;
+    private boolean defaultOrganization = Boolean.FALSE;
 
     @Column(name = "cm_created", nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
