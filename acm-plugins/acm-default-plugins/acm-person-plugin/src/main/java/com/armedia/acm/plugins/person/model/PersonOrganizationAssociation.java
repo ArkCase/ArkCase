2c13e36582e12a1fbbd5c35c49c73f6e982c55a7
Joseph McGrady <joseph.mcgrady@armedia.com>
2017-06-27 17:39:10 -0400

    default value for primary contact
    Former-commit-id: d88e425f84f49570d38fde3ab229a5799c372dc2

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/PersonOrganizationAssociation.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/model/PersonOrganizationAssociation.java
@@ -73,7 +73,7 @@ public class PersonOrganizationAssociation implements Serializable, AcmEntity
 
     @Column(name = "cm_primary_contact")
     @Convert(converter = BooleanToStringConverter.class)
-    private boolean primaryContact;
+    private boolean primaryContact = Boolean.FALSE;
 
     @Column(name = "cm_default_organization")
     @Convert(converter = BooleanToStringConverter.class)
