ea2652846c5ce6840a0aa7011c93529cae944be2
Lazo Lazarev <lazo.lazarev@armedia.com>
2017-07-05 13:12:25 +0200

    AFDP-3754 Updated configuration of ITs.
    Former-commit-id: cc0cd5e3f99cc09d18d93d59c7fb672a23c4ed77

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/test/java/com/armedia/acm/plugins/casefile/service/MergeCaseFileServiceIT.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/test/java/com/armedia/acm/plugins/casefile/service/MergeCaseFileServiceIT.java
@@ -84,7 +84,8 @@ import java.util.UUID;
         "/spring/spring-library-service-data.xml",
         "/spring/spring-library-drools-rule-monitor.xml",
         "/spring/spring-library-object-lock.xml",
-        "/spring/spring-library-email.xml"})
+        "/spring/spring-library-email.xml",
+        "/spring/spring-library-email-smtp.xml"})
 @TransactionConfiguration(defaultRollback = true)
 public class MergeCaseFileServiceIT
 {
