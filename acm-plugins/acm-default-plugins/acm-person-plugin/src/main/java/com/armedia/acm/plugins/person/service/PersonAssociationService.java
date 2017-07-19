cc89f049bb46ae3b7d4315a34c4f80b427d2a8b6
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-07-19 17:34:40 +0200

    AFDP-3584
    Former-commit-id: a405eba5700b4be1cf19da58e33c42a66ec2bc3c

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/PersonAssociationService.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/PersonAssociationService.java
@@ -2,6 +2,7 @@ package com.armedia.acm.plugins.person.service;
 
 
 import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
 import com.armedia.acm.plugins.person.model.PersonAssociation;
 import org.springframework.security.core.Authentication;
 
@@ -9,4 +10,17 @@ public interface PersonAssociationService
 {
     PersonAssociation savePersonAssociation(PersonAssociation personAssociation, Authentication authentication)
             throws AcmCreateObjectFailedException;
+
+    /**
+     * get associations for person including solr documents for parent
+     *
+     * @param personId   Person id
+     * @param parentType Parent type
+     * @param start      used for paging, from which row to start
+     * @param limit      used for paging, how many rows to return
+     * @param sort       for which field sorting should be done, default is id
+     * @param auth       Authentication
+     * @return JSON String of solr response
+     */
+    String getPersonAssociations(Long personId, String parentType, int start, int limit, String sort, Authentication auth) throws AcmObjectNotFoundException;
 }
