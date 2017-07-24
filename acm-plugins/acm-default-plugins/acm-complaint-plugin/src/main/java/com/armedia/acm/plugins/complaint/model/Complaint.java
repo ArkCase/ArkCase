d6facbf1c46b051267c0c7cdff147fbef8894b6b
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-07-24 20:28:27 +0200

    AFDP-3582 fix delete person
    Former-commit-id: d959586db9c780e649f4b54d7b15c7a10ffa7b5c

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/model/Complaint.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/model/Complaint.java
@@ -1,4 +1,4 @@
-    package com.armedia.acm.plugins.complaint.model;
+package com.armedia.acm.plugins.complaint.model;
 
 import com.armedia.acm.core.AcmNotifiableEntity;
 import com.armedia.acm.core.AcmNotificationReceiver;
@@ -145,7 +145,7 @@ public class Complaint implements Serializable, AcmAssignedObject, AcmEntity, Ac
     @Transient
     private List<String> approvers;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumns({@JoinColumn(name = "cm_person_assoc_parent_id", referencedColumnName = "cm_complaint_id"),
             @JoinColumn(name = "cm_person_assoc_parent_type", referencedColumnName = "cm_object_type")})
     @OrderBy("created ASC")
