04c26f3684d2bc99f588a608093f0f851255d87b
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2017-07-28 13:45:47 +0200

    4041, 4042 - Prevent duplicate Person/Organization roles
    Also fixes the issue AFDP-4047
    
    
    Former-commit-id: ef61dfeeeec8cc19c3ce37f16f7ab8ceac06a4b4

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/OrganizationServiceImpl.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/service/OrganizationServiceImpl.java
@@ -1,15 +1,18 @@
 package com.armedia.acm.plugins.person.service;
 
+import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmUpdateObjectFailedException;
 import com.armedia.acm.plugins.person.dao.OrganizationDao;
 import com.armedia.acm.plugins.person.model.Organization;
+import com.armedia.acm.plugins.person.model.PersonOrganizationAssociation;
 import com.armedia.acm.plugins.person.pipeline.OrganizationPipelineContext;
 import com.armedia.acm.services.pipeline.PipelineManager;
 import com.armedia.acm.services.pipeline.exception.PipelineProcessException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-
 
 public class OrganizationServiceImpl implements OrganizationService
 {
@@ -34,20 +37,71 @@ public class OrganizationServiceImpl implements OrganizationService
 
     @Override
     @Transactional
-    public Organization saveOrganization(Organization organization, Authentication auth, String ipAddress) throws PipelineProcessException
+    public Organization saveOrganization(Organization organization, Authentication auth, String ipAddress)
+            throws PipelineProcessException, AcmCreateObjectFailedException, AcmUpdateObjectFailedException
     {
+        validatePersonAssociations(organization);
         OrganizationPipelineContext pipelineContext = new OrganizationPipelineContext();
         // populate the context
         pipelineContext.setNewOrganization(organization.getId() == null);
         pipelineContext.setAuthentication(auth);
         pipelineContext.setIpAddress(ipAddress);
 
-        return organizationPipelineManager.executeOperation(organization, pipelineContext, () ->
-        {
+        return organizationPipelineManager.executeOperation(organization, pipelineContext, () -> {
             Organization saved = organizationDao.save(organization);
             log.info("Organization saved '{}'", saved);
             return saved;
         });
+    }
+
+    /**
+     * Validates the {@link PersonOrganizationAssociation}.
+     *
+     * @param organization
+     *            the {@link Organization} to validate
+     * @throws AcmCreateObjectFailedException
+     *             when at least one of the {@link PersonOrganizationAssociation} is not valid.
+     * @throws AcmDuplicatePersonAssociationException
+     *             when at least one of the {@link PersonOrganizationAssociation} is not valid.
+     */
+    private void validatePersonAssociations(Organization organization) throws AcmCreateObjectFailedException, AcmUpdateObjectFailedException
+    {
+        for (PersonOrganizationAssociation association : organization.getPersonAssociations())
+        {
+            for (PersonOrganizationAssociation otherAssociation : organization.getPersonAssociations())
+            {
+                if (!association.equals(otherAssociation))
+                {
+                    if (association.getPerson().getId().equals(otherAssociation.getPerson().getId()))
+                    {
+                        String errorMessage = null;
+                        if ((association.getOrganizationToPersonAssociationType() == null)
+                                || (association.getPersonToOrganizationAssociationType() == null))
+                        {
+                            errorMessage = "Person to organization association must have a type";
+                        }
+
+                        if ((errorMessage == null) && (association.getPersonToOrganizationAssociationType()
+                                .equals(otherAssociation.getPersonToOrganizationAssociationType())))
+                        {
+                            errorMessage = "Duplicate person to organization relation type";
+                        }
+
+                        if (errorMessage != null)
+                        {
+                            if (organization.getId() == null)
+                            {
+                                throw new AcmCreateObjectFailedException("Organization", errorMessage, null);
+                            }
+                            else
+                            {
+                                throw new AcmUpdateObjectFailedException("Organization", organization.getId(), errorMessage, null);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -59,8 +113,8 @@ public class OrganizationServiceImpl implements OrganizationService
     private Organization prepareNewOrg(String companyName, String userId)
     {
         Organization org = new Organization();
-        //possible org types
-        //complaint.organizationTypes=Non-profit=Non-profit,Government=Government,Corporation=Corporation
+        // possible org types
+        // complaint.organizationTypes=Non-profit=Non-profit,Government=Government,Corporation=Corporation
         org.setOrganizationType("Corporation");
         org.setCreator(userId);
         org.setModifier(userId);
