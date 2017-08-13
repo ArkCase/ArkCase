15eb69a3358cf11d488f6457db40a3f6323f03d3
Lazo Lazarev <lazo.lazarev@armedia.com>
2017-08-13 21:27:00 +0200

    AFDP-4015 Updated documentation.
    Former-commit-id: 8ceee0bfde8c48532046c7b3190b92f0fc9d0eca

--- acm-services/acm-service-calendar/src/main/java/com/armedia/acm/calendar/config/service/CalendarAdminService.java
+++ acm-services/acm-service-calendar/src/main/java/com/armedia/acm/calendar/config/service/CalendarAdminService.java
@@ -1,5 +1,7 @@
 package com.armedia.acm.calendar.config.service;
 
+import com.armedia.acm.calendar.service.CalendarService;
+
 /**
  * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Mar 9, 2017
  *
@@ -9,6 +11,17 @@ public interface CalendarAdminService
 
     CalendarConfigurationsByObjectType readConfiguration(boolean includePassword) throws CalendarConfigurationException;
 
+    /**
+     * Upon successful storage of the configuration, the implementation is required to raise
+     * <code>CalendarConfigurationEvent</code> to inform the implementations of <code>CalendarService</code> that the
+     * configuration has been updated.
+     *
+     * @param configuration
+     * @throws CalendarConfigurationException
+     *
+     * @see {@link CalendarService}
+     * @see CalendarConfigurationEvent
+     */
     void writeConfiguration(CalendarConfigurationsByObjectType configuration) throws CalendarConfigurationException;
 
     <CCE extends CalendarConfigurationException> CalendarConfigurationExceptionMapper<CCE> getExceptionMapper(CCE e);
