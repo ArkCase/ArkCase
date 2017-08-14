19cf5ff21ca0eb47ed0779fc1688e1ca2ba3dc23
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-08-14 12:16:18 +0200

    AFDP-3896 AFDP-3895
    Former-commit-id: 9f882f83ae69b21e923912194f5e24d93176d310

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/organizations/controllers/components/organization-actions.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/organizations/controllers/components/organization-actions.client.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('organizations').controller('Organizations.ActionsController', ['$scope', '$state', '$stateParams', '$q'
     , 'UtilService', 'ConfigService', 'ObjectService', 'Authentication', 'Object.LookupService', 'Organization.LookupService'
-    , 'Object.SubscriptionService', 'Organization.InfoService', 'Helper.ObjectBrowserService', 'Object.ModelService', 'Profile.UserInfoService'
+    , 'Object.SubscriptionService', 'Organization.InfoService', 'Helper.ObjectBrowserService', 'Object.ModelService', 'Profile.UserInfoService', '$translate'
     , function ($scope, $state, $stateParams, $q
         , Util, ConfigService, ObjectService, Authentication, ObjectLookupService, OrganizationLookupService
-        , ObjectSubscriptionService, OrganizationInfoService, HelperObjectBrowserService, ObjectModelService, UserInfoService) {
+        , ObjectSubscriptionService, OrganizationInfoService, HelperObjectBrowserService, ObjectModelService, UserInfoService, $translate) {
 
         new HelperObjectBrowserService.Component({
             scope: $scope
@@ -32,11 +32,13 @@ angular.module('organizations').controller('Organizations.ActionsController', ['
         };
 
         $scope.activate = function () {
-            console.log('button activate clicked');
+            $scope.objectInfo.status = 'ACTIVE';
+            saveObjectInfoAndRefresh();
         };
 
         $scope.deactivate = function () {
-            console.log('button deactivate clicked');
+            $scope.objectInfo.status = 'INACTIVE';
+            saveObjectInfoAndRefresh();
         };
 
         $scope.merge = function () {
@@ -46,5 +48,24 @@ angular.module('organizations').controller('Organizations.ActionsController', ['
         $scope.refresh = function () {
             $scope.$emit('report-object-refreshed', $stateParams.id);
         };
+
+        function saveObjectInfoAndRefresh() {
+            var promiseSaveInfo = Util.errorPromise($translate.instant("common.service.error.invalidData"));
+            if (OrganizationInfoService.validateOrganizationInfo($scope.objectInfo)) {
+                var objectInfo = Util.omitNg($scope.objectInfo);
+                promiseSaveInfo = OrganizationInfoService.saveOrganizationInfo(objectInfo);
+                promiseSaveInfo.then(
+                    function (objectInfo) {
+                        $scope.$emit("report-object-updated", objectInfo);
+                        return objectInfo;
+                    }
+                    , function (error) {
+                        $scope.$emit("report-object-update-failed", error);
+                        return error;
+                    }
+                );
+            }
+            return promiseSaveInfo;
+        }
     }
 ]);
