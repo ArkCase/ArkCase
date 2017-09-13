907b59aaf14fcdfa471a4d0932bb4601423dd2d3
jovan.ivanovski <jovan.ivanovski@armedia.com>
2017-09-13 10:33:51 +0200

    Fixed i18n for audit, cases, dashboard modulecomponents
    Former-commit-id: ecdcf2c0d5a8b40b660dbc09fc6285574626aecd

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/config/audit.client.routes.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/config/audit.client.routes.js
@@ -2,17 +2,20 @@
 
 angular.module('audit').config(['$stateProvider',
     function ($stateProvider) {
-        $stateProvider.
-        state('audit', {
-            url: '/audit',
-            templateUrl: 'modules/audit/views/audit.client.view.html',
-            resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('common');
-                    $translatePartialLoader.addPart('audit');
-                    return $translate.refresh();
-                }]
+        $stateProvider
+            .state('audit', {
+                url: '/audit',
+                templateUrl: 'modules/audit/views/audit.client.view.html',
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('common');
+                        $translatePartialLoader.addPart('audit');
+                        $translatePartialLoader.addPart('dashboard');
+                        return $translate.refresh();
+                    }]
             }
         });
     }
-]);
+]).run(['Helper.DashboardService', function (DashboardHelper) {
+    DashboardHelper.addLocales();
+}]);
