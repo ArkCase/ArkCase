f208ca18aa0dabfda5f5971c2ca77e985ee22952
Sharmilee Sivakumaran <sharmilee.sivakumaran@armedia.com>
2017-09-19 16:29:42 -0400

    AFDP-4142: Changing default_address to ManyToOne
    The Primary(Yes, No) was not appearing on adding/editing earlier.
    
    
    Former-commit-id: 8ef184c9c3054c0b4c1f63d996c7b840641d1a96

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-locations.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-locations.client.controller.js
@@ -111,6 +111,7 @@ angular.module('complaints').controller('Complaint.LocationsController', ['$scop
             params.address = address || {};
             params.isEdit = isEdit || false;
             params.isDefault = $scope.isDefault(address);
+            params.hideNoField = params.isDefault;
 
             var modalInstance = $modal.open({
                 animation: true,
