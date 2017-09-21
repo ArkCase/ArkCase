f0dcc8fa84621c50c562598d6af725062e0a9a68
Sharmilee Sivakumaran <sharmilee.sivakumaran@armedia.com>
2017-09-21 10:38:31 -0400

    Revert "Ldap group and subgroup names"
    This reverts commit 27195f3dd5c71c56961cd7f3775d4cef74f012b1 [formerly 42fce81cfb7a87db7d49f12ffe32281e05e605fb].
    
    
    Former-commit-id: a216743fc50f0a7929da93d76868f8136450afb9

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/group/LdapGroupService.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/group/LdapGroupService.java
@@ -15,6 +15,7 @@ import com.armedia.acm.services.users.service.ldap.LdapEntryTransformer;
 import com.armedia.acm.spring.SpringContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ldap.NameAlreadyBoundException;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.ldap.core.LdapTemplate;
@@ -36,11 +37,18 @@ public class LdapGroupService
     private Logger log = LoggerFactory.getLogger(getClass());
 
     @Transactional(rollbackFor = Exception.class)
-    public AcmGroup createLdapGroup(AcmGroup group, String directoryName) throws AcmLdapActionFailedException
+    public AcmGroup createLdapGroup(AcmGroup group, String directoryName) throws AcmLdapActionFailedException,AcmLdapActionFailedException
     {
         AcmLdapSyncConfig ldapSyncConfig = acmContextHolder.getAllBeansOfType(AcmLdapSyncConfig.class).
                 get(String.format("%s_sync", directoryName));
         String groupDN = buildDnForGroup(group.getName(), ldapSyncConfig);
+
+        AcmGroup existingGroup = getGroupDao().findByName(group.getName().toUpperCase());
+        if (existingGroup != null)
+        {
+            log.debug("Group with name:{} already exists!", group.getName());
+            throw new NameAlreadyBoundException(null);
+        }
 
         group.setName(group.getName().toUpperCase());
         group.setType(AcmGroupType.LDAP_GROUP.name());
@@ -79,6 +87,12 @@ public class LdapGroupService
 
         String groupDN = buildDnForGroup(group.getName(), ldapSyncConfig);
 
+        AcmGroup existingGroup = getGroupDao().findByName(group.getName().toUpperCase());
+        if (existingGroup != null)
+        {
+            log.debug("Group with name:{} already exists!", group.getName());
+            throw new NameAlreadyBoundException(null);
+        }
         AcmGroup parentGroup = getGroupDao().findByName(parentGroupName);
         log.debug("Found parent-group:{} for new LDAP sub-group:{}", parentGroup.getName(), group.getName());
 
