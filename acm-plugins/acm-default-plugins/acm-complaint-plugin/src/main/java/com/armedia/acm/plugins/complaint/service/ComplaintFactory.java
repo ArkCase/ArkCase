4e7dcabc949bfa8d611017855c6633e913007fc6
Marko Stojanoski <marko.stojanoski@armedia.com>
2017-09-25 11:04:36 +0200

    Refactored initiator and people from complaint frevvo form to use person picker.
    Former-commit-id: d95f2654c7cb4908eff85a7343af9549001a131a

--- acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/ComplaintFactory.java
+++ acm-plugins/acm-default-plugins/acm-complaint-plugin/src/main/java/com/armedia/acm/plugins/complaint/service/ComplaintFactory.java
@@ -3,6 +3,7 @@ package com.armedia.acm.plugins.complaint.service;
 import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
 import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
 import com.armedia.acm.data.AuditPropertyEntityAdapter;
+import com.armedia.acm.form.config.xml.PersonItem;
 import com.armedia.acm.frevvo.config.FrevvoFormFactory;
 import com.armedia.acm.frevvo.config.FrevvoFormName;
 import com.armedia.acm.plugins.addressable.model.ContactMethod;
@@ -22,6 +23,7 @@ import com.armedia.acm.plugins.complaint.model.complaint.xml.PeopleContact;
 import com.armedia.acm.plugins.complaint.model.complaint.xml.PeopleMainInformation;
 import com.armedia.acm.plugins.ecm.model.AcmContainer;
 import com.armedia.acm.plugins.ecm.service.EcmFileService;
+import com.armedia.acm.plugins.person.dao.PersonAssociationDao;
 import com.armedia.acm.plugins.person.dao.PersonDao;
 import com.armedia.acm.plugins.person.model.Organization;
 import com.armedia.acm.plugins.person.model.Person;
@@ -44,12 +46,14 @@ public class ComplaintFactory extends FrevvoFormFactory
     private static final String ANONYMOUS = "Anonymous";
     private transient final Logger log = LoggerFactory.getLogger(getClass());
     private PersonDao personDao;
+    private PersonAssociationDao personAssociationDao;
     private EcmFileService fileService;
     private AuditPropertyEntityAdapter auditPropertyEntityAdapter;
 
     public Complaint asAcmComplaint(ComplaintForm formComplaint)
     {
         Complaint retval = new Complaint();
+        List<PersonAssociation> paArray = new ArrayList<>();
 
         retval.setComplaintId(formComplaint.getComplaintId());
         retval.setComplaintNumber(formComplaint.getComplaintNumber());
@@ -73,46 +77,41 @@ public class ComplaintFactory extends FrevvoFormFactory
         retval.setTag(formComplaint.getComplaintTag());
         retval.setFrequency(formComplaint.getFrequency());
 
-        if (formComplaint.getLocation() != null)
-        {
-            retval.setLocation(formComplaint.getLocation().returnBase());
-        }
-
-        if (formComplaint.getInitiator() != null)
+        if (formComplaint.getInitiatorId() != null)
         {
             PersonAssociation pa = new PersonAssociation();
-            Person p = new Person();
+            Person initiator = getPersonDao().find(formComplaint.getInitiatorId());
 
-            if (null != formComplaint.getInitiator().getContactType() && "existingInitiator".equals(formComplaint.getInitiator().getContactType()))
+            if(initiator != null)
             {
-                if (null != formComplaint.getInitiator().getSearchResult() && null != formComplaint.getInitiator().getSearchResult().getId())
-                {
-                    p = getPersonDao().find(formComplaint.getInitiator().getSearchResult().getId());
-                }
+                pa.setPerson(initiator);
+                pa.setPersonType(formComplaint.getInitiatorType());
+                retval.setOriginator(pa);
+                paArray.add(pa);
             }
-
-            pa.setPerson(p);
-            retval.setOriginator(pa);
-
-            populatePerson(formComplaint.getInitiator().returnBase(), pa, p);
         }
 
-        if (formComplaint.getPeople() != null)
+        if (formComplaint.getPeople() != null && formComplaint.getPeople().size() > 0)
         {
-            for (Contact person : formComplaint.getPeople())
+            for(PersonItem personItem : formComplaint.getPeople())
             {
-                if (person.getMainInformation() != null)
-                {
-                    PersonAssociation pa = new PersonAssociation();
-                    Person p = new Person();
-                    pa.setPerson(p);
-                    retval.getPersonAssociations().add(pa);
+                Person person = getPersonDao().find(personItem.getId());
+                PersonAssociation personAssociation = (personItem.getPersonAssociationId() == null) ? new PersonAssociation() : getPersonAssociationDao().find(
+                        personItem.getPersonAssociationId());
 
-                    populatePerson(person.returnBase(), pa, p);
-                }
+                if(person == null)
+                    continue;
+
+                personAssociation.setPerson(person);
+                personAssociation.setPersonType(personItem.getPersonType());
+                paArray.add(personAssociation);
             }
-        }
 
+        }
+        if(paArray!= null && paArray.size() > 0)
+        {
+            retval.setPersonAssociations(paArray);
+        }
         return retval;
     }
 
@@ -142,37 +141,29 @@ public class ComplaintFactory extends FrevvoFormFactory
 
             if (complaint.getOriginator() != null && complaint.getOriginator().getPerson() != null)
             {
-                Contact contact = new InitiatorContact();
-                contact = populateFrevvoContact(contact, complaint.getOriginator(), complaint.getOriginator().getPerson());
-
-                complaintForm.setInitiator(contact);
+                complaintForm.setInitiatorId(complaint.getOriginator().getPerson().getId());
+                complaintForm.setInitiatorFullName(complaint.getOriginator().getPerson().getFullName());
+                complaintForm.setInitiatorType(complaint.getOriginator().getPersonType());
             }
 
             if (complaint.getPersonAssociations() != null && complaint.getPersonAssociations().size() > 0)
             {
-                Contact initiator = complaintForm.getInitiator();
-                List<Contact> contacts = new ArrayList<Contact>();
+                List<PersonItem> people = new ArrayList<>();
                 for (PersonAssociation personAssociation : complaint.getPersonAssociations())
                 {
-                    if (personAssociation.getPerson() != null)
+                    if (personAssociation.getPerson() != null && !personAssociation.getPersonType().equals("Initiator"))
                     {
-                        Contact contact = new PeopleContact();
-                        contact = populateFrevvoContact(contact, personAssociation, personAssociation.getPerson());
+                        PersonItem personItem = new PersonItem();
 
-                        boolean addContact = true;
-                        if (initiator != null && initiator.getId() != null && contact != null && contact.getId() != null && initiator.getId().equals(contact.getId()))
-                        {
-                            addContact = false;
-                        }
+                        personItem.setId(personAssociation.getPerson().getId());
+                        personItem.setValue(personAssociation.getPerson().getFullName());
+                        personItem.setPersonType(personAssociation.getPersonType());
+                        personItem.setPersonAssociationId(personAssociation.getId());
 
-                        if (addContact)
-                        {
-                            contacts.add(contact);
-                        }
+                        people.add(personItem);
                     }
                 }
-
-                complaintForm.setPeople(contacts);
+                complaintForm.setPeople(people);
             }
 
             try
@@ -204,207 +195,6 @@ public class ComplaintFactory extends FrevvoFormFactory
         return complaintForm;
     }
 
-    private Contact populateFrevvoContact(Contact contact, PersonAssociation personAssociation, Person person)
-    {
-        if (contact != null && person != null && personAssociation != null)
-        {
-            contact.setId(person.getId());
-            contact.setContactType(personAssociation.getPersonType());
-            contact.setNotes(personAssociation.getNotes());
-
-            // Populate main information
-            populateMainInformation(contact, person, personAssociation);
-
-            // Populate communication devices
-            populateCommunicationDevices(contact, person);
-
-            // Populate organizations
-            populateOrganizations(contact, person);
-
-            // Populate locations
-            populateLocations(contact, person);
-
-            // Populate alias
-            populateAlias(contact, person);
-        }
-
-        return contact;
-    }
-
-    private void populateMainInformation(Contact contact, Person person, PersonAssociation personAssociation)
-    {
-        MainInformation mainInformation = null;
-        if (contact instanceof InitiatorContact)
-        {
-            mainInformation = new InitiatorMainInformation();
-        } else if (contact instanceof PeopleContact)
-        {
-            mainInformation = new PeopleMainInformation();
-        }
-
-        if (mainInformation != null)
-        {
-            mainInformation.setTitle(person.getTitle());
-            mainInformation.setFirstName(person.getGivenName());
-            mainInformation.setLastName(person.getFamilyName());
-            mainInformation.setType(personAssociation.getPersonType());
-            mainInformation.setDescription(personAssociation.getPersonDescription());
-            if (personAssociation.getTags() != null)
-            {
-                for (String tag : personAssociation.getTags())
-                {
-                    if (ANONYMOUS.equals(tag))
-                    {
-                        mainInformation.setAnonymous("true");
-                        break;
-                    }
-                }
-            }
-        }
-
-        contact.setMainInformation(mainInformation);
-    }
-
-    private void populateCommunicationDevices(Contact contact, Person person)
-    {
-        if (person.getContactMethods() != null && person.getContactMethods().size() > 0)
-        {
-            List<ContactMethod> communicationDevices = new ArrayList<ContactMethod>();
-            for (ContactMethod contactMethod : person.getContactMethods())
-            {
-                if (contact instanceof InitiatorContact)
-                {
-                    ContactMethod c = new InitiatorContactMethod(contactMethod);
-                    communicationDevices.add(c);
-                } else if (contact instanceof PeopleContact)
-                {
-                    ContactMethod c = new PeopleContactMethod(contactMethod);
-                    communicationDevices.add(c);
-                }
-            }
-
-            contact.setCommunicationDevice(communicationDevices);
-        }
-    }
-
-    private void populateOrganizations(Contact contact, Person person)
-    {
-        if (person.getOrganizations() != null && person.getOrganizations().size() > 0)
-        {
-            List<Organization> organizations = new ArrayList<Organization>();
-            for (Organization organization : person.getOrganizations())
-            {
-                if (contact instanceof InitiatorContact)
-                {
-                    Organization o = new InitiatorOrganization(organization);
-                    organizations.add(o);
-                } else if (contact instanceof PeopleContact)
-                {
-                    Organization o = new PeopleOrganization(organization);
-                    organizations.add(o);
-                }
-            }
-
-            contact.setOrganization(organizations);
-        }
-    }
-
-    private void populateLocations(Contact contact, Person person)
-    {
-        if (person.getAddresses() != null && person.getAddresses().size() > 0)
-        {
-            List<PostalAddress> locations = new ArrayList<PostalAddress>();
-            for (PostalAddress postalAddress : person.getAddresses())
-            {
-                if (contact instanceof InitiatorContact)
-                {
-                    PostalAddress a = new InitiatorPostalAddress(postalAddress);
-                    locations.add(a);
-                } else if (contact instanceof PeopleContact)
-                {
-                    PostalAddress a = new PeoplePostalAddress(postalAddress);
-                    locations.add(a);
-                }
-            }
-
-            contact.setLocation(locations);
-        }
-    }
-
-    private void populateAlias(Contact contact, Person person)
-    {
-        if (person.getPersonAliases() != null && person.getPersonAliases().size() > 0)
-        {
-            PersonAlias personAlias = person.getPersonAliases().get(0);
-            if (contact instanceof InitiatorContact)
-            {
-                contact.setAlias(new InitiatorPersonAlias(personAlias));
-            } else if (contact instanceof PeopleContact)
-            {
-                contact.setAlias(new PeoplePersonAlias(personAlias));
-            }
-        }
-    }
-
-    private void populatePerson(Contact contact, PersonAssociation pa, Person p)
-    {
-        pa.setPersonDescription(contact.getMainInformation().getDescription());
-        pa.setPersonType(contact.getMainInformation().getType());
-        pa.setNotes(contact.getNotes());
-
-        if (null == p.getId())
-        {
-            p.setTitle(contact.getMainInformation().getTitle());
-            p.setGivenName(contact.getMainInformation().getFirstName());
-            p.setFamilyName(contact.getMainInformation().getLastName());
-
-            if (contact.getAlias() != null)
-            {
-                p.getPersonAliases().add(contact.getAlias().returnBase());
-            }
-
-            if (contact.getLocation() != null && !contact.getLocation().isEmpty())
-            {
-                List<PostalAddress> addresses = new ArrayList<PostalAddress>();
-                for (PostalAddress postalAddress : contact.getLocation())
-                {
-                    PostalAddress base = postalAddress.returnBase();
-                    addresses.add(base);
-                }
-                p.getAddresses().addAll(addresses);
-            }
-
-            if (contact.getOrganization() != null && !contact.getOrganization().isEmpty())
-            {
-                List<Organization> organizations = new ArrayList<Organization>();
-                for (Organization organization : contact.getOrganization())
-                {
-                    Organization base = organization.returnBase();
-                    organizations.add(base);
-                }
-                p.getOrganizations().addAll(organizations);
-            }
-
-            if (contact.getCommunicationDevice() != null && !contact.getCommunicationDevice().isEmpty())
-            {
-                List<ContactMethod> contactMethods = new ArrayList<ContactMethod>();
-                for (ContactMethod contactMethod : contact.getCommunicationDevice())
-                {
-                    ContactMethod base = contactMethod.returnBase();
-                    contactMethods.add(base);
-                }
-                p.getContactMethods().addAll(contactMethods);
-            }
-        }
-
-
-        if ("true".equalsIgnoreCase(contact.getMainInformation().getAnonymous()))
-        {
-            pa.getTags().add(ANONYMOUS);
-        }
-    }
-
-
     /**
      * @return the personDao
      */
@@ -419,6 +209,16 @@ public class ComplaintFactory extends FrevvoFormFactory
     public void setPersonDao(PersonDao personDao)
     {
         this.personDao = personDao;
+    }
+
+    public PersonAssociationDao getPersonAssociationDao()
+    {
+        return personAssociationDao;
+    }
+
+    public void setPersonAssociationDao(PersonAssociationDao personAssociationDao)
+    {
+        this.personAssociationDao = personAssociationDao;
     }
 
     public EcmFileService getFileService()
