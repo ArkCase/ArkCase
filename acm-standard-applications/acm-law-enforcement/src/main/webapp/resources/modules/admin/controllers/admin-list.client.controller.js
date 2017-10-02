98b9a2bc5b5c81af66475a709cc3d3ab78d00398
James Wu <jwu@armedia.com>
2017-10-02 13:19:45 -0400

    Complete localize rest of admin page
    Former-commit-id: 85f076e949fe9cbeab1cfdd133653f14d645232a

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/admin-list.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/admin-list.client.controller.js
@@ -1,13 +1,9 @@
 'use strict';
 
-angular.module('admin').controller('AdminListController', ['$scope', '$state', '$stateParams', '$translate'
+angular.module('admin').controller('AdminListController', ['$scope', '$state', '$stateParams', '$translate', '$timeout'
     , 'UtilService', 'ConfigService'
-    , 'ObjectService', 'Case.ListService', 'Case.InfoService', 'Helper.ObjectBrowserService'
-    , 'ServCommService', 'MessageService'
-    , function ($scope, $state, $stateParams, $translate
-        , Util, ConfigService
-        , ObjectService, CaseListService, CaseInfoService, HelperObjectBrowserService
-        , ServCommService, MessageService) {
+    , function ($scope, $state, $stateParams, $translate, $timeout
+        , Util, ConfigService) {
 
         ConfigService.getModuleConfig("admin").then(function (config) {
             $scope.treeConfig = config.tree;
@@ -18,14 +14,10 @@ angular.module('admin').controller('AdminListController', ['$scope', '$state', '
                 var tokens = Util.goodMapValue(typeDef, "type").split("/");
                 if (2 === tokens.length) {
                     count++;
-                    var nodeType = tokens[1];
-                    var nodeTitleLabel = Util.goodMapValue(typeDef, "label");
-                    //var nodeTitle = $translate.instant(Util.goodMapValue(typeDef, "label"));
                     $scope.treeData.docs.push({
                         nodeId: count
                         , nodeType: tokens[1]
-                        , nodeTitleLabel: nodeTitleLabel
-                        //, nodeTitle: nodeTitle
+                        , nodeTitleLabel: Util.goodMapValue(typeDef, "label")
                     });
                 }
             });
@@ -41,11 +33,8 @@ angular.module('admin').controller('AdminListController', ['$scope', '$state', '
             return config;
         });
 
-        $scope.treeControl = {};
-
         $scope.onLoad = function(start, n, sort, filters){
             return $scope.treeData.docs;
-            //return Util.goodMapValue($scope, "treeData.docs", []);
         };
 
         $scope.onSelect = function(selectedObject){
@@ -61,109 +50,6 @@ angular.module('admin').controller('AdminListController', ['$scope', '$state', '
                 });
             }
         };
-
-/*
-        $scope.treeConfig = {
-            "pageSize": 3,
-            "filters": [],
-            "sorters": [],
-            "nodeTypes": [
-                {
-                    "type": "prev",
-                    "icon": "fa fa-arrow-up"
-                },
-                {
-                    "type": "next",
-                    "icon": "fa fa-arrow-down"
-                },
-                {
-                    "type": "p/security",
-                    "label": "admin.security.title",
-                    "icon": "fa fa-wrench",
-                    "components": []
-                },
-                {
-                    "type": "p/security/access",
-                    "label": "admin.security.functionalAccessControl",
-                    "components": [
-                        "security.functional-access-control"
-                    ]
-                },
-                {
-                    "type": "p/security/ldap",
-                    "label": "admin.security.ldapConfig",
-                    "components": [
-                        "security.ldap-config"
-                    ]
-                },
-                {
-                    "type": "p/security/org",
-                    "label": "admin.security.organizationalHierarchy.title",
-                    "components": [
-                        "security.organizational-hierarchy"
-                    ]
-                },
-                {
-                    "type": "p/dashboard",
-                    "label": "admin.dashboard.title",
-                    "icon": "fa fa-wrench",
-                    "components": []
-                },
-                {
-                    "type": "p/dashboard/cfg",
-                    "label": "admin.dashboard.config",
-                    "components": [
-                        "dashboard.config"
-                    ]
-                },
-                {
-                    "type": "p/docMan",
-                    "label": "admin.documentManagement.title",
-                    "icon": "fa fa-wrench",
-                    "components": []
-                },
-                {
-                    "type": "p/docMan/cmis",
-                    "label": "admin.documentManagement.cmisConfiguration",
-                    "components": [
-                        "cmis-configuration"
-                    ]
-                }
-            ]
-        };
-
-        var page1 = [
-            {nodeId: 101, nodeType: "security", nodeTitle: 'security 101'},
-            {nodeId: 102, nodeType: "dashboard", nodeTitle: 'dashboard 102', nodeToolTip: ''},
-            {nodeId: 103, nodeType: "docMan", nodeTitle: 'docMan 103', nodeToolTip: 'docMan 103'}
-        ];
-        var page2 = [
-            {nodeId: 201, nodeType: "security", nodeTitle: 'Case 201', nodeToolTip: 'Case 201'},
-            {nodeId: 202, nodeType: "security", nodeTitle: 'Case 202', nodeToolTip: 'Case 202'},
-            {nodeId: 202, nodeType: "security", nodeTitle: 'Case 202', nodeToolTip: 'Case 202'}
-        ];
-        var page3 = [
-            {nodeId: 301, nodeType: "security", nodeTitle: 'Case 301', nodeToolTip: 'Case 301'}
-        ];
-
-        $scope.treeData = {};
-        $scope.treeData.docs = page1;
-        $scope.treeData.total = 3;
-
-        $scope.onLoad = function(start, n, sort, filters){
-            //query list of objects according to the parameters. Only consider 'start' here:
-            page1[0].nodeTitle = "xxxx";
-            if (0 == start) {
-                return page1;
-            } else if (2 == start) {
-                return page2;
-            } else if (4 == start) {
-                return page3;
-            } else {
-                return [];
-            }
-        };
-*/
 
     }
 ]);
