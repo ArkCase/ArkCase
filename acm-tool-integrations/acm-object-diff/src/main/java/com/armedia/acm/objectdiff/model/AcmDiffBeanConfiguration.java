53c9ee4c9df47fad7d7a87940860344de50e0e79
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-10-02 20:56:28 +0200

    Diff algorithm refactored
    Former-commit-id: 8e261cbf882ba67c71d63ef3473ee7dd338df428

--- acm-tool-integrations/acm-object-diff/src/main/java/com/armedia/acm/objectdiff/model/AcmDiffBeanConfiguration.java
+++ acm-tool-integrations/acm-object-diff/src/main/java/com/armedia/acm/objectdiff/model/AcmDiffBeanConfiguration.java
@@ -3,50 +3,61 @@ package com.armedia.acm.objectdiff.model;
 import java.io.Serializable;
 import java.util.List;
 
-public class AcmDiffBeanConfiguration implements Serializable {
+public class AcmDiffBeanConfiguration implements Serializable
+{
     private String className;
     private String name;
     private List<String> includeFields;
     private List<String> skipFields;
     private List<String> id;
 
-    public String getClassName() {
+    public String getClassName()
+    {
         return className;
     }
 
-    public void setClassName(String className) {
+    public void setClassName(String className)
+    {
         this.className = className;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public List<String> getIncludeFields() {
+    public List<String> getIncludeFields()
+    {
         return includeFields;
     }
 
-    public void setIncludeFields(List<String> includeFields) {
+    public void setIncludeFields(List<String> includeFields)
+    {
         this.includeFields = includeFields;
     }
 
-    public List<String> getSkipFields() {
+    public List<String> getSkipFields()
+    {
         return skipFields;
     }
 
-    public void setSkipFields(List<String> skipFields) {
+    public void setSkipFields(List<String> skipFields)
+    {
         this.skipFields = skipFields;
     }
 
-    public List<String> getId() {
+    public List<String> getId()
+    {
         return id;
     }
 
-    public void setId(List<String> id) {
+    public void setId(List<String> id)
+    {
         this.id = id;
     }
 }
