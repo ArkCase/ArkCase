eecde869c078ab4655da6d400a40f96b96e6a55a
ncuculova <nadica.cuculova@armedia.com>
2017-10-05 13:31:26 +0200

    Fix adding member to AdHoc group
    Former-commit-id: e864e87b19469e44704e8ea793155a2001f65a25

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/group/GroupServiceImpl.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/group/GroupServiceImpl.java
@@ -173,8 +173,6 @@ public class GroupServiceImpl implements GroupService
         }
         members = updateMembersWithDatabaseInfo(members);
         members.forEach(group::addUserMember);
-        members.forEach(member ->
-                userRoleService.saveValidUserRolesPerAddedUserGroups(member.getUserId(), new HashSet<>(Arrays.asList(group))));
         return group;
     }
 
