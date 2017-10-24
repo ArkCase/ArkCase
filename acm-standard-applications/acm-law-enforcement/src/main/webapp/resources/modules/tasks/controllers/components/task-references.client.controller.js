0b4310564baa40af978b4f211286ec28bd1c1026
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2017-10-25 00:36:40 +0200

    AFDP-3132
    Former-commit-id: 38d6fd03fbb34e1492f201a91cb8ce20265fba76

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-references.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-references.client.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('tasks').controller('Tasks.ReferencesController', ['$scope', '$stateParams'
     , 'UtilService', 'ConfigService', 'Task.InfoService', 'Helper.UiGridService', 'Helper.ObjectBrowserService'
-    , '$modal', 'Object.ReferenceService', 'ObjectService'
+    , '$modal', 'Object.ReferenceService', 'ObjectService', 'ObjectAssociation.Service'
     , function ($scope, $stateParams, Util, ConfigService, TaskInfoService, HelperUiGridService
-        , HelperObjectBrowserService, $modal, referenceService, ObjectService) {
+        , HelperObjectBrowserService, $modal, referenceService, ObjectService, ObjectAssociationService) {
 
         new HelperObjectBrowserService.Component({
             scope: $scope
@@ -25,7 +25,7 @@ angular.module('tasks').controller('Tasks.ReferencesController', ['$scope', '$st
 
         var onConfigRetrieved = function (config) {
             $scope.config = config;
-
+            gridHelper.addButton(config, "delete");
             gridHelper.setColumnDefs(config);
             gridHelper.setBasicOptions(config);
             gridHelper.disableGridScrolling(config);
@@ -34,21 +34,21 @@ angular.module('tasks').controller('Tasks.ReferencesController', ['$scope', '$st
         var onObjectInfoRetrieved = function (objectInfo) {
             $scope.objectInfo = objectInfo;
             $scope.gridOptions = $scope.gridOptions || {};
-            $scope.gridOptions.data = Util.goodArray($scope.objectInfo.references);
+            refreshGridData(objectInfo.taskId);
         };
 
         $scope.onClickObjLink = function (event, rowEntity, targetNameColumnClicked) {
             event.preventDefault();
 
-            var targetType = Util.goodMapValue(rowEntity, "targetType");
-            var targetId = Util.goodMapValue(rowEntity, "targetId");
-            var parentId = Util.goodMapValue(rowEntity, "parentId");
-            var parentType = Util.goodMapValue(rowEntity, "parentType");
-            var fileName = Util.goodMapValue(rowEntity, "targetName");
+            var targetType = Util.goodMapValue(rowEntity, "target_type_s");
+            var targetId = Util.goodMapValue(rowEntity, "target_id_s");
+            var parentId = Util.goodMapValue(rowEntity, "parent_id_s");
+            var parentType = Util.goodMapValue(rowEntity, "parent_type_s");
+            var fileName = Util.goodMapValue(rowEntity, "target_object.title_parseable");
 
-            if(targetType == ObjectService.ObjectTypes.FILE && targetNameColumnClicked){
+            if (targetType == ObjectService.ObjectTypes.FILE && targetNameColumnClicked) {
                 gridHelper.openObject(targetId, parentId, parentType, fileName);
-            }else{
+            } else {
                 gridHelper.showObject(targetType, targetId);
             }
 
@@ -58,9 +58,26 @@ angular.module('tasks').controller('Tasks.ReferencesController', ['$scope', '$st
         };
 
         ConfigService.getModuleConfig("tasks").then(function (moduleConfig) {
-        	$scope.modalConfig = _.find(moduleConfig.components, {id: "referenceSearchGrid"});
+            $scope.modalConfig = _.find(moduleConfig.components, {id: "referenceSearchGrid"});
             return moduleConfig;
         });
+
+        $scope.deleteRow = function (rowEntity) {
+            var id = Util.goodMapValue(rowEntity, "object_id_s", 0);
+            ObjectAssociationService.deleteAssociationInfo(id).then(function (data) {
+                //success
+                //remove it from the grid
+                _.remove($scope.gridOptions.data, function (row) {
+                    return row === rowEntity;
+                });
+            });
+        };
+
+        function refreshGridData(objectId) {
+            ObjectAssociationService.getObjectAssociations(objectId, ObjectService.ObjectTypes.TASK, null).then(function (response) {
+                $scope.gridOptions.data = response.response.docs;
+            });
+        }
 
         $scope.refresh = function () {
             $scope.$emit('report-object-refreshed', $stateParams.id);
@@ -92,25 +109,41 @@ angular.module('tasks').controller('Tasks.ReferencesController', ['$scope', '$st
             });
 
             modalInstance.result.then(function (chosenReference) {
-                if (chosenReference) {
-                    var reference = {};
-                    reference.referenceId = chosenReference.object_id_s;
-                    reference.referenceTitle = chosenReference.title_parseable;
-                    reference.referenceType = chosenReference.object_type_s;
-                    reference.referenceNumber = chosenReference.name;
-                    reference.referenceStatus = chosenReference.status_lcs;
-                    reference.parentId = $stateParams.id;
-                    reference.parentType = ObjectService.ObjectTypes.TASK;
-                    referenceService.addReferenceToTask(reference).then(
-                        function (objectSaved) {
-                        	$scope.refresh();
-                            return objectSaved;
-                        },
-                        function (error) {
-                        	return error;
-                        }
-                    );
-                    return;
+                var parent = $scope.objectInfo;
+                var target = chosenReference;
+                if (target) {
+                    var association = ObjectAssociationService.createAssociationInfo(
+                        parent.taskId,
+                        ObjectService.ObjectTypes.TASK,
+                        parent.title,
+                        parent.title,
+                        target.object_id_s,
+                        target.object_type_s,
+                        target.title_parseable,
+                        target.name,
+                        'REFERENCE',
+                        'REFERENCE');
+                    ObjectAssociationService.saveObjectAssociation(association).then(function (payload) {
+                        //success
+                        //append new entity as last item in the grid
+                        var rowEntity = {
+                            object_id_s: payload.id,
+                            target_object: {
+                                name: target.name,
+                                title_parseable: target.title_parseable,
+                                parent_ref_s: target.parent_ref_s,
+                                modified_date_tdt: target.modified_date_tdt,
+                                assignee_full_name_lcs: target.assignee_full_name_lcs,
+                                object_type_s: target.object_type_s,
+                                status_lcs: target.status_lcs
+                            },
+                            target_type_s: payload.targetType,
+                            target_id_s: payload.targetId
+                        };
+
+                        $scope.gridOptions.data.push(rowEntity);
+
+                    });
                 }
             }, function () {
                 // Cancel button was clicked.
