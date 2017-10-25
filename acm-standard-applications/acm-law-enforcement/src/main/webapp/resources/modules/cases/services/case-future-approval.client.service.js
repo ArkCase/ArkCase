b5014cada23feb9d8493cbecbcc2f1dede712de1
Marko Stojanoski <marko.stojanoski@armedia.com>
2017-10-25 17:09:38 +0200

    Approval Routing  backend / frontend support
    Former-commit-id: c966f100349e4e5ac90de0fc41c8bbd7df96e047

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/services/case-future-approval.client.service.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name services:Case.FutureApprovalService
+ *
+ * @description
+ *
+ *
+ * Case.FutureApprovalService provides functions for Case File Approval Routing
+ */
+angular.module('services').factory('Case.FutureApprovalService', ['$http', function ($http) {
+
+    var _getBuckslipFutureTasks = function (businessProcessId) {
+        return $http({
+            method: 'GET',
+            url: 'api/latest/plugin/task/getBuckslipFutureTasks/businessProcessId/' + businessProcessId
+        });
+    };
+
+    var _getBuckslipPastTasks = function (businessProcessId) {
+        return $http({
+            method: 'GET',
+            url: 'api/latest/plugin/task/getBuckslipPastTasks/businessProcessId/' + businessProcessId
+        });
+    };
+
+    var _getBuckslipProcessesForObject = function (objectType, objectId) {
+        return $http({
+            method: 'GET',
+            url: 'api/latest/plugin/task/getBuckslipProcessesForObject/objectType/' + objectType + '/objectId/' + objectId
+        });
+    };
+
+    var _isWorkflowInitiable = function (businessProcessId) {
+        return $http({
+            method: 'GET',
+            url: 'api/latest/plugin/task/isWorkflowInitiable/businessProcessId/' + businessProcessId
+        });
+    };
+
+    var _isWorkflowWithdrawable = function (businessProcessId) {
+        return $http({
+            method: 'GET',
+            url: 'api/latest/plugin/task/isWorkflowWithdrawable/businessProcessId/' + businessProcessId
+        });
+    };
+
+    var _initiateRoutingWorkflow = function (businessProcessId, receiveTaskId) {
+        return $http({
+            method: 'POST',
+            url: 'initiateRoutingWorkflow/businessProcessId/' + businessProcessId + '/receiveTaskId/' + receiveTaskId
+        });
+    };
+
+    var _withdrawRoutingWorkflow = function (taskId, messageName) {
+        return $http({
+            method: 'POST',
+            url: 'withdrawRoutingWorkflow/taskId/' + taskId + '/messageName/' + messageName
+        });
+    };
+
+    var _updateBuckslipProcess = function (buckslipProcess) {
+        return $http({
+            method: 'POST',
+            url: 'api/latest/plugin/task/updateBuckslipProcess',
+            data: buckslipProcess,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    };
+
+    return {
+        getBuckslipFutureTasks: _getBuckslipFutureTasks,
+        getBuckslipPastTasks: _getBuckslipPastTasks,
+        getBuckslipProcessesForObject: _getBuckslipProcessesForObject,
+        isWorkflowInitiable: _isWorkflowInitiable,
+        isWorkflowWithdrawable: _isWorkflowWithdrawable,
+        initiateRoutingWorkflow: _initiateRoutingWorkflow,
+        withdrawRoutingWorkflow: _withdrawRoutingWorkflow,
+        updateBuckslipProcess: _updateBuckslipProcess
+    }
+
+    }
+]);
