58ef10fcb525930bac08b5ef3d038ff3608ebc61
James Wu <jwu@armedia.com>
2017-10-29 11:40:31 -0400

    Bring back storage cache
    Former-commit-id: d3ca40d49df4846eaa69c889e82bce8ff00e19fa

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/welcome/controllers/welcome.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/welcome/controllers/welcome.client.controller.js
@@ -2,37 +2,37 @@
 
 angular.module('welcome').controller(
     'WelcomeController', ['$state', '$window', '$translate'
-        , 'UtilService', 'Acm.LoginService', 'Acm.AppService', 'Config.LocaleService'
+        , 'Acm.LoginService', 'Acm.AppService', 'Config.LocaleService'
         , function ($state, $window, $translate
-            , Util, AcmLoginService, AcmAppService, LocaleService) {
-
-
-            var redirectState = sessionStorage.redirectState;
-            var redirectURL = sessionStorage.redirectURL;
-            sessionStorage.clear();
+            , AcmLoginService, AcmAppService, LocaleService) {
 
             AcmLoginService.setLogin(true);
             AcmLoginService.setLastIdle();
 
             LocaleService.getLatestSettings();
 
+
+            var redirectState = sessionStorage.redirectState;
+            var redirectURL = sessionStorage.redirectURL;
+            sessionStorage.removeItem("redirectState");
+            sessionStorage.removeItem("redirectURL");
+            //TODO: check whats up with 'passwordWarningAccepted'; don't see its usage
+            //sessionStorage.removeItem('passwordWarningAccepted');
+
             if (redirectState) {
                 // redirect to the last remembered state
-                var redirectState = angular.fromJson(sessionStorage.redirectState);
+                var redirectState = angular.fromJson(redirectState);
 
                 //because of redirect bug where we are stuck in goodbye state
                 //here is the fix
                 var index = redirectState.hash.indexOf('goodbye');
                 if (index >= 0) {
-                    sessionStorage.removeItem("redirectState");
                     $state.go("dashboard");
                 } else {
-                    sessionStorage.removeItem("redirectState");
                     $state.go(redirectState.hash.split('/')[1]);
                 }
             } else if (redirectURL) {
                 // redirect to hash passed in the URL of the login page
-                sessionStorage.removeItem("redirectUrl");
                 $window.location.href = AcmAppService.getAppUrl('home.html' + redirectURL);
             } else {
                 $state.go("dashboard");
