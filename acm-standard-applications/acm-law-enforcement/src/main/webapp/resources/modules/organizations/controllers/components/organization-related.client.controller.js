0f809ed633eb16002698cf054dca8638d20766fa
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2017-11-01 15:04:05 +0100

    Organization-related, partner now is updating
    Former-commit-id: 911734f29f9d1c0220fc69cdff5612c4876c06ae

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/organizations/controllers/components/organization-related.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/organizations/controllers/components/organization-related.client.controller.js
@@ -168,48 +168,50 @@ angular.module('organizations').controller('Organizations.RelatedController', ['
             association.description = associationData.description;
             ObjectAssociationService.saveObjectAssociation(association).then(function (payload) {
                 //success
-                // if (!rowEntity) {
-                //     //append new entity as last item in the grid
-                //     rowEntity = {
-                //         target_object: {}
-                //     };
-                //     $scope.gridOptions.data.push(rowEntity);
-                // }
-                //
-                // //update row immediately
-                // rowEntity.object_id_s = payload.associationId;
-                // rowEntity.association_type_s = payload.associationType;
-                // rowEntity.target_object.type_lcs = target.organizationType;
-                // if (!Util.isEmpty(target.defaultIdentification)) {
-                //     if (!Util.isEmpty(target.defaultIdentification.identificationType)) {
-                //         rowEntity.target_object.default_identification_s = target.defaultIdentification.identificationNumber + " " + target.defaultIdentification.identificationType;
-                //     } else {
-                //         rowEntity.target_object.default_identification_s = target.defaultIdentification.identificationNumber;
-                //     }
-                // }
-                // rowEntity.target_object.title_parseable = target.organizationValue;
-                // rowEntity.target_object.value_parseable = target.organizationValue;
-                // if (!Util.isEmpty(target.primaryContact)) {
-                //     if (!Util.isEmpty(target.primaryContact.person.familyName)) {
-                //         rowEntity.target_object.primary_contact_s = target.primaryContact.person.givenName + " " + target.primaryContact.person.familyName;
-                //     } else {
-                //         rowEntity.target_object.primary_contact_s = target.primaryContact.person.givenName;
-                //     }
-                //
-                // }
-                // if (!Util.isEmpty(target.defaultPhone)) {
-                //     rowEntity.target_object.default_phone_s = target.defaultPhone.value + " [" + target.defaultPhone.subType + "]";
-                // } else {
-                //     rowEntity.target_object.default_phone_s = "";
-                // }
-                //
-                // if (!Util.isEmpty(target.defaultAddress)) {
-                //     if (!Util.isEmpty(target.defaultAddress.state)) {
-                //         rowEntity.target_object.default_location_s = target.defaultAddress.city + ", " + target.defaultAddress.state;
-                //     } else {
-                //         rowEntity.target_object.default_location_s = target.defaultAddress.city;
-                //     }
-                // }
+                if (payload.associationType.toLowerCase() !== "parentcompany"){
+                    if (!rowEntity) {
+                        //append new entity as last item in the grid
+                        rowEntity = {
+                            target_object: {}
+                        };
+                        $scope.gridOptions.data.push(rowEntity);
+                    }
+
+                    //update row immediately
+                    rowEntity.object_id_s = payload.associationId;
+                    rowEntity.association_type_s = payload.associationType;
+                    rowEntity.target_object.type_lcs = target.organizationType;
+                    if (!Util.isEmpty(target.defaultIdentification)) {
+                        if (!Util.isEmpty(target.defaultIdentification.identificationType)) {
+                            rowEntity.target_object.default_identification_s = target.defaultIdentification.identificationNumber + " " + target.defaultIdentification.identificationType;
+                        } else {
+                            rowEntity.target_object.default_identification_s = target.defaultIdentification.identificationNumber;
+                        }
+                    }
+                    rowEntity.target_object.title_parseable = target.organizationValue;
+                    rowEntity.target_object.value_parseable = target.organizationValue;
+                    if (!Util.isEmpty(target.primaryContact)) {
+                        if (!Util.isEmpty(target.primaryContact.person.familyName)) {
+                            rowEntity.target_object.primary_contact_s = target.primaryContact.person.givenName + " " + target.primaryContact.person.familyName;
+                        } else {
+                            rowEntity.target_object.primary_contact_s = target.primaryContact.person.givenName;
+                        }
+
+                    }
+                    if (!Util.isEmpty(target.defaultPhone)) {
+                        rowEntity.target_object.default_phone_s = target.defaultPhone.value + " [" + target.defaultPhone.subType + "]";
+                    } else {
+                        rowEntity.target_object.default_phone_s = "";
+                    }
+
+                    if (!Util.isEmpty(target.defaultAddress)) {
+                        if (!Util.isEmpty(target.defaultAddress.state)) {
+                            rowEntity.target_object.default_location_s = target.defaultAddress.city + ", " + target.defaultAddress.state;
+                        } else {
+                            rowEntity.target_object.default_location_s = target.defaultAddress.city;
+                        }
+                    }
+                }
                 //wait 2.5 sec and refresh because of solr indexing
                 //below functionality is disabled since we are already updating rows, however if in future we need to be refreshed from solr, than just enable code bellow
                 // $timeout(function () {
