aec962b0dcd5e80c1f244fe6d4a80772009a9331
Ivo Shurbanovski <ivo.shurbanovski@armedia.com>
2017-11-07 14:05:51 +0100

    2020
    Former-commit-id: bf672ee43fd0153716f4015edc20cc3c39a4c686

--- acm-plugins/acm-default-plugins/acm-object-lock-plugin/src/main/java/com/armedia/acm/plugins/objectlockplugin/web/api/AcmObjectLockAPIController.java
+++ acm-plugins/acm-default-plugins/acm-object-lock-plugin/src/main/java/com/armedia/acm/plugins/objectlockplugin/web/api/AcmObjectLockAPIController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by nebojsha on 28.10.2015.
@@ -33,23 +34,20 @@ public class AcmObjectLockAPIController
     /**
      * This method locks object identified with objectId and objectType.
      *
-     * @param objectType
-     *            object type
-     * @param objectId
-     *            object ID
-     * @param authentication
-     *            Authentication
+     * @param objectType     object type
+     * @param objectId       object ID
+     * @param authentication Authentication
      * @return AcmObjectLock lock details
      * @throws MuleException
      * @throws IOException
      */
     @PreAuthorize("hasPermission(#objectId, #objectType, 'lock')")
-    @RequestMapping(value = { "/api/v1/plugin/{objectType}/{objectId}/lock",
-            "/api/latest/plugin/{objectType}/{objectId}/lock" }, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = {"/api/v1/plugin/{objectType}/{objectId}/lock",
+            "/api/latest/plugin/{objectType}/{objectId}/lock"}, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public AcmObjectLock lockObject(@PathVariable(value = "objectType") String objectType, @PathVariable(value = "objectId") Long objectId,
-            @RequestParam(value = "lockType", required = false, defaultValue = "OBJECT_LOCK") String lockType,
-            @RequestParam(value = "lockInDB", required = false, defaultValue = "true") boolean lockInDB, Authentication authentication)
+                                    @RequestParam(value = "lockType", required = false, defaultValue = "OBJECT_LOCK") String lockType,
+                                    @RequestParam(value = "lockInDB", required = false, defaultValue = "true") boolean lockInDB, Authentication authentication)
             throws MuleException, IOException
 
     {
@@ -59,22 +57,19 @@ public class AcmObjectLockAPIController
     /**
      * This method unlocks already locked object identified with objectId and objectType.
      *
-     * @param objectType
-     *            object type
-     * @param objectId
-     *            object ID
-     * @param auth
-     *            Authentication
+     * @param objectType object type
+     * @param objectId   object ID
+     * @param auth       Authentication
      * @return solr response
      * @throws MuleException
      * @throws IOException
      */
     @PreAuthorize("hasPermission(#objectId, #objectType, 'unlock')")
-    @RequestMapping(value = { "/api/v1/plugin/{objectType}/{objectId}/lock",
-            "/api/latest/plugin/{objectType}/{objectId}/lock" }, method = RequestMethod.DELETE)
+    @RequestMapping(value = {"/api/v1/plugin/{objectType}/{objectId}/lock",
+            "/api/latest/plugin/{objectType}/{objectId}/lock"}, method = RequestMethod.DELETE)
     @ResponseBody
     public String unlockObject(@PathVariable(value = "objectType") String objectType, @PathVariable(value = "objectId") Long objectId,
-            @RequestParam(value = "lockType", required = false, defaultValue = "OBJECT_LOCK") String lockType, Authentication auth)
+                               @RequestParam(value = "lockType", required = false, defaultValue = "OBJECT_LOCK") String lockType, Authentication auth)
             throws MuleException, IOException
     {
         try
@@ -91,27 +86,22 @@ public class AcmObjectLockAPIController
     /**
      * This method retrieves objects documents from solr, which are locked.
      *
-     * @param objectType
-     *            object type
-     * @param firstRow
-     *            start from row
-     * @param maxRows
-     *            max results
-     * @param sort
-     *            sort by solr document field
-     * @param authentication
-     *            injected by spring
+     * @param objectType     object type
+     * @param firstRow       start from row
+     * @param maxRows        max results
+     * @param sort           sort by solr document field
+     * @param authentication injected by spring
      * @return solr response
      * @throws MuleException
      * @throws IOException
      */
-    @RequestMapping(value = { "/api/v1/plugin/objects/{objectType}/locked",
-            "/api/latest/plugin/objects/{objectType}/locked" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = {"/api/v1/plugin/objects/{objectType}/locked",
+            "/api/latest/plugin/objects/{objectType}/locked"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String listObjectsWithLock(@PathVariable(value = "objectType") String objectType,
-            @RequestParam(value = "firstRow", defaultValue = "0", required = false) int firstRow,
-            @RequestParam(value = "maxRows", defaultValue = "1000", required = false) int maxRows,
-            @RequestParam(value = "sort", defaultValue = "", required = false) String sort, Authentication authentication)
+                                      @RequestParam(value = "firstRow", defaultValue = "0", required = false) int firstRow,
+                                      @RequestParam(value = "maxRows", defaultValue = "1000", required = false) int maxRows,
+                                      @RequestParam(value = "sort", defaultValue = "", required = false) String sort, Authentication authentication)
             throws MuleException, IOException
     {
         return objectLockService.getDocumentsWithLock(objectType, authentication, null, firstRow, maxRows, sort, null);
@@ -120,31 +110,38 @@ public class AcmObjectLockAPIController
     /**
      * This method retrieves object locks documents which are indexed in solr.
      *
-     * @param objectType
-     *            object type
-     * @param firstRow
-     *            start from row
-     * @param maxRows
-     *            max results
-     * @param sort
-     *            sort by solr document field
-     * @param authentication
-     *            injected by spring
+     * @param objectType     object type
+     * @param firstRow       start from row
+     * @param maxRows        max results
+     * @param sort           sort by solr document field
+     * @param authentication injected by spring
      * @return solr response
      * @throws MuleException
      * @throws IOException
      */
-    @RequestMapping(value = { "/api/v1/plugin/locks/{objectType}",
-            "/api/latest/plugin/locks/{objectType}" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = {"/api/v1/plugin/locks/{objectType}",
+            "/api/latest/plugin/locks/{objectType}"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String listLocks(@PathVariable(value = "objectType") String objectType,
-            @RequestParam(value = "firstRow", defaultValue = "0", required = false) int firstRow,
-            @RequestParam(value = "maxRows", defaultValue = "1000", required = false) int maxRows,
-            @RequestParam(value = "sort", defaultValue = "", required = false) String sort, Authentication authentication)
+    public String listLocks(@RequestParam(value = "parentObjectId", required = false) String parentObjectId,
+                            @RequestParam(value = "creator", required = false) String creator,
+                            @RequestParam(value = "createdDate", required = false) String createdDate,
+                            @PathVariable(value = "objectType") String objectType,
+                            @RequestParam(value = "firstRow", defaultValue = "0", required = false) int firstRow,
+                            @RequestParam(value = "maxRows", defaultValue = "1000", required = false) int maxRows,
+                            @RequestParam(value = "sort", defaultValue = "", required = false) String sort, Authentication authentication)
             throws MuleException, IOException
     {
-        return objectLockService.getObjectLocks(objectType, authentication, null, firstRow, maxRows, sort, null);
+        return objectLockService.getObjectLocks(objectType, authentication, parentObjectId, creator, createdDate, firstRow, maxRows, sort, null);
 
+    }
+
+    @PreAuthorize("hasPermission(#objectIds, #objectType, 'unlock')")
+    @RequestMapping(value = {"/api/latest/plugin/locks/{objectType}/lock", "/api/v1/plugin/locks/{objectType}/lock"}, method = RequestMethod.DELETE)
+    public String releaseMultipleLock(@PathVariable(value = "objectType") String objectType,
+                                      @RequestParam(value = "requestIds") List<Long> requestIds,
+                                      @RequestParam(value = "lockType", required = false, defaultValue = "OBJECT_LOCK") String lockType, Authentication auth) throws MuleException
+    {
+        return objectLockService.removeLocksOnMultipleObjects(objectType, requestIds, lockType, auth);
     }
 
     public void setObjectLockService(AcmObjectLockService objectLockService)
