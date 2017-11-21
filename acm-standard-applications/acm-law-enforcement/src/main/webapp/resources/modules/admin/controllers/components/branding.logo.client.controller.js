d4e703957a0edcf044a6af4063df497d8f6f6cbe
wazir.shpoon <wazir.shpoon@amedia.com>
2017-11-21 09:30:25 -0500

    AFDP-4183 - fix logo upload format error message
    Former-commit-id: 6e29ce57735cc85be64936139f63cec12797972e

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/branding.logo.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/branding.logo.client.controller.js
@@ -7,6 +7,19 @@ angular.module('admin').controller('Admin.BrandingLogoController', ['$scope', 'A
         $scope.selectedHeaderFile = null;
         $scope.selectedLoginFile = null;
 
+        $scope.validateFileTypes = function(){
+            var headerLogoType, loginLogoType;
+            if($scope.selectedLoginFile !== null && $scope.selectedLoginFile.name !== null){
+                loginLogoType = $scope.selectedLoginFile.name.match(/\.png$/i);
+            }
+            if($scope.selectedHeaderFile !== null && $scope.selectedHeaderFile.name !== null){
+                headerLogoType =  $scope.selectedHeaderFile.name.match(/\.png$/i);
+            }
+            if(headerLogoType === null || loginLogoType === null || (typeof(headerLogoType) == 'undefined' && typeof(loginLogoType) == 'undefined')){
+                return false;
+            }
+        }
+
         $scope.saveFiles = function () {
             $scope.uploadingInProgress = true;
             //we need to have and form names for files upload
