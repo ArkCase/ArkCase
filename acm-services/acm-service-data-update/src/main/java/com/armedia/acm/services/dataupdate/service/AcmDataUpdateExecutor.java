573af830a03554ed1da701e54e601f8e554a44cd
ncuculova <nadica.cuculova@armedia.com>
2017-11-22 11:07:09 +0100

    - Javadocs added - small refactor in AcmDataUpdateManager
    Former-commit-id: f7509618450f856f8e9295c76d194fb924343876

--- acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/AcmDataUpdateExecutor.java
+++ acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/AcmDataUpdateExecutor.java
@@ -1,8 +1,20 @@
 package com.armedia.acm.services.dataupdate.service;
 
+/**
+ * Data update executors should implement this interface and
+ * provide unique executor id and implement operations to be executed.
+ */
 public interface AcmDataUpdateExecutor
 {
+    /**
+     * Returns the executor id.
+     *
+     * @return String
+     */
     String getUpdateId();
 
+    /**
+     * Executes update operations.
+     */
     void execute();
 }
--- acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/AcmDataUpdateManager.java
+++ acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/AcmDataUpdateManager.java
@@ -11,9 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * Class that will manage all registered {@link AcmDataUpdateExecutor} beans
+ * and ensure to execute only once. The execution is after Root Application
+ * context is initialized to ensure that all beans are initialized and can be used.
+ * Executors may depend one of another, so order of executions is enforced
+ * and planned to finish in one transaction.
+ */
 public class AcmDataUpdateManager implements ApplicationListener<ContextRefreshedEvent>
 {
     private AcmDataUpdateService dataUpdateService;
@@ -29,23 +37,21 @@ public class AcmDataUpdateManager implements ApplicationListener<ContextRefreshe
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
-        boolean rootAppContext = ((ApplicationContext) event.getSource()).getParent() == null;
-        if (rootAppContext)
+        boolean isRootContext = ((ApplicationContext) event.getSource()).getParent() == null;
+        if (isRootContext)
         {
-            Set<String> executedDataUpdates = dataUpdateService.findAll()
+            Set<String> executedExecutorIds = dataUpdateService.findAll()
                     .stream()
                     .map(AcmDataUpdateExecutorLog::getExecutorId)
                     .collect(Collectors.toSet());
 
-            Predicate<AcmDataUpdateExecutor> updatesNotExecuted = service -> !executedDataUpdates.contains(service.getUpdateId());
+            Predicate<AcmDataUpdateExecutor> updatesNotExecuted =
+                    service -> !executedExecutorIds.contains(service.getUpdateId());
+
             log.info("Starting [{}] core data update executors...", dataUpdateExecutors.size());
             dataUpdateExecutors.stream()
                     .filter(updatesNotExecuted)
-                    .forEach(service -> {
-                        log.debug("Execute updates from: [{}]", service.getUpdateId());
-                        service.execute();
-                        dataUpdateService.save(service.getUpdateId());
-                    });
+                    .forEach(dataUpdateExecutor);
 
             if (extensionDataUpdateExecutors != null && extensionDataUpdateExecutors.getExecutors() != null)
             {
@@ -54,14 +60,16 @@ public class AcmDataUpdateManager implements ApplicationListener<ContextRefreshe
 
                 extensionDataUpdateExecutors.getExecutors().stream()
                         .filter(updatesNotExecuted)
-                        .forEach(service -> {
-                            log.debug("Execute updates from: [{}]", service.getUpdateId());
-                            service.execute();
-                            dataUpdateService.save(service.getUpdateId());
-                        });
+                        .forEach(dataUpdateExecutor);
             }
         }
     }
+
+    private final Consumer<AcmDataUpdateExecutor> dataUpdateExecutor = service -> {
+        log.debug("Execute updates from: [{}]", service.getUpdateId());
+        service.execute();
+        dataUpdateService.save(service.getUpdateId());
+    };
 
     public void setDataUpdateService(AcmDataUpdateService dataUpdateService)
     {
