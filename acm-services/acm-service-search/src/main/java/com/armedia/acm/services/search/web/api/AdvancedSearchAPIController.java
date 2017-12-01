4d9cd1b78687f31a8998c8e0a4b4af1ff3241df3
ARMEDIA\petar.ilin <petar.ilin@armedia.com>
2017-12-01 13:30:45 +0100

    SSP-437 Adding HTTP POST variant of advancedSearch REST endpoint
    Former-commit-id: 45ee0a93567b7534ea65325f8923fb508d121966

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/web/api/AdvancedSearchAPIController.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/web/api/AdvancedSearchAPIController.java
@@ -8,10 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by marjan.stefanoski on 19.12.2014.
@@ -25,9 +28,20 @@ public class AdvancedSearchAPIController
 
     private ExecuteSolrQuery executeSolrQuery;
 
+    /**
+     * Solr Advanced search, HTTP GET variant (limited query length).
+     *
+     * @param query          Solr query
+     * @param sort           Solr sorting parameter
+     * @param startRow       start from
+     * @param maxRows        number of results
+     * @param authentication authentication token
+     * @return Solr response
+     * @throws MuleException on error
+     */
     @RequestMapping(value = "/advancedSearch", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public String advancedSearch(
+    public String advancedSearchGet(
             @RequestParam(value = "q", required = true) String query,
             @RequestParam(value = "s", required = false, defaultValue = "") String sort,
             @RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
@@ -35,11 +49,74 @@ public class AdvancedSearchAPIController
             Authentication authentication
     ) throws MuleException
     {
-        if (log.isDebugEnabled())
-        {
-            log.debug("User '" + authentication.getName() + "' is searching for '" + query + "'");
-        }
+        return advancedSearch(query, sort, startRow, maxRows, authentication);
+    }
 
+    /**
+     * Solr Advanced search, HTTP POST variant that allows indefinite query length.
+     * <p>
+     * the request body should look like:
+     * <p>
+     * <code>
+     * {
+     *   "q":"catch_all:test*+AND+object_type_S:CASE_FILE",
+     *   "start":0,
+     *   "n":10,
+     * "  s":"create_date_tdt DESC"
+     * }
+     * </code>
+     *
+     * @param requestParams  parameter map, as explained above
+     * @param authentication authentication token
+     * @return Solr response
+     * @throws MuleException on error
+     */
+    @RequestMapping(value = "/advancedSearch", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String advancedSearchPost(
+            @RequestBody Map<String, Object> requestParams,
+            Authentication authentication
+    ) throws MuleException
+    {
+        String query = (String) requestParams.get("q");
+        String sort = "";
+        if (requestParams.get("s") != null)
+        {
+            sort = (String) requestParams.get("s");
+        }
+        int startRow = 0;
+        if (requestParams.get("start") != null)
+        {
+            startRow = (int) requestParams.get("start");
+        }
+        int maxRows = 10;
+        if (requestParams.get("n") != null)
+        {
+            maxRows = (int) requestParams.get("n");
+        }
+        return advancedSearch(query, sort, startRow, maxRows, authentication);
+    }
+
+    /**
+     * Trigger Solr advanced search.
+     *
+     * @param query          Solr query
+     * @param sort           Solr sorting parameter
+     * @param startRow       start from
+     * @param maxRows        number of results
+     * @param authentication authentication token
+     * @return Solr response
+     * @throws MuleException on error
+     */
+    private String advancedSearch(
+            @RequestParam(value = "q", required = true) String query,
+            @RequestParam(value = "s", required = false, defaultValue = "") String sort,
+            @RequestParam(value = "start", required = false, defaultValue = "0") int startRow,
+            @RequestParam(value = "n", required = false, defaultValue = "10") int maxRows,
+            Authentication authentication
+    ) throws MuleException
+    {
+        log.debug("User [{}] is searching for [{}]", authentication.getName(), query);
 
         // Solr wants the '+' sign in the "facet.range.gap" part of the query to be encoded
         // It is because the '+' shouldn't be interpreted/used as a space but as '+'  on solr side and that's why need to be sent as %2B.
