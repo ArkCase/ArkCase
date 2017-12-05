f19ba155f238d7a2a6112e99f9debcbb13c84e13
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2017-12-05 16:26:43 +0100

    Audit date-picker now is updating the time correctly
    Former-commit-id: 29c764b226d4494e71ddfe74e633e80f1725db97

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/controllers/components/audit-datepickers.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/controllers/components/audit-datepickers.client.controller.js
@@ -6,25 +6,66 @@ angular.module('audit').controller('Audit.DatepickersController', ['$scope', 'Ut
         var todayDate = new Date();
 
         $scope.dateFromChanged = function () {
+            var todayDate = new Date();
             if(Util.isEmpty($scope.dateFrom)){
-                $scope.dateFrom = moment().year(todayDate.getFullYear()).month(todayDate.getMonth()).date(todayDate.getDate())._d;
+                $scope.dateFrom = moment()
+                    .year(todayDate.getFullYear())
+                    .month(todayDate.getMonth())
+                    .date(todayDate.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
+            } else {
+                $scope.dateFrom = moment()
+                    .year($scope.dateFrom.getFullYear())
+                    .month($scope.dateFrom.getMonth())
+                    .date($scope.dateFrom.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
             }
 
             if(moment($scope.dateFrom).isAfter($scope.dateTo)){
-                $scope.dateTo = moment().year($scope.dateFrom.getFullYear()).month($scope.dateFrom.getMonth()).date($scope.dateFrom.getDate())._d;
+                $scope.dateTo = moment()
+                    .year($scope.dateFrom.getFullYear())
+                    .month($scope.dateFrom.getMonth())
+                    .date($scope.dateFrom.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
             }
-
 
             $scope.$emit('send-date', $scope.dateFrom, $scope.dateTo, true);
         };
 
         $scope.dateToChanged = function () {
+            var todayDate = new Date();
             if(Util.isEmpty($scope.dateTo)){
-                $scope.dateTo = moment().year($scope.dateFrom.getFullYear()).month($scope.dateFrom.getMonth()).date($scope.dateFrom.getDate())._d;
+                $scope.dateTo = moment()
+                    .year($scope.dateFrom.getFullYear())
+                    .month($scope.dateFrom.getMonth())
+                    .date($scope.dateFrom.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
+            } else {
+                $scope.dateTo = moment()
+                    .year($scope.dateTo.getFullYear())
+                    .month($scope.dateTo.getMonth())
+                    .date($scope.dateTo.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
             }
 
             if(moment($scope.dateTo).isBefore($scope.dateFrom)){
-                $scope.dateTo = moment().year($scope.dateFrom.getFullYear()).month($scope.dateFrom.getMonth()).date($scope.dateFrom.getDate())._d;
+                $scope.dateTo = moment()
+                    .year($scope.dateFrom.getFullYear())
+                    .month($scope.dateFrom.getMonth())
+                    .date($scope.dateFrom.getDate())
+                    .hours(todayDate.getHours())
+                    .minutes(todayDate.getMinutes())
+                    .seconds(todayDate.getSeconds())._d;
             }
 
             $scope.$emit('send-date', $scope.dateFrom, $scope.dateTo, true);
