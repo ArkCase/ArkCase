562382561489065a997817712f9cab3df95e9b6b
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2017-12-06 16:35:24 +0100

    Riste comments fixed
    Former-commit-id: 7087e862803677061bd1faa84da73e7565fbdb87

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/controllers/components/audit-datepickers.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/audit/controllers/components/audit-datepickers.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('audit').controller('Audit.DatepickersController', ['$scope', 'UtilService', function ($scope, Util) {
+angular.module('audit').controller('Audit.DatepickersController', ['$scope', 'UtilService', 'Util.DateService', function ($scope, Util, UtilDateService) {
         $scope.dateFrom = new Date();
         $scope.dateTo = new Date();
         var todayDate = new Date();
@@ -8,66 +8,32 @@ angular.module('audit').controller('Audit.DatepickersController', ['$scope', 'Ut
         $scope.dateFromChanged = function () {
             var todayDate = new Date();
             if(Util.isEmpty($scope.dateFrom)){
-                $scope.dateFrom = moment()
-                    .year(todayDate.getFullYear())
-                    .month(todayDate.getMonth())
-                    .date(todayDate.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateFrom = new Date();
             } else {
-                $scope.dateFrom = moment()
-                    .year($scope.dateFrom.getFullYear())
-                    .month($scope.dateFrom.getMonth())
-                    .date($scope.dateFrom.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateFrom = UtilDateService.convertToCurrentTime($scope.dateFrom);
             }
 
             if(moment($scope.dateFrom).isAfter($scope.dateTo)){
-                $scope.dateTo = moment()
-                    .year($scope.dateFrom.getFullYear())
-                    .month($scope.dateFrom.getMonth())
-                    .date($scope.dateFrom.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateTo = UtilDateService.convertToCurrentTime($scope.dateFrom);
             }
 
+            $scope.dateFrom = UtilDateService.setSameDateTime($scope.dateFrom, $scope.dateTo);
             $scope.$emit('send-date', $scope.dateFrom, $scope.dateTo, true);
         };
 
         $scope.dateToChanged = function () {
             var todayDate = new Date();
             if(Util.isEmpty($scope.dateTo)){
-                $scope.dateTo = moment()
-                    .year($scope.dateFrom.getFullYear())
-                    .month($scope.dateFrom.getMonth())
-                    .date($scope.dateFrom.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateTo = UtilDateService.convertToCurrentTime($scope.dateFrom);
             } else {
-                $scope.dateTo = moment()
-                    .year($scope.dateTo.getFullYear())
-                    .month($scope.dateTo.getMonth())
-                    .date($scope.dateTo.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateTo = UtilDateService.convertToCurrentTime($scope.dateTo);
             }
 
             if(moment($scope.dateTo).isBefore($scope.dateFrom)){
-                $scope.dateTo = moment()
-                    .year($scope.dateFrom.getFullYear())
-                    .month($scope.dateFrom.getMonth())
-                    .date($scope.dateFrom.getDate())
-                    .hours(todayDate.getHours())
-                    .minutes(todayDate.getMinutes())
-                    .seconds(todayDate.getSeconds())._d;
+                $scope.dateTo = UtilDateService.convertToCurrentTime($scope.dateFrom);
             }
 
+            $scope.dateTo = UtilDateService.setSameDateTime($scope.dateTo, $scope.dateFrom);
             $scope.$emit('send-date', $scope.dateFrom, $scope.dateTo, true);
         };
 
--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/util/util-date.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/util/util-date.client.service.js
@@ -203,7 +203,7 @@ angular.module('services').factory('Util.DateService', ['$translate', 'UtilServi
             }
             /**
              * @ngdoc method
-             * @name setDifferenceOfOneDay
+             * @name setSameDateTime
              * @methodOf services:Util.DateService
              *
              * @description
