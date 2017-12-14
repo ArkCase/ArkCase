99dc26f5e0ef7be3edfc0df1745b0abd496a92f2
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2017-12-14 16:16:37 +0100

    Implemented date validation for report module
    Former-commit-id: 2380b586555839c8d46800fc4eec584920074cf7

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
@@ -1,8 +1,42 @@
 'use strict';
 
-angular.module('reports').controller('Reports.DatepickersController', ['$scope', function ($scope) {
-        $scope.opened = {};
-        $scope.opened.openedStart = false;
-        $scope.opened.openedEnd = false;
-    }
+angular.module('reports').controller('Reports.DatepickersController', ['$scope', 'UtilService', 'Util.DateService', function ($scope, Util, UtilDateService) {
+
+    $scope.startDateChanged = function () {
+        var todayDate = new Date();
+        if (Util.isEmpty($scope.data.startDate)) {
+            $scope.data.startDate = new Date();
+        } else {
+            $scope.data.startDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
+        }
+
+        if (moment($scope.data.startDate).isAfter(todayDate)) {
+            $scope.data.startDate = UtilDateService.convertToCurrentTime(todayDate);
+        }
+
+        if (moment($scope.data.startDate).isAfter($scope.data.endDate)) {
+            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
+        }
+
+        $scope.data.startDate = UtilDateService.setSameTime($scope.data.startDate, $scope.data.endDate);
+    };
+
+    $scope.endDateChanged = function () {
+        if (Util.isEmpty($scope.data.endDate)) {
+            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
+        } else {
+            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.endDate);
+        }
+
+        if (moment($scope.data.endDate).isBefore($scope.data.startDate)) {
+            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
+        }
+
+        $scope.data.endDate = UtilDateService.setSameTime($scope.data.endDate, $scope.data.startDate);
+    };
+
+    $scope.opened = {};
+    $scope.opened.openedStart = false;
+    $scope.opened.openedEnd = false;
+}
 ]);
