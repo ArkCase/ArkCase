6c135ab2bfb911c7c6f12575a6fcacd9b97e2c43
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2017-12-18 15:25:45 +0100

    Fixed David comment, clean code
    Former-commit-id: ea59dee62f717e5cefa1805d4c42b1f70432e7f4

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/reports/controllers/components/reports-datepickers.client.controller.js
@@ -3,36 +3,15 @@
 angular.module('reports').controller('Reports.DatepickersController', ['$scope', 'UtilService', 'Util.DateService', function ($scope, Util, UtilDateService) {
 
     $scope.startDateChanged = function () {
-        var todayDate = new Date();
-        if (Util.isEmpty($scope.data.startDate)) {
-            $scope.data.startDate = new Date();
-        } else {
-            $scope.data.startDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
-        }
-
-        if (moment($scope.data.startDate).isAfter(todayDate)) {
-            $scope.data.startDate = UtilDateService.convertToCurrentTime(todayDate);
-        }
-
-        if (moment($scope.data.startDate).isAfter($scope.data.endDate)) {
-            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
-        }
-
-        $scope.data.startDate = UtilDateService.setSameTime($scope.data.startDate, $scope.data.endDate);
+        var validateDate = UtilDateService.validateFromDate($scope.data.startDate, $scope.data.endDate);
+        $scope.data.startDate = validateDate.from;
+        $scope.data.endDate = validateDate.to;
     };
 
     $scope.endDateChanged = function () {
-        if (Util.isEmpty($scope.data.endDate)) {
-            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
-        } else {
-            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.endDate);
-        }
-
-        if (moment($scope.data.endDate).isBefore($scope.data.startDate)) {
-            $scope.data.endDate = UtilDateService.convertToCurrentTime($scope.data.startDate);
-        }
-
-        $scope.data.endDate = UtilDateService.setSameTime($scope.data.endDate, $scope.data.startDate);
+        var validateDate = UtilDateService.validateToDate($scope.data.startDate, $scope.data.endDate);
+        $scope.data.startDate = validateDate.from;
+        $scope.data.endDate = validateDate.to;
     };
 
     $scope.opened = {};
