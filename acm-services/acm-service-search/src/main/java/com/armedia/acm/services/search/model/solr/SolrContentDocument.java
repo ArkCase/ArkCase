8a130f437e838a16ac2bd064305974aa902110f2
Riste Tutureski <riste.tutureski@armedia.com>
2018-01-03 18:49:35 +0100

    AFDP-5072: Code review changes
    Former-commit-id: b572a8ab0b1465fb9981b639f8201d34546fa634

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/model/solr/SolrContentDocument.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/model/solr/SolrContentDocument.java
@@ -19,26 +19,26 @@ public class SolrContentDocument extends SolrAdvancedSearchDocument
         String url = "literal.allow_acl_ss=" + (getAllow_acl_ss() == null ? null : String.join("&literal.allow_acl_ss=", getAllow_acl_ss())) +
                         "&literal.deny_acl_ss=" + (getDeny_acl_ss() == null ? null : String.join("&literal.deny_acl_ss=", getDeny_acl_ss())) +
                         "&literal.hidden_b=" + isHidden_b() +
-                        "&literal.parent_ref_s=" + getParent_ref_s() +
-                        "&literal.status_lcs=" + getStatus_lcs() +
+                        "&literal.parent_ref_s=" + encode(getParent_ref_s()) +
+                        "&literal.status_lcs=" + encode(getStatus_lcs()) +
                         "&literal.protected_object_b=" + isProtected_object_b() +
                         "&literal.public_doc_b=" + isPublic_doc_b() +
-                        "&literal.id=" + getId() +
-                        "&literal.object_type_s=" + getObject_type_s() +
-                        "&literal.object_id_s=" + getObject_id_s() +
+                        "&literal.id=" + encode(getId()) +
+                        "&literal.object_type_s=" + encode(getObject_type_s()) +
+                        "&literal.object_id_s=" + encode(getObject_id_s()) +
                         "&literal.modified_date_tdt=" + getModified_date_tdt() +
-                        "&literal.modifier_lcs=" + getModifier_lcs() +
+                        "&literal.modifier_lcs=" + encode(getModifier_lcs()) +
                         "&literal.create_date_tdt=" + getCreate_date_tdt() +
-                        "&literal.creator_lcs=" + getCreator_lcs() +
+                        "&literal.creator_lcs=" + encode(getCreator_lcs()) +
                         "&literal.name=" + encode(getName()) +
-                        "&literal.parent_id_s=" + getParent_id_s() +
-                        "&literal.parent_type_s=" + getParent_type_s() +
-                        "&literal.parent_number_lcs=" + getParent_number_lcs() +
+                        "&literal.parent_id_s=" + encode(getParent_id_s()) +
+                        "&literal.parent_type_s=" + encode(getParent_type_s()) +
+                        "&literal.parent_number_lcs=" + encode(getParent_number_lcs()) +
                         "&literal.title_parseable=" + encode(getTitle_parseable()) +
                         "&literal.title_parseable_lcs=" + encode(getTitle_parseable_lcs()) +
-                        "&literal.assignee_full_name_lcs=" + getAssignee_full_name_lcs() +
-                        "&literal.type_lcs=" + getType_lcs() +
-                        "&literal.ext_s=" + getExt_s() +
+                        "&literal.assignee_full_name_lcs=" + encode(getAssignee_full_name_lcs()) +
+                        "&literal.type_lcs=" + encode(getType_lcs()) +
+                        "&literal.ext_s=" + encode(getExt_s()) +
                         "&literal.mime_type_s=" + encode(getMime_type_s());
 
         if (getAdditionalProperties() != null)
@@ -47,7 +47,8 @@ public class SolrContentDocument extends SolrAdvancedSearchDocument
             {
                 if (getSkipAdditionalPropertiesInURL() != null && !getSkipAdditionalPropertiesInURL().contains(entry.getKey()))
                 {
-                    url += "&literal." + entry.getKey() + "=" + entry.getValue();
+
+                    url += "&literal." + entry.getKey() + "=" + entry.getValue() instanceof String ? encode((String) entry.getValue()) : entry.getValue();
                 }
             }
         }
