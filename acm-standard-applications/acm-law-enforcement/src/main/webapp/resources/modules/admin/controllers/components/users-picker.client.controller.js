552669872782140faaebe494d2bb5f00a7fbc84f
ncuculova <nadica.cuculova@armedia.com>
2018-01-10 17:20:38 +0100

    AFDP-4917: New functionality: add existing adhoc group - improvements when adding members on ui and server side - make async sending of the reset password email
    Former-commit-id: f1f6aa282361b7aeb951613eefce2525df25d017

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/users-picker.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('admin').controller('Admin.UsersPicker', ['$scope', '$modalInstance', '$config', '$filter',
-    function ($scope, $modalInstance, $config, $filter) {
-        $scope.filter = $filter;
-        $scope.modalInstance = $modalInstance;
-        $scope.config = $config;
-    }
-]);
