327b2939d024be8fce62507a63a4acdcc423d664
adrijana.maneva <adrijana.maneva@armedia.com>
2018-01-11 13:45:33 +0100

    AFDP-5054 WebSocketsListener fixed
    Former-commit-id: 3374080690bd1e04627b6916fbdb8a49bd68273d

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/websockets/websockets-core.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/services/websockets/websockets-core.client.service.js
@@ -104,7 +104,7 @@ angular.module("services").factory("WebSocketsListener", ['$q', '$timeout', 'Web
     }]).run(
     function (WebSocketsListener) {
         if (WebSocketsListener.shouldStart) {
-            // WebSocketsListener.connect();
+            WebSocketsListener.connect();
         }
     }
 );
