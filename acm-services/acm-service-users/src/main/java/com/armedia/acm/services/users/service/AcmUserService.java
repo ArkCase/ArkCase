9a8fcfeba922e69a860d46212545729675a42ad2
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2018-01-15 17:39:55 +0100

    In development
    Former-commit-id: 2e7890bc88dae02ef44b1d8c8db83b7c30ff36a6

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/AcmUserService.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/AcmUserService.java
@@ -1,6 +1,8 @@
 package com.armedia.acm.services.users.service;
 
 import com.armedia.acm.services.users.model.AcmUser;
+import org.mule.api.MuleException;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
@@ -11,6 +13,7 @@ public interface AcmUserService
 {
     /**
      * queries each user for given id's and returns list of users
+     *
      * @param usersIds given id's
      * @return List of users
      */
@@ -18,8 +21,17 @@ public interface AcmUserService
 
     /**
      * extracts userId from User and returns a list of id's
+     *
      * @param users given users
      * @return List of users id's
      */
     List<String> extractIdsFromUserList(List<AcmUser> users);
+
+    /**
+     * Retrieve all valid USERS
+     *
+     * @param auth, searchFilter, sortBy, sortDirection, startRow, maxRows
+     * @return USER groups
+     */
+    String test(Authentication auth, String searchFilter, String sortBy, String sortDirection, int startRow, int maxRows) throws MuleException;
 }
