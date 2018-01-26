b59fe85b0d2d87159c1a27ee86e009021b6056a5
James Wu <jwu@armedia.com>
2018-01-26 16:35:02 -0500

    i18n bug fix for dates format; Save button message
    Former-commit-id: ab08a146930b0bb30b5864aa26d8c0d5fa8420ec

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-details/controllers/document-details.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-details/controllers/document-details.client.controller.js
@@ -1,115 +1,134 @@
 'use strict';
 
-angular.module('document-details').controller('DocumentDetailsController', ['$scope', '$stateParams', '$sce', '$q'
-    , '$timeout', 'TicketService', 'ConfigService', 'LookupService', 'SnowboundService', 'Authentication', 'EcmService'
-    , 'Helper.LocaleService'
-    , function ($scope, $stateParams, $sce, $q, $timeout
-        , TicketService, ConfigService, LookupService, SnowboundService, Authentication, EcmService, LocaleHelper
-    ) {
+angular.module('document-details').controller(
+        'DocumentDetailsController',
+        [
+                '$scope',
+                '$stateParams',
+                '$sce',
+                '$q',
+                '$timeout',
+                'TicketService',
+                'ConfigService',
+                'LookupService',
+                'SnowboundService',
+                'Authentication',
+                'EcmService',
+                'Helper.LocaleService',
+                function($scope, $stateParams, $sce, $q, $timeout, TicketService, ConfigService, LookupService, SnowboundService,
+                        Authentication, EcmService, LocaleHelper) {
 
-        new LocaleHelper.Locale({scope: $scope});
+                    new LocaleHelper.Locale({
+                        scope : $scope
+                    });
 
-        $scope.acmTicket = '';
-        $scope.userId = '';
-        $scope.ecmFileProperties = {};
-        $scope.snowboundUrl = '';
-        $scope.ecmFileEvents = [];
-        $scope.ecmFileParticipants = [];
-        $scope.userList = [];
-        $scope.caseInfo = {};
-        $scope.fileInfo = {
-            id: $stateParams['id'],
-            containerId: $stateParams['containerId'],
-            containerType: $stateParams['containerType'],
-            name: $stateParams['name'],
-            selectedIds: $stateParams['selectedIds']
-        };
-        $scope.showVideoPlayer = false;
-        
-        /**
-         * Builds the snowbound url based on the parameters passed into the controller state and opens the
-         * specified document in an iframe which points to snowbound
-         */
-        $scope.openSnowboundViewer = function () {
-            var viewerUrl = SnowboundService.buildSnowboundUrl($scope.ecmFileProperties, $scope.acmTicket, $scope.userId, $scope.fileInfo);
-            $scope.documentViewerUrl = $sce.trustAsResourceUrl(viewerUrl);
-        };
+                    $scope.acmTicket = '';
+                    $scope.userId = '';
+                    $scope.ecmFileProperties = {};
+                    $scope.snowboundUrl = '';
+                    $scope.ecmFileEvents = [];
+                    $scope.ecmFileParticipants = [];
+                    $scope.userList = [];
+                    $scope.caseInfo = {};
+                    $scope.fileInfo = {
+                        id : $stateParams['id'],
+                        containerId : $stateParams['containerId'],
+                        containerType : $stateParams['containerType'],
+                        name : $stateParams['name'],
+                        selectedIds : $stateParams['selectedIds']
+                    };
+                    $scope.showVideoPlayer = false;
 
-        $scope.$bus.subscribe('update-viewer-opened-versions', function (openedVersions) {
-            $scope.fileInfo.selectedIds = openedVersions.map(function (openedVersion, index) {
-                if (index == 0) {
-                    $scope.fileInfo.id = $stateParams['selectedIds'] + ":" + openedVersion.versionTag;
-                }
-                return $stateParams['selectedIds'] + ":" + openedVersion.versionTag;
-            }).join(',');
-            
-            $scope.openSnowboundViewer();
-        });
-        
-        // Obtains authentication token for ArkCase
-        var ticketInfo = TicketService.getArkCaseTicket();
+                    /**
+                     * Builds the snowbound url based on the parameters passed into the controller state and opens the
+                     * specified document in an iframe which points to snowbound
+                     */
+                    $scope.openSnowboundViewer = function() {
+                        var viewerUrl = SnowboundService.buildSnowboundUrl($scope.ecmFileProperties, $scope.acmTicket, $scope.userId,
+                                $scope.fileInfo);
+                        $scope.documentViewerUrl = $sce.trustAsResourceUrl(viewerUrl);
+                    };
 
-        // Obtains the currently logged in user
-        var userInfo = Authentication.queryUserInfo();
+                    $scope.$bus.subscribe('update-viewer-opened-versions', function(openedVersions) {
+                        $scope.fileInfo.selectedIds = openedVersions.map(function(openedVersion, index) {
+                            if (index == 0) {
+                                $scope.fileInfo.id = $stateParams['selectedIds'] + ":" + openedVersion.versionTag;
+                            }
+                            return $stateParams['selectedIds'] + ":" + openedVersion.versionTag;
+                        }).join(',');
 
-        // Obtains a list of all users in ArkCase
-        var totalUserInfo = LookupService.getUsers();
+                        $scope.openSnowboundViewer();
+                    });
 
-        // Retrieves the properties from the ecmFileService.properties file (including Snowbound configuration)
-        var ecmFileConfig = LookupService.getConfig("ecmFileService");
+                    // Obtains authentication token for ArkCase
+                    var ticketInfo = TicketService.getArkCaseTicket();
 
-        var formsConfig = LookupService.getConfig("acm-forms");
+                    // Obtains the currently logged in user
+                    var userInfo = Authentication.queryUserInfo();
 
+                    // Obtains a list of all users in ArkCase
+                    var totalUserInfo = LookupService.getUsers();
 
-        // Retrieves the metadata for the file which is being opened in the viewer
-        var ecmFileInfo = EcmService.getFile({fileId: $stateParams['id']});
-        var ecmFileEvents = EcmService.getFileEvents({fileId: $stateParams['id']});
-        var ecmFileParticipants = EcmService.getFileParticipants({fileId: $stateParams['id']});
+                    // Retrieves the properties from the ecmFileService.properties file (including Snowbound configuration)
+                    var ecmFileConfig = LookupService.getConfig("ecmFileService");
 
-        $q.all([ticketInfo, userInfo, totalUserInfo, ecmFileConfig,
-                ecmFileInfo.$promise, ecmFileEvents.$promise, ecmFileParticipants.$promise, formsConfig])
-            .then(function (data) {
-                    $scope.acmTicket = data[0].data;
-                    $scope.userId = data[1].userId;
-                    $scope.userList = data[2];
-                    $scope.ecmFileProperties = data[3];
-                    $scope.ecmFile = data[4];
-                    $scope.ecmFileEvents = data[5];
-                    $scope.ecmFileParticipants = data[6];
-                    $scope.formsConfig = data[7];
-                    $timeout(function () {
-                        $scope.$broadcast('document-data', $scope.ecmFile);
-                    }, 1000);
+                    var formsConfig = LookupService.getConfig("acm-forms");
 
-                var key = $scope.ecmFile.fileType + ".name";
-                // Search for descriptive file type in acm-forms.properties
-                $scope.fileType = $scope.formsConfig[key];
-                if ($scope.fileType === undefined){
-                    // If descriptive file type does not exist, fallback to previous raw file type
-                    $scope.fileType = $scope.ecmFile.fileType;
-                }
+                    // Retrieves the metadata for the file which is being opened in the viewer
+                    var ecmFileInfo = EcmService.getFile({
+                        fileId : $stateParams['id']
+                    });
+                    var ecmFileEvents = EcmService.getFileEvents({
+                        fileId : $stateParams['id']
+                    });
+                    var ecmFileParticipants = EcmService.getFileParticipants({
+                        fileId : $stateParams['id']
+                    });
 
-                $scope.mediaType = $scope.ecmFile.fileActiveVersionMimeType.indexOf("video") === 0 ? "video" : ($scope.ecmFile.fileActiveVersionMimeType.indexOf("audio") === 0 ? "audio" : "other"); 
-                
-                if ($scope.mediaType === "video" || $scope.mediaType === "audio") {
-                    $scope.config = {
-                            sources: [
-                                {src: $sce.trustAsResourceUrl('api/latest/plugin/ecm/stream/' + $scope.ecmFile.fileId), type: $scope.ecmFile.fileActiveVersionMimeType}
-                            ],
-                            theme: "lib/videogular-themes-default/videogular.css",
-                            plugins: {
-                                poster: "branding/loginlogo.png"
-                            },
-                            autoPlay: false
-                        };
-                    $scope.showVideoPlayer = true;
-                } else {
-                    // Opens the selected document in the snowbound viewer
-                    $scope.openSnowboundViewer();
-                }
-                
-                
-                }
-            );
-    }
-]);
+                    $q.all(
+                            [ ticketInfo, userInfo, totalUserInfo, ecmFileConfig, ecmFileInfo.$promise, ecmFileEvents.$promise,
+                                    ecmFileParticipants.$promise, formsConfig ]).then(
+                            function(data) {
+                                $scope.acmTicket = data[0].data;
+                                $scope.userId = data[1].userId;
+                                $scope.userList = data[2];
+                                $scope.ecmFileProperties = data[3];
+                                $scope.ecmFile = data[4];
+                                $scope.ecmFileEvents = data[5];
+                                $scope.ecmFileParticipants = data[6];
+                                $scope.formsConfig = data[7];
+                                $timeout(function() {
+                                    $scope.$broadcast('document-data', $scope.ecmFile);
+                                }, 1000);
+
+                                var key = $scope.ecmFile.fileType + ".name";
+                                // Search for descriptive file type in acm-forms.properties
+                                $scope.fileType = $scope.formsConfig[key];
+                                if ($scope.fileType === undefined) {
+                                    // If descriptive file type does not exist, fallback to previous raw file type
+                                    $scope.fileType = $scope.ecmFile.fileType;
+                                }
+
+                                $scope.mediaType = $scope.ecmFile.fileActiveVersionMimeType.indexOf("video") === 0 ? "video"
+                                        : ($scope.ecmFile.fileActiveVersionMimeType.indexOf("audio") === 0 ? "audio" : "other");
+
+                                if ($scope.mediaType === "video" || $scope.mediaType === "audio") {
+                                    $scope.config = {
+                                        sources : [ {
+                                            src : $sce.trustAsResourceUrl('api/latest/plugin/ecm/stream/' + $scope.ecmFile.fileId),
+                                            type : $scope.ecmFile.fileActiveVersionMimeType
+                                        } ],
+                                        theme : "lib/videogular-themes-default/videogular.css",
+                                        plugins : {
+                                            poster : "branding/loginlogo.png"
+                                        },
+                                        autoPlay : false
+                                    };
+                                    $scope.showVideoPlayer = true;
+                                } else {
+                                    // Opens the selected document in the snowbound viewer
+                                    $scope.openSnowboundViewer();
+                                }
+
+                            });
+                } ]);
