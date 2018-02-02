e52e2571981b4250b573d5995802d76cf12ae686
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2018-02-02 17:16:39 +0100

    Returning deleted code
    Former-commit-id: 4443a5b3b8e89c61931ee6fc45cf38d573732dcd

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/object-authorization/object.authorization.roles.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/object-authorization/object.authorization.roles.directive.js
@@ -157,3 +157,22 @@ angular.module('directives').directive('objectAuthorizationRoles',
                 }
             };
         } ]);
+
+angular.module('directives').filter('orderObjectBy', function() {
+    return function(input) {
+        if (!angular.isObject(input))
+            return input;
+
+        var array = [];
+        for ( var objectKey in input) {
+            array.push(input[objectKey]);
+        }
+
+        array.sort(function(a, b) {
+            a = a["name"];
+            b = b["name"];
+            return a > b ? 1 : a < b ? -1 : 0;
+        });
+        return array;
+    }
+});
