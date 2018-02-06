d13e7b4936101c730f08ccb4810db1c67851c23b
ncuculova <nadica.cuculova@armedia.com>
2018-02-06 13:44:40 +0100

    AFDP-3992: Initiate sync methods refactored
    Former-commit-id: 94ab1fbfc8ca33b89674d4fc489e0f3aa3db5f1e

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/web/api/LdapSyncAPIController.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/web/api/LdapSyncAPIController.java
@@ -2,6 +2,7 @@ package com.armedia.acm.services.users.web.api;
 
 import com.armedia.acm.services.users.service.ldap.LdapSyncService;
 import com.armedia.acm.spring.SpringContextHolder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -27,12 +28,12 @@ public class LdapSyncAPIController
     @ResponseBody
     public ResponseEntity initiatePartialSync(@PathVariable String directory, Authentication authentication)
     {
-        LdapSyncService ldapSyncService = acmContextHolder.getAllBeansOfType(LdapSyncService.class).
-                get(String.format("%s_ldapSyncJob", directory));
+        LdapSyncService ldapSyncService = acmContextHolder.getAllBeansOfType(LdapSyncService.class)
+                .get(String.format("%s_ldapSyncJob", directory));
         if (ldapSyncService != null)
         {
             log.debug("User [{}] initiated partial sync of directory [{}]", authentication.getName(), directory);
-            ldapSyncService.initiatePartialSync(authentication.getName());
+            ldapSyncService.initiateSync(authentication.getName(), false);
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(Collections.singletonMap("message", "Service for directory [ " + directory + " ] not found"),
@@ -43,12 +44,12 @@ public class LdapSyncAPIController
     @ResponseBody
     public ResponseEntity initiateFullSync(@PathVariable String directory, Authentication authentication)
     {
-        LdapSyncService ldapSyncService = acmContextHolder.getAllBeansOfType(LdapSyncService.class).
-                get(String.format("%s_ldapSyncJob", directory));
+        LdapSyncService ldapSyncService = acmContextHolder.getAllBeansOfType(LdapSyncService.class)
+                .get(String.format("%s_ldapSyncJob", directory));
         if (ldapSyncService != null)
         {
             log.debug("User [{}] initiated full sync of directory [{}]", authentication.getName(), directory);
-            ldapSyncService.initiateFullSync(authentication.getName());
+            ldapSyncService.initiateSync(authentication.getName(), true);
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(Collections.singletonMap("message", "Service for directory [ " + directory + " ] not found"),
