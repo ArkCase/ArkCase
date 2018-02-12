d8dca19c44f8afcf1a0075b440825bf945e5771b
Amarnath Beedimane Hanumantharaya <amar.hanumantharaya@armedia.com>
2018-02-12 12:12:13 -0500

    fix for valid url
    Former-commit-id: 7ca97ae79c278347590e1b1012627ec07ad4635a

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-urls-modal.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/people/controllers/components/person-urls-modal.client.controller.js
@@ -10,6 +10,19 @@ angular.module('people').controller(
                         return contactMethodTypes;
                     });
 
+                    //This function checks for Url is  valid or not
+                    //You can add patterns if more complicated valid  url is required.
+
+                    function validate() {
+                        var urls = document.getElementById("urls").value;
+                        var pattern = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+                        if (pattern.test(urls)) {
+                            return true;
+                        }
+                        alert("Url is not valid! Please type valid Url");
+                        return false;
+
+                    }
                     $scope.url = params.url;
                     $scope.isEdit = params.isEdit;
                     $scope.isDefault = params.isDefault;
@@ -19,10 +32,12 @@ angular.module('people').controller(
                         $modalInstance.dismiss('Cancel');
                     };
                     $scope.onClickOk = function() {
-                        $modalInstance.close({
-                            url : $scope.url,
-                            isDefault : $scope.isDefault,
-                            isEdit : $scope.isEdit
-                        });
+                        if (validate()) {
+                            $modalInstance.close({
+                                url : $scope.url,
+                                isDefault : $scope.isDefault,
+                                isEdit : $scope.isEdit
+                            });
+                        }
                     };
                 } ]);
