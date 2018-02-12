85dd7c2075c6cbd4fb7bdb0d4cabc5c9f047ac70
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2018-02-12 14:08:15 +0100

    AFDP-3259 Ldap user management scroll - 3 panels
    Former-commit-id: e036a6870d3231060a516549a4dcdf907980dd09

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/scroll/scroll.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/scroll/scroll.client.directive.js
@@ -5,11 +5,9 @@ angular.module('directives').directive('scroll', [ 'UtilService', function(Util)
             panelPagination : "@?"
         },
         link : function(scope, elem, attr) {
-            var scrollElement = angular.element(document.querySelector(".scroll"));
-
-            scrollElement.bind("scroll", function() {
-                var scrolledpx = scrollElement.context.clientHeight + scrollElement.context.scrollTop;
-                if (scrolledpx === scrollElement.context.scrollHeight && !Util.isEmpty(scope.panelPagination)) {
+            elem.bind("scroll", function() {
+                var scrolledpx = elem.context.clientHeight + elem.context.scrollTop;
+                if (scrolledpx === elem.context.scrollHeight && !Util.isEmpty(scope.panelPagination)) {
                     scope.$bus.publish(scope.panelPagination + "Scroll");
                 }
             });
