a6ca47a8ed0f5d36ef5246693aa5bdd466cf8a5d
Dimitar Stefanovski <dimitar.stefanovski@armedia.com>
2018-02-12 15:39:55 +0100

    AFDP-3259 Generic events for scroll
    Former-commit-id: 540be18e5c6460733e80f367e45cfe55f1a4e8bb

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/scroll/scroll.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/scroll/scroll.client.directive.js
@@ -2,13 +2,13 @@ angular.module('directives').directive('scroll', [ 'UtilService', function(Util)
     return {
         restrict : 'A',
         scope : {
-            panelPagination : "@?"
+            panelNameAlias : "@?"
         },
         link : function(scope, elem, attr) {
             elem.bind("scroll", function() {
                 var scrolledpx = elem.context.clientHeight + elem.context.scrollTop;
-                if (scrolledpx === elem.context.scrollHeight && !Util.isEmpty(scope.panelPagination)) {
-                    scope.$bus.publish(scope.panelPagination + "Scroll");
+                if (scrolledpx === elem.context.scrollHeight && !Util.isEmpty(scope.panelNameAlias)) {
+                    scope.$bus.publish(scope.panelNameAlias + "Scroll");
                 }
             });
         }
