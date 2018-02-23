55645a3998e22e1dec6f0bffceb241231c7b6e73
ARMEDIA\ana.serafimoska <ana.serafimoska@armedia.com>
2018-02-23 13:13:11 +0100

    Disabled save button
    Former-commit-id: f10cd73c650b1f9ab4313f2dbb2f3ffaf468eba6

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/security-email-sender-configuration.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/security-email-sender-configuration.client.controller.js
@@ -1,88 +1,69 @@
 'use strict';
-angular
-        .module('admin')
-        .controller(
-                'Admin.SecurityEmailSenderConfigurationController',
-                [
-                        '$scope',
-                        'Admin.EmailSenderConfigurationService',
-                        'MessageService',
-                        '$translate',
-                        function($scope, EmailSenderConfigurationService, MessageService, $translate) {
-                            $scope.emailSenderConfigDataModel = {};
+angular.module('admin').controller(
+        'Admin.SecurityEmailSenderConfigurationController',
+        [ '$scope', 'Admin.EmailSenderConfigurationService', 'MessageService', '$translate',
+                function($scope, EmailSenderConfigurationService, MessageService, $translate) {
+                    $scope.emailSenderConfigDataModel = {};
+                    $scope.isSmtpValid = null;
+
+                    $scope.serverTypeSelectOptions = [ {
+                        value : 'smtp',
+                        label : 'admin.security.emailConfiguration.emailConfigForm.serverTypeOptions.smtp'
+                    }, {
+                        value : 'outlook',
+                        label : 'admin.security.emailConfiguration.emailConfigForm.serverTypeOptions.outlook'
+                    } ];
+
+                    $scope.encryptionSelectOptions = [ {
+                        value : 'off',
+                        label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.off'
+                    }, {
+                        value : 'ssl-tls',
+                        label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.ssl-tls'
+                    }, {
+                        value : 'starttls',
+                        label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.starttls'
+                    } ];
+
+                    EmailSenderConfigurationService.getEmailSenderConfiguration().then(function(res) {
+                        $scope.emailSenderConfigDataModel = res.data;
+                    });
+
+                    $scope.changeEncryption = function(encryptionType) {
+                        switch (encryptionType) {
+                        case 'off':
+                            $scope.emailSenderConfigDataModel.port = 25;
+                            break;
+                        case 'ssl-tls':
+                            $scope.emailSenderConfigDataModel.port = 465;
+                            break;
+                        case 'starttls':
+                            $scope.emailSenderConfigDataModel.port = 587;
+                        }
+                    };
+
+                    $scope.validateSmtpConfiguration = function(smtpConfiguration) {
+                        EmailSenderConfigurationService.validateSmtpConfiguration(smtpConfiguration).then(function(res) {
+                            if (res.data && $scope.emailSenderConfigDataModel.host) {
+                                $scope.isSmtpValid = true;
+                            } else {
+                                $scope.isSmtpValid = false;
+                            }
+                        }, function(err) {
+                            MessageService.errorAction();
+                        });
+                    };
+
+                    $scope.save = function() {
+                        EmailSenderConfigurationService.saveEmailSenderConfiguration($scope.emailSenderConfigDataModel).then(function(res) {
+                            MessageService.succsessAction();
                             $scope.isSmtpValid = null;
-                            $scope.showMessage = false;
-
-                            $scope.serverTypeSelectOptions = [ {
-                                value : 'smtp',
-                                label : 'admin.security.emailConfiguration.emailConfigForm.serverTypeOptions.smtp'
-                            }, {
-                                value : 'outlook',
-                                label : 'admin.security.emailConfiguration.emailConfigForm.serverTypeOptions.outlook'
-                            } ];
-
-                            $scope.encryptionSelectOptions = [ {
-                                value : 'off',
-                                label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.off'
-                            }, {
-                                value : 'ssl-tls',
-                                label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.ssl-tls'
-                            }, {
-                                value : 'starttls',
-                                label : 'admin.security.emailConfiguration.emailConfigForm.encryptionOptions.starttls'
-                            } ];
-
-                            EmailSenderConfigurationService.getEmailSenderConfiguration().then(function(res) {
-                                $scope.emailSenderConfigDataModel = res.data;
-                            });
-
-                            $scope.changeEncryption = function(encryptionType) {
-                                switch (encryptionType) {
-                                case 'off':
-                                    $scope.emailSenderConfigDataModel.port = 25;
-                                    break;
-                                case 'ssl-tls':
-                                    $scope.emailSenderConfigDataModel.port = 465;
-                                    break;
-                                case 'starttls':
-                                    $scope.emailSenderConfigDataModel.port = 587;
-                                }
+                        }, function(err) {
+                            if (err.status === 400) {
+                                MessageService.errorAction();
+                            } else {
+                                MessageService.errorAction();
                             }
-
-                            $scope.validateSmtpConfiguration = function(smtpConfiguration) {
-                                EmailSenderConfigurationService.validateSmtpConfiguration(smtpConfiguration).then(function(res) {
-                                    if (res.data && $scope.emailSenderConfigDataModel.host) {
-                                        $scope.isSmtpValid = true;
-                                    } else {
-                                        $scope.isSmtpValid = false;
-                                    }
-                                }, function(err) {
-                                    MessageService.errorAction();
-                                });
-                            };
-
-                            function sendEmailSenderConfiguration() {
-                                EmailSenderConfigurationService.saveEmailSenderConfiguration($scope.emailSenderConfigDataModel).then(
-                                        function(res) {
-                                            MessageService.succsessAction();
-                                            $scope.isSmtpValid = null;
-                                        }, function(err) {
-                                            if (err.status === 400) {
-                                                MessageService.errorAction();
-                                            } else {
-                                                MessageService.errorAction();
-                                            }
-                                        });
-                            }
-
-                            $scope.save = function() {
-                                if ($scope.emailSenderConfigDataModel.allowDocuments
-                                        && !($scope.emailSenderConfigDataModel.allowAttachments || $scope.emailSenderConfigDataModel.allowHyperlinks)) {
-
-                                    $scope.showMessage = true;
-
-                                } else {
-                                    sendEmailSenderConfiguration();
-                                }
-                            };
-                        } ]);
+                        });
+                    };
+                } ]);
