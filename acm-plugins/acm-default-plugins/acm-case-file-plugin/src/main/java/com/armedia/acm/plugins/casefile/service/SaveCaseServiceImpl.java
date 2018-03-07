67c7d8147e098498e9d637c2e1801e7ae49c4ec7
adrijana.maneva <adrijana.maneva@armedia.com>
2018-03-07 16:00:19 +0100

    foia-fdic-demo create casefile(request) with multipart form data (files)
    Former-commit-id: 2e98340de6e280668ed7f46e9f422c968e8454a9

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/service/SaveCaseServiceImpl.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/service/SaveCaseServiceImpl.java
@@ -1,8 +1,14 @@
 package com.armedia.acm.plugins.casefile.service;
 
+import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
+import com.armedia.acm.core.exceptions.AcmUpdateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
 import com.armedia.acm.plugins.casefile.dao.CaseFileDao;
 import com.armedia.acm.plugins.casefile.model.CaseFile;
 import com.armedia.acm.plugins.casefile.pipeline.CaseFilePipelineContext;
+import com.armedia.acm.plugins.ecm.service.AcmFolderService;
+import com.armedia.acm.plugins.ecm.service.EcmFileService;
 import com.armedia.acm.services.pipeline.PipelineManager;
 import com.armedia.acm.services.pipeline.exception.PipelineProcessException;
 
@@ -10,6 +16,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by armdev on 8/29/14.
@@ -19,6 +29,8 @@ public class SaveCaseServiceImpl implements SaveCaseService
     private CaseFileDao caseFileDao;
 
     private PipelineManager<CaseFile, CaseFilePipelineContext> pipelineManager;
+    private EcmFileService ecmFileService;
+    private AcmFolderService acmFolderService;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -42,6 +54,34 @@ public class SaveCaseServiceImpl implements SaveCaseService
         });
     }
 
+    @Override
+    @Transactional
+    public CaseFile saveCase(CaseFile caseFile, List<MultipartFile> files, Authentication authentication, String ipAddress)
+            throws AcmUserActionFailedException,
+            AcmCreateObjectFailedException, AcmUpdateObjectFailedException, AcmObjectNotFoundException, PipelineProcessException,
+            IOException
+    {
+        CaseFile saved = saveCase(caseFile, authentication, ipAddress);
+
+        for (MultipartFile file : files)
+        {
+            if (file != null)
+            {
+
+                String folderId = saved.getContainer().getAttachmentFolder() == null ? saved.getContainer().getFolder().getCmisFolderId()
+                        : saved.getContainer().getAttachmentFolder().getCmisFolderId();
+
+                log.debug("Uploading document for FOIA Request [{}] as [{}]", saved.getId(), file.getOriginalFilename());
+
+                getEcmFileService().upload(file.getOriginalFilename(), "other", "Document", file.getInputStream(), "",
+                        file.getOriginalFilename(), authentication,
+                        folderId, saved.getObjectType(), saved.getId());
+            }
+        }
+
+        return saved;
+    }
+
     public CaseFileDao getCaseFileDao()
     {
         return caseFileDao;
@@ -60,5 +100,25 @@ public class SaveCaseServiceImpl implements SaveCaseService
     public void setPipelineManager(PipelineManager pipelineManager)
     {
         this.pipelineManager = pipelineManager;
+    }
+
+    public EcmFileService getEcmFileService()
+    {
+        return ecmFileService;
+    }
+
+    public void setEcmFileService(EcmFileService ecmFileService)
+    {
+        this.ecmFileService = ecmFileService;
+    }
+
+    public AcmFolderService getAcmFolderService()
+    {
+        return acmFolderService;
+    }
+
+    public void setAcmFolderService(AcmFolderService acmFolderService)
+    {
+        this.acmFolderService = acmFolderService;
     }
 }
