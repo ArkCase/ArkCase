44c85d8972bd31958c16bb6672da29e56d91cf91
adrijana.maneva <adrijana.maneva@armedia.com>
2018-03-07 16:00:19 +0100

    foia-fdic-demo create casefile(request) with multipart form data (files)
    Former-commit-id: 2e98340de6e280668ed7f46e9f422c968e8454a9

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/api/SaveCaseFileAPIController.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/web/api/SaveCaseFileAPIController.java
@@ -2,6 +2,9 @@ package com.armedia.acm.plugins.casefile.web.api;
 
 import com.armedia.acm.auth.AuthenticationUtils;
 import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmObjectNotFoundException;
+import com.armedia.acm.core.exceptions.AcmUpdateObjectFailedException;
+import com.armedia.acm.core.exceptions.AcmUserActionFailedException;
 import com.armedia.acm.plugins.casefile.model.CaseFile;
 import com.armedia.acm.plugins.casefile.service.SaveCaseService;
 import com.armedia.acm.plugins.casefile.utility.CaseFileEventUtility;
@@ -17,12 +20,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpSession;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping({ "/api/v1/plugin/casefile", "/api/latest/plugin/casefile" })
@@ -77,6 +84,51 @@ public class SaveCaseFileAPIController
         catch (PipelineProcessException | PersistenceException e)
         {
             throw new AcmCreateObjectFailedException("Case File", e.getMessage(), e);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public CaseFile insertCaseMultipart(@RequestPart(name = "casefile") CaseFile in,
+            @RequestPart(name = "files") List<MultipartFile> files, HttpSession session, Authentication auth)
+            throws AcmCreateObjectFailedException, AcmUpdateObjectFailedException, AcmUserActionFailedException, AcmObjectNotFoundException,
+            IOException
+    {
+        String ipAddress = (String) session.getAttribute("acm_ip_address");
+
+        userTrackerService.trackUser(ipAddress);
+        try
+        {
+            boolean isNew = in.getId() == null;
+
+            // explicitly set modifier and modified to trigger transformer to reindex data
+            // fixes problem when some child objects are changed (e.g participants) and solr document is not updated
+            in.setModifier(AuthenticationUtils.getUsername());
+            in.setModified(new Date());
+
+            CaseFile saved = getSaveCaseService().saveCase(in, files, auth, ipAddress);
+
+            // since the approver list is not persisted to the database, we want to send them back to the caller...
+            // the approver list is only here to send to the Activiti engine. After the workflow is started the
+            // approvers are stored in Activiti.
+            saved.setApprovers(in.getApprovers());
+
+            if (isNew)
+            {
+                caseFileEventUtility.raiseEvent(saved, "created", new Date(), ipAddress, auth.getName(), auth);
+                caseFileEventUtility.raiseEvent(saved, saved.getStatus(), new Date(), ipAddress, auth.getName(), auth);
+            }
+            else
+            {
+                caseFileEventUtility.raiseEvent(saved, "updated", new Date(), ipAddress, auth.getName(), auth);
+            }
+
+            return saved;
+        }
+        catch (PipelineProcessException | PersistenceException e)
+        {
+            log.error("Error while saving Case: [{}]", in, e);
+            throw new AcmCreateObjectFailedException("CaseFile", e.getMessage(), e);
         }
     }
 
