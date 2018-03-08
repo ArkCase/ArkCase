05087768294d6708d1ed79e242a94238177929f5
ARMEDIA\ana.serafimoska <ana.serafimoska@armedia.com>
2018-03-08 17:19:03 +0100

    Fix
    Former-commit-id: 24c626ea187c8a22b5c551241cccf0882743e44d

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/transcription-management.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/transcription-management.client.controller.js
@@ -1,31 +1,42 @@
 'use strict';
 
-angular.module('admin').controller(
-        'Admin.TranscriptionManagementController',
-        [ '$scope', 'Admin.TranscriptionManagementService', 'ConfigService', 'MessageService',
-                function($scope, TranscriptionManagementService, ConfigService, MessageService) {
+angular
+        .module('admin')
+        .controller(
+                'Admin.TranscriptionManagementController',
+                [
+                        '$scope',
+                        'Admin.TranscriptionManagementService',
+                        'ConfigService',
+                        'MessageService',
+                        function($scope, TranscriptionManagementService, ConfigService, MessageService) {
 
-                    $scope.isLoading = false;
-                    $scope.transcribeConfigDataModel = {};
-
-                    TranscriptionManagementService.getTranscribeConfiguration().then(function(res) {
-                        $scope.transcribeConfigDataModel = res.data;
-                        $scope.transcriptionForNewVersion = res.data.newTranscriptionForNewVersion;
-                    }, function(err) {
-                        MessageService.error(err.data);
-                    });
-
-                    $scope.saveChanges = function() {
-                        $scope.isLoading = true;
-                        $scope.transcribeConfigDataModel.newTranscriptionForNewVersion = !$scope.transcriptionForNewVersion;
-                        $scope.transcribeConfigDataModel.copyTranscriptionForNewVersion = $scope.transcriptionForNewVersion;
-                        TranscriptionManagementService.saveTranscribeConfiguration($scope.transcribeConfigDataModel).then(function(res) {
                             $scope.isLoading = false;
-                            MessageService.succsessAction();
-                        }, function(err) {
-                            $scope.isLoading = false;
-                            MessageService.errorAction();
-                        });
-                    };
+                            $scope.transcribeConfigDataModel = {};
 
-                } ]);
+                            TranscriptionManagementService.getTranscribeConfiguration().then(function(res) {
+                                $scope.transcribeConfigDataModel = res.data;
+                                $scope.transcriptionForNewVersion = res.data.newTranscriptionForNewVersion;
+                            }, function(err) {
+                                MessageService.error(err.data);
+                            });
+
+                            $scope.saveChanges = function() {
+                                $scope.isLoading = true;
+                                $scope.transcribeConfigDataModel.newTranscriptionForNewVersion = $scope.transcriptionForNewVersion;
+                                $scope.transcribeConfigDataModel.copyTranscriptionForNewVersion = !$scope.transcribeConfigDataModel.newTranscriptionForNewVersion;
+                                TranscriptionManagementService.saveTranscribeConfiguration($scope.transcribeConfigDataModel).then(
+                                        function(res) {
+                                            $scope.isLoading = false;
+                                            MessageService.succsessAction();
+                                        }, function(err) {
+                                            $scope.isLoading = false;
+                                            MessageService.errorAction();
+                                        });
+                            };
+
+                            $scope.checkButtonValue = function(val) {
+                                $scope.transcriptionForNewVersion = val;
+                            }
+
+                        } ]);
