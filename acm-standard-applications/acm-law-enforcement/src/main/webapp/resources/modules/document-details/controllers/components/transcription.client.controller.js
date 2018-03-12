345a608d9881c7c073ff48740b99befd0fdb1692
ARMEDIA\ana.serafimoska <ana.serafimoska@armedia.com>
2018-03-12 11:10:59 +0100

    Create "Transcript" tab with needed components and buttons
    Former-commit-id: 15ada2584a20e08c767696b4a045904ff49a3baf

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/document-details/controllers/components/transcription.client.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('document-details').controller('Document.TranscriptionController', [ '$scope', function($scope) {
+
+    //Resize textarea based on content length
+    var tx = document.getElementsByTagName('textarea');
+    for (var i = 0; i < tx.length; i++) {
+        tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight + tx[i].height) + 'px;overflow-y:hidden;');
+        tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    }
+
+} ]);
