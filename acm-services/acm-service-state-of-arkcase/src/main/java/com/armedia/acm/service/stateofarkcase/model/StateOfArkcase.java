413e3a41157abcbe8b2178a5fa9e6fb48a83dca2
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2018-03-27 18:42:16 +0200

    AFDP-5405 code review fix
    Former-commit-id: 437476374b4bf6bb56227455268622719c648039

--- acm-services/acm-service-state-of-arkcase/src/main/java/com/armedia/acm/service/stateofarkcase/model/StateOfArkcase.java
+++ acm-services/acm-service-state-of-arkcase/src/main/java/com/armedia/acm/service/stateofarkcase/model/StateOfArkcase.java
@@ -1,15 +1,17 @@
 package com.armedia.acm.service.stateofarkcase.model;
 
+import com.armedia.acm.service.stateofarkcase.interfaces.StateOfModule;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StateOfArkcase
 {
     private LocalDateTime dateGenerated;
-    private Map<String, Object> modulesState = new HashMap<>();
+    private Map<String, StateOfModule> modulesState = new HashMap<>();
 
     public LocalDateTime getDateGenerated()
     {
@@ -22,13 +24,23 @@ public class StateOfArkcase
     }
 
     @JsonAnyGetter
-    public Map<String, Object> getModulesState()
+    public Map<String, StateOfModule> getModulesState()
     {
-        return modulesState;
+        return Collections.unmodifiableMap(modulesState);
     }
 
-    public void addModuleState(String moduleName, Object moduleState)
+    public void addModuleState(String moduleName, StateOfModule moduleState)
     {
         modulesState.put(moduleName, moduleState);
+    }
+
+    public boolean containsModuleState(String moduleName)
+    {
+        return modulesState.containsKey(moduleName);
+    }
+
+    public void removeModuleState(String moduleName)
+    {
+        modulesState.remove(moduleName);
     }
 }
