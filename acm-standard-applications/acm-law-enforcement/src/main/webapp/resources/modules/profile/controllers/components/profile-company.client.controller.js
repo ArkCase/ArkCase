a2167aee85eb414c1b8b507cd1d4cfe81880e47b
ivana.shekerova <ivana.shekerova@armedia.com>
2018-03-30 10:38:14 +0200

    changed so that it adds http:// infront of the website, if the user doesn't enter it that way.
    Former-commit-id: 9c321fb966befed832eadae14f391179cfd67aec

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/profile/controllers/components/profile-company.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/profile/controllers/components/profile-company.client.controller.js
@@ -2,58 +2,58 @@
 
 angular.module('profile').controller(
         'Profile.CompanyController',
-        [ '$scope', 'Profile.UserInfoService', 'MessageService', '$translate', '$window',
-                function($scope, UserInfoService, MessageService, $translate, $window) {
+        [ '$scope', '$stateParams', 'Profile.UserInfoService', 'UtilService', function($scope, $stateParams, UserInfoService, Util) {
 
-                    $scope.editDisabled = false;
+            $scope.editDisabled = false;
+            var refresh = function() {
+                UserInfoService.getUserInfo().then(function(data) {
+                    $scope.id = data.userOrgId;
+                    $scope.userId = data.userId;
+                    $scope.profileCompanyName = data.companyName;
+                    $scope.profileCompanyAddress1 = data.firstAddress;
+                    $scope.profileCompanyAddress2 = data.secondAddress;
+                    $scope.profileCompanyCity = data.city;
+                    $scope.profileCompanyState = data.state;
+                    $scope.profileCompanyZip = data.zip;
+                    $scope.profileCompanyMainPhone = data.mainOfficePhone;
+                    $scope.profileCompanyFax = data.fax;
+                    $scope.profileCompanyWebsite = data.website;
+                });
+            };
 
-                    $scope.update = function() {
-                        var profileInfo;
-                        UserInfoService.getUserInfo().then(function(infoData) {
-                            profileInfo = infoData;
-                            profileInfo.companyName = $scope.profileCompanyName;
-                            profileInfo.firstAddress = $scope.profileCompanyAddress1;
-                            profileInfo.secondAddress = $scope.profileCompanyAddress2;
-                            profileInfo.city = $scope.profileCompanyCity;
-                            profileInfo.state = $scope.profileCompanyState;
-                            profileInfo.zip = $scope.profileCompanyZip;
-                            profileInfo.mainOfficePhone = $scope.profileCompanyMainPhone;
-                            profileInfo.fax = $scope.profileCompanyFax;
-                            profileInfo.website = $scope.profileCompanyWebsite;
-                            UserInfoService.updateUserInfo(profileInfo);
+            refresh();
 
-                        });
-                        $scope.disableEdit();
-                    };
+            $scope.update = function() {
+                var profileInfo;
+                UserInfoService.getUserInfo().then(function(infoData) {
+                    profileInfo = infoData;
+                    profileInfo.companyName = $scope.profileCompanyName;
+                    profileInfo.firstAddress = $scope.profileCompanyAddress1;
+                    profileInfo.secondAddress = $scope.profileCompanyAddress2;
+                    profileInfo.city = $scope.profileCompanyCity;
+                    profileInfo.state = $scope.profileCompanyState;
+                    profileInfo.zip = $scope.profileCompanyZip;
+                    profileInfo.mainOfficePhone = $scope.profileCompanyMainPhone;
+                    profileInfo.fax = $scope.profileCompanyFax;
 
-                    $scope.enableEdit = function() {
-                        $scope.editDisabled = true;
-                    };
-
-                    $scope.disableEdit = function() {
-                        $scope.editDisabled = false;
-                    };
-
-                    UserInfoService.getUserInfo().then(function(data) {
-                        $scope.id = data.userOrgId;
-                        $scope.userId = data.userId;
-                        $scope.profileCompanyName = data.companyName;
-                        $scope.profileCompanyAddress1 = data.firstAddress;
-                        $scope.profileCompanyAddress2 = data.secondAddress;
-                        $scope.profileCompanyCity = data.city;
-                        $scope.profileCompanyState = data.state;
-                        $scope.profileCompanyZip = data.zip;
-                        $scope.profileCompanyMainPhone = data.mainOfficePhone;
-                        $scope.profileCompanyFax = data.fax;
-                        $scope.profileCompanyWebsite = data.website;
+                    var res = $scope.profileCompanyWebsite.toLowerCase().match("(http://|https://).+");
+                    if (!Util.isEmpty(res)) {
+                        profileInfo.website = $scope.profileCompanyWebsite;
+                    } else {
+                        profileInfo.website = "http://" + $scope.profileCompanyWebsite;
+                    }
+                    UserInfoService.updateUserInfo(profileInfo).then(function() {
+                        refresh();
                     });
+                });
+                $scope.disableEdit();
+            };
 
-                    $scope.openUrl = function(websiteUrl) {
-                        var res = websiteUrl.match("(http://|https://).+");
-                        if (res == null) {
-                            websiteUrl = "http://" + websiteUrl;
-                        }
-                        $window.open(websiteUrl, '_blank');
-                    };
+            $scope.enableEdit = function() {
+                $scope.editDisabled = true;
+            };
 
-                } ]);
+            $scope.disableEdit = function() {
+                $scope.editDisabled = false;
+            };
+        } ]);
