5a9a1e1524123449e2a480b458c23ce949e44a82
David Miller <david.miller@armedia.com>
2018-03-31 14:22:55 -0400

    Sample code for Spring JMS / RestTemplate
    Former-commit-id: c83892d2d2bccd76832422e57e8f6d95e8ee84fb

---
+++ acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/service/SlowWebConsumerConfig.java
@@ -0,0 +1,10 @@
+package com.armedia.acm.plugins.ecm.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class SlowWebConsumerConfig
+{
+}
