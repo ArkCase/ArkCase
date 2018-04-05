81320750489a77898652fe3399406bcad24de82e
David Miller <david.miller@armedia.com>
2018-04-05 17:59:34 -0400

    AFDP-5061 ensure content file posts to Solr are really retryable; restrict the list of retryable errors
    Former-commit-id: 629913d14ff907ef6b644a3f9a2c1d4c0f154b69

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/SolrPostClient.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/SolrPostClient.java
@@ -1,21 +1,14 @@
 package com.armedia.acm.services.search.service.solr;
 
 import com.armedia.acm.services.search.model.SolrCore;
-import com.armedia.acm.services.search.model.solr.SolrContentDocument;
 
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
-
-import java.io.InputStream;
 
 public interface SolrPostClient
 {
 
     @Retryable(maxAttempts = 10, value = SolrPostException.class, backoff = @Backoff(delay = 3000, multiplier = 1.5, random = true))
     void sendToSolr(SolrCore core, String json) throws SolrPostException;
-
-    @Retryable(maxAttempts = 10, value = SolrPostException.class, backoff = @Backoff(delay = 3000, multiplier = 1.5, random = true))
-    void sendToSolr(SolrCore core, SolrContentDocument solrContentDocument, InputStream stream, long contentLength)
-            throws SolrPostException;
 
 }
