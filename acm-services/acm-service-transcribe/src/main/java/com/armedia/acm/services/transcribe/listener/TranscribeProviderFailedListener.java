ba980f4f46668a20c814a66d545a5202606c4aab
Riste Tutureski <riste.tutureski@armedia.com>
2018-04-11 14:08:07 +0200

    AFDP-5520: Create Transcribe Events and publish them on appropriate place
    Former-commit-id: 7bf69050606e9a698d1cbf1ece9c82fb7348fefc

---
+++ acm-services/acm-service-transcribe/src/main/java/com/armedia/acm/services/transcribe/listener/TranscribeProviderFailedListener.java
@@ -0,0 +1,62 @@
+package com.armedia.acm.services.transcribe.listener;
+
+import com.armedia.acm.data.AuditPropertyEntityAdapter;
+import com.armedia.acm.services.transcribe.exception.SaveTranscribeException;
+import com.armedia.acm.services.transcribe.model.Transcribe;
+import com.armedia.acm.services.transcribe.model.TranscribeConstants;
+import com.armedia.acm.services.transcribe.model.TranscribeProviderFailedEvent;
+import com.armedia.acm.services.transcribe.service.ArkCaseTranscribeService;
+import org.mule.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * Created by Riste Tutureski <riste.tutureski@armedia.com> on 04/02/2018
+ */
+public class TranscribeProviderFailedListener implements ApplicationListener<TranscribeProviderFailedEvent>
+{
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private ArkCaseTranscribeService arkCaseTranscribeService;
+    private AuditPropertyEntityAdapter auditPropertyEntityAdapter;
+
+    @Override
+    public void onApplicationEvent(TranscribeProviderFailedEvent event)
+    {
+        if (event != null && event.isSucceeded())
+        {
+            getAuditPropertyEntityAdapter().setUserId(TranscribeConstants.TRANSCRIBE_SYSTEM_USER);
+
+            Transcribe transcribe = event.getSource();
+            try
+            {
+                getArkCaseTranscribeService().fail(transcribe.getId());
+            }
+            catch (SaveTranscribeException e)
+            {
+                LOG.error("Could not set as failed Transcribe with ID=[{}]. REASON=[{}]", transcribe.getId(), e.getMessage());
+            }
+        }
+    }
+
+    public ArkCaseTranscribeService getArkCaseTranscribeService()
+    {
+        return arkCaseTranscribeService;
+    }
+
+    public void setArkCaseTranscribeService(ArkCaseTranscribeService arkCaseTranscribeService)
+    {
+        this.arkCaseTranscribeService = arkCaseTranscribeService;
+    }
+
+    public AuditPropertyEntityAdapter getAuditPropertyEntityAdapter()
+    {
+        return auditPropertyEntityAdapter;
+    }
+
+    public void setAuditPropertyEntityAdapter(AuditPropertyEntityAdapter auditPropertyEntityAdapter)
+    {
+        this.auditPropertyEntityAdapter = auditPropertyEntityAdapter;
+    }
+}
