211fadcba4172d834e8829fa0f20729634a00a1d
adrijana.maneva <adrijana.maneva@armedia.com>
2018-04-25 00:45:42 +0200

    Holiday Schedule configuration improvements
    Former-commit-id: f76e177d820036e0aa5881d6e04ebcd9e926ef77

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/due-date.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/due-date.client.service.js
@@ -13,7 +13,12 @@
 angular.module('services').service('DueDate.Service', function() {
 
     return ({
-        dueDateWorkingDays : dueDateWorkingDays
+        dueDateWorkingDays : dueDateWorkingDays,
+        dueDateWithWeekends : dueDateWithWeekends,
+        workingDays : workingDays,
+        workingDaysWithWeekends : workingDaysWithWeekends,
+        daysLeft : daysLeft,
+        daysLeftWithWeekends : daysLeftWithWeekends
     });
 
     function dueDateWorkingDays(startDate, days, holidays) {
@@ -22,17 +27,90 @@ angular.module('services').service('DueDate.Service', function() {
         while (count < days) {
             momentObject.add(1, 'days');
 
-            if (momentObject.isoWeekday() !== 6 && momentObject.isoWeekday() !== 7) {
-                var holidayDate = _.find(holidays, function(holiday) {
-                    return holiday.holidayDate === momentObject.format("YYYY-MM-DD");
-                });
-                if (holidayDate === undefined) {
-                    count += 1;
-                }
+            if (!isWeekend(momentObject) && !isHoliday(holidays, momentObject)) {
+                count += 1;
             }
 
         }
         return momentObject.format("YYYY-MM-DD");
+    }
+
+    function dueDateWithWeekends(startDate, days, holidays) {
+        var momentObject = moment(startDate).utc();
+        var count = 0;
+        while (count < days) {
+            momentObject.add(1, 'days');
+
+            if (!isHoliday(holidays, momentObject)) {
+                count += 1;
+            }
+
+        }
+        return momentObject.format("YYYY-MM-DD");
+    }
+
+    function workingDays(startDate, holidays) {
+        var momentObject = moment(startDate).utc();
+        var today = moment();
+        var days = 0;
+        while (momentObject < today) {
+            momentObject.add(1, 'days');
+
+            if (!isWeekend(momentObject) && !isHoliday(holidays, momentObject)) {
+                days += 1;
+            }
+        }
+        return days;
+    }
+
+    function workingDaysWithWeekends(startDate, holidays) {
+        var momentObject = moment(startDate).utc();
+        var today = moment();
+        var days = 0;
+        while (momentObject < today) {
+            momentObject.add(1, 'days');
+
+            if (!isHoliday(holidays, momentObject)) {
+                days += 1;
+            }
+        }
+        return days;
+    }
+
+    function daysLeft(holidays, dueDate) {
+        var dueDate = moment(dueDate).utc();
+        var momentDate = moment();
+        var days = 0;
+        while (momentDate < dueDate) {
+            momentDate.add(1, 'days');
+            if (!isWeekend(momentDate) && !isHoliday(holidays, momentDate)) {
+                days += 1;
+            }
+        }
+        return days;
+    }
+
+    function daysLeftWithWeekends(holidays, dueDate) {
+        var dueDate = moment(dueDate).utc();
+        var momentDate = moment();
+        var days = 0;
+        while (momentDate < dueDate) {
+            momentDate.add(1, 'days');
+            if (!isHoliday(holidays, momentDate)) {
+                days += 1;
+            }
+        }
+        return days;
+    }
+
+    function isWeekend(momentObject) {
+        return momentObject.isoWeekday() === 6 || momentObject.isoWeekday() === 7;
+    }
+
+    function isHoliday(holidays, momentObject) {
+        return _.find(holidays, function(holiday) {
+            return holiday.holidayDate === momentObject.format("YYYY-MM-DD");
+        }) !== undefined;
     }
 
 });
