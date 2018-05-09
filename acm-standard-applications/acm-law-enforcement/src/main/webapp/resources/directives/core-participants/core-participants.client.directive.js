2a321ee42e07f5c5f476b3e3fce158bc7c1bead0
Marjan Trifunov <marjan.trifunov@armedia.com>
2018-05-09 10:55:20 +0200

    remove version from pom + participant merge fix
    Former-commit-id: 5728ad85ef44e26726091f80a0e84facab6810df

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
@@ -257,18 +257,12 @@ angular.module('directives').directive(
                             };
 
                             scope.deleteRow = function(rowEntity) {
-
-                                if (rowEntity.participantType == typeOwningGroup) {
-                                    MessageService.error($translate.instant("common.directive.coreParticipants.message.error.owninggroupDelete"));
-                                } else if (rowEntity.participantType == typeAssignee) {
-                                    MessageService.error($translate.instant("common.directive.coreParticipants.message.error.assigneeDelete"));
-                                } else {
-                                    gridHelper.deleteRow(rowEntity);
-                                    var id = Util.goodMapValue(rowEntity, "id", 0);
-                                    if (0 < id) { //do not need to call service when deleting a new row
-                                        saveObjectInfoAndRefresh();
-                                    }
-                            };
+                                gridHelper.deleteRow(rowEntity);
+                                var id = Util.goodMapValue(rowEntity, "id", 0);
+                                if (0 < id) { //do not need to call service when deleting a new row
+                                    saveObjectInfoAndRefresh();
+                                }
+                            }
 
                             scope.onClickReplaceChildrenParticipants = function() {
                                 var len = scope.objectInfo.participants.length;
