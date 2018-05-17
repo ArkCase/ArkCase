cb338bca0d120b475bad27814d6e865d2db4fd54
Lazo Lazarev <lazo.lazarev@armedia.com>
2018-05-17 15:23:08 +0200

    AFDP-5667
    Folder conversion infrastructure with default implementation and
    convertors for .docx, .pptx, .jpeg, .png, .gif file types.
    
    
    Former-commit-id: e96c4d7d32fd231599bd05f826b7b3951bafa63a

---
+++ acm-services/acm-service-convert-folder/src/main/java/com/armedia/acm/convertfolder/ConversionException.java
@@ -0,0 +1,54 @@
+package com.armedia.acm.convertfolder;
+
+/*-
+ * #%L
+ * ACM Service: Folder Converting Service
+ * %%
+ * Copyright (C) 2014 - 2018 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+/**
+ * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Apr 26, 2018
+ *
+ */
+public class ConversionException extends Exception
+{
+
+    private static final long serialVersionUID = 7374431669313584823L;
+
+    public ConversionException(String message)
+    {
+        super(message);
+    }
+
+    public ConversionException(Throwable t)
+    {
+        super(t);
+    }
+
+    public ConversionException(String message, Throwable t)
+    {
+        super(message, t);
+    }
+
+}
---
+++ acm-services/acm-service-convert-folder/src/main/java/com/armedia/acm/convertfolder/DocXToPdfConverter.java
@@ -0,0 +1,111 @@
+package com.armedia.acm.convertfolder;
+
+/*-
+ * #%L
+ * ACM Service: Folder Converting Service
+ * %%
+ * Copyright (C) 2014 - 2018 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.armedia.acm.plugins.ecm.model.EcmFile;
+
+import org.apache.commons.io.FileUtils;
+import org.docx4j.Docx4J;
+import org.docx4j.convert.out.FOSettings;
+import org.docx4j.model.fields.FieldUpdater;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Lazo Lazarev a.k.a. Lazarius Borg @ zerogravity Apr 26, 2018
+ *
+ */
+public class DocXToPdfConverter extends PdfConverterBase implements FileConverter
+{
+
+    private static final List<String> SUPPORTED_TYPES_EXTENSIONS = Collections.unmodifiableList(Arrays.asList(".docx"));
+
+    /**
+     * Logger instance.
+     */
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /*
+     * (non-Javadoc)
+     * @see com.armedia.acm.convertfolder.FileConverter#getTypeExtensions()
+     */
+    @Override
+    public List<String> getSupportedTypesExtensions()
+    {
+        return SUPPORTED_TYPES_EXTENSIONS;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.armedia.acm.convertfolder.PdfConverterBase#performConversion(java.lang.String, java.io.File,
+     * java.io.File)
+     */
+    @Override
+    protected void performConversion(EcmFile file, String tempUploadFolderPath, File tempOriginFile, File tempPdfFile)
+            throws ConversionException
+    {
+        try (InputStream fileByteStream = new FileInputStream(tempOriginFile))
+        {
+            WordprocessingMLPackage wordProcessor = WordprocessingMLPackage.load(fileByteStream);
+            FieldUpdater updater = new FieldUpdater(wordProcessor);
+            updater.update(true);
+
+            try (FileOutputStream fos = new FileOutputStream(tempPdfFile))
+            {
+                FOSettings settings = Docx4J.createFOSettings();
+                settings.setWmlPackage(wordProcessor);
+                Docx4J.toFO(settings, fos, Docx4J.FLAG_EXPORT_PREFER_XSL);
+            }
+
+        }
+        catch (IOException | Docx4JException e)
+        {
+            FileUtils.deleteQuietly(tempPdfFile);
+            String fileName = file.getFileName() + "." + file.getFileExtension();
+            log.warn("Failed to convert file [{}] with id [{}] of type [{}].", fileName, file.getId(), file.getFileExtension(), e);
+            throw new ConversionException(String.format("Failed to convert file [%s] with id [%s] of type [%s].", fileName, file.getId(),
+                    file.getFileExtension()), e);
+        }
+        finally
+        {
+            FileUtils.deleteQuietly(tempOriginFile);
+        }
+    }
+
+}
