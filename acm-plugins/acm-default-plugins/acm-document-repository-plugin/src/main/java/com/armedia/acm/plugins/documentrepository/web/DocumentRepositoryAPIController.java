5fc58052651b1c8af98a37b7b84342330292d33d
Marjan Trifunov <marjan.trifunov@armedia.com>
2018-05-23 16:51:06 +0200

    AFDP-6076-documents-access-contro-fix
    Former-commit-id: fb205d2c59ed3caab692580c6fcaba03120eb1b7

--- acm-plugins/acm-default-plugins/acm-document-repository-plugin/src/main/java/com/armedia/acm/plugins/documentrepository/web/DocumentRepositoryAPIController.java
+++ acm-plugins/acm-default-plugins/acm-document-repository-plugin/src/main/java/com/armedia/acm/plugins/documentrepository/web/DocumentRepositoryAPIController.java
@@ -43,15 +43,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.TransactionException;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.Objects;
@@ -96,6 +92,7 @@ public class DocumentRepositoryAPIController
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @DecoratedAssignedObjectParticipants
+    @PreAuthorize("hasPermission(#id, 'DOC_REPO', 'getDocumentRepository')")
     @ResponseBody
     public DocumentRepository findById(@PathVariable(value = "id") Long id) throws AcmObjectNotFoundException
     {
