7d62b86171517a1a88f2b283ad3f36db8f30d2fc
Marjan Trifunov <marjan.trifunov@armedia.com>
2018-05-23 16:51:06 +0200

    AFDP-6076-documents-access-contro-fix
    Former-commit-id: fb205d2c59ed3caab692580c6fcaba03120eb1b7

--- acm-plugins/acm-default-plugins/acm-document-repository-plugin/src/main/java/com/armedia/acm/plugins/documentrepository/service/DocumentRepositoryToSolrTransformer.java
+++ acm-plugins/acm-default-plugins/acm-document-repository-plugin/src/main/java/com/armedia/acm/plugins/documentrepository/service/DocumentRepositoryToSolrTransformer.java
@@ -135,7 +135,7 @@ public class DocumentRepositoryToSolrTransformer implements AcmObjectToSolrDocTr
         solr.setDescription_no_html_tags_parseable(in.getDetails());
         solr.setStatus_s(in.getStatus());
 
-        String assigneeUserId = ParticipantUtils.getAssigneeIdFromParticipants(in.getParticipants());
+        String assigneeUserId = ParticipantUtils.getOwnerIdFromParticipants(in.getParticipants());
         solr.setAssignee_s(assigneeUserId);
 
         // needed a _lcs property for sorting
