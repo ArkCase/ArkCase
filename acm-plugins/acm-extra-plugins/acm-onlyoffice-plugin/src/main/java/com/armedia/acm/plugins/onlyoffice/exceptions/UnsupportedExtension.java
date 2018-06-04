73ff4b543762d8c0e9ece9e5cb04efc8284d17b5
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2018-06-04 14:52:47 +0200

    AFDP-6071 improvements
    Former-commit-id: f2b85477baeaa427e5e0f9fbb3a6007f46d740c1

---
+++ acm-plugins/acm-extra-plugins/acm-onlyoffice-plugin/src/main/java/com/armedia/acm/plugins/onlyoffice/exceptions/UnsupportedExtension.java
@@ -0,0 +1,9 @@
+package com.armedia.acm.plugins.onlyoffice.exceptions;
+
+public class UnsupportedExtension extends RuntimeException
+{
+    public UnsupportedExtension(String message)
+    {
+        super(message);
+    }
+}
