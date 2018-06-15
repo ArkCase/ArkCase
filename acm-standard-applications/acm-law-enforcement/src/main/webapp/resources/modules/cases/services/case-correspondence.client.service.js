21f6d30c3d7bc49ed35a21177202d197a348d1eb
Marko Stojanoski <marko.stojanoski@armedia.com>
2018-06-16 00:24:59 +0200

    Move Multi Correspondence Template to Core.
    Former-commit-id: 77659b88a94b2940ae1e90707448844cc0dab842

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cases/services/case-correspondence.client.service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('services').factory('CaseCorrespondence.MultiTemplate', ['$http', function($http) {
+
+    var _generateMultiTemplateCorrespondence = function(templates, parentObjectType, parentObjectId, folderId) {
+        return $http({
+            method: "POST",
+            url: "api/latest/service/correspondence/multitemplate",
+            data: templates,
+            params: {
+                parentObjectType: parentObjectType,
+                parentObjectId: parentObjectId,
+                folderId: folderId
+            },
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    };
+
+    return {
+        generateMultiTemplateCorrespondence: _generateMultiTemplateCorrespondence
+    }
+}]);
