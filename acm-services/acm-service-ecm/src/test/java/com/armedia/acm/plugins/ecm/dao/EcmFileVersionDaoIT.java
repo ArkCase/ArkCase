fc5e511767fa6f966291d3c0d0d45ad627adc610
David Miller <david.miller@armedia.com>
2018-06-25 12:23:14 -0400

    ArkCase 3.2.12 integration test fixes
    Former-commit-id: d3025f4b3523f2aa197f66b3ce74d21be87a8e60

--- acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/dao/EcmFileVersionDaoIT.java
+++ acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/dao/EcmFileVersionDaoIT.java
@@ -56,7 +56,8 @@ import java.time.LocalDateTime;
         "/spring/spring-library-drools-rule-monitor.xml",
         "/spring/spring-library-object-lock.xml",
         "/spring/spring-library-object-converter.xml",
-        "/spring/spring-library-ecm-file-lock.xml" })
+        "/spring/spring-library-ecm-file-lock.xml",
+        "/spring/spring-library-service-data.xml" })
 public class EcmFileVersionDaoIT
 {
 
