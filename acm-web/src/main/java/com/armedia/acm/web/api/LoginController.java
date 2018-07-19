fc64e4a5d29e277c6948dce2d44d58e821dd287f
ncuculova <nadica.cuculova@armedia.com>
2018-07-19 16:55:25 +0200

    AFDP-6234: Fix view name
    Former-commit-id: b43dce3a6be2a3b5d7fd645f67a0d1f7935ea7c9

--- acm-web/src/main/java/com/armedia/acm/web/api/LoginController.java
+++ acm-web/src/main/java/com/armedia/acm/web/api/LoginController.java
@@ -64,7 +64,7 @@ public class LoginController
         else
         {
             loginWarningMessageService.buildModel(model);
-            return "/login";
+            return "login";
         }
     }
 
