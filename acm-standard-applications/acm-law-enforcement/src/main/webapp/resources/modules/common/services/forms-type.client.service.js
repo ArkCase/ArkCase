73ea4cb34cd4d4471d6ed3c18b1a2dd876454c05
ivana.shekerova <ivana.shekerova@armedia.com>
2018-07-26 13:51:55 +0200

    added configuration in the admin module for the type of the forms (angular or frevvo)
    Former-commit-id: 4bf7bd464ff60b695d9a8120ef6038eefab7602f

---
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/forms-type.client.service.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name services:FormsType.Service
+ *
+ * @description
+ *
+ * {@link https://gitlab.armedia.com/arkcase/ACM3/tree/develop/acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/forms-type.client.service.js modules/common/services/forms-type.client.service.js}
+ *
+ * FormsType.Service provides function which returns boolean whether the type of the forms is angular or not
+ */
+angular.module('services').service('FormsType.Service', [ 'Admin.ApplicationSettingsService', function(ApplicationSettingsService) {
+
+    return ({
+        isAngularFormType: isAngularFormType
+    });
+
+    function isAngularFormType() {
+        var isAngularFormType = true;
+        ApplicationSettingsService.getProperty(ApplicationSettingsService.PROPERTIES.FORMS_TYPE).then(function(response) {
+            var formsType = response.data[ApplicationSettingsService.PROPERTIES.FORMS_TYPE];
+            if (formsType == "angular") {
+                isAngularFormType = true;
+            } else {
+                isAngularFormType = false;
+            }
+        });
+
+        return isAngularFormType;
+    }
+
+} ]);
