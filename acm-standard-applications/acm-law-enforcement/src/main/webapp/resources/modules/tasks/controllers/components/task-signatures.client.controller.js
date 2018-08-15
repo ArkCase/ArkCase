01b7ab1087afda87fd02a8fc7276aa56381d7002
stefan.sanevski <stefan.sanevski@armedia.com>
2018-08-15 10:31:41 +0200

    AFDP-5853 - Signature is not displayed in the eSignature table after the task is signed.
    Former-commit-id: 33a308b82ad055669ed7cc2665b9e249fa1a2be9

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-signatures.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/tasks/controllers/components/task-signatures.client.controller.js
@@ -1,49 +1,45 @@
 'use strict';
 
 angular.module('tasks').controller(
-    'Tasks.SignaturesController',
-    ['$rootScope', '$scope', '$stateParams', '$q', 'UtilService', 'ConfigService', 'ObjectService', 'Object.SignatureService', 'Task.InfoService', 'Helper.UiGridService', 'Helper.ObjectBrowserService',
-        function ($rootScope, $scope, $stateParams, $q, Util, ConfigService, ObjectService, ObjectSignatureService, TaskInfoService, HelperUiGridService, HelperObjectBrowserService) {
+        'Tasks.SignaturesController',
+        [ '$scope', '$stateParams', '$q', 'UtilService', 'ConfigService', 'ObjectService', 'Object.SignatureService', 'Task.InfoService', 'Helper.UiGridService', 'Helper.ObjectBrowserService',
+                function($scope, $stateParams, $q, Util, ConfigService, ObjectService, ObjectSignatureService, TaskInfoService, HelperUiGridService, HelperObjectBrowserService) {
 
-            var componentHelper = new HelperObjectBrowserService.Component({
-                moduleId: "tasks",
-                componentId: "signatures",
-                scope: $scope,
-                stateParams: $stateParams,
-                retrieveObjectInfo: TaskInfoService.getTaskInfo,
-                validateObjectInfo: TaskInfoService.validateTaskInfo,
-                onConfigRetrieved: function (componentConfig) {
-                    return onConfigRetrieved(componentConfig);
-                }
-            });
-
-            $rootScope.$on('task-signed', function () {
-                $scope.retrieveGridData();
-            });
-
-            var gridHelper = new HelperUiGridService.Grid({
-                scope: $scope
-            });
-
-            var onConfigRetrieved = function (config) {
-                $scope.config = config;
-                gridHelper.setColumnDefs(config);
-                gridHelper.setBasicOptions(config);
-                gridHelper.disableGridScrolling(config);
-                gridHelper.showUserFullNames();
-
-                $scope.retrieveGridData();
-            };
-
-            $scope.retrieveGridData = function () {
-                if (Util.goodPositive(componentHelper.currentObjectId, false)) {
-                    var promiseQueryAudit = ObjectSignatureService.findSignatures(ObjectService.ObjectTypes.TASK, componentHelper.currentObjectId);
-
-                    $q.all([promiseQueryAudit]).then(function (data) {
-                        var signatures = data[0];
-                        $scope.gridOptions.data = signatures;
-                        $scope.gridOptions.totalItems = signatures.length;
+                    var componentHelper = new HelperObjectBrowserService.Component({
+                        moduleId: "tasks",
+                        componentId: "signatures",
+                        scope: $scope,
+                        stateParams: $stateParams,
+                        retrieveObjectInfo: TaskInfoService.getTaskInfo,
+                        validateObjectInfo: TaskInfoService.validateTaskInfo,
+                        onConfigRetrieved: function(componentConfig) {
+                            return onConfigRetrieved(componentConfig);
+                        }
                     });
-                }
-            };
-        }]);
+
+                    var gridHelper = new HelperUiGridService.Grid({
+                        scope: $scope
+                    });
+
+                    var onConfigRetrieved = function(config) {
+                        $scope.config = config;
+                        gridHelper.setColumnDefs(config);
+                        gridHelper.setBasicOptions(config);
+                        gridHelper.disableGridScrolling(config);
+                        gridHelper.showUserFullNames();
+
+                        $scope.retrieveGridData();
+                    };
+
+                    $scope.retrieveGridData = function() {
+                        if (Util.goodPositive(componentHelper.currentObjectId, false)) {
+                            var promiseQueryAudit = ObjectSignatureService.findSignatures(ObjectService.ObjectTypes.TASK, componentHelper.currentObjectId);
+
+                            $q.all([ promiseQueryAudit ]).then(function(data) {
+                                var signatures = data[0];
+                                $scope.gridOptions.data = signatures;
+                                $scope.gridOptions.totalItems = signatures.length;
+                            });
+                        }
+                    };
+                } ]);
