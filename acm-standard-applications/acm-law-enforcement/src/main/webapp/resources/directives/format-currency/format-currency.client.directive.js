3181a6c9da0e8f607fb8793aad0fd6e711401b31
ARMEDIA\ana.serafimoska <ana.serafimoska@armedia.com>
2018-08-17 13:31:44 +0200

    Change the regex to match amount with 2 decimal points and add additional events needed for formatting the amount
    Former-commit-id: c1d97c595600bdb3e2f867fdce08893d2bcd4378

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/format-currency/format-currency.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/format-currency/format-currency.client.directive.js
@@ -15,28 +15,62 @@ angular
     .directive('formatCurrency', ['$filter', 'Config.LocaleService', 'UtilService', function ($filter, LocaleService, Util) {
         return {
             require: '?ngModel',
+            scope: {
+                isValidAmount: '=',
+                amount: '='
+            },
             link: function (scope, elem, attrs, ctrl) {
                 if (!ctrl) return;
 
-            var numberRegExp = new RegExp("^[0-9-][.0-9]*$");
-            $(elem).keypress(function(event){
-                if(!numberRegExp.test(event.target.value + event.key)){
-                    return false;
-                }
-            });
+                var numberRegExp = new RegExp("^-?\\d{1,}$|^-?\\d{1,}\\.\\d{1,2}$");
 
-            var currencySymbol = LocaleService.getCurrencySymbol();
+                var keyDownExecuted = false;
+                var strKeyPress = "";
+                var str = "";
+                $(elem).keypress(function(event){
+                    if (!keyDownExecuted) {
+                        strKeyPress = event.target.value;
+                    }else {
+                        return false;
+                    }
+                    keyDownExecuted = true;
+                });
 
-            elem.bind('blur', function(event) {
-                var plainNumber = elem.val().replace(/[^\d|\-+|\.+]/g, '');
-                if(!Util.isEmpty(plainNumber) && plainNumber != 0) {
-                    elem.val($filter("currency")(plainNumber, currencySymbol));
-                }
-                if(plainNumber == 0){
-                    elem.val('');
-                }
-            });
+                $(elem).keyup(function(event){
+                    str = event.target.value;
+                    if((str.length > 1 ||
+                            (str.length === 1 && str !== '-')) &&
+                        !str.endsWith('.') &&
+                        !numberRegExp.test(str)){
+                        str = strKeyPress;
+                        elem.val(str);
+                    }
+                    keyDownExecuted = false;
+                    scope.amount = str;
+                });
 
-        }
-    };
-}]);
+                var currencySymbol = LocaleService.getCurrencySymbol();
+
+                elem.bind('blur', function(event) {
+                    if(!numberRegExp.test(str)){
+                        scope.isValidAmount = false;
+                        return false;
+                    }
+                    scope.isValidAmount = true;
+                    var plainNumber = elem.val().replace(/[^\d|\-+|\.+]/g, '');
+                    if(!Util.isEmpty(plainNumber) && plainNumber != 0) {
+                        elem.val($filter("currency")(plainNumber, currencySymbol));
+                    }
+                    if(plainNumber == 0){
+                        elem.val('');
+                    }
+                });
+
+
+                elem.bind('focus', function(event) {
+                    str = elem.val().replace(/\$|,|\./g, "");
+                    elem.val(str);
+                });
+            }
+        };
+    }]);
