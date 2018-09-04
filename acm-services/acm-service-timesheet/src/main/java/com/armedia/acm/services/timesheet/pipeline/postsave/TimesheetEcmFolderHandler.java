85a0ad3cfeae1fd9ce8714d0b7ea75ca24171420
ivana.shekerova <ivana.shekerova@armedia.com>
2018-09-04 11:14:56 +0200

    changes for new timesheet: added Handlers for TimesheetContainer, TimesheetEcmFolder, TimesheetDocument and TimesheetEvent, new view and controller (frontend) for generating new timesheet

---
+++ acm-services/acm-service-timesheet/src/main/java/com/armedia/acm/services/timesheet/pipeline/postsave/TimesheetEcmFolderHandler.java
@@ -0,0 +1,72 @@
+package com.armedia.acm.services.timesheet.pipeline.postsave;
+
+/*-
+ * #%L
+ * ACM Service: Timesheet
+ * %%
+ * Copyright (C) 2014 - 2018 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.armedia.acm.core.exceptions.AcmCreateObjectFailedException;
+import com.armedia.acm.plugins.ecm.service.EcmFileService;
+import com.armedia.acm.services.pipeline.exception.PipelineProcessException;
+import com.armedia.acm.services.pipeline.handler.PipelineHandler;
+
+import com.armedia.acm.services.timesheet.model.AcmTimesheet;
+import com.armedia.acm.services.timesheet.pipeline.TimesheetPipelineContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Create Alfresco folder on saving a Timesheet.
+ */
+public class TimesheetEcmFolderHandler implements PipelineHandler<AcmTimesheet, TimesheetPipelineContext> {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private EcmFileService ecmFileService;
+
+    @Override
+    public void execute(AcmTimesheet entity, TimesheetPipelineContext ctx) throws PipelineProcessException {
+        log.trace("Timesheet with id [{}] and title [{}] entering TimesheetEcmFolderHandler", entity.getId(), entity.getTitle());
+        if (entity.getEcmFolderPath() != null) {
+            try {
+                String folderId = ecmFileService.createFolder(entity.getEcmFolderPath());
+                entity.getContainer().getFolder().setCmisFolderId(folderId);
+            } catch (AcmCreateObjectFailedException e) {
+                throw new PipelineProcessException(e);
+            }
+
+        } else {
+            log.info("There is no need to create folder");
+        }
+        log.trace("Timesheet with id [{}] and title [{}] exiting TimesheetEcmFolderHandler", entity.getId(), entity.getTitle());
+    }
+
+    @Override
+    public void rollback(AcmTimesheet entity, TimesheetPipelineContext ctx) throws PipelineProcessException {
+        // TODO: implement CMIS folder deletion
+    }
+
+    public void setEcmFileService(EcmFileService ecmFileService) {
+        this.ecmFileService = ecmFileService;
+    }
+}
--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/time-tracking/controllers/components/time-tracking-actions.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/time-tracking/controllers/components/time-tracking-actions.client.controller.js
@@ -1,35 +1,76 @@
 'use strict';
 
 angular.module('time-tracking').controller('TimeTracking.ActionsController',
-        [ '$scope', '$state', '$stateParams', '$translate', 'UtilService', 'ConfigService', 'TimeTracking.InfoService', 'Helper.ObjectBrowserService', function($scope, $state, $stateParams, $translate, Util, ConfigService, TimeTrackingInfoService, HelperObjectBrowserService) {
+    ['$scope', '$state', '$stateParams', '$translate', 'UtilService', 'ConfigService', 'TimeTracking.InfoService', 'Helper.ObjectBrowserService', '$modal', 'FormsType.Service', function ($scope, $state, $stateParams, $translate, Util, ConfigService, TimeTrackingInfoService, HelperObjectBrowserService, $modal, FormsTypeService) {
 
-            new HelperObjectBrowserService.Component({
-                scope: $scope,
-                stateParams: $stateParams,
-                moduleId: "time-tracking",
-                componentId: "actions",
-                retrieveObjectInfo: TimeTrackingInfoService.getTimesheetInfo,
-                validateObjectInfo: TimeTrackingInfoService.validateTimesheet,
-                onObjectInfoRetrieved: function(objectInfo) {
-                    onObjectInfoRetrieved(objectInfo);
+        new HelperObjectBrowserService.Component({
+            scope: $scope,
+            stateParams: $stateParams,
+            moduleId: "time-tracking",
+            componentId: "actions",
+            retrieveObjectInfo: TimeTrackingInfoService.getTimesheetInfo,
+            validateObjectInfo: TimeTrackingInfoService.validateTimesheet,
+            onObjectInfoRetrieved: function (objectInfo) {
+                onObjectInfoRetrieved(objectInfo);
+            }
+        });
+
+        var onObjectInfoRetrieved = function (objectInfo) {
+            var frevvoDateFormat = $translate.instant("common.frevvo.defaultDateFormat");
+            var startDate = moment(objectInfo.startDate).format(frevvoDateFormat);
+
+            $scope.editTimesheetParams = {
+                period: startDate
+            };
+        };
+
+        FormsTypeService.isAngularFormType().then(function (isAngularFormType) {
+            $scope.isAngularFormType = isAngularFormType;
+        });
+
+        FormsTypeService.isFrevvoFormType().then(function (isFrevvoFormType) {
+            $scope.isFrevvoFormType = isFrevvoFormType;
+        });
+
+        $scope.newTimesheet = function () {
+            var params = {
+                isEdit: false
+            };
+            showModal(params);
+        };
+
+        $scope.editTimesheet = function () {
+            $scope.editParams = {
+                isEdit: true,
+                timesheet: $scope.objectInfo
+            };
+            showModal($scope.editParams, true);
+        };
+
+        function showModal(params) {
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'modules/time-tracking/views/components/time-tracking-new-timesheet-modal.client.view.html',
+                controller: 'TimeTracking.NewTimesheetController',
+                size: 'lg',
+                resolve: {
+                    modalParams: function () {
+                        return params;
+                    }
                 }
             });
 
-            var onObjectInfoRetrieved = function(objectInfo) {
-                var frevvoDateFormat = $translate.instant("common.frevvo.defaultDateFormat");
-                var startDate = moment(objectInfo.startDate).format(frevvoDateFormat);
+            modalInstance.result.then(function (data) {
+                //Do nothing
+            });
+        }
 
-                $scope.editTimesheetParams = {
-                    period: startDate
-                };
-            };
+        $scope.refresh = function () {
+            $scope.$emit('report-object-refreshed', $stateParams.id);
+        };
 
-            $scope.refresh = function() {
-                $scope.$emit('report-object-refreshed', $stateParams.id);
-            };
+        $scope.isVisible = function () {
+            return !Util.isEmpty($scope.objectInfo) && $scope.objectInfo.status === 'DRAFT';
+        };
 
-            $scope.isVisible = function() {
-                return !Util.isEmpty($scope.objectInfo) && $scope.objectInfo.status === 'DRAFT';
-            };
-
-        } ]);
+    }]);
