0bc456dd4eb6a898b96b488e5ba1c623fd21bcdf
stefan.sanevski <stefan.sanevski@armedia.com>
2018-09-05 13:49:39 +0200

    AFDP-6521 - Label Management functionality can be exploited for a Directory Traversal attack

--- acm-plugins/acm-default-plugins/acm-admin-plugin/src/main/java/com/armedia/acm/plugins/admin/web/api/LabelManagementUpdateResource.java
+++ acm-plugins/acm-default-plugins/acm-admin-plugin/src/main/java/com/armedia/acm/plugins/admin/web/api/LabelManagementUpdateResource.java
@@ -6,22 +6,22 @@ package com.armedia.acm.plugins.admin.web.api;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -30,8 +30,6 @@ package com.armedia.acm.plugins.admin.web.api;
 import com.armedia.acm.plugins.admin.exception.AcmLabelConfigurationException;
 import com.armedia.acm.services.labels.exception.AcmLabelManagementException;
 import com.armedia.acm.services.labels.service.LabelManagementService;
-
-import org.apache.commons.lang.LocaleUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -45,7 +43,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -59,20 +56,19 @@ import java.util.Set;
 @Controller
 @RequestMapping({
         "/api/v1/plugin/admin",
-        "/api/latest/plugin/admin" })
-public class LabelManagementUpdateResource
-{
+        "/api/latest/plugin/admin"})
+public class LabelManagementUpdateResource {
+    private static final Set<String> ISO_LANGUAGES = new HashSet<String>(Arrays.asList(Locale.getISOLanguages()));
     private Logger log = LoggerFactory.getLogger(getClass());
     private LabelManagementService labelManagementService;
-    private static final Set<String> ISO_LANGUAGES = new HashSet<String>(Arrays.asList(Locale.getISOLanguages()));
 
     @RequestMapping(value = "/labelmanagement/admin-resource", method = RequestMethod.PUT, produces = {
             MediaType.APPLICATION_JSON_UTF8_VALUE,
-            MediaType.TEXT_PLAIN_VALUE })
+            MediaType.TEXT_PLAIN_VALUE})
     @ResponseBody
     public String updateResource(@RequestParam("lang") String lang, @RequestParam("ns") String ns, @RequestBody String resource,
-            HttpServletResponse response) throws IOException, AcmLabelConfigurationException, AcmLabelManagementException {
-        if(ISO_LANGUAGES.contains(lang)) {
+                                 HttpServletResponse response) throws IOException, AcmLabelConfigurationException, AcmLabelManagementException {
+        if (ISO_LANGUAGES.contains(lang)) {
             try {
 
                 JSONObject value = new JSONObject(resource);
@@ -92,14 +88,13 @@ public class LabelManagementUpdateResource
                 log.error(msg, e);
                 throw new AcmLabelManagementException(msg, e);
             }
-        }else{
+        } else {
             throw new AcmLabelManagementException("Language parameter is not valid");
         }
 
     }
 
-    public void setLabelManagementService(LabelManagementService labelManagementService)
-    {
+    public void setLabelManagementService(LabelManagementService labelManagementService) {
         this.labelManagementService = labelManagementService;
     }
 }
