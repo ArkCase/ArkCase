c98f4b897bd2d8424160cbf2a82edf24d3f7c035
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2018-09-08 02:40:35 +0200

    Add assignment on creation new case.

--- acm-services/acm-service-object-history/src/main/java/com/armedia/acm/service/objecthistory/dao/AcmAssignmentDao.java
+++ acm-services/acm-service-object-history/src/main/java/com/armedia/acm/service/objecthistory/dao/AcmAssignmentDao.java
@@ -33,6 +33,10 @@ package com.armedia.acm.service.objecthistory.dao;
 import com.armedia.acm.data.AcmAbstractDao;
 import com.armedia.acm.service.objecthistory.model.AcmAssignment;
 
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.TypedQuery;
+
 /**
  * @author riste.tutureski
  *
@@ -44,6 +48,28 @@ public class AcmAssignmentDao extends AcmAbstractDao<AcmAssignment>
     protected Class<AcmAssignment> getPersistenceClass()
     {
         return AcmAssignment.class;
+    }
+
+    public AcmAssignment findByObjectIdAndType(Long objectId, String objectType)
+    {
+
+        String queryText = "SELECT acmAssignment " +
+                "FROM AcmAssignment acmAssignment " +
+                "WHERE acmAssignment.objectId = :objectId " +
+                "AND acmAssignment.objectType = :objectType";
+
+        TypedQuery<AcmAssignment> query = getEm().createQuery(queryText, AcmAssignment.class);
+
+        query.setParameter("objectId", objectId);
+        query.setParameter("objectType", objectType.toUpperCase());
+
+        return query.getSingleResult();
+    }
+
+    @Transactional
+    public void delete(AcmAssignment acmAssignment)
+    {
+        getEm().remove(acmAssignment);
     }
 
 }
