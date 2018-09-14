0550d09cdda438774374391263340244d2cce3d4
Nebojsha Davidovikj <nebojsha.davidovikj@armedia.com>
2018-09-14 21:54:30 +0200

    AFDP-6123

--- acm-plugins/acm-extra-plugins/acm-onlyoffice-plugin/src/test/java/com/armedia/acm/plugins/onlyoffice/service/JWTSigningServiceImplTest.java
+++ acm-plugins/acm-extra-plugins/acm-onlyoffice-plugin/src/test/java/com/armedia/acm/plugins/onlyoffice/service/JWTSigningServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.armedia.acm.plugins.onlyoffice.service;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 
 public class JWTSigningServiceImplTest {
     private JWTSigningServiceImpl signingService;
@@ -39,6 +39,7 @@ public class JWTSigningServiceImplTest {
                 "  \"iat\": 1516239022\n" +
                 "}";
 
-        assertTrue(signingService.verifyCallback(jsonString, "eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTIzNDU2Nzg5MCIsCiAgIm5hbWUiOiAiSm9obiBEb2UiLAogICJpYXQiOiAxNTE2MjM5MDIyCn0.0Gh1Ilzj9aeD2gxmjTn2U-Yo-NxpW8hMet_CY6bDkKg"));
+        assertTrue(signingService.verifyToken(
+                "eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTIzNDU2Nzg5MCIsCiAgIm5hbWUiOiAiSm9obiBEb2UiLAogICJpYXQiOiAxNTE2MjM5MDIyCn0.0Gh1Ilzj9aeD2gxmjTn2U-Yo-NxpW8hMet_CY6bDkKg"));
     }
 }
