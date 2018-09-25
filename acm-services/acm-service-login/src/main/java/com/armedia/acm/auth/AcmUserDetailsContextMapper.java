c6e7575f72b8441e9ef75744c10248aa140cb16d
ncuculova <nadica.cuculova@armedia.com>
2018-09-25 11:34:26 +0200

    AFDP-6727: - Add new profile externalSaml - Cleanup

--- acm-services/acm-service-login/src/main/java/com/armedia/acm/auth/AcmUserDetailsContextMapper.java
+++ acm-services/acm-service-login/src/main/java/com/armedia/acm/auth/AcmUserDetailsContextMapper.java
@@ -1,10 +1,10 @@
-/**
- * 
- */
 package com.armedia.acm.auth;
 
 import com.armedia.acm.services.users.model.ldap.AcmLdapSyncConfig;
 
+import com.armedia.acm.services.users.model.ldap.AcmUserContextMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,20 +13,19 @@ import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
 import java.util.Collection;
 
 /**
- * @author nadica.cuculova
+ * @author ncuculova
  *
  */
 public class AcmUserDetailsContextMapper extends LdapUserDetailsMapper
 {
-
     private AcmLdapSyncConfig acmLdapSyncConfig;
+    private static final Logger log = LoggerFactory.getLogger(AcmUserContextMapper.class);
 
     /**
-     * @param acmLdapSyncConfig
+     * @param acmLdapSyncConfig LDAP directory properties configuration
      */
     public AcmUserDetailsContextMapper(AcmLdapSyncConfig acmLdapSyncConfig)
     {
-        super();
         this.acmLdapSyncConfig = acmLdapSyncConfig;
     }
 
@@ -39,9 +38,9 @@ public class AcmUserDetailsContextMapper extends LdapUserDetailsMapper
     @Override
     public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> authorities)
     {
-        // TODO Auto-generated method stub
-        username = String.format("%s@%s", username, acmLdapSyncConfig.getUserDomain());
-        return super.mapUserFromContext(ctx, username, authorities);
+        String usernameWithDomain = String.format("%s@%s", username, acmLdapSyncConfig.getUserDomain());
+        log.info("Map authenticated user [{}] from LDAP context", usernameWithDomain);
+        return super.mapUserFromContext(ctx, usernameWithDomain, authorities);
     }
 
 }
