8197eb57712cd60e91ec496ad70727ecaa8753d4
Riste Tutureski <riste.tutureski@armedia.com>
2018-10-09 13:35:19 +0200

    AFDP-6729: Fixing the problem where Frevvo files who supposed to be hidden are shown

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileTransactionImpl.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileTransactionImpl.java
@@ -91,6 +91,10 @@ public class EcmFileTransactionImpl implements EcmFileTransaction
         List<String> keys = new ArrayList<>();
         for (Map.Entry<String, List<String>> entry : mimeTypesByTika.entrySet())
         {
+            if(value.contains(";"))
+            {
+                value = value.split(";")[0];
+            }
             if (entry.getValue().contains(value))
             {
                 keys.add(entry.getKey());
@@ -124,13 +128,14 @@ public class EcmFileTransactionImpl implements EcmFileTransaction
                 log.error("Could not extract metadata with Tika: [{}]", e.getMessage(), e);
             }
 
-            if(metadata.getFileActiveVersionMimeType().contains(";"))
+            String activeVersionMimeType = metadata.getFileActiveVersionMimeType();
+            if(activeVersionMimeType.contains(";"))
             {
-                metadata.setFileActiveVersionMimeType(metadata.getFileActiveVersionMimeType().split(";")[0]);
+                activeVersionMimeType = metadata.getFileActiveVersionMimeType().split(";")[0];
             }
 
-            if ((detectedMetadata.getContentType().equals(metadata.getFileActiveVersionMimeType())) ||
-                    (getAllTikaMimeTypesForFile(mimeTypesByTika, metadata.getFileActiveVersionMimeType())
+            if ((detectedMetadata.getContentType().equals(activeVersionMimeType)) ||
+                    (getAllTikaMimeTypesForFile(mimeTypesByTika, activeVersionMimeType)
                             .contains(detectedMetadata.getContentType())))
             {
 
