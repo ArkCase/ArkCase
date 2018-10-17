1f3671b4cd135c735892134e02fce178d19cc1df
adrijana.maneva <adrijana.maneva@armedia.com>
2018-10-17 13:56:43 +0200

    AFDP-6747 Add timestamp to modified date in request modules

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-info.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/complaints/controllers/components/complaint-info.client.controller.js
@@ -21,8 +21,9 @@ angular.module('complaints').controller(
                 'Object.ParticipantService',
                 'SearchService',
                 'Search.QueryBuilderService',
+                'Dialog.BootboxService',
                 function($scope, $stateParams, $translate, $modal, Util, UtilDateService, ConfigService, ObjectLookupService, ComplaintLookupService, ComplaintInfoService, ObjectModelService, HelperObjectBrowserService, MessageService, ObjectService, HelperUiGridService, ObjectParticipantService,
-                        SearchService, SearchQueryBuilder) {
+                        SearchService, SearchQueryBuilder, DialogService) {
 
                     new HelperObjectBrowserService.Component({
                         scope: $scope,
@@ -156,6 +157,7 @@ angular.module('complaints').controller(
                         $scope.objectInfo = objectInfo;
                         $scope.dateInfo = $scope.dateInfo || {};
                         $scope.dateInfo.dueDate = $scope.objectInfo.dueDate;
+                        $scope.dueDateBeforeChange = $scope.dateInfo.dueDate;
 
                         $scope.assignee = ObjectModelService.getAssignee(objectInfo);
                         $scope.owningGroup = ObjectModelService.getGroup(objectInfo);
@@ -194,10 +196,21 @@ angular.module('complaints').controller(
                     $scope.updateAssignee = function() {
                         ObjectModelService.setAssignee($scope.objectInfo, $scope.assignee);
                     };
-                    $scope.updateDueDate = function() {
-                        var correctedDueDate = UtilDateService.convertToCurrentTime($scope.dateInfo.dueDate);
-                        $scope.objectInfo.dueDate = moment.utc(UtilDateService.dateToIso(correctedDueDate)).format();
-                        $scope.saveComplaint();
+                    $scope.updateDueDate = function(data) {
+                        if (!Util.isEmpty(data)) {
+                            var correctedDueDate = new Date(data);
+                            var startDate = new Date($scope.objectInfo.create_date_tdt);
+                            if(correctedDueDate < startDate){
+                                $scope.objectInfo.dueDate = $scope.dueDateBeforeChange;
+                                DialogService.alert("Due Date must be biger then create date " + $filter("date")(startDate, $translate.instant('common.defaultDateTimeUIFormat')));
+                            }else {
+                                $scope.objectInfo.dueDate = moment.utc(UtilDateService.dateToIso(correctedDueDate)).format();
+                                $scope.saveComplaint();
+                            }
+                        }else {
+                            $scope.objectInfo.dueDate = $scope.dueDateBeforeChange;
+                            $scope.saveComplaint();
+                        }
                     };
 
                 } ]);
