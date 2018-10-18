9ae851138ec7ae0fa1258473b4b6b5153fe993bc
ivana.shekerova <ivana.shekerova@armedia.com>
2018-10-18 16:25:58 +0200

    Costsheet naming and object tree title changed

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cost-tracking/controllers/components/cost-tracking-new-costsheet-modal.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cost-tracking/controllers/components/cost-tracking-new-costsheet-modal.client.controller.js
@@ -35,7 +35,7 @@ angular.module('cost-tracking').controller(
                                 $scope.costsheet.parentId = $scope.modalParams.parentId;
                                 $scope.costsheet.parentType = $scope.modalParams.parentType;
                                 $scope.costsheet.parentNumber = $scope.modalParams.parentNumber;
-                                $scope.costsheet.title =  "Costsheet" + " " + $scope.modalParams.parentNumber;
+                                $scope.costsheet.title =  $scope.costsheet.user.fullName + " - " + $scope.modalParams.parentNumber;
                                 $scope.isTypeSelected = true;
                                 $scope.disableCostType = true;
                             }
@@ -161,7 +161,7 @@ angular.module('cost-tracking').controller(
                             if (!Util.isEmpty(selected)) {
                                 $scope.costsheet.parentNumber = selected.name;
                                 $scope.costsheet.parentId = selected.object_id_s;
-                                $scope.costsheet.title = "Costsheet" + " " + selected.name;
+                                $scope.costsheet.title = $scope.costsheet.user.fullName + " - " + selected.name;
                             }
                         });
 
