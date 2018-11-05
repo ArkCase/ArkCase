680f7af3010a62953d023d696360cae790ad7346
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2018-11-05 14:59:24 +0100

    Merge External ActiveMQ with Spring JMS and REST and Retry from hds-release-6-core-base

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/SolrRestClient.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/SolrRestClient.java
@@ -6,22 +6,22 @@ package com.armedia.acm.services.search.service.solr;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -29,7 +29,6 @@ package com.armedia.acm.services.search.service.solr;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,59 +36,17 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
 public class SolrRestClient
 {
 
-    private transient final RestTemplate restTemplate = new RestTemplate();
+
     private transient final Logger logger = LoggerFactory.getLogger(getClass());
+    private transient final RestTemplate restTemplate = new RestTemplate();
     private String solrHost;
     private int solrPort;
     private String solrContextRoot;
 
-    public void postToSolr(String core, String contentHandler, HttpEntity<InputStreamResource> entity, String logText,
-            String urlWithPlaceholders, Map<String, Object> urlValues)
-            throws SolrPostException
-    {
-        String url = String.format("https://%s:%s/%s/%s/%s?overwrite=true&%s",
-                getSolrHost(),
-                getSolrPort(),
-                getSolrContextRoot(),
-                core,
-                contentHandler,
-                urlWithPlaceholders);
-
-        try
-        {
-            ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class, urlValues);
-            if (isRecoverable(response.getStatusCode()))
-            {
-                logger.error("Could not post to Solr: [{}], got response code {}", logText, response.getStatusCode().value());
-                throw new SolrPostException(String.format("Could not post [%s] to Solr: %s", logText, response.getStatusCode()));
-            }
-
-            logger.debug("Posted to Solr: [{}], got response code {}", logText, response.getStatusCode().value());
-        }
-        catch (HttpStatusCodeException e)
-        {
-            if (isRecoverable(e.getStatusCode()))
-            {
-                throw new SolrPostException(String.format("Could not post [%s] to Solr: %s", logText, e.getMessage()));
-            }
-            else
-            {
-                logger.error("Could not send [{}] to Solr, got an unrecoverable error {}", logText, e.getStatusCode());
-            }
-        }
-        catch (RestClientException e)
-        {
-            throw new SolrPostException(String.format("Could not post [%s] to Solr: %s", logText, e.getMessage()));
-        }
-
-    }
-
-    public void postToSolr(String core, String contentHandler, HttpEntity<? extends Object> entity, String logText)
+    public void postToSolr(String core, String contentHandler, HttpEntity<? extends Object> entity, String logText, String extraUrlParams)
             throws SolrPostException
     {
         String url = String.format("https://%s:%s/%s/%s/%s?overwrite=true",
@@ -98,6 +55,10 @@ public class SolrRestClient
                 getSolrContextRoot(),
                 core,
                 contentHandler);
+        if (extraUrlParams != null && !extraUrlParams.trim().isEmpty())
+        {
+            url += extraUrlParams.startsWith("&") ? extraUrlParams : "&" + extraUrlParams;
+        }
 
         try
         {
