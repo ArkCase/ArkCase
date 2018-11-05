cf132d87eb1d8d6428cc41f5d49e18ab1f19ce1d
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2018-11-05 14:59:24 +0100

    Merge External ActiveMQ with Spring JMS and REST and Retry from hds-release-6-core-base

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/ContentFileSolrPostClient.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/ContentFileSolrPostClient.java
@@ -6,22 +6,22 @@ package com.armedia.acm.plugins.ecm.service.impl;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -107,7 +107,7 @@ public class ContentFileSolrPostClient implements SolrPostClient, ApplicationEve
         InputStreamResource inputStreamResource = new InputStreamResource(contentStream.getStream());
         HttpEntity<InputStreamResource> entity = new HttpEntity<>(inputStreamResource, headers);
 
-        getSolrRestClient().postToSolr(core.getCore(), getSolrContentFileHandler(), entity, logText, urlWithPlaceholders, urlValues);
+        getSolrRestClient().postToSolr(core.getCore(), getSolrContentFileHandler(), entity, logText, urlWithPlaceholders);
 
         applicationEventPublisher.publishEvent(new EcmFileContentIndexedEvent(solrContentDocument));
 
--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/DefaultSolrPostClient.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/solr/DefaultSolrPostClient.java
@@ -6,22 +6,22 @@ package com.armedia.acm.services.search.service.solr;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -58,7 +58,7 @@ public class DefaultSolrPostClient implements SolrPostClient
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
         HttpEntity<String> entity = new HttpEntity<>(json, headers);
 
-        getSolrRestClient().postToSolr(core.getCore(), getSolrUpdateHandler(), entity, logJson);
+        getSolrRestClient().postToSolr(core.getCore(), getSolrUpdateHandler(), entity, logJson, null);
     }
 
     public String getSolrUpdateHandler()
