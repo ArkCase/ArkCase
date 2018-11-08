ecc23ac21f006570306fc95979707fc5675c86b6
Yibu Lin <lypted1212@gmail.com>
2018-11-08 14:11:40 -0500

    AFDP-6831 Reopen. Not remove old assignee from child
    Fixing bug: After changing assignee on the Object Participants, new assignee is added to the Child (document) participant list, but the old one is not removed

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/core-participants/core-participants.client.directive.js
@@ -152,9 +152,7 @@ angular.module('directives').directive(
                                                 //set for AFDP-6831 to inheritance in the Folder/file participants
                                                 var len = scope.objectInfo.participants.length;
                                                 for (var i = 0; i < len; i++) {
-                                                    if(scope.objectInfo.participants[i].participantType =='assignee'){
-                                                        scope.objectInfo.participants[i].replaceChildrenParticipant = true;
-                                                    }
+                                                    scope.objectInfo.participants[i].replaceChildrenParticipant = true;
                                                 }
 
                                                 scope.assignee = selectedUser.object_id_s;
@@ -178,9 +176,7 @@ angular.module('directives').directive(
                                                 //set for AFDP-6831 to inheritance in the Folder/file participants
                                                 var len = scope.objectInfo.participants.length;
                                                 for (var i = 0; i < len; i++) {
-                                                    if(scope.objectInfo.participants[i].participantType =='owning group') {
-                                                        scope.objectInfo.participants[i].replaceChildrenParticipant = true;
-                                                    }
+                                                    scope.objectInfo.participants[i].replaceChildrenParticipant = true;
                                                 }
 
 
