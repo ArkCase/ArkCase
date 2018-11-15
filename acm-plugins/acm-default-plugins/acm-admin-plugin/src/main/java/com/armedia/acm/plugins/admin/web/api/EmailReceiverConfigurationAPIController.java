c391d3087c9193bcee5f5f38e2f29b57e6339136
stefan.sanevski <stefan.sanevski@armedia.com>
2018-11-15 18:48:19 +0100

    AFDP-6278 - Create case file from incoming email

--- acm-plugins/acm-default-plugins/acm-admin-plugin/src/main/java/com/armedia/acm/plugins/admin/web/api/EmailReceiverConfigurationAPIController.java
+++ acm-plugins/acm-default-plugins/acm-admin-plugin/src/main/java/com/armedia/acm/plugins/admin/web/api/EmailReceiverConfigurationAPIController.java
@@ -2,11 +2,15 @@ package com.armedia.acm.plugins.admin.web.api;
 
 import com.armedia.acm.core.exceptions.AcmEncryptionException;
 import com.armedia.acm.services.email.receiver.modal.EmailReceiverConfiguration;
+import com.armedia.acm.services.email.receiver.service.EmailReceiverConfigurationService;
 import com.armedia.acm.services.email.receiver.service.EmailReceiverConfigurationServiceImpl;
+import com.armedia.acm.services.email.service.AcmEmailConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,21 +25,32 @@ public class EmailReceiverConfigurationAPIController
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    private EmailReceiverConfigurationServiceImpl emailReceiverConfigurationService;
+    private EmailReceiverConfigurationService emailReceiverConfigurationService;
 
     @RequestMapping(value = "/email/receiver/configuration", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public EmailReceiverConfiguration getConfiguration()
+    public EmailReceiverConfiguration getConfiguration() throws AcmEncryptionException
     {
         return emailReceiverConfigurationService.readConfiguration();
     }
 
     @RequestMapping(value = "/email/receiver/configuration", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public void updateConfiguration(@RequestBody EmailReceiverConfiguration emailReceiverConfiguration, Authentication auth)
-            throws AcmEncryptionException
+    public ResponseEntity<?> updateConfiguration(@RequestBody EmailReceiverConfiguration emailReceiverConfiguration, Authentication auth)
+            throws AcmEncryptionException, AcmEmailConfigurationException
     {
-        emailReceiverConfigurationService.writeConfiguration(emailReceiverConfiguration, auth);
+        try
+        {
+            log.debug("Writing email receiver configuration [{}] ", emailReceiverConfiguration);
+            emailReceiverConfigurationService.writeConfiguration(emailReceiverConfiguration, auth);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            log.error("Can not write configuration [{}] ", emailReceiverConfiguration, e);
+            throw new AcmEmailConfigurationException("Can not write configuration");
+        }
+
     }
 
     /**
