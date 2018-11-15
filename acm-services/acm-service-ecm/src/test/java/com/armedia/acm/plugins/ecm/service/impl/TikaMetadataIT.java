675e31d0d798f47c068a531f686277d47882925f
ARMEDIA\aleksandar.bujaroski <aleksandar.bujaroski@armedia.com>
2018-11-15 15:03:05 +0100

    Imports fixed

--- acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/service/impl/TikaMetadataIT.java
+++ acm-services/acm-service-ecm/src/test/java/com/armedia/acm/plugins/ecm/service/impl/TikaMetadataIT.java
@@ -27,7 +27,9 @@ package com.armedia.acm.plugins.ecm.service.impl;
  * #L%
  */
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import com.armedia.acm.plugins.ecm.service.EcmTikaFileService;
 
