3f29165823cb01f6781dea3e3f2e560b2e88e6e2
jovan.ivanovski <jovan.ivanovski@armedia.com>
2018-11-27 17:32:35 +0100

    AFDP-6153, Improvements on UI in admin/file uploader setting section. User is able to type bigger numbers which get store in bytes but are presented as KB, MB, GB, TB etc. bytes.js library added strictly to the util service because it cannot be imported otherwise.

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/file-uploader.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/controllers/components/file-uploader.client.controller.js
@@ -1,20 +1,22 @@
 'use strict';
-
 angular.module('admin').controller('Admin.FileUploaderController',
     [ '$scope', '$q', '$modal', '$translate', 'UtilService', 'Admin.ApplicationSettingsService', 'Dialog.BootboxService', 'MessageService', function($scope, $q, $modal, $translate, Util, ApplicationSettingsService, DialogService, messageService) {
+
         var saved = {};
         ApplicationSettingsService.getProperty(ApplicationSettingsService.PROPERTIES.UPLOAD_FILE_SIZE_LIMIT).then(function(response) {
-            var uploadFileSizeLimitInBytes = Util.goodValue(response.data[ApplicationSettingsService.PROPERTIES.UPLOAD_FILE_SIZE_LIMIT], 52428800);
-            $scope.uploadFileSizeLimit = Util.convertBytesToSize(uploadFileSizeLimitInBytes, 'MB');
+            var defaultLimit = 52428800; //50mb
+            var uploadFileSizeLimitInBytes = Util.goodValue(response.data[ApplicationSettingsService.PROPERTIES.UPLOAD_FILE_SIZE_LIMIT], defaultLimit);
+            $scope.uploadFileSizeLimit = Util.bytes(uploadFileSizeLimitInBytes);
             saved.uploadFileSizeLimit = $scope.uploadFileSizeLimit;
         });
 
         $scope.applyChanges = function() {
             if (saved.uploadFileSizeLimit != $scope.uploadFileSizeLimit) {
+
+                $scope.uploadFileSizeLimit = Util.bytes($scope.uploadFileSizeLimit);
                 ApplicationSettingsService.setProperty(ApplicationSettingsService.PROPERTIES.UPLOAD_FILE_SIZE_LIMIT, $scope.uploadFileSizeLimit);
                 saved.uploadFileSizeLimit = $scope.uploadFileSizeLimit;
 
-                //change for AFDP-6803
                 bootbox.alert({
                     message: $translate.instant("admin.documentManagement.fileUploader.inform"),
                     buttons: {
