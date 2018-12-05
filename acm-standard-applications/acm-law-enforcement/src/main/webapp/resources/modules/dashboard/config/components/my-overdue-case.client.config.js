a2dc25e76384b5e451cbb33c6424f409c263a4a9
stefan.sanevski <stefan.sanevski@armedia.com>
2018-12-05 13:19:27 +0100

    AFDP-6907 - FOIA - Create New "My Overdue Requests" Widget

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/config/components/my-overdue-case.client.config.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/dashboard/config/components/my-overdue-case.client.config.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('dashboard.my-overdue-case', [ 'adf.provider' ]).config(function(dashboardProvider) {
-    dashboardProvider.widget('myOverdueCase', {
+angular.module('dashboard.my-overdue-cases', [ 'adf.provider' ]).config(function(dashboardProvider) {
+    dashboardProvider.widget('myOverdueCases', {
         title: 'dashboard.widgets.myOverdueCases.title',
         description: 'dashboard.widgets.myOverdueCases.description',
         controller: 'Dashboard.MyOverdueCasesController',
-        controllerAs: 'myOverdueCase',
+        controllerAs: 'myOverdueCases',
         reload: true,
         templateUrl: 'modules/dashboard/views/components/my-overdue-case.client.view.html'
 
