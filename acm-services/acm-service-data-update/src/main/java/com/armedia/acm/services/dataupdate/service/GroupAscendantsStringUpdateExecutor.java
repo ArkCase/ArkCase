a39f5848ed777ba41816d1e88c0386befd1673a8
ncuculova <nadica.cuculova@armedia.com>
2018-12-14 16:38:55 +0100

    Set modifier for groups

--- acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/GroupAscendantsStringUpdateExecutor.java
+++ acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/GroupAscendantsStringUpdateExecutor.java
@@ -1,16 +1,19 @@
 package com.armedia.acm.services.dataupdate.service;
 
+import com.armedia.acm.data.AuditPropertyEntityAdapter;
 import com.armedia.acm.services.users.dao.group.AcmGroupDao;
 import com.armedia.acm.services.users.model.group.AcmGroup;
 import com.armedia.acm.services.users.service.group.AcmGroupUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public class GroupAscendantsStringUpdateExecutor implements AcmDataUpdateExecutor
 {
     private AcmGroupDao groupDao;
+    private AuditPropertyEntityAdapter auditPropertyEntityAdapter;
     private static final Logger log = LoggerFactory.getLogger(GroupAscendantsStringUpdateExecutor.class);
 
     @Override
@@ -20,14 +23,18 @@ public class GroupAscendantsStringUpdateExecutor implements AcmDataUpdateExecuto
     }
 
     @Override
+    @Transactional
     public void execute()
     {
+        auditPropertyEntityAdapter.setUserId(AcmDataUpdateService.DATA_UPDATE_MODIFIER);
+
         log.info("Start executing {} update", getUpdateId());
         List<AcmGroup> acmGroups = groupDao.findAll();
         acmGroups.forEach(group -> {
             log.info("Recompute ascendants list string for group:[{}]", group.getName());
             String ascendantsListString = AcmGroupUtils.buildAncestorsStringForAcmGroup(group);
             group.setAscendantsList(ascendantsListString);
+            groupDao.save(group);
         });
     }
 
@@ -39,5 +46,15 @@ public class GroupAscendantsStringUpdateExecutor implements AcmDataUpdateExecuto
     public void setGroupDao(AcmGroupDao groupDao)
     {
         this.groupDao = groupDao;
+    }
+
+    public AuditPropertyEntityAdapter getAuditPropertyEntityAdapter()
+    {
+        return auditPropertyEntityAdapter;
+    }
+
+    public void setAuditPropertyEntityAdapter(AuditPropertyEntityAdapter auditPropertyEntityAdapter)
+    {
+        this.auditPropertyEntityAdapter = auditPropertyEntityAdapter;
     }
 }
