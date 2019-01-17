da8cbbf88258b7cc14179e47cb46bed5b20d0c2a
Mario Gjurcheski <mario.gjurcheski@armedia.com>
2019-01-17 11:30:18 +0100

    Validation when end time is before the start time in Report Scheduling

--- acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/web/api/ScheduleReportAPIController.java
+++ acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/web/api/ScheduleReportAPIController.java
@@ -6,32 +6,32 @@ package com.armedia.acm.plugins.report.web.api;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
+import com.armedia.acm.objectonverter.DateFormats;
 import com.armedia.acm.plugins.report.model.PentahoReportSchedule;
 import com.armedia.acm.plugins.report.model.PentahoScheduleRequest;
 import com.armedia.acm.plugins.report.model.ScheduleReportException;
 import com.armedia.acm.plugins.report.service.ScheduleReportService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -42,13 +42,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by dwu on 6/13/2017.
  */
 @Controller
-@RequestMapping({ "/api/v1/plugin/report", "/api/latest/plugin/report" })
-public class ScheduleReportAPIController
-{
+@RequestMapping({"/api/v1/plugin/report", "/api/latest/plugin/report"})
+public class ScheduleReportAPIController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleReportAPIController.class);
     private ScheduleReportService scheduleReportService;
     private String scheduleInputFolder;
@@ -57,12 +60,21 @@ public class ScheduleReportAPIController
     @RequestMapping(value = "/schedule", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public void processReportSchedule(@RequestBody PentahoReportSchedule pentahoReportSchedule)
-            throws ScheduleReportException
-    {
+            throws ScheduleReportException {
 
-        if (pentahoReportSchedule == null || pentahoReportSchedule.getUiPassParam() == null)
-        {
+        if (pentahoReportSchedule == null || pentahoReportSchedule.getUiPassParam() == null) {
             throw new ScheduleReportException("Invalid input given or missing schedule type: DAILY, WEEKLY, etc.");
+        }
+
+        try {
+            Date startTime = convertTime(pentahoReportSchedule.getStartTime());
+            Date endTime = convertTime(pentahoReportSchedule.getEndTime());
+            if (startTime.after(endTime)) {
+                throw new ScheduleReportException("End time cannot be before start time");
+            }
+        } catch (ParseException e) {
+            LOGGER.error("Error due parsing the start or end time", e);
+            throw new ScheduleReportException("Error due parsing the start or end time");
         }
 
         PentahoScheduleRequest pentahoScheduleRequest = new PentahoScheduleRequest(pentahoReportSchedule.getUiPassParam(),
@@ -78,8 +90,7 @@ public class ScheduleReportAPIController
 
     @RequestMapping(value = "/schedule", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String retrieveSchedules() throws ScheduleReportException
-    {
+    public String retrieveSchedules() throws ScheduleReportException {
         LOGGER.info("Retrieving the list of existing report schedules");
 
         String schedules = getScheduleReportService().retrieveSchedules();
@@ -90,8 +101,7 @@ public class ScheduleReportAPIController
     @RequestMapping(value = "/schedule/byId", method = RequestMethod.DELETE, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String deleteSchedule(@RequestParam("id") String scheduleId)
-            throws ScheduleReportException
-    {
+            throws ScheduleReportException {
         LOGGER.info("Deleting schedule [{}]", scheduleId);
 
         String response = getScheduleReportService().deleteSchedule(scheduleId);
@@ -99,33 +109,32 @@ public class ScheduleReportAPIController
         return response;
     }
 
-    public ScheduleReportService getScheduleReportService()
-    {
+    public ScheduleReportService getScheduleReportService() {
         return scheduleReportService;
     }
 
-    public void setScheduleReportService(ScheduleReportService scheduleReportService)
-    {
+    public void setScheduleReportService(ScheduleReportService scheduleReportService) {
         this.scheduleReportService = scheduleReportService;
     }
 
-    public String getScheduleInputFolder()
-    {
+    public String getScheduleInputFolder() {
         return scheduleInputFolder;
     }
 
-    public void setScheduleInputFolder(String scheduleInputFolder)
-    {
+    public void setScheduleInputFolder(String scheduleInputFolder) {
         this.scheduleInputFolder = scheduleInputFolder;
     }
 
-    public String getScheduleOutputFolder()
-    {
+    public String getScheduleOutputFolder() {
         return scheduleOutputFolder;
     }
 
-    public void setScheduleOutputFolder(String scheduleOutputFolder)
-    {
+    public void setScheduleOutputFolder(String scheduleOutputFolder) {
         this.scheduleOutputFolder = scheduleOutputFolder;
+    }
+
+    private Date convertTime(String date) throws ParseException {
+        Date formattedDate = new SimpleDateFormat(DateFormats.DEFAULT_DATE_FORMAT).parse(date);
+        return formattedDate;
     }
 }
