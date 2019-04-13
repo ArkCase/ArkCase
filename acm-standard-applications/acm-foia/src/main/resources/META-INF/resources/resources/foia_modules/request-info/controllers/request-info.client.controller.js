a46b42682b4ba2d9d9600a4a760fe22554808700
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2019-04-13 12:04:19 +0200

    Reappling snowbound corrections from AFDP-7330.

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/request-info/controllers/request-info.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/request-info/controllers/request-info.client.controller.js
@@ -576,7 +576,7 @@ angular.module('request-info').controller(
                         $scope.requestInfo.disposition = $scope.dispositionTypes[0].key;
                     }
                 });
-            };
+            }
 
             function populateRequestTrack(objectInfo) {
                 ObjectLookupService.getRequestTrack().then(function (requestTrack) {
@@ -668,7 +668,7 @@ angular.module('request-info').controller(
                             fileId: $stateParams['fileId']
                         });
                         ecmFile.$promise.then(function (file) {
-                            var fileInfo = buildFileInfo(file, $stateParams.id);
+                            var fileInfo = buildFileInfo(file, file.container.id);
                             $scope.openOtherDocuments.push(fileInfo);
                             openViewerMultiple();
                         });
@@ -873,7 +873,7 @@ angular.module('request-info').controller(
                     fileId: fileId,
                     removeOlderFileVersionFromSnowboundTabs: true && !$scope.versionChangeRequest
                 }]);
-            }
+            };
 
             $scope.$bus.subscribe('update-viewer-open-documents', function (data) {
                 // Retrieves the metadata for the file which is being opened in the viewer
@@ -893,15 +893,15 @@ angular.module('request-info').controller(
                         $scope.openOtherDocuments = _.filter($scope.openOtherDocuments, function (currentObject) {
                             if (typeof currentObject.id === 'string' || currentObject.id instanceof String) {
                                 if (data[index].removeOlderFileVersionFromSnowboundTabs) {
-                                    return !currentObject.id.startsWith(fileInfo.id + ":");
+                                    return !currentObject.id.startsWith(fileInfo.fileId + ":");
                                 }
-                                return !(currentObject.id.startsWith(fileInfo.id + ":") && currentObject.versionTag === fileInfo.versionTag);
+                                return !(currentObject.id.startsWith(fileInfo.fileId + ":") && currentObject.versionTag === fileInfo.versionTag);
                             }
 
                             if (data[index].removeOlderFileVersionFromSnowboundTabs) {
-                                return !(currentObject.id === fileInfo.id);
+                                return !(currentObject.id === fileInfo.fileId);
                             }
-                            return !(currentObject.id === fileInfo.id && currentObject.versionTag === fileInfo.versionTag);
+                            return !(currentObject.id === fileInfo.fileId && currentObject.versionTag === fileInfo.versionTag);
                         });
                         $scope.openOtherDocuments.push(fileInfo);
                     });
@@ -1331,12 +1331,13 @@ angular.module('request-info').controller(
 
             function buildFileInfo(file, containerId) {
                 return {
-                    id: file.fileId,
+                    fileId: file.fileId,
+                    id: file.fileId + ':' + file.activeVersionTag,
                     containerId: containerId,
                     containerType: 'CASE_FILE',
                     name: file.fileName,
                     mimeType: file.fileActiveVersionMimeType,
-                    selectedIds: '',
+                    selectedIds: file.fileId + ':' + file.activeVersionTag,
                     versionTag: file.activeVersionTag
                 };
             }
@@ -1440,13 +1441,23 @@ angular.module('request-info').controller(
             // Release editing lock on window unload, if acquired
             $window.addEventListener('beforeunload', function () {
                 if ($scope.editingMode) {
-                    ObjectLockingService.unlockObject($scope.ecmFile.fileId, ObjectService.ObjectTypes.FILE, ObjectService.LockTypes.WRITE);
+                    ObjectLockingService.unlockObject($scope.fileInfo.fileId, ObjectService.ObjectTypes.FILE, ObjectService.LockTypes.WRITE, true);
                 }
             });
-
-            $rootScope.$bus.subscribe("object.changed/FILE/" + $stateParams.id, function () {
-                DialogService.alert($translate.instant("documentDetails.fileChangedAlert")).then(function () {
-                    $scope.openSnowboundViewer();
+            
+            $rootScope.$bus.subscribe("object.changed/FILE/" + $stateParams.fileId, function() {
+                var ecmFile = EcmService.getFile({
+                    fileId: $scope.ecmFile.fileId
+                });
+                ecmFile.$promise.then(function(file) {
+                    $scope.ecmFile = file;
+                    $scope.fileId = file.fileId;
+                    $scope.fileInfo.id= file.fileId + ':' + file.activeVersionTag;
+                    $scope.fileInfo.selectedIds= file.fileId + ':' + file.activeVersionTag;
+                    $scope.fileInfo.versionTag= file.activeVersionTag;
+                    DialogService.alert($translate.instant("documentDetails.fileChangedAlert")).then(function() {
+                        $scope.openSnowboundViewer();
+                    });
                 });
             });
         }]);
