d8dec71b203f1d1a3293f69d061df73171e8eaf6
stefan.sanevski <stefan.sanevski@armedia.com>
2019-08-23 16:56:25 +0200

    AFDP-8125 - 5-day reminder email is being sent to all assignees

--- acm-standard-applications/acm-foia/src/main/java/gov/foia/service/DueDateReminder.java
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/service/DueDateReminder.java
@@ -1,13 +1,44 @@
 package gov.foia.service;
 
+/*-
+ * #%L
+ * ACM Standard Application: Freedom of Information Act
+ * %%
+ * Copyright (C) 2014 - 2019 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.armedia.acm.data.AuditPropertyEntityAdapter;
 import com.armedia.acm.services.holiday.service.HolidayConfigurationService;
 import com.armedia.acm.services.notification.dao.NotificationDao;
 import com.armedia.acm.services.notification.model.Notification;
+import com.armedia.acm.services.notification.model.NotificationConstants;
 import com.armedia.acm.services.users.dao.UserDao;
 import com.armedia.acm.services.users.model.AcmUser;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
+import java.util.Date;
 import java.util.List;
 
 import gov.foia.dao.FOIARequestDao;
@@ -19,36 +50,44 @@ public class DueDateReminder
     private FOIARequestDao foiaRequestDao;
     private HolidayConfigurationService holidayConfigurationService;
     private UserDao userDao;
+    private AuditPropertyEntityAdapter auditPropertyEntityAdapter;
 
     public void sendDueDateReminder()
     {
+        auditPropertyEntityAdapter.setUserId(NotificationConstants.SYSTEM_USER);
 
-        LocalDate oneDayFromNow = holidayConfigurationService.addWorkingDaysToDate(LocalDate.now(), 1);
-        LocalDate fiveDaysFromNow = holidayConfigurationService.addWorkingDaysToDate(LocalDate.now(), 5);
+        Date oneDayFromNow = holidayConfigurationService.addWorkingDaysToDate(new Date(), 1);
+        Date fiveDaysFromNow = holidayConfigurationService.addWorkingDaysToDate(new Date(), 5);
 
         List<FOIARequest> foiaRequests = foiaRequestDao.findAllNotReleasedRequests();
         for (FOIARequest request : foiaRequests)
         {
-            if (request.getDueDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().equals(oneDayFromNow)
-                    || request.getDueDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().equals(fiveDaysFromNow))
+            if (request.getDueDate() != null)
             {
-                AcmUser user = getUserDao().findByUserId(request.getAssigneeLdapId());
+                int daysDiffOneDay = Days.daysBetween(new DateTime(oneDayFromNow.getTime()), new DateTime(request.getDueDate().getTime()))
+                        .getDays();
+                int daysDiffFiveDays = Days
+                        .daysBetween(new DateTime(fiveDaysFromNow.getTime()), new DateTime(request.getDueDate().getTime())).getDays();
 
-                String dueDateRemainingDays = request.getDueDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-                        .equals(oneDayFromNow) ? "1" : "5";
+                if (daysDiffOneDay == 0 || daysDiffFiveDays == 0)
+                {
+                    AcmUser user = getUserDao().findByUserId(request.getAssigneeLdapId());
 
-                Notification notification = new Notification();
-                notification.setNote(dueDateRemainingDays);
-                notification.setTitle(String.format("Request:%s assigned to %s", request.getCaseNumber(), user.getFullName()));
-                notification.setParentId(request.getId());
-                notification.setParentType(request.getObjectType());
-                notification.setParentName(request.getCaseNumber());
-                notification.setParentTitle(request.getTitle());
-                notification.setUser(request.getAssigneeLdapId());
-                notification.setEmailAddresses(user.getMail());
-                notification.setTemplateModelName("requestAssigneeDueDateReminder");
-                notification.setAttachFiles(false);
-                notificationDao.save(notification);
+                    String dueDateRemainingDays = daysDiffOneDay == 0 ? "1" : "5";
+
+                    Notification notification = new Notification();
+                    notification.setNote(dueDateRemainingDays);
+                    notification.setTitle(String.format("Request:%s assigned to %s", request.getCaseNumber(), user.getFullName()));
+                    notification.setParentId(request.getId());
+                    notification.setParentType(request.getObjectType());
+                    notification.setParentName(request.getCaseNumber());
+                    notification.setParentTitle(request.getTitle());
+                    notification.setUser(request.getAssigneeLdapId());
+                    notification.setEmailAddresses(user.getMail());
+                    notification.setTemplateModelName("requestAssigneeDueDateReminder");
+                    notification.setAttachFiles(false);
+                    notificationDao.save(notification);
+                }
             }
         }
     }
@@ -91,5 +130,15 @@ public class DueDateReminder
     public void setUserDao(UserDao userDao)
     {
         this.userDao = userDao;
+    }
+
+    public AuditPropertyEntityAdapter getAuditPropertyEntityAdapter()
+    {
+        return auditPropertyEntityAdapter;
+    }
+
+    public void setAuditPropertyEntityAdapter(AuditPropertyEntityAdapter auditPropertyEntityAdapter)
+    {
+        this.auditPropertyEntityAdapter = auditPropertyEntityAdapter;
     }
 }
