4d202a8f04e31835dfbf00e395d9aa0e6c3d0f88
Aleksandar Bujaroski <aleksandar.bujaroski@armedia.com>
2019-09-05 08:25:27 -0400

    AFDP-8065-paho changes moved to core

---
+++ acm-services/acm-service-login/src/main/java/com/armedia/acm/auth/ad/ActiveDirectoryContextSource.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2014 - 2018 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software.
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail. Otherwise, the software is
+ * provided under the following open source license terms:
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package com.armedia.acm.auth.ad;
+
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.ldap.InitialLdapContext;
+
+import java.util.Hashtable;
+
+public class ActiveDirectoryContextSource extends ActiveDirectoryAbstractContextSource
+{
+    @Override
+    protected DirContext getDirContextInstance(Hashtable environment) throws NamingException
+    {
+        return new InitialLdapContext(environment, null);
+    }
+
+}
