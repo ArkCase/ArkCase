ae2da689a207a32f92de683e80745caae0aa81bb
stefan.sanevski <stefan.sanevski@armedia.com>
2019-09-05 14:49:16 +0200

    BCGEU-100 - Sending Email without Document - Added History Event

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree-ext.email.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree-ext.email.client.service.js
@@ -233,7 +233,7 @@ angular.module('services').factory('DocTreeExt.Email',
                     emailData.body = emailModel.body;
                     emailData.footer = '\n\n' + emailModel.footer;
                     emailData.emailAddresses = emailModel.recipients;
-                    emailData.parentType = match[Util.goodMapValue(DocTree, "treeConfig.email.objectTypeRegexGroup")];
+                    emailData.parentType = DocTree.objectInfo.acmObjectOriginator ? DocTree.objectInfo.acmObjectOriginator.parentType : match[Util.goodMapValue(DocTree, "treeConfig.email.objectTypeRegexGroup")].toUpperCase();
                     var objectNumber = match[Util.goodMapValue(DocTree, "treeConfig.email.objectNumberRegexGroup")];
                     emailData.parentNumber = DocTree.objectInfo[objectNumber];
                     emailData.modelReferenceName = 'plainEmail';
