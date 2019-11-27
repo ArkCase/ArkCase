56fa6f32e9f67a1388203b7419881630af0b4b14
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2019-11-27 14:00:35 +0100

    AFDP-8288 FOIA - Queues module- Open Request in Request module, not the documents view - Formating fix

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/queues/controllers/components/requests-list.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/queues/controllers/components/requests-list.client.controller.js
@@ -167,7 +167,9 @@ angular.module('queues').controller(
                      * @param row
                      */
                     function rowClick(row) {
-                        $state.go('cases.main', {id: row.entity.request_id_lcs});
+                        $state.go('cases.main', {
+                            id: row.entity.request_id_lcs
+                        });
                     }
 
                     function applyConfig(e, componentId, config) {
