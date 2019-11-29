d7181baeaf5bfaab4e9b9f4768a44e336a2c9c07
MartinJovanovski <jovanovskimartin85@gmail.com>
2019-11-29 16:35:09 +0100

    no message

--- acm-plugins/acm-default-plugins/acm-addressable-plugin/src/main/java/com/armedia/acm/plugins/addressable/service/PhoneRegexConfig.java
+++ acm-plugins/acm-default-plugins/acm-addressable-plugin/src/main/java/com/armedia/acm/plugins/addressable/service/PhoneRegexConfig.java
@@ -9,6 +9,7 @@ public class PhoneRegexConfig
     @Value("${phone.phone_regex}")
     private String phoneRegex;
 
+    //.substring is put here because Yaml is parsing the string and can't accept ^ sign at the beginning
     public String getPhoneRegex()
     {
         return phoneRegex.substring(1, phoneRegex.length() - 1);
