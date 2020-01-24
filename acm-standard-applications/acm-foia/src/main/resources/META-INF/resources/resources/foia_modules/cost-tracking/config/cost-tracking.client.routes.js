053995a7dc793e9e629e1dee85eb3feb2d0c8bca
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2020-01-24 09:15:02 +0000

    Merge branch 'AFDP-8524-label-litigation-cost-sheet-not-translated' into 'develop'
    The labels in Cost Tracking, New Costheet and Time Tracking, New Timesheet are translated
    
    Closes AFDP-8524
    
    See merge request arkcase/ArkCase!1674
    
    (cherry picked from commit 3dfe0e2517b8154cf37eab6d883901a053b36651)
    
    cb154e3d The labels in Cost Tracking, New Costheet and Time Tracking, New Timesheet are translated
    6df66774 labels are translated in costsheet, new costsheet, edit costsheet
    e5d8635d code review fix

---
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cost-tracking/config/cost-tracking.client.routes.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.module('cost-tracking').config([ '$stateProvider', function($stateProvider) {
+    $stateProvider.state('cost-tracking', {
+        url: '/cost-tracking',
+        templateUrl: 'modules/cost-tracking/views/cost-tracking.client.view.html',
+        resolve: {
+            translatePartialLoader: [ '$translate', '$translatePartialLoader', function($translate, $translatePartialLoader) {
+                $translatePartialLoader.addPart('common');
+                $translatePartialLoader.addPart('cost-tracking');
+                $translatePartialLoader.addPart('request-info');
+                return $translate.refresh();
+            } ]
+        }
+    })
+
+        .state('cost-tracking.main', {
+            url: '/:id/main',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-main.client.view.html',
+            params: {
+                "type": "COSTSHEET"
+            }
+        })
+
+        .state('cost-tracking.details', {
+            url: '/:id/details',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-details.client.view.html'
+        })
+
+        .state('cost-tracking.person', {
+            url: '/:id/person',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-person.client.view.html'
+        })
+
+        .state('cost-tracking.summary', {
+            url: '/:id/summary',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-summary.client.view.html'
+        })
+
+        .state('cost-tracking.tags', {
+            url: '/:id/tags',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-tags.client.view.html'
+        })
+
+        .state('cost-tracking.tasks', {
+            url: '/:id/tasks',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-tasks.client.view.html'
+        })
+
+        .state('cost-tracking.documents', {
+            url: '/:id/documents',
+            templateUrl: 'modules/cost-tracking/views/components/cost-tracking-documents.client.view.html'
+        })
+
+} ]).run([ 'Helper.DashboardService', function(DashboardHelper) {
+    DashboardHelper.addLocales();
+} ]);
