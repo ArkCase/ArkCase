e5d8635dc075efc17b020c57547f51d4571f2afa
martin.bozinovski <bozinovskimartin@gmail.com>
2020-01-24 10:13:39 +0100

    code review fix

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cost-tracking/config/cost-tracking.client.routes.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/cost-tracking/config/cost-tracking.client.routes.js
@@ -8,7 +8,6 @@ angular.module('cost-tracking').config([ '$stateProvider', function($stateProvid
             translatePartialLoader: [ '$translate', '$translatePartialLoader', function($translate, $translatePartialLoader) {
                 $translatePartialLoader.addPart('common');
                 $translatePartialLoader.addPart('cost-tracking');
-                $translatePartialLoader.addPart('request-info');
                 return $translate.refresh();
             } ]
         }
