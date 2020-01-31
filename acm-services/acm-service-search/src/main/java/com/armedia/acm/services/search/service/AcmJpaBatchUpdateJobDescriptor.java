96143409c5ccf291ccfa7cf012a489fbe1819420
Nadica Deleva <nadica.cuculova@armedia.com>
2020-01-31 11:18:40 +0100

    Fix Solr indexing job. Use trigger's jobData only the very first time the job is executed. Next time last run date should be stored in job's jobData.

--- acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/AcmJpaBatchUpdateJobDescriptor.java
+++ acm-services/acm-service-search/src/main/java/com/armedia/acm/services/search/service/AcmJpaBatchUpdateJobDescriptor.java
@@ -6,22 +6,22 @@ package com.armedia.acm.services.search.service;
  * %%
  * Copyright (C) 2014 - 2019 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -30,11 +30,9 @@ package com.armedia.acm.services.search.service;
 import com.armedia.acm.quartz.scheduler.AcmJobDescriptor;
 import com.armedia.acm.services.search.model.SearchConstants;
 import com.armedia.acm.spring.SpringContextHolder;
-
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.quartz.PersistJobDataAfterExecution;
 
 import java.text.DateFormat;
@@ -61,19 +59,13 @@ public class AcmJpaBatchUpdateJobDescriptor extends AcmJobDescriptor
     @Override
     public void executeJob(JobExecutionContext context) throws InterruptedException
     {
-        JobDataMap triggerDataMap = context.getTrigger().getJobDataMap();
+        JobDataMap triggerJobDataMap = context.getTrigger().getJobDataMap();
         JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
-
-            if (triggerDataMap != null && triggerDataMap.size() != 0)
-            {
-                jpaBatchUpdateService.jpaBatchUpdate(triggerDataMap);
-                jobDataMap.clear();
-                jobDataMap.putAll(triggerDataMap);
-            }
-            else
-            {
-                jpaBatchUpdateService.jpaBatchUpdate(jobDataMap);
-            }
+        if (jobDataMap.isEmpty())
+        {
+            jobDataMap.putAll(triggerJobDataMap);
+        }
+        jpaBatchUpdateService.jpaBatchUpdate(jobDataMap);
     }
 
     @Override
