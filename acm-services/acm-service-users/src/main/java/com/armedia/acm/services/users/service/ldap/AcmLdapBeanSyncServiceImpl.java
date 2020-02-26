8bdc1018bb0bb5eaeaba185dd6e61c3e2aad8a2c
Mario Gjurcheski <mario.gjurcheski@armedia.com>
2020-02-26 17:19:10 +0100

    AFDP 8061 - fixes affter comments on MR.

--- acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/ldap/AcmLdapBeanSyncServiceImpl.java
+++ acm-services/acm-service-users/src/main/java/com/armedia/acm/services/users/service/ldap/AcmLdapBeanSyncServiceImpl.java
@@ -1,5 +1,32 @@
 package com.armedia.acm.services.users.service.ldap;
 
+/*-
+ * #%L
+ * ACM Service: Users
+ * %%
+ * Copyright (C) 2014 - 2020 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import com.armedia.acm.configuration.service.ConfigurationPropertyService;
 import com.armedia.acm.services.users.model.ldap.AcmLdapConstants;
 
@@ -57,7 +84,6 @@ public class AcmLdapBeanSyncServiceImpl implements AcmLdapBeanSyncService, Initi
         Map<String, Object> ldapGroupSyncConfigAttributes = acmLdapConfig.getAttributes().get(LDAP_ADD_GROUP_CONFIG);
 
         ldapGroupSyncConfigAttributes.keySet().forEach(directory -> {
-            logger.debug("Register bean definition for: [{}]", AcmLdapGroupSyncConfig.class);
             logger.debug("Register bean definition for: [{}]", AcmLdapGroupSyncConfig.class);
             BeanDefinitionBuilder jobDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(AcmLdapGroupSyncConfig.class);
             jobDefinitionBuilder.addPropertyValue("attributes", ldapGroupSyncConfigAttributes.get(directory));
