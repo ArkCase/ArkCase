39a7c3b5306a4d072802f55ca35d336462daf77c
Sasko Tanaskoski <sasko.tanaskoski@armedia.com>
2020-03-24 09:59:25 +0100

    Unit test fix.

--- acm-services/acm-service-users/src/test/java/com/armedia/acm/services/users/model/ldap/MapperUtilsTest.java
+++ acm-services/acm-service-users/src/test/java/com/armedia/acm/services/users/model/ldap/MapperUtilsTest.java
@@ -126,7 +126,7 @@ public class MapperUtilsTest
         config.setUserDomain("armedia.com");
         config.setUserIdAttributeName("uid");
         String userId = MapperUtils.buildUserId("veryyyyyylongusername", config);
-        assertEquals("dcoveryyyyyylongusername@armedia.com", userId);
+        assertEquals("dco.veryyyyyylongusername@armedia.com", userId);
     }
 
     @Test
