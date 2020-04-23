9e3d1bfa96d044397e9ae88d20f748753aa440b9
ivana.shekerova <ivana.shekerova@armedia.com>
2020-04-23 19:39:26 +0200

    AFDP-8838 - FOIA - Appeal Disposition Improvements

---
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/dao/DispositionReasonDao.java
@@ -0,0 +1,61 @@
+package gov.foia.dao;
+
+/*-
+ * #%L
+ * ACM Standard Application: Freedom of Information Act
+ * %%
+ * Copyright (C) 2014 - 2020 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.armedia.acm.data.AcmAbstractDao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import gov.foia.model.DispositionReason;
+
+/**
+ * Created by ivana.shekerova
+ */
+
+public class DispositionReasonDao extends AcmAbstractDao<DispositionReason>
+{
+    private transient final Logger LOG = LogManager.getLogger(getClass());
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    protected Class<DispositionReason> getPersistenceClass()
+    {
+        return DispositionReason.class;
+    }
+
+    public EntityManager getEntityManager()
+    {
+        return entityManager;
+    }
+}
