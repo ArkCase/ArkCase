6124ae2eca4c85b3aad120d8874ae534ce519c1b
ana.serafimoska <ana.serafimoska@armedia.com>
2020-05-05 13:33:49 +0200

    Fix review comments

--- acm-standard-applications/acm-foia/src/main/java/gov/foia/transformer/FOIAUserToSolrTransformer.java
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/transformer/FOIAUserToSolrTransformer.java
@@ -11,8 +11,6 @@ import com.armedia.acm.spring.SpringContextHolder;
 
 import org.springframework.beans.factory.annotation.Value;
 
-import java.util.stream.Collectors;
-
 /**
  * Created by ana.serafimoska
  */
@@ -31,44 +29,18 @@ public class FOIAUserToSolrTransformer extends UserToSolrTransformer
 
         String userPrefix = prefixTrailingDot(ldapSyncConfig.getUserPrefix());
 
-        SolrAdvancedSearchDocument solr = new SolrAdvancedSearchDocument();
-        solr.setId(in.getUserId() + "-USER");
-        solr.setObject_id_s(in.getUserId() + "");
-        solr.setObject_type_s("USER");
-        solr.setName(in.getFullName());
-        solr.setFirst_name_lcs(in.getFirstName());
-        solr.setLast_name_lcs(in.getLastName());
-        solr.setEmail_lcs(in.getMail());
+        SolrAdvancedSearchDocument solr;
 
-        solr.setTitle_parseable(in.getFullName());
+        solr = super.toSolrAdvancedSearch(in);
 
-        solr.setCreate_date_tdt(in.getCreated());
-        solr.setModified_date_tdt(in.getModified());
-
-        solr.setStatus_lcs(in.getUserState().name());
-
-        // Add groups
-        solr.setGroups_id_ss(in.getGroupNames().count() == 0 ? null : in.getGroupNames().collect(Collectors.toList()));
-
-        solr.setAdditionalProperty("directory_name_s", in.getUserDirectoryName());
-        solr.setAdditionalProperty("country_s", in.getCountry());
-        solr.setAdditionalProperty("country_abbreviation_s", in.getCountryAbbreviation());
-        solr.setAdditionalProperty("department_s", in.getDepartment());
-        solr.setAdditionalProperty("company_s", in.getCompany());
-        solr.setAdditionalProperty("title_s", in.getTitle());
-        solr.setAdditionalProperty("name_partial", in.getFullName());
-        solr.setAdditionalProperty("name_lcs", in.getFullName());
-
-        // TODO find a way to add Organization
-        // TODO find a way to add Application Title
-        // TODO find a way to add Location
-
-        // set hidden_b to true if user has portal prefix
-        if (solr.getObject_id_s().startsWith(userPrefix))
+        if (solr != null)
         {
-            solr.setHidden_b(true);
+            // set hidden_b to true if user has portal prefix
+            if (solr.getObject_id_s().startsWith(userPrefix))
+            {
+                solr.setHidden_b(true);
+            }
         }
-
         return solr;
     }
 
@@ -80,24 +52,17 @@ public class FOIAUserToSolrTransformer extends UserToSolrTransformer
 
         String userPrefix = prefixTrailingDot(ldapSyncConfig.getUserPrefix());
 
-        SolrDocument solr = new SolrDocument();
-        solr.setName(in.getFullName());
-        solr.setTitle_parseable(in.getFullName());
-        solr.setObject_id_s(in.getUserId() + "");
-        solr.setObject_type_s("USER");
-        solr.setId(in.getUserId() + "-USER");
+        SolrDocument solr;
 
-        solr.setCreate_tdt(in.getCreated());
-        solr.setLast_modified_tdt(in.getModified());
+        solr = super.toSolrQuickSearch(in);
 
-        solr.setStatus_s(in.getUserState().name());
-        solr.setAdditionalProperty("name_partial", in.getFullName());
-        solr.setAdditionalProperty("name_lcs", in.getFullName());
-
-        // set hidden_b to true if user has portal prefix
-        if (solr.getObject_id_s().startsWith(userPrefix))
+        if (solr != null)
         {
-            solr.setHidden_b(true);
+            // set hidden_b to true if user has portal prefix
+            if (solr.getObject_id_s().startsWith(userPrefix))
+            {
+                solr.setHidden_b(true);
+            }
         }
         return solr;
     }
