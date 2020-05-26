c3d6f559b47a84b34856d949fb259c7a789f1656
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2020-05-26 06:18:41 -0400

    AFDP-9003 Denied request with disposition closed date issue

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-main.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-main.client.controller.js
@@ -80,7 +80,7 @@ angular.module('cases').controller(
                     }
                 }
                 $scope.originalDueDate = $scope.previousDueDate;
-                $scope.enableDispositionClosedDate = objectInfo.dispositionClosedDate == null;
+                $scope.enableDispositionClosedDate = objectInfo.dispositionClosedDate == null && !objectInfo.deniedFlag;
 
                 var otherExistsInAppealReasons = _.some($scope.objectInfo.dispositionReasons, function (value) {
                     return value.reason === 'other';
