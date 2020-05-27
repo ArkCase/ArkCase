1e0d735154bd2da88c434167e355a02c2d8de425
ana.serafimoska <ana.serafimoska@armedia.com>
2020-05-27 18:13:36 +0200

    AFDP-9017 Exemption Codes Description

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-exemption-codes-description-modal.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-exemption-codes-description-modal.client.controller.js
@@ -3,38 +3,40 @@
 angular.module('cases').controller('Cases.ExemptionCodesDescriptionModalController', ['$scope', '$modalInstance', 'ConfigService', 'Object.LookupService',
     function($scope, $modalInstance, ConfigService, ObjectLookupService){
 
-
-        $scope.gridOptions = {
-            enableColumnResizing: true,
-            enableRowSelection: true,
-            enableRowHeaderSelection: false,
-            multiSelect: false,
-            noUnselect: false,
-            totalItems: 0,
-            data: [],
-            rowHeight: 95
-        };
-        
         ConfigService.getModuleConfig('cases').then(function(moduleConfig){
             var codesDescriptionConfig = _.find(moduleConfig.components, {
                 id: 'exemptionCodesDescription'
             });
 
+
             $scope.config = codesDescriptionConfig;
+
+            $scope.gridOptions = {
+                enableColumnResizing: true,
+                enableRowSelection: true,
+                enableRowHeaderSelection: false,
+                paginationPageSizes: $scope.config.paginationPageSizes,
+                paginationPageSize: $scope.config.paginationPageSize,
+                multiSelect: false,
+                noUnselect: false,
+                totalItems: 0,
+                data: []
+            };
+
             $scope.gridOptions.columnDefs = $scope.config.columnDefs;
             reloadGrid();
         });
 
 
-        function reloadGrid(){
-            ObjectLookupService.getAnnotationTags().then(function(annotationTags){
-                $scope.annotationTags = annotationTags;
+        function reloadGrid() {
+            ObjectLookupService.getExemptionStatutes().then(function (exemptionStatute) {
+                $scope.exemptionStatutes = exemptionStatute;
                 $scope.gridOptions = $scope.gridOptions || {};
-                $scope.gridOptions.data = $scope.annotationTags;
+                $scope.gridOptions.data = $scope.exemptionStatutes;
             });
         }
 
-        $scope.onClickCancel = function() {
+        $scope.onClickCancel = function () {
             $modalInstance.dismiss('Cancel');
         };
 
