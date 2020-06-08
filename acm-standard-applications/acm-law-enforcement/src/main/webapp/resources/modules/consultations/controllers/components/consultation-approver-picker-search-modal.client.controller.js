593e942839b0b4f824e1c6aebb060fdd473cd10b
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2020-06-08 16:39:40 +0200

    AFDP-8961 Remove buckslip approval from consultations UI

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/consultations/controllers/components/consultation-approver-picker-search-modal.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('consultations').controller('Consultations.ApproverPickerController', [ '$scope', '$modalInstance', 'modalParams', function($scope, $modalInstance, modalParams) {
-    $scope.header = modalParams.header;
-    $scope.filter = modalParams.filter;
-    $scope.extraFilter = modalParams.extraFilter;
-    $scope.config = modalParams.config;
-    $scope.modalInstance = $modalInstance;
-} ]);
