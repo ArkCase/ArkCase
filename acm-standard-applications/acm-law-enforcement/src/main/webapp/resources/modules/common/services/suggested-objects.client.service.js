d618e51d6bc9856e3ea7351e443cca3255780422
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2020-06-16 15:09:32 +0200

    AFDP-9060 Refactor UI case and consultation suggestion services to support all object types

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/suggested-objects.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/services/suggested-objects.client.service.js
@@ -1,18 +1,18 @@
 'use strict';
 
-angular.module('services').factory('Cases.SuggestedCases', ['$http', 'base64', function ($http, base64) {
+angular.module('services').factory('SuggestedObjectsService', ['$http', 'base64', function ($http, base64) {
 
     return ({
-        getSuggestedCases: getSuggestedCases
+        getSuggestedObjects: getSuggestedObjects
     });
 
-    function getSuggestedCases(title, id) {
+    function getSuggestedObjects(title, type, id) {
         return $http({
             url: 'api/latest/service/suggestion/' + base64.urlencode(title),
             method: 'GET',
             params: {
                 objectId: id,
-                objectType: "CASE_FILE"
+                objectType: type
             }
         });
     }
