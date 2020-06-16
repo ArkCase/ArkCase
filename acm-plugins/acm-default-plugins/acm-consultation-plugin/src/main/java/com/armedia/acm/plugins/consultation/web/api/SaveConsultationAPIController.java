a3b14ccf97f352da21f37a720cda6d6bb72cce33
stefan.sanevski <stefan.sanevski@armedia.com>
2020-06-16 16:27:59 +0200

    AFDP-9075 - Consultation - Create New Consultation with attachments

--- acm-plugins/acm-default-plugins/acm-consultation-plugin/src/main/java/com/armedia/acm/plugins/consultation/web/api/SaveConsultationAPIController.java
+++ acm-plugins/acm-default-plugins/acm-consultation-plugin/src/main/java/com/armedia/acm/plugins/consultation/web/api/SaveConsultationAPIController.java
@@ -51,12 +51,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -90,11 +92,22 @@ public class SaveConsultationAPIController
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
     public Consultation createConsultationMutipart(@RequestPart(name = "consultation") Consultation in,
-            @RequestPart(name = "files") Map<String, List<MultipartFile>> filesMap, HttpSession session, Authentication auth)
+            MultipartHttpServletRequest request, HttpSession session, Authentication auth)
             throws AcmCreateObjectFailedException, AcmUpdateObjectFailedException, AcmUserActionFailedException, AcmObjectNotFoundException,
             IOException
     {
-        return saveConsultation(in, filesMap, session, auth);
+        Map<String, List<MultipartFile>> attachments = request.getMultiFileMap();
+        Map<String, List<MultipartFile>> files = new HashMap<>();
+
+        for (Map.Entry<String, List<MultipartFile>> entry : attachments.entrySet()) {
+            String type = entry.getKey();
+            if (!"consultation".equalsIgnoreCase(type)){
+                final List<MultipartFile> attachmentsList = entry.getValue();
+
+                files.put(type, attachmentsList);
+            }
+        }
+        return saveConsultation(in, files, session, auth);
     }
 
     private Consultation saveConsultation(Consultation in, Map<String, List<MultipartFile>> filesMap, HttpSession session,
