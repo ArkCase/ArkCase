cb9294dd58f724f5c8817b6678494b184d34870e
vladimir.cherepnalko <vladimir.cherepnalkovski@armedia.com>
2020-06-24 09:55:12 +0200

    Add My Consultations widget on FOIA

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/dashboard/dashboard.client.module.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/dashboard/dashboard.client.module.js
@@ -4,4 +4,4 @@ ApplicationConfiguration.registerModule('dashboard', [ 'adf', 'adf.provider', 'c
         'dashboard.details', 'dashboard.pictures', 'dashboard.locations', 'dashboard.phones', 'dashboard.faxes', 'dashboard.emails', 'dashboard.urls', 'dashboard.aliases', 'dashboard.dbas', 'dashboard.people', 'dashboard.ids', 'dashboard.complaints', 'dashboard.cases', 'dashboard.organizations',
         'dashboard.reworkDetails', 'dashboard.docReview', 'dashboard.parentDocs', 'dashboard.documents', 'dashboard.participants', 'dashboard.notes', 'dashboard.tasks', 'dashboard.references', 'dashboard.workflow', 'dashboard.relOrganizations', 'dashboard.related', 'dashboard.history',
         'dashboard.time', 'dashboard.cost', 'dashboard.signature', 'dashboard.person', 'dashboard.expenses', 'dashboard.hoursSummary', 'dashboard.tags', 'dashboard.calendar', 'dashboard.approvalRouting', 'dashboard.new-cases', 'dashboard.workflow-report', 'dashboard.approvers',
-        'dashboard.websites-widget', 'dashboard.my-overdue-requests', 'dashboard.my-consultations',]);
+        'dashboard.websites-widget', 'dashboard.my-overdue-requests', 'dashboard.my-consultations']);
