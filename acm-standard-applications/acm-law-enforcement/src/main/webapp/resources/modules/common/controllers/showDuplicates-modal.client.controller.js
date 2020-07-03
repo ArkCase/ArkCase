ddc1d0508e008a47ce9de5c32116d81eb1f07aa5
Martin Jovanovski <martin.jovanovski@armedia.com>
2020-07-03 11:56:08 +0200

    no message

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/controllers/showDuplicates-modal.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/controllers/showDuplicates-modal.client.controller.js
@@ -2,33 +2,41 @@
 
 angular.module('common').controller(
     'Common.ShowDuplicates',
-    ['$scope','$modal', '$modalInstance', 'params',
-        function ($scope, $modal, $modalInstance, params) {
+    ['$scope', '$modal', '$translate','$modalInstance', 'ConfigService', 'UtilService',
+        function ($scope, $modal, $translate, $modalInstance, ConfigService, Util) {
+
+            ConfigService.getModuleConfig("common").then(function(moduleConfig) {
+                $scope.commonConfig = moduleConfig;
+                return moduleConfig;
+            });
             $scope.modalInstance = $modalInstance;
-            $scope.header = params.header;
-            $scope.config = params.config;
+            $scope.header = $translate.instant("common.duplicates.header");
+            $scope.config = Util.goodMapValue($scope.commonConfig, "showDuplicates");
+            $scope.data = data;
+
 
             $scope.gridOptions = {
                 enableColumnResizing: true,
-                enableRowSelection: false,
-                enableRowHeaderSelection: false,
-                multiSelect: false,
+                enableRowSelection: true,
+                multiSelect: true,
                 noUnselect: false,
                 columnDefs: $scope.config.columnDefs,
-                onRegisterApi: function(gridApi){
+                onRegisterApi: function (gridApi) {
                     //set gridApi on scope
                     $scope.gridApi = gridApi;
-                    gridApi.selection.on.rowSelectionChanged($scope,function(row){
+                    gridApi.selection.on.rowSelectionChanged($scope, function (row) {
                         $scope.selectedItem = row.entity;
                     });
                 }
             };
 
-            $scope.onClickOk = function() {
+            $scope.gridOptions.data = params.data;
+
+            $scope.onClickOk = function () {
                 $modalInstance.close($scope.selectedItem);
             };
 
-            $scope.onClickCancel = function() {
+            $scope.onClickCancel = function () {
                 $modalInstance.dismiss('Cancel');
             };
 
