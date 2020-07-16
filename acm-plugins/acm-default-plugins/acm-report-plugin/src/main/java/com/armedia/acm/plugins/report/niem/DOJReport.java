621c16ff5b96f6972c51f2c04224cd335de7bfdd
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2020-07-16 11:31:24 +0200

    AFDP-9138 Add multiple DOJ Report conversions and additional test resources

--- acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/niem/DOJReport.java
+++ acm-plugins/acm-default-plugins/acm-report-plugin/src/main/java/com/armedia/acm/plugins/report/niem/DOJReport.java
@@ -15,10 +15,24 @@ public enum DOJReport
     APPEAL_DISPOSITION_APPLIED_EXEMPTIONS("ADE", "foia:AppealDispositionAppliedExemptionsSection"),
     REQUEST_DISPOSITION_APPLIED_EXEMPTIONS("RDE", "foia:RequestDispositionAppliedExemptionsSection"),
     APPEAL_PROCESSED_COMPARISON("APC", "foia:ProcessedAppealComparisonSection"),
-    REQUEST_PROCESSED_COMPARISON("RPC", "foia:ProcessedRequestComparisonSection");
+    REQUEST_PROCESSED_COMPARISON("RPC", "foia:ProcessedRequestComparisonSection"),
+    PROCESSED_APPEALS("PA", "foia:ProcessedAppealSection"),
+    PROCESSED_REQUESTS("PS", "foia:ProcessedRequestSection"),
+    PROCESSED_CONSULTATIONS("PCN", "foia:ProcessedConsultationSection"),
+    APPEAL_NON_EXEMPTION_DENIAL("ANE", "foia:AppealNonExemptionDenialSection"),
+    APPEAL_RESPONSE_TIME("ART", "foia:AppealResponseTimeSection"),
+    EXPEDITED_PROCESSING("EP", "foia:ExpeditedProcessingSection"),
+    FEE_WAIVER("FW", "foia:FeeWaiverSection"),
+    PERSONNEL_AND_COST("PC", "foia:PersonnelAndCostSection"),
+    FEES_COLLECTED("FC", "foia:FeesCollectedSection"),
+    SUBSECTION_USED("SU", "foia:SubsectionUsedSection"),
+    SUBSECTION_POST("SP", "foia:SubsectionPostSection"),
+    BACKLOG("BK", "foia:Backlog"),
+    BACKLOG_REQUEST_COMPARISON("RBC", "foia:BackloggedRequestComparisonSection"),
+    BACKLOG_APPEAL_COMPARISON("ABC", "foia:BackloggedAppealComparisonSection");
 
-    private String idPrefix;
-    private String sectionName;
+    private final String idPrefix;
+    private final String sectionName;
 
     DOJReport(String idPrefix, String sectionName)
     {
