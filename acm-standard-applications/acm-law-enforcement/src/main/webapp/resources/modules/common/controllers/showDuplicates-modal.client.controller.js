1acc2bebb1aec0d431c53e0ae8260c740b840c07
Martin Jovanovski <martin.jovanovski@armedia.com>
2020-07-31 17:52:40 +0200

    no message

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/controllers/showDuplicates-modal.client.controller.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/common/controllers/showDuplicates-modal.client.controller.js
@@ -2,18 +2,29 @@
 
 angular.module('common').controller(
     'Common.ShowDuplicates',
-    ['$scope', '$modal', '$translate', '$modalInstance', 'ConfigService', 'UtilService', 'params',
-        function ($scope, $modal, $translate, $modalInstance, ConfigService, Util, params) {
+    ['$scope', '$modal', '$translate', '$modalInstance', '$state', 'ConfigService', 'UtilService', 'params', 'EcmService', 'MessageService',
+        function ($scope, $modal, $translate, $modalInstance, $state, ConfigService, Util, params, Ecm, MessageService) {
 
             $scope.modalInstance = $modalInstance;
             $scope.gridOptions = {};
             $scope.header = $translate.instant("common.duplicates.header");
             $scope.data = params.data;
+
+            for (var i = 0; i < $scope.data.length; i++) {
+                if ($scope.data[i].className === "gov.foia.model.FOIAFile") {
+                    var activeVersion = parseInt($scope.data[i].activeVersionTag) - 1;
+                    $scope.data[i].reviewStatus = $scope.data[i].versions[activeVersion].reviewStatus === "" ?
+                        "No review Status" : $scope.data[i].versions[activeVersion].reviewStatus;
+                    $scope.data[i].redactionStatus = $scope.data[i].versions[activeVersion].redactionStatus === "" ?
+                        "Not Redacted" : $scope.data[i].versions[activeVersion].redactionStatus;
+                }
+            }
             $scope.moduleConfig = ConfigService.getModuleConfig("common").then(function (moduleConfig) {
                 $scope.config = Util.goodMapValue(moduleConfig, "showDuplicates");
                 $scope.gridOptions = {
                     enableColumnResizing: true,
                     enableRowSelection: true,
+                    enableRowHeaderSelection: false,
                     multiSelect: true,
                     noUnselect: false,
                     columnDefs: $scope.config.columnDefs,
@@ -21,16 +32,52 @@ angular.module('common').controller(
                     onRegisterApi: function (gridApi) {
                         //set gridApi on scope
                         $scope.gridApi = gridApi;
-                        gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-                            $scope.selectedItem = row.entity;
+                        gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+                            $scope.selectedRows = gridApi.selection.getSelectedRows();
                         });
                     }
                 };
             });
 
+            $scope.onClickObjLink = function(event, rowEntity) {
+                event.preventDefault();
+                $state.go($scope.modalParams.returnState, {
+                    id: rowEntity.container.containerObjectId,
+                    fileId: rowEntity.fileId
+                }, true);
+            };
 
-            $scope.onClickOk = function () {
-                $modalInstance.close($scope.selectedItem);
+            $scope.openCaseFile = function(event, rowEntity) {
+                event.preventDefault();
+                $state.go('cases.main', {
+                    id: rowEntity.container.containerObjectId,
+                }, true);
+            };
+
+            $scope.deleteFile = function(rowEntity) {
+                var dfd = $.Deferred();
+                for (var i = 0; i < rowEntity.length; i++) {
+                    var fileId = rowEntity[i].fileId;
+                    Util.serviceCall({
+                        service: Ecm.deleteFileTemporary,
+                        param: {
+                            fileId: fileId
+                        },
+                        data: {},
+                        onSuccess: function(data) {
+                            MessageService.succsessAction();
+                        },
+                        onError: function(error) {
+                            MessageService.error(error.data.message);
+                            dfd.reject();
+                        }
+                    }).then(function(deletedFileId) {
+                        dfd.resolve(deletedFileId);
+                    }, function(errorData) {
+                        MessageService.error(errorData.data);
+                        dfd.reject();
+                    });
+                }
             };
 
             $scope.onClickCancel = function () {
