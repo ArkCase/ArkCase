bee2d8aea8735632e05d6cd36d4b92d17788369d
Mario Gjurcheski <mario.gjurcheski@armedia.com>
2020-08-17 12:50:43 +0200

    AFDP-9278 - Update queries for all the properties that contains "." in their property key

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_services/object/object-lookup.client.service.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_services/object/object-lookup.client.service.js
@@ -974,10 +974,14 @@ angular.module('services').factory('Object.LookupService', ['$q', '$resource', '
      *
      * @returns {Object} An array returned by $resource
      */
-
     Service.getRequestCategories = function () {
-        return Service.getLookupByLookupName('requestCategory');
-
+        var selectEntry = {key: null, value: "Select Request Category"};
+        var requestCategories = [];
+        return Service.getLookupByLookupName("requestCategory").then(function (categories) {
+            requestCategories = categories;
+            requestCategories.unshift(selectEntry);
+            return requestCategories;
+        });
     };
 
     /**
@@ -992,8 +996,13 @@ angular.module('services').factory('Object.LookupService', ['$q', '$resource', '
      */
 
     Service.getDeliveryMethodOfResponses = function () {
-        return Service.getLookupByLookupName('deliveryMethodOfResponses');
-
+        var selectEntry = {key: null, value: "core.lookups.deliveryMethodOfResponses.select"};
+        var deliveryMethods = [];
+        return Service.getLookupByLookupName("deliveryMethodOfResponses").then(function (methods) {
+            deliveryMethods = methods;
+            deliveryMethods.unshift(selectEntry);
+            return deliveryMethods;
+        });
     };
 
     /**
@@ -1006,10 +1015,14 @@ angular.module('services').factory('Object.LookupService', ['$q', '$resource', '
      *
      * @returns {Object} An array returned by $resource
      */
-
     Service.getPayFees = function () {
-        return Service.getLookupByLookupName('payFees');
-
+        var selectEntry = {key: null, value: "Select Pay Fee"};
+        var payFees = [];
+        return Service.getLookupByLookupName("payFees").then(function (fees) {
+            payFees = fees;
+            payFees.unshift(selectEntry);
+            return payFees;
+        });
     };
 
 
