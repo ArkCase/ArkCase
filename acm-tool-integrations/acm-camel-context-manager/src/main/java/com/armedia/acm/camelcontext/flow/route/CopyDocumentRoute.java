79367255b8ab135ea462ce441d35ff5bcf7c5128
vladimir.cherepnalko <vladimir.cherepnalkovski@armedia.com>
2020-08-21 11:44:03 +0200

    Add new camel-cmis jar Add cmis:name parameter in Exchange message on copyDocumentRoute

--- acm-tool-integrations/acm-camel-context-manager/src/main/java/com/armedia/acm/camelcontext/flow/route/CopyDocumentRoute.java
+++ acm-tool-integrations/acm-camel-context-manager/src/main/java/com/armedia/acm/camelcontext/flow/route/CopyDocumentRoute.java
@@ -69,6 +69,7 @@ public class CopyDocumentRoute extends ArkCaseAbstractRoute
                     routeProperties = (Map<String, Object>) exchange.getIn().getBody();
                     exchange.getIn().getHeaders().put(PropertyIds.OBJECT_TYPE_ID, CamelCMISConstants.CMIS_DOCUMENT);
                     exchange.getIn().getHeaders().put(CamelCMISConstants.CMIS_OBJECT_ID, routeProperties.get("cmisDocumentId"));
+                    exchange.getIn().getHeaders().put(PropertyIds.NAME, routeProperties.get(PropertyIds.NAME));
                     exchange.getIn().getHeaders().put(CamelCMISConstants.CMIS_DESTIONATION_FOLDER_ID, routeProperties.get("dstFolderId"));
                     exchange.getIn().getHeaders().put(CamelCMISConstants.CMIS_ACTION, CamelCMISActions.COPY_DOCUMENT);
                     MDC.put(HttpInvokerUtil.EVENT_MDC_REQUEST_ALFRESCO_USER_ID_KEY,
