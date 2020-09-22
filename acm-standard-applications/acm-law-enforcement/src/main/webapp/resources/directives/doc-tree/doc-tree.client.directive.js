59d4313223747d6ab9cde11da06075642842dadb
Martin Jovanovski <martin.jovanovski@armedia.com>
2020-09-22 14:53:26 +0200

    no message

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/directives/doc-tree/doc-tree.client.directive.js
@@ -3163,7 +3163,7 @@ angular.module('directives').directive(
                                             data: response
                                         };
                                         var modalInstance = $modal.open({
-                                            templateUrl: "modules/common/views/showDuplicates-modal.client.view.html",
+                                            templateUrl: "modules/common/views/show-duplicates-modal.client.view.html",
                                             controller: "Common.ShowDuplicates",
                                             animation: true,
                                             windowClass: 'modal-width-80',
