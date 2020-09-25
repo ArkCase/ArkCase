a087b943613b46ad548c4e8ef740166d1bc5aaae
Ivan Denkovski <ivan.denkovski@armedia.com>
2020-09-25 13:58:00 +0200

    Removed obsolete methods which used File object or filename of stylesheet

--- acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfService.java
+++ acm-tool-integrations/acm-pdf-utilities/src/main/java/com/armedia/acm/pdf/service/PdfService.java
@@ -53,20 +53,6 @@ public interface PdfService
      * Generate PDF file based on XSL-FO stylesheet, XML data source and replacement parameters. NOTE: the caller is
      * responsible for deleting the file afterwards (not to leave mess behind)
      *
-     * @param xslFile
-     *            XSL-FO stylesheet
-     * @param source
-     *            XML data source required for XML transformation
-     * @return path to the newly generated PDF file (random filename stored in temp folder)
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    String generatePdf(File xslFile, Source source) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet, XML data source and replacement parameters. NOTE: the caller is
-     * responsible for deleting the file afterwards (not to leave mess behind)
-     *
      * @param xslStream
      *            XSL-FO stylesheet
      * @param baseURI
@@ -78,52 +64,6 @@ public interface PdfService
      *             on PDF creation error
      */
     String generatePdf(InputStream xslStream, URI baseURI, Source source) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet, XML data source and replacement parameters.
-     * NOTE: the caller is responsible for deleting the file afterwards (not to leave mess behind)
-     *
-     * @param xslFile
-     *            XSL-FO stylesheet
-     * @param source
-     *            XML data source required for XML transformation
-     * @param parameters
-     *            a key-value map of parameters to be replaced in the stylesheet
-     * @return path to the newly generated PDF file (random filename stored in temp folder)
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    String generatePdf(File xslFile, Source source, Map<String, String> parameters) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet, XML data source and replacement parameters.
-     * NOTE: the caller is responsible for deleting the file afterwards (not to leave mess behind)
-     *
-     * @param xslFilename
-     *            path to XSL-FO stylesheet
-     * @param source
-     *            XML data source required for XML transformation
-     * @param parameters
-     *            a key-value map of parameters to be replaced in the stylesheet
-     * @return path to the newly generated PDF file (random filename stored in temp folder)
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    String generatePdf(String xslFilename, Source source, Map<String, String> parameters) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet and replacement parameters (no XML data source).
-     * NOTE: the caller is responsible for deleting the file afterwards (not to leave mess behind)
-     *
-     * @param xslFile
-     *            XSL-FO stylesheet
-     * @param parameters
-     *            a key-value map of parameters to be replaced in the stylesheet
-     * @return path to the newly generated PDF file (random filename stored in temp folder)
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    String generatePdf(File xslFile, Map<String, String> parameters) throws PdfServiceException;
 
     /**
      * Generate PDF file based on XSL-FO stylesheet and replacement parameters (no XML data source).
@@ -140,37 +80,6 @@ public interface PdfService
      *             on PDF creation error
      */
     String generatePdf(InputStream xslStream, URI baseURI, Map<String, String> parameters) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet and replacement parameters (no XML data source).
-     * NOTE: the caller is responsible for deleting the file afterwards (not to leave mess behind)
-     *
-     * @param xslFilename
-     *            path to XSL-FO stylesheet
-     * @param parameters
-     *            a key-value map of parameters to be replaced in the stylesheet
-     * @return path to the newly generated PDF file (random filename stored in temp folder)
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    String generatePdf(String xslFilename, Map<String, String> parameters) throws PdfServiceException;
-
-    /**
-     * Generate PDF file based on XSL-FO stylesheet and replacement parameters (no XML data source) to the given
-     * targetStream.
-     *
-     * @param xslFile
-     *            XSL-FO stylesheet
-     * @param source
-     *            XML data source required for XML transformation
-     * @param targetStream
-     *            the stream to write the generated file
-     * @param parameters
-     *            a key-value map of parameters to be replaced in the stylesheet
-     * @throws PdfServiceException
-     *             on PDF creation error
-     */
-    void generatePdf(File xslFile, Source source, OutputStream targetStream, Map<String, String> parameters) throws PdfServiceException;
 
     /**
      * Generate PDF file based on XSL-FO stylesheet and replacement parameters (no XML data source) to the given
