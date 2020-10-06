0b8b7e45dda7eefd54c6910e25a66f1c8e813d96
ana.serafimoska <ana.serafimoska@armedia.com>
2020-10-06 16:40:58 +0200

    ACFP-6 Fixed review comments

--- acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/services/portal.client.service.js
+++ acm-standard-applications/acm-law-enforcement/src/main/webapp/resources/modules/admin/services/portal.client.service.js
@@ -51,14 +51,14 @@ angular.module('admin').factory('Admin.PortalConfigurationService', [ '$http', f
     var getAuthenticatedMode = function () {
         return $http({
             method: 'GET',
-            url: 'api/latest/service/portalgateway/admin/portals/authenticatedMode'
+            url: 'api/latest/service/portalgateway/admin/portals/config'
         });
     };
 
     var saveAuthenticatedMode = function (portal) {
         return $http({
             method: 'PUT',
-            url: 'api/latest/service/portalgateway/admin/portals/authenticatedMode',
+            url: 'api/latest/service/portalgateway/admin/portals/config',
             data: portal,
             headers: {
                 "Content-Type": "application/json"
