98eb8f21f680706ee479be37fe430232a8268ab1
Martin Jovanovski <martin.jovanovski@armedia.com>
2020-10-30 09:03:05 +0100

    WIP: ACFP-196

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/EcmFileEditingEnabledConfig.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/EcmFileEditingEnabledConfig.java
@@ -1,8 +1,8 @@
-package com.armedia.acm.services.email.service;
+package com.armedia.acm.plugins.ecm.model;
 
 /*-
  * #%L
- * ACM Service: Email
+ * ACM Service: Enterprise Content Management
  * %%
  * Copyright (C) 2014 - 2020 ArkCase LLC
  * %%
@@ -27,24 +27,20 @@ package com.armedia.acm.services.email.service;
  * #L%
  */
 
-import javax.mail.Message;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Optional;
+public class EcmFileEditingEnabledConfig {
 
-/**
- * Created by Aleksandar Acevski <aleksandar.acevski@armedia.com> on October, 2020
- */
-public interface OriginalEmailExtractorStrategy
-{
+    @JsonProperty("file.editing_enabled")
+    @Value("${file.editing_enabled}")
+    private Boolean enableEditing;
 
-    /**
-     * 
-     * Get the forwarded message from the contents, headers or attachments of an email
-     * 
-     * @param message
-     *            Email message
-     * @return The original email if the extraction was successful
-     */
-    Optional<Message> getForwardedMessage(Message message);
+    public Boolean getEnableEditing() {
+        return enableEditing;
+    }
 
+    public void setEnableEditing(Boolean enableEditing) {
+        this.enableEditing = enableEditing;
+    }
 }
