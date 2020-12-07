dce265e06bc909122f793c3d0d6b77ec7abe0a63
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2020-12-07 16:34:19 +0000

    Merge branch 'ACFP-348-error-message-is-shown-when-uploading-determination-letter-on-unassigned-object' into 'develop'
    ACFP-348 display custom error message that the assignee is required
    
    Closes ACFP-348
    
    See merge request arkcase/ArkCase!2721
    
    (cherry picked from commit 36ee4336a741c7822ecfa8e333d98a81e4c826da)
    
    59ad44e2 ACFP-348 display custom error message that the assignee is required

---
+++ acm-services/acm-service-correspondence/src/main/java/com/armedia/acm/correspondence/exception/CorrespondenceTemplateMissingAssigneeException.java
@@ -0,0 +1,10 @@
+package com.armedia.acm.correspondence.exception;
+
+public class CorrespondenceTemplateMissingAssigneeException extends RuntimeException
+{
+
+    public CorrespondenceTemplateMissingAssigneeException(String message)
+    {
+        super(message);
+    }
+}
