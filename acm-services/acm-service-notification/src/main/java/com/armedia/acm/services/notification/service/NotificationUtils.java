3d09f055df51f8ee34aa43b961d77332abd18be9
Viktorija Kostovska <viktorija.kostovska@armedia.com>
2021-02-04 11:17:58 +0100

    Added log when portal configuration does not exist

--- acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/service/NotificationUtils.java
+++ acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/service/NotificationUtils.java
@@ -43,6 +43,8 @@ import com.armedia.acm.services.users.model.AcmUserState;
 import com.armedia.acm.services.users.model.group.AcmGroup;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.UnsatisfiedDependencyException;
 
 import java.util.Collections;
@@ -197,6 +199,7 @@ public class NotificationUtils
         }
         catch (UnsatisfiedDependencyException e)
         {
+            LOG.warn("Portal configuration does not exist");
             return true;
         }
 
