d0c88c3c1b93879a76c1fb7e5b8ed6d27582aa7e
ana.serafimoska <ana.serafimoska@armedia.com>
2021-03-04 12:15:55 +0100

    Added new "mailChannelReceiver" channel that will read all incoming emails from the configure sender

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/admin/controllers/components/security-email-templates.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/admin/controllers/components/security-email-templates.client.controller.js
@@ -10,9 +10,7 @@ angular.module('admin').controller('Admin.SecurityEmailTemplatesController',
                     enableBurstingAttachments: result.data["email.enableBurstingAttachments"],
                     enableRequest: result.data["email.create.case.enabled"],
                     user_case: result.data["email.CASE_FILE.user"].replace('%40', '@'),
-                    pass_case: result.data["email.CASE_FILE.password"],
-                    user_task: result.data["email.TASK.user"].replace('%40', '@'),
-                    pass_task: result.data["email.TASK.password"]
+                    pass_case: result.data["email.CASE_FILE.password"]
                 }
             });
 
@@ -21,9 +19,7 @@ angular.module('admin').controller('Admin.SecurityEmailTemplatesController',
                     "email.create.case.enabled": $scope.emailReceiverConfiguration.enableRequest,
                     "email.CASE_FILE.user": $scope.emailReceiverConfiguration.user_case.replace('@', '%40'),
                     "email.CASE_FILE.password": $scope.emailReceiverConfiguration.pass_case,
-                    "email.enableBurstingAttachments": $scope.originalEmailReceiverConfiguration["email.enableBurstingAttachments"],
-                    "email.TASK.user": $scope.emailReceiverConfiguration.user_task.replace('@', '%40'),
-                    "email.TASK.password": $scope.emailReceiverConfiguration.pass_task,
+                    "email.enableBurstingAttachments": $scope.originalEmailReceiverConfiguration["email.enableBurstingAttachments"]
                 };
                 emailTemplatesService.saveEmailReceiverConfiguration(newEmailReceiverConfiguration).then(function(value) {
                     MessageService.succsessAction();
@@ -37,9 +33,7 @@ angular.module('admin').controller('Admin.SecurityEmailTemplatesController',
                     "email.create.case.enabled": $scope.originalEmailReceiverConfiguration["email.create.case.enabled"],
                     "email.CASE_FILE.user": $scope.originalEmailReceiverConfiguration["email.CASE_FILE.user"],
                     "email.CASE_FILE.password": $scope.originalEmailReceiverConfiguration["email.CASE_FILE.password"],
-                    "email.enableBurstingAttachments": $scope.emailReceiverConfiguration.enableBurstingAttachments,
-                    "email.TASK.user": $scope.emailReceiverConfiguration.user_task.replace('@', '%40'),
-                    "email.TASK.password": $scope.emailReceiverConfiguration.pass_task
+                    "email.enableBurstingAttachments": $scope.emailReceiverConfiguration.enableBurstingAttachments
                 };
                 emailTemplatesService.saveEmailReceiverConfiguration(emailReceiverConfiguration).then(function() {
                     MessageService.succsessAction();
