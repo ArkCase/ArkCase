a1aa454b12b2e523c546fae5ac88d56cb5e31731
Viktorija Kostovska <viktorija.kostovska@armedia.com>
2021-03-09 10:38:26 +0100

    Fixed type of new column in DB

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-actions.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-actions.client.controller.js
@@ -203,7 +203,6 @@ angular.module('cases').controller(
 
             $scope.sendEmail = function () {
                 var params = {
-                    request: $scope.objectInfo,
                     objectId: $scope.objectInfo.id,
                     objectType: ObjectService.ObjectTypes.CASE_FILE,
                     objectNumber: $scope.objectInfo.caseNumber,
