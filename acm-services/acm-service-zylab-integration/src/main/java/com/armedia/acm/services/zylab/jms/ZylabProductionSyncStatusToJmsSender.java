4b0c468d53c8384d4151d27b63b292ae68e7df86
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2021-03-24 11:59:28 +0100

    UWP-34 Change Arkcase ZyLAB Integration queue names

--- acm-services/acm-service-zylab-integration/src/main/java/com/armedia/acm/services/zylab/jms/ZylabProductionSyncStatusToJmsSender.java
+++ acm-services/acm-service-zylab-integration/src/main/java/com/armedia/acm/services/zylab/jms/ZylabProductionSyncStatusToJmsSender.java
@@ -1,5 +1,32 @@
 package com.armedia.acm.services.zylab.jms;
 
+/*-
+ * #%L
+ * ACM Service: Arkcase ZyLAB Integration
+ * %%
+ * Copyright (C) 2014 - 2021 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import javax.jms.ConnectionFactory;
 
 import org.apache.logging.log4j.LogManager;
@@ -26,7 +53,7 @@ public class ZylabProductionSyncStatusToJmsSender implements InitializingBean
 
     public void sendProductionSyncStatus(ZylabProductionSyncDTO zylabProductionSyncDTO)
     {
-        sendToJmsQueue(zylabProductionSyncDTO, "arkcase-zylab-integration");
+        sendToJmsQueue(zylabProductionSyncDTO, "arkcase-zylab-queue");
     }
 
     private void sendToJmsQueue(ZylabProductionSyncDTO zylabProductionSyncDTO, String queueName)
