0b251d467974825d314d613ec227eb50bb5b0d1b
Nadica Deleva <nadica.cuculova@armedia.com>
2021-04-06 12:30:55 +0200

    ACFP-676: Update existing person on portal user registration

--- acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/dao/OrganizationDao.java
+++ acm-plugins/acm-default-plugins/acm-person-plugin/src/main/java/com/armedia/acm/plugins/person/dao/OrganizationDao.java
@@ -68,11 +68,7 @@ public class OrganizationDao extends AcmAbstractDao<Organization>
         String query = "SELECT o FROM Organization o WHERE LOWER(o.organizationValue) = :organizationName";
         TypedQuery<Organization> dbQuery = getEm().createQuery(query, Organization.class);
         dbQuery.setParameter("organizationName", organizationName.toLowerCase());
-        List<Organization> results = dbQuery.getResultList();
-        if (results.isEmpty()) {
-            return null;
-        }
-        return results;
+        return dbQuery.getResultList();
     }
 
     @Transactional
