694b86cf80b704fd9eede18bef7867370ba05532
Yibu Lin <yibu.lin@armedia.com>
2021-04-09 11:50:55 -0400

    uwp-319 original check

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileServiceImpl.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/service/impl/EcmFileServiceImpl.java
@@ -43,6 +43,7 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -262,6 +263,16 @@ public class EcmFileServiceImpl implements ApplicationEventPublisherAware, EcmFi
 
         log.info("The user '{}' uploaded file: '{}'", authentication.getName(), fileName);
 
+        //Solve the problem of Yahoo mail filename encode issue
+        if(fileName.startsWith("=?UTF-8?b?")) {
+            fileName = fileName.substring(10, fileName.length() - 3);
+            byte[] decodedBytes = Base64.getDecoder().decode(fileName.getBytes());
+
+            fileName = new String(decodedBytes);
+            log.debug("FileName after Decode: " + fileName);
+
+            arkcaseFileName = fileName.substring(0,fileName.lastIndexOf('.'));
+        }
         EcmFile metadata = new EcmFile();
         metadata.setFileType(fileType);
         metadata.setCategory(fileCategory);
