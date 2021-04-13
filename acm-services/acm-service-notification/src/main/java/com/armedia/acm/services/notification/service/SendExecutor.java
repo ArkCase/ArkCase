294623771f1a6499d361a0c105c567c9cb707bb6
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-04-13 14:53:45 +0000

    Merge branch 'UWP-507_registration_email_not_sent' into 'develop'
    UWP-507 fixing registration email and adding try catch in executor
    
    Closes UWP-507
    
    See merge request arkcase/ArkCase!3063
    
    (cherry picked from commit 8ce39e1e7f2c38188d8ff38634cb1ebdee524da1)
    
    8078b5cd UWP-507 fixing registration email and adding try catch in executor

--- acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/service/SendExecutor.java
+++ acm-services/acm-service-notification/src/main/java/com/armedia/acm/services/notification/service/SendExecutor.java
@@ -76,9 +76,18 @@ public class SendExecutor implements Executor
 
         for (NotificationSenderFactory senderFactory : senderFactoryList.values())
         {
-            if (notification.getState() == null || !notification.getState().equals(NotificationConstants.STATE_SENT))
+
+on            if (notification.getState() == null || !notification.getState().equals(NotificationConstants.STATE_SENT))
             {
-                notification = senderFactory.getNotificationSender().send(notification, templateModelProvider.getModel(notification));
+                try
+                {
+                    notification = senderFactory.getNotificationSender().send(notification, templateModelProvider.getModel(notification));
+                }
+                catch (Exception e)
+                {
+                    log.error("Can not send the notification. Error: {}", e.getMessage());
+                    notification.setStatus(NotificationConstants.STATE_NOT_SENT);
+                }
             }
         }
 
