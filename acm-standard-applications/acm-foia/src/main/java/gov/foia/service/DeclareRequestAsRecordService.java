b19f6b7d30d8a6564b21763c4c343ca6ba677a66
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2021-04-14 14:52:00 +0200

    UWP-513 Add ability to declare requests as records, immediately after release or after a configurable delay

--- acm-standard-applications/acm-foia/src/main/java/gov/foia/service/DeclareRequestAsRecordService.java
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/service/DeclareRequestAsRecordService.java
@@ -1,32 +1,5 @@
 package gov.foia.service;
 
-/*-
- * #%L
- * ACM Standard Application: Freedom of Information Act
- * %%
- * Copyright (C) 2014 - 2021 ArkCase LLC
- * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
- * provided under the following open source license terms:
- * 
- * ArkCase is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *  
- * ArkCase is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
- * #L%
- */
-
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Date;
@@ -79,21 +52,6 @@ public class DeclareRequestAsRecordService
                 declareRecords(request);
             }
         }
-    }
-
-    public void declareRecordsIfEnabled(Long requestId)
-    {
-        FOIARequest request = requestDao.find(requestId);
-
-        if (request != null && shouldDeclareRequestAsRecordWithoutDelay())
-        {
-            declareRecords(request);
-        }
-    }
-
-    private boolean shouldDeclareRequestAsRecordWithoutDelay()
-    {
-        return foiaConfig.getDeclareRequestAsRecordsEnabled() && foiaConfig.getDeclareRequestAsRecordsDaysDelay() == 0;
     }
 
     public void declareRecords(FOIARequest request)
