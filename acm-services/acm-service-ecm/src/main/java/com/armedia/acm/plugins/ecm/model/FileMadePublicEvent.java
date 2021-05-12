ddef8eeb6f1f6c5a5fbfe0921c276736261e2cfb
Martin Jovanovski <martin.jovanovski@armedia.com>
2021-05-12 06:18:24 -0700

    fixing compiler issue

--- acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/FileMadePublicEvent.java
+++ acm-services/acm-service-ecm/src/main/java/com/armedia/acm/plugins/ecm/model/FileMadePublicEvent.java
@@ -1,14 +1,40 @@
 package com.armedia.acm.plugins.ecm.model;
 
+/*-
+ * #%L
+ * ACM Service: Enterprise Content Management
+ * %%
+ * Copyright (C) 2014 - 2021 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import com.armedia.acm.core.model.AcmEvent;
 import org.springframework.security.core.Authentication;
-import sun.text.normalizer.ICUBinary;
 
 import java.util.Date;
 
 public class FileMadePublicEvent extends AcmEvent
 {
-    private static final long serialVersionUID = 7492500722858546075L;
+    private static final long serialVersionUID = -2309165680659166953L;
 
     public FileMadePublicEvent(EcmFile source, Authentication auth)
     {
