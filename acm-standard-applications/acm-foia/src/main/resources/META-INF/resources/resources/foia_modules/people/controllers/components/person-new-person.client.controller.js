a4b0145c2079cfffa13c682511c2066bc73284b3
Martin Jovanovski <martin.jovanovski@armedia.com>
2021-05-17 01:37:43 -0700

    UWP-567 fixing error thats shown after creating person

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/people/controllers/components/person-new-person.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/people/controllers/components/person-new-person.client.controller.js
@@ -215,7 +215,7 @@ angular.module('people').controller(
                     MessageService.info(personWasCreatedMessage);
                     $timeout(function () {
                         ObjectService.showObject(ObjectService.ObjectTypes.PERSON, objectInfo.data.id);
-                    }, 1500);
+                    }, 1000);
                     $scope.onModalClose();
                     $scope.loading = false;
                     $scope.loadingIcon = "fa fa-floppy-o";
