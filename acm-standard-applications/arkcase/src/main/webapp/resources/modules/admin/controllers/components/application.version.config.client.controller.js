8f65eaa351d4a1f62ae1fb45bb035a640ebd7b4f
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-05-25 14:45:13 +0000

    Merge branch 'ACFP-569-application-version-date-last-installed-show-two-date-last-installed' into 'develop'
    ACFP-569 Display two last installed dates
    
    Closes ACFP-569
    
    See merge request arkcase/ArkCase!3181
    
    (cherry picked from commit ca906e9c0693e459bcdb6aa7ff7af23112416c0b)
    
    e3544a01 ACFP-569 Display two last installed dates

--- acm-standard-applications/arkcase/src/main/webapp/resources/modules/admin/controllers/components/application.version.config.client.controller.js
+++ acm-standard-applications/arkcase/src/main/webapp/resources/modules/admin/controllers/components/application.version.config.client.controller.js
@@ -5,18 +5,9 @@ angular.module('admin').controller('Admin.ApplicationVersionController', [ '$sco
     $scope.applicationVersion = "";
     ApplicationVersionService.getApplicationVersion().then(function (result) {
         $scope.applicationVersion = result.data["Implementation-Version"];
-        var warBuildTime = moment.utc(result.data["Build-Time"]).format("MMMM DD, YYYY");
-        var jarModifiedTime = "";
-        var isJarTimeNewer = false;
+        $scope.buildTime = moment.utc(result.data["Build-Time"]).format("MMMM DD, YYYY");
         if (result.data["JarModifiedTime"]) {
-            jarModifiedTime = moment.utc(result.data["JarModifiedTime"]).format("MMMM DD, YYYY");
-            isJarTimeNewer = moment(jarModifiedTime).isAfter(warBuildTime);
-        }
-
-        if (jarModifiedTime !== "" && isJarTimeNewer === true) {
-            $scope.buildTime = jarModifiedTime;
-        } else {
-            $scope.buildTime = warBuildTime;
+            $scope.jarModifiedTime = moment.utc(result.data["JarModifiedTime"]).format("MMMM DD, YYYY");
         }
         if (result.data["extensionVersion"]) {
             $scope.extensionVersion = result.data["extensionVersion"];
