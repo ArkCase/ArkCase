b2e1734f3de1e280198acfc48a3f9e7d55e21edb
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-05-31 10:36:21 +0000

    Merge branch 'ACFP-784_fixing_date_time_issues' into 'develop'
    UWP 784 fixing date and time issues
    
    Closes ACFP-784
    
    See merge request arkcase/ArkCase!3190
    
    (cherry picked from commit 84e71079e200ec2a45af7644d543965695cd7fdc)
    
    4ece649d UWP 784 fixing date and time issues

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-info.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-info.client.controller.js
@@ -90,7 +90,7 @@ angular.module('cases').controller(
                         else {
                             $scope.calculateDaysObj = DueDateService.daysLeftWithWeekends($scope.holidays, $scope.objectInfo.dueDate);
                         }
-                        $scope.dueDate = moment.utc($scope.objectInfo.dueDate).local().format('MMM/DD/YYYY h:mmA');
+                        $scope.dueDate = moment.utc($scope.objectInfo.dueDate).local().format('MM/DD/YYYY h:mmA');
                     }
                     CaseLookupService.getApprovers($scope.owningGroup, $scope.assignee).then(function (approvers) {
                         var options = [];
@@ -289,7 +289,7 @@ angular.module('cases').controller(
 
             function dueDateChanged(e, newDueDate) {
                 $scope.objectInfo.dueDate = new Date(newDueDate).toISOString();
-                $scope.dueDate = moment.utc($scope.objectInfo.dueDate).local().format('MMM/DD/YYYY h:mmA');
+                $scope.dueDate = moment.utc($scope.objectInfo.dueDate).local().format('MM/DD/YYYY h:mmA');
                 if(!$scope.includeWeekends) {
                     $scope.calculateDaysObj = DueDateService.daysLeft($scope.holidays, $scope.objectInfo.dueDate);
                 }
