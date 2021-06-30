8f90e7803b97b160a8a2d0aa197c9ccc6a9c0b2f
kellavivek <vivek.kella@armedia.com>
2021-06-30 14:21:32 +0530

    increased milton repo version from 2.8.0.3 to 3.0.0.194

--- acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/pipeline/postsave/CaseFileDueDateHandler.java
+++ acm-plugins/acm-default-plugins/acm-case-file-plugin/src/main/java/com/armedia/acm/plugins/casefile/pipeline/postsave/CaseFileDueDateHandler.java
@@ -1,5 +1,32 @@
 package com.armedia.acm.plugins.casefile.pipeline.postsave;
 
+/*-
+ * #%L
+ * ACM Default Plugin: Case File
+ * %%
+ * Copyright (C) 2014 - 2021 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import com.armedia.acm.plugins.casefile.model.CaseFile;
 import com.armedia.acm.plugins.casefile.pipeline.CaseFilePipelineContext;
 import com.armedia.acm.services.holiday.service.HolidayConfigurationService;
