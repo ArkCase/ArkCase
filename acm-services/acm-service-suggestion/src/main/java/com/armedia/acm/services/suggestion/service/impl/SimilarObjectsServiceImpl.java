1487c195a0142556af0c3463b05876f76cee9eb4
kellavivek <vivek.kella@armedia.com>
2021-07-06 13:05:28 +0530

    replaced slf4j with log4j

--- acm-services/acm-service-suggestion/src/main/java/com/armedia/acm/services/suggestion/service/impl/SimilarObjectsServiceImpl.java
+++ acm-services/acm-service-suggestion/src/main/java/com/armedia/acm/services/suggestion/service/impl/SimilarObjectsServiceImpl.java
@@ -37,8 +37,8 @@ import com.armedia.acm.services.suggestion.service.SimilarObjectsService;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
 
 import java.io.UnsupportedEncodingException;
@@ -58,7 +58,7 @@ public class SimilarObjectsServiceImpl implements SimilarObjectsService
 
     private static final int MAX_SIMILAR_OBJECTS = 100;
 
-    private final Logger log = LoggerFactory.getLogger(getClass().getName());
+    private final Logger log = LogManager.getLogger(getClass().getName());
 
     private ExecuteSolrQuery executeSolrQuery;
 
