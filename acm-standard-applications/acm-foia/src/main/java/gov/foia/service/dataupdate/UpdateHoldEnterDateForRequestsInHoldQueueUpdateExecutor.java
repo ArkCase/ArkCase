c5ddfef8e01ecd5c86ad6d381c0f5db13b4a976a
Martin Jovanovski <martin.jovanovski@armedia.com>
2021-07-12 17:13:50 +0200

    ACFP-910 adding date updater for requests in hold queue

---
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/service/dataupdate/UpdateHoldEnterDateForRequestsInHoldQueueUpdateExecutor.java
@@ -0,0 +1,44 @@
+package gov.foia.service.dataupdate;
+
+import com.armedia.acm.services.dataupdate.service.AcmDataUpdateExecutor;
+import gov.foia.dao.FOIARequestDao;
+import gov.foia.model.FOIARequest;
+import org.springframework.scheduling.annotation.Async;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class UpdateHoldEnterDateForRequestsInHoldQueueUpdateExecutor implements AcmDataUpdateExecutor
+{
+
+    private FOIARequestDao requestDao;
+
+    @Override
+    public String getUpdateId()
+    {
+        return "update_hold_enter_date_for_requests_in_hold_queue";
+    }
+
+    @Override
+    @Async
+    public void execute()
+    {
+        List<FOIARequest> requestList = getRequestDao().getAllRequestsInHoldBefore(LocalDate.now());
+
+        for (FOIARequest request : requestList)
+        {
+            request.setHoldEnterDate(LocalDate.now());
+            requestDao.save(request);
+        }
+    }
+
+    public FOIARequestDao getRequestDao()
+    {
+        return requestDao;
+    }
+
+    public void setRequestDao(FOIARequestDao requestDao)
+    {
+        this.requestDao = requestDao;
+    }
+}
