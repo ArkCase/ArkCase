b2f2879308b3846b0da1c1eb9e81f55632fa44c4
Martin Jovanovski <martin.jovanovski@armedia.com>
2021-07-16 15:15:32 +0200

    ACFP-627 administrator should be able to delete request

--- acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-actions-footer.client.controller.js
+++ acm-standard-applications/acm-foia/src/main/resources/META-INF/resources/resources/foia_modules/cases/controllers/components/case-actions-footer.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('cases').controller('Cases.ActionsFooterController',
-    ['$q', '$scope', '$translate', '$state', '$stateParams', 'Case.InfoService', 'Helper.ObjectBrowserService', 'QueuesService', '$modal', 'Object.NoteService', 'Admin.FoiaConfigService',
-        function ($q, $scope, $translate, $state, $stateParams, CaseInfoService, HelperObjectBrowserService, QueuesService, $modal, NotesService, AdminFoiaConfigService) {
+    ['$q', '$scope', '$translate', '$state', '$stateParams', 'Case.InfoService', 'Helper.ObjectBrowserService', 'QueuesService', '$modal', 'Object.NoteService', 'Admin.FoiaConfigService', 'PermissionsService',
+        function ($q, $scope, $translate, $state, $stateParams, CaseInfoService, HelperObjectBrowserService, QueuesService, $modal, NotesService, AdminFoiaConfigService, PermissionsService) {
 
             new HelperObjectBrowserService.Component({
                 scope: $scope,
@@ -27,12 +27,23 @@ angular.module('cases').controller('Cases.ActionsFooterController',
             $scope.loading = false;
             $scope.loadingIcon = "fa fa-check";
 
+            $scope.hasDeletePermission = false;
+
             $scope.availableQueues = [];
             var onObjectInfoRetrieved = function (objectInfo) {
                 QueuesService.queryNextPossibleQueues(objectInfo.id).then(function (data) {
                     setQueueButtons(data);
                 });
             };
+
+            PermissionsService.getActionPermission('deleteRequest', "", {
+                objectType: "CASE_FILE"
+            }).then(function(result) {
+                if (result)
+                {
+                    $scope.hasDeletePermission = result;
+                }
+            });
 
             function setupNextQueue(name, deferred) {
                 var nextQueue = name;
@@ -458,6 +469,8 @@ angular.module('cases').controller('Cases.ActionsFooterController',
                     if (defaultNextQueue) {
                         availableQueues.push("Complete");
                     }
+                        availableQueues = _.without(availableQueues, 'Delete');
+
                 }
                 availableQueues = availableQueues.map(function (item) {
                     var tmpObj = {};
