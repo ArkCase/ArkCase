7a4f8930b164a12338777d3840da2fdddba614b3
Aleksandar Acevski <aleksandar.acevski@armedia.com>
2021-07-20 13:36:39 +0200

    ACFP-981 Add ability to remove the "Forgot Username" and "Forgot Password" Links from Login

--- acm-core-api/src/main/java/com/armedia/acm/core/model/ApplicationConfig.java
+++ acm-core-api/src/main/java/com/armedia/acm/core/model/ApplicationConfig.java
@@ -6,29 +6,29 @@ package com.armedia.acm.core.model;
  * %%
  * Copyright (C) 2014 - 2019 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
-import com.armedia.acm.core.UnmodifiableConfigProperty;
 import com.armedia.acm.core.DynamicApplicationConfig;
+import com.armedia.acm.core.UnmodifiableConfigProperty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -161,6 +161,10 @@ public class ApplicationConfig implements DynamicApplicationConfig
     @JsonProperty("application.properties.listFilesInFolder")
     @Value("${application.properties.listFilesInFolder}")
     private String listFilesInFolder;
+
+    @JsonProperty("application.properties.allowForgotUsernameAndPasswordOnLogin")
+    @Value("${application.properties.allowForgotUsernameAndPasswordOnLogin}")
+    private Boolean allowForgotUsernameAndPasswordOnLogin;
 
     public Integer getIdleLimit()
     {
@@ -410,5 +414,13 @@ public class ApplicationConfig implements DynamicApplicationConfig
     public void setListFilesInFolder(String listFilesInFolder)
     {
         this.listFilesInFolder = listFilesInFolder;
+    }
+
+    public Boolean getAllowForgotUsernameAndPasswordOnLogin() {
+        return allowForgotUsernameAndPasswordOnLogin;
+    }
+
+    public void setAllowForgotUsernameAndPasswordOnLogin(Boolean allowForgotUsernameAndPasswordOnLogin) {
+        this.allowForgotUsernameAndPasswordOnLogin = allowForgotUsernameAndPasswordOnLogin;
     }
 }
