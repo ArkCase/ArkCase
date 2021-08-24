050d945e53dbfc9d76f01fd33d1d72802f32b0be
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-08-24 11:10:18 +0000

    Merge branch 'ACFP-1059' into 'develop'
    ACFP-1059
    
    Closes ACFP-1059
    
    See merge request arkcase/ArkCase!3391
    
    (cherry picked from commit 52eff86af21c8b3899ef441ac4a11d0d7f162e38)
    
    8cd8916d ACFP-1059

--- acm-standard-applications/arkcase/src/main/webapp/resources/modules/consultations/controllers/components/consultation-new-consultation-modal.client.controller.js
+++ acm-standard-applications/arkcase/src/main/webapp/resources/modules/consultations/controllers/components/consultation-new-consultation-modal.client.controller.js
@@ -15,7 +15,6 @@ angular.module('consultations').controller(
             $scope.isPickExistingPerson = false;
             $scope.uploadFiles = [];
             $scope.primaryAddressIndex = 0;
-
             $scope.config = null;
             
             var moduleConfig = ConfigService.getModuleConfig("consultations");
@@ -81,8 +80,8 @@ angular.module('consultations').controller(
                 $scope.config.data.originator.person.addresses[0].type = defaultAddressType ? defaultAddressType.key : addressTypes[0].key;
 
                 $scope.config.data.organizationAssociations = [];
-                $scope.config.data.receivedDate = moment.utc().format("YYYY-MM-DDTHH:mm:ss.sss");
-                $scope.config.data.dueDate = moment.utc().format("YYYY-MM-DDTHH:mm:ss.sss");
+                $scope.minDueDate = moment($scope.config.data.receivedDate);
+                $scope.maxReceivedDate = moment(new Date());
 
                 $scope.consultationPeopleConfig = _.find(moduleConfig.components, {
                     id: "people"
@@ -121,20 +120,13 @@ angular.module('consultations').controller(
 
             
 
-            $scope.receivedDateChanged = function () {
-                var todayDate = moment.utc().format("YYYY-MM-DDTHH:mm:ss.sss");
-                if (Util.isEmpty($scope.config.data.receivedDate) || moment($scope.config.data.receivedDate).isAfter(todayDate)) {
-                    $scope.config.data.receivedDate = todayDate;
+            $scope.receivedDateChanged = function (data) {
+                if ($scope.config && $scope.config.data &&
+                    moment($scope.config.data.receivedDate).isAfter($scope.config.data.dueDate)) {
+                    $scope.config.data.dueDate = data.dateInPicker;
+                    $scope.dateChangedManually = true;
                 }
-            };
-
-            $scope.dueDateChanged = function() {
-                var todayDate = moment.utc().format("YYYY-MM-DDTHH:mm:ss.sss");
-                if(Util.isEmpty($scope.config.data.dueDate) || moment($scope.config.data.dueDate).isBefore($scope.config.data.receivedDate)) {
-                    $scope.config.data.dueDate = todayDate;
-                } else {
-                    $scope.config.data.dueDate = $scope.config.data.dueDate;
-                }
+                $scope.minDueDate = moment($scope.config.data.receivedDate);
             };
 
             $scope.pickExistingUserChange = function () {
