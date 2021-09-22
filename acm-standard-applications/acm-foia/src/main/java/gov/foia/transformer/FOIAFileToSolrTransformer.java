0bda39fade5446fcd81bc8128082d6577e6abd36
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-09-22 13:32:48 +0000

    Merge branch 'ACFP-1025_adding_madePublicDate_field' into 'develop'
    ACFP-1025 adding made public field
    
    Closes ACFP-1025
    
    See merge request arkcase/ArkCase!3492
    
    (cherry picked from commit aa7f4d057eb2b3ea4aaa5d0f57ce96ee0c12525f)
    
    3f1eb513 WIP: ACFP-1025 adding made public field
    81697734 Merge branch 'develop' of gitlab.armedia.com:arkcase/ArkCase into...
    7e5576df 1025 adding madePublicDate additional changes
    c17873e2 fixing formatting
    4d417a43 no message
    d5cfa0ca Adding data updater that would set MadePublicDate to lastModifiedDate
    00ef3d5e Adding solr file reindex

--- acm-standard-applications/acm-foia/src/main/java/gov/foia/transformer/FOIAFileToSolrTransformer.java
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/transformer/FOIAFileToSolrTransformer.java
@@ -31,15 +31,13 @@ import com.armedia.acm.plugins.ecm.model.EcmFile;
 import com.armedia.acm.plugins.ecm.service.EcmFileToSolrTransformer;
 import com.armedia.acm.services.search.model.solr.SolrAdvancedSearchDocument;
 import com.armedia.acm.services.search.model.solr.SolrContentDocument;
-
+import gov.foia.model.FOIAEcmFileVersion;
+import gov.foia.model.FOIAFile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 import java.util.Optional;
-
-import gov.foia.model.FOIAEcmFileVersion;
-import gov.foia.model.FOIAFile;
 
 public class FOIAFileToSolrTransformer extends EcmFileToSolrTransformer
 {
@@ -109,6 +107,7 @@ public class FOIAFileToSolrTransformer extends EcmFileToSolrTransformer
     private void mapRequestProperties(FOIAFile file, Map<String, Object> additionalProperties)
     {
         additionalProperties.put("public_flag_b", file.getPublicFlag());
+        additionalProperties.put("made_public_date_tdt", file.getMadePublicDate());
 
         Optional<FOIAEcmFileVersion> activeFileVersion = file.getVersions()
                 .stream()
