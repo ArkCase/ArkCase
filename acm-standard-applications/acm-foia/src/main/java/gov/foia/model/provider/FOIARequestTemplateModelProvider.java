ca9aabec74bf68eb63e980e755cf493c45960112
martin.bozinovski <bozinovskimartin@gmail.com>
2021-09-22 07:57:48 +0200

    ACFP-1126 Add exemption codes to foiaRequestTemplateModelProvider

--- acm-standard-applications/acm-foia/src/main/java/gov/foia/model/provider/FOIARequestTemplateModelProvider.java
+++ acm-standard-applications/acm-foia/src/main/java/gov/foia/model/provider/FOIARequestTemplateModelProvider.java
@@ -29,16 +29,28 @@ package gov.foia.model.provider;
 
 import com.armedia.acm.core.model.ApplicationConfig;
 import com.armedia.acm.core.provider.TemplateModelProvider;
+import com.armedia.acm.correspondence.model.FormattedMergeTerm;
+import com.armedia.acm.correspondence.model.FormattedRun;
 import com.armedia.acm.plugins.objectassociation.dao.ObjectAssociationDao;
 import com.armedia.acm.plugins.objectassociation.model.ObjectAssociation;
 import com.armedia.acm.plugins.profile.model.UserOrg;
 import com.armedia.acm.plugins.profile.service.UserOrgService;
+import com.armedia.acm.services.config.lookups.model.StandardLookupEntry;
+import com.armedia.acm.services.config.lookups.service.LookupDao;
+import com.armedia.acm.services.exemption.exception.GetExemptionCodeException;
+import com.armedia.acm.services.exemption.model.ExemptionCode;
 import com.armedia.acm.services.users.dao.UserDao;
 import com.armedia.acm.services.users.model.AcmUser;
 import gov.foia.model.FOIARequest;
 import gov.foia.model.FOIARequestModel;
+import gov.foia.service.FOIAExemptionService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author darko.dimitrievski
@@ -50,7 +62,10 @@ public class FOIARequestTemplateModelProvider implements TemplateModelProvider<F
     private ApplicationConfig applicationConfig;
     private UserDao userDao;
     private UserOrgService userOrgService;
-    
+    private FOIAExemptionService foiaExemptionService;
+    private LookupDao lookupDao;
+    private transient final Logger LOG = LogManager.getLogger(getClass());
+
     @Override
     public FOIARequestModel getModel(Object foiaRequest)
     {
@@ -74,9 +89,42 @@ public class FOIARequestTemplateModelProvider implements TemplateModelProvider<F
             }
             request.setAssigneeFullName(assignee.getFirstName() + " " + assignee.getLastName());
         }
+
+        List<ExemptionCode> exemptionCodes;
+        try
+        {
+            exemptionCodes = foiaExemptionService.getExemptionCodes(request.getId(), request.getObjectType());
+        }
+        catch (GetExemptionCodeException e)
+        {
+            LOG.warn("Failed to fetch exemption codes for object with type [{}] and id [{}]", request.getObjectType(), request.getId());
+            exemptionCodes = new ArrayList<>();
+        }
+        String exemptionCodesNames = exemptionCodes.stream()
+                .map(ExemptionCode::getExemptionCode)
+                .collect(Collectors.joining("and"));
+        FormattedMergeTerm exemptionCodesAndDescription = new FormattedMergeTerm();
+        List<FormattedRun> runs = getExemptionCodesAndDescriptionRuns(exemptionCodes);
+        exemptionCodesAndDescription.setRuns(runs);
+
         FOIARequestModel requestModel = new FOIARequestModel();
+        requestModel.setExemptionCodesAndDescription(exemptionCodesAndDescription);
+        requestModel.setExemptionCodeSummary(exemptionCodesNames);
         requestModel.setRequest(request);
         return requestModel;
+    }
+
+    public List<FormattedRun> getExemptionCodesAndDescriptionRuns(List<ExemptionCode> exemptionCodes)
+    {
+        List<StandardLookupEntry> lookupEntries = (List<StandardLookupEntry>) getLookupDao().getLookupByName("annotationTags").getEntries();
+        Map<String, String> codeDescriptions = lookupEntries.stream()
+                .collect(Collectors.toMap(StandardLookupEntry::getKey, StandardLookupEntry::getValue));
+        List<FormattedRun> runs = new ArrayList<>();
+        for (ExemptionCode exCode : exemptionCodes)
+        {
+            foiaExemptionService.createAndStyleRunsForCorrespondenceLetters(codeDescriptions, runs, exCode);
+        }
+        return runs;
     }
 
     @Override
@@ -119,4 +167,23 @@ public class FOIARequestTemplateModelProvider implements TemplateModelProvider<F
         this.userOrgService = userOrgService;
     }
 
+    public LookupDao getLookupDao()
+    {
+        return lookupDao;
+    }
+
+    public void setLookupDao(LookupDao lookupDao)
+    {
+        this.lookupDao = lookupDao;
+    }
+
+    public FOIAExemptionService getFoiaExemptionService()
+    {
+        return foiaExemptionService;
+    }
+
+    public void setFoiaExemptionService(FOIAExemptionService foiaExemptionService)
+    {
+        this.foiaExemptionService = foiaExemptionService;
+    }
 }
