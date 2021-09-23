e836203fd87bd56e5b97d858b099967bf1b894f3
ARMEDIA\gabriela.miteva <gabriela.miteva@armedia.com>
2021-09-23 13:55:40 +0200

    Case Files Reindex

---
+++ acm-services/acm-service-data-update/src/main/java/com/armedia/acm/services/dataupdate/service/SolrReindexCaseFileExecutor.java
@@ -0,0 +1,59 @@
+package com.armedia.acm.services.dataupdate.service;
+
+/*-
+ * #%L
+ * ACM Service: Data Update Service
+ * %%
+ * Copyright (C) 2014 - 2021 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software. 
+ * 
+ * If the software was purchased under a paid ArkCase license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.armedia.acm.plugins.casefile.model.CaseFile;
+
+import java.util.Arrays;
+
+public class SolrReindexCaseFileExecutor implements AcmDataUpdateExecutor {
+
+    private SolrReindexService solrReindexService;
+
+    @Override
+    public String getUpdateId()
+    {
+        return "solr_caseFile_reindex_v2";
+    }
+
+    @Override
+    public void execute()
+    {
+        solrReindexService.reindex(Arrays.asList(CaseFile.class));
+    }
+
+    public SolrReindexService getSolrReindexService()
+    {
+        return solrReindexService;
+    }
+
+    public void setSolrReindexService(SolrReindexService solrReindexService)
+    {
+        this.solrReindexService = solrReindexService;
+    }
+}
