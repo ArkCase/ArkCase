e25728c7a5b39859c0e2be07054b29e0cfdae523
Bojan Milenkoski <bojan.milenkoski@armedia.com>
2021-11-23 15:08:06 +0000

    Merge branch 'ACFP-1336-Remove_change_of_modifier_on_manually_triggered_reindexing' into 'develop'
    ACFP-1336 Remove change of modifier on manually made modified date changes to...
    
    Closes ACFP-1336
    
    See merge request arkcase/ArkCase!3665
    
    (cherry picked from commit 385c345e8ac2f5ca728157a55cbf56983b841ad2)
    
    26ab56f0 ACFP-1336 Remove change of modifier on manually made modified date changes to...

--- acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AuditPropertyEntityAdapter.java
+++ acm-tool-integrations/acm-spring-data-source/src/main/java/com/armedia/acm/data/AuditPropertyEntityAdapter.java
@@ -6,33 +6,33 @@ package com.armedia.acm.data;
  * %%
  * Copyright (C) 2014 - 2018 ArkCase LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.persistence.descriptors.DescriptorEvent;
 import org.eclipse.persistence.descriptors.DescriptorEventAdapter;
 import org.eclipse.persistence.mappings.DatabaseMapping;
 import org.eclipse.persistence.sessions.Record;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 import java.util.Date;
 
@@ -139,12 +139,22 @@ public class AuditPropertyEntityAdapter extends DescriptorEventAdapter
             String modifier = getDatabaseColumnName(event, AcmEntity.MODIFIER_PROPERTY_NAME);
             if (modifier != null)
             {
-                record.put(modifier, getUserId());
+                /*
+                We are doing this to not change the modifier on manually triggered updates (which are done to update SOLR data) ACFP-1336.
+                All other changes outside of just changing the modified date manually will result with a change in the modifier.
+                */
+                boolean hasMeaningfulChanges = record.size() > 1 || record.get(modified) == null;
+
+                if (hasMeaningfulChanges)
+                {
+                    record.put(modifier, getUserId());
+                    entity.setModifier(user);
+                }
+
             }
 
             // note, we still have to update the object itself, so the client will have the right values
             entity.setModified(today);
-            entity.setModifier(user);
         }
         else
         {
